022a8553c9369cb5957af6f2560e3103e253936a
Wang Yanzhang <i8run15@gmail.com>
2016-09-20 17:22:35 +0800

    support for mkl dnn api, which is migrated from WebscaleML.

---
+++ dl/src/main/scala/com/intel/analytics/sparkdl/nn/mkl/SpatialConvolution.scala
@@ -0,0 +1,337 @@
+package com.intel.analytics.sparkdl.nn.mkl
+
+import com.intel.analytics.sparkdl.mkl.MKL
+import com.intel.analytics.sparkdl.nn.Module
+import com.intel.analytics.sparkdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.sparkdl.tensor._
+import com.intel.analytics.sparkdl.utils.RandomGenerator._
+
+import scala.language.implicitConversions
+
+import com.intel.analytics.sparkdl.nn.InitializationMethod
+import com.intel.analytics.sparkdl.nn.Default
+import com.intel.analytics.sparkdl.nn.Xavier
+
+import scala.reflect.ClassTag
+
+class SpatialConvolution[@specialized(Float, Double) T:ClassTag] (
+                                                                   val nInputPlane : Int, // The number of expected input planes in the image given into forward()
+                                                                   val nOutputPlane : Int, // The number of output planes the convolution layer will produce.
+                                                                   val kernelWidth : Int, // The kernel width of the convolution
+                                                                   val kernelHeight : Int, // The kernel height of the convolution
+                                                                   val strideWidth : Int = 1, // The step of the convolution in the width dimension.
+                                                                   val strideHeight : Int = 1, //The step of the convolution in the height dimension
+                                                                   val padWidth : Int = 0, // The additional zeros added per width to the input planes. A good number is (kW-1)/2.
+                                                                   val padHeight : Int = 0, // The additional zeros added per height to the input planes. A good number is (kH-1)/2.
+                                                                   val needCompute : Boolean = true,
+                                                                   val groups: Int = 1,
+                                                                   private var initMethod: InitializationMethod = Default
+                                                                 )(implicit ev: TensorNumeric[T]) extends Module[T] {
+  val weight : Tensor[T] = Tensor[T](nOutputPlane, nInputPlane, kernelHeight, kernelWidth)
+  val bias : Tensor[T]   = Tensor[T](nOutputPlane)
+  this.gradInput        = Tensor[T](nOutputPlane, nInputPlane, kernelHeight, kernelWidth)
+  this.gradBias          = Tensor[T](nOutputPlane)
+  this.gradWeight         = Tensor[T](nOutputPlane, nInputPlane, kernelHeight, kernelWidth)
+  val fInput             = Tensor[T]()
+  val fGradInput         = Tensor[T]()
+  reset()
+
+  private var im2colTime = 0L
+  private var col2imTime = 0L
+
+  var classPtr = 0L
+  private var firstPass = true
+
+  override def getClassPtr(): Long = classPtr
+
+  def getIm2ColTime() = im2colTime
+  def getCol2ImgTime() = col2imTime
+
+  def setInitMethod(initMethod: InitializationMethod): this.type = {
+    this.initMethod = initMethod
+    this
+  }
+
+  // this is pointer to the layout of MKL used internal and the memory is allocated in native code.
+  // the magic codes are:
+  // layoutMKL(0) -> input
+  // layoutMKL(1) -> inputDiff / gradInput
+  // layoutMKL(2) -> output
+  // layoutMKL(3) -> outputDiff
+  // layoutMKL(4) -> kernel / filter
+  // layoutMKL(5) -> kernelDiff / gradWeight
+  // layoutMKL(6) -> bias
+  // layoutMKL(7) -> biasDiff / gradBias
+  val layoutMKL = Array.fill[Long](10)(-1)
+
+  override def reset(): Unit ={
+    val stdv = 1.0 /math.sqrt(kernelWidth * kernelHeight * nInputPlane)
+    weight.apply1(_=>ev.fromType[Double](RNG.uniform(0,1)*2*stdv - stdv)) //todo, better to support uniform
+    bias.apply1(_=>ev.fromType[Double](RNG.uniform(0,1)*2*stdv - stdv))
+  }
+
+  override def updateOutput(input: Tensor[T]): Tensor[T] = {
+    //var time = System.nanoTime()
+    require(input.dim() == 3 || input.dim() == 4, "Only support 3D or 4D(batch mode) input")
+    // TODO the requirement of contiguous input may be not necessary for MKL 2017.
+    //      because it supports the api of groups convolution.
+    require(input.isContiguous(), "input is not contiguous")
+
+    // compute the output height and width
+    def computeOut(input:Int, pad:Int, kernel:Int, stride:Int): Int = {
+      (input + 2 * pad - kernel) / stride + 1
+    }
+
+    // +---------+-------+-------+
+    // |         | 3-dim | 4-dim |
+    // +=========+=======+=======+
+    // | Number  | ?     | 1     |
+    // +---------+-------+-------+
+    // | Channel | 1     | 2     |
+    // +---------+-------+-------+
+    // | Height  | 2     | 3     |
+    // +---------+-------+-------+
+    // | Width   | 3     | 4     |
+    // +---------+-------+-------+
+    // Table: Index of 3-dim/4-dim input
+
+    /*
+    for (i <- 1 to input.dim()) printf("%d\t", input.size(i))
+    println("")
+    for (i <- 1 to input.dim()) printf("%d\t", input.stride(i))
+    println("")
+    */
+
+    val inputWidth = input.size(input.dim())
+    val inputHeight = input.size(input.dim() - 1)
+    val inputChannel = input.size(input.dim() - 2)
+    // TODO we may set input.size(input.dim() - 3) == 1 if input.dim() == 3
+    val inputNumber = if (input.dim() == 3) 1 else input.size(input.dim() - 3)
+
+    // output number is as same as input number
+    val outputNumber = inputNumber
+    val outputChannel = nOutputPlane
+    val outputWidth = computeOut(inputWidth, padWidth, kernelWidth, strideWidth)
+    val outputHeight = computeOut(inputHeight, padHeight, kernelHeight, strideHeight)
+
+    require(outputWidth >= 1 && outputHeight >= 1, "output size is too small")
+    if (input.dim() == 3)
+      output.resize(Array(outputChannel, outputHeight, outputWidth))
+    else
+      output.resize(Array(outputNumber, outputChannel, outputHeight, outputWidth))
+
+    // kernel number and bias number are as same as nOutputPlane
+    val biasNumber   = nOutputPlane
+    val kernelNumber = nOutputPlane
+    // TODO kernel channel equals to input channel now
+    val kernelChannel = inputChannel
+
+    val inputOffset  = input.storageOffset()  - 1
+    val outputOffset = output.storageOffset() - 1
+    val biasOffset   = bias.storageOffset()   - 1
+    val kernelOffset = weight.storageOffset() - 1
+
+    if (firstPass) {
+      ev.getType() match {
+        case "Double" => classPtr = MKL.ConvolutionInitDouble(
+          inputNumber, inputChannel, inputHeight, inputWidth,
+          kernelNumber, kernelChannel, kernelHeight, kernelWidth, strideHeight, strideWidth, padHeight,
+          padWidth, 4, groups)
+        case "Float" => classPtr = MKL.ConvolutionInitFloat(
+          inputNumber, inputChannel, inputHeight, inputWidth,
+          kernelNumber, kernelChannel, kernelHeight, kernelWidth, strideHeight, strideWidth, padHeight,
+          padWidth, 4, groups)
+        case _ => throw new UnsupportedOperationException(s"Only Float supported")
+      }
+      firstPass = false
+    }
+
+    implicit def bool2int(b:Boolean) = if (b) 1 else 0
+    val start = System.nanoTime()
+    ev.getType() match {
+      case "Double" => MKL.ConvolutionForwardDouble(
+        input.storage().array().asInstanceOf[Array[Double]], inputOffset,
+        output.storage().array().asInstanceOf[Array[Double]], outputOffset,
+        weight.storage().array().asInstanceOf[Array[Double]], kernelOffset,
+        bias.storage().array().asInstanceOf[Array[Double]], biasOffset,
+        classPtr
+      )
+      case "Float" => MKL.ConvolutionForwardFloat(
+        input.storage().array().asInstanceOf[Array[Float]], inputOffset,
+        output.storage().array().asInstanceOf[Array[Float]], outputOffset,
+        weight.storage().array().asInstanceOf[Array[Float]], kernelOffset,
+        bias.storage().array().asInstanceOf[Array[Float]], biasOffset,
+        classPtr
+      )
+
+      case _ => throw new UnsupportedOperationException(s"Only Float supported")
+    }
+    //println("[SCALA] spatialconvolution forward call JNI " + (System.nanoTime() - start) / 1e6)
+
+    output
+  }
+
+  override def updateGradInput(input: Tensor[T], gradOutput: Tensor[T]) : Tensor[T] = {
+    require(input.nDimension() == 3 || input.nDimension() == 4, "Only support 3D or 4D input")
+    require(nOutputPlane == (if (input.nDimension() == 3) gradOutput.size(1) else gradOutput.size(2)),
+      "Number of output features is not equal to nOutputPlane")
+    require(input.isContiguous(), "input is not contiguous")
+    require(gradInput.isContiguous(), "gradInput is not contiguous")
+    gradInput.resizeAs(input)
+
+    val gradInputOffset = gradInput.storageOffset() - 1
+    val gradKernelOffset = gradWeight.storageOffset() - 1
+    val gradOutputOffset = gradOutput.storageOffset() - 1
+    val gradBiasOffset = gradBias.storageOffset() - 1
+
+    // +---------+-------+-------+
+    // |         | 3-dim | 4-dim |
+    // +=========+=======+=======+
+    // | Number  | ?     | 1     |
+    // +---------+-------+-------+
+    // | Channel | 1     | 2     |
+    // +---------+-------+-------+
+    // | Height  | 2     | 3     |
+    // +---------+-------+-------+
+    // | Width   | 3     | 4     |
+    // +---------+-------+-------+
+    // Table: Index of 3-dim/4-dim input
+
+    val inputWidth = input.size(input.dim())
+    val inputHeight = input.size(input.dim() - 1)
+    val inputChannel = input.size(input.dim() - 2)
+    // TODO we may set input.size(input.dim() - 3) == 1 if input.dim() == 3
+    val inputNumber = if (input.dim() == 3) 1 else input.size(input.dim() - 3)
+
+    val kernelNumber = nOutputPlane
+    val kernelChannel = inputChannel
+
+    val inputOffset = input.storageOffset() - 1
+    val biasOffset = bias.storageOffset() - 1
+    val kernelOffset = weight.storageOffset() - 1
+
+    implicit def bool2int(b:Boolean) = if (b) 1 else 0
+    val start = System.nanoTime()
+    if (needCompute) {
+      ev.getType() match {
+        case "Double" => MKL.ConvolutionBackwardDataDouble(
+          input.storage().array().asInstanceOf[Array[Double]], inputOffset,
+          gradOutput.storage().array().asInstanceOf[Array[Double]], gradOutputOffset,
+          gradInput.storage().array().asInstanceOf[Array[Double]], gradInputOffset,
+          weight.storage().array().asInstanceOf[Array[Double]], kernelOffset,
+          bias.storage().array().asInstanceOf[Array[Double]], biasOffset, classPtr
+        )
+        case "Float" => MKL.ConvolutionBackwardDataFloat(
+          input.storage().array().asInstanceOf[Array[Float]], inputOffset,
+          gradOutput.storage().array().asInstanceOf[Array[Float]], gradOutputOffset,
+          gradInput.storage().array().asInstanceOf[Array[Float]], gradInputOffset,
+          weight.storage().array().asInstanceOf[Array[Float]], kernelOffset,
+          bias.storage().array().asInstanceOf[Array[Float]], biasOffset, classPtr
+        )
+
+        case _ => throw new UnsupportedOperationException(s"Only Float/Double supported")
+      }
+    }
+    ev.getType() match {
+      case "Double" =>
+        MKL.ConvolutionBackwardKernelDouble(
+          input.storage().array().asInstanceOf[Array[Double]], inputOffset,
+          gradOutput.storage().array().asInstanceOf[Array[Double]], gradOutputOffset,
+          gradWeight.storage().array().asInstanceOf[Array[Double]], gradKernelOffset,
+          weight.storage().array().asInstanceOf[Array[Double]], kernelOffset,
+          bias.storage().array().asInstanceOf[Array[Double]], biasOffset, classPtr
+        )
+      case "Float" =>
+        MKL.ConvolutionBackwardKernelFloat(
+          input.storage().array().asInstanceOf[Array[Float]], inputOffset,
+          gradOutput.storage().array().asInstanceOf[Array[Float]], gradOutputOffset,
+          gradWeight.storage().array().asInstanceOf[Array[Float]], gradKernelOffset,
+          weight.storage().array().asInstanceOf[Array[Float]], kernelOffset,
+          bias.storage().array().asInstanceOf[Array[Float]], biasOffset, classPtr
+        )
+      case _ => throw new UnsupportedOperationException(s"Only Float/Double supported")
+    }
+    ev.getType() match {
+      case "Double" =>
+        MKL.ConvolutionBackwardBiasDouble(
+          input.storage().array().asInstanceOf[Array[Double]], inputOffset,
+          gradOutput.storage().array().asInstanceOf[Array[Double]], gradOutputOffset,
+          gradBias.storage().array().asInstanceOf[Array[Double]], gradBiasOffset,
+          weight.storage().array().asInstanceOf[Array[Double]], kernelOffset,
+          bias.storage().array().asInstanceOf[Array[Double]], biasOffset, classPtr
+        )
+
+      case "Float" =>
+        MKL.ConvolutionBackwardBiasFloat(
+          input.storage().array().asInstanceOf[Array[Float]], inputOffset,
+          gradOutput.storage().array().asInstanceOf[Array[Float]], gradOutputOffset,
+          gradBias.storage().array().asInstanceOf[Array[Float]], gradBiasOffset,
+          weight.storage().array().asInstanceOf[Array[Float]], kernelOffset,
+          bias.storage().array().asInstanceOf[Array[Float]], biasOffset, classPtr
+        )
+
+      case _ => throw new UnsupportedOperationException(s"Only Float/Double supported")
+    }
+    //println("[SCALA] spatialconvolution backward call JNI " + (System.nanoTime() - start) / 1e6)
+    gradInput
+  }
+
+  override def updateParameters(learningRate:T): Unit ={
+    weight.map(gradWeight, (a, b)=>ev.minus(a, ev.times(learningRate,b)))
+    bias.map(gradBias,(a,b)=>ev.minus(a, ev.times(learningRate,b)))
+  }
+
+  override def zeroGradParameters(): Unit = {
+    gradWeight.zero()
+    gradBias.zero()
+  }
+
+  override  def parameters(): (Array[Tensor[T]], Array[Tensor[T]]) ={
+    (Array(this.weight, this.bias), Array(this.gradWeight, this.gradBias))
+  }
+
+  override def equals(obj : Any) : Boolean = {
+    if(!super.equals(obj)) {
+      return false
+    }
+
+    if(!obj.isInstanceOf[SpatialConvolution[T]])
+      return false
+    val other = obj.asInstanceOf[SpatialConvolution[T]]
+    if(this.eq(other))
+      return true
+
+    nInputPlane == other.nInputPlane &&
+      nOutputPlane == other.nOutputPlane &&
+      kernelWidth == other.kernelWidth &&
+      kernelHeight == other.kernelHeight &&
+      strideWidth == other.strideWidth &&
+      strideHeight == other.strideHeight &&
+      padWidth == other.padWidth &&
+      padHeight == other.padHeight &&
+      weight == other.weight &&
+      bias == other.bias &&
+      gradWeight == other.gradWeight &&
+      gradBias == other.gradBias
+  }
+
+  override def toString() : String = {
+    s"mkl.SpatialConvolution($nInputPlane -> $nOutputPlane, $kernelWidth x $kernelHeight, $strideWidth, $strideHeight, $padWidth, $padHeight)"
+  }
+
+  override def findModel(paramOffset : Int, indexes : Array[Int]) : (Module[T], Int, Array[Int]) = {
+    (this, paramOffset - nOutputPlane * nInputPlane * kernelHeight * kernelWidth - nOutputPlane, indexes)
+  }
+
+  /*mkl-dnn's convolution_backward has done updateGradInput and accGradParameters, so accGradParameters does nothing
+   *
+  override def updateGradInput(input: Tensor[T], gradOutput: Tensor[T]): Tensor[T] = {
+    backward(input, gradOutput)
+  }
+    */
+
+  override def accGradParameters(input: Tensor[T], gradOutput: Tensor[T], scale: Double = 1.0): Unit = {
+
+  }
+}
+
---
+++ mkl/native/src/main/c/jni/relu.cpp
@@ -0,0 +1,288 @@
+#include <jni.h>
+
+#include "debug.h"
+#include "layer.h"
+#include "memory.h"
+#include "utils.h"
+
+template <typename DType>
+class MKLReLU : public MKLLayer<DType>
+{
+ public:
+  MKLReLU();
+  ~MKLReLU();
+
+  void init(size_t inputNumber, size_t inputChannel, size_t inputHeight,
+            size_t inputWidth, int dimension);
+
+  void updateOutput(DType *input, DType *output);
+  void updateGradInput(DType *input, DType *gradOutput, DType *gradInput);
+
+ private:
+  // this method is not the same as createMklLayout in MKLMemory
+  void firstPass();
+  void preExecute(DType *input);
+
+  size_t inputSize[4];
+  size_t inputStrides[4];
+
+  size_t outputSize[4];
+  size_t outputStrides[4];
+
+  DType nagtiveSlope;
+};
+
+template <typename DType>
+MKLReLU<DType>::MKLReLU()
+{
+  nagtiveSlope = static_cast<DType>(0.0);
+}
+
+template <typename DType>
+MKLReLU<DType>::~MKLReLU()
+{
+}
+
+template <typename DType>
+void MKLReLU<DType>::init(size_t inputNumber, size_t inputChannel,
+                          size_t inputHeight, size_t inputWidth, int dimension)
+{
+  this->dimension = dimension;
+
+  inputSize[0] = inputWidth;
+  inputSize[1] = inputHeight;
+  inputSize[2] = inputChannel;
+  inputSize[3] = inputNumber;
+
+  inputStrides[0] = 1;
+  for (int i        = 1; i < 4; i++)
+    inputStrides[i] = inputStrides[i - 1] * inputSize[i - 1];
+
+  // the output channel is as same as the number of kernel.
+  // and the output number must be as same as the number of input too.
+  outputSize[0] = inputWidth;
+  outputSize[1] = inputHeight;
+  outputSize[2] = inputChannel;
+  outputSize[3] = inputNumber;
+
+  outputStrides[0] = 1;
+  for (int i         = 1; i < 4; i++)
+    outputStrides[i] = outputStrides[i - 1] * outputSize[i - 1];
+
+  // create usr layout
+  this->input->createUsrLayout(dimension, inputSize, inputStrides);
+  this->output->createUsrLayout(dimension, outputSize, outputStrides);
+
+  this->gradInput->createUsrLayout(dimension, inputSize, inputStrides);
+  this->gradOutput->createUsrLayout(dimension, outputSize, outputStrides);
+}
+
+template <typename DType>
+void MKLReLU<DType>::firstPass()
+{
+  dnnError_t status = E_UNIMPLEMENTED;
+  dnnLayout_t layout;
+
+  status =
+      dnnLayoutCreate<DType>(&layout, this->dimension, inputSize, inputStrides);
+  CHECK_EQ(status, E_SUCCESS);
+
+  // forward
+  status = dnnReLUCreateForward<DType>(&(this->forwardPrim), NULL, layout,
+                                       nagtiveSlope);
+  CHECK_EQ(status, E_SUCCESS);
+
+  this->input->createMklLayout(this->forwardPrim, dnnResourceSrc);
+  this->output->createMklLayout(this->forwardPrim, dnnResourceDst);
+
+  // backward data
+  // the input layout is as same as input diff layout
+  status = dnnReLUCreateBackward<DType>(&(this->backwardPrim), NULL, layout,
+                                        layout, nagtiveSlope);
+  CHECK_EQ(status, E_SUCCESS);
+
+  this->gradOutput->createMklLayout(this->backwardPrim, dnnResourceDiffDst);
+  this->gradInput->createMklLayout(this->backwardPrim, dnnResourceDiffSrc);
+
+  // we create the layout only at the first time
+  this->isFirstPass = false;
+}
+
+template <typename DType>
+void MKLReLU<DType>::preExecute(DType *input)
+{
+  this->input->createConversion();
+}
+
+template <typename DType>
+void MKLReLU<DType>::updateOutput(DType *input, DType *output)
+{
+  if (this->isFirstPass) firstPass();
+
+  // Because the address will change every time, so we need create conversion
+  // every forward/backward.
+  // TODO Should we set the kernel and bias address every time?
+  preExecute(input);
+  this->output->createConversion();
+
+#ifdef DEBUG
+  printData<DType>(reinterpret_cast<DType *>(this->input->getUsrData()),
+                   this->inputSize[3], this->inputSize[2], this->inputSize[1],
+                   this->inputSize[0], "Forward input");
+#endif
+
+  dnnError_t status;
+  void *resources[dnnResourceNumber];
+
+  resources[dnnResourceSrc] = this->input->getConvertedData();
+  resources[dnnResourceDst] = this->output->getData();
+
+  PERFSTART();
+  status = dnnExecute<DType>(this->forwardPrim, resources);
+  PERFEND("main computing");
+  CHECK_EQ(status, E_SUCCESS);
+
+#ifdef DEBUG
+  printData<DType>(reinterpret_cast<DType *>(this->output->getData()),
+                   outputSize[3], outputSize[2], outputSize[1], outputSize[0],
+                   "Forward output");
+#endif
+
+  if (!this->output->isUseNext()) {
+    this->output->backToUsr();
+  }
+}
+
+template <typename DType>
+void MKLReLU<DType>::updateGradInput(DType *input, DType *gradOutput,
+                                     DType *gradInput)
+{
+  dnnError_t status;
+  void *resources[dnnResourceNumber];
+
+  preExecute(input);
+
+  this->gradOutput->createConversion();
+  this->gradInput->createConversion();
+
+  resources[dnnResourceDiffDst] = this->gradOutput->getConvertedData();
+  resources[dnnResourceDiffSrc] = this->gradInput->getData();
+  resources[dnnResourceSrc]     = this->input->getConvertedData();
+
+  // 4. main computing parts.
+  PERFSTART();
+  status = dnnExecute<DType>(this->backwardPrim, resources);
+  CHECK_EQ(status, E_SUCCESS);
+  PERFEND("main computing");
+
+  if (!this->gradInput->isUsePrev()) {
+    this->gradInput->backToUsr();
+  }
+
+#ifdef DEBUG
+  printData<DType>(reinterpret_cast<DType *>(this->gradInput->getUsrData()),
+                   inputSize[3], inputSize[2], inputSize[1], inputSize[0],
+                   "backward gradient input");
+#endif
+}
+
+template <typename ArrayType, typename DType>
+jlong JNIReLUInit(JNIEnv *env, jclass thisClass, jint inputNumber,
+                  jint inputChannel, jint inputHeight, jint inputWidth,
+                  jint dimension)
+{
+  MKLReLU<DType> *ptr = new MKLReLU<DType>();
+  ptr->init(inputNumber, inputChannel, inputHeight, inputWidth, dimension);
+
+  return reinterpret_cast<long>(ptr);
+}
+
+template <typename ArrayType, typename DType>
+void JNIReLUUpdateOutput(JNIEnv *env, jclass thisClass, ArrayType input,
+                         jint inputOffset, ArrayType output, jint outputOffset,
+                         long classPtr)
+{
+  MKLReLU<DType> *ptr = reinterpret_cast<MKLReLU<DType> *>(classPtr);
+
+  std::shared_ptr<ZipArray<ArrayType, DType>> jInput(
+      new ZipArray<ArrayType, DType>(env, input, inputOffset, ptr->input));
+
+  std::shared_ptr<ZipArray<ArrayType, DType>> jOutput(
+      new ZipArray<ArrayType, DType>(env, output, outputOffset, ptr->output));
+
+  ptr->updateOutput(jInput->getPtr(), jOutput->getPtr());
+}
+
+template <typename ArrayType, typename DType>
+void JNIReLUUpdateGradInput(JNIEnv *env, jclass thisClass, ArrayType input,
+                            jint inputOffset, ArrayType outputDiff,
+                            jint outputDiffOffset, ArrayType inputDiff,
+                            jint inputDiffOffset, long classPtr)
+{
+  MKLReLU<DType> *ptr = reinterpret_cast<MKLReLU<DType> *>(classPtr);
+  std::shared_ptr<ZipArray<ArrayType, DType>> jInput(
+      new ZipArray<ArrayType, DType>(env, input, inputOffset, ptr->input));
+
+  std::shared_ptr<ZipArray<ArrayType, DType>> jOutputDiff(
+      new ZipArray<ArrayType, DType>(env, outputDiff, outputDiffOffset,
+                                     ptr->gradOutput));
+
+  std::shared_ptr<ZipArray<ArrayType, DType>> jInputDiff(
+      new ZipArray<ArrayType, DType>(env, inputDiff, inputDiffOffset,
+                                     ptr->gradInput));
+
+  ptr->updateGradInput(jInput->getPtr(), jOutputDiff->getPtr(),
+                       jInputDiff->getPtr());
+}
+
+// Macro
+#define ReLUInit(DType, JType, JArrayType)                                \
+  JNIEXPORT                                                               \
+  jlong JNICALL Java_com_intel_analytics_sparkdl_mkl_MKL_ReLUInit##DType( \
+      JNIEnv *env, jclass thisClass, jint inputNumber, jint inputChannel, \
+      jint inputHeight, jint inputWidth, jint dimension)                  \
+  {                                                                       \
+    return JNIReLUInit<JArrayType, JType>(env, thisClass, inputNumber,    \
+                                          inputChannel, inputHeight,      \
+                                          inputWidth, dimension);         \
+  }
+
+#define ReLUForward(DType, JType, JArrayType)                                  \
+  JNIEXPORT                                                                    \
+  void JNICALL Java_com_intel_analytics_sparkdl_mkl_MKL_ReLUForward##DType(    \
+      JNIEnv *env, jclass thisClass, JArrayType input, jint inputOffset,       \
+      JArrayType output, jint outputOffset, long classPtr)                     \
+  {                                                                            \
+    JNIReLUUpdateOutput<JArrayType, JType>(env, thisClass, input, inputOffset, \
+                                           output, outputOffset, classPtr);    \
+  }
+
+#define ReLUBackward(DType, JType, JArrayType)                               \
+  JNIEXPORT                                                                  \
+  void JNICALL Java_com_intel_analytics_sparkdl_mkl_MKL_ReLUBackward##DType( \
+      JNIEnv *env, jclass thisClass, JArrayType input, jint inputOffset,     \
+      JArrayType outputDiff, jint outputDiffOffset, JArrayType inputDiff,    \
+      jint inputDiffOffset, long classPtr)                                   \
+  {                                                                          \
+    JNIReLUUpdateGradInput<JArrayType, JType>(                               \
+        env, thisClass, input, inputOffset, outputDiff, outputDiffOffset,    \
+        inputDiff, inputDiffOffset, classPtr);                               \
+  }
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// double
+ReLUInit(Double, jdouble, jdoubleArray);
+ReLUForward(Double, jdouble, jdoubleArray);
+ReLUBackward(Double, jdouble, jdoubleArray);
+
+// float
+ReLUInit(Float, jfloat, jfloatArray);
+ReLUForward(Float, jfloat, jfloatArray);
+ReLUBackward(Float, jfloat, jfloatArray);
+
+#ifdef __cplusplus
+}
+#endif
