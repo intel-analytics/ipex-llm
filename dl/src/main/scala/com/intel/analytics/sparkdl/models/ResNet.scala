fc61d31304045a990fab18a2c97e99f5bfc4e5ba
JerryYanWan <ywanad@connect.ust.hk>
2016-09-27 09:32:44 +0800

    add convolutional layer in bottleneck function in ResNet.scala and update test

--- dl/src/main/scala/com/intel/analytics/sparkdl/models/ResNet.scala
+++ dl/src/main/scala/com/intel/analytics/sparkdl/models/ResNet.scala
@@ -10,12 +10,16 @@ import scala.reflect.ClassTag
   * Created by ywan on 16-9-23.
   */
 
+class Ichannels (myIchannel: Int = 0) {
+  var ichannel: Int = myIchannel
+}
+
 object ResNet {
   def apply[T: ClassTag](opt: Table)(implicit ev: TensorNumeric[T]): Module[T] = {
 
     val depth = opt.get("depth")
     val shortcutType = if (opt.get("shortcutType") != null) opt.get("shortcutType") else "B"
-    var iChannels: Int = 0
+    var iChannel = new Ichannels(0)
 
     def shortcut(nInputPlane: Int, nOutputPlane: Int, stride: Int): Module[T] = {
       val useConv = shortcutType == "C" || (shortcutType == "B" && nInputPlane != nOutputPlane)
@@ -28,17 +32,17 @@ object ResNet {
       } else if (nInputPlane != nOutputPlane) {
         val model = new Sequential[T]()
         model.add(new SpatialAveragePooling[T](1, 1, stride, stride))
-        model.add(new ConcatAddTable[T](true)
+        model.add(new Concat[T](2)
                   .add(new Identity[T]())
-                  .add(new MulConstant[T](0)))
+                  .add(new MulConstant[T](ev.fromType(0))))
         model
         // sc.add(new Concat[T](2).add(Identity).add(MulConstant(0)) //  -- ZhangYao's code here
       }  else { val model = new Identity(); model} //    -- ZhangYao's code here
     }
 
     def basicblock(n: Int, stride: Int): Module[T] = {
-      var nInputPlane = iChannels
-      iChannels       = n
+      var nInputPlane = iChannel.ichannel
+      iChannel.ichannel = n
 
       val s = new Sequential[T]()
       s.add(new SpatialConvolution[T](nInputPlane, n, 3, 3, stride, stride, 1, 1))
@@ -61,11 +65,14 @@ object ResNet {
     }
 
     def bottleneck(n: Int, stride: Int): Module[T] = {
-      var nInputPlane = iChannels
-      iChannels       = n * 4
+      var nInputPlane   = iChannel.ichannel
+      iChannel.ichannel = n * 4
 
       val s = new Sequential[T]()
       s.add(new SpatialConvolution[T](nInputPlane, n, 1, 1, 1, 1, 0, 0))
+      s.add(new SpatialBatchNormalization[T](n))
+      s.add(new ReLU[T](true))
+      s.add(new SpatialConvolution[T](n, n, 3, 3, stride, stride, 1, 1))
       s.add(new SpatialBatchNormalization[T](n))
       s.add(new ReLU[T](true))
       s.add(new SpatialConvolution[T](n, n*4, 1, 1, 1, 1, 0, 0))
@@ -96,14 +103,14 @@ object ResNet {
     }
 
     val model = new Sequential[T]()
-    if (opt.get("dataset") == "imagenet") {
+    //if (opt.get("dataset") == "imagenet") {
 
       // configuration for ResNet-50
       val block: String = "bottleneck"
       val loopConfig = Array(3, 4, 6, 3)
       val nFeatures  = 2048
 
-      iChannels = 64
+      iChannel.ichannel = 64
       println(" | ResNet-" + depth + " ImageNet")
 
       //-- The ResNet ImageNet Model
@@ -120,7 +127,7 @@ object ResNet {
       model.add(new View[T](nFeatures).setNumInputDims(3))
       model.add(new Linear[T](nFeatures, 1000))
 
-    }
+    //}
     model
 
   }
