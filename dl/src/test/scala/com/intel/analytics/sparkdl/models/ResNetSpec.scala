6fbe03ae39c2bb05e39388f445aba9381d6b051c
JerryYanWan <ywanad@connect.ust.hk>
2016-09-29 10:51:44 +0800

    Only Scala for ResNet-50 batchsize 256

--- dl/src/test/scala/com/intel/analytics/sparkdl/models/ResNetSpec.scala
+++ dl/src/test/scala/com/intel/analytics/sparkdl/models/ResNetSpec.scala
@@ -203,17 +203,17 @@ class ResNetSpec extends FlatSpec with BeforeAndAfter with Matchers {
 
       """
 
-    TH.runNM(code, Map("input" -> input, "labels" -> labels), Array("output", "gradOutput", "err",
-        "parameters_initial", "gradParameters_initial", "gradInput", "model"))
+    //TH.runNM(code, Map("input" -> input, "labels" -> labels), Array("output", "gradOutput", "err",
+    //    "parameters_initial", "gradParameters_initial", "gradInput", "model"))
 
-    val parameterTorch = TH.map("parameters_initial").asInstanceOf[Tensor[Double]]
+    //val parameterTorch = TH.map("parameters_initial").asInstanceOf[Tensor[Double]]
     val parameters = model.getParameters()._1.asInstanceOf[Tensor[Double]]
 
-    for (i <- 0 until parameters.nElement()) {
+    /*for (i <- 0 until parameters.nElement()) {
       if (abs(parameters.storage().array()(i) - parameterTorch.storage().array()(i)) > 1e-8) {
         println(s"${parameters.storage().array()(i)} ${parameterTorch.storage().array()(i)}")
       }
-    }
+    }*/
 
     //val criterion = new ClassNLLCriterion[Double]()
     val (weights, grad) = model.getParameters()
@@ -231,27 +231,27 @@ class ResNetSpec extends FlatSpec with BeforeAndAfter with Matchers {
       sgd.optimize(_ => (loss, grad), weights, state, state)
     }
 
-    val output = TH.map("output").asInstanceOf[Tensor[Double]]
+    //val output = TH.map("output").asInstanceOf[Tensor[Double]]
     val outputTest = model.forward(input)
 
-   var abss = 0.0
+   /*var abss = 0.0
     for (i <- 0 until outputTest.nElement()) {
       val tmp = abs(outputTest.storage().array()(i) - output.storage().array()(i))
       abss += tmp
     }
     assert(abss < 1e-2)
     println(s"outputAbs:$abss")
-
+*/
     val errTest = criterion.forward(outputTest, labels)
     println(s"Test scala loss: $errTest")
-    val err = TH.map("err").asInstanceOf[Double]
-    println(s"${abs(errTest - err)}")
-    assert(abs(errTest - err) < 1.5e-6)
+    //val err = TH.map("err").asInstanceOf[Double]
+    //println(s"${abs(errTest - err)}")
+    //assert(abs(errTest - err) < 1.5e-6)
 
     val gradOutputTest = criterion.backward(outputTest, labels)
-    val gradOutput = TH.map("gradOutput").asInstanceOf[Tensor[Double]]
-    abss = 0.0
-    for (i <- 0 until gradOutputTest.nElement()) {
+    //val gradOutput = TH.map("gradOutput").asInstanceOf[Tensor[Double]]
+    //abss = 0.0
+    /*for (i <- 0 until gradOutputTest.nElement()) {
       val tmp = abs(gradOutputTest.storage().array()(i) - gradOutput.storage().array()(i))
       abss += tmp
     }
@@ -269,7 +269,7 @@ class ResNetSpec extends FlatSpec with BeforeAndAfter with Matchers {
     }
     assert(abss < 2e-6)
     println(s"gradInputTestAbs:$abss")
-
+*/
 //    val (weights, grad) = model.getParameters()
 //    val modelTorch = TH.map("model").asInstanceOf[Module[Double]]
 //    val (weightsTorch, gradTorch) = modelTorch.getParameters()
