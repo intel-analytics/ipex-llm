e65f29111065f9ca91659ac1095715cf7a3be63e
JerryYanWan <ywanad@connect.ust.hk>
2016-10-09 18:26:26 +0800

    sharedGradInput revision v1

--- dl/src/test/scala/com/intel/analytics/sparkdl/models/ResNetSpec.scala
+++ dl/src/test/scala/com/intel/analytics/sparkdl/models/ResNetSpec.scala
@@ -41,14 +41,14 @@ class ResNetSpec extends FlatSpec with BeforeAndAfter with Matchers {
 
     Random.setSeed(1)
     val classNum: Int = 1000
-    val input = Tensor[Double](256, 3, 224, 224).apply1(e => Random.nextDouble())
-    val labels = Tensor[Double](256).apply1(e => Random.nextInt(classNum))
+    val input = Tensor[Double](8, 3, 224, 224).apply1(e => Random.nextDouble())
+    val labels = Tensor[Double](8).apply1(e => Random.nextInt(classNum))
 
     val seed = 100
     RNG.setSeed(seed)
     val opt: Table = new Table()
     opt("shortcutType") = "B"
-    opt("depth") = 50
+    opt("depth") = 18
     opt("imagenet") = "imagenet"
     val model = ResNet[Float](classNum, opt)
     model.zeroGradParameters()
@@ -206,11 +206,11 @@ class ResNetSpec extends FlatSpec with BeforeAndAfter with Matchers {
 
       """
 
-    TH.runNM(code, immutable.Map("input" -> input, "labels" -> labels), Array("output", "gradOutput", "err",
-        "parameters_initial", "gradParameters_initial", "gradInput", "model"))
-
-    val parameterTorch = TH.map("parameters_initial").asInstanceOf[Tensor[Double]]
-    val parameters = model.getParameters()._1.asInstanceOf[Tensor[Float]]
+//    TH.runNM(code, immutable.Map("input" -> input, "labels" -> labels), Array("output", "gradOutput", "err",
+//        "parameters_initial", "gradParameters_initial", "gradInput", "model"))
+//
+//    val parameterTorch = TH.map("parameters_initial").asInstanceOf[Tensor[Double]]
+//    val parameters = model.getParameters()._1.asInstanceOf[Tensor[Float]]
 
     /*for (i <- 0 until parameters.nElement()) {
       if (abs(parameters.storage().array()(i) - parameterTorch.storage().array()(i)) > 1e-8) {
@@ -247,55 +247,55 @@ class ResNetSpec extends FlatSpec with BeforeAndAfter with Matchers {
       println(loss)
     }
 
-    val output = TH.map("output").asInstanceOf[Tensor[Double]]
-    val outputTest = model.forward(floatInput)
-
-    var abss = 0.0
-    for (i <- 0 until outputTest.nElement()) {
-      val tmp = abs(outputTest.storage().array()(i) - output.storage().array()(i))
-      abss += tmp
-    }
-    //assert(abss < 1e-2)
-    println(s"this should be small: outputAbs:$abss")
-
-    val errTest = criterion.forward(outputTest, floatLabels)
-    println(s"Test scala loss: $errTest")
-    val err = TH.map("err").asInstanceOf[Double]
-    println(s"Test torch loss: $errTest")
-    println(s"${abs(errTest - err)}")
-    //assert(abs(errTest - err) < 1.5e-6)
-
-    val gradOutputTest = criterion.backward(outputTest, floatLabels)
-    val gradOutput = TH.map("gradOutput").asInstanceOf[Tensor[Double]]
-    abss = 0.0
-    for (i <- 0 until gradOutputTest.nElement()) {
-      val tmp = abs(gradOutputTest.storage().array()(i) - gradOutput.storage().array()(i))
-      abss += tmp
-    }
-    //assert(abss == 0.0)
-    //assert(abss < 2e-6)
-    println(s"this should be small: gradOutputTestAbs:$abss")
-
-    val gradInput = model.backward(floatInput, gradOutputTest)
-    val gradInputTorch = TH.map("gradInput").asInstanceOf[Tensor[Double]]
-
-    abss = 0.0
-    for (i <- 0 until gradInputTorch.nElement()) {
-      val tmp = abs(gradInputTorch.storage().array()(i) - gradInput.storage().array()(i))
-      abss += tmp
-    }
-    //assert(abss < 2e-6)
-    println(s"this should be small: gradInputTestAbs:$abss")
-
-    println(s"compare output between Lua and Scala:")
-    abss = 0.0
-    for (i <- 0 until outputTest.nElement()) {
-      val tmp = abs(outputTest.storage().array()(i) - output.storage().array()(i))
-      abss += tmp
-      val thOut = output.storage().array()(i)
-      val scOut = outputTest.storage().array()(i)
-      println(s"Lua = $thOut , Scala = $scOut")
-    }
+//    val output = TH.map("output").asInstanceOf[Tensor[Double]]
+//    val outputTest = model.forward(floatInput)
+//
+//    var abss = 0.0
+//    for (i <- 0 until outputTest.nElement()) {
+//      val tmp = abs(outputTest.storage().array()(i) - output.storage().array()(i))
+//      abss += tmp
+//    }
+//    //assert(abss < 1e-2)
+//    println(s"this should be small: outputAbs:$abss")
+//
+//    val errTest = criterion.forward(outputTest, floatLabels)
+//    println(s"Test scala loss: $errTest")
+//    val err = TH.map("err").asInstanceOf[Double]
+//    println(s"Test torch loss: $errTest")
+//    println(s"${abs(errTest - err)}")
+//    //assert(abs(errTest - err) < 1.5e-6)
+//
+//    val gradOutputTest = criterion.backward(outputTest, floatLabels)
+//    val gradOutput = TH.map("gradOutput").asInstanceOf[Tensor[Double]]
+//    abss = 0.0
+//    for (i <- 0 until gradOutputTest.nElement()) {
+//      val tmp = abs(gradOutputTest.storage().array()(i) - gradOutput.storage().array()(i))
+//      abss += tmp
+//    }
+//    //assert(abss == 0.0)
+//    //assert(abss < 2e-6)
+//    println(s"this should be small: gradOutputTestAbs:$abss")
+//
+//    val gradInput = model.backward(floatInput, gradOutputTest)
+//    val gradInputTorch = TH.map("gradInput").asInstanceOf[Tensor[Double]]
+//
+//    abss = 0.0
+//    for (i <- 0 until gradInputTorch.nElement()) {
+//      val tmp = abs(gradInputTorch.storage().array()(i) - gradInput.storage().array()(i))
+//      abss += tmp
+//    }
+//    //assert(abss < 2e-6)
+//    println(s"this should be small: gradInputTestAbs:$abss")
+//
+//    println(s"compare output between Lua and Scala:")
+//    abss = 0.0
+//    for (i <- 0 until outputTest.nElement()) {
+//      val tmp = abs(outputTest.storage().array()(i) - output.storage().array()(i))
+//      abss += tmp
+//      val thOut = output.storage().array()(i)
+//      val scOut = outputTest.storage().array()(i)
+//      println(s"Lua = $thOut , Scala = $scOut")
+//    }
 
 //    val (weights, grad) = model.getParameters()
 //    val modelTorch = TH.map("model").asInstanceOf[Module[Double]]
@@ -427,8 +427,7 @@ gradInput = model:backward(input, gradOutput)
 
   def shareGradInput[@specialized(Float, Double) T: ClassTag](model: Module[T])
     (implicit ev: TensorNumeric[T]): Unit = {
-    def sharingKey(m: Module[T]) =
-      if (m.getName()!= null) m.getName() else m.getClass.getName
+    def sharingKey(m: Module[T]) = m.getClass.getName
 
     val cache = Map[Any, Storage[T]]()
     model.mapModules(m => {
