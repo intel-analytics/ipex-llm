079c16b31f2152e1cfbb6f91b9b0c433a6a41bb6
Yao <psyyz10@gmail.com>
2016-10-28 02:39:25 +0800

    Add Exp layer

--- dl/src/main/scala/com/intel/analytics/sparkdl/tensor/DenseTensorMath.scala
+++ dl/src/main/scala/com/intel/analytics/sparkdl/tensor/DenseTensorMath.scala
@@ -307,7 +307,9 @@ object DenseTensorMath {
 
   def exp[@specialized(Float, Double) T: ClassTag](self: DenseTensor[T], x: Tensor[T])
     (implicit ev: TensorNumeric[T]): Tensor[T] = {
-    require(self.nElement() == x.nElement())
+    if (self.nElement() != x.nElement())
+      self.resizeAs(x)
+
     if (MKL.isMKLLoaded && self.isContiguous() && x.isContiguous()) {
       ev.vExp(self.nElement(), x.storage().array(), x.storageOffset() - 1,
         self.storage().array(), self.storageOffset() - 1)
