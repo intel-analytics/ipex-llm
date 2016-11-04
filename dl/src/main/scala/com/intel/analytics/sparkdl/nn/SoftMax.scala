32f08910b71fa35f2622fe2f9a8280e9eedb357f
qiuxin2012 <qiuxin2012cs@gmail.com>
2016-11-04 13:55:46 +0800

    SoftMin

--- dl/src/main/scala/com/intel/analytics/sparkdl/nn/SoftMax.scala
+++ dl/src/main/scala/com/intel/analytics/sparkdl/nn/SoftMax.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intel.analytics.sparkdl.nn
 
 import com.intel.analytics.sparkdl.tensor.Tensor
@@ -14,6 +31,8 @@ class SoftMax[T: ClassTag]()(implicit ev: TensorNumeric[T]) extends TensorModule
   private var results: Array[Future[Unit]] = null
 
   override def updateOutput(input: Tensor[T]): Tensor[T] = {
+    require(1 <= input.nDimension() && input.nDimension() <= 4,
+      "1D, 2D, 3D or 4D tensor expected")
     val (nFrame, stride) = if (input.nDimension() == 1) {
       (1, 1)
     } else if (input.nDimension() == 2) {
@@ -40,9 +59,9 @@ class SoftMax[T: ClassTag]()(implicit ev: TensorNumeric[T]) extends TensorModule
 
 object SoftMax{
   // Notice: SoftMin will call this function
-  private[nn] def updateOutput[T: ClassTag](input: Tensor[T], output: Tensor[T], results: Array[Future[Unit]])
-    (implicit ev: TensorNumeric[T]) : Tensor[T] = {
-    require(1 <= input.nDimension() && input.nDimension() <= 4, "1D, 2D, 3D or 4D tensor expected")
+  private[nn] def updateOutput[T: ClassTag](input: Tensor[T], output: Tensor[T],
+    results: Array[Future[Unit]]) (implicit ev: TensorNumeric[T]): Tensor[T] = {
+
     val (nFrame, dim, stride) = if (input.nDimension() == 1) {
       (1, input.size(1), 1)
     } else if (input.nDimension() == 2) {
@@ -67,7 +86,7 @@ object SoftMax{
         val inputOffset = (_t / stride) * dim * stride + _t % stride
         val outputOffset = (_t / stride) * dim * stride + _t % stride
 
-        var inputMax : T =  ev.fromType[Float](Float.MinValue)
+        var inputMax = ev.fromType[Float](Float.MinValue)
 
         var d = 0
         while (d < dim) {
@@ -107,8 +126,11 @@ object SoftMax{
   }
 
   private[nn] def updateGradInput[T: ClassTag](input: Tensor[T], gradOutput: Tensor[T],
-    gradInput: Tensor[T], output: Tensor[T], results: Array[Future[Unit]])(implicit ev: TensorNumeric[T]): Tensor[T] = {
-    require(input.size().deep == gradOutput.size().deep)
+    gradInput: Tensor[T], output: Tensor[T],
+    results: Array[Future[Unit]])(implicit ev: TensorNumeric[T]): Tensor[T] = {
+
+    require(input.size().deep == gradOutput.size().deep,
+      "input should have the same size with gradOutput")
     val (nFrame, dim, stride) = if (output.nDimension() == 1) {
       (1, output.size(1), 1)
     } else if (output.nDimension() == 2) {
