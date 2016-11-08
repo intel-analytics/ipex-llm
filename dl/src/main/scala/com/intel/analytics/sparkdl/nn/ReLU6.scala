9c47ff8cde338272aed89b99336cc169399eb05a
Yao <psyyz10@gmail.com>
2016-11-08 09:36:27 +0800

    Implement and test Clamp and ReLu6

---
+++ dl/src/main/scala/com/intel/analytics/sparkdl/nn/ReLU6.scala
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intel.analytics.sparkdl.nn
+
+import com.intel.analytics.sparkdl.tensor.Tensor
+import com.intel.analytics.sparkdl.tensor.TensorNumericMath.TensorNumeric
+
+import scala.reflect.ClassTag
+
+class ReLU6[T: ClassTag](inplace: Boolean = false)
+  (implicit ev: TensorNumeric[T]) extends HardTanh[T](0, 6, inplace) {
+
+  override def updateOutput(input: Tensor[T]): Tensor[T] = {
+    super.updateOutput(input)
+  }
+
+  override def updateGradInput(input: Tensor[T], gradOutput: Tensor[T]): Tensor[T] = {
+    super.updateGradInput(input, gradOutput)
+  }
+
+  override def toString(): String = {
+    s"nn.ReLU6"
+  }
+}
