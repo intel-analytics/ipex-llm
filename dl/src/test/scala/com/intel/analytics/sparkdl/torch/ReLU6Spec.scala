4dbc9ddd8b0a85396754de4b0b0bfa8216c55342
Yao <psyyz10@gmail.com>
2016-11-08 09:36:27 +0800

    Implement and test Clamp and ReLu6

--- dl/src/test/scala/com/intel/analytics/sparkdl/torch/ReLU6Spec.scala
+++ dl/src/test/scala/com/intel/analytics/sparkdl/torch/ReLU6Spec.scala
@@ -16,21 +16,21 @@
  */
 package com.intel.analytics.sparkdl.torch
 
-import com.intel.analytics.sparkdl.nn.HardTanh
+import com.intel.analytics.sparkdl.nn.ReLU6
 import com.intel.analytics.sparkdl.tensor.Tensor
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
+import scala.math._
 
-class HardTanhSpec  extends FlatSpec with BeforeAndAfter with Matchers {
+class ReLU6Spec extends FlatSpec with BeforeAndAfter with Matchers {
   before {
     if (!TH.hasTorch()) {
       cancel("Torch is not installed")
     }
   }
 
-  "A HardTanh Module " should
-    "generate correct output and grad not inplace with contiguous input" in {
-    val module = new HardTanh[Double]()
+  "A ReLU6 Module " should "generate correct output and grad not inplace" in {
+    val module = new ReLU6[Double]()
     val input = Tensor[Double](2, 2, 2)
     input(Array(1, 1, 1)) = -0.97008799016476
     input(Array(1, 1, 2)) = -0.89318234380335
@@ -56,7 +56,7 @@ class HardTanhSpec  extends FlatSpec with BeforeAndAfter with Matchers {
     val end = System.nanoTime()
     val scalaTime = end - start
 
-    val code = "module = nn.HardTanh()\n" +
+    val code = "module = nn.ReLU6()\n" +
       "output = module:forward(input)\n" +
       "gradInput = module:backward(input,gradOutput)"
 
@@ -65,14 +65,20 @@ class HardTanhSpec  extends FlatSpec with BeforeAndAfter with Matchers {
     val luaOutput1 = torchResult("output").asInstanceOf[Tensor[Double]]
     val luaOutput2 = torchResult("gradInput").asInstanceOf[Tensor[Double]]
 
-    luaOutput1 should be (output)
-    luaOutput2 should be (gradInput)
+    luaOutput1.map(output, (v1, v2) => {
+      assert(abs(v1 - v2) == 0)
+      v1
+    })
+    luaOutput2.map(gradInput, (v1, v2) => {
+      assert(abs(v1 - v2) == 0)
+      v1
+    })
 
-    println("Test case : HardTanh, Torch : " + luaTime + " s, Scala : " + scalaTime / 1e9 + " s")
+    println("Test case : ReLU, Torch : " + luaTime + " s, Scala : " + scalaTime / 1e9 + " s")
   }
 
-  "A HardTanh Module " should "generate correct output and grad inplace with contiguous input" in {
-    val module = new HardTanh[Double](inplace = true)
+  "A ReLU6 Module " should "generate correct output and grad inplace" in {
+    val module = new ReLU6[Double](true)
     val input = Tensor[Double](2, 2, 2)
     input(Array(1, 1, 1)) = -0.97008799016476
     input(Array(1, 1, 2)) = -0.89318234380335
@@ -98,7 +104,7 @@ class HardTanhSpec  extends FlatSpec with BeforeAndAfter with Matchers {
     val end = System.nanoTime()
     val scalaTime = end - start
 
-    val code = "module = nn.HardTanh(-1, 1, true)\n" +
+    val code = "module = nn.ReLU6(true)\n" +
       "output = module:forward(input)\n" +
       "gradInput = module:backward(input,gradOutput)"
 
@@ -107,79 +113,15 @@ class HardTanhSpec  extends FlatSpec with BeforeAndAfter with Matchers {
     val luaOutput1 = torchResult("output").asInstanceOf[Tensor[Double]]
     val luaOutput2 = torchResult("gradInput").asInstanceOf[Tensor[Double]]
 
-    luaOutput1 should be (output)
-    luaOutput2 should be (gradInput)
+    luaOutput1.map(output, (v1, v2) => {
+      assert(abs(v1 - v2) == 0)
+      v1
+    })
+    luaOutput2.map(gradInput, (v1, v2) => {
+      assert(abs(v1 - v2) == 0)
+      v1
+    })
 
-    println("Test case : HardTanh, Torch : " + luaTime + " s, Scala : " + scalaTime / 1e9 + " s")
-  }
-
-  "A HardTanh Module " should
-    "generate correct output and grad not inplace with not contiguous input" in {
-    val module = new HardTanh[Double]()
-    val input = Tensor[Double](2, 2)
-    input(Array(1, 1)) = -0.97008799016476
-    input(Array(1, 2)) = -0.65073125436902
-    input(Array(2, 2)) = -0.35406025126576
-    input(Array(2, 1)) = 1.0360766677186
-    val gradOutput = Tensor[Double](2, 2)
-    gradOutput(Array(1, 1)) = 0.43442418193445
-    gradOutput(Array(1, 2)) = 0.97614445211366
-    gradOutput(Array(2, 2)) = 0.081252868985757
-    gradOutput(Array(2, 1)) = 0.24688877537847
-
-    val start = System.nanoTime()
-    val output = module.forward(input.t())
-    val gradInput = module.backward(input.t(), gradOutput.t())
-    val end = System.nanoTime()
-    val scalaTime = end - start
-
-    val code = "module = nn.HardTanh()\n" +
-      "output = module:forward(input:t())\n" +
-      "gradInput = module:backward(input:t(),gradOutput:t())"
-
-    val (luaTime, torchResult) = TH.run(code, Map("input" -> input, "gradOutput" -> gradOutput),
-      Array("output", "gradInput"))
-    val luaOutput1 = torchResult("output").asInstanceOf[Tensor[Double]]
-    val luaOutput2 = torchResult("gradInput").asInstanceOf[Tensor[Double]]
-
-    luaOutput1 should be (output)
-    luaOutput2 should be (gradInput)
-
-    println("Test case : HardTanh, Torch : " + luaTime + " s, Scala : " + scalaTime / 1e9 + " s")
-  }
-
-  "A HardTanh Module " should
-    "generate correct output and grad inplace with not contiguous input" in {
-    val module = new HardTanh[Double](inplace = true)
-    val input = Tensor[Double](2, 2)
-    input(Array(1, 1)) = -0.97008799016476
-    input(Array(1, 2)) = -0.65073125436902
-    input(Array(2, 2)) = -0.35406025126576
-    input(Array(2, 1)) = 1.0360766677186
-    val gradOutput = Tensor[Double](2, 2)
-    gradOutput(Array(1, 1)) = 0.43442418193445
-    gradOutput(Array(1, 2)) = 0.97614445211366
-    gradOutput(Array(2, 2)) = 0.081252868985757
-    gradOutput(Array(2, 1)) = 0.24688877537847
-
-    val start = System.nanoTime()
-    val output = module.forward(input.t())
-    val gradInput = module.backward(input.t(), gradOutput.t())
-    val end = System.nanoTime()
-    val scalaTime = end - start
-
-    val code = "module = nn.HardTanh(-1, 1, true)\n" +
-      "output = module:forward(input:t())\n" +
-      "gradInput = module:backward(input:t(),gradOutput:t())"
-
-    val (luaTime, torchResult) = TH.run(code, Map("input" -> input, "gradOutput" -> gradOutput),
-      Array("output", "gradInput"))
-    val luaOutput1 = torchResult("output").asInstanceOf[Tensor[Double]]
-    val luaOutput2 = torchResult("gradInput").asInstanceOf[Tensor[Double]]
-
-    luaOutput1 should be (output)
-    luaOutput2 should be (gradInput)
-
-    println("Test case : HardTanh, Torch : " + luaTime + " s, Scala : " + scalaTime / 1e9 + " s")
+    println("Test case : ReLU, Torch : " + luaTime + " s, Scala : " + scalaTime / 1e9 + " s")
   }
 }
