58a0cad0a80c2ca13d6a74455275dc834c772d23
Yao <psyyz10@gmail.com>
2016-11-11 08:47:58 +0800

    Implement and test ParalleTable

---
+++ dl/src/test/scala/com/intel/analytics/sparkdl/nn/ParallelTableSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intel.analytics.sparkdl.nn
+
+import com.intel.analytics.sparkdl.tensor.Tensor
+import com.intel.analytics.sparkdl.utils.T
+import org.scalatest.{FlatSpec, Matchers}
+
+class ParallelTableSpec extends FlatSpec with Matchers {
+  "A MapTable" should "generate correct output" in {
+    val input = T(
+      Tensor[Float](10).randn(),
+      Tensor[Float](10).randn())
+
+    val gradOutput = T(
+      Tensor[Float](3).randn(),
+      Tensor[Float](3).randn())
+
+    val linear1 = new Linear[Float](10, 3)
+    val linear2 = new Linear[Float](10, 3)
+    val expectedOutput = T(
+      linear1.updateOutput(input(1)),
+      linear2.updateOutput(input(2)))
+
+    val module = new ParallelTable[Float]()
+    module.add(linear1)
+    module.add(linear2)
+    val mapOutput = module.forward(input)
+    mapOutput should equal (expectedOutput)
+
+    val expectedGradInput = T(
+      linear1.updateGradInput(input(1), gradOutput(1)),
+      linear2.updateGradInput(input(2), gradOutput(2)))
+    val mapGradInput = module.backward(input, gradOutput)
+
+    mapGradInput should equal (expectedGradInput)
+  }
+}
