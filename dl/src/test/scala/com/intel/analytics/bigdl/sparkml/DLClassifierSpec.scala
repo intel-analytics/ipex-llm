e95857464f865e128d217952fc34311c64f14d2b
zhangli <cherry.zhang@intel.com>
2016-12-23 15:08:32 +0800

    spark 2.0 support

--- dl/src/test/scala/com/intel/analytics/bigdl/sparkml/DLClassifierSpec.scala
+++ dl/src/test/scala/com/intel/analytics/bigdl/sparkml/DLClassifierSpec.scala
@@ -17,13 +17,7 @@
 
 package com.intel.analytics.bigdl.sparkml
 
-import java.io.File
-import java.nio.file.Paths
-
-import com.intel.analytics.bigdl.dataset._
-import com.intel.analytics.bigdl.dataset.image.{GreyImgNormalizer, GreyImgToBatch, SampleToGreyImg}
 import com.intel.analytics.bigdl.models.lenet.LeNet5
-import com.intel.analytics.bigdl.models.lenet.Utils._
 import com.intel.analytics.bigdl.tensor.Tensor
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
@@ -31,10 +25,11 @@ import org.apache.spark.ml.DLClassifier
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.{Row, SQLContext}
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
 class DLClassifierSpec extends FlatSpec with Matchers{
 
@@ -46,9 +41,7 @@ class DLClassifierSpec extends FlatSpec with Matchers{
     }
   }
 
-  case class Person(orgLabel: Float, features: Tensor[Float])
-
-  "DLClassifier for MNIST" should "get good result" in {
+  "DLClassifier" should "get good result" in {
     Logger.getLogger("org").setLevel(Level.WARN)
     Logger.getLogger("akka").setLevel(Level.WARN)
 
@@ -57,45 +50,40 @@ class DLClassifierSpec extends FlatSpec with Matchers{
     val batchSize = 10
     val model = LeNet5(10)
 
-    // read test data
-    val resource = getClass().getClassLoader().getResource("mnist")
-    val validationData = Paths.get(processPath(resource.getPath()) + File.separator,
-      "t10k-images.idx3-ubyte")
-    val validationLabel = Paths.get(processPath(resource.getPath()) + File.separator,
-      "t10k-labels.idx1-ubyte")
-
-    val validationSet = DataSet.array(load(validationData, validationLabel))
-      .transform(SampleToGreyImg(28, 28))
-    val normalizerVal = GreyImgNormalizer(validationSet)
-    val valSet = validationSet.transform(normalizerVal)
-      .transform(GreyImgToBatch(batchSize))
-    val valData = valSet.data(looped = false)
-
     // init
-    val valTrans = new DLClassifier().setInputCol("features").setOutputCol("predict")
+    val valTrans = new DLClassifier[Float]().setInputCol("features").setOutputCol("predict")
     val paramsTrans = ParamMap(valTrans.modelTrain -> model,
       valTrans.batchSize -> Array(10, 28, 28))
-    val tensorBuffer = new ArrayBuffer[LabeledPoint]()
-    var res: DataFrame = null
 
-    while (valData.hasNext) {
-      val batch = valData.next()
-      val input = batch.data.storage().array()
-      val target = batch.labels.storage().array()
+    val tensorBuffer = new ArrayBuffer[LabeledPoint]()
+    var m = 0
+    while (m < 10) {
+      // generate test data
+      val input = Tensor[Float](10, 28, 28).apply1(e => Random.nextFloat())
+      val target = model.forward(input).toTensor[Float]
+
+      val inputArr = input.storage().array()
+      val targetArr = target.max(2)._2.squeeze().storage().array()
 
       var i = 0
       while (i < batchSize) {
-        tensorBuffer.append(new LabeledPoint(target(i),
-          new DenseVector(input.slice(i * 28 * 28, (i + 1 ) * 28 * 28).map(_.toDouble))))
+        tensorBuffer.append(new LabeledPoint(targetArr(i),
+          new DenseVector(inputArr.slice(i * 28 * 28, (i + 1) * 28 * 28).map(_.toDouble))))
         i += 1
       }
 
       val rowRDD = sc.parallelize(tensorBuffer)
       val testData = sqlContext.createDataFrame(rowRDD)
-      res = valTrans.transform(testData, paramsTrans)
-      res.select("label", "predict").show()
+
+      valTrans.transform(testData, paramsTrans)
+        .select("label", "predict")
+         .collect()
+        .foreach { case Row(label: Double, predict: Int) =>
+          label.toInt should be(predict)
+        }
 
       tensorBuffer.clear()
+      m += 1
     }
     sc.stop()
   }
