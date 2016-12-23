7889f9883a974a1a2e49c18e87cad075b2f2dfbf
Yao <psyyz10@gmail.com>
2016-12-23 09:32:31 +0800

    reset CAddTable back
    Former-commit-id: 435103620d88f3317fa59a164d86a7898fc0db0e

--- dl/src/main/scala/com/intel/analytics/bigdl/nn/CAddTable.scala
+++ dl/src/main/scala/com/intel/analytics/bigdl/nn/CAddTable.scala
@@ -22,7 +22,7 @@ import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.utils.{T, Table}
 
-import scala.reflect.ClassTag
+import scala.reflect._
 
 @SerialVersionUID(7959261460060075605L)
 class CAddTable[@specialized(Float, Double) T: ClassTag](val inplace: Boolean = false)(
@@ -30,40 +30,33 @@ class CAddTable[@specialized(Float, Double) T: ClassTag](val inplace: Boolean = 
 
   override def updateOutput(input: Table): Tensor[T] = {
     if (inplace) {
-      output = input[Tensor[T]](1)
+      output.set(input[Tensor[T]](1))
     } else {
-      val input1 = input[Tensor[T]](1)
-      if (null == output) {
-        output = input1.clone()
-      } else {
-        output.resizeAs(input1).copy(input1)
-      }
+      output.resizeAs(input[Tensor[T]](1)).copy(input[Tensor[T]](1))
     }
-
     var i = 2
     while (i <= input.length()) {
       output.add(input[Tensor[T]](i))
       i += 1
     }
-
     output
   }
 
   override def updateGradInput(input: Table, gradOutput: Tensor[T]) : Table = {
     var i = 1
     while (i <= input.length()) {
+      if (i > gradInput.length) gradInput.insert(i, Tensor[T]().resizeAs(input(1)))
       if (inplace) {
-        gradInput(i) = gradOutput
+        gradInput[Tensor[T]](i).set(gradOutput)
       } else {
-        if (gradInput.contains(i)) {
-          gradInput[Tensor[T]](i).resizeAs(gradOutput).copy(gradOutput)
-        } else {
-          gradInput.insert(i, gradOutput.clone())
-        }
+        gradInput[Tensor[T]](i).resizeAs(gradOutput).copy(gradOutput)
       }
       i += 1
     }
-
+    i = input.length + 1
+    while (i <= gradInput.length) {
+      gradInput.remove(i)
+    }
     gradInput
   }
 
@@ -71,6 +64,7 @@ class CAddTable[@specialized(Float, Double) T: ClassTag](val inplace: Boolean = 
     "nn.CAddTable"
   }
 }
+
 
 object CAddTable {
   def apply[@specialized(Float, Double) T: ClassTag](
