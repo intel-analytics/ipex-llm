1fffd743d3adfb4eae98065d066bed0774caf143
ian <yihengw@gmail.com>
2016-12-24 19:30:59 +0800

    set a seed for random input

--- dl/src/test/scala/com/intel/analytics/bigdl/nn/RecurrentSpec.scala
+++ dl/src/test/scala/com/intel/analytics/bigdl/nn/RecurrentSpec.scala
@@ -31,7 +31,8 @@ class RecurrentSpec extends FlatSpec with Matchers {
     val inputSize = 5
     val outputSize = 5
     val bpttTruncate = 3
-
+    val seed = 100
+    RNG.setSeed(seed)
 
     val model = Sequential[Double]()
     model.add(Recurrent[Double](hiddenSize, bpttTruncate)
