d00b03ba3fe924d0ec0390442490a49d1e6b7a9d
Yao <psyyz10@gmail.com>
2017-02-17 02:28:34 +0800

    implement buildGate function in LSTMCell

---
+++ dl/src/main/scala/com/intel/analytics/bigdl/nn/LSTMCell.scala
@@ -0,0 +1,68 @@
+/*
+ * Licensed to Intel Corporation under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * Intel Corporation licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.nn
+
+import com.intel.analytics.bigdl.nn.abstractnn.AbstractModule
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.utils.Table
+
+import scala.reflect.ClassTag
+
+class LSTMCell[T : ClassTag] (
+  inputSize: Int = 4,
+  hiddenSize: Int = 3,
+  private var initMethod: InitializationMethod = Default)
+  (implicit ev: TensorNumeric[T])
+  extends AbstractModule[Table, Table, T] {
+
+  def buildGate(): Sequential[T] = {
+    val gate = Sequential()
+      .add(NarrowTable(1, 2))
+
+    val i2g = Sequential()
+      .add(Linear(inputSize, hiddenSize))
+    val h2g = Sequential()
+      .add(Linear(hiddenSize, hiddenSize))
+
+    gate
+      .add(ParallelTable()
+        .add(i2g)
+        .add(h2g))
+      .add(CAddTable())
+      .add(Sigmoid())
+  }
+
+  /**
+   * Computes the output using the current parameter set of the class and input. This function
+   * returns the result which is stored in the output field.
+   *
+   * @param input
+   * @return
+   */
+  override def updateOutput(input: Table): Table = ???
+
+  /**
+   * Computing the gradient of the module with respect to its own input. This is returned in
+   * gradInput. Also, the gradInput state variable is updated accordingly.
+   *
+   * @param input
+   * @param gradOutput
+   * @return
+   */
+  override def updateGradInput(input: Table, gradOutput: Table): Table = ???
+}
