cb7efbfe759dbcff5c5dfaf6a4781085ebe487c3
Yao <psyyz10@gmail.com>
2017-02-20 03:03:02 +0800

    make lstm work

--- dl/src/test/scala/com/intel/analytics/bigdl/models/LSTMSpec.scala
+++ dl/src/test/scala/com/intel/analytics/bigdl/models/LSTMSpec.scala
@@ -17,15 +17,64 @@
 
 package com.intel.analytics.bigdl.models
 
-import com.intel.analytics.bigdl.torch.TH
+import com.intel.analytics.bigdl.nn._
+import com.intel.analytics.bigdl.optim.SGD
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.utils.RandomGenerator._
+import com.intel.analytics.bigdl.utils.T
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 class LSTMSpec extends FlatSpec with BeforeAndAfter with Matchers {
-  "LSTM" should "generate correct output" in {
-    if (!TH.hasTorch()) {
-      cancel("Torch is not installed")
+  "A Recurrent LSTM Language Model Module " should "converge" in {
+    val hiddenSize = 4
+    val inputSize = 5
+    val outputSize = 5
+    val bpttTruncate = 3
+    val seed = 100
+    RNG.setSeed(seed)
+
+    val model = Sequential[Double]()
+    model.add(Recurrent[Double](hiddenSize, bpttTruncate)
+      .add(LSTMCell[Double](inputSize, hiddenSize))
+      .add(Tanh()))
+      .add(Select(1, 1))
+      .add(Linear[Double](hiddenSize, outputSize))
+
+    val criterion = CrossEntropyCriterion[Double]()
+    val logSoftMax = LogSoftMax[Double]()
+
+    val (weights, grad) = model.getParameters()
+
+    val input = Tensor[Double](Array(1, 5, inputSize))
+    val labels = Tensor[Double](Array(1, 5))
+    for (i <- 1 to 5) {
+      val rdmLabel = Math.ceil(RNG.uniform(0.0, 1.0)*inputSize).toInt
+      val rdmInput = Math.ceil(RNG.uniform(0.0, 1.0)*inputSize).toInt
+      input.setValue(1, i, rdmInput, 1.0)
+      labels.setValue(1, i, rdmLabel)
     }
 
+    val state = T("learningRate" -> 0.5, "momentum" -> 0.0,
+      "weightDecay" -> 0.0, "dampening" -> 0.0)
+    val sgd = new SGD[Double]
+    def feval(x: Tensor[Double]): (Double, Tensor[Double]) = {
+      val output = model.forward(input).asInstanceOf[Tensor[Double]]
+      val _loss = criterion.forward(output, labels)
+      model.zeroGradParameters()
+      val gradInput = criterion.backward(output, labels)
+      model.backward(input, gradInput)
+      (_loss, grad)
+    }
 
+    for (i <- 1 to 10000) {
+      val (_, loss) = sgd.optimize(feval, weights, state)
+      println(s"${i}-th loss = ${loss(0)}")
+    }
+
+    val output = model.forward(input).asInstanceOf[Tensor[Double]]
+    val logOutput = logSoftMax.forward(output)
+    val prediction = logOutput.max(2)._2
+
+    labels.squeeze() should be (prediction.squeeze())
   }
 }
