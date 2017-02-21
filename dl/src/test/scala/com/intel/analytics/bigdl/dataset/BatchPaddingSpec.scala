b6cc049b893a2da7754b735c24cd69cf3ec2009f
zhangli <cherry.zhang@intel.com>
2017-02-21 14:15:16 +0800

    featureValue support Tensor

--- dl/src/test/scala/com/intel/analytics/bigdl/dataset/BatchPaddingSpec.scala
+++ dl/src/test/scala/com/intel/analytics/bigdl/dataset/BatchPaddingSpec.scala
@@ -20,197 +20,40 @@ package com.intel.analytics.bigdl.dataset
 import java.util
 
 import com.intel.analytics.bigdl.dataset.text._
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.tensor.{Storage, Tensor}
+import com.intel.analytics.bigdl.utils.Engine
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
+import scala.collection.Iterator
+import scala.reflect.ClassTag
 import scala.util.Random
 
 class BatchPaddingSpec extends FlatSpec with Matchers with BeforeAndAfter {
-  "SampleToBatchPaddingLM " should "be good when batchsize = 1" in {
-    val totalCount = 100
-    val trainData = new Array[LabeledSentence[Float]](totalCount)
-    var i = 0
-    var base = Array(1.0f)
-    while (i < totalCount) {
-      val input = Array(0.0f) ++ base ++ Array(10.0f)
-      val label = Array(1.0f) ++ base ++ Array(100.0f)
-      trainData(i) = new LabeledSentence[Float](input, label)
-      i += 1
-      base = base ++ Array(i.toFloat)
-    }
-    val trainMaxLength = 20
-    val batchSize = 1
-    val dictionaryLength = 4001
-    val trainDataReverse = trainData.reverse
-    val trainSet1 = DataSet.array(trainDataReverse)
-      .transform(LabeledSentenceToSample(dictionaryLength))
-      .transform(SampleToBatch(batchSize = batchSize))
 
-    val trainSet2 = DataSet.array(trainDataReverse)
-      .transform(LabeledSentenceToSample(dictionaryLength))
-      .transform(SampleToBatchPaddingLM[Float](batchSize, 0, 100, true))
-
-    val data1 = trainSet1.toLocal().data(train = false)
-    val data2 = trainSet2.toLocal().data(train = false)
-
-    while (data1.hasNext && data2.hasNext) {
-      val batch1 = data1.next()
-      val input1 = batch1.data.storage().array()
-      val label1 = batch1.labels.storage().array()
-
-      val batch2 = data2.next()
-      val input2 = batch2.data.storage().array()
-      val label2 = batch2.labels.storage().array()
-      val length = batch2.labels.size().product
-
-      var i = 0
-      while (i < length) {
-        label1(i) should be (label2(i))
-        i += 1
-      }
-
-      i = 0
-      while (i < dictionaryLength * length) {
-        input1(i) should be (input2(i))
-        i += 1
-      }
-    }
-    data1.hasNext should be (false)
-    data2.hasNext should be (false)
-  }
-
-  "SampleToBatchPaddingLM " should "be good when padding to same length with batchsize > 1" in {
-    val totalCount = 100
-    val trainData = new Array[LabeledSentence[Float]](totalCount)
-    var i = 0
-    var base = Array(1.0f)
-    while (i < totalCount) {
-      val input = Array(0.0f) ++ base ++ Array(10.0f)
-      val label = Array(1.0f) ++ base ++ Array(100.0f)
-      trainData(i) = new LabeledSentence[Float](input, label)
-      i += 1
-      base = base ++ Array(i.toFloat)
-    }
-
-    val trainMaxLength = 110
-    val batchSize = 2
-    val dictionaryLength = 4001
-    val trainSet1 = DataSet.array(trainData)
-      .transform(LabeledSentenceToSample(dictionaryLength,
-        Some(trainMaxLength), Some(trainMaxLength)))
-      .transform(SampleToBatch(batchSize = batchSize))
-
-    val trainSet2 = DataSet.array(trainData)
-      .transform(LabeledSentenceToSample(dictionaryLength))
-      .transform(SampleToBatchPaddingLM(batchSize, 0, 100, true, Some(0), Some(trainMaxLength)))
-
-    val data1 = trainSet1.toLocal().data(train = false)
-    val data2 = trainSet2.toLocal().data(train = false)
-
-    while (data1.hasNext && data2.hasNext) {
-      val batch1 = data1.next()
-      val batch2 = data2.next()
-      batch1.data should be (batch2.data)
-      batch1.labels should be (batch2.labels)
-    }
-    data1.hasNext should be (false)
-    data2.hasNext should be (false)
-  }
-
-  "SampleToBatchPaddingLM " should "be good when batchsize > 1" in {
-    val input2 = Array(0.0f, 2.0f, 3.0f)
-    val target2 = Array(2.0f, 3.0f, 4.0f)
-    val input1 = Array(0.0f, 1.0f, 0.0f, 2.0f)
-    val target1 = Array(1.0f, 0.0f, 2.0f, 4.0f)
-    val input3 = Array(0.0f, 3.0f)
-    val target3 = Array(3.0f, 4.0f)
-    val labeledSentence1 = new LabeledSentence[Float](input1, target1)
-    val labeledSentence2 = new LabeledSentence[Float](input2, target2)
-    val labeledSentence3 = new LabeledSentence[Float](input3, target3)
-
-    val trainData =
-      Array[LabeledSentence[Float]](labeledSentence1, labeledSentence2, labeledSentence3)
-    val trainMaxLength = 5
-    val batchSize = 5
-    val dictionaryLength = 5
-    val trainSet = DataSet.array(trainData)
-      .transform(LabeledSentenceToSample(dictionaryLength))
-      .transform(SampleToBatchPaddingLM[Float](batchSize, 0, 4, true))
-
-    val iter = trainSet.toLocal().data(train = false)
-    val tensorInput = Tensor[Float](Storage(
-      Array(1.0f, 0, 0, 0, 0,
-        0, 1.0f, 0, 0, 0,
-        1.0f, 0, 0, 0, 0,
-        0, 0, 1.0f, 0, 0,
-        1.0f, 0, 0, 0, 0,
-        0, 0, 1.0f, 0, 0,
-        0, 0, 0, 1.0f, 0,
-        0, 0, 0, 0, 1.0f,
-        1.0f, 0, 0, 0, 0,
-        0, 0, 0, 1.0f, 0,
-        0, 0, 0, 0, 1.0f,
-        0, 0, 0, 0, 1.0f)), 1, Array(3, 4, 5))
-    val tensorTarget = Tensor[Float](Storage(
-      Array(2.0f, 1.0f, 3.0f, 5.0f,
-        3.0f, 4.0f, 5.0f, 1.0f,
-        4.0f, 5.0f, 1.0f, 1.0f)), 1, Array(3, 4))
-
-    val batch = iter.next()
-    batch.labels should be (tensorTarget)
-    batch.data should be (tensorInput)
-  }
-
-  "SampleToBatchPaddingLM " should "be same to SampleToBatch when no padding" in {
-    val batchSize = 3
-    val dictionaryLength = 5
-    val totalCount = 100
-    val trainData = new Array[Sample[Float]](totalCount)
-    var i = 0
-    var base = Array(1.0f)
-    while (i < totalCount) {
-      val input = Tensor[Float](10, dictionaryLength, 100).apply1(e => Random.nextFloat())
-      val label = Tensor[Float](10, 2).apply1(e => Random.nextFloat())
-      trainData(i) = new Sample[Float](input, label)
-      i += 1
-    }
-    val trainSet1 = DataSet.array(trainData)
-      .transform(SampleToBatchPaddingLM[Float](batchSize))
-    val trainSet2 = DataSet.array(trainData)
-      .transform(SampleToBatch(batchSize))
-
-    val data1 = trainSet1.toLocal().data(train = false)
-    val data2 = trainSet2.toLocal().data(train = false)
-
-    while (data1.hasNext && data2.hasNext) {
-      val batch1 = data1.next()
-      val batch2 = data2.next()
-      batch1.data should be (batch2.data)
-      batch1.labels should be (batch2.labels)
-    }
-    data1.hasNext should be (false)
-    data2.hasNext should be (false)
-  }
-
-  "SampleToBatchPadding " should "be good when padding to same length in one batch" in {
+  "SampleToBatchPadding " should "be good when padding first dimension to same length" +
+    "in one batch" in {
+    Engine.setNodeNumber(None)
     val batchSize = 3
     val dictionaryLength = 5
 
     val input2 = Tensor[Float](3, dictionaryLength).apply1(e => Random.nextFloat())
-    val target2 = Tensor(Storage(Array(2.0f, 1.0f)), 1, Array(2))
+    val target2 = Tensor(Storage(Array(2.0f)), 1, Array(1))
     val input1 = Tensor[Float](4, dictionaryLength).apply1(e => Random.nextFloat())
     val target1 = Tensor(Storage(Array(1.0f, 1.0f)), 1, Array(2))
     val input3 = Tensor[Float](2, dictionaryLength).apply1(e => Random.nextFloat())
-    val target3 = Tensor(Storage(Array(3.0f, 1.0f)), 1, Array(2))
+    val target3 = Tensor(Storage(Array(3.0f, 1.0f, 2.0f)), 1, Array(3))
 
     val sample1 = new Sample[Float](input1, target1)
     val sample2 = new Sample[Float](input2, target2)
     val sample3 = new Sample[Float](input3, target3)
 
+    val featurePadding = Tensor[Float](dictionaryLength).fill(100.0f)
+
     val trainData =
       Array[Sample[Float]](sample1, sample2, sample3, sample3, sample3, sample3)
     val trainSet = DataSet.array(trainData)
-      .transform(SampleToBatchPadding[Float](batchSize, true, Some(100.0f)))
+      .transform(SampleToBatch[Float](batchSize, Some(featurePadding), Some(10.0f)))
 
     val iter = trainSet.toLocal().data(train = false)
 
@@ -225,11 +68,11 @@ class BatchPaddingSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
     val tensorInput1 = Tensor[Float](Storage(storage1), 1, Array(3, 4, 5))
     val tensorTarget1 = Tensor[Float](Storage(
-      Array(1.0f, 1.0f, 2.0f, 1.0f, 3.0f, 1.0f)), 1, Array(3, 2))
+      Array(1.0f, 1.0f, 10.0f, 2.0f, 10.0f, 10.0f, 3.0f, 1.0f, 2.0f)), 1, Array(3, 3))
 
     val tensorInput2 = Tensor[Float](Storage(storage2), 1, Array(3, 2, 5))
     val tensorTarget2 = Tensor[Float](Storage(
-      Array(3.0f, 1.0f, 3.0f, 1.0f, 3.0f, 1.0f)), 1, Array(3, 2))
+      Array(3.0f, 1.0f, 2.0f, 3.0f, 1.0f, 2.0f, 3.0f, 1.0f, 2.0f)), 1, Array(3, 3))
 
     var batch = iter.next()
     var label = batch.labels
@@ -245,24 +88,27 @@ class BatchPaddingSpec extends FlatSpec with Matchers with BeforeAndAfter {
   }
 
   "SampleToBatchPadding " should "be good when padding to same length for all batch" in {
+    Engine.setNodeNumber(None)
     val batchSize = 3
     val dictionaryLength = 5
 
     val input1 = Tensor[Float](3, dictionaryLength).apply1(e => Random.nextFloat())
-    val target1 = Tensor(Storage(Array(2.0f, 1.0f)), 1, Array(2))
+    val target1 = Tensor(Storage(Array(2.0f)), 1, Array(1))
     val input2 = Tensor[Float](4, dictionaryLength).apply1(e => Random.nextFloat())
-    val target2 = Tensor(Storage(Array(1.0f, 1.0f)), 1, Array(2))
+    val target2 = Tensor(Storage(Array(1.0f, 1.0f, 10.0f, 4.0f, 2.0f)), 1, Array(5))
     val input3 = Tensor[Float](2, dictionaryLength).apply1(e => Random.nextFloat())
-    val target3 = Tensor(Storage(Array(3.0f, 1.0f)), 1, Array(2))
+    val target3 = Tensor(Storage(Array(3.0f, 1.0f, 4.0f)), 1, Array(3))
 
     val sample1 = new Sample[Float](input1, target1)
     val sample2 = new Sample[Float](input2, target2)
     val sample3 = new Sample[Float](input3, target3)
 
+    val featurePadding = Tensor[Float](dictionaryLength).fill(100.0f)
+
     val trainData =
       Array[Sample[Float]](sample1, sample2, sample3, sample3, sample3, sample3)
-    val trainSet = DataSet.array(trainData)
-      .transform(SampleToBatchPadding[Float](batchSize, true, Some(100.0f), Some(10)))
+    val trainSet = DataSet.array(trainData).transform(
+      SampleToBatch[Float](batchSize, Some(featurePadding), Some(80.0f), Some(10)))
 
     val iter = trainSet.toLocal().data(train = false)
 
@@ -280,11 +126,15 @@ class BatchPaddingSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
     val tensorInput1 = Tensor[Float](Storage(storage1), 1, Array(3, 10, 5))
     val tensorTarget1 = Tensor[Float](Storage(
-      Array(2.0f, 1.0f, 1.0f, 1.0f, 3.0f, 1.0f)), 1, Array(3, 2))
+      Array(2.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f,
+        1.0f, 1.0f, 10.0f, 4.0f, 2.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f,
+        3.0f, 1.0f, 4.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f)), 1, Array(3, 10))
 
     val tensorInput2 = Tensor[Float](Storage(storage2), 1, Array(3, 10, 5))
     val tensorTarget2 = Tensor[Float](Storage(
-      Array(3.0f, 1.0f, 3.0f, 1.0f, 3.0f, 1.0f)), 1, Array(3, 2))
+      Array(3.0f, 1.0f, 4.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f,
+        3.0f, 1.0f, 4.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f,
+        3.0f, 1.0f, 4.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f, 80.0f)), 1, Array(3, 10))
 
     var batch = iter.next()
     var label = batch.labels
@@ -300,12 +150,11 @@ class BatchPaddingSpec extends FlatSpec with Matchers with BeforeAndAfter {
   }
 
   "SampleToBatchPadding " should "be same to SampleToBatch when no padding" in {
+    Engine.setNodeNumber(None)
     val batchSize = 3
-    val dictionaryLength = 5
     val totalCount = 100
     val trainData = new Array[Sample[Float]](totalCount)
     var i = 0
-    var base = Array(1.0f)
     while (i < totalCount) {
       val input = Tensor[Float](3, 224, 224).apply1(e => Random.nextFloat())
       val label = Tensor[Float](2, 2).apply1(e => Random.nextFloat())
@@ -313,9 +162,9 @@ class BatchPaddingSpec extends FlatSpec with Matchers with BeforeAndAfter {
       i += 1
     }
     val trainSet1 = DataSet.array(trainData)
-      .transform(SampleToBatchPadding[Float](batchSize, false))
+      .transform(SampleToBatch[Float](batchSize))
     val trainSet2 = DataSet.array(trainData)
-      .transform(SampleToBatch(batchSize))
+      .transform(SampleToBatchNoPadding(batchSize))
 
     val data1 = trainSet1.toLocal().data(train = false)
     val data2 = trainSet2.toLocal().data(train = false)
@@ -328,5 +177,109 @@ class BatchPaddingSpec extends FlatSpec with Matchers with BeforeAndAfter {
     }
     data1.hasNext should be (false)
     data2.hasNext should be (false)
+  }
+
+  "SampleToBatchPadding " should "be same to LabeledSentenceToSample and SampleToBatch " +
+    "when padding" in {
+    Engine.setNodeNumber(None)
+    val batchSize = 3
+    val totalCount = 9
+    val trainData = new Array[LabeledSentence[Float]](totalCount)
+    var i = 0
+    var base = Array[Float](1.0f)
+    while (i < totalCount) {
+      val input = Array[Float](3998) ++ base
+      val label = base ++ Array[Float](3999)
+      trainData(i) = new LabeledSentence[Float](input, label)
+      base = base ++ Array[Float](i + 1)
+      i += 1
+    }
+
+    val dictionaryLength = 4001
+    val trainMaxLength = 10
+    val featurePadding = Tensor[Float](dictionaryLength).fill(0.0f)
+    featurePadding(4000) = 1
+    val trainSet1 = DataSet.array(trainData)
+      .transform(LabeledSentenceToSample(dictionaryLength))
+      .transform(SampleToBatch[Float]
+        (batchSize, Some(featurePadding), Some(3999), Some(trainMaxLength)))
+
+    val trainSet2 = DataSet.array(trainData)
+      .transform(LabeledSentenceToSample(dictionaryLength,
+        Some(trainMaxLength), Some(trainMaxLength)))
+      .transform(SampleToBatchNoPadding(batchSize))
+
+    val data1 = trainSet1.toLocal().data(train = false)
+    val data2 = trainSet2.toLocal().data(train = false)
+
+    while (data1.hasNext && data2.hasNext) {
+      val batch1 = data1.next()
+      val batch2 = data2.next()
+      batch1.data should be (batch2.data)
+      batch1.labels should be (batch2.labels)
+    }
+    data1.hasNext should be (false)
+    data2.hasNext should be (false)
+  }
+}
+
+object SampleToBatchNoPadding {
+  def apply[T: ClassTag]
+  (batchSize: Int)
+  (implicit ev: TensorNumeric[T]): SampleToBatchNoPadding[T]
+  = new SampleToBatchNoPadding[T](batchSize)
+}
+
+class SampleToBatchNoPadding[T: ClassTag]
+(totalBatch: Int)
+(implicit ev: TensorNumeric[T])
+  extends Transformer[Sample[T], MiniBatch[T]] {
+
+  override def apply(prev: Iterator[Sample[T]]): Iterator[MiniBatch[T]] = {
+    new Iterator[MiniBatch[T]] {
+      private var featureTensor: Tensor[T] = Tensor[T]()
+      private var labelTensor: Tensor[T] = Tensor[T]()
+      private var featureData: Array[T] = null
+      private var labelData: Array[T] = null
+      private val batchSize = Utils.getBatchSize(totalBatch)
+      private var featureSize: Array[Int] = null
+      private var labelSize: Array[Int] = null
+      private var oneFeatureLength: Int = 0
+      private var oneLabelLength: Int = 0
+      override def hasNext: Boolean = prev.hasNext
+
+      override def next(): MiniBatch[T] = {
+        if (prev.hasNext) {
+          var i = 0
+          while (i < batchSize && prev.hasNext) {
+            val sample = prev.next()
+            if(!sample.feature().isContiguous() || !sample.label().isContiguous()) {
+              throw new IllegalArgumentException(
+                "Only support contiguous tensor, pls use tensor.contiguous() before batching")
+            }
+            if (featureData == null) {
+              oneFeatureLength = sample.feature().nElement()
+              oneLabelLength = sample.label().nElement()
+              featureSize = Array(1) ++ sample.feature().size
+              labelSize = Array(1) ++ sample.label().size
+              featureData = new Array[T](batchSize * oneFeatureLength)
+              labelData = new Array[T](batchSize * oneLabelLength)
+            }
+            sample.copyFromLabel(labelData, i*oneLabelLength, oneLabelLength)
+            sample.copyFromFeature(featureData, i*oneFeatureLength, oneFeatureLength)
+            i += 1
+          }
+          featureSize(0) = i
+          labelSize(0) = i
+          featureTensor.set(Storage[T](featureData),
+            storageOffset = 1, sizes = featureSize)
+          labelTensor.set(Storage[T](labelData),
+            storageOffset = 1, sizes = labelSize)
+          MiniBatch(featureTensor, labelTensor)
+        } else {
+          null
+        }
+      }
+    }
   }
 }
