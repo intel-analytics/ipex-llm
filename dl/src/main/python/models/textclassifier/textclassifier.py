e758642d3493f1ed836eb507786c5717112550cf
zhichao-li <zhichao.li@intel.com>
2017-03-06 15:09:01 +0800

    simplify model

--- dl/src/main/python/models/textclassifier/textclassifier.py
+++ dl/src/main/python/models/textclassifier/textclassifier.py
@@ -18,31 +18,20 @@
 # Still in experimental stage!
 
 import itertools
-import os
 import re
-import sys
 from optparse import OptionParser
 
-import nltk
-import numpy as np
-
+from dataset import news20
 from nn.layer import *
 from optim.optimizer import *
 from util.common import *
 from util.common import Sample
-from dataset import news20
-
-# nltk.download('stopwords')
-# from nltk.corpus import stopwords  # Import the stop word list
-#
-# stops = set(stopwords.words("english"))
 
 
 def text_to_words(review_text):
     letters_only = re.sub("[^a-zA-Z]", " ", review_text)
     words = letters_only.lower().split()
     return words
-    # return [w for w in words if len(w) > 2 and w not in stops]
 
 
 def analyze_texts(data_rdd):
@@ -80,22 +69,12 @@ def build_model(class_num):
     model = Sequential()
 
     model.add(Reshape([embedding_dim, 1, sequence_len]))
-
     model.add(SpatialConvolution(embedding_dim, 128, 5, 1))
     model.add(ReLU())
-
     model.add(SpatialMaxPooling(5, 1, 5, 1))
-
     model.add(SpatialConvolution(128, 128, 5, 1))
     model.add(ReLU())
-
     model.add(SpatialMaxPooling(5, 1, 5, 1))
-
-    model.add(SpatialConvolution(128, 128, 5, 1))
-    model.add(ReLU())
-
-    model.add(SpatialMaxPooling(35, 1, 35, 1))
-
     model.add(Reshape([128]))
     model.add(Linear(128, 100))
     model.add(Linear(100, class_num))
@@ -108,22 +87,27 @@ def train(sc,
           sequence_len, max_words, embedding_dim, training_split):
     print('Processing text dataset')
     texts = news20.get_news20()
-    w2v = news20.get_glove_w2v()
-    w2v = sc.broadcast(w2v)
-    data_rdd = sc.parallelize(texts)
+    data_rdd = sc.parallelize(texts, 2)
 
     word_to_ic = analyze_texts(data_rdd)
 
     # Only take the top wc between [10, sequence_len]
     word_to_ic = dict(word_to_ic[10: max_words])
-    word_to_ic = sc.broadcast(word_to_ic)
+    bword_to_ic = sc.broadcast(word_to_ic)
+
+    w2v = news20.get_glove_w2v(dim=embedding_dim)
+    filtered_w2v = {w: v for w, v in w2v.items() if w in word_to_ic}
+    bfiltered_w2v = sc.broadcast(filtered_w2v)
+
     tokens_rdd = data_rdd.map(lambda (text, label):
-                              ([w for w in text_to_words(text) if w in word_to_ic.value], label))
+                              ([w for w in text_to_words(text) if
+                                w in bword_to_ic.value], label))
     padded_tokens_rdd = tokens_rdd.map(
         lambda (tokens, label): (pad(tokens, "##", sequence_len), label))
     vector_rdd = padded_tokens_rdd.map(lambda (tokens, label):
-                                       ([to_vec(w, w2v.value, embedding_dim) for w in tokens],
-                                        label))
+                                       ([to_vec(w, bfiltered_w2v.value,
+                                                embedding_dim) for w in
+                                         tokens], label))
     sample_rdd = vector_rdd.map(
         lambda (vectors, label): to_sample(vectors, label, embedding_dim))
 
@@ -138,9 +122,10 @@ def train(sc,
         model=build_model(news20.CLASS_NUM),
         training_rdd=train_rdd,
         criterion=ClassNLLCriterion(),
+        end_trigger=MaxEpoch(2),
+        batch_size=batch_size,
         optim_method="Adagrad",
-        state=state,
-        end_trigger=MaxEpoch(2))
+        state=state)
 
     optimizer.setvalidation(
         batch_size=batch_size,
@@ -148,17 +133,15 @@ def train(sc,
         trigger=EveryEpoch(),
         val_method=["top1"]
     )
-    model = optimizer.optimize()
-
+    train_model = optimizer.optimize()
 
 if __name__ == "__main__":
-
     parser = OptionParser()
     parser.add_option("-a", "--action", dest="action", default="train")
-    parser.add_option("-c", "--coreNum", dest="coreNum", default="4")
+    parser.add_option("-c", "--coreNum", dest="coreNum", default="2")
     parser.add_option("-n", "--nodeNum", dest="nodeNum", default="1")
     parser.add_option("-b", "--batchSize", dest="batchSize", default="128")
-    parser.add_option("-e", "--embedding_dim", dest="embedding_dim", default="100")  # noqa
+    parser.add_option("-e", "--embedding_dim", dest="embedding_dim", default="50")  # noqa
 
     (options, args) = parser.parse_args(sys.argv)
     if options.action == "train":
@@ -166,8 +149,8 @@ if __name__ == "__main__":
         core_num = int(options.coreNum)
         batch_size = int(options.batchSize)
         embedding_dim = int(options.embedding_dim)
-        sequence_len = 1000
-        max_words = 20000
+        sequence_len = 50
+        max_words = 1000
         training_split = 0.8
         sparkConf = create_spark_conf(node_num, core_num)
         sc = SparkContext(appName="text_classifier", conf=sparkConf)
