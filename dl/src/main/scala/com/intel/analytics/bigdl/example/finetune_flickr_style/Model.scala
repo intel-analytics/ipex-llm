ac0b989147c2df25627a9bfe39273d02326c4f0d
jenniew <jenniewang123@gmail.com>
2017-03-07 19:59:09 -0800

    update style

--- dl/src/main/scala/com/intel/analytics/bigdl/example/finetune_flickr_style/Model.scala
+++ dl/src/main/scala/com/intel/analytics/bigdl/example/finetune_flickr_style/Model.scala
@@ -1,8 +1,8 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
+ * Licensed to Intel Corporation under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
+ * Intel Corporation licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.intel.analytics.bigdl.example.finetune_flickr_style
 
 import com.intel.analytics.bigdl._
