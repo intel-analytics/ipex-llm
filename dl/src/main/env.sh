432c12e47efbb61b099831ad88ea0fc87c6fb15e
neil <lightbulb@sjtu.edu.cn>
2017-03-09 21:32:46 +0800

    clean unnecessary file

--- dl/src/main/env.sh
+++
@@ -1,2 +0,0 @@
-export PYTHONPATH=/home/arda/bin/spark-1.6.0-bin-hadoop2.6/python:~/BigDL/dl/src/main/python
-export SPARK_HOME=/home/arda/bin/spark-1.6.0-bin-hadoop2.6 
--- env.sh
+++
@@ -1,3 +0,0 @@
-export SPARK_CLASSPATH=~/BigDL/dl/target/bigdl-0.1.0-SNAPSHOT-jar-with-dependencies.jar
-export PYTHONPATH=~/bin/spark-1.6.0-bin-hadoop2.6/python:~/BigDL/dl/src/main/python:$PYTHONPATH
-
