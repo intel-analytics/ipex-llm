da1b35ad9cac6dcee1d8bc4dc42f833155607703
JerryYanWan <ywanad@connect.ust.hk>
2017-03-15 16:12:52 +0800

    bi Recurrent
    revise headlings
    
    remove Identity layer for brnn; add a timeDim as a parameter in Constructor
    
    revise BiRecurrent to container

---
+++ dl/src/main/scala/com/intel/analytics/bigdl/nn/BiRecurrent.scala
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.nn
+
+import com.intel.analytics.bigdl._
+import com.intel.analytics.bigdl.nn.abstractnn.{AbstractModule, Activity, TensorModule}
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.utils.T
+
+import scala.reflect.ClassTag
+
+class BiRecurrent[T : ClassTag] ()
+  (implicit ev: TensorNumeric[T]) extends Container[Tensor[T], Tensor[T], T] {
+
+  val timeDim = 2
+  val layer: Recurrent[T] = Recurrent[T]()
+  val revLayer: Recurrent[T] = Recurrent[T]()
+  val birnn = Sequential[T]()
+      .add(ConcatTable()
+        .add(Identity[T]())
+        .add(Identity[T]()))
+      .add(ParallelTable[T]()
+        .add(layer)
+        .add(Sequential[T]()
+          .add(Reverse[T](timeDim))
+          .add(revLayer)
+          .add(Reverse[T](timeDim))))
+      .add(JoinTable[T](timeDim, 2))
+
+  override def add(module: AbstractModule[_ <: Activity, _ <: Activity, T]):
+    BiRecurrent.this.type = {
+    layer.add(module)
+    revLayer.add(module.cloneModule())
+    this
+  }
+
+  override def updateOutput(input: Tensor[T]): Tensor[T] = {
+    output = birnn.updateOutput(input).toTensor[T]
+    output
+  }
+
+  override def accGradParameters(input: Tensor[T], gradOutput: Tensor[T],
+                                 scale: Double = 1.0): Unit = {
+    birnn.accGradParameters(input, gradOutput, scale)
+  }
+
+  override def updateGradInput(input: Tensor[T], gradOutput: Tensor[T]): Tensor[T] = {
+    gradInput = birnn.updateGradInput(input, gradOutput).toTensor[T]
+    gradInput
+  }
+
+  /**
+   * This function returns two arrays. One for the weights and the other the gradients
+   * Custom modules should override this function if they have parameters
+   *
+   * @return (Array of weights, Array of grad)
+   */
+  override def parameters(): (Array[Tensor[T]], Array[Tensor[T]]) = birnn.parameters()
+
+  override def updateParameters(learningRate: T): Unit = birnn.updateParameters(learningRate)
+
+  /**
+   * If the module has parameters, this will zero the accumulation of the gradients with respect
+   * to these parameters. Otherwise, it does nothing.
+   */
+  override def zeroGradParameters(): Unit = birnn.zeroGradParameters()
+
+  override def training(): BiRecurrent.this.type = {
+    super.training()
+    birnn.training()
+    this
+  }
+
+  override def evaluate(): BiRecurrent.this.type = {
+    super.evaluate()
+    birnn.evaluate()
+    this
+  }
+
+  override def canEqual(other: Any): Boolean = other.isInstanceOf[BiRecurrent[T]]
+
+
+  /**
+   * Clear cached activities to save storage space or network bandwidth. Note that we use
+   * Tensor.set to keep some information like tensor share
+   *
+   * The subclass should override this method if it allocate some extra resource, and call the
+   * super.clearState in the override method
+   *
+   * @return
+   */
+  override def clearState(): BiRecurrent.this.type = {
+    birnn.clearState()
+    this
+  }
+
+  override def toString(): String = s"BiRecurrent($timeDim, $birnn)"
+
+  override def equals(other: Any): Boolean = other match {
+    case that: BiRecurrent[T] =>
+      super.equals(that) &&
+        (that canEqual this) &&
+        birnn == that.birnn
+    case _ => false
+  }
+
+  override def hashCode(): Int = {
+    val state = Seq(super.hashCode(), birnn)
+    state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+  }
+}
+
+object BiRecurrent {
+  def apply[@specialized(Float, Double) T: ClassTag]()
+    (implicit ev: TensorNumeric[T]) : BiRecurrent[T] = {
+    new BiRecurrent[T]()
+  }
+}
---
+++ dl/src/test/scala/com/intel/analytics/bigdl/nn/BiRecurrentSpec.scala
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.torch
+
+import java.io.PrintWriter
+
+import com.intel.analytics.bigdl.nn._
+import com.intel.analytics.bigdl.numeric.NumericDouble
+import com.intel.analytics.bigdl.optim.SGD
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.utils.RandomGenerator._
+import com.intel.analytics.bigdl.utils.T
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+import scala.sys.process._
+
+@com.intel.analytics.bigdl.tags.Serial
+class BiRecurrentSpec  extends FlatSpec with BeforeAndAfter with Matchers {
+  before {
+    if (!TH.hasTorch()) {
+      cancel("Torch is not installed")
+    }
+    val tmpFile = java.io.File.createTempFile("checkRNN", ".lua")
+    val writer = new PrintWriter(tmpFile)
+    writer.write("exist = (pcall(require, 'rnn'))\n print(exist)")
+    writer.close()
+
+    val existsRNN =
+      Seq(System.getProperty("torch_location", "th"), tmpFile.getAbsolutePath).!!.trim
+    if (!existsRNN.contains("true")) {
+      cancel("Torch rnn is not installed")
+    }
+  }
+
+  "A BiRecurrent " should "has same loss as torch rnn" in {
+
+    val hiddenSize = 4
+    val linearHidden = 8
+    val inputSize = 6
+    val outputSize = 5
+    val bpttTruncate = 3
+    val seqLength = 5
+    val seed = 100
+
+    val input = Tensor[Double](Array(1, seqLength, inputSize))
+    val labels = Tensor[Double](Array(1, seqLength))
+    for (i <- 1 to seqLength) {
+      val rdmLabel = Math.ceil(math.random * outputSize).toInt
+      val rdmInput = Math.ceil(math.random * inputSize).toInt
+      input.setValue(1, i, rdmInput, 1.0)
+      labels.setValue(1, i, rdmLabel)
+    }
+
+    RNG.setSeed(seed)
+
+    val brec = BiRecurrent[Double]()
+      .add(RnnCell[Double](inputSize, hiddenSize, Sigmoid[Double]()))
+
+    val model = Sequential[Double]()
+      .add(brec)
+      .add(TimeDistributed[Double](Linear[Double](linearHidden, outputSize)))
+    val criterion = TimeDistributedCriterion[Double](
+      CrossEntropyCriterion[Double]())
+    val logSoftMax = TimeDistributed[Double](LogSoftMax[Double]())
+
+    val (weights, grad) = model.getParameters()
+    val code =
+      s"""
+         |
+      |-- 1.4. Combine 1.1 and 1.3 to produce final model
+         |require 'rnn'
+         |torch.manualSeed($seed)
+         |
+      |local rm =  nn.Sequential() -- input is {x[t], h[t-1]}
+         |         :add(nn.ParallelTable()
+         |            :add(nn.Linear($inputSize, $hiddenSize)) -- input layer
+         |            :add(nn.Linear($hiddenSize, $hiddenSize))) -- recurrent layer
+         |         :add(nn.CAddTable()) -- merge
+         |         :add(nn.Sigmoid()) -- transfer
+         |     --    :add(nn.Tanh()) -- transfer
+         |
+      |  local rm1 =  nn.Sequential() -- input is {x[t], h[t-1]}
+         |         :add(nn.ParallelTable()
+         |            :add(nn.Linear($inputSize, $hiddenSize)) -- input layer
+         |            :add(nn.Linear($hiddenSize, $hiddenSize))) -- recurrent layer
+         |         :add(nn.CAddTable()) -- merge
+         |         :add(nn.Sigmoid()) -- transfer
+         |      rnn = nn.Recurrence(rm, $hiddenSize, 1)
+         |      rnn1 = nn.Recurrence(rm1, $hiddenSize, 1)
+         | --     rnn.userPrevOutput = torch.Tensor(1, $hiddenSize):zero()
+         |
+      |model = nn.Sequential()
+         |:add(nn.SplitTable(1))
+         |  :add(nn.BiSequencer(rnn, rnn1))
+         |  :add(nn.JoinTable(1, 5))
+         |--:add(nn.Sequencer(
+         |-- nn.Sequential()
+         |--   --:add(nn.LSTM($inputSize, $hiddenSize, 1, true))
+         |--   :add(nn.FastLSTM($inputSize, $hiddenSize))
+         |   :add(nn.Linear($linearHidden, $outputSize))
+         |--   ))
+         |
+         |
+         |local parameters, gradParameters = model:getParameters()
+         |model:zeroGradParameters()
+         |parameters:copy(weights)
+         |
+         |parameters_initial = parameters : clone()
+         |gradParameters_initial = gradParameters : clone()
+         |
+         |local criterion =  nn.SequencerCriterion(nn.CrossEntropyCriterion())
+         |
+      |
+      |state = {
+         |  learningRate = 0.5,
+         |  momentum = 0.0,
+         |  dampening = 0.0,
+         |  weightDecay = 0.0
+         |}
+         |
+      |feval = function(x)
+         |model:zeroGradParameters()
+         |model_initial = model : clone()
+         |
+      |local output1 = model:forward(input)
+         |local err1 = criterion:forward(output1, labels)
+         |local gradOutput1 = criterion:backward(output1, labels)
+         |model:backward(input, gradOutput1)
+         |return err1, gradParameters
+         |end
+         |
+      |for i = 1,10,1 do
+         |   optim.sgd(feval, parameters, state)
+         |end
+         |
+         |labels = labels
+         |err=criterion.output
+         |err2=criterion.gradInput
+         |output = model.output
+         |gradInput = model.gradInput
+    """.stripMargin
+
+    val (luaTime, torchResult) = TH.run(code,
+      Map("input" -> input.transpose(1, 2), "weights" -> weights,
+        "labels" -> labels(1)),
+        // "labels" -> SplitTable[Double](1).forward(labels.t())),
+      Array("err", "parameters", "gradParameters", "output", "gradInput", "err2", "labels"))
+
+    val luaOutput2 = torchResult("err").asInstanceOf[Double]
+    val luaweight = torchResult("parameters").asInstanceOf[Tensor[Double]]
+
+    val state = T("learningRate" -> 0.5, "momentum" -> 0.0,
+      "weightDecay" -> 0.0, "dampening" -> 0.0)
+    val sgd = new SGD[Double]
+    def feval(x: Tensor[Double]): (Double, Tensor[Double]) = {
+      val output = model.forward(input).asInstanceOf[Tensor[Double]]
+      val _loss = criterion.forward(output, labels)
+      model.zeroGradParameters()
+      val gradInput = criterion.backward(output, labels)
+      model.backward(input, gradInput)
+      (_loss, grad)
+    }
+
+    val start = System.nanoTime()
+    var loss: Array[Double] = null
+    for (i <- 1 to 10) {
+      loss = sgd.optimize(feval, weights, state)._2
+      println(s"${i}-th loss = ${loss(0)}")
+    }
+    val end = System.nanoTime()
+    println("Time: " + (end - start) / 1E6)
+
+    val output = model.output.toTensor[Double]
+    val logOutput = logSoftMax.forward(output)
+    val prediction = logOutput.max(3)._2
+
+    luaOutput2 should be(loss(0) +- 1e-5)
+  }
+
+}
