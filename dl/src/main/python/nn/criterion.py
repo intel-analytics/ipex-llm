b0ca91efc6375921f09ee4621745e33bee754b59
plrectco <lightbulb@sjtu.edu.cn>
2017-03-17 16:51:51 +0800

    add more nn layer

--- dl/src/main/python/nn/criterion.py
+++ dl/src/main/python/nn/criterion.py
@@ -58,3 +58,208 @@ class MSECriterion(Criterion):
     '''
     def __init__(self, bigdl_type="float"):
             JavaValue.__init__(self, None, bigdl_type)
+
+
+class AbsCriterion(Criterion):
+
+    '''
+    >>> absCriterion = AbsCriterion(True)
+    creating: createAbsCriterion
+    '''
+
+    def __init__(self,
+                 size_average=True,
+                 bigdl_type="float"):
+        super(AbsCriterion, self).__init__(None, bigdl_type,
+                                           size_average)
+
+
+class ClassSimplexCriterion(Criterion):
+
+    '''
+    >>> classSimplexCriterion = ClassSimplexCriterion(2)
+    creating: createClassSimplexCriterion
+    '''
+
+    def __init__(self,
+                 n_classes,
+                 bigdl_type="float"):
+        super(ClassSimplexCriterion, self).__init__(None, bigdl_type,
+                                                    n_classes)
+
+
+class CosineEmbeddingCriterion(Criterion):
+
+    '''
+    >>> cosineEmbeddingCriterion = CosineEmbeddingCriterion(1e-5, True)
+    creating: createCosineEmbeddingCriterion
+    '''
+
+    def __init__(self,
+                 margin=0.0,
+                 size_average=True,
+                 bigdl_type="float"):
+        super(CosineEmbeddingCriterion, self).__init__(None, bigdl_type,
+                                                       margin,
+                                                       size_average)
+
+
+class DistKLDivCriterion(Criterion):
+
+    '''
+    >>> distKLDivCriterion = DistKLDivCriterion(True)
+    creating: createDistKLDivCriterion
+    '''
+
+    def __init__(self,
+                 size_average=True,
+                 bigdl_type="float"):
+        super(DistKLDivCriterion, self).__init__(None, bigdl_type,
+                                                 size_average)
+
+
+class HingeEmbeddingCriterion(Criterion):
+
+    '''
+    >>> hingeEmbeddingCriterion = HingeEmbeddingCriterion(1e-5, True)
+    creating: createHingeEmbeddingCriterion
+    '''
+
+    def __init__(self,
+                 margin=1,
+                 size_average=True,
+                 bigdl_type="float"):
+        super(HingeEmbeddingCriterion, self).__init__(None, bigdl_type,
+                                                      margin,
+                                                      size_average)
+
+
+class L1HingeEmbeddingCriterion(Criterion):
+
+    '''
+    >>> l1HingeEmbeddingCriterion = L1HingeEmbeddingCriterion(1e-5)
+    creating: createL1HingeEmbeddingCriterion
+    '''
+
+    def __init__(self,
+                 margin=1,
+                 bigdl_type="float"):
+        super(L1HingeEmbeddingCriterion, self).__init__(None, bigdl_type,
+                                                        margin)
+
+
+class MarginCriterion(Criterion):
+
+    '''
+    >>> marginCriterion = MarginCriterion(1e-5, True)
+    creating: createMarginCriterion
+    '''
+
+    def __init__(self,
+                 margin=1.0,
+                 size_average=True,
+                 bigdl_type="float"):
+        super(MarginCriterion, self).__init__(None, bigdl_type,
+                                              margin,
+                                              size_average)
+
+
+class MarginRankingCriterion(Criterion):
+
+    '''
+    >>> marginRankingCriterion = MarginRankingCriterion(1e-5, True)
+    creating: createMarginRankingCriterion
+    '''
+
+    def __init__(self,
+                 margin=1.0,
+                 size_average=True,
+                 bigdl_type="float"):
+        super(MarginRankingCriterion, self).__init__(None, bigdl_type,
+                                                     margin,
+                                                     size_average)
+
+
+class MultiCriterion(Criterion):
+
+    '''
+    >>> multiCriterion = MultiCriterion()
+    creating: createMultiCriterion
+    '''
+
+    def __init__(self,
+                 bigdl_type="float"):
+        super(MultiCriterion, self).__init__(None, bigdl_type)
+
+
+class MultiLabelMarginCriterion(Criterion):
+
+    '''
+    >>> multiLabelMarginCriterion = MultiLabelMarginCriterion(True)
+    creating: createMultiLabelMarginCriterion
+    '''
+
+    def __init__(self,
+                 size_average=True,
+                 bigdl_type="float"):
+        super(MultiLabelMarginCriterion, self).__init__(None, bigdl_type,
+                                                        size_average)
+
+
+class ParallelCriterion(Criterion):
+
+    '''
+    >>> parallelCriterion = ParallelCriterion(True)
+    creating: createParallelCriterion
+    '''
+
+    def __init__(self,
+                 repeat_target=False,
+                 bigdl_type="float"):
+        super(ParallelCriterion, self).__init__(None, bigdl_type,
+                                                repeat_target)
+
+
+class SmoothL1Criterion(Criterion):
+
+    '''
+    >>> smoothL1Criterion = SmoothL1Criterion(True)
+    creating: createSmoothL1Criterion
+    '''
+
+    def __init__(self,
+                 size_average=True,
+                 bigdl_type="float"):
+        super(SmoothL1Criterion, self).__init__(None, bigdl_type,
+                                                size_average)
+
+
+class SmoothL1CriterionWithWeights(Criterion):
+
+    '''
+    >>> smoothL1CriterionWithWeights = SmoothL1CriterionWithWeights(1e-5, 1)
+    creating: createSmoothL1CriterionWithWeights
+    '''
+
+    def __init__(self,
+                 sigma,
+                 num=0,
+                 bigdl_type="float"):
+        super(SmoothL1CriterionWithWeights, self).__init__(None, bigdl_type,
+                                                           sigma,
+                                                           num)
+
+
+class SoftmaxWithCriterion(Criterion):
+
+    '''
+    >>> softmaxWithCriterion = SoftmaxWithCriterion("VALID")
+    creating: createSoftmaxWithCriterion
+    '''
+
+    def __init__(self,
+                 normalize_mode="VALID",
+                 bigdl_type="float"):
+        super(SoftmaxWithCriterion, self).__init__(None, bigdl_type,
+                                                   None,
+                                                   normalize_mode)
