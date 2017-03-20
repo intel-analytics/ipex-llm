7059358cf34847d599d148392d7c262891072218
zhichao-li <zhichao.li@intel.com>
2017-03-20 10:21:42 +0800

    add a default value for View

--- dl/src/main/python/nn/layer.py
+++ dl/src/main/python/nn/layer.py
@@ -324,13 +324,13 @@ class Dropout(Model):
 
 class View(Model):
     '''
-    >>> view = View([1024,2],2)
+    >>> view = View([1024,2])
     creating: createView
     '''
 
     def __init__(self,
                  sizes,
-                 num_input_dims,
+                 num_input_dims=0,
                  bigdl_type="float"):
         super(View, self).__init__(None, bigdl_type,
                                    sizes,
