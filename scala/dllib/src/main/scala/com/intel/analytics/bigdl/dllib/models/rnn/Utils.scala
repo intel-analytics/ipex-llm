d15a8cbccf84041712693d2e32b5a3d9bc771250
JerryYanWan <ywanad@connect.ust.hk>
2017-03-22 13:06:14 +0800

    rnn running on Spark Cluster
    modify readme
    
    modify SampleToBatch

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/models/rnn/Utils.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/models/rnn/Utils.scala
@@ -18,32 +18,50 @@ package com.intel.analytics.bigdl.models.rnn
 import scopt.OptionParser
 import java.io._
 
-import com.intel.analytics.bigdl.dataset.text.LabeledSentence
+import com.intel.analytics.bigdl._
+import com.intel.analytics.bigdl.dataset.DataSet
+import com.intel.analytics.bigdl.dataset.text._
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 
 import scala.util.Random
 import org.apache.log4j.Logger
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+
+import scala.io.Source
+import scala.reflect.ClassTag
 
 object Utils {
+
   case class TrainParams(
-    folder: String =
-      "./",
-    modelSnapshot: Option[String] = None,
-    stateSnapshot: Option[String] = None,
-    checkpoint: Option[String] = None,
-    learningRate: Double = 0.1,
-    momentum: Double = 0.0,
-    weightDecay: Double = 0.0,
-    dampening: Double = 0.0,
-    hiddenSize: Int = 40,
-    vocabSize: Int = 4000,
-    bptt: Int = 4,
-    nEpochs: Int = 30
-  )
+                          dataFolder: String = "./",
+                          saveFolder: String = "./",
+                          modelSnapshot: Option[String] = None,
+                          stateSnapshot: Option[String] = None,
+                          checkpoint: Option[String] = None,
+                          batchSize: Int = 128,
+                          learningRate: Double = 0.1,
+                          momentum: Double = 0.0,
+                          weightDecay: Double = 0.0,
+                          dampening: Double = 0.0,
+                          hiddenSize: Int = 40,
+                          vocabSize: Int = 4000,
+                          bptt: Int = 4,
+                          nEpochs: Int = 30,
+                          sentFile: Option[String] = None,
+                          tokenFile: Option[String] = None,
+                          overWriteCheckpoint: Boolean = false)
 
   val trainParser = new OptionParser[TrainParams]("BigDL SimpleRNN Train Example") {
-    opt[String]('f', "folder")
+    opt[String]('f', "dataFolder")
       .text("where you put the text data")
-      .action((x, c) => c.copy(folder = x))
+      .action((x, c) => c.copy(dataFolder = x))
+      .required()
+
+    opt[String]('s', "saveFolder")
+      .text("where you save the processed text data")
+      .action((x, c) => c.copy(saveFolder = x))
+      .required()
 
     opt[String]("model")
       .text("model snapshot location")
@@ -55,6 +73,11 @@ object Utils {
     opt[String]("checkpoint")
       .text("where to cache the model and state")
       .action((x, c) => c.copy(checkpoint = Some(x)))
+
+    opt[Int]('b', "batchSize")
+      .text("batchSize of rnn")
+      .action((x, c) => c.copy(batchSize = x))
+      .required()
 
     opt[Double]('r', "learningRate")
       .text("learning rate")
@@ -87,18 +110,32 @@ object Utils {
     opt[Int]('e', "nEpochs")
       .text("epoch numbers")
       .action((x, c) => c.copy(nEpochs = x))
+
+    opt[String]("sent")
+      .text("sentence dictionary to split document into sentences")
+      .action((x, c) => c.copy(sentFile = Some(x)))
+
+    opt[String]("token")
+      .text("token dictionary to split sentence into tokens")
+      .action((x, c) => c.copy(tokenFile = Some(x)))
+
+    opt[Unit]("overWrite")
+      .text("overwrite checkpoint files")
+      .action( (_, c) => c.copy(overWriteCheckpoint = true) )
   }
 
   case class TestParams(
      folder: String = "./",
      modelSnapshot: Option[String] = None,
-     numOfWords: Option[Int] = None
+     numOfWords: Option[Int] = None,
+     batchSize: Int = 2
   )
 
   val testParser = new OptionParser[TestParams]("BigDL rnn Test Example") {
     opt[String]('f', "folder")
-      .text("where you put the text data")
+      .text("where you put the dictionary data")
       .action((x, c) => c.copy(folder = x))
+      .required()
 
     opt[String]("model")
       .text("model snapshot location")
@@ -109,8 +146,12 @@ object Utils {
       .text("number of words to write")
       .action((x, c) => c.copy(numOfWords = Some(x)))
       .required()
-  }
 
+    opt[Int]('b', "batchSize")
+      .text("batchSize of rnn")
+      .action((x, c) => c.copy(batchSize = x))
+      .required()
+  }
 
   private[bigdl] def readSentence(directory: String)
   : Array[Array[String]] = {
@@ -122,180 +163,32 @@ object Utils {
       .getLines().map(_.split("\\W+")).toArray
     lines
   }
+}
 
-  class Dictionary()
-  extends Serializable {
+object SequencePreprocess {
+  def apply(
+    fileName: String,
+    sc: SparkContext,
+    sentBin: Option[String],
+    tokenBin: Option[String])
+  : DataSet[Array[String]] = {
 
-    private var vocab2index: Map[String, Int] = null
-    private var index2vocab: Map[Int, String] = null
-    private var vocabLength: Int = 0
-    private var discard: Array[String] = null
-    private var discardLength: Int = 0
-
-    val logger = Logger.getLogger(getClass)
-
-    def this(directory: String) = {
-      this()
-
-      if (!new File(directory + "/dictionary.txt").exists()) {
-        throw new IllegalArgumentException("dictionary file not exists!")
-      }
-      if (!new File(directory + "/discard.txt").exists()) {
-        throw new IllegalArgumentException("discard file not exists!")
-      }
-
-      import scala.io.Source
-      vocab2index = Source.fromFile(directory + "/dictionary.txt")
-        .getLines.map(_.stripLineEnd.split("->", -1))
-        .map(fields => fields(0).stripSuffix(" ") -> fields(1).stripPrefix(" ").toInt)
-        .toMap[String, Int]
-      index2vocab = vocab2index.map(x => (x._2, x._1))
-      vocabLength = vocab2index.size
-      discard = Source.fromFile(directory + "/discard.txt")
-        .getLines().toArray
-      discardLength = discard.length
-    }
-
-    def getIndex(word: String): Int = {
-      vocab2index.getOrElse(word, vocabLength)
-    }
-
-    def getWord(index: Float): String = {
-      getWord(index.toInt)
-    }
-
-    def getWord(index: Double): String = {
-      getWord(index.toInt)
-    }
-
-    def getWord(index: Int): String = {
-      index2vocab.getOrElse(index,
-        discard(Random.nextInt(discardLength)))
-    }
-
-    def length(): Int = vocabLength
-
-    def print(): Unit = {
-      vocab2index.foreach(x =>
-        logger.info(x._1 + " -> " + x._2))
-    }
-
-    def printDiscard(): Unit = {
-      discard.foreach(x =>
-        logger.info(x))
-    }
+    val sentenceSplitter = SentenceSplitter(sentBin)
+    val sentenceTokenizer = SentenceTokenizer(tokenBin)
+    val lines = load(fileName)
+    val tokens = DataSet.array(lines, sc)
+      .transform(sentenceSplitter).toDistributed().data(false)
+      .collect().flatten
+    DataSet.array(tokens, sc)
+      .transform(SentenceBiPadding())
+      .transform(sentenceTokenizer)
   }
 
-  class WordTokenizer(
-    inputFile: String,
-    saveDirectory: String,
-    dictionaryLength: Int)
-    extends Serializable {
-
-    def process() {
-      if (!new File(saveDirectory + "/mapped_data.txt").exists) {
-        import scala.io.Source
-
-        val lines = Source.fromFile(inputFile).getLines.toArray
-          .filter(_.length > 0)
-
-        // Special Words
-        val sentence_start_token = "SENTENCE_START"
-        val sentence_end_token = "SENTENCE_END"
-        val unknown_token = "UNKNOWN_TOKEN"
-
-        // Create dictionary with frequency as value for each word
-        val sentences = lines.map(x => sentence_start_token + " " + x + " " + sentence_end_token)
-        val freqDict = sentences.flatMap(_.split("\\W+"))
-          .foldLeft(Map.empty[String, Int]) {
-            (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
-          }.toSeq.sortBy(_._2)
-
-        // Select most common words
-        val length = math.min(dictionaryLength - 1, freqDict.length)
-        val vocabDict = freqDict.drop(freqDict.length - length).map(_._1)
-        val vocabSize = vocabDict.length
-        val word2index = vocabDict.zipWithIndex.toMap
-        val discardDict = freqDict.take(freqDict.length - length).map(_._1)
-
-        // save dictionary
-        new PrintWriter(saveDirectory + "/dictionary.txt") {
-          write(word2index.mkString("\n")); close
-        }
-
-        // save discard dictionary
-        new PrintWriter(saveDirectory + "/discard.txt") {
-          write(discardDict.mkString("\n")); close
-        }
-
-        // Convert the string texts to integer arrays
-        val mappedDF = sentences.map(x => x.split("\\W+")
-          .map(word => word2index.getOrElse(word, vocabSize)))
-
-        // save converted data
-        new PrintWriter(saveDirectory + "/mapped_data.txt") {
-          write(mappedDF.map(_.mkString(",")).mkString("\n")); close
-        }
-      }
-    }
-  }
-
-  private[bigdl] def loadInData(filedirect: String, dictionarySize: Int)
-  : (Array[LabeledSentence[Float]], Array[LabeledSentence[Float]],
-    Int, Int) = {
-
+  private[bigdl] def load(fileName: String): Array[String] = {
     import scala.io.Source
-
-    val logData = Source.fromFile(filedirect + "/mapped_data.txt").getLines().toArray
-    val dataFlow = logData.map(x => {
-      val seq = x.split(",").toList.asInstanceOf[Seq[Int]]
-      (seq.take(seq.length - 1), seq.drop(1))
-    })
-
-    val length = dataFlow.length
-    val seq = Random.shuffle((1 to length).toList)
-    val seqTrain = seq.take(Math.floor(seq.length*0.8).toInt).toArray
-    val seqVal = seq.drop(Math.floor(seq.length*0.8).toInt).toArray
-
-    val trainFlow = seqTrain.collect(dataFlow)
-    val valFlow = seqVal.collect(dataFlow)
-
-    var trainMaxLength = 0
-
-    val trainData = trainFlow.map(x => {
-      val data = x._1
-      val label = x._2
-      val numOfWords = data.length
-      trainMaxLength = math.max(trainMaxLength, numOfWords)
-      val input = new Array[Float](numOfWords)
-      val target = new Array[Float](numOfWords)
-      var i = 0
-      while (i < numOfWords) {
-        input(i) = data(i).toString.toInt.toFloat
-        target(i) = label(i).toString.toInt.toFloat
-        i += 1
-      }
-      new LabeledSentence[Float](input, target)
-    })
-
-    var valMaxLength = 0
-
-    val valData = valFlow.map(x => {
-      val data = x._1
-      val label = x._2
-      val numOfWords = data.length
-      valMaxLength = math.max(valMaxLength, numOfWords)
-      val input = new Array[Float](numOfWords)
-      val target = new Array[Float](numOfWords)
-      var i = 0
-      while (i < numOfWords) {
-        input(i) = data(i).toString.toInt.toFloat
-        target(i) = label(i).toString.toInt.toFloat
-        i += 1
-      }
-      new LabeledSentence[Float](input, target)
-    })
-
-    (trainData, valData, trainMaxLength, valMaxLength)
+    require(new File(fileName).exists(),
+      s"data file ${fileName} not exists!")
+    val lines = Source.fromFile(fileName).getLines().toArray
+    lines
   }
 }
