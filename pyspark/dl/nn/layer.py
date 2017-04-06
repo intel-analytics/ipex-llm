9abd8f7729048098c10e920ae4c786cd698e3c30
Yao <psyyz10@gmail.com>
2017-04-06 09:42:26 +0800

    add dropout to lstmPeepwhole

--- pyspark/dl/nn/layer.py
+++ pyspark/dl/nn/layer.py
@@ -462,7 +462,7 @@ class GRU(Model):
     creating: createGRU
     '''
 
-    def __init__(self,  input_size, hidden_size, p=0.0, bigdl_type="double"):
+    def __init__(self,  input_size, hidden_size, p=0.0, bigdl_type="float"):
         super(GRU, self).__init__(None, bigdl_type, input_size, hidden_size, p)
 
 
