60c8e487fa12a8f1cce8224613067afc193559cb
zhichao-li <zhichao.li@intel.com>
2017-04-28 16:07:37 +0800

    python 3.5 support

--- pyspark/dl/nn/layer.py
+++ pyspark/dl/nn/layer.py
@@ -102,7 +102,7 @@ class Model(JavaValue):
         if type(output) is JTensor:
             return output.to_ndarray()
         else:
-            return map(lambda x: x.to_ndarray(), output)
+            return [x.to_ndarray() for x in output]
 
     def forward(self, input):
         """
@@ -155,10 +155,10 @@ class Model(JavaValue):
             return {
                 param_name: np.array(values[0],
                                      dtype=self.get_dtype()).reshape(
-                    values[1]) for param_name, values in params.iteritems()}
+                    values[1]) for param_name, values in params.items()}
 
         return {layer_name: to_ndarray(params) for layer_name, params in
-                name_to_params.iteritems()}
+                name_to_params.items()}
 
     def predict(self, data_rdd):
         """
