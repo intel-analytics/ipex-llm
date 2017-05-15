81c9b2954cf1a5f52224b45daff022a454464b95
zhichao-li <zhichao.li@intel.com>
2017-05-15 10:05:49 +0800

    header

--- pyspark/dl/dataset/news20.py
+++ pyspark/dl/dataset/news20.py
@@ -1,7 +1,22 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import tarfile
 from dataset import base
 import os
-import sys
 
 NEWS20_URL = 'http://qwone.com/~jason/20Newsgroups/20news-19997.tar.gz'  # noqa
 GLOVE_URL = 'http://nlp.stanford.edu/data/glove.6B.zip'  # noqa
