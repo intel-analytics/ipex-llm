d7184526383b3bd6df81858c4f4e10bb40fc05b5
ding <ding@localhost.localdomain>
2017-05-15 16:59:19 -0400

    meet comments

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/optim/DistriOptimizer.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/optim/DistriOptimizer.scala
@@ -796,8 +796,6 @@ class DistriOptimizer[T: ClassTag] (
             models = DistriOptimizer.initThreadModels(newModel, dataset, criterion, state,
               nodeNumber, coresPerNode, checkSingleton, parameters, validationMethods, optimMethod)
           } else {
-            DistriOptimizer.logger.info("Failed to recover since no model snapshot" +
-              "checkpoint path is not set")
             throw t
           }
       }
