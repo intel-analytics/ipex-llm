70791cdfc03d44cee439cc93a7f1fc512da6c789
Xin Qiu <qiuxin2012@users.noreply.github.com>
2017-05-17 13:42:36 +0800

    Refact sample and minibatch  (#781)
    Sample and MiniBatch refactor

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/dataset/SampleSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/dataset/SampleSpec.scala
@@ -29,61 +29,21 @@ class SampleSpec extends FlatSpec with Matchers {
     val tensorLabel1 = Tensor[Float](Storage[Float](label1.content), 1, Array(3, 32, 32))
     tensorInput1.rand()
     tensorLabel1.rand()
-    val sample = Sample[Float](tensorInput1, tensorLabel1)
-    sample.feature() should be (tensorInput1)
-    sample.label() should be (tensorLabel1)
+    val sample = new TensorSample[Float](tensorInput1, tensorLabel1)
+    sample.featureTensor should be (tensorInput1)
+    sample.labelTensor should be (tensorLabel1)
   }
-  "SampleSpec with Float Tensor input and Tensor label" should "set well" in {
-    val input1 = new LabeledBGRImage(32, 32)
-    val label1 = new LabeledBGRImage(32, 32)
-    val tensorInput1 = Tensor[Float](Storage[Float](input1.content), 1, Array(3, 32, 32))
-    val tensorLabel1 = Tensor[Float](Storage[Float](label1.content), 1, Array(3, 32, 32))
-    tensorInput1.rand()
-    tensorLabel1.rand()
-    val sample = Sample[Float]()
-    sample.set(tensorInput1.storage().array(),
-      tensorLabel1.storage().array(),
-      tensorInput1.size,
-      tensorLabel1.size)
-    sample.feature() should be (tensorInput1)
-    sample.label() should be (tensorLabel1)
-  }
-  "SampleSpec with Float Tensor input and Tensor label" should "clone well" in {
-    val input1 = new LabeledBGRImage(32, 32)
-    val label1 = new LabeledBGRImage(32, 32)
-    val tensorInput1 = Tensor[Float](Storage[Float](input1.content), 1, Array(3, 32, 32))
-    val tensorLabel1 = Tensor[Float](Storage[Float](label1.content), 1, Array(3, 32, 32))
-    tensorInput1.rand()
-    tensorLabel1.rand()
-    val sample = Sample[Float](tensorInput1, tensorLabel1)
-    val otherSample = sample.clone()
-    sample.feature() should be (otherSample.feature())
-    sample.label() should be (otherSample.label())
-  }
-  "SampleSpec with Float Tensor input and Tensor label" should "copyFromFeature" +
-    "and copyFromLabel well" in {
-    val size = 4
-    val input1 = new LabeledBGRImage(size, size)
-    val label1 = new LabeledBGRImage(size, size)
-    val tensorInput1 = Tensor[Float](Storage[Float](input1.content), 1, Array(size, size))
-    val tensorLabel1 = Tensor[Float](Storage[Float](label1.content), 1, Array(size, size))
-    tensorInput1.rand()
-    tensorLabel1.rand()
-    val sample = Sample[Float](tensorInput1, tensorLabel1)
-    val input2 = new Array[Float](size*size)
-    val label2 = new Array[Float](size*size)
-    sample.copyFromFeature(input2, 0, size*size)
-    sample.copyFromLabel(label2, 0, size*size)
 
-    var i = 0
-    while (i < input2.length) {
-      input2(i) should be (sample.feature().storage().array()(i))
-      i += 1
-    }
-    i = 0
-    while (i < label2.length) {
-      label2(i) should be (sample.label().storage().array()(i))
-      i += 1
-    }
+  "TensorSample" should "clone well" in {
+    val input1 = new LabeledBGRImage(32, 32)
+    val label1 = new LabeledBGRImage(32, 32)
+    val tensorInput1 = Tensor[Float](Storage[Float](input1.content), 1, Array(3, 32, 32))
+    val tensorLabel1 = Tensor[Float](Storage[Float](label1.content), 1, Array(3, 32, 32))
+    tensorInput1.rand()
+    tensorLabel1.rand()
+    val sample = new TensorSample[Float](tensorInput1, tensorLabel1)
+    val otherSample = sample.clone()
+    sample.featureTensor should be (otherSample.featureTensor)
+    sample.labelTensor should be (otherSample.labelTensor)
   }
 }
