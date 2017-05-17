88b31c77d1cd2155e4b74042a15468fb1597961f
Sean Owen <sowen@cloudera.com>
2017-05-17 22:49:10 +0100

    Use reflection to call BlockManager.putBytes in Spark 2.x to handle different method signatures

--- spark/spark-version/2.0/src/main/scala/org/apache/spark/storage/BlockManagerWrapper.scala
+++ spark/spark-version/2.0/src/main/scala/org/apache/spark/storage/BlockManagerWrapper.scala
@@ -16,10 +16,13 @@
 
 package org.apache.spark.storage
 
+import java.lang.{Boolean => JBoolean}
 import java.nio.ByteBuffer
 
 import org.apache.spark.SparkEnv
 import org.apache.spark.util.io.ChunkedByteBuffer
+
+import scala.reflect.ClassTag
 
 object BlockManagerWrapper {
 
@@ -29,7 +32,7 @@ object BlockManagerWrapper {
     require(bytes != null, "Bytes is null")
     val blockManager = SparkEnv.get.blockManager
     blockManager.removeBlock(blockId)
-    blockManager.putBytes(blockId, new ChunkedByteBuffer(bytes), level)
+    doPutBytes(blockId, new ChunkedByteBuffer(bytes), level)
   }
 
   def getLocal(blockId: BlockId): Option[BlockResult] = {
@@ -57,4 +60,48 @@ object BlockManagerWrapper {
       blockInfoManager.unlock(blockId)
     }
   }
+
+  private val putBytesMethod = {
+    val bmClass = classOf[BlockManager]
+    // Spark 2.0.0 - 2.1.0, and 2.2.0+ (as of this writing), declare the method:
+    // def putBytes[T: ClassTag](
+    //   blockId: BlockId,
+    //   bytes: ChunkedByteBuffer,
+    //   level: StorageLevel,
+    //   tellMaster: Boolean = true): Boolean
+    try {
+      bmClass.getMethod("putBytes",
+        classOf[BlockId], classOf[ChunkedByteBuffer], classOf[StorageLevel],
+        classOf[Boolean], classOf[ClassTag[_]])
+    } catch {
+      case _: NoSuchMethodException =>
+        // But Spark 2.1.1 and distros like Cloudera 2.0.0 / 2.1.0 had an extra boolean
+        // param:
+        //   def putBytes[T: ClassTag](
+        //     blockId: BlockId,
+        //     bytes: ChunkedByteBuffer,
+        //     level: StorageLevel,
+        //     tellMaster: Boolean = true,
+        //     encrypt: Boolean = false): Boolean
+        bmClass.getMethod("putBytes",
+          classOf[BlockId], classOf[ChunkedByteBuffer], classOf[StorageLevel],
+          classOf[Boolean], classOf[Boolean], classOf[ClassTag[_]])
+    }
+  }
+  private val putBytesArgCount = putBytesMethod.getParameterTypes.length
+
+  private def doPutBytes(
+      blockId: BlockId,
+      bytes: ChunkedByteBuffer,
+      level: StorageLevel): Unit = {
+    putBytesArgCount match {
+      case 5 =>
+        putBytesMethod.invoke(SparkEnv.get.blockManager,
+          blockId, bytes, level, JBoolean.TRUE, null)
+      case 6 =>
+        putBytesMethod.invoke(SparkEnv.get.blockManager,
+          blockId, bytes, level, JBoolean.TRUE, JBoolean.FALSE, null)
+    }
+  }
+
 }
