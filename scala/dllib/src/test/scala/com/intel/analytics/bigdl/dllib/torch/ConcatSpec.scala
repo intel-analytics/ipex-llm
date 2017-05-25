b1162d08dfff57430035daf2a4d66e0b008d2020
Yao Zhang <psyyz10@nottingham.ac.uk>
2017-05-25 10:48:32 +0800

    fix bug in Concat layer and Add backward to ConcatTable (#902)

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/torch/ConcatSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/torch/ConcatSpec.scala
@@ -71,7 +71,71 @@ class ConcatSpec extends TorchSpec {
 
     val start = System.nanoTime()
     val output = module.forward(input)
-//    val gradInput = module.backward(input, gradOutput)
+    val gradInput = module.backward(input, gradOutput)
+    val end = System.nanoTime()
+    val scalaTime = end - start
+
+    luaOutput.map(output, (v1, v2) => {
+      assert(abs(v1 - v2) == 0)
+      v1
+    })
+    luaGradInput.map(gradInput, (v1, v2) => {
+      assert(abs(v1 - v2) == 0)
+      v1
+    })
+
+    println("Test case : Concat, Torch : " + luaTime + " s, Scala : " + scalaTime / 1e9 + " s")
+  }
+
+  "A Concat Container updateGradInput and acc with Linear" should
+    "generate correct output and grad " in {
+    val seed = 2
+    RNG.setSeed(seed)
+    val module = new Concat[Double](2)
+    val layer1 = new Sequential[Double]()
+    val layer2 = new Sequential[Double]()
+    layer1.add(new SpatialBatchNormalization[Double](3, 1e-3))
+    layer2.add(new SpatialBatchNormalization[Double](3, 1e-3))
+    module.add(layer1).add(layer2)
+
+    val input = Tensor[Double](4, 3, 4, 4).randn()
+    val gradOutput = Tensor[Double](4, 6, 4, 4).randn()
+
+    val code = "torch.manualSeed(" + seed + ")\n" +
+      """
+    module = nn.Concat(2)
+    layer1 = nn.Sequential()
+    layer2 = nn.Sequential()
+    layer1:add(nn.SpatialBatchNormalization(3, 1e-3))
+    layer2:add(nn.SpatialBatchNormalization(3, 1e-3))
+    module:add(layer1):add(layer2)
+    local parameters, gradParameters = module:getParameters()
+    module:zeroGradParameters()
+    parameters_initial = parameters : clone()
+    gradParameters_initial = gradParameters : clone()
+
+    output = module:forward(input)
+    gradInput = module:backward(input,gradOutput)
+      """
+
+    val (luaTime, torchResult) = TH.run(code, Map("input" -> input, "gradOutput" -> gradOutput),
+      Array("output", "gradInput", "module", "parameters_initial", "gradParameters_initial",
+      "parameters", "gradParameters"))
+    val luaOutput = torchResult("output").asInstanceOf[Tensor[Double]]
+    val gradParametersInitial = torchResult("gradParameters_initial").asInstanceOf[Tensor[Double]]
+    val parametersInitial = torchResult("parameters_initial").asInstanceOf[Tensor[Double]]
+    val gradParametersLua = torchResult("gradParameters").asInstanceOf[Tensor[Double]]
+    val parametersLua = torchResult("parameters").asInstanceOf[Tensor[Double]]
+    val luaGradInput = torchResult("gradInput").asInstanceOf[Tensor[Double]]
+    val luaModule = torchResult("module")
+      .asInstanceOf[Module[Double]]
+
+    val (parameters, gradParameters) = module.getParameters()
+    require(gradParametersInitial == gradParameters)
+    require(parametersInitial == parameters)
+
+    val start = System.nanoTime()
+    val output = module.forward(input)
     val gradInput = module.updateGradInput(input, gradOutput)
     module.accGradParameters(input, gradOutput)
     val end = System.nanoTime()
@@ -85,7 +149,14 @@ class ConcatSpec extends TorchSpec {
       assert(abs(v1 - v2) == 0)
       v1
     })
-
+    gradParametersLua.map(gradParameters, (v1, v2) => {
+      assert(abs(v1 - v2) == 0)
+      v1
+    })
+    parametersLua.map(parameters, (v1, v2) => {
+      assert(abs(v1 - v2) == 0)
+      v1
+    })
     println("Test case : Concat, Torch : " + luaTime + " s, Scala : " + scalaTime / 1e9 + " s")
   }
 
