f1a896de7b9456a7baa787150ac646eb3147a048
qiuxin2012 <qiuxin2012cs@gmail.com>
2017-06-05 15:59:11 +0800

    add finally

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/utils/File.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/utils/File.scala
@@ -17,10 +17,10 @@
 package com.intel.analytics.bigdl.utils
 
 import java.io._
-import java.nio.file._
+import java.nio.file.{Files, Paths}
 
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.Path
+import org.apache.hadoop.fs.{FSDataInputStream, FSDataOutputStream, FileSystem, Path}
 import org.apache.hadoop.io.IOUtils
 
 object File {
@@ -66,11 +66,18 @@ object File {
       if (Files.exists(Paths.get(fileName)) && !isOverwrite) {
         throw new RuntimeException("file exists!")
       }
-      val os = new FileOutputStream(fileName)
-      val objFile = new ObjectOutputStream(os)
-      objFile.writeObject(obj)
-      objFile.close()
-      os.close()
+      var os: FileOutputStream = null
+      var objFile: ObjectOutputStream = null
+      try {
+        os = new FileOutputStream(fileName)
+        objFile = new ObjectOutputStream(os)
+        objFile.writeObject(obj)
+        objFile.close()
+        os.close()
+      } finally {
+        if (null != objFile) objFile.close()
+        if (null != os) os.close()
+      }
     }
   }
 
@@ -84,21 +91,30 @@ object File {
     require(fileName.startsWith(File.hdfsPrefix),
       s"hdfs path ${fileName} should have prefix 'hdfs:'")
     val dest = new Path(fileName)
-    val fs = dest.getFileSystem(new Configuration())
-    if (fs.exists(dest)) {
-      if (overwrite) {
-        fs.delete(dest, true)
-      } else {
-        throw new RuntimeException(s"file $fileName already exists")
+    var fs: FileSystem = null
+    var out: FSDataOutputStream = null
+    var objFile: ObjectOutputStream = null
+    try {
+      fs = dest.getFileSystem(new Configuration())
+      if (fs.exists(dest)) {
+        if (overwrite) {
+          fs.delete(dest, true)
+        } else {
+          throw new RuntimeException(s"file $fileName already exists")
+        }
       }
+      out = fs.create(dest)
+      val byteArrayOut = new ByteArrayOutputStream()
+      objFile = new ObjectOutputStream(byteArrayOut)
+      objFile.writeObject(obj)
+      IOUtils.copyBytes(new ByteArrayInputStream(byteArrayOut.toByteArray), out, 1024, true)
+      objFile.close()
+      out.close()
+    } finally {
+      if (null != objFile) objFile.close()
+      if (null != out) out.close()
+      if (null != fs) fs.close()
     }
-    val out = fs.create(dest)
-    val byteArrayOut = new ByteArrayOutputStream()
-    val objFile = new ObjectOutputStream(byteArrayOut)
-    objFile.writeObject(obj)
-    IOUtils.copyBytes(new ByteArrayInputStream(byteArrayOut.toByteArray), out, 1024, true)
-    objFile.close()
-    out.close()
   }
 
   /**
@@ -108,11 +124,15 @@ object File {
    */
   def loadFromHdfs[T](fileName: String): T = {
     val byteArrayOut = readHdfsByte(fileName)
-
-    val objFile = new ObjectInputStream(new ByteArrayInputStream(byteArrayOut))
-    val result = objFile.readObject()
-    objFile.close()
-    result.asInstanceOf[T]
+    var objFile: ObjectInputStream = null
+    try {
+      objFile = new ObjectInputStream(new ByteArrayInputStream(byteArrayOut))
+      val result = objFile.readObject()
+      objFile.close()
+      result.asInstanceOf[T]
+    } finally {
+      if (null != objFile) objFile.close()
+    }
   }
 
   /**
@@ -124,12 +144,19 @@ object File {
     val res = if (fileName.startsWith(File.hdfsPrefix)) {
       loadFromHdfs[T](fileName)
     } else {
-      val is = new FileInputStream(fileName)
-      val objFile = new ObjectInputStream(is)
-      val result = objFile.readObject().asInstanceOf[T]
-      objFile.close()
-      is.close()
-      result
+      var is: FileInputStream = null
+      var objFile: ObjectInputStream = null
+      try {
+        is = new FileInputStream(fileName)
+        objFile = new ObjectInputStream(is)
+        val result = objFile.readObject().asInstanceOf[T]
+        objFile.close()
+        is.close()
+        result
+      } finally {
+        if (null != objFile) objFile.close()
+        if (null != is) is.close()
+      }
     }
     res.asInstanceOf[T]
   }
@@ -141,11 +168,19 @@ object File {
    */
   def readHdfsByte(fileName: String): Array[Byte] = {
     val src: Path = new Path(fileName)
-    val fs = src.getFileSystem(new Configuration())
-    val in = fs.open(src)
-    val byteArrayOut = new ByteArrayOutputStream()
-    IOUtils.copyBytes(in, byteArrayOut, 1024, true)
-    in.close()
-    byteArrayOut.toByteArray
+    var fs: FileSystem = null
+    var in: FSDataInputStream = null
+    try {
+      fs = src.getFileSystem(new Configuration())
+      in = fs.open(src)
+      val byteArrayOut = new ByteArrayOutputStream()
+      IOUtils.copyBytes(in, byteArrayOut, 1024, true)
+      in.close()
+      fs.close()
+      byteArrayOut.toByteArray
+    } finally {
+      if (null != in) in.close()
+      if (null != fs) fs.close()
+    }
   }
 }
