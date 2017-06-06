ccb7b6073ce94aac4319d1079ab9ac0a9360c477
zhangxiaoli73 <380761639@qq.com>
2017-06-06 15:34:20 +0800

    fix add bug (#968)

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/torch/AddSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/torch/AddSpec.scala
@@ -18,6 +18,7 @@ package com.intel.analytics.bigdl.torch
 import com.intel.analytics.bigdl.nn.Add
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.utils.RandomGenerator._
+import spire.syntax.module
 
 @com.intel.analytics.bigdl.tags.Serial
 class AddSpec extends TorchSpec {
@@ -70,4 +71,52 @@ class AddSpec extends TorchSpec {
     luaBias should be(bias)
 
   }
+    "A Add Module " should "generate correct output and grad with batchsize > 1" in {
+        torchCheck()
+        val inputN = 5
+        val seed = 100
+        RNG.setSeed(seed)
+        val module = new Add[Double](inputN)
+        val input = Tensor[Double](2, 5)
+        input(Array(1, 1)) = 1
+        input(Array(1, 2)) = 2
+        input(Array(1, 3)) = 3
+        input(Array(1, 4)) = 4
+        input(Array(1, 5)) = 5
+
+        val gradOutput = Tensor[Double](2, 5)
+        gradOutput(Array(1, 1)) = 1
+        gradOutput(Array(1, 2)) = 2
+        gradOutput(Array(1, 3)) = 3
+        gradOutput(Array(1, 4)) = 4
+        gradOutput(Array(1, 5)) = 5
+
+        val code = "torch.manualSeed(" + seed + ")\n" +
+          "module = nn.Add(5)\n" +
+          "module:reset()\n" +
+          "bias = module.bias\n" +
+          "output = module:forward(input)\n" +
+          "gradInput = module:backward(input, gradOutput)\n" +
+          "ones = module._ones\n"
+
+        val (luaTime, torchResult) = TH.run(code, Map("input" -> input, "gradOutput" -> gradOutput),
+            Array("output", "gradInput", "bias", "ones"))
+
+        val luaOutput1 = torchResult("output").asInstanceOf[Tensor[Double]]
+        val luaOutput2 = torchResult("gradInput").asInstanceOf[Tensor[Double]]
+        val luaBias = torchResult("bias").asInstanceOf[Tensor[Double]]
+        val luaOnes = torchResult("ones").asInstanceOf[Tensor[Double]]
+
+        val start = System.nanoTime()
+        module.reset()
+        val bias = module.bias
+        val output = module.forward(input)
+        val gradInput = module.backward(input, gradOutput)
+        val end = System.nanoTime()
+        val scalaTime = end - start
+
+        luaOutput1 should be(output)
+        luaOutput2 should be(gradInput)
+        luaBias should be(bias)
+    }
 }
