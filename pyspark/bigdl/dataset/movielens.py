b735100c461302c057b4d115f3fdae64584ad3c2
yunhuiguo <gyhui@zju.edu.cn>
2017-06-16 09:44:25 +0800

    add integration test
    update

--- pyspark/bigdl/dataset/movielens.py
+++ pyspark/bigdl/dataset/movielens.py
@@ -31,21 +31,21 @@ def read_data_sets(data_dir):
     :return: a 2D numpy array with user index and item index in each row 
     """
 
-    WHOLE_DATA = 'ml-1m.zip'
-    local_file = base.maybe_download(WHOLE_DATA, data_dir, SOURCE_URL + WHOLE_DATA)
-    zip_ref = zipfile.ZipFile(local_file, 'r')
+	WHOLE_DATA = 'ml-1m.zip'
+	local_file = base.maybe_download(WHOLE_DATA, data_dir, SOURCE_URL + WHOLE_DATA)
+	zip_ref = zipfile.ZipFile(local_file, 'r')
 
-    extracted_to = os.path.join(data_dir, "ml-1m")
-    if not os.path.exists(extracted_to):
-	print("Extracting %s to %s" % (local_file, data_dir))
-	zip_ref.extractall(data_dir)
-	zip_ref.close()
-    rating_files = os.path.join(extracted_to,"ratings.dat")
-    rating_list = [i.strip().split("::")[:2] for i in open(rating_files,"r").readlines()]    
-    movielens_data = np.array(rating_list)
+	extracted_to = os.path.join(data_dir, "ml-1m")
+	if not os.path.exists(extracted_to):
+		print("Extracting %s to %s" % (local_file, data_dir))
+		zip_ref.extractall(data_dir)
+		zip_ref.close()
+		rating_files = os.path.join(extracted_to,"ratings.dat")
+	rating_list = [i.strip().split("::")[:2] for i in open(rating_files,"r").readlines()]    
+	movielens_data = np.array(rating_list)
     
-    return movielens_data 
+	return movielens_data 
 
 
 if __name__ == "__main__":
-    movielens_data = read_data_sets("./tmp/movielens/")
+	movielens_data = read_data_sets("./tmp/movielens/")
