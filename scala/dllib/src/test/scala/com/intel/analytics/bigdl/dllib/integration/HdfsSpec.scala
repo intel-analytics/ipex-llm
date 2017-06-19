c607918f8f2dc8141d996541130299d8bee6187d
Yanzhang Wang <i8run15@gmail.com>
2017-06-19 17:00:40 +0800

    [FIX943] bash environment variables to java properties (#988)
    * feat: make all environment virables to java properties
    
    * fix: typo
    
    * refactor: add api for setting mkl environment
    
    * fix: get back the omp environments
    
    * fix: set mkl use one omp thread, which means sequential mode
    
    * fix: typo of waitPolicyPasssive
    
    * test: add tests for localmode and corenumber
    
    Converting localmode variable to method. The only method to make
    the mode bigdl running is through java propery
    `-Dbigdl.localmode=true`. The default value is false.
    
    * fix: set wrong mode in SummarySpec and change name of mkl properties
    
    * fix: delete the bigdl.sh in all documents
    
    * fix: code style exceeds 100 chars
    
    * refactor: move mkl environment variables setting to MKL.
    
    The previous patch will only effect the driver side, except executor
    side. All sides will load library and execute setMklEnv method after
    loading completed if we put these code snippets in MKL.java which is in
    BigDL-core project.
    
    * fix: bigdl-native back to 0.1.0
    
    * fix: before Engine.init we should not call Engine.coreNumber
    
    * feat: We convert all environment variables to mkl/omp apis.
    
    Because we have modify the BigDL-core, which contains the new JNI
    methods called in MKL.java. So we should modify the bigdl.native to
    0.2.0-SNAPSHOT in spark/dl/pom.xml.
    
    * fix: delete all bigdl.sh in shell scripts and documents

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/integration/HdfsSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/integration/HdfsSpec.scala
@@ -70,7 +70,7 @@ class HdfsSpec extends FlatSpec with Matchers with BeforeAndAfter{
   }
 
   "read/write event file from hdfs" should "work properly" in {
-    Engine.localMode = false
+    System.setProperty("bigdl.localMode", "true")
     Engine.init(1, 4, true)
     val logdir = hdfs + s"/${com.google.common.io.Files.createTempDir().getPath()}"
     val writer = new FileWriter(logdir, 100)
@@ -94,6 +94,7 @@ class HdfsSpec extends FlatSpec with Matchers with BeforeAndAfter{
       result(i)._1 should be (i + 1)
       result(i)._2 should be (i)
     }
+    System.clearProperty("bigdl.localMode")
   }
 
   "load caffe model from hdfs" should "work properly" in {
