2b4fdc44ee881a0bad9aa87585e18189582ca39c
Xin Qiu <qiuxin2012@users.noreply.github.com>
2017-06-21 13:22:12 +0800

    update some doc (#1050)

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/ReLU6.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/ReLU6.scala
@@ -23,7 +23,7 @@ import scala.reflect.ClassTag
 /**
  * Same as ReLU except that the rectifying function f(x) saturates at x = 6
  * ReLU6 is defined as:
- * f(x) = min(max(0, x), 6)
+ * `f(x) = min(max(0, x), 6)`
  *
  * @param inplace either true = in-place or false = keeping separate state
  */
