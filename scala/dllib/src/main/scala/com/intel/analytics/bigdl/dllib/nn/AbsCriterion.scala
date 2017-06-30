4e1bcab8caf622d5e544ea2483a0abab03ac9d82
zhangxiaoli73 <380761639@qq.com>
2017-06-30 13:37:50 +0800

    makedown docs for some API (#1075)
    * add some api md
    
    * change according to reviews

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/AbsCriterion.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/AbsCriterion.scala
@@ -22,7 +22,7 @@ import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import scala.reflect.ClassTag
 
 /**
- * measures the mean absolute value of the element-wise difference between input
+ * measures the mean absolute value of the element-wise difference between input and target
  */
 @SerialVersionUID( - 7860793007567513758L)
 class AbsCriterion[@specialized(Float, Double) T: ClassTag](val sizeAverage: Boolean = true)
