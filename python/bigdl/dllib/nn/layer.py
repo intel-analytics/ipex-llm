ab0f4c72cbadf40889b1c9fbf18c207ba9ef0495
jenniew <jenniewang123@gmail.com>
2017-07-04 21:21:30 -0700

    update layer docs

--- python/bigdl/dllib/nn/layer.py
+++ python/bigdl/dllib/nn/layer.py
@@ -1921,7 +1921,7 @@ class MapTable(Container):
     '''
 
     def __init__(self,
-                 module,
+                 module=None,
                  bigdl_type="float"):
         super(MapTable, self).__init__(None, bigdl_type,
                                        module)
