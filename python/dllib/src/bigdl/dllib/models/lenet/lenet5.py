b2d2631d04f3b06dec247cbd9870840ed48a76c2
li,zhichao <zhichao.li@intel.com>
2017-07-06 12:44:22 +0800

    Change type of ValidationMethod from string to ValidationMethod instance (#1149)
    * refactor ValidationMethod
    
    * add
    
    * minor
    
    * refactor

--- python/dllib/src/bigdl/dllib/models/lenet/lenet5.py
+++ python/dllib/src/bigdl/dllib/models/lenet/lenet5.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
             batch_size=options.batchSize,
             val_rdd=test_data,
             trigger=EveryEpoch(),
-            val_method=["Top1Accuracy"]
+            val_method=[Top1Accuracy()]
         )
         optimizer.set_checkpoint(EveryEpoch(), options.checkpointPath)
         trained_model = optimizer.optimize()
@@ -107,7 +107,7 @@ if __name__ == "__main__":
         test_data = get_mnist(sc, "test").map(
             normalizer(mnist.TEST_MEAN, mnist.TEST_STD))
         model = Model.load(options.modelPath)
-        results = model.test(test_data, options.batchSize, ["Top1Accuracy"])
+        results = model.test(test_data, options.batchSize, [Top1Accuracy()])
         for result in results:
             print(result)
     sc.stop()
