2aa95b5daeaa79c3294568dc391faa5103d1138d
yunhuiguo <gyhui@zju.edu.cn>
2017-07-07 15:59:50 +0800

    Add MAE for validation and update the interface of movielens dataset module (#1148)
    * add MAE for validation
    
    upadte MAE
    
    add MAE test
    
    * upate movielens dataset module
    
    update movielens
    
    update movielens
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    resolve conflicts
    
    update
    
    * add MAE in python validation method
    
    update
    
    update

--- python/bigdl/dllib/feature/dataset/movielens.py
+++ python/bigdl/dllib/feature/dataset/movielens.py
@@ -22,7 +22,6 @@ import numpy as np
 from bigdl.dataset import base
 
 SOURCE_URL = 'http://files.grouplens.org/datasets/movielens/'
-
 def read_data_sets(data_dir):
     """
     Parse or download movielens 1m  data if train_dir is empty.
@@ -30,7 +29,6 @@ def read_data_sets(data_dir):
     :param data_dir: The directory storing the movielens data
     :return: a 2D numpy array with user index and item index in each row 
     """
-
     WHOLE_DATA = 'ml-1m.zip'
     local_file = base.maybe_download(WHOLE_DATA, data_dir, SOURCE_URL + WHOLE_DATA)
     zip_ref = zipfile.ZipFile(local_file, 'r')
@@ -40,9 +38,19 @@ def read_data_sets(data_dir):
         zip_ref.extractall(data_dir)
         zip_ref.close()
     rating_files = os.path.join(extracted_to,"ratings.dat")
-    rating_list = [i.strip().split("::")[:2] for i in open(rating_files,"r").readlines()]    
+
+    rating_list = [i.strip().split("::") for i in open(rating_files,"r").readlines()]    
     movielens_data = np.array(rating_list).astype(int)
     return movielens_data 
+
+def get_id_pairs(data_dir):
+	movielens_data = read_data_sets(data_dir)
+	return movielens_data[:, 0:2]
+
+def get_id_ratings(data_dir):
+	movielens_data = read_data_sets(data_dir)
+	return movielens_data[:, 0:3]
+
 
 if __name__ == "__main__":
     movielens_data = read_data_sets("/tmp/movielens/")
