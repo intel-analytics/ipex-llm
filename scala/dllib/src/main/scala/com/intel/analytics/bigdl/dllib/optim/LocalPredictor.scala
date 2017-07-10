36a9d5aa51f83b873d96ace405bbbaf1b69ecb97
jenniew <jenniewang123@gmail.com>
2017-07-10 13:16:45 -0700

    update local jvm with comments

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/optim/LocalPredictor.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/optim/LocalPredictor.scala
@@ -19,7 +19,7 @@ package com.intel.analytics.bigdl.optim
 import com.intel.analytics.bigdl._
 import com.intel.analytics.bigdl.dataset.{LocalDataSet, MiniBatch, Sample, SampleToBatch}
 import com.intel.analytics.bigdl.nn.abstractnn.Activity
-import com.intel.analytics.bigdl.tensor.{Storage, Tensor}
+import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.utils.{Engine, MklBlas}
 
@@ -27,30 +27,6 @@ import com.intel.analytics.bigdl.utils.{Engine, MklBlas}
 import scala.reflect.ClassTag
 
 object LocalPredictor {
-  def getAndClearWeightBias[T: ClassTag](parameters: (Array[Tensor[T]], Array[Tensor[T]]))
-                                        (implicit ev: TensorNumeric[T]): Array[Tensor[T]] = {
-    var i = 0
-    val weightsBias = new Array[Tensor[T]](parameters._1.length)
-    while (i < parameters._1.length) {
-      if (parameters._1(i) != null) {
-        val wb = parameters._1(i)
-        weightsBias(i) = Tensor[T](Storage(wb.storage().array()),
-          wb.storageOffset(), wb.size(), wb.stride())
-      }
-      i += 1
-    }
-    i = 0
-    while (i < parameters._1.length) {
-      if (parameters._1(i) != null) {
-        parameters._1(i).set()
-      }
-      if (parameters._2(i) != null) {
-        parameters._2(i).set()
-      }
-      i += 1
-    }
-    weightsBias
-  }
 
   def apply[T: ClassTag](model: Module[T], weightsBias: Array[Tensor[T]])
                         (implicit ev: TensorNumeric[T]): LocalPredictor[T] = {
