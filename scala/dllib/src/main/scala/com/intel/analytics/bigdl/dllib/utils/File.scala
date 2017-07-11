6b61ad49d86dcf8d01908eb0f22d8f166f05999f
Xin Qiu <qiuxin2012@users.noreply.github.com>
2017-07-11 21:23:44 +0800

    save/load model from s3 (#1136)

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/utils/File.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/utils/File.scala
@@ -25,6 +25,7 @@ import org.apache.hadoop.io.IOUtils
 
 object File {
   private[bigdl] val hdfsPrefix: String = "hdfs:"
+  private[bigdl] val s3aPrefix: String = "s3a:"
 
   /**
    * Load torch object from a torch binary file
@@ -53,31 +54,61 @@ object File {
   }
 
   /**
-   * Save scala object into a local/hdfs path
+   * Save scala object into a local/hdfs/s3 path
+   *
+   * Notice: S3 path should be like s3a://bucket/xxx.
+   *
+   * See (hadoop aws)[http://hadoop.apache.org/docs/r2.7.3/hadoop-aws/tools/hadoop-aws/index.html]
+   * for details, if you want to save model to s3.
    *
    * @param obj object to be saved.
    * @param fileName local/hdfs output path.
    * @param isOverwrite if overwrite.
    */
   def save(obj: Serializable, fileName: String, isOverwrite: Boolean = false): Unit = {
-    if (fileName.startsWith(File.hdfsPrefix)) {
-      saveToHdfs(obj, fileName, isOverwrite)
+    val conf = getConfiguration(fileName)
+    save(obj, fileName, isOverwrite, conf)
+  }
+
+  private[bigdl] def getFileSystem(fileName: String): org.apache.hadoop.fs.FileSystem = {
+    val src = new Path(fileName)
+    val fs = src.getFileSystem(File.getConfiguration(fileName))
+    require(fs.exists(src), src + " does not exists")
+    fs
+  }
+
+  private[bigdl] def getConfiguration(fileName: String): Configuration = {
+    if (fileName.startsWith(File.hdfsPrefix) || fileName.startsWith(s3aPrefix)) {
+      new Configuration()
     } else {
-      if (Files.exists(Paths.get(fileName)) && !isOverwrite) {
-        throw new RuntimeException("file exists!")
+      new Configuration(false)
+    }
+  }
+
+  private def save(obj: Serializable, fileName: String, overwrite: Boolean,
+                   conf: Configuration): Unit = {
+    val dest = new Path(fileName)
+    var fs: FileSystem = null
+    var out: FSDataOutputStream = null
+    var objFile: ObjectOutputStream = null
+    try {
+      fs = dest.getFileSystem(conf)
+      if (fs.exists(dest)) {
+        if (overwrite) {
+          fs.delete(dest, true)
+        } else {
+          throw new RuntimeException(s"file $fileName already exists")
+        }
       }
-      var os: FileOutputStream = null
-      var objFile: ObjectOutputStream = null
-      try {
-        os = new FileOutputStream(fileName)
-        objFile = new ObjectOutputStream(os)
-        objFile.writeObject(obj)
-        objFile.close()
-        os.close()
-      } finally {
-        if (null != objFile) objFile.close()
-        if (null != os) os.close()
-      }
+      out = fs.create(dest)
+      val byteArrayOut = new ByteArrayOutputStream()
+      objFile = new ObjectOutputStream(byteArrayOut)
+      objFile.writeObject(obj)
+      IOUtils.copyBytes(new ByteArrayInputStream(byteArrayOut.toByteArray), out, 1024, true)
+    } finally {
+      if (null != objFile) objFile.close()
+      if (null != out) out.close()
+      if (null != fs) fs.close()
     }
   }
 
@@ -108,8 +139,6 @@ object File {
       objFile = new ObjectOutputStream(byteArrayOut)
       objFile.writeObject(obj)
       IOUtils.copyBytes(new ByteArrayInputStream(byteArrayOut.toByteArray), out, 1024, true)
-      objFile.close()
-      out.close()
     } finally {
       if (null != objFile) objFile.close()
       if (null != out) out.close()
@@ -136,29 +165,44 @@ object File {
   }
 
   /**
-   * Load a scala object from a local/hdfs path.
+   * Load a scala object from a local/hdfs/s3 path.
+   *
+   * Notice: S3 path should be like s3a://bucket/xxx.
+   *
+   * See (hadoop aws)[http://hadoop.apache.org/docs/r2.7.3/hadoop-aws/tools/hadoop-aws/index.html]
+   * for details, if you want to load model from s3.
    *
    * @param fileName file name.
    */
   def load[T](fileName: String): T = {
-    val res = if (fileName.startsWith(File.hdfsPrefix)) {
-      loadFromHdfs[T](fileName)
-    } else {
-      var is: FileInputStream = null
-      var objFile: ObjectInputStream = null
-      try {
-        is = new FileInputStream(fileName)
-        objFile = new ObjectInputStream(is)
-        val result = objFile.readObject().asInstanceOf[T]
-        objFile.close()
-        is.close()
-        result
-      } finally {
-        if (null != objFile) objFile.close()
-        if (null != is) is.close()
-      }
+    val conf = getConfiguration(fileName)
+    load[T](fileName, conf)
+  }
+
+  /**
+   * Load a scala object from a local/hdfs/s3 path.
+   *
+   * @param fileName file name.
+   * @param conf hadoop Configuration.
+   */
+  private def load[T](fileName: String, conf: Configuration): T = {
+    val src: Path = new Path(fileName)
+    var fs: FileSystem = null
+    var in: FSDataInputStream = null
+    var objFile: ObjectInputStream = null
+    try {
+      fs = src.getFileSystem(conf)
+      in = fs.open(src)
+      val byteArrayOut = new ByteArrayOutputStream()
+      IOUtils.copyBytes(in, byteArrayOut, 1024, true)
+      objFile = new ObjectInputStream(new ByteArrayInputStream(byteArrayOut.toByteArray))
+      val result = objFile.readObject()
+      result.asInstanceOf[T]
+    } finally {
+      if (null != in) in.close()
+      if (null != fs) fs.close()
+      if (null != objFile) objFile.close()
     }
-    res.asInstanceOf[T]
   }
 
   /**
@@ -175,8 +219,6 @@ object File {
       in = fs.open(src)
       val byteArrayOut = new ByteArrayOutputStream()
       IOUtils.copyBytes(in, byteArrayOut, 1024, true)
-      in.close()
-      fs.close()
       byteArrayOut.toByteArray
     } finally {
       if (null != in) in.close()
