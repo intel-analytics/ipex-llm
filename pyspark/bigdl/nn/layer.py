873c7f65a4df023594717c751989d4e7d39e130c
yangw <yang3.wang@intel.com>
2017-07-12 16:53:50 +0800

    add doc

--- pyspark/bigdl/nn/layer.py
+++ pyspark/bigdl/nn/layer.py
@@ -376,12 +376,17 @@ class Model(Container):
     All inputs should be able to connect to outputs through some paths in the graph.
     It is allowed that some successors of the inputs node are not connect to outputs.
     If so, these nodes will be excluded in the computation.
+    
+    We also support initialzing a Graph directly from a tensorflow module. In this case, you should
+    pass your tensorflow nodes as inputs and outputs and also specify the byte_order parameter ("little_endian"
+     or "big_endian") and node_type parameter ("bigdl" or "tensorflow")
+    node_type parameter.
     """
 
     def __init__(self,
                  inputs,
                  outputs,
-                 bigdl_type="float", byte_order = "little_endian", node_type="bigdl"):
+                 bigdl_type="float", byte_order="little_endian", node_type="bigdl"):
         if node_type == "bigdl":
             super(Model, self).__init__(None, bigdl_type,
                                     to_list(inputs),
