2e8d077a4f92801aa4af6c99f090ca98c00141ae
qiuxin2012 <qiuxin2012cs@gmail.com>
2017-07-21 14:17:34 +0800

    change version to 0.2.0

--- scripts/run.example.sh
+++ scripts/run.example.sh
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-BIGDL_VERSION=0.3.0-SNAPSHOT
+BIGDL_VERSION=0.2.0
 
 SPARK1_DIR=spark-1.6.3-bin-hadoop2.6
 SPARK1_LINK=https://www.apache.org/dist/spark/spark-1.6.3/$SPARK1_DIR.tgz
