da0557280b4dade2dcecc9422c41fcdba95a8e75
ding <ding@localhost.localdomain>
2017-07-26 17:59:35 -0400

    fix comments

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/Recurrent.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/Recurrent.scala
@@ -266,7 +266,7 @@ class Recurrent[T : ClassTag]()
     require(cells != null && cells(times - 1).output != null,
       "getFinalStateAndCell need to be called after updateOutput")
     val cell = cells(times - 1).output.toTable(hidDim).asInstanceOf[Activity]
-    val cellState = if (cell.isInstanceOf[Table]) cell.asInstanceOf[Table]
+    val cellState = if (cell.isTable) cell.asInstanceOf[Table]
       .getOrElse[Tensor[T]](hidDim, null)
       else null
     val finalState = cells(times - 1).output.toTable[Tensor[T]](inputDim)
