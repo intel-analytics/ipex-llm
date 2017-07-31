57556430fa8f1b25cc50c2184e01a8092f6a55ba
Shane Huang <shengsheng.huang@intel.com>
2017-07-31 00:01:01 -0400

    fix broken links and revise script

--- docs/gen_site.py
+++ docs/gen_site.py
@@ -10,7 +10,7 @@ import subprocess
 def run_cmd(cmds, err_msg, s=False):
     cmd = cmds
     if s:
-        cmd = ' '.join(cmds) 
+        cmd = ' '.join(cmds)
     try:
         # print cmd
         p = Popen(cmd, shell=s)
@@ -25,7 +25,7 @@ def run_cmd(cmds, err_msg, s=False):
 
 parser = argparse.ArgumentParser(description='Process BigDL docs.')
 parser.add_argument('-s', '--scaladocs',
-    dest='scaladocsflag', action='store_true', 
+    dest='scaladocsflag', action='store_true',
     help='Add scala doc to site')
 parser.add_argument('-p', '--pythondocs',
     dest='pythondocsflag', action='store_true',
@@ -34,7 +34,8 @@ parser.add_argument('-m', '--startserver',
     dest='port', type=int,
     help='Start server at PORT after building')
 parser.add_argument('-d', '--startmkdocserve',
-    dest='mkdocserveflag', action='store_true',
+    dest='debugport', type=int,
+    #dest='mkdocserveflag', action='store_true',
     help=argparse.SUPPRESS)
 
 args = parser.parse_args()
@@ -43,7 +44,6 @@ scaladocs = args.scaladocsflag
 
 pythondocs = args.pythondocsflag
 
-mkdoc_serve = args.mkdocserveflag
 
 
 script_path = os.path.realpath(__file__)
@@ -54,7 +54,7 @@ run_cmd(['rm', '-rf', '{}/mkdocs_windmill'.format(dir_name)],
     'rm theme folder error')
 
 # check if mkdoc is installed
-run_cmd(['mkdocs', '--version'], 
+run_cmd(['mkdocs', '--version'],
     'Please install mkdocs and run this script again\n\te.g., pip install mkdocs')
 
 # git clone docs
@@ -63,7 +63,7 @@ run_cmd(['rm', '-rf', '/tmp/bigdl-doc'],
 
 # run_cmd(['git', 'clone', 'https://github.com/helenlly/bigdl-project.github.io.git', '/tmp/bigdl-doc'],
 #     'git clone readthedocs error')
-# 
+#
 run_cmd(['git', 'clone', 'https://github.com/bigdl-project/bigdl-project.github.io.git', '/tmp/bigdl-doc'],
     'git clone theme repo error')
 
@@ -77,7 +77,7 @@ run_cmd(['rm', '-rf', '/tmp/bigdl-doc'],
     'rm theme folder error')
 
 # mkdocs build
-run_cmd(['mkdocs', 'build'], 
+run_cmd(['mkdocs', 'build'],
     'mkdocs build error')
 
 if scaladocs:
@@ -102,17 +102,15 @@ if pythondocs:
 
 os.chdir(dir_name)
 
-if mkdoc_serve and args.port != None:
-    print 'Cannot start http server in debug mode'
-    sys.exit()
 
-if mkdoc_serve:
-    print 'start mkdoc server'
-    run_cmd(['mkdocs', 'serve', '--dev-addr=*:8000'], 
-        'mkdocs start serve error')
+if args.debugport != None:
+    print 'starting mkdoc server in debug mode'
+    addr = '--dev-addr=*:'+str(args.debugport)
+    run_cmd(['mkdocs', 'serve', addr],
+      'mkdocs start serve error')
 
 if args.port != None:
     os.chdir(dir_name + '/site')
     run_cmd(['python', '-m', 'SimpleHTTPServer', '{}'.format(args.port)],
         'start http server error')
-   
+
