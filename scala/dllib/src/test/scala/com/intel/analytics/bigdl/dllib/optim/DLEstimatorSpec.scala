a9f9133515572de273674cf8f08c94e07d6b70f6
Yuhao Yang <yuhao.yang@intel.com>
2017-08-08 15:17:37 -0700

    add more parameter to DLEstimator/DLClassifier (#1396)
    * add more parameter to DLEstimator
    
    * add unit test for optimMethod
    
    * set default to sgd

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/optim/DLEstimatorSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/optim/DLEstimatorSpec.scala
@@ -21,6 +21,7 @@ import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric.NumericFloat
 import com.intel.analytics.bigdl.utils.Engine
 import com.intel.analytics.bigdl.utils.RandomGenerator.RNG
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
 import org.apache.spark.ml.feature.MinMaxScaler
 import org.apache.spark.ml.{DLEstimator, DLModel, Pipeline, PipelineModel}
@@ -62,6 +63,9 @@ class DLEstimatorSpec extends FlatSpec with Matchers with BeforeAndAfter {
     assert(estimator.getLabelCol == "label")
     assert(estimator.getMaxEpoch == 100)
     assert(estimator.getBatchSize == 1)
+    assert(estimator.getLearningRate == 1.0)
+    assert(estimator.getLearningRateDecay == 0)
+
   }
 
   "An DLEstimator" should "fit on feature(one dimension Array[Double]) and label(Double)" in {
@@ -218,10 +222,8 @@ class DLEstimatorSpec extends FlatSpec with Matchers with BeforeAndAfter {
       val criterion = ClassNLLCriterion[Float]()
       val estimator = new DLEstimator[Float](model, criterion, Array(6), Array(1))
         .setBatchSize(nRecords)
-        // intentionally set low since this only validates data format compatibitliy
         .setMaxEpoch(maxEpoch)
         .setFeaturesCol("scaled")
-
       val pipeline = new Pipeline().setStages(Array(scaler, estimator))
 
       val pipelineModel = pipeline.fit(df)
