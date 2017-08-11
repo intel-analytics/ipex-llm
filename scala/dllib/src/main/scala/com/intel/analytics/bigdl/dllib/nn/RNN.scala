eb39a8fe3e1ee80aadc1af4e1f00a436fe4ce715
Yan Wan <ywanad@connect.ust.hk>
2017-08-11 13:57:10 +0800

    SimpleRNN to Graph (#1431)
    * SimpleRNN to Graph
    
    * move i2h, h2h to buildGraph in SimpleRNN

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/RNN.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/RNN.scala
@@ -16,6 +16,7 @@
 
 package com.intel.analytics.bigdl.nn
 
+import com.intel.analytics.bigdl.nn.Graph.ModuleNode
 import com.intel.analytics.bigdl.nn.abstractnn.{AbstractModule, Activity, TensorModule}
 import com.intel.analytics.bigdl.nn.abstractnn.TensorModule
 import com.intel.analytics.bigdl.optim.Regularizer
@@ -53,14 +54,6 @@ class RnnCell[T : ClassTag] (
   (implicit ev: TensorNumeric[T])
   extends Cell[T](Array(hiddenSize)) {
 
-  val parallelTable = ParallelTable[T]()
-  val i2h = Identity[T]()
-  val h2h = Linear[T](hiddenSize, hiddenSize,
-    wRegularizer = uRegularizer)
-  parallelTable.add(i2h)
-  parallelTable.add(h2h)
-  val cAddTable = CAddTable[T](false)
-
   override def preTopology: AbstractModule[Activity, Activity, T] =
     TimeDistributed[T](
       Linear[T](inputSize,
@@ -69,14 +62,18 @@ class RnnCell[T : ClassTag] (
         bRegularizer = bRegularizer))
     .asInstanceOf[AbstractModule[Activity, Activity, T]]
 
-  override var cell: AbstractModule[Activity, Activity, T] =
-    Sequential[T]()
-    .add(parallelTable)
-    .add(cAddTable)
-    .add(activation)
-    .add(ConcatTable()
-      .add(Identity[T]())
-      .add(Identity[T]()))
+  override var cell: AbstractModule[Activity, Activity, T] = buildGraph
+
+  private def buildGraph: Graph[T] = {
+    val i2h = Input()
+    val h2h = Linear[T](hiddenSize, hiddenSize,
+      wRegularizer = uRegularizer).inputs()
+    val add = CAddTable[T](false).inputs(i2h, h2h)
+    val activate = activation.inputs(add)
+    val out1 = Identity[T].inputs(activate)
+    val out2 = Identity[T].inputs(activate)
+    Graph(Array(i2h, h2h), Array(out1, out2))
+  }
 
   /**
    * Clear cached activities to save storage space or network bandwidth. Note that we use
@@ -99,16 +96,12 @@ class RnnCell[T : ClassTag] (
     case that: RnnCell[T] =>
       super.equals(that) &&
         (that canEqual this) &&
-        parallelTable == that.parallelTable &&
-        i2h == that.i2h &&
-        h2h == that.h2h &&
-        cAddTable == that.cAddTable &&
         cell == that.cell
     case _ => false
   }
 
   override def hashCode(): Int = {
-    val state = Seq(super.hashCode(), parallelTable, i2h, h2h, cAddTable, cell)
+    val state = Seq(super.hashCode(), cell)
     state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
   }
 }
