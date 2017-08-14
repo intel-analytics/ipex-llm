a631a3e7d730f267218eea53c98ffaf3bfcf1072
Wang Yanzhang <i8run15@gmail.com>
2017-08-14 16:01:54 +0800

    fix: should be to shouldEqual

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/torch/CrossEntropyCriterionSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/torch/CrossEntropyCriterionSpec.scala
@@ -52,8 +52,8 @@ class CrossEntropyCriterionSpec extends TorchSpec {
     val luaOutput1 = torchResult("output").asInstanceOf[Double]
     val luaOutput2 = torchResult("gradInput").asInstanceOf[Tensor[Double]]
 
-    luaOutput1 should be(output)
-    luaOutput2 should be(gradInput)
+    luaOutput1 shouldEqual output
+    luaOutput2 shouldEqual gradInput
 
     println("Test case : CrossEntropyCriterion, Torch : " + luaTime +
       " s, Scala : " + scalaTime / 1e9 + " s")
