1092c7445510cf65d858df1619a4c4a6425f571e
Yao Zhang <psyyz10@nottingham.ac.uk>
2017-08-16 17:12:24 +0800

    Add abstract Operation, Add, Substract, Multiply and Divide operations (#1462)
    * finsh Operation, Add, Substract, Multiply and Divide operations
    
    * meet code review
    
    * make acc not be supported
    
    * meet code review
    
    * make documentation better
    
    * rename package object
    
    * rename package object

---
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/ops/ModuleToOperation.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intel.analytics.bigdl.nn.ops
+
+import com.intel.analytics.bigdl.nn.abstractnn.{AbstractModule, Activity}
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+
+import scala.reflect.ClassTag
+
+/**
+ * Wrap a nn module to an [[Operation]]
+ *
+ * @param module an nn module
+ * @tparam A Input data type
+ * @tparam T Numeric type. Only support float/double now
+ */
+class ModuleToOperation[A <: Activity: ClassTag, T: ClassTag]
+(module: AbstractModule[A, Tensor[T], T])
+  (implicit ev: TensorNumeric[T])
+  extends Operation[A, T]{
+
+  override def updateOutput(input: A): Tensor[T] = {
+    output = module.forward(input)
+    output
+  }
+}
+
+object ModuleToOperation {
+  def apply[A <: Activity: ClassTag, T: ClassTag](model: AbstractModule[A, Tensor[T], T])
+    (implicit ev: TensorNumeric[T]): ModuleToOperation[A, T] = new ModuleToOperation(model)
+}
