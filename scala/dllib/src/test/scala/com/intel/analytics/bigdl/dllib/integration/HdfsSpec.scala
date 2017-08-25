dd70bde5847160f1c5bfe940031d065ba548fdc8
Fu Zhouwang <frankfzw@gmail.com>
2017-08-25 10:06:32 +0800

    add Tensorflow model save/load test in HdfsSpec  (#1373)
    * add method to get/put file in both local and remote fs
    
    * add test case to save/load tensorflow model on hdfs
    
    * add test case of Tensorflow model load/save for s3
    
    * revert code
    
    * change read/write method in TensorflowLoader/TensorflowSaver

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/integration/HdfsSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/integration/HdfsSpec.scala
@@ -15,7 +15,9 @@
  */
 package com.intel.analytics.bigdl.integration
 
+import java.nio.ByteOrder
 import java.nio.file.{Files, Paths}
+import java.util.UUID
 
 import com.google.protobuf.GeneratedMessage
 import com.intel.analytics.bigdl.models.lenet.LeNet5
@@ -27,6 +29,7 @@ import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric.NumericDouble
 import com.intel.analytics.bigdl.utils.caffe.{CaffeLoader, CaffePersister}
+import com.intel.analytics.bigdl.utils.tf.{Tensorflow, TensorflowLoader, TensorflowSaver}
 import com.intel.analytics.bigdl.utils.{Engine, File}
 import com.intel.analytics.bigdl.visualization.Summary
 import com.intel.analytics.bigdl.visualization.tensorboard.{FileReader, FileWriter}
@@ -143,6 +146,32 @@ class HdfsSpec extends FlatSpec with Matchers with BeforeAndAfter{
 
     model.getParameters() should be (modelFromHdfs.getParameters())
 
+  }
+
+  "Load tensorflow lenet to/from HDFS" should "works properly" in {
+    System.setProperty("bigdl.enableNHWC", "true")
+    val conv1 = SpatialConvolution[Float](1, 6, 5, 5).setName("conv1").inputs()
+    val tanh1 = Tanh[Float]().setName("tanh1").inputs(conv1)
+    val pool1 = SpatialMaxPooling[Float](2, 2, 2, 2).setName("pool1").inputs(tanh1)
+    val tanh2 = Tanh[Float]().setName("tanh2").inputs(pool1)
+    val conv2 = SpatialConvolution[Float](6, 12, 5, 5).setName("conv2").inputs(tanh2)
+    val pool2 = SpatialMaxPooling[Float](2, 2, 2, 2).setName("output").inputs(conv2)
+
+    val funcModel = Graph[Float](conv1, pool2)
+    val inputData = Tensor[Float](4, 1, 28, 28).rand()
+    val outputData = funcModel.forward(inputData).toTensor[Float]
+
+    val hdfsDir = hdfs + s"/${ com.google.common.io.Files.createTempDir().getPath() }"
+    TensorflowSaver.saveGraph[Float](funcModel, Seq(("input", Seq(4, 28, 28, 1))), hdfsDir)
+
+
+    val loadedModel = TensorflowLoader.load[Float](hdfsDir,
+      Seq("input"),
+      Seq("output"),
+      ByteOrder.LITTLE_ENDIAN)
+    val loadedOutput = loadedModel.forward(inputData).toTensor[Float]
+    loadedOutput.almostEqual(outputData, 1e-7)
+    System.setProperty("bigdl.enableNHWC", "false")
   }
 
   "Persist and Load Caffe to/from HDFS" should "works properly" in {
