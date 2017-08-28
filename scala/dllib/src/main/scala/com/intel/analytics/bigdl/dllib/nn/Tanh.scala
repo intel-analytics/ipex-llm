e2196bcff99df8dae0c9ab51ad5c198412cee4fa
zhangxiaoli73 <380761639@qq.com>
2017-08-28 09:19:07 +0800

    change tanh backward (#1502)

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/Tanh.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/Tanh.scala
@@ -31,6 +31,9 @@ import scala.reflect.ClassTag
 @SerialVersionUID(9062199894710333035L)
 class Tanh[@specialized(Float, Double) T: ClassTag](
   implicit ev: TensorNumeric[T]) extends TensorModule[T] {
+
+  private val buffer: Tensor[T] = Tensor[T]()
+
   override def updateOutput(input: Tensor[T]): Tensor[T] = {
     output.resizeAs(input)
     output.tanh(input)
@@ -39,10 +42,16 @@ class Tanh[@specialized(Float, Double) T: ClassTag](
 
   override def updateGradInput(input: Tensor[T], gradOutput: Tensor[T]): Tensor[T] = {
     gradInput.resizeAs(gradOutput)
-    gradInput.copy(gradOutput)
-    gradInput.map(output, (gradValue, outputValue) => ev.times(
-      gradValue, ev.minus(ev.fromType[Int](1), ev.times(outputValue, outputValue))))
+    buffer.resizeAs(output)
+    buffer.pow(output, ev.fromType(2)).cmul(gradOutput)
+    gradInput.sub(gradOutput, buffer)
     gradInput
+  }
+
+  override def clearState(): this.type = {
+    super.clearState()
+    buffer.set()
+    this
   }
 }
 
