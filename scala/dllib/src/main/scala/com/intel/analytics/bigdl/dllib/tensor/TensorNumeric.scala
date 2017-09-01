0e453020b7902df873a70d92c8718d78c51c8cf6
Yao Zhang <psyyz10@nottingham.ac.uk>
2017-09-01 16:34:21 +0800

    Add Squeeze, Prod, Sum, Reshape, Identity, ReLU (#1477)
    * finsh Operation, Add, Substract, Multiply and Divide operations
    
    * meet code review
    
    * make acc not be supported
    
    * meet code review
    
    * add some operations
    
    * add prodcut
    
    * add default prod method in UndefinedTensorNumeric class

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/tensor/TensorNumeric.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/tensor/TensorNumeric.scala
@@ -138,6 +138,8 @@ object TensorNumericMath {
 
     def sum(n: Int, a: Array[T], aOffset: Int, stride: Int): T
 
+    def prod(n: Int, a: Array[T], aOffset: Int, stride: Int): T
+
     def arraycopy(src: Array[T], srcPos: Int,
                   dest: Array[T], destPos: Int, length: Int): Unit
 
@@ -178,6 +180,10 @@ object TensorNumericMath {
         " in tensor does not support divide operation")
 
     def exp(x: T): T =
+      throw new UnsupportedOperationException(typeName +
+        " in tensor does not support exp operation")
+
+    def prod(n: Int, a: Array[T], aOffset: Int, stride: Int): T =
       throw new UnsupportedOperationException(typeName +
         " in tensor does not support exp operation")
 
@@ -584,6 +590,16 @@ object TensorNumericMath {
         }
       }
 
+      override def prod(n: Int, a: Array[Float], aOffset: Int, stride: Int): Float = {
+        var i = 0
+        var r = 1.0f
+        while (i < n) {
+          r *= a(aOffset + i * stride)
+          i += 1
+        }
+        r
+      }
+
       override def sum(n: Int, a: Array[Float], aOffset: Int, stride: Int): Float = {
         var i = 0
         var r = 0.0f
@@ -835,6 +851,16 @@ object TensorNumericMath {
             i += 1
           }
         }
+      }
+
+      override def prod(n: Int, a: Array[Double], aOffset: Int, stride: Int): Double = {
+        var i = 0
+        var r = 1.0
+        while (i < n) {
+          r *= a(aOffset + i * stride)
+          i += 1
+        }
+        r
       }
 
       override def sum(n: Int, a: Array[Double], aOffset: Int, stride: Int): Double = {
