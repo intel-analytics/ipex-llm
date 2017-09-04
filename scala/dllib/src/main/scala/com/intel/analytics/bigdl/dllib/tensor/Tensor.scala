6e4833551fe7df9d467f96288e9a6c946f737093
Ian Wong <yiheng.wang@intel.com>
2017-09-04 11:19:57 +0800

    Allow module output multiple tensor in Graph (#1507)
    * Allow module output multiple tensor in Graph
    
    * fix failed unit test
    
    * meet code review
    
    * meet code review
    
    * more unit test
    
    * add more unit tests
    
    * fix unit tests
    
    * fix style check
    
    * fix style check and unit test
    
    * meet code review
    
    * support nested output
    
    * meet code review

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/tensor/Tensor.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/tensor/Tensor.scala
@@ -701,6 +701,11 @@ object Tensor {
   MKL.isMKLLoaded
 
   /**
+   * Start index in BigDL. We count from 1.
+   */
+  val START_INDEX = 1
+
+  /**
    * Returns an empty tensor.
    *
    * @param ev
@@ -743,7 +748,18 @@ object Tensor {
     val flatTable = xs.flatten()
     val content = new Array[T](flatTable.length())
     for (i <- 1 to content.length) {
-      content(i - 1) = flatTable(i)
+      content(i - 1) = flatTable[Any](i) match {
+        case e: Boolean => ev.fromType(e)
+        case e: Char => ev.fromType(e)
+        case e: Short => ev.fromType(e)
+        case e: Int => ev.fromType(e)
+        case e: Long => ev.fromType(e)
+        case e: Float => ev.fromType(e)
+        case e: Double => ev.fromType(e)
+        case e: String => ev.fromType(e)
+        case _ => throw new IllegalArgumentException(s"Not support numeric type " +
+          flatTable[Any](i).getClass.getName)
+      }
     }
 
     val dims = new ArrayBuffer[Int]()
