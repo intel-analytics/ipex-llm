5903648f3a6fccf24c716ea6e5b8c89fea2b51e7
Yang Wang <tiandiwoxin@icloud.com>
2017-09-04 14:38:04 +0800

    support loading a subset of tf graph and support control dependency in graph (#1499)
    * support loading a subset of tf graph
    
    * support load model with specified inputs
    
    * add control dependency support in Graph
    
    * fix file reader conflict
    
    * fix bug
    
    * add some comments
    
    * fix control dep bug
    
    * fix doc

--- scala/dllib/src/test/resources/tf/models/control_dep.py
+++ scala/dllib/src/test/resources/tf/models/control_dep.py
@@ -14,27 +14,29 @@
 # limitations under the License.
 #
 import tensorflow as tf
-from nets import vgg
 from sys import argv
 
 from util import run_model
 
 def main():
-    """
-    You can also run these commands manually to generate the pb file
-    1. git clone https://github.com/tensorflow/models.git
-    2. export PYTHONPATH=Path_to_your_model_folder
-    3. python alexnet.py
-    """
-    height, width = 224, 224
-    inputs = tf.Variable(tf.random_uniform((1, height, width, 3)), name='input')
+
+    inputs = tf.Variable(tf.reshape(tf.range(0.0, 4.0), [4, 1]), name = 'input')
     inputs = tf.identity(inputs, "input_node")
-    net, end_points  = vgg.vgg_a(inputs, is_training = False)
-    print("nodes in the graph")
-    for n in end_points:
-        print(n + " => " + str(end_points[n]))
+    W1 = tf.Variable(tf.zeros([1,10])+0.2)
+    b1 = tf.Variable(tf.zeros([10])+0.1)
+    out1 = tf.nn.bias_add(tf.matmul(inputs, W1), b1)
+
+    W2 = tf.Variable(tf.zeros([1,10])+0.2)
+    b2 = tf.Variable(tf.zeros([10])+0.1)
+    out2 = tf.nn.bias_add(tf.matmul(inputs, W2), b2)
+
+    with tf.control_dependencies([inputs]):
+        output = tf.add_n([out1, out2])
+
+    named_output = tf.nn.relu(output, name="output")
+
     net_outputs = map(lambda x: tf.get_default_graph().get_tensor_by_name(x), argv[2].split(','))
-    run_model(net_outputs, argv[1], 'vgg_a', argv[3] == 'True')
+    run_model(net_outputs, argv[1], 'control_dep', argv[3] == 'True')
 
 if __name__ == "__main__":
     main()
