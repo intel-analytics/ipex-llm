18fdc33101b12434982e430bfda5c5204ac46272
yangw <yang3.wang@intel.com>
2017-09-04 15:54:52 +0800

    fix toTensor bug

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/python/api/PythonBigDL.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/python/api/PythonBigDL.scala
@@ -126,7 +126,7 @@ class PythonBigDL[T: ClassTag](implicit ev: TensorNumeric[T]) extends Serializab
     // clone here in case the the size of storage larger then the size of tensor.
     require(tensor != null, "tensor cannot be null")
     if (tensor.nElement() == 0) {
-      JTensor(Array(0), Array(0), typeName)
+      JTensor(Array(), Array(0), typeName)
     } else {
       val cloneTensor = tensor.clone()
       val result = JTensor(cloneTensor.storage().array().map(i => ev.toType[Float](i)),
