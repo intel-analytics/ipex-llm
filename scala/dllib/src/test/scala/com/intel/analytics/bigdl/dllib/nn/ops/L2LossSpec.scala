8a97044457413ba2445d1601f7ce29c54f164b5c
Yao Zhang <psyyz10@nottingham.ac.uk>
2017-09-15 10:34:58 +0800

    Add Floor L2Loss RandomUniform Rank MatMul SoftMax operations (#1533)
    * make a layer support different type
    
    * finish equal and make layer suppor different type
    
    * fix conflict
    
    * add Floor L2Loss RandomUniform Rank MatMul SoftMax
    
    * add TruncatedNormal
    
    * fit new code
    
    * make jenkens pass

---
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/nn/ops/L2LossSpec.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intel.analytics.bigdl.nn.ops
+
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.utils.T
+import org.scalatest.{FlatSpec, Matchers}
+
+class L2LossSpec extends FlatSpec with Matchers {
+  "L2Loss Double operation" should "works correctly" in {
+    val input =
+      Tensor[Double](
+        T(
+          T(1.5, 2.1, 2.9),
+          T(0.5, 1.1, 1.9)
+        ))
+
+    val expectOutput = Tensor[Double](T(10.07))
+
+    val output = L2Loss[Double]().forward(input)
+    output should be(expectOutput)
+  }
+
+  "L2Loss Float operation" should "works correctly" in {
+    val input =
+      Tensor[Float](
+        T(
+          T(1.5f, 2.1f, 2.9f),
+          T(0.5f, 1.1f, 1.9f)
+        ))
+
+    val expectOutput = Tensor[Float](T(10.07f))
+
+    val output = L2Loss[Float]().forward(input)
+    output should be(expectOutput)
+  }
+}
