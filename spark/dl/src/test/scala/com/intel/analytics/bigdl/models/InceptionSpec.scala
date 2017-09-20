1ee26c35452f211947a5f82acf07fd83cfb2eb21
qiuxin2012 <qiuxin2012cs@gmail.com>
2017-09-20 21:46:19 +0800

    revert unit test changed in #1424

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/models/InceptionSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/models/InceptionSpec.scala
@@ -774,7 +774,7 @@ class InceptionSpec extends TorchSpec {
     val loss = criterion.forward(output, labels)
 
     // since we already set the seed, the loss should match exactly
-    loss should be (6.9011583f)
+    loss should be (6.901158f)
   }
 
   "Inception_Layer_V1 graph" should "be correct" in {
