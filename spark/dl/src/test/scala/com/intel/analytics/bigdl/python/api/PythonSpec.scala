40b1a57dee659ff9227bc4703897d3e786c100c7
hkvision <huangkaivision@gmail.com>
2017-09-25 13:03:11 +0800

    Change modelTest to modelEvaluate

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/python/api/PythonSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/python/api/PythonSpec.scala
@@ -230,7 +230,7 @@ class PythonSpec extends FlatSpec with Matchers with BeforeAndAfter {
     // TODO: verify the parameters result
     val parameters = pp.modelGetParameters(trainedModel)
 //    println(parameters)
-    val testResult = pp.modelTest(trainedModel,
+    val testResult = pp.modelEvaluate(trainedModel,
       data.toJavaRDD(),
       batchSize = 32,
       valMethods = util.Arrays.asList(new Top1Accuracy()))
