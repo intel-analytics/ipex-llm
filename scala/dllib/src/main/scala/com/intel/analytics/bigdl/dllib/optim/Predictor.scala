1b5fdb1bdcbd4e49096a632fdb76a74a1a3835e1
zhangxiaoli73 <380761639@qq.com>
2017-09-26 14:48:56 +0800

    refactor predictor (#1582)
    * refactor predictor
    
    * predictClass share model output memory
    
    * refactor repeatMemory to shareBuffer

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/optim/Predictor.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/optim/Predictor.scala
@@ -22,6 +22,7 @@ import com.intel.analytics.bigdl.models.utils.ModelBroadcast
 import com.intel.analytics.bigdl.nn.abstractnn.Activity
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import org.apache.spark.rdd.RDD
+import org.dmg.pmml.False
 
 import scala.reflect.ClassTag
 
@@ -36,8 +37,8 @@ class Predictor[T: ClassTag] private[optim](
 
   private val batchPerPartition = 4
 
-  def predictClass(dataSet: RDD[Sample[T]]): RDD[Int] = {
-    val result = predict(dataSet)
+  def predictClass(dataSet: RDD[Sample[T]], batchSize: Int = -1): RDD[Int] = {
+    val result = predict(dataSet, batchSize, true)
     result.mapPartitions { partition =>
       partition.map(output => {
         val _output = output.toTensor[T]
@@ -48,18 +49,32 @@ class Predictor[T: ClassTag] private[optim](
     }
   }
 
-  def predict(dataSet: RDD[Sample[T]]): RDD[Activity] = {
+  def predict(dataSet: RDD[Sample[T]], batchSize: Int = -1,
+              shareBuffer: Boolean = false): RDD[Activity] = {
     val modelBroad = ModelBroadcast[T].broadcast(dataSet.sparkContext, model.evaluate())
     val partitionNum = dataSet.partitions.length
+    val totalBatch = if (batchSize > 0) {
+      require(batchSize % partitionNum == 0, s"Predictor.predict: total batch size $batchSize " +
+        s"should be divided by partitionNum ${partitionNum}")
+      batchSize
+    } else {
+      batchPerPartition * partitionNum
+    }
     val otherBroad = dataSet.sparkContext.broadcast(SampleToMiniBatch(
-      batchSize = batchPerPartition * partitionNum, partitionNum = Some(partitionNum)))
+      batchSize = totalBatch,
+      partitionNum = Some(partitionNum)), shareBuffer)
     dataSet.mapPartitions { partition =>
       val localModel = modelBroad.value()
-      val localTransformer = otherBroad.value.cloneTransformer()
+      val localTransformer = otherBroad.value._1.cloneTransformer()
+      val repeatMemory = otherBroad.value._2
       val miniBatch = localTransformer(partition)
       miniBatch.flatMap( batch => {
         val output = localModel.forward(batch.getInput).toTensor[T]
-        output.split(1)
+        if (shareBuffer) {
+          output.split(1)
+        } else {
+          output.clone().split(1)
+        }
       })
     }
   }
