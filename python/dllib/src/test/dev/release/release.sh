417f4b91c0dfba67765514166f1f9173b0f2418d
Kai Huang <huangkaivision@gmail.com>
2017-09-27 17:16:16 +0800

    Handle jar path different spark versions; Redirect Spark Logs in Python (#1543)
    * Use `spark.driver.extraClassPath` to get the jar for spark 2.2+.
    * Show BigDL INFO logs and redirect spark logs in python models.

--- python/dllib/src/test/dev/release/release.sh
+++ python/dllib/src/test/dev/release/release.sh
@@ -25,7 +25,7 @@ BIGDL_PYTHON_DIR="$(cd ${RUN_SCRIPT_DIR}/../../../../pyspark; pwd)"
 echo $BIGDL_PYTHON_DIR
 
 if (( $# < 2)); then
-  echo "Bad parameters. Uasge: release.sh mac spark_2.x"
+  echo "Bad parameters. Usage: release.sh mac spark_2.x"
   exit -1
 fi
 
