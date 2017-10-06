9006930ec9d4da98be7a88615eb3a752bbe3b14f
ding <ding@localhost.localdomain>
2017-10-06 07:57:42 -0400

    add seq2seq ut

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/Seq2seq.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/Seq2seq.scala
@@ -24,49 +24,49 @@ import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import scala.reflect.ClassTag
 import com.intel.analytics.bigdl.utils.{T, Table}
 
-class Seq2seq[T: ClassTag](encoderCells: Array[Cell[T]], decoderCells: Array[Cell[T]],
+class Seq2seq[T: ClassTag](encoderRecs: Array[Recurrent[T]], decoderRecs: Array[Recurrent[T]],
   preEncoder: AbstractModule[Activity, Activity, T] = null,
   preDecoder: AbstractModule[Activity, Activity, T] = null,
-  feedPreviousOutput: Boolean = false, outputLength: Int = -1,
   decoderInputType: DecoderInputType = DecoderInputType.ENCODERINPUTLASTTIME)
   (implicit ev: TensorNumeric[T]) extends AbstractModule[Activity, Tensor[T], T] {
-  var encoderRecs = new Array[Recurrent[T]](encoderCells.length)
-  var decoderRecs = new Array[Recurrent[T]](decoderCells.length)
   var decoderInput: Tensor[T] = null
   var encoderInput: Tensor[T] = null
   var encoderOutput: Tensor[T] = null
 
   val encoder = if (preDecoder == null) Sequential() else Sequential().add(preEncoder)
-  for (i <- 0 until encoderCells.size) {
-    val rec = Recurrent[T]().add(encoderCells(i))
-    encoderRecs(i) = rec
-    encoder.add(rec)
-  }
+//  for (i <- 0 until encoderCells.size) {
+//    val rec = Recurrent[T]().add(encoderCells(i))
+//    encoderRecs(i) = rec
+//    encoder.add(rec)
+//  }
+  encoderRecs.foreach(encoder.add(_))
 
   val decoder = if (preDecoder == null) Sequential() else Sequential().add(preDecoder)
-  if (feedPreviousOutput) {
-    require(outputLength >= 1, "output length must be greater than 0")
-    decoderRecs(0) = if (decoderCells.length == 1) {
-      RecurrentDecoder(outputLength).add(decoderCells.head)
-    } else RecurrentDecoder(outputLength).add(MultiRNNCell(decoderCells))
-    decoder.add(decoderRecs.head)
-  } else {
-    for ((x, i) <- decoderCells.view.zipWithIndex) {
-      decoderRecs(i) = Recurrent[T]().add(x)
-      decoder.add(decoderRecs(i))
-    }
-  }
+//  if (feedPreviousOutput) {
+//    require(outputLength >= 1, "output length must be greater than 0")
+//    decoderRecs(0) = if (decoderCells.length == 1) {
+//      RecurrentDecoder(outputLength).add(decoderCells.head)
+//    } else RecurrentDecoder(outputLength).add(MultiRNNCell(decoderCells))
+//    decoder.add(decoderRecs.head)
+//  } else {
+//    for ((x, i) <- decoderCells.view.zipWithIndex) {
+//      decoderRecs(i) = Recurrent[T]().add(x)
+//      decoder.add(decoderRecs(i))
+//    }
+//  }
+  decoderRecs.foreach(decoder.add(_))
 
   val module: AbstractModule[Activity, Activity, T] = Sequential().add(encoder).add(decoder)
 
   override def updateOutput(input: Activity): Tensor[T] = {
+    val feedbackPreviousOutput = decoderRecs.head.isInstanceOf[RecurrentDecoder[T]]
     decoderInputType match {
       case DecoderInputType.ENCODERINPUTSPLIT =>
         encoderInput = input.toTable[Tensor[T]](1)
         decoderInput = input.toTable[Tensor[T]](2)
       case DecoderInputType.ENCODERINPUTLASTTIME =>
-        require(feedPreviousOutput == true, "ENCODERINPUTLASTTIME can" +
-          "only work with feedPreviousOuptput is true")
+        require(feedbackPreviousOutput, "ENCODERINPUTLASTTIME can" +
+          "only work with RecurrentDecoder")
         encoderInput = input.toTensor
         decoderInput = input.toTensor.select(2, input.toTensor.size(2))
       case DecoderInputType.ZEROS =>
@@ -76,19 +76,19 @@ class Seq2seq[T: ClassTag](encoderCells: Array[Cell[T]], decoderCells: Array[Cel
     }
     encoderOutput = encoder.forward(encoderInput).toTensor
     if (decoderInput == null) {
-      decoderInput = if (feedPreviousOutput) {
+      decoderInput = if (feedbackPreviousOutput) {
         val _outputSize = encoderOutput.size()
         Tensor[T](Array(_outputSize(0)) ++ _outputSize.drop(2))
       } else Tensor[T](encoderOutput.size())
     }
 
-    if (feedPreviousOutput) {
-      if (decoderCells.length == 1) {
-        decoderRecs.head.setHiddenState(encoderRecs.map(_.getHiddenState()).head)
-      } else {
+    if (feedbackPreviousOutput) {
+//      if (decoderCells.length == 1) {
+//        decoderRecs.head.setHiddenState(encoderRecs.map(_.getHiddenState()).head)
+//      } else {
         decoderRecs.head.asInstanceOf[RecurrentDecoder[T]]
           .setHiddenStates(encoderRecs.map(_.getHiddenState()))
-      }
+//      }
     } else {
       for ((x, i) <- encoderRecs.view.zipWithIndex) {
         decoderRecs(i).setHiddenState(x.getHiddenState())
@@ -111,20 +111,20 @@ class Seq2seq[T: ClassTag](encoderCells: Array[Cell[T]], decoderCells: Array[Cel
   }
 
   override def backward(input: Activity, gradOutput: Tensor[T]): Tensor[T] = {
-    decoder.backward(decoderInput, gradOutput)
-    if (feedPreviousOutput) {
-      if (decoderCells.length == 1) {
-        val gradState = decoderRecs.head.getGradHiddenState()
-        encoderRecs(encoderCells.length - 1).setGradHiddenState(gradState)
-      } else {
+    val ttt = decoder.backward(decoderInput, gradOutput)
+    if (decoderRecs.head.isInstanceOf[RecurrentDecoder[T]]) {
+//      if (decoderCells.length == 1) {
+//        val gradState = decoderRecs.head.getGradHiddenState()
+//        encoderRecs(encoderCells.length - 1).setGradHiddenState(gradState)
+//      } else {
         val gradHiddenStates = decoderRecs.head
           .asInstanceOf[RecurrentDecoder[T]].getGradHiddenStates()
         for ((x, i) <- gradHiddenStates.view.zipWithIndex) {
           encoderRecs(i).setGradHiddenState(x)
         }
-      }
+//      }
     } else {
-      for ((x, i) <- encoderRecs.view.zipWithIndex) {
+      for ((x, i) <- decoderRecs.view.zipWithIndex) {
         encoderRecs(i).setGradHiddenState(x.getGradHiddenState())
       }
     }
@@ -144,14 +144,12 @@ class Seq2seq[T: ClassTag](encoderCells: Array[Cell[T]], decoderCells: Array[Cel
 }
 
 object Seq2seq {
-  def apply[@specialized(Float, Double) T: ClassTag](encoderCells: Array[Cell[T]],
-    decoderCells: Array[Cell[T]], preEncoder: AbstractModule[Activity, Activity, T] = null,
+  def apply[@specialized(Float, Double) T: ClassTag](encoderCells: Array[Recurrent[T]],
+    decoderCells: Array[Recurrent[T]], preEncoder: AbstractModule[Activity, Activity, T] = null,
     preDecoder: AbstractModule[Activity, Activity, T] = null,
-    outputLength: Int = -1, feedPreviousOutput: Boolean = false,
     decoderInputType: DecoderInputType = DecoderInputType.ENCODERINPUTLASTTIME)
     (implicit ev: TensorNumeric[T]): Seq2seq[T] = {
-    new Seq2seq[T](encoderCells, decoderCells, preEncoder, preDecoder,
-      feedPreviousOutput, outputLength, decoderInputType)
+    new Seq2seq[T](encoderCells, decoderCells, preEncoder, preDecoder, decoderInputType)
   }
 }
 
--- spark/dl/src/test/scala/com/intel/analytics/bigdl/nn/Seq2seqSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/nn/Seq2seqSpec.scala
@@ -41,19 +41,19 @@ class Seq2seqSpec extends FlatSpec with BeforeAndAfter with Matchers {
     val input = Tensor[Double](batchSize, seqLength, inputSize, 5, 5).rand
     val gradOutput = Tensor[Double](batchSize, seqLength, hiddenSize, 5, 5).rand
 
-    val encoderCells = Array(ConvLSTMPeephole[Double](
+    val encoderRecs = Array(Recurrent().add(ConvLSTMPeephole[Double](
       inputSize,
       hiddenSize,
       kernalW, kernalH,
-      1), ConvLSTMPeephole[Double](
+      1)), Recurrent().add(ConvLSTMPeephole[Double](
       inputSize,
       hiddenSize,
       kernalW, kernalH,
-      1), ConvLSTMPeephole[Double](
+      1)), Recurrent().add(ConvLSTMPeephole[Double](
       inputSize,
       hiddenSize,
       kernalW, kernalH,
-      1)).asInstanceOf[Array[Cell[Double]]]
+      1)))
 
     val decoderCells = Array(ConvLSTMPeephole[Double](
       inputSize,
@@ -69,15 +69,18 @@ class Seq2seqSpec extends FlatSpec with BeforeAndAfter with Matchers {
       kernalW, kernalH,
       1)).asInstanceOf[Array[Cell[Double]]]
 
-    val model = Seq2seq(encoderCells, decoderCells, feedPreviousOutput = true,
-      outputLength = seqLength, decoderInputType = DecoderInputType.ENCODERINPUTLASTTIME)
+    var decoderRecs = Array(RecurrentDecoder(seqLength).add(MultiRNNCell(decoderCells))
+      .asInstanceOf[Recurrent[Double]])
+    val model = Seq2seq(encoderRecs, decoderRecs,
+      decoderInputType = DecoderInputType.ENCODERINPUTLASTTIME)
 
     for (i <- 0 until 3) {
       model.forward(input).toTensor
       model.backward(input, gradOutput)
     }
 
-    val model2 = Seq2seq(encoderCells, decoderCells,
+    decoderRecs = decoderCells.map(Recurrent().add(_))
+    val model2 = Seq2seq(encoderRecs, decoderRecs,
       decoderInputType = DecoderInputType.ENCODERINPUTSPLIT)
 
     for (i <- 0 until 3) {
@@ -85,7 +88,7 @@ class Seq2seqSpec extends FlatSpec with BeforeAndAfter with Matchers {
       model2.backward(input, gradOutput)
     }
 
-    val model3 = Seq2seq(encoderCells, decoderCells,
+    val model3 = Seq2seq(decoderRecs, decoderRecs,
       decoderInputType = DecoderInputType.ZEROS)
 
     for (i <- 0 until 3) {
@@ -94,43 +97,56 @@ class Seq2seqSpec extends FlatSpec with BeforeAndAfter with Matchers {
     }
   }
 
-  "A Seq2seq" should "work2 " in {
+  "A Seq2seq" should "generate the same result with torch " in {
     import com.intel.analytics.bigdl.numeric.NumericDouble
     val hiddenSize = 2
     val inputSize = 5
-    val seqLength = 3
     val seed = 123
-    val batchSize = 2
 
     RNG.setSeed(seed)
     val encInSeq = Array(1.0, 2, 3, 3, 2, 1)
     val decInSeq = Array(1.0, 2, 3, 4, 4, 3, 2, 1)
-    val decOutData = Array(2.0, 3, 4, 1, 1, 2, 3, 4)
+    val decOutData = Array(2.0, 3, 4, 1, 1, 2, 4, 3)
     val input1 = Tensor[Double](encInSeq, Array(2, 3))
-    val input = T(input1, Tensor[Double](decInSeq, Array(2, 4)))
+    val input2 = Tensor[Double](decInSeq, Array(2, 4))
+    val input = T(input1, input2)
     val decOutSeq = Tensor[Double](decOutData, Array(2, 4))
-    
-    val lstm = LSTM[Double](
-      hiddenSize,
-      hiddenSize)
-    val encoderCells = Array(lstm).asInstanceOf[Array[Cell[Double]]]
-    val enclstm = Recurrent().add(lstm).getParameters()._1
 
-    val lstm2 = LSTM[Double](
+    val enclstm = Recurrent().add(LSTM[Double](
       hiddenSize,
-      hiddenSize)
-    val decoderCells = Array(lstm2).asInstanceOf[Array[Cell[Double]]]
-    val lstmP2 = lstm2.getParameters()._1
+      hiddenSize))
+    val encoderRec = Array(enclstm)
+    val (enclstmW, enclstmG) = enclstm.getParameters()
 
-    val lookuptable1 = LookupTable(inputSize, hiddenSize)
-    val lookuptableP1 = lookuptable1.getParameters()._1
+    val declstm = Recurrent().add(LSTM[Double](
+      hiddenSize,
+      hiddenSize))
+    val decoderRec = Array(declstm)
+    val (declstmW, declstmG) = declstm.getParameters()
+
+    val enclookuptable = LookupTable(inputSize, hiddenSize)
+    val (enclookuptableW, enclookuptableG) = enclookuptable.getParameters()
+
+    val declookuptable = LookupTable(inputSize, hiddenSize)
+    val (declookuptableW, declookuptableG) = declookuptable.getParameters()
+
+    val linear = Linear(hiddenSize, inputSize)
+    val (linearW, linearG) = linear.getParameters()
     
     val code =
       s"""
          |
       |-- 1.4. Combine 1.1 and 1.3 to produce final model
-         |require 'rnn'
+         |require 'rnn'         
          |
+         |local function forwardConnect(encLSTM, decLSTM)
+         |    decLSTM.userPrevOutput = nn.rnn.recursiveCopy(decLSTM.userPrevOutput, encLSTM.outputs[3])
+         |    decLSTM.userPrevCell = nn.rnn.recursiveCopy(decLSTM.userPrevCell, encLSTM.cells[3])
+         |end
+         |
+         |local function backwardConnect(encLSTM, decLSTM)
+         |    encLSTM:setGradHiddenState(3, decLSTM:getGradHiddenState(0))
+         |end
          |local enc1 = nn.Sequential()
          |lookup = nn.LookupTable(5, 2)
          |local parameters, gradParameters = lookup:getParameters()
@@ -138,54 +154,89 @@ class Seq2seqSpec extends FlatSpec with BeforeAndAfter with Matchers {
          |enc1:add(lookup)
          |enc1:add(nn.SplitTable(1, 2)) -- works for both online and mini-batch mode
          |enc1output = enc1:forward(input)
+         |enc1:zeroGradParameters()
          |
          |local enc = nn.Sequential()
          |local encLSTM = nn.FastLSTM(2, 2)         
          |enc:add(nn.Sequencer(encLSTM))
          |enc:add(nn.SelectTable(-1))
          |local parameters2, gradParameters2 = enc:getParameters()
-         |--parameters2:copy(enclstm)         
-         |         
-         |local output = enc:forward(enc1output)
-         |--local gradInput = model:backward(input, gradOutput)
+         |parameters2:copy(enclstm)         
+         |enc:zeroGradParameters()
+         |
+         |local dec = nn.Sequential()
+         |lookup2 = nn.LookupTable(5, 2)
+         |local parameters3, gradParameters3 = lookup2:getParameters()
+         |parameters3:copy(lookuptableP2)
+         |dec:add(lookup2)
+         |dec:add(nn.SplitTable(1, 2)) -- works for both online and mini-batch mode
+         |local decLSTM = nn.FastLSTM(2, 2)
+         |local parameters4, gradParameters4 = decLSTM:getParameters()
+         |parameters4:copy(declstm)
+         |dec:add(nn.Sequencer(decLSTM))
+         |linear2 = nn.Linear(2, 5)
+         |dec:add(nn.Sequencer(linear2))
+         |local parameters5, gradParameters5 = linear2:getParameters()
+         |parameters5:copy(linear2P)
+         |dec:add(nn.Sequencer(nn.LogSoftMax()))
+         |dec:zeroGradParameters()
+         |
+         |local encOutput = enc:forward(enc1output)
+         |forwardConnect(encLSTM, decLSTM)
+         |local decOutput = dec:forward(input2)
+         |local criterion = nn.SequencerCriterion(nn.ClassNLLCriterion())
+         |local decOut = nn.SplitTable(1, 1):forward(decOutSeq)
+         |local Edec = criterion:forward(decOutput, decOut)
+         |
+         |local gEdec = criterion:backward(decOutput, decOut)
+         |local decGradInput = dec:backward(input2, gEdec)         
+         |backwardConnect(encLSTM, decLSTM)
+         |local zeroTensor = torch.zeros(encOutput:size())         
+         |local encGradInput = enc:backward(enc1output, zeroTensor)
+         |local enc1GradInput = enc1:backward(input, encGradInput)       
     """.stripMargin
     scala.Seq
 
     val (luaTime, torchResult) = TH.run(code,
-      Map("input" -> input1, "lookuptableP1" -> lookuptableP1, "enclstm" -> enclstm),
-      Array("output", "enc1output", "parameters2"))
+      Map("input" -> input1, "lookuptableP1" -> enclookuptableW, "enclstm" -> enclstmW,
+        "input2" -> input2, "lookuptableP2" -> declookuptableW, "declstm" -> declstmW,
+        "decOutSeq" -> decOutSeq, "linear2P" -> linearW),
+      Array("gradParameters2", "decOutput", "Edec",
+      "encGradInput", "gEdec", "gradParameters4"))
 
-    val luaOutput = torchResult("output").asInstanceOf[Tensor[Double]]
-    val enc1output = torchResult("enc1output").asInstanceOf[Table]
-    val luagradParameters = torchResult("parameters2").asInstanceOf[Tensor[Double]]
+    val luaOutput = torchResult("decOutput").asInstanceOf[Table]
+    val luaLoss = torchResult("Edec").asInstanceOf[Double]
+    val luagEdc = torchResult("gEdec").asInstanceOf[Table]
+    val luaencGradInput = torchResult("encGradInput").asInstanceOf[Table]
+    val luaenclstmG = torchResult("gradParameters2").asInstanceOf[Tensor[Double]]
+    val luadeclstmG = torchResult("gradParameters4").asInstanceOf[Tensor[Double]]
 
-    val tt2 = lookuptable1.forward(input1)
-    val tt3 = Recurrent().add(encoderCells.head).forward(tt2)
-
-    val preEncoder = Sequential().add(lookuptable1)
-
-    val lookuptable2 = LookupTable(inputSize, hiddenSize)
-    val lookuptableP2 = lookuptable2.getParameters()._1
-
-    val preDecoder = Sequential().add(lookuptable2)
-    val seq2seq = Seq2seq(encoderCells, decoderCells, preEncoder = preEncoder,
-      preDecoder = preDecoder, feedPreviousOutput = false,
-      decoderInputType = DecoderInputType.ENCODERINPUTSPLIT)
-    val model = Sequential().add(seq2seq)
+    val preEncoder = Sequential().add(enclookuptable)
+    val preDecoder = Sequential().add(declookuptable)
+    val seq2seq = Seq2seq(encoderRec, decoderRec, preEncoder = preEncoder,
+      preDecoder = preDecoder, decoderInputType = DecoderInputType.ENCODERINPUTSPLIT)
     
-    val linear = Linear(hiddenSize, inputSize)
-    model.add(TimeDistributed(linear))
+    val model = Sequential().add(seq2seq).add(TimeDistributed(linear))
       .add(TimeDistributed(LogSoftMax()))
+    val output = model.forward(input).toTensor
+    for (i <- 1 to 4) {
+      output.select(2, i).almostEqual(luaOutput.toTable[Tensor[Double]](i), 1e-8)
+    }
 
     val criterion = TimeDistributedCriterion(ClassNLLCriterion())
-
-    
-    val output = model.forward(input).toTensor
     val loss = criterion.forward(output, decOutSeq)
-    
-//    val gEdec = criterion.backward(output, decOutSeq)
-//    model.backward(input, gEdec)
-    val t = 0
+    assert(abs(loss - luaLoss) <= 1e-8)
+
+    val gEdec = criterion.backward(output, decOutSeq)
+    for (i <- 1 to 4) {
+      gEdec.select(2, i).almostEqual(luagEdc.toTable[Tensor[Double]](i), 1e-8)
+    }
+    model.backward(input, gEdec)
+    for (i <- 1 to 3) {
+      luaencGradInput.toTable[Tensor[Double]](i).almostEqual(enclstm.gradInput.select(2, i), 1e-8)
+    }
+    luaenclstmG.almostEqual(enclstmG, 1e-8)
+    luadeclstmG.almostEqual(declstmG, 1e-8)
   }
 }
 
