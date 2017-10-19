23e9f41acf4ef1e7ae45e4c343632325db267630
Ian Wong <yiheng.wang@intel.com>
2017-10-19 14:35:17 +0800

    Support load more tensorflow operations (#1656)
    * Support load more tf operation
    
    * refine session
    
    * fix style issue
    
    * meet code reivew

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/utils/tf/loaders/Transpose.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/utils/tf/loaders/Transpose.scala
@@ -21,6 +21,7 @@ import com.intel.analytics.bigdl.Module
 import com.intel.analytics.bigdl.nn.{Contiguous, Sequential, Transpose}
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.utils.tf.Context
 import org.tensorflow.framework.NodeDef
 
 import scala.collection.mutable.ArrayBuffer
@@ -30,8 +31,8 @@ class Transpose extends TensorflowOpsLoader {
 
   import Utils._
 
-  override def build[T: ClassTag](nodeDef: NodeDef, byteOrder: ByteOrder)
-                                 (implicit ev: TensorNumeric[T]): Module[T] = {
+  override def build[T: ClassTag](nodeDef: NodeDef, byteOrder: ByteOrder
+  , context: Context[T])(implicit ev: TensorNumeric[T]): Module[T] = {
 
     Adapter[T](Array(2), tensorArrays => {
       // this must be defined inside this function, otherwise the loader will be
