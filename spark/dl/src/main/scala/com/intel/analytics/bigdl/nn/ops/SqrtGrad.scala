738ced74742cb4fc4e19ce830be9ac45501bfcfc
yangw <yang3.wang@intel.com>
2017-11-02 14:25:48 +0800

    add square related ops

---
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/ops/SqrtGrad.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intel.analytics.bigdl.nn.ops
+
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.{NumericWildcard, TensorNumeric}
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.utils.Table
+
+import scala.reflect.ClassTag
+
+class SqrtGrad[T: ClassTag](implicit ev: TensorNumeric[T])
+  extends Operation[Table, Tensor[_], T] {
+
+  override def updateOutput(inputs: Table): Tensor[_] = {
+    val grads = inputs[Tensor[NumericWildcard]](2)
+    val y = inputs[Tensor[NumericWildcard]](1)
+
+    require(grads.getType() == y.getType(), "The numeric type of x and y must be the same," +
+      " but got x: ${grads.getType()}, y: ${y.getType()}")
+
+    if (output.getType() != grads.getType()) {
+      output = grads.emptyInstance()
+    }
+
+    output.asInstanceOf[Tensor[NumericWildcard]]
+      .resizeAs(grads).copy(grads).mul(0.5f).div(y)
+
+    output
+  }
+}
+
+object SqrtGrad {
+  def apply[T: ClassTag]()(implicit ev: TensorNumeric[T]): SqrtGrad[T] = new SqrtGrad()
+}
