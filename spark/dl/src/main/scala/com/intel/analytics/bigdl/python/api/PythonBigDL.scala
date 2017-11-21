b2b71e70a4c9c304bf03bb39d4cc31386e852432
wzhongyuan <wzhongyuan@gmail.com>
2017-11-21 17:42:29 +0800

    keras support to add CosineProximityCriterion

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/python/api/PythonBigDL.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/python/api/PythonBigDL.scala
@@ -2221,6 +2221,10 @@ class PythonBigDL[T: ClassTag](implicit ev: TensorNumeric[T]) extends Serializab
   def setRunningStd(module: BatchNormalization[T], runningStd: JTensor): Unit = {
     module.runningVar.set(toTensor(runningStd))
   }
+
+  def createCosineProximityCriterion(): CosineProximityCriterion[T] = {
+    CosineProximityCriterion[T]()
+  }
 }
 
 object PythonBigDLUtils {
