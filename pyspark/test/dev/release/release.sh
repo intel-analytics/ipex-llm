842a45cca53697204f3d41eae476b8cd092a9eb4
Hawkwood <2041829103@qq.com>
2017-11-21 14:56:59 +0800

    Update release.sh

--- pyspark/test/dev/release/release.sh
+++ pyspark/test/dev/release/release.sh
@@ -68,4 +68,4 @@ ${wheel_command}
 upload_command="twine upload dist/BigDL-${bigdl_version}-py2.py3-none-${verbose_pname}.whl"
 echo "Please manually upload with this command:  $upload_command"
 
-
+$upload_command
