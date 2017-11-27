9c0cfc455962ce79bc30438315eebf544af47698
lizhichao <lisurprise@gmail.com>
2017-11-27 17:35:24 +0800

    backend

--- pyspark/bigdl/keras/backend.py
+++ pyspark/bigdl/keras/backend.py
@@ -42,11 +42,12 @@ def to_sample_rdd(sc, x, y):
 class KerasModelWrapper():
     def __init__(self, kmodel):
         self.bmodel = DefinitionLoader.from_kmodel(kmodel)
+        WeightLoader.load_weights_from_kmodel(self.bmodel, kmodel)  # share the same weight.
         self.criterion = OptimConverter.to_bigdl_criterion(kmodel.loss)
         self.optim_method = OptimConverter.to_bigdl_optim_method(kmodel.optimizer)
         self.metrics = OptimConverter.to_bigdl_metrics(kmodel.metrics) if kmodel.metrics else None
 
-    def evaluate(self, x, y, batch_size=32, verbose=1, sample_weight=None):
+    def evaluate_distributed(self, x, y, batch_size=32, verbose=1, sample_weight=None):
         if self.metrics:
             sc = get_spark_context()
             sample_rdd = to_sample_rdd(sc, x, y)
@@ -54,8 +55,26 @@ class KerasModelWrapper():
         else:
             raise Exception("No Metrics found.")
 
-    # TODO: Support batch_size??
+    def evaluate(self, x, y, batch_size=32, verbose=1, sample_weight=None):
+        raise Exception("Haven't be implemented yet")
+
     def predict(self, x, batch_size=32, verbose=0):
+        """Generates output predictions for the input samples,
+        processing the samples in a batched way.
+
+        # Arguments
+            x: the input data, as a Numpy array
+                (or list of Numpy arrays if the model has multiple outputs).
+            batch_size: integer.
+            verbose: verbosity mode, 0 or 1.
+
+        # Returns
+            A Numpy array of predictions.
+        """
+        return self.bmodel.predict_local(x)
+
+    # TODO: Support batch_size??
+    def predict_distributed(self, x, batch_size=32, verbose=0):
         """Generates output predictions for the input samples,
         processing the samples in a batched way.
 
@@ -73,8 +92,7 @@ class KerasModelWrapper():
             lambda i: bcommon.Sample.from_ndarray(i, np.zeros((1))))
         return np.asarray(self.bmodel.predict(x_rdd).collect())
 
-
-    def fit(self, x, y, batch_size=32, nb_epoch=10, verbose=1, callbacks=None,
+    def fit(self, X, y, batch_size=32, nb_epoch=10, verbose=1, callbacks=None,
             validation_split=0., validation_data=None, shuffle=True,
             class_weight=None, sample_weight=None, initial_epoch=0):
         if callbacks:
@@ -90,6 +108,36 @@ class KerasModelWrapper():
         if validation_split != 0.:
             unsupport_exp("validation_split")
 
+        sc = get_spark_context()
+        bopt = boptimizer.LocalOptimizer(
+            X=X,
+            y=y,
+            model=self.bmodel,
+            criterion=self.criterion,
+            end_trigger=boptimizer.MaxEpoch(nb_epoch),
+            batch_size=batch_size,
+            optim_method=self.optim_method,
+            cores=None
+        )
+        # TODO: enable validation for local optimizer.
+        bopt.optimize()
+
+
+    def fit_distributed(self, x, y, batch_size=32, nb_epoch=10, verbose=1, callbacks=None,
+            validation_split=0., validation_data=None, shuffle=True,
+            class_weight=None, sample_weight=None, initial_epoch=0):
+        if callbacks:
+            raise Exception("We don't support callbacks in fit for now")
+        if class_weight:
+            unsupport_exp("class_weight")
+        if sample_weight:
+            unsupport_exp("sample_weight")
+        if initial_epoch != 0:
+            unsupport_exp("initial_epoch")
+        if shuffle != True:
+            unsupport_exp("shuffle")
+        if validation_split != 0.:
+            unsupport_exp("validation_split")
 
         sc = get_spark_context()
         bopt = boptimizer.Optimizer(
@@ -112,11 +160,5 @@ class KerasModelWrapper():
 def with_bigdl_backend(kmodel):
         # TODO: maybe we don't need batch_size, verbose and sample_weight
     bcommon.init_engine()
-    # bmodel = ModelLoader.load_def_from_kmodel(kmodel)
-    # kmodel.__old_fit = kmodel.fit
-    # kmodel.fit = bfit
-    # kmodel.predict = bpredict
-    # kmodel.evaluate = bevaluate
-    # kmodel.get_weights = bmodel.get_weights
     return KerasModelWrapper(kmodel)
 
