04e4a9d6a36b07a5e15044255c6980a129ac6f1f
Yang Wang <tiandiwoxin@icloud.com>
2017-12-14 12:47:29 +0800

    set random seed on loaders spec (#2029)
    * set random seed on loaders spec
    
    * meet code review

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/utils/tf/TensorflowSpecHelper.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/utils/tf/TensorflowSpecHelper.scala
@@ -24,7 +24,7 @@ import com.intel.analytics.bigdl.nn.Module
 import com.intel.analytics.bigdl.nn.abstractnn.Activity
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.{NumericWildCard, TensorNumeric}
-import com.intel.analytics.bigdl.utils.{BigDLSpecHelper, FileWriter, T}
+import com.intel.analytics.bigdl.utils.{BigDLSpecHelper, FileWriter, RandomGenerator, T}
 import com.intel.analytics.bigdl.utils.tf.Tensorflow.const
 import org.tensorflow.framework.{GraphDef, NodeDef}
 
