f1ed53787208d8d0edfc114b7e26387a35492191
Yao <psyyz10@gmail.com>
2017-12-15 12:12:08 +0800

    meet code review

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/nn/LocallyConnected2DSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/nn/LocallyConnected2DSpec.scala
@@ -22,23 +22,6 @@ import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.utils.RandomGenerator
 
 
-/*
-  * Copyright 2016 The BigDL Authors.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
- */
-
-
 class LocallyConnected2DSpec extends KerasBaseSpec {
 
   "LocallyConnected1D NCHW Float" should "be ok" in {
@@ -129,28 +112,5 @@ class LocallyConnected2DSpec extends KerasBaseSpec {
 
     checkOutputAndGrad(locallyConnected1d, kerasCode, wc)
 
-  }
-
-  "a" should "a" in {
-    import com.intel.analytics.bigdl.numeric.NumericFloat
-    val seed = 100
-    val nInputPlane = 2
-    val inputWidth = 5
-    val inputHeight = 6
-    val nOutputPlane = 3
-    val kernelW = 2
-    val kernelH = 2
-    val layer =
-      LocallyConnected2D(nInputPlane, inputWidth, inputHeight, nOutputPlane, kernelW, kernelH)
-    RandomGenerator.RNG.setSeed(seed)
-
-    val input = Tensor(2, 6, 5).rand()
-    val gradOutput = Tensor(3, 5, 4).rand()
-
-    val output = layer.updateOutput(input)
-    println(output)
-
-    val gradInput = layer.updateGradInput(input, gradOutput)
-    println(gradInput)
   }
 }
