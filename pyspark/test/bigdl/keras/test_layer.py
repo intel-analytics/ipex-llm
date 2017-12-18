fix softmax

pyspark/test/bigdl/keras/test_layer.py
@@ -50,7 +50,7 @@ class TestLayer(BigDLTestCase):
         self.modelTestSingleLayer(input_data, layer)
 
     def test_softmax(self):
-        input_data = np.random.random_sample([5, 6])
+        input_data = np.random.random_sample([2, 3, 5])
         layer = Activation('softmax')
         self.modelTestSingleLayer(input_data, layer)
 