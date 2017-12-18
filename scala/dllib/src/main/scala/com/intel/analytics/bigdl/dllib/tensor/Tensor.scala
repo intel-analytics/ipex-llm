66c3c04d6a449736c3200d10d1571353d54112a4
Xin Qiu <qiuxin2012@users.noreply.github.com>
2017-12-18 09:35:05 +0800

    Lookup table for multivalue (#2016)
    Layer LookupTableSparse

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/tensor/Tensor.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/tensor/Tensor.scala
@@ -25,6 +25,7 @@ import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.utils.{File, Table}
 import org.apache.spark.mllib.linalg.{DenseMatrix, DenseVector, Matrix, Vector}
 
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.reflect.ClassTag
 
@@ -583,6 +584,15 @@ trait Tensor[T] extends Serializable with TensorMath[T] with Activity {
   }
 
   def view(sizes: Array[Int]): Tensor[T]
+
+  /**
+   * Count the number of non-zero elements in first dimension.
+   * For SparseTensor only.
+   * @return an array number of non-zero elements in first dimension.
+   */
+  def numNonZeroByRow(): Array[Int] = {
+    throw new UnsupportedOperationException("countNonZero for sparse tensor only")
+  }
 
   /**
  *
@@ -1296,5 +1306,58 @@ object Tensor {
         tensors: Seq[Tensor[T]],
         res: Tensor[T])(implicit ev: TensorNumeric[T]): Tensor[T] = {
     SparseTensor.concat(dim, tensors, res)
+  }
+
+  /**
+   * Find the distinct value and its indices in a 1D tensor.
+   * @param tensor a 1D tensor
+   * @param distinctBuffer a buffer for its distinct values.
+   * @param indicesBuffer a buffer for its indcies.
+   * @return (distinctValues, indices)
+   */
+  def unique[T: ClassTag](
+        tensor: Tensor[T],
+        distinctBuffer: Tensor[T] = null,
+        indicesBuffer: Tensor[Int] = null
+        )(implicit ev: TensorNumeric[T]): (Tensor[T], Tensor[Int]) = {
+    require(tensor.isContiguous(), "unique only support contiguous tensor")
+    require(tensor.dim() == 1, "unique only support 1D tensor")
+    val array = tensor.storage().array()
+    val arrayOffset = tensor.storageOffset() - 1
+
+    val distinctTensor = if (null != distinctBuffer) {
+      distinctBuffer.resizeAs(tensor)
+      distinctBuffer
+    } else {
+      Tensor().resizeAs(tensor)
+    }
+    val tensorIndices = if (null != indicesBuffer) {
+      indicesBuffer.resizeAs(tensor)
+      indicesBuffer
+    } else {
+      Tensor[Int]().resizeAs(tensor)
+    }
+
+    val distinctValues = distinctTensor.storage().array()
+    val distinctValuesOffset = distinctTensor.storageOffset() - 1
+    val indicesArray = tensorIndices.storage().array()
+    val indicesOffset = tensorIndices.storageOffset() - 1
+    val seen = mutable.HashMap[T, Int]()
+    var i = 0
+    var nonZero = 0
+    while (i < tensor.nElement()) {
+      val x = array(i + arrayOffset)
+      if (!seen.contains(x)) {
+        distinctValues(nonZero + distinctValuesOffset) = x
+        seen.put(x, nonZero)
+        nonZero += 1
+      }
+      indicesArray(i + indicesOffset) = seen(x)
+      i += 1
+    }
+    // Resize distinctTensor to number of non-zero elements.
+    distinctTensor.resize(nonZero)
+
+    (distinctTensor, tensorIndices)
   }
 }
