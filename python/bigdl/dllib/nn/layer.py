219bc46e02cb9c29c677acd6e3d3e259a7a94551
Kai Huang <huangkaivision@gmail.com>
2017-12-18 19:34:38 +0800

    Add keras imdb example (#2039)
    * add imdb example; refactor load keras API
    
    * update yml
    
    * meet review

--- python/bigdl/dllib/nn/layer.py
+++ python/bigdl/dllib/nn/layer.py
@@ -744,25 +744,33 @@ class Model(Container):
         return Layer.of(jmodel)
 
     @staticmethod
-    def load_keras(def_path, weights_path=None, by_name=False):
+    def load_keras(json_path=None, hdf5_path=None, by_name=False):
         """
         Load a pre-trained Keras model.
 
-        :param def_path: The json path containing the keras model definition.
-        :param weights_path: The HDF5 path containing the pre-trained keras model weights.
-        :return: A pre-trained model.
+        :param json_path: The json path containing the keras model definition.
+        :param hdf5_path: The HDF5 path containing the pre-trained keras model weights with or without the model architecture.
+        :return: A bigdl model.
         """
         import os
         try:
             import tensorflow
         except ImportError:
             os.environ['KERAS_BACKEND'] = "theano"
-            from theano import ifelse
+            try:
+                from theano import ifelse
+            except ImportError:
+                raise Exception("No backend is found for Keras. "
+                                "Please install either tensorflow or theano.")
         from bigdl.keras.converter import DefinitionLoader, WeightLoader
-        if weights_path:
-            return WeightLoader.load_weights_from_json_hdf5(def_path, weights_path, by_name=by_name)
-        else:
-            return DefinitionLoader.from_json_path(def_path)
+        if json_path and not hdf5_path:
+            return DefinitionLoader.from_json_path(json_path)
+        elif json_path and hdf5_path:
+            return WeightLoader.load_weights_from_json_hdf5(json_path, hdf5_path, by_name=by_name)
+        elif hdf5_path and not json_path:
+            kmodel, bmodel = DefinitionLoader.from_hdf5_path(hdf5_path)
+            WeightLoader.load_weights_from_kmodel(bmodel, kmodel)
+            return bmodel
 
     @staticmethod
     def load_caffe(model, defPath, modelPath, match_all=True, bigdl_type="float"):
