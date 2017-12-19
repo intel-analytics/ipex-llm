e5247f78da4099430787f472e021bf6847d3abd3
Guoqiong Song <guoqiong.song@intel.com>
2017-12-18 22:53:59 -0800

    serilizer (#2056)

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/keras/LocallyConnected1DSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/keras/LocallyConnected1DSpec.scala
@@ -1,4 +1,3 @@
-// scalastyle:off
 /*
  * Copyright 2016 The BigDL Authors.
  *
@@ -17,25 +16,8 @@
 
 package com.intel.analytics.bigdl.keras
 
-import com.intel.analytics.bigdl.nn.{HardSigmoid, LocallyConnected1D, Module}
+import com.intel.analytics.bigdl.nn.LocallyConnected1D
 import com.intel.analytics.bigdl.tensor.Tensor
-
-
-/*
-  * Copyright 2016 The BigDL Authors.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  *     http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
- */
 
 
 class LocallyConnected1DSpec extends KerasBaseSpec {
@@ -118,23 +100,6 @@ class LocallyConnected1DSpec extends KerasBaseSpec {
 
     output.size() should be(Array(d1l, d2l))
     _output.storage().map(x => x.toInt).toArray should be(output.storage().map(x => x.toInt).toArray)
-  }
-
-
-  "LocallyConnected1D serializer" should "be ok" in {
-    val module = LocallyConnected1D[Float](6, 2, outputFrameSize = 2, kernelW = 3, strideW = 1)
-
-    val input = Tensor[Float](6, 2).randn()
-
-    val res1 = module.forward(input).clone()
-    val tmpFile = java.io.File.createTempFile("module", ".bigdl")
-    module.saveModule(tmpFile.getAbsolutePath, null, true)
-    val loaded = Module.loadModule[Float](tmpFile.getAbsolutePath)
-    val res2 = loaded.forward(input)
-    res1 should be(res2)
-    if (tmpFile.exists()) {
-      tmpFile.delete()
-    }
   }
 
 }
