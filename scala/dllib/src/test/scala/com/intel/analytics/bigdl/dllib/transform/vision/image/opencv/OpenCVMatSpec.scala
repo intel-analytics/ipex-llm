14a660fd52018bad6ea2ea2f0ea933ec56f2cc5e
Xianyan <SeaOfOcean@users.noreply.github.com>
2017-12-22 19:52:52 +0800

    Add transformer doc and some fix (#2092)

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/transform/vision/image/opencv/OpenCVMatSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/transform/vision/image/opencv/OpenCVMatSpec.scala
@@ -22,13 +22,18 @@ import com.intel.analytics.bigdl.opencv.OpenCV
 import com.intel.analytics.bigdl.transform.vision.image.util.BoundingBox
 import com.intel.analytics.bigdl.utils.Engine
 import org.apache.commons.io.FileUtils
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext
+import org.opencv.core.CvType
 import org.opencv.imgcodecs.Imgcodecs
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 class OpenCVMatSpec extends FlatSpec with Matchers with BeforeAndAfter {
   val resource = getClass().getClassLoader().getResource("pascal/000025.jpg")
 
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  Logger.getLogger("akka").setLevel(Level.ERROR)
+  Logger.getLogger("breeze").setLevel(Level.ERROR)
   "toFloatsPixels" should "work properly" in {
     val img = OpenCVMat.read(resource.getFile)
     val floats = new Array[Float](img.height() * img.width() * img.channels())
@@ -81,6 +86,16 @@ class OpenCVMatSpec extends FlatSpec with Matchers with BeforeAndAfter {
     bytes1._1 should equal(bytes2._1)
   }
 
+  "imencode with float type" should "work not affect pixels" in {
+    val img = OpenCVMat.read(resource.getFile)
+    OpenCVMat.toFloatPixels(img)
+    val bytes = OpenCVMat.imencode(img)
+    val mat = OpenCVMat.fromImageBytes(bytes)
+    val bytes1 = OpenCVMat.toBytePixels(img)
+    val bytes2 = OpenCVMat.toBytePixels(mat)
+    bytes1._1 should equal(bytes2._1)
+  }
+
 
   var sc: SparkContext = null
   before {
@@ -95,10 +110,26 @@ class OpenCVMatSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
   "serialize" should "work properly" in {
     val img = OpenCVMat.read(resource.getFile)
+    val bytes = OpenCVMat.toBytePixels(img)
     val shape = img.shape()
     val rdd = sc.parallelize(Array(img))
     val collect = rdd.collect()
+    collect(0).`type`() should be (CvType.CV_8UC3)
+    val bytes2 = OpenCVMat.toBytePixels(collect(0))
     collect(0).shape() should be(shape)
+    bytes._1 should equal (bytes2._1)
+  }
+
+  "serialize float mat" should "work properly" in {
+    val img = OpenCVMat.read(resource.getFile)
+    val floats = OpenCVMat.toFloatPixels(img)
+    val shape = img.shape()
+    val rdd = sc.parallelize(Array(img))
+    val collect = rdd.collect()
+    collect(0).`type`() should be (CvType.CV_32FC3)
+    val floats2 = OpenCVMat.toFloatPixels(collect(0))
+    collect(0).shape() should be(shape)
+    floats._1 should equal (floats2._1)
   }
 
   "release" should "work properly" in {
@@ -106,6 +137,14 @@ class OpenCVMatSpec extends FlatSpec with Matchers with BeforeAndAfter {
     img.release()
     img.isReleased should be (true)
     img.shape() should be (0, 0, 3)
+  }
+
+  "empty serialize" should "work properly" in {
+    OpenCV.isOpenCVLoaded
+    val img = new OpenCVMat()
+    val rdd = sc.parallelize(Array(img))
+    val out = rdd.collect()
+    OpenCVMat.toBytePixels(out(0))._1.length should be (0)
   }
 
   "drawBoundingBox" should "work properly" in {
