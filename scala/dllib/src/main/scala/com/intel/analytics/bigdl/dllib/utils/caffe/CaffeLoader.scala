55173c3cf5065af3cc8893d9aec6eb6465d826a3
Jerry Wu <wzhongyuan@gmail.com>
2017-12-27 15:07:05 +0800

    fix one node filter issue (#2106)

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/utils/caffe/CaffeLoader.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/utils/caffe/CaffeLoader.scala
@@ -283,7 +283,7 @@ class CaffeLoader[T: ClassTag](prototxtPath: String, modelPath: String,
   : (Module[T], ParallelCriterion[T]) = {
     loadCaffe(prototxtPath, modelPath)
     registerCustomizedConverter()
-    val layers = createLayers()
+    val layers = createLayers(outputNames)
     val inputs = layers.filter(layer => layer.prevNodes.isEmpty).toArray
     val outputs = layers.filter(layer => layer.nextNodes.isEmpty ||
       outputNames.contains(layer.element.getName())).toArray
@@ -338,7 +338,7 @@ class CaffeLoader[T: ClassTag](prototxtPath: String, modelPath: String,
   }
 
   // create directed graph based on the module relationships
-  private def createLayers() : ArrayBuffer[ModuleNode[T]] = {
+  private def createLayers(outputNames: Array[String]) : ArrayBuffer[ModuleNode[T]] = {
     val layers = ArrayBuffer[ModuleNode[T]]()
     val layersMap = new mutable.HashMap[String, ModuleNode[T]]()
     val top2LayerMap = new mutable.HashMap[String, String]()
@@ -446,7 +446,8 @@ class CaffeLoader[T: ClassTag](prototxtPath: String, modelPath: String,
         }
       })
     })
-    layers.filter(layer => !(layer.prevNodes.isEmpty && layer.nextNodes.isEmpty))
+    layers.filter(layer => !(layer.prevNodes.isEmpty && layer.nextNodes.isEmpty)
+    || outputNames.contains(layer.element.getName))
   }
 
   private def convertCaffeLayer(layer : GeneratedMessage): Seq[ModuleNode[T]] = {
