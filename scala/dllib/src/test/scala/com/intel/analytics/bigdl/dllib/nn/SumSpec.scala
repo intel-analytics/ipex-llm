eee02bbb836f42650635bcf550ee0238cc09138d
Yang Wang <tiandiwoxin@icloud.com>
2018-01-05 15:55:23 +0800

    Wrap layers that requires two ClassTags into TensorModuleWrapper and move to tf package (#2122)
    * add TensorModuleToOperation
    
    * fix tests
    
    * address comments
    
    * fix test
    
    * rebase master
    
    * fix style

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/nn/SumSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/nn/SumSpec.scala
@@ -27,7 +27,7 @@ class SumSpec extends FlatSpec with Matchers {
       T(3.0f, 4.0f)
     ))
 
-    val layer = Sum[Float, Float](dimension = 2)
+    val layer = Sum[Float](dimension = 2)
 
     val expect = Tensor[Float](T(3.0f, 7.0f))
 
@@ -40,7 +40,7 @@ class SumSpec extends FlatSpec with Matchers {
       T(3.0f, 4.0f)
     ))
 
-    val layer = Sum[Float, Float](dimension = 2, squeeze = false)
+    val layer = Sum[Float](dimension = 2, squeeze = false)
 
     val expect = Tensor[Float](T(T(3.0f), T(7.0f)))
 
@@ -49,7 +49,7 @@ class SumSpec extends FlatSpec with Matchers {
 
   "sum" should "be correct when squeeze on vector" in {
     val vector = Tensor[Int](T(1, 2, 3))
-    val sum = Sum[Float, Int](dimension = 1, squeeze = true)
+    val sum = Sum[Int](dimension = 1, squeeze = true)
     sum.forward(vector) should be(Tensor.scalar(6))
   }
 }
