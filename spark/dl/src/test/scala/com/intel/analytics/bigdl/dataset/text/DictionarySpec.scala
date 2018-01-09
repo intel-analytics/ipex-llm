12846d27a50c953789db3b6062b88f1e3c6dd55b
Dongjie Shi <glorysdj@gmail.com>
2018-01-09 16:29:57 +0800

    fix ut failure of DictionarySpec

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/dataset/text/DictionarySpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/dataset/text/DictionarySpec.scala
@@ -52,7 +52,7 @@ class DictionarySpec extends FlatSpec with Matchers with BeforeAndAfter {
 
     val sentences = Array(sentence1, sentence2, sentence3)
 
-    new PrintWriter(tmpFile) {
+    new PrintWriter(tmpFile, "UTF-8") {
       write(sentences.mkString("\n")); close
     }
 
@@ -82,11 +82,11 @@ class DictionarySpec extends FlatSpec with Matchers with BeforeAndAfter {
 
     val sentences = Array(sentence1, sentence2, sentence3)
 
-    new PrintWriter(tmpFile) {
+    new PrintWriter(tmpFile, "UTF-8") {
       write(sentences.mkString("\n")); close
     }
 
-    val logData = Source.fromFile(tmpFile).getLines().toArray
+    val logData = Source.fromFile(tmpFile, "UTF-8").getLines().toArray
     val tokens = DataSet.array(logData
       .filter(!_.isEmpty)).transform(SentenceTokenizer())
     val output = tokens.toLocal().data(train = false)
