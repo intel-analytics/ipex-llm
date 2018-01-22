42d3b9b6cb37f066a5ba9190e5e0991da660f6e7
wzhongyuan <wzhongyuan@gmail.com>
2018-01-22 11:04:14 +0800

    refine hierarchy

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/utils/serializer/converters/DataReaderWriter.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/utils/serializer/converters/DataReaderWriter.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intel.analytics.bigdl.utils.serializer
+package com.intel.analytics.bigdl.utils.serializer.converters
 
 import java.io.{DataInputStream, DataOutputStream}
 
