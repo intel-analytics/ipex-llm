b6abe462b11db901833343ec407b3b8df9734011
QuincyWu <412363303@qq.com>
2018-01-25 13:35:14 +0800

    add GaussianNoise

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/keras/nn/GaussianNoiseSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/keras/nn/GaussianNoiseSpec.scala
@@ -17,26 +17,27 @@
 package com.intel.analytics.bigdl.keras.nn
 
 import com.intel.analytics.bigdl.keras.KerasBaseSpec
-import com.intel.analytics.bigdl.nn.keras.GaussianDropout
+import com.intel.analytics.bigdl.nn.keras.GaussianNoise
 import com.intel.analytics.bigdl.nn.keras.{Sequential => KSequential}
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.utils.Shape
 
-class GaussianDropoutSpec extends KerasBaseSpec {
+class GaussianNoiseSpec extends KerasBaseSpec {
 
-  "GaussianDropout" should "generate the correct outputShape" in {
+  "GaussianNoise" should "generate the correct outputShape" in {
     val seq = KSequential[Float]()
-    val gaussiandropout = GaussianDropout[Float](0.5, inputShape = Shape(3))
-    seq.add(gaussiandropout)
-    gaussiandropout.getOutputShape().toSingle().toArray should be(Array(-1, 3))
+    val gaussiannoise = GaussianNoise[Float](0.5, inputShape = Shape(3))
+    seq.add(gaussiannoise)
+    gaussiannoise.getOutputShape().toSingle().toArray should be(Array(-1, 3))
   }
 
-  "GaussianDropout forward and backward" should "work properly" in {
+  
+  "GaussianNoise forward and backward" should "work properly" in {
     val seq = KSequential[Float]()
     val input = Tensor[Float](Array(2, 28, 28, 1)).rand()
-    val gaussiandropout = GaussianDropout[Float](0.6, inputShape = Shape(3))
-    seq.add(gaussiandropout)
-    val output = gaussiandropout.forward(input)
-    val gradInput = gaussiandropout.backward(input, output)
+    val gaussiannoise = GaussianNoise[Float](0.6, inputShape = Shape(3))
+    seq.add(gaussiannoise)
+    val output = gaussiannoise.forward(input)
+    val gradInput = gaussiannoise.backward(input, output)
   }
 }
