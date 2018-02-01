9212c8c51abd274a7a9a179e96b0688e111e63df
dding3 <ding.ding@intel.com>
2018-01-31 19:30:46 -0800

    Support gradual increase learning rate and SequentialSchedule in learningratescheduler (#2093)
    * support SequentialSchedule and WarmupSchedule

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/models/inception/Options.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/models/inception/Options.scala
@@ -34,7 +34,12 @@ object Options {
     maxIteration: Int = 62000,
     weightDecay: Double = 0.0001,
     checkpointIteration: Int = 620,
-    graphModel: Boolean = false
+    graphModel: Boolean = false,
+    maxLr: Option[Double] = None,
+    warmupEpoch: Option[Int] = None,
+    gradientL2NormThreshold: Option[Double] = None,
+    gradientMin: Option[Double] = None,
+    gradientMax: Option[Double] = None
   )
 
   val trainParser = new OptionParser[TrainParams]("BigDL Inception Example") {
@@ -79,6 +84,21 @@ object Options {
     opt[Unit]('g', "graphModel")
       .text("use graph model")
       .action((x, c) => c.copy(graphModel = true))
+    opt[Double]("maxLr")
+      .text("max Lr after warm up")
+      .action((x, c) => c.copy(maxLr = Some(x)))
+    opt[Int]("warmupEpoch")
+      .text("warm up epoch numbers")
+      .action((x, c) => c.copy(warmupEpoch = Some(x)))
+    opt[Double]("gradientL2NormThreshold")
+      .text("gradient L2-Norm threshold")
+      .action((x, c) => c.copy(gradientL2NormThreshold = Some(x)))
+    opt[Double]("gradientMax")
+      .text("max gradient clipping by")
+      .action((x, c) => c.copy(gradientMax = Some(x)))
+    opt[Double]("gradientMin")
+      .text("min gradient clipping by")
+      .action((x, c) => c.copy(gradientMin = Some(x)))
   }
 
   case class TestParams(
