978802edf54d6292a202dea9b62677aae301a826
seaofocean <xianyan.jia@intel.com>
2018-02-04 12:12:29 +0800

    update

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/dataset/DataSet.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/dataset/DataSet.scala
@@ -581,7 +581,8 @@ object DataSet {
         imf(ImageFeature.originalSize) = (height, width, 3)
         imf
       }).filter(_[Tensor[Float]](ImageFeature.label).valueAt(1) <= classNum)
-      ImageFrame.rdd(rawData.coalesce(num, true))
+//      ImageFrame.rdd(rawData.coalesce(num, true))
+      ImageFrame.rdd(rawData)
     }
 
     private[bigdl] def findFiles(path: Path): Array[LocalSeqFilePath] = {
