cf9e8f03c6231e6985517011dcbd7c6ecba63a2c
seaofocean <xianyan.jia@intel.com>
2018-02-08 15:01:21 +0800

    fix ut

--- pyspark/test/bigdl/test_simple_integration.py
+++ pyspark/test/bigdl/test_simple_integration.py
@@ -629,15 +629,15 @@ class TestSimple():
         image_frame = DistributedImageFrame(self.sc.parallelize(images),
                                             self.sc.parallelize(labels))
 
-        transformer = Pipeline([Resize(256, 256), CenterCrop(224, 224),
+        transformer = Pipeline([BytesToMat(), Resize(256, 256), CenterCrop(224, 224),
                                 ChannelNormalize(0.485, 0.456, 0.406, 0.229, 0.224, 0.225),
                                 MatToTensor(), ImageFrameToSample(target_keys=['label'])])
         data_set = DataSet.image_frame(image_frame).transform(transformer)
 
         model = Sequential()
-        model.add(SpatialConvolution(3, 6, 5, 5))
-        model.add(View([6 * 220 * 220]))
-        model.add(Linear(6 * 220 * 220, 20))
+        model.add(SpatialConvolution(3, 1, 5, 5))
+        model.add(View([1 * 220 * 220]))
+        model.add(Linear(1 * 220 * 220, 20))
         model.add(LogSoftMax())
         optim_method = SGD(learningrate=0.01)
         optimizer = Optimizer.create(
@@ -656,7 +656,7 @@ class TestSimple():
 
         trained_model = optimizer.optimize()
 
-        predict_result = trained_model.predict_image(image_frame)
+        predict_result = trained_model.predict_image(image_frame.transform(transformer))
         assert_allclose(predict_result.get_predict().count(), 8)
 
 
