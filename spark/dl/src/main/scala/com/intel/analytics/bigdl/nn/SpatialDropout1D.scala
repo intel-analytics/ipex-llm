99c6410243b8b1d335f4c7106c3af79e65f09ce2
Quincy2014 <412363303@qq.com>
2018-02-08 16:42:18 +0800

    Keras-like API Advanced Activations, dropout and noise layers (#2222)
    * Keras API for ELU
    
    * fix style check error
    
    * fix weightConverter
    
    * add one more unit test for ELU
    
    * remove blank line
    
    * Keras API for LeakyReLU
    
    * remove useless empty lines in LeakyReLU
    
    * add GaussianDropout
    
    * add GaussianNoise
    
    * remove UID and unnecessary import
    
    * fix two Gaussian unit test
    
    * add layer Masking
    
    * add layer SpatialDropout1D
    
    * change 3D to 4D
    
    * Revert "change 3D to 4D"
    
    This reverts commit 9efdb0a630f342596475f83feea8fa370f8caf05.
    
    * change unit test from 4D to 3D
    
    * add layer SpatialDropout2D
    
    * add layer PReLU. Unit test success without weight
    
    * add 3D unit test for PReLU
    
    * add layer ParametricSoftPlus. Unit test success without weight
    
    * add layer SpatialDropout3D
    
    * add layer ThresholdedReLU
    
    * fix the above problems
    
    * fix problems
    
    * add format lowercase to support both uppercase and lowercase
    
    * fix format problem
    
    * SReLU
    
    * add documentation and serializer
    
    * remove a blank in documentation and change inputshape from var to val
    
    * delete four files
    
    * update
    
    * modify
    
    * modify problem
    
    * modify
    
    * update
    
    * modify style

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/SpatialDropout1D.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/SpatialDropout1D.scala
@@ -15,7 +15,7 @@
  */
 package com.intel.analytics.bigdl.nn
 
-import com.intel.analytics.bigdl.nn.abstractnn.{DataFormat, TensorModule}
+import com.intel.analytics.bigdl.nn.abstractnn.{DataFormat, IdentityOutputShape, TensorModule}
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 
@@ -37,7 +37,7 @@ import scala.reflect.ClassTag
 @SerialVersionUID(- 4636332259181125718L)
 class SpatialDropout1D[T: ClassTag](
   val initP: Double = 0.5)(
-  implicit ev: TensorNumeric[T]) extends TensorModule[T] {
+  implicit ev: TensorNumeric[T]) extends TensorModule[T] with IdentityOutputShape {
   var p = initP
   var noise = Tensor[T]()
 
