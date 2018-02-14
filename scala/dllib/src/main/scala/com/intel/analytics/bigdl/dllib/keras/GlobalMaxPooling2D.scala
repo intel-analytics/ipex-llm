2cd2fb1b00ded5d941ae74cc7764a29b139af033
Kai Huang <huangkaivision@gmail.com>
2018-02-14 15:43:30 +0800

    Keras-like API Python Wrapper for layers + lenet example (#2296)
    * python lenet
    
    * docs
    
    * style
    
    * wrap getoutputshape
    
    * update input output shape
    
    * update
    
    * python layers
    
    * clean
    
    * fix ut
    
    * more layers
    
    * fix ut
    
    * more layers
    
    * fix ut
    
    * more layers
    
    * wrapper
    
    * remove
    
    * style
    
    * style and srelu
    
    * style
    
    * refine lenet
    
    * update
    
    * add ut for input output shape
    
    * fix float
    
    * refine lenet
    
    * update
    
    * meet review
    
    * clean
    
    * todo
    
    * update
    
    * style
    
    * readme
    
    * style
    
    * style
    
    * fix
    
    * style

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/keras/GlobalMaxPooling2D.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/keras/GlobalMaxPooling2D.scala
@@ -25,7 +25,7 @@ import com.intel.analytics.bigdl.utils.Shape
 import scala.reflect.ClassTag
 
 /**
- * Global max pooling operation for spatial data.
+ * Applies global max pooling operation for spatial data.
  * The input of this layer should be 4D.
  *
  * When you use this layer as the first layer of a model, you need to provide the argument
