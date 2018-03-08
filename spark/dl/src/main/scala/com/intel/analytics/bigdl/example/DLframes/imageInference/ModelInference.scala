329a1ce96bbb3679dc27a14e2c6392d25c4bfe5e
Guoqiong Song (Intel) <guoqiong@guoqiong-mac01.local>
2018-03-08 15:00:29 -0800

    need test on cluster

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/example/DLframes/imageInference/ModelInference.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/example/DLframes/imageInference/ModelInference.scala
@@ -1,33 +1,53 @@
 package com.intel.analytics.bigdl.example.DLframes.imageInference
 
-import java.net.URL
-import java.nio.file.Paths
-
-import com.intel.analytics.bigdl.dataset.Sample
-import com.intel.analytics.bigdl.dataset.image._
 import com.intel.analytics.bigdl.dlframes.{DLClassifierModel, DLModel}
-import com.intel.analytics.bigdl.example.DLframes.imageInference.Utils.LocalParams
-import com.intel.analytics.bigdl.example.imageclassification.MlUtils._
-import com.intel.analytics.bigdl.example.imageclassification.RowToByteRecords
-import com.intel.analytics.bigdl.example.loadmodel.InceptionPreprocessor.imageSize
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import scopt.OptionParser
+import com.intel.analytics.bigdl.dataset.Sample
 import com.intel.analytics.bigdl.nn.Module
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric.NumericFloat
-import com.intel.analytics.bigdl.transform.vision.image._
 import com.intel.analytics.bigdl.transform.vision.image.augmentation.{CenterCrop, ChannelNormalize, Resize}
+import com.intel.analytics.bigdl.transform.vision.image.{ImageFeature, ImageFrame, ImageFrameToSample, MatToTensor}
 import com.intel.analytics.bigdl.utils.Engine
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.functions.{col, udf}
-import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
-import scopt.OptionParser
+import org.apache.spark.sql.SQLContext
 
-import scala.io.Source
+object ModelInference {
+
+  def main(args: Array[String]): Unit = {
+
+    val defaultParams = Utils.LocalParams()
+    Utils.parser.parse(args, defaultParams).map { params =>
+
+      val conf = Engine.createSparkConf().setAppName("ModelInference")
+        .setMaster("local[8]")
+      val spark = SparkSession.builder().config(conf).getOrCreate()
+      Engine.init
+
+      val imagesDF = Utils.loadImages(params.folder, params.batchSize, spark.sqlContext)
+
+      imagesDF.show(10)
+      imagesDF.printSchema()
+
+      val model = Module.loadCaffeModel[Float](params.caffeDefPath, params.modelPath)
+      val dlmodel: DLModel[Float] = new DLClassifierModel[Float](
+        model, Array(3, 224, 224))
+        .setBatchSize(params.batchSize)
+        .setFeaturesCol("features")
+        .setPredictionCol("prediction")
+
+      val tranDF = dlmodel.transform(imagesDF)
+
+      tranDF.select("imageName", "prediction").show(100, false)
+    }
+  }
+}
 
 object Utils {
 
-  //TODO delete local path
-  case class LocalParams(caffeDefPath: String = "/Users/guoqiong/intelWork/git/caffe/models/bvlc_googlenet/deploy.prototxt",
-                         modelPath: String = "/Users/guoqiong/intelWork/projects/dlFrames/model/caffe/bvlc_googlenet.caffemodel",
-                         folder: String = "/Users/guoqiong/intelWork/projects/dlFrames/data/ILSVRC2012_img_val_20/",
+  case class LocalParams(caffeDefPath: String = " ",
+                         modelPath: String = " ",
+                         folder: String = " ",
                          batchSize: Int = 16,
                          nEpochs: Int = 10
                         )
@@ -52,40 +72,7 @@ object Utils {
       .action((x, c) => c.copy(nEpochs = x))
   }
 
-  //TODO update with ImageTransfer
   def loadImages(path: String, partitionNum: Int, sqlContext: SQLContext): DataFrame = {
-
-    val paths = LocalImageFiles.readPaths(Paths.get(path), hasLabel = false)
-    val valRDD = sqlContext.sparkContext.parallelize(imagesLoad(paths, 256), partitionNum)
-
-    val transf = RowToByteRecords() ->
-      BytesToBGRImg() ->
-      BGRImgCropper(imageSize, imageSize) ->
-      BGRImgNormalizer(123, 117, 104, 1, 1, 1) ->
-      BGRImgToImageVector()
-    val imagesDF: DataFrame = transformDF(sqlContext.createDataFrame(valRDD), transf)
-
-    imagesDF
-  }
-
-  // Resnet
-  def loadImages2(path: String, partitionNum: Int, sqlContext: SQLContext): DataFrame = {
-
-    val paths = LocalImageFiles.readPaths(Paths.get(path), hasLabel = false)
-    val valRDD = sqlContext.sparkContext.parallelize(imagesLoad(paths, 256), partitionNum)
-
-    val transf = RowToByteRecords() ->
-      BytesToBGRImg() ->
-      new BGRImgCropper(imageSize, imageSize, cropperMethod = CropCenter) ->
-      BGRImgNormalizer(123, 117, 104, 1, 1, 1) ->
-      BGRImgToImageVector()
-    val imagesDF: DataFrame = transformDF(sqlContext.createDataFrame(valRDD), transf)
-
-    imagesDF
-  }
-
-
-  def loadImages3(path: String, partitionNum: Int, sqlContext: SQLContext): DataFrame = {
 
     val imageFrame: ImageFrame = ImageFrame.read(path, sqlContext.sparkContext)
     val transformer = Resize(256, 256) -> CenterCrop(224, 224) ->
@@ -100,48 +87,4 @@ object Utils {
     imageDF
   }
 
-}
-
-object ModelInference {
-
-  def main(args: Array[String]): Unit = {
-
-    val defaultParams = LocalParams()
-    Utils.parser.parse(args, defaultParams).map { params =>
-
-      val conf = Engine.createSparkConf().setAppName("ModelInference")
-        .setMaster("local[8]")
-      val spark = SparkSession.builder().config(conf).getOrCreate()
-      Engine.init
-
-      Logger.getLogger("org").setLevel(Level.ERROR)
-      spark.sparkContext.setLogLevel("ERROR")
-
-      val imagesDF = Utils.loadImages3(params.folder, params.batchSize, spark.sqlContext)
-
-      imagesDF.show(10)
-      imagesDF.printSchema()
-
-      val model = Module.loadCaffeModel[Float](params.caffeDefPath, params.modelPath)
-      val labelFile: URL = getClass().getResource("/imagenet_classname.txt")
-      val labelMap = Source.fromURL(labelFile).getLines().zipWithIndex.map(x => (x._2, x._1)).toMap
-      val toStringLabel = udf { predict: Int =>
-        labelMap(predict - 1)
-      }
-      //val model = Module.loadModule("/Users/guoqiong/intelWork/projects/dlFrames/model/bigdl/bigdl_inception-v1_imagenet_0.4.0.model")
-
-      val dlmodel: DLModel[Float] = new DLClassifierModel[Float](
-        model, Array(3, 224, 224))
-        .setBatchSize(params.batchSize)
-        .setFeaturesCol("features")
-        .setPredictionCol("prediction")
-
-      val tranDF = dlmodel.transform(imagesDF)
-
-      tranDF.select("imageName", "prediction")
-        .withColumn("top1Class", toStringLabel(col("prediction")))
-        .show(100, false)
-
-    }
-  }
 }
--- spark/dl/src/main/scala/com/intel/analytics/bigdl/example/DLframes/imageInference/README.md
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/example/DLframes/imageInference/README.md
@@ -3,7 +3,7 @@
    The current version of Deep Learning Frames provides a suite of tools around working with and processing images using deep learning. 
    Two exmaples demostrates how to use BigDL for transfer learning and applying deep learning models as scale.
 
-## Model Inference
+## Image Model Inference
    1. You can apply your own or known popular models to image data to make predictions or transform them into features.
             
             val imagesDF = loadImages(param.folder, param.batchSize, spark.sqlContext)
@@ -39,7 +39,7 @@
                 spark-submit \
                 --master local[physcial_core_number] \
                 --driver-memory 10g --executor-memory 20g \
-                --class com.intel.analytics.bigdl.example.DLFrames.ModelInference \
+                --class com.intel.analytics.bigdl.example.DLFrames.ImageModelInference \
                 ./dist/lib/bigdl-VERSION-jar-with-dependencies.jar \
                 --modelPath ./model/bvlc_googlenet.caffemodel \
                 --caffeDefPath ./model/deploy.prototxt \
@@ -56,82 +56,10 @@
                 --deploy-mode client \
                 --executor-cores 8 \
                 --num-executors 4 \
-                --class com.intel.analytics.bigdl.example.imageclassification.ImagePredictor \
+                --class com.intel.analytics.bigdl.example.imageclassification.ImageModelInference \
                 ./dist/lib/bigdl-VERSION-jar-with-dependencies.jar \
                 --modelPath ./model/bvlc_googlenet.caffemodel \
                 --folder ./predict \
                 --batchSizePerCore 16 \
                 --nEpochs 10
-        ```
-## Transfer Learning 
-   1. DLFrames provides utilities to perform transfer learning on images, which is one of the fastest (code and run-time-wise) ways to start using deep learning.             
-         
-             val imagesDF: DataFrame = loadImages(params.folder, params.batchSize, spark.sqlContext)
-       
-             val Array(validationDF, trainingDF) = imagesDF.randomSplit(Array(0.2, 0.8), seed = 1)
-       
-             val criterion = ClassNLLCriterion[Float]()
-       
-             val loaded: AbstractModule[Activity, Activity, Float] = Module.loadCaffeModel[Float](params.caffeDefPath, params.modelPath)
-             val model = Sequential[Float]().add(loaded)
-               .add(Linear[Float](1000, 2)).add(ReLU()).add(LogSoftMax())
-       
-             val dlc: DLClassifier[Float] = new DLClassifier[Float](model, criterion, Array(3, 224, 224))
-               .setBatchSize(4)
-               .setOptimMethod(new Adam())
-               .setLearningRate(1e-2)
-               .setLearningRateDecay(1e-5)
-               .setMaxEpoch(10)
-               .setFeaturesCol("features")
-               .setLabelCol("label")
-               .setPredictionCol("prediction")
-          
-             val dlModel: DLModel[Float] = dlc.fit(trainingDF)
-          
-             val predictions = dlModel.setBatchSize(2).transform(validationDF)
-       
-             predictions.show(5)
-         
-   2. You can run the full TransferLearning example by following steps.
-        
-        2.1 Prepare pre-trained model and defenition file as Model Inference.
-        
-        2.2 Prepare dataset
-        Put your image data for training and validation in the ./data folder. Alternatively, you may also use kaggle [Dogs vs. Cats](https://www.kaggle.com/c/dogs-vs-cats/data) train dataset to run the example. After you download the file (train.zip), run the follow commands to prepare the data.
-    
-            ```
-                bash
-                mkdir data
-                unzip -xvf train.tar -C ./data/
-            ```
-  
-        2.3 Run this example
-
-        Command to run the example in Spark local mode:
-        ```
-                spark-submit \
-                --master local[physcial_core_number] \
-                --driver-memory 10g --executor-memory 20g \
-                --class com.intel.analytics.bigdl.example.DLFrames.TransferLearning \
-                ./dist/lib/bigdl-VERSION-jar-with-dependencies.jar \
-                --modelPath ./model/bvlc_googlenet.caffemodel \
-                --caffeDefPath ./model/deploy.prototxt \
-                --batchSize 32 \
-                --folder ./data \
-                --nEpochs 10                
-        ```
-
-        Command to run the example in Spark yarn mode(TODO):
-        ```
-                spark-submit \
-                --master yarn \
-                --deploy-mode client \
-                --executor-cores 8 \
-                --num-executors 4 \
-                --class com.intel.analytics.bigdl.example.imageclassification.TransferLearning \
-                ./dist/lib/bigdl-VERSION-jar-with-dependencies.jar \
-                --modelPath ./model/bvlc_googlenet.caffemodel \
-                --folder ./data \
-                --batchSize 32 \
-                --nEpochs 10                
         ```
