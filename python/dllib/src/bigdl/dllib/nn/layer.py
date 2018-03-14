742c39f4f14e1055581cf4d628559d6ff6f0d4b1
Kai Huang <huangkaivision@gmail.com>
2018-03-14 20:11:19 +0800

    Keras-like API for training and evaluation (#2306)
    * update scala compile fit
    
    * update
    
    * python topology
    
    * refactor lenet
    
    * update
    
    * refactor fit
    
    * add python ut
    
    * ut for image dataset
    
    * fix ut
    
    * fix
    
    * update docs
    
    * update readme
    
    * fix

--- python/dllib/src/bigdl/dllib/nn/layer.py
+++ python/dllib/src/bigdl/dllib/nn/layer.py
@@ -60,7 +60,6 @@ class Node(JavaValue):
         callJavaFunc(self.value.removeNextEdges)
 
 
-
 class Layer(JavaValue):
     """
     Layer is the basic component of a neural network
@@ -365,7 +364,7 @@ class Layer(JavaValue):
         :return: An RDD represent the predict result.
         """
         result = callBigDlFunc(self.bigdl_type,
-                             "modelPredictRDD", self.value, data_rdd)
+                               "modelPredictRDD", self.value, data_rdd)
         return result.map(lambda data: data.to_ndarray())
 
     def predict_class_distributed(self, data_rdd):
