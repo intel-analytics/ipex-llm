3dc78840071ea7e299b2cff2b39a480f4f30f608
Kai Huang <huangkaivision@gmail.com>
2018-04-04 15:44:13 +0800

    Wrap Sequential and Model (#11)
    * extend container
    
    * input
    
    * python wrapper
    
    * fix topology
    
    * update
    
    * fix
    
    * trial
    
    * fix java version
    
    * remove
    
    * update
    
    * update pom

--- python/dllib/src/bigdl/dllib/keras/layers/layer.py
+++ python/dllib/src/bigdl/dllib/keras/layers/layer.py
@@ -21,11 +21,49 @@ if sys.version >= '3':
     unicode = str
 
 
-class ZooKerasLayer(KerasLayer):
+class ZooKerasCreator(JavaValue):
     def jvm_class_constructor(self):
         name = "createZooKeras" + self.__class__.__name__
         print("creating: " + name)
         return name
+
+
+class ZooKerasLayer(ZooKerasCreator, KerasLayer):
+    pass
+
+
+class Input(ZooKerasCreator, Node):
+    """
+    Used to instantiate an input node.
+
+    # Arguments
+    shape: A shape tuple, not including batch.
+    name: String to set the name of the input node. If not specified, its name will by default to be a generated string.
+
+    >>> input = Input(name="input1", shape=(3, 5))
+    creating: createZooKerasInput
+    """
+    def __init__(self, shape=None, name=None, bigdl_type="float"):
+        super(Input, self).__init__(None, bigdl_type,
+                                    name,
+                                    list(shape) if shape else None)
+
+
+class InputLayer(ZooKerasLayer):
+    """
+    Used as an entry point into a model.
+
+    # Arguments
+    input_shape: A shape tuple, not including batch.
+    name: String to set the name of the input layer. If not specified, its name will by default to be a generated string.
+
+    >>> inputlayer = InputLayer(input_shape=(3, 5), name="input1")
+    creating: createZooKerasInputLayer
+    """
+    def __init__(self, input_shape=None, **kwargs):
+        super(InputLayer, self).__init__(None,
+                                         list(input_shape) if input_shape else None,
+                                         **kwargs)
 
 
 class Dense(ZooKerasLayer):
