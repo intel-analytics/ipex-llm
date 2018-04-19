7a05111293c63f2d61c5ea89290e5e4ecf8737c0
weiguang huang <fdjoey@gmail.com>
2018-04-19 10:24:52 +0800

    Python layers separated into categories (#64)
    * python layers separated into categories
    
    * update python layers
    
    * update PythonZooKeras import
    
    * update test_integration import
    
    * update test_integration import
    
    * add flatten, reshape in pythonZooKeras
    
    * revert test_integration.py
    
    * update layers python doc style

--- python/dllib/src/bigdl/dllib/keras/layers/embeddings.py
+++ python/dllib/src/bigdl/dllib/keras/layers/embeddings.py
@@ -21,3 +21,35 @@ from ..engine.topology import ZooKerasLayer
 if sys.version >= '3':
     long = int
     unicode = str
+
+
+class Embedding(ZooKerasLayer):
+    """
+    Turn positive integers (indexes) into dense vectors of fixed size.
+    The input of this layer should be 2D.
+
+    This layer can only be used as the first layer in a model, you need to provide the argument
+    input_shape (a shape tuple, does not include the batch dimension).
+
+    # Arguments
+    input_dim: Size of the vocabulary. Int > 0.
+    output_dim: Dimension of the dense embedding. Int >= 0.
+    init: String representation of the initialization method for the weights of the layer.
+          Default is 'uniform'.
+    W_regularizer: An instance of [[Regularizer]], (eg. L1 or L2 regularization),
+                   applied to the embedding matrix. Default is None.
+    input_shape: A shape tuple, not including batch.
+    name: String to set the name of the layer. If not specified, its name will by default to be a generated string.
+
+    >>> embedding = Embedding(1000, 32, input_shape=(10, ), name="embedding1")
+    creating: createZooKerasEmbedding
+    """
+    def __init__(self, input_dim, output_dim, init="uniform", W_regularizer=None,
+                 input_shape=None, **kwargs):
+        super(Embedding, self).__init__(None,
+                                        input_dim,
+                                        output_dim,
+                                        init,
+                                        W_regularizer,
+                                        list(input_shape) if input_shape else None,
+                                        **kwargs)
--- python/dllib/src/bigdl/dllib/keras/layers/noise.py
+++ python/dllib/src/bigdl/dllib/keras/layers/noise.py
@@ -21,3 +21,52 @@ from ..engine.topology import ZooKerasLayer
 if sys.version >= '3':
     long = int
     unicode = str
+
+
+class GaussianNoise(ZooKerasLayer):
+    """
+    Apply additive zero-centered Gaussian noise.
+    This is useful to mitigate overfitting (you could see it as a form of random data augmentation).
+    Gaussian Noise is a natural choice as corruption process for real valued inputs.
+    As it is a regularization layer, it is only active at training time.
+
+    When you use this layer as the first layer of a model, you need to provide the argument
+    input_shape (a shape tuple, does not include the batch dimension).
+
+    # Arguments
+    sigma: Float, standard deviation of the noise distribution.
+    input_shape: A shape tuple, not including batch.
+    name: String to set the name of the layer. If not specified, its name will by default to be a generated string.
+
+    >>> gaussiannoise = GaussianNoise(0.45, input_shape=(3, 4, 5), name="gaussiannoise1")
+    creating: createZooKerasGaussianNoise
+    """
+    def __init__(self, sigma, input_shape=None, **kwargs):
+        super(GaussianNoise, self).__init__(None,
+                                            float(sigma),
+                                            list(input_shape) if input_shape else None,
+                                            **kwargs)
+
+
+class GaussianDropout(ZooKerasLayer):
+    """
+    Apply multiplicative 1-centered Gaussian noise.
+    As it is a regularization layer, it is only active at training time.
+
+    When you use this layer as the first layer of a model, you need to provide the argument
+    input_shape (a shape tuple, does not include the batch dimension).
+
+    # Arguments
+    p: Drop probability. Float between 0 and 1.
+       The multiplicative noise will have standard deviation 'sqrt(p/(1-p))'.
+    input_shape: A shape tuple, not including batch.
+    name: String to set the name of the layer. If not specified, its name will by default to be a generated string.
+
+    >>> gaussiandropout = GaussianDropout(0.45, input_shape=(4, 8))
+    creating: createZooKerasGaussianDropout
+    """
+    def __init__(self, p, input_shape=None, **kwargs):
+        super(GaussianDropout, self).__init__(None,
+                                              float(p),
+                                              list(input_shape) if input_shape else None,
+                                              **kwargs)
