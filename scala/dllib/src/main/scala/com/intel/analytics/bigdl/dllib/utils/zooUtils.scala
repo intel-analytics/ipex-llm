8a13d095b15ddaa626b93c750a59029b1ae7a7c5
dding3 <ding.ding@intel.com>
2018-04-23 20:55:54 -0700

    support object detection (#25)
    * add scala object detection api and examples

---
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/utils/zooUtils.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.zoo.common
+
+import java.io._
+
+import com.intel.analytics.bigdl.utils.File
+import org.apache.commons.io.filefilter.WildcardFileFilter
+
+import scala.collection.mutable.ArrayBuffer
+
+private[zoo] object Utils {
+
+  def listLocalFiles(path: String): Array[File] = {
+    val files = new ArrayBuffer[File]()
+    listFiles(path, files)
+    files.toArray
+  }
+
+  def listFiles(path: String, files: ArrayBuffer[File]): Unit = {
+    val file = new File(path)
+    if (file.isDirectory) {
+      file.listFiles().foreach(x => listFiles(x.getAbsolutePath, files))
+    } else if (file.isFile) {
+      files.append(file)
+    } else {
+      val filter = new WildcardFileFilter(file.getName)
+      file.getParentFile.listFiles(new FilenameFilter {
+        override def accept(dir: File, name: String): Boolean = filter.accept(dir, name)
+      }).foreach(x => listFiles(x.getAbsolutePath, files))
+    }
+  }
+
+  def saveBytes(bytes: Array[Byte], fileName: String, isOverwrite: Boolean = false) : Unit = {
+    File.saveBytes(bytes, fileName, isOverwrite)
+  }
+}
