21bda2bab03742c7b287e0883cdebed66be3dda1
Yuhao Yang <hhbyyh@gmail.com>
2018-04-29 22:05:11 -0700

    python constructor

--- python/dllib/src/bigdl/dllib/nnframes/nn_classifier.py
+++ python/dllib/src/bigdl/dllib/nnframes/nn_classifier.py
@@ -88,26 +88,58 @@ class NNEstimator(JavaEstimator, HasFeaturesCol, HasLabelCol, HasPredictionCol, 
     Double, Float and ImageSchema (refer to NNImageReader).
     """
 
-    def __init__(self,  model, criterion, feature_transformer, label_transformer, jvalue=None, bigdl_type="float"):
+    def __init__(self,  model, criterion, sample_transformer, jvalue=None, bigdl_type="float"):
         """
         create a NNEstimator with BigDL Model.
         :param model: BigDL Model to be trained.
         :param criterion: BigDL criterion.
-        :param feature_size: The size (Tensor dimensions) of the feature data. e.g. an image may be with
-                             width * height = 28 * 28, featureSize = Array(28, 28).
+        :param sample_transformer: Expert param. A transformer that transforms the (feature, label) tuple
+               to a BigDL Sample[T], where T is decided by the BigDL model.
+
+               Note that sampleTransformer should be able to handle the case that label = null.
+               During fit, NNEstimator will extract (feature, label) tuple from input DataFrame and use
+               sampleTransformer to transform the tuple into BigDL Sample to be ingested by the model.
+               If Label column is not available, (feature, null) will be sent to sampleTransformer.
+ 
+               The sampleTransformer will also be copied to the generated NNModel and applied to feature
+               column during transform, where (feature, null) will be passed to the sampleTransformer.
+               sampleTransformer should be a subClass of com.intel.analytics.bigdl.dataset.Transformer.
         :param label_size: The size (Tensor dimensions) of the feature data.
         :param jvalue: Java object create by Py4j
         :param bigdl_type: optional parameter. data type of model, "float"(default) or "double".
         """
         super(NNEstimator, self).__init__()
         self.value = jvalue if jvalue else callBigDlFunc(
-            bigdl_type, self.jvm_class_constructor(), model, criterion, feature_transformer, label_transformer)
+            bigdl_type, self.jvm_class_constructor(), model, criterion, sample_transformer)
         self.bigdl_type = bigdl_type
         self._java_obj = self.value
         self.maxEpoch = Param(self, "maxEpoch", "number of max Epoch")
         self.learningRate = Param(self, "learningRate", "learning rate")
         self._setDefault(maxEpoch=50, learningRate=1e-3, batchSize=1)
-        self.feature_transformer = feature_transformer
+        self.sample_transformer = sample_transformer
+
+    @classmethod
+    def withTensorTransformer(cls, model, criterion, feature_transformer, label_transformer, jvalue=None,
+                              bigdl_type="float"):
+        """
+        create a NNEstimator with BigDL Model.
+        :param model: BigDL Model to be trained.
+        :param criterion: BigDL criterion.
+        :param feature_transformer: A transformer that transforms the feature data to a Tensor[T].
+               featureTransformer should be a subClass of com.intel.analytics.bigdl.dataset.Transformer.
+               Some common transformers have been defined in package
+               pipeline.nnframes.nn_transformers. E.g. SeqToTensor is used
+               to transform Array[_] to Tensor, and NumToTensor transform a number to a Tensor. Multiple
+               Transformer can be combined as a ChainedTransformer.
+               E.g. For a feature column that contains 28 * 28 floats in an Array, Users can set
+               SeqToTensor(Array(28, 28)) as featureTransformer, which will convert the feature data into
+               Tensors with dimension 28 * 28 to be processed by Model.
+        :param label_size: The size (Tensor dimensions) of the feature data.
+        :param jvalue: Java object create by Py4j
+        :param bigdl_type: optional parameter. data type of model, "float"(default) or "double".
+        """
+        return cls(model, criterion, FeatureLabelTransformer(feature_transformer, label_transformer), jvalue,
+                   bigdl_type)
 
     def setMaxEpoch(self, val):
         """
@@ -136,7 +168,7 @@ class NNEstimator(JavaEstimator, HasFeaturesCol, HasLabelCol, HasPredictionCol, 
         return self.getOrDefault(self.learningRate)
 
     def _create_model(self, java_model):
-        nnModel = NNModel.of(java_model, self.feature_transformer, self.bigdl_type)
+        nnModel = NNModel.of(java_model, FeatureToTupleAdapter(self.sample_transformer), self.bigdl_type)
         nnModel.setFeaturesCol(self.getFeaturesCol()) \
             .setPredictionCol(self.getPredictionCol()) \
             .setBatchSize(self.getBatchSize())
@@ -151,7 +183,7 @@ class NNModel(JavaTransformer, HasFeaturesCol, HasPredictionCol, HasBatchSize, J
     Internally NNModel use features column as storage of the feature data, and create
     Tensors according to the constructor parameter featureSize.
     """
-    def __init__(self,  model, feature_transformer, jvalue=None, bigdl_type="float"):
+    def __init__(self,  model, sample_transformer, jvalue=None, bigdl_type="float"):
         """
         create a NNModel with a BigDL model
         :param model: trained BigDL model to use in prediction.
@@ -160,13 +192,18 @@ class NNModel(JavaTransformer, HasFeaturesCol, HasPredictionCol, HasBatchSize, J
         """
         super(NNModel, self).__init__()
         self.value = jvalue if jvalue else callBigDlFunc(
-            bigdl_type, self.jvm_class_constructor(), model, feature_transformer)
+            bigdl_type, self.jvm_class_constructor(), model, sample_transformer)
         self._java_obj = self.value
         self.bigdl_type = bigdl_type
 
     @classmethod
-    def of(self, jvalue, feature_transformer=None, bigdl_type="float"):
-        model = NNModel(model=None, feature_transformer=feature_transformer, jvalue=jvalue, bigdl_type=bigdl_type)
+    def withTensorTransformer(cls, model, feature_transformer, jvalue=None, bigdl_type="float"):
+        chained_transformer = ChainedTransformer([feature_transformer, TensorToSample()])
+        return NNModel(model, chained_transformer, jvalue, bigdl_type)
+
+    @classmethod
+    def of(self, jvalue, sample_transformer=None, bigdl_type="float"):
+        model = NNModel(model=None, sample_transformer=sample_transformer, jvalue=jvalue, bigdl_type=bigdl_type)
         return model
 
 
@@ -184,7 +221,7 @@ class NNClassifier(NNEstimator):
                              image may be with featureSize = 28 * 28).
         :param bigdl_type(optional): Data type of BigDL model, "float"(default) or "double".
         """
-        super(NNClassifier, self).__init__(model, criterion, feature_transformer, NumToTensor(), None, bigdl_type)
+        super(NNClassifier, self).__init__(model, criterion, feature_transformer, None, bigdl_type)
 
 
 class NNClassifierModel(NNModel):
