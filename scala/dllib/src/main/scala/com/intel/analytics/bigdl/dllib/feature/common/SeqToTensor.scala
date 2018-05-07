2569aad4e30fcdec240d33ca9e610a937c4ab5fe
Yuhao Yang <yuhao.yang@intel.com>
2018-05-07 11:07:06 -0700

    NN Estimator/Classifier supports creation with old interface (Tensor size) (#172)
    * construct with size
    
    * style fix

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/feature/common/SeqToTensor.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/feature/common/SeqToTensor.scala
@@ -17,41 +17,42 @@ package com.intel.analytics.zoo.feature.common
 
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
-
 import scala.reflect.ClassTag
 
 /**
- * a Preprocessing that converts an Seq[_] to a Tensor.
+ * a Preprocessing that converts Float, Double, Array[Float], Array[Double] or MLlib Vector to a
+ * Tensor.
  * @param size dimensions of target Tensor.
  */
 class SeqToTensor[T: ClassTag](size: Array[Int])(implicit ev: TensorNumeric[T])
-  extends Preprocessing[Seq[AnyVal], Tensor[T]] {
+  extends Preprocessing[Any, Tensor[T]] {
 
-  override def apply(prev: Iterator[Seq[AnyVal]]): Iterator[Tensor[T]] = {
+  override def apply(prev: Iterator[Any]): Iterator[Tensor[T]] = {
     prev.map { f =>
-      val feature = f.head match {
-        case dd: Double => f.asInstanceOf[Seq[Double]].map(ev.fromType(_))
-        case ff: Float => f.asInstanceOf[Seq[Float]].map(ev.fromType(_))
+      val feature = f match {
+        case ff: Float => Array(ff).map(ev.fromType(_))
+        case dd: Double => Array(dd).map(ev.fromType(_))
+        case ii: Int => Array(ii).map(ev.fromType(_))
+        case sd: Seq[Any] => matchSeq(sd)
+        case mllibVec: org.apache.spark.mllib.linalg.Vector =>
+          f.asInstanceOf[org.apache.spark.mllib.linalg.Vector ].toArray.map(ev.fromType(_))
         case _ => throw new IllegalArgumentException("SeqToTensor only supports Float and Double")
       }
-      Tensor(feature.toArray, size)
+      Tensor(feature, size)
     }
   }
+
+  def matchSeq(list: Seq[Any]): Array[T] = list.head match {
+    case dd: Double => list.asInstanceOf[Seq[Double]].map(ev.fromType(_)).toArray
+    case ff: Float => list.asInstanceOf[Seq[Float]].map(ev.fromType(_)).toArray
+    case ii: Int => list.asInstanceOf[Seq[Int]].map(ev.fromType(_)).toArray
+    case _ => throw new IllegalArgumentException(s"SeqToTensor only supports Array[Int], " +
+      s"Array[Float] and Array[Double] for ArrayType, but got $list")
+  }
 }
+
 
 object SeqToTensor {
   def apply[T: ClassTag](size: Array[Int])(implicit ev: TensorNumeric[T]): SeqToTensor[T] =
     new SeqToTensor[T](size)
-}
-
-/**
- * a Preprocessing that converts an Array[_] to a Tensor.
- * @param size dimensions of target Tensor.
- */
-class ArrayToTensor[T: ClassTag](size: Array[Int])(implicit ev: TensorNumeric[T])
-  extends SeqToTensor[T](size)
-
-object ArrayToTensor {
-  def apply[T: ClassTag](size: Array[Int])(implicit ev: TensorNumeric[T]): ArrayToTensor[T] =
-    new ArrayToTensor[T](size)
 }
