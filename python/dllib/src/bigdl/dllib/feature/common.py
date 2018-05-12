7f89c37536834051b4a854acf79a90936413961d
Yuhao Yang <yuhao.yang@intel.com>
2018-05-11 20:38:55 -0700

    New NNFrames interface (#162)
    * scala
    
    * separate
    
    * python update
    
    * create unit test
    
    * style fix
    
    * zoo

--- python/dllib/src/bigdl/dllib/feature/common.py
+++ python/dllib/src/bigdl/dllib/feature/common.py
@@ -56,7 +56,7 @@ class SeqToTensor(Preprocessing):
     a Transformer that converts an Array[_] or Seq[_] to a Tensor.
     :param size dimensions of target Tensor.
     """
-    def __init__(self, size, bigdl_type="float"):
+    def __init__(self, size=[], bigdl_type="float"):
         super(SeqToTensor, self).__init__(bigdl_type, size)
 
 
@@ -123,3 +123,11 @@ class FeatureToTupleAdapter(Preprocessing):
 class BigDLAdapter(Preprocessing):
     def __init__(self, bigdl_transformer, bigdl_type="float"):
         super(BigDLAdapter, self).__init__(bigdl_type, bigdl_transformer)
+
+
+class ToTuple(Preprocessing):
+    """
+     a Transformer that converts Feature to (Feature, None).
+    """
+    def __init__(self, bigdl_type="float"):
+        super(ToTuple, self).__init__(bigdl_type)
