886396c75c8157b66df3c939e0df12bf79bd4c05
Maurice Nsabimana <mutabazi@users.noreply.github.com>
2018-05-21 17:05:45 -0400

    Update nn_image_reader.py (#225)
    Rectify minor typo.

--- python/dllib/src/bigdl/dllib/nnframes/nn_image_reader.py
+++ python/dllib/src/bigdl/dllib/nnframes/nn_image_reader.py
@@ -29,7 +29,7 @@ class NNImageReader:
     """
 
     @staticmethod
-    def readImages(path, sc=None, minParitions=1, bigdl_type="float"):
+    def readImages(path, sc=None, minPartitions=1, bigdl_type="float"):
         """
         Read the directory of images into DataFrame from the local or remote source.
         :param path Directory to the input data files, the path can be comma separated paths as the
@@ -38,6 +38,6 @@ class NNImageReader:
         :return DataFrame with a single column "image"; Each record in the column represents
                 one image record: Row (uri, height, width, channels, CvType, bytes).
         """
-        df = callBigDlFunc(bigdl_type, "nnReadImage", path, sc, minParitions)
+        df = callBigDlFunc(bigdl_type, "nnReadImage", path, sc, minPartitions)
         df._sc._jsc = sc._jsc
         return df
