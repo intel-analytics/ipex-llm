036a071263d73a172dfe858095e1106ab5c2dfe2
wzhongyuan <wzhongyuan@gmail.com>
2018-05-23 14:54:23 +0800

    add new transformers

---
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/transform/vision/image/augmentation/RandomResize.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.transform.vision.image.augmentation
+
+import com.intel.analytics.bigdl.transform.vision.image.{FeatureTransformer, ImageFeature}
+import com.intel.analytics.bigdl.utils.RandomGenerator
+import org.opencv.imgproc.Imgproc
+class RandomResize(minSize: Int, maxSize : Int) extends FeatureTransformer {
+  override def transformMat(feature: ImageFeature): Unit = {
+    if (feature.isValid) {
+      var height = feature.opencvMat.height
+      var width = feature.opencvMat.width
+      val shorterSize = RandomGenerator.RNG.uniform(1e-2, maxSize - minSize + 1).toInt + minSize
+      if (height < width) {
+        width = (width.toFloat / height * shorterSize).toInt
+        height = shorterSize
+      } else {
+        height = (height.toFloat / width * shorterSize).toInt
+        width = shorterSize
+      }
+
+      Resize.transform(feature.opencvMat(), feature.opencvMat(), width, height, Imgproc.INTER_CUBIC)
+    }
+  }
+}
+
+object RandomResize {
+  def apply(minSize: Int, maxSize: Int): RandomResize = new RandomResize(minSize, maxSize)
+}
