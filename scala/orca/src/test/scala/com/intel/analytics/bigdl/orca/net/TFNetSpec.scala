30e4e3e66f6d608db7cd2748add90d33f333249c
Yang Wang <tiandiwoxin@icloud.com>
2018-05-30 12:24:15 +0800

    support different data types in TFNet (#284)
    * support different data types in TFNet
    
    * add docs

--- scala/orca/src/test/scala/com/intel/analytics/bigdl/orca/net/TFNetSpec.scala
+++ scala/orca/src/test/scala/com/intel/analytics/bigdl/orca/net/TFNetSpec.scala
@@ -15,6 +15,8 @@
  */
 package com.intel.analytics.zoo.pipeline.api.net
 
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.utils.T
 import com.intel.analytics.zoo.common.NNContext
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
@@ -32,5 +34,26 @@ class TFNetSpec extends FlatSpec with Matchers with BeforeAndAfter {
     if (sc != null) {
       sc.stop()
     }
+  }
+
+  "TFNet " should "work with different data types" in  {
+
+    val resource = getClass().getClassLoader().getResource("tf")
+    val path = resource.getPath + "/" + "multi_type_inputs_outputs.pb"
+
+    val inputs = Seq("float_input:0", "double_input:0",
+      "int_input:0", "long_input:0", "uint8_input:0")
+    val outputs = Seq("float_output:0", "double_output:0",
+      "int_output:0", "long_output:0", "uint8_output:0")
+    val net = TFNet(path, inputs, outputs)
+    val data = T(Tensor[Float](Array[Float](1.0f), Array(1, 1)),
+      Tensor[Float](Array[Float](2.0f), Array(1, 1)),
+      Tensor[Float](Array[Float](3.0f), Array(1, 1)),
+      Tensor[Float](Array[Float](4.0f), Array(1, 1)),
+      Tensor[Float](Array[Float](255.0f), Array(1, 1))
+    )
+    val result = net.forward(data)
+
+    result should be (data)
   }
 }
