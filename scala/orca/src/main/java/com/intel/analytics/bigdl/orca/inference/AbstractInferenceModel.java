430f5f1ea4a6c1b8ac2632c1ffce755e4f3fd23e
Dongjie Shi <glorysdj@gmail.com>
2018-05-30 19:07:15 +0800

    add api for inference model (#297)
    * add api for inference model
    
    * adjust code format
    
    * add ModelLoader to avoid java can not find scala classes
    
    * addjust format
    
    * addjust format
    
    * add concurrent support api

---
+++ scala/orca/src/main/java/com/intel/analytics/bigdl/orca/inference/AbstractInferenceModel.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.zoo.pipeline.inference;
+
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public abstract class AbstractInferenceModel {
+  private FloatInferenceModel model;
+
+  private int supportedConcurrentNum = 1;
+
+  public AbstractInferenceModel(){
+  }
+
+  public AbstractInferenceModel(int supportedConcurrentNum){
+    this.supportedConcurrentNum = supportedConcurrentNum;
+  }
+
+  public void load(String modelPath) {
+    load(modelPath, null);
+  }
+
+  public void load(String modelPath, String weightPath) {
+    this.model = InferenceModelFactory.loadFloatInferenceModel(modelPath, weightPath);
+  }
+
+  public void reload(String modelPath) {
+    load(modelPath, null);
+  }
+
+  public void reload(String modelPath, String weightPath) {
+    this.model = InferenceModelFactory.loadFloatInferenceModel(modelPath, weightPath);
+  }
+
+  public List<Float> predict(List<List<Float>> input) {
+     return model.predict(input);
+  }
+}
