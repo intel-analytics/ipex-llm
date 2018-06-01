3770cca45332f32d973b0926642f3f260170bf0a
simanw304 <30874441+simanw304@users.noreply.github.com>
2018-06-01 09:38:24 +0800

    add softsign, softplus and unittests (#288)
    * update
    
    * md
    
    * md
    
    * style

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/autograd/math.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/autograd/math.scala
@@ -128,6 +128,20 @@ object AutoGrad {
       implicit ev: TensorNumeric[T]): Variable[T] = {
     Variable(Power[T](a).inputs(x.node))
   }
+
+  def softsign[T: ClassTag](a: Variable[T])(
+    implicit ev: TensorNumeric[T]): Variable[T] = {
+    val o: KerasLayer[Activity, Activity, T] =
+      new KerasLayerWrapper(bnn.SoftSign[T]().asInstanceOf[AbstractModule[Activity, Activity, T]])
+    Variable(o.inputs(a.node))
+  }
+
+  def softplus[T: ClassTag](a: Variable[T])(
+    implicit ev: TensorNumeric[T]): Variable[T] = {
+    val o: KerasLayer[Activity, Activity, T] =
+      new KerasLayerWrapper(bnn.SoftPlus[T]().asInstanceOf[AbstractModule[Activity, Activity, T]])
+    Variable(o.inputs(a.node))
+  }
 }
 
 object Variable extends {
@@ -281,3 +295,4 @@ class Variable[T: ClassTag] private (val node: ModuleNode[T])(
     Tensor[T](getInputShape().copyAndUpdate(0, batchSize).toSingle().toArray).fill(fillValue)
   }
 }
+
