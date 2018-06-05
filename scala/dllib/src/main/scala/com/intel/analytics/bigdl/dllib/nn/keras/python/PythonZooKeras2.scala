24c9bb2da9327ba6d297126927063960eb419ea7
li,zhichao <zhichao.li@intel.com>
2018-06-05 13:23:25 +0800

    Init the strcuture for supporting keras 2.1.6 (#320)
    * add keras2.1.6
    
    * add gen scripts
    
    * style and test
    
    * add tags for keras2
    
    * refactor the structure
    
    * style
    
    * add keras
    
    * clean

---
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/keras/python/PythonZooKeras2.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.zoo.pipeline.api.keras.python
+
+import java.util.{List => JList}
+
+import com.intel.analytics.bigdl.optim.Regularizer
+import com.intel.analytics.bigdl.python.api.PythonBigDLKeras
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.zoo.pipeline.api.keras2.layers.Dense
+
+import scala.reflect.ClassTag
+
+object PythonZooKeras2 {
+
+  def ofFloat(): PythonZooKeras2[Float] = new PythonZooKeras2[Float]()
+
+  def ofDouble(): PythonZooKeras2[Double] = new PythonZooKeras2[Double]()
+}
+
+class PythonZooKeras2[T: ClassTag](implicit ev: TensorNumeric[T]) extends PythonBigDLKeras[T] {
+
+  def createZooKeras2Dense(
+      units: Int,
+      kernelInitializer: String = "glorot_uniform",
+      biasInitializer: String = "zero",
+      activation: String = null,
+      kernelRegularizer: Regularizer[T] = null,
+      biasRegularizer: Regularizer[T] = null,
+      useBias: Boolean = true,
+      inputShape: JList[Int] = null): Dense[T] = {
+     Dense[T](
+      units = units,
+      kernelInitializer = kernelInitializer,
+      biasInitializer = biasInitializer,
+      activation = activation,
+      kernelRegularizer = kernelRegularizer,
+      biasRegularizer = biasRegularizer,
+      useBias = useBias,
+      inputShape = toScalaShape(inputShape))
+  }
+
+}
+
