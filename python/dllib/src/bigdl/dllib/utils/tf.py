16936da6d2811ac698bea5dd9f832463c3ef33ef
Yang Wang <tiandiwoxin@icloud.com>
2018-06-08 14:11:02 +0800

    Add util to Export tensorflow graph (#363)
    * add export tf util
    
    * fix types
    
    * add doc
    
    * preserve
    
    * address comments
    
    * fix bug

---
+++ python/dllib/src/bigdl/dllib/utils/tf.py
@@ -0,0 +1,178 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from tensorflow.python.framework import dtypes
+from tensorflow.core.framework import attr_value_pb2
+from tensorflow.core.framework import graph_pb2
+from tensorflow.core.framework import node_def_pb2
+from tensorflow.python.framework import graph_util
+from tensorflow.python.platform import gfile
+import os
+import json
+import copy
+
+
+def export_tf(sess, folder, inputs, outputs):
+    """
+    Export the frozen tensorflow graph as well as the inputs/outputs information
+    to the folder for inference.
+
+    This function will
+    1. freeze the graph (replace all variables with constants)
+    2. strip all unused node as specified by inputs and outputs
+    3. add placeholder nodes as needed
+    4. write the frozen graph and inputs/outputs names to the folder
+
+    Note: There should not be any queuing operation between inputs and outputs
+
+    :param sess: tensorflow session holding the variables to be saved
+    :param folder: the folder where graph file and inputs/outputs information are saved
+    :param inputs: a list of tensorflow tensors that will be fed during inference
+    :param outputs: a list of tensorflow tensors that will be fetched during inference
+    :return:
+    """
+
+    output_node_names = list({t.op.name for t in outputs})
+
+    graph_def = sess.graph_def
+
+    # clear device specifications
+    for node in graph_def.node:
+        node.device = ""
+
+    non_placeholder_input_names = []
+    type_enums = []
+    for input_tensor in inputs:
+        if input_tensor.op.type != "Placeholder":
+            non_placeholder_input_names.append(input_tensor.name)
+            type_enums.append(input_tensor.dtype.as_datatype_enum)
+
+    output_names = map(lambda o: o.name, outputs)
+
+    # freeze graph
+    frozen_graph_def = graph_util.convert_variables_to_constants(
+        sess,
+        graph_def,
+        output_node_names
+    )
+
+    optimized_graph_def, old_names2new = strip_unused(frozen_graph_def,
+                                                      non_placeholder_input_names,
+                                                      output_names,
+                                                      type_enums)
+
+    new_input_names = []
+    for t in inputs:
+        if t.name in old_names2new:
+            new_input_names.append(old_names2new[t.name])
+        else:
+            new_input_names.append(t.name)
+
+    # check all placeholder in the graph are listed in the new_input_names:
+    new_input_nodes = {name.split(":")[0] for name in new_input_names}
+    for node in optimized_graph_def.node:
+        if node.op == "Placeholder" and node.name not in new_input_nodes:
+            raise ValueError(
+                "Node %s is a Placeholder but not listed in inputs, inputs are %s"
+                % (node.name, inputs))
+
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
+
+    with gfile.GFile(os.path.join(folder, "frozen_inference_graph.pb"), "wb") as f:
+        f.write(optimized_graph_def.SerializeToString())
+
+    meta = {
+        "input_names": new_input_names,
+        "output_names": output_names
+    }
+    with open(os.path.join(folder, "graph_meta.json"), "w") as f:
+        f.write(json.dumps(meta))
+
+
+def strip_unused(input_graph_def, input_tensor_names, output_tensor_names,
+                 placeholder_type_enum):
+    """Removes unused nodes from a GraphDef.
+
+  Args:
+    input_graph_def: A graph with nodes we want to prune.
+    input_tensor_names: A list of the nodes we use as inputs.
+    output_tensor_names: A list of the output nodes.
+    placeholder_type_enum: The AttrValue enum for the placeholder data type, or
+        a list that specifies one value per input node name.
+
+  Returns:
+    A `GraphDef` with all unnecessary ops removed. and a map containing the old input
+    names to the new input names
+
+  Raises:
+    ValueError: If any element in `input_node_names` refers to a tensor instead
+      of an operation.
+    KeyError: If any element in `input_node_names` is not found in the graph.
+  """
+    for name in input_tensor_names:
+        if ":" not in name:
+            raise ValueError("Input '%s' appears to refer to a Operation, "
+                             "not a Tensor." % name)
+
+    old2new = {}
+
+    # Here we replace the nodes we're going to override as inputs with
+    # placeholders so that any unused nodes that are inputs to them are
+    # automatically stripped out by extract_sub_graph().
+    not_found = {name for name in input_tensor_names}
+    input_node_names = {name.split(":")[0] for name in input_tensor_names}
+    output_node_names = list({name.split(":")[0] for name in output_tensor_names})
+    inputs_replaced_graph_def = graph_pb2.GraphDef()
+    for node in input_graph_def.node:
+        if node.name not in input_node_names:
+            for i in range(len(node.input)):
+                if _append_port(node.input[i]) in input_tensor_names:
+                    old_name = _append_port(node.input[i])
+                    not_found.remove(old_name)
+                    new_input_name = node.input[i].replace(":", "_")
+                    placeholder_node = node_def_pb2.NodeDef()
+                    placeholder_node.op = "Placeholder"
+                    placeholder_node.name = new_input_name
+                    if isinstance(placeholder_type_enum, list):
+                        input_node_index = input_tensor_names.index(old_name)
+                        placeholder_node.attr["dtype"].CopyFrom(
+                            attr_value_pb2.AttrValue(type=placeholder_type_enum[
+                                input_node_index]))
+                    else:
+                        placeholder_node.attr["dtype"].CopyFrom(
+                            attr_value_pb2.AttrValue(type=placeholder_type_enum))
+                    if "_output_shapes" in node.attr:
+                        placeholder_node.attr["_output_shapes"].CopyFrom(
+                            node.attr["_output_shapes"])
+                    node.input[i] = new_input_name
+                    old2new[old_name] = new_input_name + ":0"
+                    inputs_replaced_graph_def.node.extend([placeholder_node])
+            inputs_replaced_graph_def.node.extend([copy.deepcopy(node)])
+
+    if not_found:
+        raise KeyError("The following input nodes were not found: %s\n" % not_found)
+
+    output_graph_def = graph_util.extract_sub_graph(inputs_replaced_graph_def,
+                                                    output_node_names)
+    return output_graph_def, old2new
+
+
+def _append_port(input_name):
+    if input_name.find(":") == -1:
+        return input_name + ":0"
+    else:
+        return input_name
