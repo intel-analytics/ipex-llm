ecf3ee1437acfc2ba9a11f9b7608d17a8abf38db
edieYoung <370676740@qq.com>
2018-06-27 10:47:01 +0800

    Add dog-vs-cats app test (#424)
    * add timer in run_app_tests
    
    * add timer to run_app_tests
    
    * add_timer_to_app_tests
    
    * add exception exit in apps and example tests
    
    * switch test order
    
    * switch test order to verify
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * example_update
    
    * example_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * fix_apptest
    
    * test_update
    
    * test_update
    
    * test_update
    
    * test_update
    
    * fix_apptest
    
    * fix_apptest
    
    * fix_apptest
    
    * fix_apptest
    
    * add dogs-vs-cats test
    
    * add dogs-vs-cats
    
    * add dogs-vs-cats test
    
    * add dogs-vs-cats test
    
    * add dogs-vs-cats test
    
    * add_dogs-vs-cats
    
    * add_dogs-vs-cats
    
    * add_dogs-vs-cats
    
    * add_dogs-vs-cats
    
    * add_dogs-vs-cats
    
    * add_dogs-vs-cats
    
    * add_dogs-vs-cats
    
    * add_dogs-vs-cats
    
    * add_dogs-vs-cats
    
    * add_dogs-vs-cats

--- python/orca/dev/example/run-example-tests.sh
+++ python/orca/dev/example/run-example-tests.sh
@@ -10,7 +10,11 @@ export ANALYTICS_ZOO_PYZIP=`find ${ANALYTICS_ZOO_HOME_DIST}/lib -type f -name "a
 export ANALYTICS_ZOO_CONF=${ANALYTICS_ZOO_HOME_DIST}/conf/spark-analytics-zoo.conf
 export PYTHONPATH=${ANALYTICS_ZOO_PYZIP}:$PYTHONPATH
 
+set -e
+
 echo "#1 start example test for textclassification"
+#timer
+start=$(date "+%s")
 if [ -f analytics-zoo-data/data/glove.6B.zip ]
 then
     echo "analytics-zoo-data/data/glove.6B.zip already exists" 
@@ -31,19 +35,20 @@ if [ ! -d analytics-zoo-models ]
 then
     mkdir analytics-zoo-models
 fi
-if [ -f analytics-zoo-models/analytics-zoo_squeezenet_imagenet_0.1.0 ]
+
+if [ -f analytics-zoo-models/image-classification/analytics-zoo_squeezenet_imagenet_0.1.0.model ]
 then
-    echo "analytics-zoo-models/analytics-zoo_squeezenet_imagenet_0.1.0 already exists"
+    echo "analytics-zoo-models/image-classification/analytics-zoo_squeezenet_imagenet_0.1.0.model already exists"
 else
-    wget $FTP_URI/analytics-zoo-models/imageclassification/imagenet/analytics-zoo_squeezenet_imagenet_0.1.0 \
+    wget $FTP_URI/analytics-zoo-models/image-classification/analytics-zoo_squeezenet_imagenet_0.1.0.model\
     -P analytics-zoo-models
 fi
-if [ -f analytics-zoo-models-new/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model ]
+if [ -f analytics-zoo-models/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model ]
 then
-    echo "analytics-zoo-models-new/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model already exists"
+    echo "analytics-zoo-models/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model already exists"
 else
-    wget $FTP_URI/analytics-zoo-models-new/object-detection/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model \
-    -P analytics-zoo-models-new
+    wget $FTP_URI/analytics-zoo-models/object-detection/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model \
+    -P analytics-zoo-models
 fi
 
 ${SPARK_HOME}/bin/spark-submit \
@@ -58,7 +63,13 @@ ${SPARK_HOME}/bin/spark-submit \
     --nb_epoch 2 \
     --data_path analytics-zoo-data/data
 
+
+now=$(date "+%s")
+time1=$((now-start))
+
 echo "#2 start example test for customized loss and layer (Funtional API)"
+#timer
+start=$(date "+%s")
 ${SPARK_HOME}/bin/spark-submit \
     --master ${MASTER} \
     --driver-memory 20g \
@@ -69,7 +80,12 @@ ${SPARK_HOME}/bin/spark-submit \
     --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
     ${ANALYTICS_ZOO_HOME}/pyzoo/zoo/examples/autograd/custom.py \
     --nb_epoch 2
+now=$(date "+%s")
+time2=$((now-start))
 
+echo "#3 start example test for image-classification"
+#timer
+start=$(date "+%s")
 ${SPARK_HOME}/bin/spark-submit \
     --master ${MASTER} \
     --driver-memory 20g \
@@ -80,9 +96,14 @@ ${SPARK_HOME}/bin/spark-submit \
     --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
     ${ANALYTICS_ZOO_HOME}/pyzoo/zoo/examples/imageclassification/predict.py \
     -f hdfs://172.168.2.181:9000/kaggle/train_100 \
-    --model analytics-zoo-models/analytics-zoo_squeezenet_imagenet_0.1.0 \
+    --model analytics-zoo-models/analytics-zoo_squeezenet_imagenet_0.1.0.model \
     --topN 5
+now=$(date "+%s")
+time3=$((now-start))
 
+echo "#4 start example test for object-detection"
+#timer
+start=$(date "+%s")
 ${SPARK_HOME}/bin/spark-submit \
     --master ${MASTER} \
     --driver-memory 20g \
@@ -92,4 +113,13 @@ ${SPARK_HOME}/bin/spark-submit \
     --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_JAR} \
     --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_JAR} \
     ${ANALYTICS_ZOO_HOME}/pyzoo/zoo/examples/objectdetection/predict.py \
-    analytics-zoo-models-new/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model hdfs://172.168.2.181:9000/kaggle/train_100 /tmp
+    analytics-zoo-models/analytics-zoo_ssd-mobilenet-300x300_PASCAL_0.1.0.model hdfs://172.168.2.181:9000/kaggle/train_100 /tmp
+now=$(date "+%s")
+time4=$((now-start))
+
+
+
+echo "#1 textclassification time used:$time1 seconds"
+echo "#2 customized loss and layer time used:$time2 seconds"
+echo "#3 image-classification time used:$time3 seconds"
+echo "#4 object-detection loss and layer time used:$time4 seconds"
