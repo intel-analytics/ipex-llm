95b3df9feaee166b8ee31987a055a423ca00a56c
dding3 <ding.ding@intel.com>
2018-07-02 15:24:43 -0700

    support nhwc for imagemattotensor (#406)
    * support nhwc for imagemattotensor

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/feature/image/ImageMatToTensor.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/feature/image/ImageMatToTensor.scala
@@ -15,30 +15,75 @@
  */
 package com.intel.analytics.zoo.feature.image
 
+import com.intel.analytics.bigdl.nn.abstractnn.DataFormat
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
-import com.intel.analytics.bigdl.transform.vision.image.ImageFeature
+import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.transform.vision.image.{ImageFeature, MatToFloats, MatToTensor}
 import com.intel.analytics.bigdl.transform.vision.image
 
 import scala.reflect.ClassTag
 
+/**
+ * transform opencv mat to tensor
+ * @param toRGB BGR to RGB (default is BGR)
+ * @param tensorKey key to store transformed tensor
+ * @param format DataFormat.NCHW or DataFormat.NHWC
+ */
 class ImageMatToTensor[T: ClassTag](
     toRGB: Boolean = false,
     tensorKey: String = ImageFeature.imageTensor,
-    shareBuffer: Boolean = true)(implicit ev: TensorNumeric[T])
+    shareBuffer: Boolean = true,
+    format: DataFormat = DataFormat.NCHW)(implicit ev: TensorNumeric[T])
   extends ImageProcessing {
+  private val imageTensor: Tensor[T] = Tensor[T]()
+  private val matToFloats = MatToFloats()
 
-  private val internalResize = new image.MatToTensor[T](toRGB, tensorKey, shareBuffer)
+  private val internalMatToTensor = new image.MatToTensor[T](toRGB, tensorKey, shareBuffer)
   override def apply(prev: Iterator[ImageFeature]): Iterator[ImageFeature] = {
-    internalResize.apply(prev)
+    format match {
+      case DataFormat.NHWC => prev.map { iter =>
+        transformWithNHWC(iter)
+      }
+      case DataFormat.NCHW => internalMatToTensor.apply(prev)
+      case other => throw new IllegalArgumentException(s"Unsupported format:" +
+        s" $format. Only NCHW and NHWC are supported.")
+    }
+  }
+
+  def transformWithNHWC(feature: ImageFeature): ImageFeature = {
+    if (!feature.isValid) return feature
+    try {
+      val (height, width, channel) = feature.getSize
+      matToFloats.transform(feature)
+      if (channel == 1) {
+        imageTensor.resize(height, width)
+      } else {
+        imageTensor.resize(height, width, channel)
+      }
+      System.arraycopy(feature.floats(), 0, imageTensor.storage().array(),
+        0, imageTensor.nElement())
+      if (!shareBuffer) {
+        feature(tensorKey) = imageTensor.clone()
+      } else {
+        feature(tensorKey) = imageTensor
+      }
+    } catch {
+      case e: Exception =>
+        val uri = feature.uri()
+        MatToTensor.logger.warn(s"float to tensor fail for ${uri}")
+        e.printStackTrace()
+        feature.isValid = false
+    }
+    feature
   }
 }
 
 object ImageMatToTensor {
-
   def apply[T: ClassTag](
       toRGB: Boolean = false,
       tensorKey: String = ImageFeature.imageTensor,
-      shareBuffer: Boolean = true
+      shareBuffer: Boolean = true,
+      format: DataFormat = DataFormat.NCHW
   )(implicit ev: TensorNumeric[T]): ImageMatToTensor[T] =
-    new ImageMatToTensor[T](toRGB, tensorKey, shareBuffer)
+    new ImageMatToTensor[T](toRGB, tensorKey, shareBuffer, format)
 }
--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/feature/python/PythonImageFeature.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/feature/python/PythonImageFeature.scala
@@ -18,12 +18,12 @@ package com.intel.analytics.zoo.feature.python
 
 import java.util.{List => JList}
 
+import com.intel.analytics.bigdl.nn.abstractnn.DataFormat
 import com.intel.analytics.bigdl.python.api.{JTensor, PythonBigDL}
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.transform.vision.image._
 import com.intel.analytics.zoo.feature.common.Preprocessing
 import com.intel.analytics.zoo.feature.image._
-
 import org.apache.spark.api.java.{JavaRDD, JavaSparkContext}
 import org.opencv.imgproc.Imgproc
 
@@ -136,8 +136,16 @@ class PythonImageFeature[T: ClassTag](implicit ev: TensorNumeric[T]) extends Pyt
       stdR.toFloat, stdG.toFloat, stdB.toFloat)
   }
 
-  def createImageMatToTensor(): ImageMatToTensor[T] = {
-    ImageMatToTensor()
+  def createImageMatToTensor(toRGB: Boolean = false,
+                             tensorKey: String = ImageFeature.imageTensor,
+                             shareBuffer: Boolean = true,
+                             format: String = "NCHW"): ImageMatToTensor[T] = {
+    format match {
+      case "NCHW" => ImageMatToTensor(toRGB, tensorKey, shareBuffer, DataFormat.NCHW)
+      case "NHWC" => ImageMatToTensor(toRGB, tensorKey, shareBuffer, DataFormat.NHWC)
+      case other => throw new IllegalArgumentException(s"Unsupported format:" +
+        s" $format. Only NCHW and NHWC are supported.")
+    }
   }
 
   def createImageHue(deltaLow: Double, deltaHigh: Double): ImageHue = {
