e7063e0c9fbce2b2f284df6a46d8db42137226af
Kai Huang <huangkaivision@gmail.com>
2018-07-10 11:37:32 +0800

    Support ImageSet in fit (#332)
    * update
    
    * dataset for imageset
    
    * update python ut
    
    * python
    
    * update
    
    * style

--- python/dllib/src/bigdl/dllib/keras/engine/topology.py
+++ python/dllib/src/bigdl/dllib/keras/engine/topology.py
@@ -15,6 +15,7 @@
 #
 
 import zoo.pipeline.api.autograd as autograd
+from zoo.feature.image import ImageSet
 from zoo.pipeline.api.keras.base import ZooKerasLayer
 from zoo.pipeline.api.keras.utils import *
 
@@ -122,12 +123,12 @@ class KerasNet(ZooKerasLayer):
         Train a model for a fixed number of epochs on a dataset.
 
         # Arguments
-        x: Input data. A Numpy array or RDD of Sample or Image DataSet.
+        x: Input data. A Numpy array or RDD of Sample or ImageSet.
         y: Labels. A Numpy array. Default is None if x is already RDD of Sample or Image DataSet.
         batch_size: Number of samples per gradient update. Default is 32.
         nb_epoch: Number of iterations to train.
-        validation_data: Tuple (x_val, y_val) where x_val and y_val are both Numpy arrays.
-                         Or RDD of Sample. Default is None if no validation is involved.
+        validation_data: Tuple (x_val, y_val) where x_val and y_val are both Numpy arrays. Or
+                         RDD of Sample. Or ImageSet. Default is None if no validation is involved.
         distributed: Boolean. Whether to train the model in distributed mode or local mode.
                      Default is True. In local mode, x and y must both be Numpy arrays.
         """
@@ -136,7 +137,7 @@ class KerasNet(ZooKerasLayer):
                 training_data = to_sample_rdd(x, y)
                 if validation_data:
                     validation_data = to_sample_rdd(*validation_data)
-            elif (isinstance(x, RDD) or isinstance(x, DataSet)) and not y:
+            elif (isinstance(x, RDD) or isinstance(x, ImageSet)) and not y:
                 training_data = x
             else:
                 raise TypeError("Unsupported training data type: %s" % type(x))
