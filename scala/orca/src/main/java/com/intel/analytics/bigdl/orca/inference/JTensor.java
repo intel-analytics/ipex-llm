9fd278ec401b45053923b2c6607e46bc0c89af96
Chengshun Zhang <chengshun.zhang@outlook.com>
2018-07-26 11:01:45 +0800

    Change data type to float[] and shape to int[] in JTensor (#555)
    * solved conflicts
    
    * solve conflicts
    
    * fix the error
    
    * solve the problem without changing the code
    
    * solve the problem without changing the code
    
    * copy by defaul, option for no copy
    
    * delete redundant function transferInputToSample

--- scala/orca/src/main/java/com/intel/analytics/bigdl/orca/inference/JTensor.java
+++ scala/orca/src/main/java/com/intel/analytics/bigdl/orca/inference/JTensor.java
@@ -5,43 +5,108 @@ import java.util.Arrays;
 import java.util.List;
 
 public class JTensor {
-  private List<Float> data;
-  private List<Integer> shape;
+  private float[] data;
+  private int[] shape;
 
   public JTensor() {
   }
 
   public JTensor(List<Float> data, List<Integer> shape) {
-    this.data = data;
-    this.shape = shape;
-  }
-
-  public JTensor(List<Float> data, Integer[] shape) {
-    this.data = data;
-    this.shape = Arrays.asList(shape);
-  }
-
-  public JTensor(List<Float> data, int[] shape) {
-    this.data = data;
-    this.shape = new ArrayList<Integer>();
-    for(int s : shape) {
-      this.shape.add(s);
+    this.data = new float[data.size()];
+    for (int i = 0; i < data.size(); i++){
+      this.data[i] = data.get(i);
+    }
+    this.shape = new int[shape.size()];
+    for (int i = 0; i < shape.size(); i++){
+      this.shape[i] = shape.get(i);
     }
   }
 
-  public List<Float> getData() {
+  public JTensor(List<Float> data, Integer[] shape) {
+    this.data = new float[data.size()];
+    for (int i = 0; i < data.size(); i++){
+      this.data[i] = data.get(i);
+    }
+    this.shape = new int[shape.length];
+    for (int i = 0; i < shape.length; i++){
+      this.shape[i] = shape[i];
+    }
+  }
+
+  public JTensor(List<Float> data, int[] shape) {
+    this.data = new float[data.size()];
+    for (int i = 0; i < data.size(); i++){
+      this.data[i] = data.get(i);
+    }
+    this.shape = new int[shape.length];
+    for (int i = 0; i < shape.length; i++){
+      this.shape[i] = shape[i];
+    }
+  }
+
+  public JTensor(float[] data, List<Integer> shape) {
+    this.data = new float[data.length];
+    for (int i = 0; i < data.length; i++){
+      this.data[i] = data[i];
+    }
+    this.shape = new int[shape.size()];
+    for (int i = 0; i < shape.size(); i++){
+      this.shape[i] = shape.get(i);
+    }
+  }
+
+  public JTensor(float[] data, Integer[] shape) {
+    this.data = new float[data.length];
+    for (int i = 0; i < data.length; i++){
+      this.data[i] = data[i];
+    }
+    this.shape = new int[shape.length];
+    for (int i = 0; i < shape.length; i++){
+      this.shape[i] = shape[i];
+    }
+  }
+
+  public JTensor(float[] data, int[] shape) {
+    this.data = new float[data.length];
+    for (int i = 0; i < data.length; i++){
+      this.data[i] = data[i];
+    }
+    this.shape = new int[shape.length];
+    for (int i = 0; i < shape.length; i++){
+      this.shape[i] = shape[i];
+    }
+  }
+
+  public JTensor(float[] data, int[] shape, boolean copy){
+    if (copy) {
+      this.data = new float[data.length];
+      for (int i = 0; i < data.length; i++){
+        this.data[i] = data[i];
+      }
+      this.shape = new int[shape.length];
+      for (int i = 0; i < shape.length; i++){
+        this.shape[i] = shape[i];
+      }
+    }
+    else {
+      this.data = data;
+      this.shape = shape;
+    }
+  }
+
+  public float[] getData() {
     return data;
   }
 
-  public void setData(List<Float> data) {
+  public void setData(float[] data) {
     this.data = data;
   }
 
-  public List<Integer> getShape() {
+  public int[] getShape() {
     return shape;
   }
 
-  public void setShape(List<Integer> shape) {
+  public void setShape(int[] shape) {
     this.shape = shape;
   }
 
