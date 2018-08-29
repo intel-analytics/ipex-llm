74aadb10b2a3faa69f66e164c3b461a2f52e8fb9
qiuxin2012 <qiuxin2012cs@gmail.com>
2018-08-29 10:54:50 +0800

    Parallel Adam

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/optim/AdamSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/optim/AdamSpec.scala
@@ -18,14 +18,27 @@ package com.intel.analytics.bigdl.optim
 
 import com.intel.analytics.bigdl.nn.{CrossEntropyCriterion, Linear, Sequential}
 import com.intel.analytics.bigdl.tensor.Tensor
-import com.intel.analytics.bigdl.utils.{RandomGenerator, T, TestUtils}
-import org.scalatest.{FlatSpec, Matchers}
+import com.intel.analytics.bigdl.utils.{Engine, RandomGenerator, T, TestUtils}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 @com.intel.analytics.bigdl.tags.Parallel
-class AdamSpec extends FlatSpec with Matchers {
+class AdamSpec extends FlatSpec with Matchers with BeforeAndAfter {
+
+  before {
+    System.setProperty("bigdl.localMode", "true")
+    System.setProperty("spark.master", "local[2]")
+    Engine.init
+  }
+
+  after {
+    System.clearProperty("bigdl.localMode")
+    System.clearProperty("spark.master")
+  }
+
+
   val start = System.currentTimeMillis()
   "adam" should "perform well on rosenbrock function" in {
     val x = Tensor[Double](2).fill(0)
@@ -52,59 +65,31 @@ class AdamSpec extends FlatSpec with Matchers {
     x(Array(1)) should be(1.0 +- 0.01)
     x(Array(2)) should be(1.0 +- 0.01)
   }
-  "adam" should " work fast with MKL" in {
-    RandomGenerator.RNG.setSeed(100)
-    val inputSize = 500
-    val hiddenSize = 500
-    val outputSize = 10
-    val batchSize = 10
-    val model = Sequential[Float]()
-      .add(Linear[Float](inputSize, hiddenSize))
-    for (i <- 1 to 3) {
-      model.add(Linear[Float](hiddenSize, hiddenSize))
-    }
-    model.add(Linear[Float](hiddenSize, outputSize))
-    val criterion = CrossEntropyCriterion[Float]()
 
-    val input = Tensor[Float](batchSize, inputSize).rand
-    val label = Tensor[Float](batchSize).zero
-    for (i <- 1 to batchSize) {
-      val nextLabel = Random.nextInt(outputSize) + 1
-      label.setValue(i, nextLabel)
+  "ParallelAdam" should "perform well on rosenbrock function" in {
+    val x = Tensor[Double](2).fill(0)
+    val optm = new ParallelAdam[Double](learningRate = 0.002, parallelNum = 2)
+    var fx = new ArrayBuffer[Double]
+    for (i <- 1 to 10001) {
+      val result = optm.optimize(TestUtils.rosenBrock, x)
+      if ((i - 1) % 1000 == 0) {
+        fx += result._2(0)
+      }
     }
 
-    val (weights, grad) = model.getParameters()
-
-    val state = T("learningRate" -> 1e-1, "momentum" -> 0.9, "weightDecay" -> 5e-4,
-      "dampening" -> 0.0)
-
-    val adam = new Adam[Float]
-
-    def feval(x: Tensor[Float]): (Float, Tensor[Float]) = {
-      model.forward(input)
-      criterion.forward(model.output.asInstanceOf[Tensor[Float]], label)
-      model.zeroGradParameters()
-      val gradOutputTest = criterion.backward(model.output.asInstanceOf[Tensor[Float]], label)
-      model.backward(input, gradOutputTest)
-      (criterion.output, grad)
+    println(s"x is \n$x")
+    println("fx is")
+    for (i <- 1 to fx.length) {
+      println(s"${(i - 1) * 1000 + 1}, ${fx(i - 1)}")
     }
 
-    val warmUp = 30
-    val iter = 50
-    for (i <- 1 to warmUp) {
-      adam.optimize(feval, weights, state)
-    }
-    var startTime = System.nanoTime
-    var duration = (System.nanoTime() - startTime) / 1e9
-    var sum = 0.0
-    for (i <- 1 to iter) {
-      startTime = System.nanoTime
-      adam.optimize(feval, weights, state)
-      duration = (System.nanoTime() - startTime) / 1e9
-      sum += duration
-      println(s"iter-${i}, eta = ${duration} seconds")
-    }
-    println(s"average eta = ${sum / iter} seconds")
+    val spend = System.currentTimeMillis() - start
+    println("Time Cost: " + spend + "ms")
+
+    (fx.last < 1e-9) should be(true)
+    x(Array(1)) should be(1.0 +- 0.01)
+    x(Array(2)) should be(1.0 +- 0.01)
   }
+
 }
 
