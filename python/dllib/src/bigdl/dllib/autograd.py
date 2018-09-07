7cebc6714298d4390d0f53b65a65b029f04365e0
li,zhichao <zhichao.li@intel.com>
2018-09-07 12:09:28 +0800

    Enable running test per node for loading ONNX (#641)
    * run_node
    
    * add constant
    
    * clean

--- python/dllib/src/bigdl/dllib/autograd.py
+++ python/dllib/src/bigdl/dllib/autograd.py
@@ -245,7 +245,7 @@ def mm(x, y, axes):
 
 class Variable(kbase.ZooKerasCreator):
     def __init__(self, input_shape, node=None, jvalue=None, name=None):
-
+        self.name = name
         if jvalue:
             self.value = jvalue
             self.bigdl_type = "float"
@@ -453,15 +453,16 @@ class Parameter(kbase.ZooKerasLayer):
     A trainable Variable. The default init_method is RandomUniform(-0.05, 0.05).
     You can also specify the init_weight by passing a ndarray.
     """
-    def __init__(self, input_shape, init_method=None,
-                 init_weight=None, **kwargs):
+    def __init__(self, shape, init_method=None,
+                 init_weight=None, trainable=True, **kwargs):
         if not init_method:
             from bigdl.nn.initialization_method import RandomUniform
             init_method = RandomUniform(-0.05, 0.05)
         super(Parameter, self).__init__(None,
-                                        list(input_shape),
+                                        list(shape),
                                         init_method,
                                         init_weight,
+                                        trainable,
                                         ** kwargs)
 
     def get_weight(self):
