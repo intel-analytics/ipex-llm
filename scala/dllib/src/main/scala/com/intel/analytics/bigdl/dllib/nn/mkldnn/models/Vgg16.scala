b9c2478f5925fdbdcbcca9c98cd5fc083480fda4
Yanzhang Wang <i8run15@gmail.com>
2018-09-10 01:34:21 -0400

    feat: add dnn vgg model. (#2627)
    * feat: add dnn vgg model.
    
    * fix: rename the ResNet50Perf to Perf

---
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/mkldnn/models/Vgg16.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.nn.mkldnn.models
+
+import com.intel.analytics.bigdl.Module
+import com.intel.analytics.bigdl.mkl.Memory
+import com.intel.analytics.bigdl.nn.Dropout
+import com.intel.analytics.bigdl.nn.mkldnn._
+
+
+object Vgg_16 {
+  def apply(batchSize: Int, classNum: Int, hasDropout: Boolean = true): Sequential = {
+    val model = Sequential()
+    model.add(Input(Array(batchSize, 3, 224, 224), Memory.Format.nchw))
+    model.add(SpatialConvolution(3, 64, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(SpatialConvolution(64, 64, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(MaxPooling(2, 2, 2, 2))
+
+    model.add(SpatialConvolution(64, 128, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(MaxPooling(2, 2, 2, 2))
+
+    model.add(SpatialConvolution(128, 256, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(SpatialConvolution(256, 256, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(SpatialConvolution(256, 256, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(MaxPooling(2, 2, 2, 2))
+
+    model.add(SpatialConvolution(256, 512, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(MaxPooling(2, 2, 2, 2))
+
+    model.add(SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(SpatialConvolution(512, 512, 3, 3, 1, 1, 1, 1))
+    model.add(ReLU())
+    model.add(MaxPooling(2, 2, 2, 2))
+
+    model.add(Linear(512 * 7 * 7, 4096))
+    model.add(ReLU())
+    if (hasDropout) model.add(Dropout(0.5))
+    model.add(Linear(4096, 4096))
+    model.add(ReLU())
+    if (hasDropout) model.add(Dropout(0.5))
+    model.add(Linear(4096, classNum))
+    model.add(ReorderMemory(HeapData(Array(batchSize, classNum), Memory.Format.nc)))
+
+    model
+  }
+}
+
