57f9d675ff59f0e40338bfb283e4fb408f08ad4c
dding3 <ding.ding@intel.com>
2018-09-27 16:40:36 -0700

    add a transformer to support 0-1 image normalize (#597)
    * add a transformer to support 0-1 image normalize
    
    * support normalize including minmax scaler

--- python/dllib/src/bigdl/dllib/feature/image/imagePreprocessing.py
+++ python/dllib/src/bigdl/dllib/feature/image/imagePreprocessing.py
@@ -85,6 +85,28 @@ class ImageChannelNormalize(ImagePreprocessing):
                                                     mean_b, std_r, std_g, std_b)
 
 
+class PerImageNormalize(ImagePreprocessing):
+    """
+    Normalizes the norm or value range per image, similar to opencv::normalize
+    https://docs.opencv.org/ref/master/d2/de8/group__core__array.html
+    #ga87eef7ee3970f86906d69a92cbf064bd
+    ImageNormalize normalizes scale and shift the input features. Various normalize
+    methods are supported,
+    Eg. NORM_INF, NORM_L1, NORM_L2 or NORM_MINMAX
+    Pleas notice it's a per image normalization.
+    :param min lower range boundary in case of the range normalization or
+    norm value to normalize
+    :param max upper range boundary in case of the range normalization.
+    It is not used for the norm normalization.
+    :param norm_type normalization type, see opencv:NormTypes.
+    https://docs.opencv.org/ref/master/d2/de8/group__core__array.html
+    #gad12cefbcb5291cf958a85b4b67b6149f
+    Default Core.NORM_MINMAX
+    """
+    def __init__(self, min, max, norm_type=32, bigdl_type="float"):
+        super(PerImageNormalize, self).__init__(bigdl_type, min, max, norm_type)
+
+
 class ImageMatToTensor(ImagePreprocessing):
     """
     MatToTensor
