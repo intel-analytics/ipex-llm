ba0d93f53c2a4baf66dc9cdf20d74d89df2cbc10
Jerry Wu <wzhongyuan@gmail.com>
2018-10-09 10:46:51 +0800

    Refine the synchronizer to support prioirty and also make it event driven (#2634)
    * refinement
    
    * refinement
    
    * refinement
    
    * refinement per comments
    
    * refinemnt per review

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/utils/DistributedSynchronizerSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/utils/DistributedSynchronizerSpec.scala
@@ -20,7 +20,7 @@ import com.intel.analytics.bigdl.tensor.Tensor
 import org.apache.spark.{SparkContext, TaskContext}
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
-class DistributedSynchronizerSpec  extends FlatSpec with Matchers with BeforeAndAfter {
+class DistributedSynchronizerSpec extends FlatSpec with Matchers with BeforeAndAfter {
 
   var sc: SparkContext = null
 
@@ -39,21 +39,22 @@ class DistributedSynchronizerSpec  extends FlatSpec with Matchers with BeforeAnd
       val partitionID = TaskContext.getPartitionId
       val sync = new BlockManagerParameterSynchronizer[Float](partitionID, partition)
       val tensor = Tensor[Float](10).fill(partitionID.toFloat + 1.0f)
-      sync.init(s"testPara", 10)
+      sync.init(s"testPara", 10, weights = null, grads = tensor)
       var res : Iterator[_] = null
-      sync.put(s"testPara", tensor)
+      sync.put(s"testPara")
       res = Iterator.single(sync.get(s"testPara"))
       sync.clear
       res
     }).collect
     res.length should be  (4)
-    res(0) should be (Tensor[Float](10).fill(2.5f))
-    res(1) should be (Tensor[Float](10).fill(2.5f))
-    res(2) should be (Tensor[Float](10).fill(2.5f))
-    res(3) should be (Tensor[Float](10).fill(2.5f))
+    res(0).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](10).fill(2.5f))
+    res(1).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](10).fill(2.5f))
+    res(2).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](10).fill(2.5f))
+    res(3).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](10).fill(2.5f))
   }
 
-  "DistributedSynchronizer with parameter size less than partition" should "worl properly" in {
+  "DistributedSynchronizer with parameter size less than partition" should "work properly" in {
+    val cores1 = Runtime.getRuntime().availableProcessors
     val partition = 4
     val cores = 4
     val res = sc.parallelize((0 until partition), partition).mapPartitions(p => {
@@ -61,18 +62,18 @@ class DistributedSynchronizerSpec  extends FlatSpec with Matchers with BeforeAnd
       val partitionID = TaskContext.getPartitionId
       val sync = new BlockManagerParameterSynchronizer[Float](partitionID, partition)
       val tensor = Tensor[Float](2).fill(partitionID.toFloat + 1.0f)
-      sync.init(s"testPara", 2)
+      sync.init(s"testPara", 2, weights = null, grads = tensor)
       var res : Iterator[_] = null
-      sync.put(s"testPara", tensor)
+      sync.put(s"testPara")
       res = Iterator.single(sync.get(s"testPara"))
       sync.clear
       res
     }).collect
     res.length should be  (4)
-    res(0) should be (Tensor[Float](2).fill(2.5f))
-    res(1) should be (Tensor[Float](2).fill(2.5f))
-    res(2) should be (Tensor[Float](2).fill(2.5f))
-    res(3) should be (Tensor[Float](2).fill(2.5f))
+    res(0).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](2).fill(2.5f))
+    res(1).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](2).fill(2.5f))
+    res(2).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](2).fill(2.5f))
+    res(3).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](2).fill(2.5f))
   }
 
   "DistributedSynchronizer with parameter offset > 1" should "work properly" in {
@@ -84,18 +85,18 @@ class DistributedSynchronizerSpec  extends FlatSpec with Matchers with BeforeAnd
       val sync = new BlockManagerParameterSynchronizer[Float](partitionID, partition)
       val tensor = Tensor[Float](20)
       val parameter = tensor.narrow(1, 10, 10).fill(partitionID.toFloat + 1.0f)
-      sync.init(s"testPara", 10)
+      sync.init(s"testPara", 10, weights = null, grads = parameter)
       var res : Iterator[_] = null
-      sync.put(s"testPara", parameter)
+      sync.put(s"testPara")
       res = Iterator.single(sync.get(s"testPara"))
       sync.clear
       res
     }).collect
     res.length should be  (4)
-    res(0) should be (Tensor[Float](10).fill(2.5f))
-    res(1) should be (Tensor[Float](10).fill(2.5f))
-    res(2) should be (Tensor[Float](10).fill(2.5f))
-    res(3) should be (Tensor[Float](10).fill(2.5f))
+    res(0).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](10).fill(2.5f))
+    res(1).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](10).fill(2.5f))
+    res(2).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](10).fill(2.5f))
+    res(3).asInstanceOf[Tuple2[_, _]]._2 should be (Tensor[Float](10).fill(2.5f))
   }
 
   after {
