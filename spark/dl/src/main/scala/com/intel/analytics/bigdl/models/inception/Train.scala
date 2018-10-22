f5f8caa7a974add74c688cb23f38f7fd1be5fca7
wzhongyuan <wzhongyuan@gmail.com>
2018-10-22 09:56:47 +0800

    refinement inception v1 training code

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/models/inception/Train.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/models/inception/Train.scala
@@ -17,7 +17,7 @@ package com.intel.analytics.bigdl.models.inception
 
 import com.intel.analytics.bigdl._
 import com.intel.analytics.bigdl.nn.{ClassNLLCriterion, Module}
-import com.intel.analytics.bigdl.optim.SGD.{Poly, SequentialSchedule, Warmup}
+import com.intel.analytics.bigdl.optim.SGD.{MultiStep, Poly, SequentialSchedule, Warmup}
 import com.intel.analytics.bigdl.optim._
 import com.intel.analytics.bigdl.utils.{Engine, LoggerFilter, T, Table}
 import org.apache.log4j.{Level, Logger}
@@ -78,7 +78,7 @@ object TrainInceptionV1 {
           else (param.maxLr.getOrElse(param.learningRate) - param.learningRate) / warmupIteration
         val polyIteration = maxIteration - warmupIteration
         val lrSchedule = SequentialSchedule(iterationPerEpoch)
-          .add(Warmup(warmupDelta), warmupIteration).add(Poly(0.5, polyIteration), polyIteration)
+          .add(Warmup(warmupDelta), warmupIteration).add(Poly(0.5, maxIteration), polyIteration)
         new SGD[Float](learningRate = param.learningRate, learningRateDecay = 0.0,
           weightDecay = param.weightDecay, momentum = 0.9, dampening = 0.0, nesterov = false,
           learningRateSchedule = lrSchedule)
