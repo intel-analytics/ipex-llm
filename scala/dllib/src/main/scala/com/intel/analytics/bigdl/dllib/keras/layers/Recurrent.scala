649de404c6dcd3327d70056785eae5ecf5c16662
li,zhichao <zhichao.li@intel.com>
2018-10-23 12:10:43 +0800

    Extend LSTM, GRU and SimpleRNN from zoo.keras.Recurrent and fix the serde issue (#809)
    * fix ser
    
    * fix serde
    
    * update
    
    * try
    
    * continue
    
    * try
    
    * continue
    
    * con
    
    * revert InternalRecurrent
    
    * revert unitest

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/keras/layers/Recurrent.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/keras/layers/Recurrent.scala
@@ -16,12 +16,14 @@
 
 package com.intel.analytics.zoo.pipeline.api.keras.layers
 
-import com.intel.analytics.bigdl.nn.Reverse
+import com.intel.analytics.bigdl.nn.{Reverse, Select => TSelect}
 import com.intel.analytics.bigdl.nn.abstractnn.AbstractModule
-import com.intel.analytics.bigdl.nn.keras.{Recurrent => BKerasRecurrent, Sequential => TSequential}
+import com.intel.analytics.bigdl.nn.keras.{Recurrent => BKerasRecurrent}
+import com.intel.analytics.bigdl.nn.{Sequential => TSequential}
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.utils.Shape
+import com.intel.analytics.zoo.pipeline.api.keras.layers.internal.InternalRecurrent
 
 import scala.reflect.ClassTag
 
@@ -36,10 +38,10 @@ abstract class Recurrent[T: ClassTag](
     val input = inputShape.toSingle().toArray
     val model = TSequential[T]()
     if (goBackwards) model.add(Reverse(2))
-    val rec = new com.intel.analytics.zoo.pipeline.api.keras.layers.internal.InternalRecurrent[T]()
+    val rec = new InternalRecurrent[T]()
     rec.add(buildCell(input))
     model.add(rec)
-    if (!returnSequences) model.add(Select(2, -1))
+    if (!returnSequences) model.add(TSelect(2, -1))
     model.asInstanceOf[AbstractModule[Tensor[T], Tensor[T], T]]
   }
 }
