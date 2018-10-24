aa0179f617fdcf0bfe0bef988024f8da9cf8a689
zhangli <cherry.zhang@intel.com>
2018-10-24 15:00:14 +0800

    add dnn graph

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/mkldnn/Perf.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/mkldnn/Perf.scala
@@ -18,6 +18,7 @@ package com.intel.analytics.bigdl.nn.mkldnn
 
 import com.intel.analytics.bigdl.Module
 import com.intel.analytics.bigdl.mkl.{Memory, MklDnn}
+import com.intel.analytics.bigdl.nn.Graph._
 import com.intel.analytics.bigdl.nn._
 import com.intel.analytics.bigdl.nn.abstractnn.Activity
 import com.intel.analytics.bigdl.nn.mkldnn.Phase.{InferencePhase, TrainingPhase}
@@ -254,6 +255,140 @@ object ResNet {
 
     modelInit(model)
     model
+  }
+
+  def graph(batchSize: Int, classNum: Int, opt: Table): DnnGraph = {
+
+    def modelInit(graph: DnnGraph): Unit = {
+      graph.getSortedForwardExecutions.foreach(n => {
+        n.element match {
+          case conv: SpatialConvolution =>
+            val n: Float = conv.kernelW * conv.kernelW * conv.nOutputPlane
+            val weight = Tensor[Float].resize(conv.weight.size()).apply1 { _ =>
+              RNG.normal(0, Math.sqrt(2.0f / n)).toFloat
+            }
+            val bias = Tensor[Float].resize(conv.bias.size()).apply1(_ => 0.0f)
+            conv.weight.copy(weight)
+            conv.bias.copy(bias)
+
+          case bn: SpatialBatchNormalization =>
+            val weightAndBias = Tensor[Float]().resize(Array(2, bn.nOutput))
+            weightAndBias.select(1, 1).fill(1)
+            weightAndBias.select(1, 2).fill(0)
+            bn.weightAndBias.copy(weightAndBias.view(Array(bn.nOutput * 2)))
+
+          case linear: Linear =>
+            val bias = Tensor[Float](linear.bias.size()).apply1(_ => 0.0f)
+            linear.bias.copy(bias)
+
+          case _ =>
+        }
+      })
+    }
+
+    val depth = opt.get("depth").getOrElse(18)
+    val shortCutType = opt.get("shortcutType")
+    val shortcutType = shortCutType.getOrElse(ShortcutType.B).asInstanceOf[ShortcutType]
+    val dataSet = opt.getOrElse[DatasetType]("dataSet", DatasetType.CIFAR10)
+    val optnet = opt.get("optnet").getOrElse(true)
+
+    def shortcut(input: ModuleNode[Float], nInputPlane: Int, nOutputPlane: Int,
+                 stride: Int, name: String): ModuleNode[Float] = {
+      val useConv = shortcutType == ShortcutType.C ||
+        (shortcutType == ShortcutType.B && nInputPlane != nOutputPlane)
+
+      if (useConv) {
+        val conv = Convolution(nInputPlane, nOutputPlane, 1, 1, stride, stride, optnet = optnet)
+            .setName(s"res${name}_branch1").inputs(input)
+        SbnDnn(nOutputPlane).setName(s"bn${name}_branch1").inputs(conv)
+      } else if (nInputPlane != nOutputPlane) {
+        throw new IllegalArgumentException(s"useConv false")
+      } else {
+        Identity().inputs(input)
+      }
+    }
+
+    def bottleneck(input: ModuleNode[Float], n: Int, stride: Int, name: String = "")
+      : ModuleNode[Float] = {
+      val nInputPlane = iChannels
+      iChannels = n * 4
+
+      val conv1 = Convolution(nInputPlane, n, 1, 1, 1, 1, 0, 0, optnet = optnet)
+          .setName(s"res${name}_branch2a").inputs(input)
+      val bn1 = SbnDnn(n).setName(s"bn${name}_branch2a").inputs(conv1)
+      val relu1 = ReLU().setName(s"res${name}_branch2a_relu").inputs(bn1)
+      val conv2 = Convolution(n, n, 3, 3, stride, stride, 1, 1, optnet = optnet).setName(
+          s"res${name}_branch2b").inputs(relu1)
+      val bn2 = SbnDnn(n).setName(s"bn${name}_branch2b").inputs(conv2)
+      val relu3 = ReLU().setName(s"res${name}_branch2b_relu").inputs(bn2)
+      val conv3 = Convolution(n, n*4, 1, 1, 1, 1, 0, 0, optnet = optnet).setName(
+          s"res${name}_branch2c").inputs(relu3)
+      val bn3 = SbnDnn(n * 4).setInitMethod(Zeros, Zeros).setName(
+          s"bn${name}_branch2c").inputs(conv3)
+
+      val short = shortcut(input, nInputPlane, n*4, stride, name)
+      val cadd = CAddTable().setName(s"res$name").
+          inputs(Array(bn3.asInstanceOf[ModuleNode[Float]], short))
+      val relu = ReLU().setName(s"res${name}_relu").inputs(cadd)
+      relu
+    }
+
+    def getName(i: Int, name: String): String = {
+      val name1 = i match {
+        case 1 => name + "a"
+        case 2 => name + "b"
+        case 3 => name + "c"
+        case 4 => name + "d"
+        case 5 => name + "e"
+        case 6 => name + "f"
+      }
+      return name1
+    }
+
+    def layer(input: ModuleNode[Float],
+              block: (ModuleNode[Float], Int, Int, String) => ModuleNode[Float],
+              features: Int,
+              count: Int, stride: Int = 1, name : String): ModuleNode[Float] = {
+      var in = input
+      for (i <- 1 to count) {
+        val res = block(in, features, if (i == 1) stride else 1, getName(i, name))
+        in = res
+      }
+      in
+    }
+
+    if (dataSet == DatasetType.ImageNet) {
+      val cfg = Map(
+        50 -> ((3, 4, 6, 3), 2048,
+          bottleneck: (ModuleNode[Float], Int, Int, String) => ModuleNode[Float])
+      )
+
+      require(cfg.keySet.contains(depth), s"Invalid depth ${depth}")
+
+      val (loopConfig, nFeatures, block) = cfg.get(depth).get
+      iChannels = 64
+
+      val input = Input(Array(batchSize, 3, 224, 224), Memory.Format.nchw).inputs()
+      val conv1 = SpatialConvolution(3, 64, 7, 7, 2, 2, 3, 3, propagateBack = false)
+        .setName("conv1").inputs(input)
+      val bn1 = SbnDnn(64).setName("bn_conv1").inputs(conv1)
+      val relu1 = ReLU().setName("conv1_relu").inputs(bn1)
+      val pool1 = MaxPooling(3, 3, 2, 2).setName("pool1").inputs(relu1)
+      val layer1 = layer(pool1, block, 64, loopConfig._1, name = "2")
+      val layer2 = layer(layer1, block, 128, loopConfig._2, 2, name = "3")
+      val layer3 = layer(layer2, block, 256, loopConfig._3, 2, name = "4")
+      val layer4 = layer(layer3, block, 512, loopConfig._4, 2, name = "5")
+      val pool2 = AvgPooling(7, 7, 1, 1).setName("pool5").inputs(layer4)
+      val fc = Linear(nFeatures, classNum).setInitMethod(RandomNormal(0.0, 0.01), Zeros).setName(
+          "fc1000").inputs(pool2)
+      val output = ReorderMemory(HeapData(Array(batchSize, classNum), Memory.Format.nc)).inputs(fc)
+
+      val model = DnnGraph(Array(input), Array(output))
+      modelInit(model)
+      model
+    } else {
+      throw new IllegalArgumentException(s"Invalid dataset ${dataSet}")
+    }
   }
 
   /**
