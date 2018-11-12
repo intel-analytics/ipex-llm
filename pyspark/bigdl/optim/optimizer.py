9c3757670fa7eebe02dcb398a147ccd0933d0cba
qiuxin2012 <qiuxin2012cs@gmail.com>
2018-11-12 11:28:12 +0800

    add python API

--- pyspark/bigdl/optim/optimizer.py
+++ pyspark/bigdl/optim/optimizer.py
@@ -253,6 +253,41 @@ class MinLoss(JavaValue):
         """
         JavaValue.__init__(self, None, bigdl_type, min)
 
+class And(JavaValue):
+    """
+    A trigger contains other triggers and triggers when all of them trigger (logical AND)
+
+
+    >>> a = And(MinLoss(0.1), MaxEpoch(2))
+    creating: createAnd
+    """
+    def __init__(self, first, *other):
+        """
+        Create a And trigger.
+
+
+        :param first: first Trigger
+        :param other: other Trigger
+        """
+        JavaValue.__init__(self, None, first, list(other))
+
+class Or(JavaValue):
+    """
+    A trigger contains other triggers and triggers when any of them trigger (logical OR)
+
+
+    >>> o = Or(MinLoss(0.1), MaxEpoch(2))
+    creating: createOr
+    """
+    def __init__(self, first, *other):
+        """
+        Create a Or trigger.
+
+
+        :param first: first Trigger
+        :param other: other Trigger
+        """
+        JavaValue.__init__(self, None, first, list(other))
 
 class Poly(JavaValue):
     """
