3273e4c3207bf1b5ef9d8d5823faceb4c94ed092
Yuhao Yang <yuhao.yang@intel.com>
2018-12-28 18:26:23 -0800

    Adam with learning rate schedule (#992)
    * init
    
    * adam
    
    * move to optim
    
    * style fix
    
    * python comment
    
    * change package
    
    * header
    
    * comment
    
    * use reflection
    
    * ut
    
    * python format
    
    * optim

---
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/keras/optimizers/OptimizersSpec.scala
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2018 Analytics Zoo Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.intel.analytics.zoo.pipeline.api.keras.optimizers
+
+import com.intel.analytics.bigdl.nn.{BCECriterion, Linear, Sequential, Sigmoid}
+import com.intel.analytics.bigdl.optim.SGD.Plateau
+import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric.NumericFloat
+import com.intel.analytics.bigdl.utils.Engine
+import com.intel.analytics.bigdl.utils.RandomGenerator.RNG
+import com.intel.analytics.zoo.common.NNContext
+import com.intel.analytics.zoo.pipeline.nnframes.{NNClassifier, NNClassifierModel, NNEstimatorSpec}
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+import scala.util.Random
+
+class OptimizersSpec extends FlatSpec with Matchers with BeforeAndAfter {
+
+  var sc: SparkContext = _
+  var sqlContext: SQLContext = _
+  var smallData: Seq[(Array[Double], Double)] = _
+  val nRecords = 100
+
+  before {
+    val conf = Engine.createSparkConf().setAppName("OptimizersSpec").setMaster("local[1]")
+    sc = NNContext.initNNContext(conf)
+    sqlContext = new SQLContext(sc)
+    smallData = NNEstimatorSpec.generateTestInput(
+      nRecords, Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0), -1.0, 42L)
+    Random.setSeed(42)
+    RNG.setSeed(42)
+
+    Engine.init
+  }
+
+  after {
+    if (sc != null) {
+      sc.stop()
+    }
+  }
+
+  "Adam" should "support learning rate schedule" in {
+    val model = new Sequential().add(Linear[Float](6, 10)).add(Linear[Float](10, 1))
+      .add(Sigmoid[Float]())
+    val criterion = BCECriterion[Float]()
+    val classifier = NNClassifier(model, criterion, Array(6))
+      .setOptimMethod(new Adam[Float](
+        lr = 0.01,
+        schedule = Plateau("Loss", 0.99f, 1, "min", 0.01f, 0, 1e-15f)
+      ))
+      .setBatchSize(10)
+      .setMaxEpoch(10)
+    val data = sc.parallelize(smallData.map(t => (t._1, t._2 - 1.0)))
+    val df = sqlContext.createDataFrame(data).toDF("features", "label")
+
+    val nnModel = classifier.fit(df)
+    nnModel.isInstanceOf[NNClassifierModel[_]] should be(true)
+    val correctCount = nnModel.transform(df).where("prediction=label").count()
+    assert(correctCount > nRecords * 0.8)
+  }
+
+}
