c8133a03a9c8615210c6dfa682baaf36405d005d
Xin Qiu <qiuxin2012@users.noreply.github.com>
2019-01-16 19:34:08 +0800

    remove pmem so (#1071)
    * add pmem so and loader
    
    * meet code review

--- scala/dllib/src/main/java/com/intel/analytics/bigdl/dllib/feature/pmem/PersistentMemoryAllocator.java
+++ scala/dllib/src/main/java/com/intel/analytics/bigdl/dllib/feature/pmem/PersistentMemoryAllocator.java
@@ -20,7 +20,7 @@ public class PersistentMemoryAllocator implements BasicMemoryAllocator {
     private static volatile PersistentMemoryAllocator instance;
 
     private PersistentMemoryAllocator() {
-        new com.intel.analytics.zoo.feature.pmem.NativeLoader().init();
+        com.intel.analytics.zoo.core.pmem.PMemNativeLoader.load();
     }
 
     public static PersistentMemoryAllocator getInstance() {
