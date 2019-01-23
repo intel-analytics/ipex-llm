9f4ac97708ab4994efae774d3d1b3a19b306716e
Kai Huang <huangkaivision@gmail.com>
2019-01-23 13:26:35 +0800

    Update doc for metrics in compile (#1105)
    * update compile metrics doc
    
    * update

--- python/dllib/src/bigdl/dllib/keras/engine/topology.py
+++ python/dllib/src/bigdl/dllib/keras/engine/topology.py
@@ -37,7 +37,9 @@ class KerasNet(ZooKerasLayer):
         loss: Criterion to be used. One can alternatively pass in the corresponding string
               representation, such as 'mse'.
         metrics: List of validation methods to be used. Default is None if no validation is needed.
-                 One can alternatively use ['accuracy'].
+                 For convenience, string representations are supported: 'accuracy' (or 'acc'),
+                 'top5accuracy' (or 'top5acc'), 'mae', 'auc', 'treennaccuracy' and 'loss'.
+                 For example, you can either use [Accuracy()] or ['accuracy'].
         """
         if isinstance(optimizer, six.string_types):
             optimizer = to_bigdl_optim_method(optimizer)
