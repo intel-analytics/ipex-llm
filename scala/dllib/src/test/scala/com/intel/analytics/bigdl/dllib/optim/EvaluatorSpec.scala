c8ebd42736506ec33d043c59bf83812c3f37a632
zhangxiaoli73 <380761639@qq.com>
2019-03-12 13:21:26 +0800

    refactor predict for dnn model (#2737)
    * refactor predict for dnn model

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/optim/EvaluatorSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/optim/EvaluatorSpec.scala
@@ -16,13 +16,12 @@
 
 package com.intel.analytics.bigdl.optim
 
-import com.intel.analytics.bigdl.dataset.{DataSet, Sample}
+import com.intel.analytics.bigdl.dataset.{DataSet, MiniBatch, Sample}
 import com.intel.analytics.bigdl.models.lenet.LeNet5
 import com.intel.analytics.bigdl.nn.CrossEntropyCriterion
 import com.intel.analytics.bigdl.tensor.Tensor
-import com.intel.analytics.bigdl.utils.Engine
+import com.intel.analytics.bigdl.utils.{Engine, MklBlas, MklDnn, SparkContextLifeCycle}
 import com.intel.analytics.bigdl.utils.RandomGenerator._
-import com.intel.analytics.bigdl.utils.SparkContextLifeCycle
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 import com.intel.analytics.bigdl._
@@ -49,6 +48,30 @@ class EvaluatorSpec extends SparkContextLifeCycle with Matchers {
       val input = Tensor[Float](28, 28).fill(0.8f)
       val label = Tensor[Float](1).fill(1.0f)
       tmp(i) = Sample(input, label)
+      i += 1
+    }
+    val model = LeNet5(classNum = 10)
+    val dataSet = DataSet.array(tmp, sc).toDistributed().data(train = false)
+
+    val result = model.evaluate(dataSet, Array(new Top1Accuracy[Float](), new Top5Accuracy[Float](),
+      new Loss[Float](CrossEntropyCriterion[Float]())))
+
+    result(0)._1 should be (new AccuracyResult(0, 100))
+    result(1)._1 should be (new AccuracyResult(100, 100))
+    result(2)._1 should be (new LossResult(57.610695f, 25))
+    result(0)._1.result()._1 should be (0f)
+    result(1)._1.result()._1 should be (1f)
+    result(2)._1.result()._1 should be (2.3044279f+-0.000001f)
+  }
+
+  "Evaluator MiniBatch" should "be correct" in {
+    RNG.setSeed(100)
+    val tmp = new Array[MiniBatch[Float]](25)
+    var i = 0
+    while (i < tmp.length) {
+      val input = Tensor[Float](4, 28, 28).fill(0.8f)
+      val label = Tensor[Float](4).fill(1.0f)
+      tmp(i) = MiniBatch(input, label)
       i += 1
     }
     val model = LeNet5(classNum = 10)
