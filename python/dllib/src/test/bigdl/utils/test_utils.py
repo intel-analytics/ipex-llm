a9637b4485238b11ab73bf76173da48646d0d67e
Yang Wang <tiandiwoxin@icloud.com>
2019-03-27 18:45:24 +0800

    support tfdataset without batch (#1249)
    * support tfdataset without batch
    
    * address comments
    
    * fix style
    
    * add test code
    
    * fix style
    
    * fix bug

--- python/dllib/src/test/bigdl/utils/test_utils.py
+++ python/dllib/src/test/bigdl/utils/test_utils.py
@@ -39,8 +39,8 @@ class ZooTestCase(TestCase):
         K.set_image_dim_ordering("th")
         sparkConf = init_spark_conf().setMaster("local[4]").setAppName("zoo test case")
         assert str(sparkConf.get("spark.shuffle.reduceLocality.enabled")) == "false"
-        assert str(sparkConf.get("spark.serializer")) \
-            == "org.apache.spark.serializer.JavaSerializer"
+        assert \
+            str(sparkConf.get("spark.serializer")) == "org.apache.spark.serializer.JavaSerializer"
         assert SparkContext._active_spark_context is None
         self.sc = init_nncontext(sparkConf)
         self.sc.setLogLevel("ERROR")
@@ -84,7 +84,7 @@ class ZooTestCase(TestCase):
             np.testing.assert_allclose(a, b, rtol=rtol, atol=atol, err_msg=msg)
 
     def assert_list_allclose(self, a, b, rtol=1e-6, atol=1e-6, msg=None):
-        for(i1, i2) in zip(a, b):
+        for (i1, i2) in zip(a, b):
             self.assert_allclose(i1, i2, rtol, atol, msg)
 
     def compare_loss(self, y_a, y_b, kloss, zloss, rtol=1e-6, atol=1e-6):
@@ -195,3 +195,17 @@ class ZooTestCase(TestCase):
         if model1.get_weights():
             model2.set_weights(model1.get_weights())
         self.compare_output_and_grad_input(model1, model2, input_data, rtol, atol)
+
+    def intercept(self, func, error_message):
+
+        error = False
+        try:
+            func()
+        except Exception as e:
+            if error_message not in str(e):
+                raise Exception("error_message not in the exception raised. " +
+                                "error_message: %s, exception: %s" % (error_message, e))
+            error = True
+
+        if not error:
+            raise Exception("exception is not raised")
