70cbc70d579038683dc6cac0087549f16e637f51
dding3 <ding.ding@intel.com>
2019-05-03 11:44:40 -0700

    Suuport BertAdam (#1302)
    * add scala and python adamweightdecay

--- python/dllib/src/bigdl/dllib/keras/optimizers.py
+++ python/dllib/src/bigdl/dllib/keras/optimizers.py
@@ -61,3 +61,47 @@ class Adam(OptimMethod, ZooKerasCreator):
             schedule if (schedule) else Default()
         )
         self.bigdl_type = bigdl_type
+
+
+class AdamWeightDecay(OptimMethod, ZooKerasCreator):
+    """
+    >>> adam = AdamWeightDecay()
+    creating: createZooKerasAdamWeightDecay
+    """
+    def __init__(self,
+                 lr=1e-3,
+                 warmup_portion=-1.0,
+                 total=-1,
+                 schedule="linear",
+                 beta1=0.9,
+                 beta2=0.999,
+                 epsilon=1e-6,
+                 weight_decay=0.01,
+                 bigdl_type="float"):
+        """
+        :param lr learning rate
+        :param warmupPortion portion of total for the warmup, -1 means no warmup. Default: -1
+        :param total total number of training steps for the learning
+         rate schedule, -1 means constant learning rate. Default: -1
+        :param schedule schedule to use for the warmup. Default: 'linear'
+        :param beta1 first moment coefficient
+        :param beta2 second moment coefficient
+        :param epsilon for numerical stability
+        :param weightDecay weight decay
+        """
+
+        # explicitly reimplement the constructor since:
+        # 1. This class need to be a subclass of OptimMethod
+        # 2. The constructor of OptimMethod invokes JavaValue.jvm_class_constructor() directly
+        #    and does not take the polymorphism.
+        self.value = callBigDlFunc(
+            bigdl_type, ZooKerasCreator.jvm_class_constructor(self),
+            lr,
+            warmup_portion,
+            total,
+            schedule,
+            beta1,
+            beta2,
+            epsilon,
+            weight_decay)
+        self.bigdl_type = bigdl_type
