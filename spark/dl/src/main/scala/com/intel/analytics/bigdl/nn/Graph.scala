9b56957645c727eef3c1018ac2d5258b1df27b2a
LeicongLi <leicongli@gmail.com>
2019-05-07 12:18:22 +0800

    Update Graph.scala

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/Graph.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/Graph.scala
@@ -39,9 +39,6 @@ import scala.collection.JavaConverters._
 import org.tensorflow.framework.GraphDef
 
 
-
-
-
 /**
  * A graph container. The modules in the container are connected as a directed Graph. Each module
  * can output one tensor or multiple tensors(as table). The edges between modules in the graph
