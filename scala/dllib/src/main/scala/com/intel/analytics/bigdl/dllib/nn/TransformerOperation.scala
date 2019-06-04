7f1b5a5056471ed034d9251cbd64e8de48bc4fc5
zhangxiaoli73 <380761639@qq.com>
2019-06-04 16:55:47 +0800

    [New feature] add transformer layer (#2825)
    * add transformer
    
    * refactor class name
    
    * use same embedding for translation
    
    * fix pr comments

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/TransformerOperation.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/nn/TransformerOperation.scala
@@ -20,6 +20,8 @@ import com.intel.analytics.bigdl.nn.abstractnn.{AbstractModule, TensorModule}
 import com.intel.analytics.bigdl.optim.Regularizer
 import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
+import com.intel.analytics.bigdl.tensor.{Storage, Tensor}
+import com.intel.analytics.bigdl.utils.{EngineType, T}
 
 import scala.reflect.ClassTag
 
@@ -55,4 +57,96 @@ private[nn] object TransformerOperation {
     model.add(Transpose[T](Array((2, 4))))
     model.asInstanceOf[AbstractModule[Tensor[T], Tensor[T], T]]
   }
+
+  /**
+   * Calculate bias tensor from padding values in tensor.
+   * Bias tensor that is added to the pre-softmax multi-headed attention logits,
+   * which has shape [batch_size, num_heads, length, length]. The tensor is zero at
+   * non-padding locations, and -1e9 (negative infinity) at padding locations.
+   * Args: x: int tensor with shape [batch_size, length]
+   * Returns: Attention bias tensor of shape [batch_size, 1, 1, length].
+   * @param input
+   * @tparam T
+   * @return
+   */
+  def getPaddingBias[T: ClassTag](input: Tensor[T])(implicit ev: TensorNumeric[T]): Tensor[T] = {
+    val res = getPadding[T](input).mul(ev.fromType(-1e9))
+    res.addSingletonDimension(res, 2)
+    res.addSingletonDimension(res, 3)
+  }
+
+  /**
+   * Return float tensor representing the padding values in x.
+   * Args:
+   * x: int tensor with any shape
+   * padding_value: int value that
+   * Returns:float tensor with same shape as x containing values 0 or 1.
+   *   0 -> non-padding, 1 -> padding
+   */
+  def getPadding[T: ClassTag](input: Tensor[T], paddingValue: Float = 0.0f)
+                             (implicit ev: TensorNumeric[T]): Tensor[T] = {
+    input.apply1(e => {if (e == paddingValue) ev.one else ev.zero})
+  }
+
+  // Shift the second dimension of x right by one.
+  def shiftRight3D[T: ClassTag](input: Tensor[T], output: Tensor[T])
+    (implicit ev: TensorNumeric[T]): Tensor[T] = {
+    output.resizeAs(input).zero()
+    val index = input.size(2)
+    output.narrow(2, 2, index - 1).copy(input.narrow(2, 1, index - 1))
+    output
+  }
+
+  def initRangeTensor[T: ClassTag](length: Int, rangeBuffer: Tensor[T])
+    (implicit ev: TensorNumeric[T]): Unit = {
+    rangeBuffer.resize(Array(length, 2))
+    val arr = rangeBuffer.select(2, 1).storage().array()
+    for (i <- 0 to (length - 1)) {
+      arr(i * 2) = ev.fromType(i)
+      arr(i * 2 + 1) = ev.fromType(i)
+    }
+  }
+
+  /**
+   * Args:length: Sequence length.
+   * channels: Size of the hidden
+   * min_timescale: Minimum scale that will be applied at each position
+   * max_timescale: Maximum scale that will be applied at each position
+   * Returns: Tensor with shape [length, hidden_size]
+   */
+  def addTimingSignal1D[T: ClassTag](
+    length: Int,
+    channels: Int,
+    min_timescale : Float = 1.0f,
+    max_timescale: Float = 1.0e4f,
+    rangeBuffer: Tensor[T],
+    timeBuffer: Tensor[T])(implicit ev: TensorNumeric[T]): Tensor[T] = {
+    // get_timing_signal_1d, return (1, length, channels)
+    val num_timescales = channels / 2
+    val log_timescale_increment = math.log(max_timescale / min_timescale) /
+      math.max(num_timescales - 1, 1)
+    // tf.range(num_timescales)
+    val inv_timescales = new Array[Double](num_timescales)
+    var i = 0
+    while (i < inv_timescales.length) {
+      inv_timescales(i) = min_timescale * math.exp(i * - log_timescale_increment)
+      i += 1
+    }
+    rangeBuffer.select(2, 1).mul(ev.fromType[Double](inv_timescales(0)))
+    rangeBuffer.select(2, 2).mul(ev.fromType[Double](inv_timescales(1)))
+
+    val sinRes = rangeBuffer.clone().apply1(e =>
+      ev.fromType(math.sin(ev.toType[Float](e))))
+    val cosRes = rangeBuffer.clone().apply1(e =>
+      ev.fromType(math.cos(ev.toType[Float](e))))
+
+    timeBuffer.narrow(2, 1, sinRes.size(2)).copy(sinRes)
+    timeBuffer.narrow(2, sinRes.size(2) + 1, cosRes.size(2)).copy(cosRes)
+    timeBuffer
+  }
 }
+
+sealed trait TransformerType
+
+case object Translation extends TransformerType
+case object LanguageModel extends TransformerType
