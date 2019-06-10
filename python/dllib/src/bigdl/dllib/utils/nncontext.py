72ed5949ca66a7cc59902445f5775de449085bfc
li,zhichao <zhichao.li@intel.com>
2019-06-10 15:06:39 +0800

    Support init_spark_on_yarn and RayContext (#1344)
    * rayrunner
    
    * add a jvm killer
    
    * disable killer from spark job and rely on jvm killer only
    
    * add env and verify the cv2 installation
    
    * enhance
    
    * minor
    
    * style
    
    * comments
    
    * local and enhancement
    
    * better local strategy
    
    * doc and style
    
    * doc
    
    * style
    
    * revert
    
    * doc
    
    * disable
    
    * comments
    
    * fix test

--- python/dllib/src/bigdl/dllib/utils/nncontext.py
+++ python/dllib/src/bigdl/dllib/utils/nncontext.py
@@ -20,7 +20,82 @@ import multiprocessing
 import os
 
 
-def init_nncontext(conf=None):
+def init_spark_on_local(cores=2, conf=None, python_location=None, spark_log_level="WARN",
+                        redirect_spark_log=True):
+    """
+    Create a SparkContext with Zoo configuration in local machine.
+    :param cores: The default value is 2 and you can also set it to *
+     meaning all of the available cores. i.e `init_on_local(cores="*")`
+    :param conf: A key value dictionary appended to SparkConf.
+    :param python_location: The path to your running python executable.
+    :param spark_log_level: Log level of Spark
+    :param redirect_spark_log: Redirect the Spark log to local file or not.
+    :return:
+    """
+    from zoo.ray.util.spark import SparkRunner
+    sparkrunner = SparkRunner(spark_log_level=spark_log_level,
+                              redirect_spark_log=redirect_spark_log)
+    return sparkrunner.init_spark_on_local(cores=cores, conf=conf,
+                                           python_location=python_location)
+
+
+def init_spark_on_yarn(hadoop_conf,
+                       conda_name,
+                       num_executor,
+                       executor_cores,
+                       executor_memory="2g",
+                       driver_memory="1g",
+                       driver_cores=4,
+                       extra_executor_memory_for_ray=None,
+                       extra_python_lib=None,
+                       penv_archive=None,
+                       hadoop_user_name="root",
+                       spark_yarn_archive=None,
+                       spark_log_level="WARN",
+                       redirect_spark_log=True):
+    """
+    Create a SparkContext with Zoo configuration on Yarn cluster on "Yarn-client" mode.
+    You should create a conda env and install the python dependencies in that env.
+    Conda env and the python dependencies only need to be installed in the driver machine.
+    It's not necessary create and install those on the whole yarn cluster.
+
+    :param hadoop_conf: path to the yarn configuration folder.
+    :param conda_name: Name of the conda env.
+    :param num_executor: Number of the Executors.
+    :param executor_cores: Cores for each Executor.
+    :param executor_memory: Memory for each Executor.
+    :param driver_memory: Memory for the Driver.
+    :param driver_cores: Number of cores for the Driver.
+    :param extra_executor_memory_for_ray: Memory size for the Ray services.
+    :param extra_python_lib:
+    :param penv_archive: Ideally, program would auto-pack the conda env which is specified by
+           `conda_name`, but you can also pass the path to a packed file in "tar.gz" format here.
+    :param hadoop_user_name: User name for running in yarn cluster. Default value is: root
+    :param spark_log_level: Log level of Spark
+    :param redirect_spark_log:
+    :return: Direct the Spark log to local file or not.
+    """
+    from zoo.ray.util.spark import SparkRunner
+    sparkrunner = SparkRunner(spark_log_level=spark_log_level,
+                              redirect_spark_log=redirect_spark_log)
+    sc = sparkrunner.init_spark_on_yarn(
+        hadoop_conf=hadoop_conf,
+        conda_name=conda_name,
+        num_executor=num_executor,
+        executor_cores=executor_cores,
+        executor_memory=executor_memory,
+        driver_memory=driver_memory,
+        driver_cores=driver_cores,
+        extra_executor_memory_for_ray=extra_executor_memory_for_ray,
+        extra_python_lib=extra_python_lib,
+        penv_archive=penv_archive,
+        hadoop_user_name=hadoop_user_name,
+        spark_yarn_archive=spark_yarn_archive,
+        jars=None)
+    return sc
+
+
+def init_nncontext(conf=None, redirect_spark_log=True):
     """
     Creates or gets a SparkContext with optimized configuration for BigDL performance.
     The method will also initialize the BigDL engine.
@@ -36,8 +111,9 @@ def init_nncontext(conf=None):
     else:
         sc = getOrCreateSparkContext(conf=conf)
     check_version()
-    redire_spark_logs()
-    show_bigdl_info_logs()
+    if redirect_spark_log:
+        redire_spark_logs()
+        show_bigdl_info_logs()
     init_engine()
     return sc
 
