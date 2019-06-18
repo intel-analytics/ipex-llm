94a9a75c05903f0474652e13a5b5b6720fa806c4
Dongjie Shi <glorysdj@gmail.com>
2019-06-18 16:44:40 +0800

    update docker to support spark master (#1448)

--- docker/zoo/start-notebook.sh
+++ docker/zoo/start-notebook.sh
@@ -30,6 +30,7 @@ echo $SPARK_MAJOR_VERSION
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir=$ANALYTICS_ZOO_TUTORIALS_HOME --ip=0.0.0.0 --port=$NotebookPort --no-browser --NotebookApp.token=$NotebookToken --allow-root"
 
+echo $RUNTIME_SPARK_MASTER
 echo $RUNTIME_EXECUTOR_CORES
 echo $RUNTIME_DRIVER_CORES
 echo $RUNTIME_DRIVER_MEMORY
@@ -37,8 +38,34 @@ echo $RUNTIME_EXECUTOR_CORES
 echo $RUNTIME_EXECUTOR_MEMORY
 echo $RUNTIME_TOTAL_EXECUTOR_CORES
 
+if [ -z "${KMP_AFFINITY}" ]; then
+    export KMP_AFFINITY=granularity=fine,compact,1,0
+fi
+
+if [ -z "${OMP_NUM_THREADS}" ]; then
+    if [ -z "${ZOO_NUM_MKLTHREADS}" ]; then
+        export OMP_NUM_THREADS=1
+    else
+        if [ `echo $ZOO_NUM_MKLTHREADS | tr '[A-Z]' '[a-z]'` == "all" ]; then
+            export OMP_NUM_THREADS=`nproc`
+        else
+            export OMP_NUM_THREADS=${ZOO_NUM_MKLTHREADS}
+        fi
+    fi
+fi
+
+if [ -z "${KMP_BLOCKTIME}" ]; then
+    export KMP_BLOCKTIME=0
+fi
+
+# verbose for OpenMP
+if [[ $* == *"verbose"* ]]; then
+    export KMP_SETTINGS=1
+    export KMP_AFFINITY=${KMP_AFFINITY},verbose
+fi
+
 ${SPARK_HOME}/bin/pyspark \
-  --master local[${RUNTIME_EXECUTOR_CORES}] \
+  --master ${RUNTIME_SPARK_MASTER} \
   --driver-cores ${RUNTIME_DRIVER_CORES} \
   --driver-memory ${RUNTIME_DRIVER_MEMORY} \
   --executor-cores ${RUNTIME_EXECUTOR_CORES} \
@@ -49,4 +76,5 @@ ${SPARK_HOME}/bin/pyspark \
   --jars ${ANALYTICS_ZOO_HOME}/lib/analytics-zoo-bigdl_${BIGDL_VERSION}-spark_${SPARK_VERSION}-${ANALYTICS_ZOO_VERSION}-jar-with-dependencies.jar \
   --conf spark.driver.extraClassPath=${ANALYTICS_ZOO_HOME}/lib/analytics-zoo-bigdl_${BIGDL_VERSION}-spark_${SPARK_VERSION}-${ANALYTICS_ZOO_VERSION}-jar-with-dependencies.jar \
   --conf spark.executor.extraClassPath=${ANALYTICS_ZOO_HOME}/lib/analytics-zoo-bigdl_${BIGDL_VERSION}-spark_${SPARK_VERSION}-${ANALYTICS_ZOO_VERSION}-jar-with-dependencies.jar \
-  --conf spark.driver.extraJavaOptions=-Dderby.stream.error.file=/tmp
+  --conf spark.driver.extraJavaOptions=-Dderby.stream.error.file=/tmp \
+  --conf spark.sql.catalogImplementation='in-memory'
