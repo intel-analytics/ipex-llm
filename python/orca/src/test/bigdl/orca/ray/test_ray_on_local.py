3ce1e40429f86b4340ec29c213e4b5cb8b4a295b
Jieru Hong <30741238+respecteverything@users.noreply.github.com>
2019-08-15 10:02:37 +0800

    modify run-pytests to check version before test ray (#1532)
    * Create .keep
    
    * update .keep path
    
    * add rl_pong example
    
    * move to rl_pong direction
    
    * remove original file
    
    * add parameter server example
    
    * add license
    
    * PEP8 checks
    
    * PEP8 checks
    
    * Add into integration test
    
    * Wrap tests into bash function
    
    * Update license
    
    * PEP8 checks
    
    * Correct syntax of rl_pong
    
    * modify run-pytests to check version before test ray
    
    * test pyspark version and spark home
    
    * add check spark_home's pyspark in case pyspark can't be found
    
    * add check version before run ray examples
    
    * change spark home
    
    * change spark home
    
    * install packages which are needed in ray examples
    
    * check error
    
    * fix error
    
    * change execution to spark-submit
    
    * change memory
    
    * change object memory to test
    
    * add atari_py dependency
    
    * remove .keep
    
    * move ray test to new files
    
    * change some ray-pip lines into function
    
    * remove rl_pong and fix parameter_server iterations
    
    * add iteration
    
    * change iterate, print info
    
    * add more info
    
    * add __init__ files
    
    * change ray to rayexample to avoid conflict and change spark-submit to python to submit tasks
    
    * renamed foreach_evaluator to foreach_worker because rllib update and rename file rllib to rllibexample
    
    * add a dedicated file for the ray test
    
    * PEP8 check fix
    
    * PEP8 check fix
    
    * remove test_split
    
    * remove --doctest-modules about ray
    
    * add time.sleep

--- python/orca/src/test/bigdl/orca/ray/test_ray_on_local.py
+++ python/orca/src/test/bigdl/orca/ray/test_ray_on_local.py
@@ -19,6 +19,7 @@ import numpy as np
 import psutil
 import pytest
 import ray
+import time
 
 from zoo import init_spark_on_local
 from zoo.ray.util.raycontext import RayContext
@@ -44,6 +45,7 @@ class TestUtil(TestCase):
         print([ray.get(actor.hostname.remote()) for actor in actors])
         ray_ctx.stop()
         sc.stop()
+        time.sleep(1)
         for process_info in ray_ctx.ray_processesMonitor.process_infos:
             for pid in process_info.pids:
                 assert not psutil.pid_exists(pid)
