4cf2c9324dd6076e725330eeb0a560e8a8a396a4
Leicong Li <leicong.li@intel.com>
2019-08-16 18:15:40 +0800

    add shape layer

--- pyspark/bigdl/nn/layer.py
+++ pyspark/bigdl/nn/layer.py
@@ -5691,7 +5691,7 @@ def _test():
     import doctest
     from pyspark import SparkContext
     from bigdl.nn import layer
-    from bigdl.nn.onnx.layer import *
+    import bigdl.nn.onnx.layer as onnx_layer
     from bigdl.util.common import init_engine
     from bigdl.util.common import create_spark_conf
     globs = layer.__dict__.copy()
