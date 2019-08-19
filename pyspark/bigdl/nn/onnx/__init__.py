e5a4718a3009b6f16ef03c651032c68b9fefdd0c
Leicong Li <leicong.li@intel.com>
2019-08-19 16:09:25 +0800

    add shape layer

--- pyspark/bigdl/nn/onnx/__init__.py
+++ pyspark/bigdl/nn/onnx/__init__.py
@@ -13,3 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from bigdl.util.common import *
+
+init_engine()
+redire_spark_logs()
+show_bigdl_info_logs()
