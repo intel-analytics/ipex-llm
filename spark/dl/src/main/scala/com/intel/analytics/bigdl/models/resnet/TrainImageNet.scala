37ca80194520fdd2e2d1b3c7adeeb10c58569578
zhangxiaoli73 <380761639@qq.com>
2019-08-26 10:40:42 +0800

    fix memory leak for ir graph training (#2895)

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/models/resnet/TrainImageNet.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/models/resnet/TrainImageNet.scala
@@ -68,31 +68,23 @@ object TrainImageNet {
       val model = if (param.modelSnapshot.isDefined) {
         Module.load[Float](param.modelSnapshot.get)
       } else {
-        Engine.getEngineType() match {
-          case MklBlas =>
-            val curModel =
-              ResNet(classNum = param.classes, T("shortcutType" -> shortcut, "depth" -> param.depth,
-                "optnet" -> param.optnet, "dataSet" -> dataSetType))
-            if (param.optnet) {
-              ResNet.shareGradInput(curModel)
-            }
-            ResNet.modelInit(curModel)
-
-            /* Here we set parallism specificall for BatchNormalization and its Sub Layers, this is
-            very useful especially when you want to leverage more computing resources like you want
-            to use as many cores as possible but you cannot set batch size too big for each core due
-            to the memory limitation, so you can set batch size per core smaller, but the smaller
-            batch size will increase the instability of convergence, the synchronization among BN
-            layers basically do the parameters synchronization among cores and thus will avoid the
-            instability while improves the performance a lot. */
-            val parallisim = Engine.coreNumber
-            setParallism(curModel, parallisim)
-
-            curModel
-          case MklDnn =>
-            nn.mkldnn.ResNet.graph(param.batchSize / Engine.nodeNumber(), param.classes,
-              T("depth" -> 50, "dataSet" -> ImageNet))
+        val curModel =
+          ResNet(classNum = param.classes, T("shortcutType" -> shortcut, "depth" -> param.depth,
+            "optnet" -> param.optnet, "dataSet" -> dataSetType))
+        if (param.optnet) {
+          ResNet.shareGradInput(curModel)
         }
+        ResNet.modelInit(curModel)
+
+        /* Here we set parallism specificall for BatchNormalization and its Sub Layers, this is
+        very useful especially when you want to leverage more computing resources like you want
+        to use as many cores as possible but you cannot set batch size too big for each core due
+        to the memory limitation, so you can set batch size per core smaller, but the smaller
+        batch size will increase the instability of convergence, the synchronization among BN
+        layers basically do the parameters synchronization among cores and thus will avoid the
+        instability while improves the performance a lot. */
+        if (Engine.getEngineType() == MklBlas) setParallism(curModel, Engine.coreNumber)
+        curModel
       }
 
       println(model)
