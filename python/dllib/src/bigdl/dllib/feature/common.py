aa2430f8f979a174f1a69a1bbd369fa81abfb71f
cyita <33650826+cyita@users.noreply.github.com>
2019-09-10 15:00:09 +0800

    Add Estimator Python API and Inception Example (#1597)

--- python/dllib/src/bigdl/dllib/feature/common.py
+++ python/dllib/src/bigdl/dllib/feature/common.py
@@ -246,15 +246,37 @@ class FeatureSet(DataSet):
     def image_set(cls, imageset, memory_type="DRAM", bigdl_type="float"):
         """
         Create FeatureSet from ImageFrame.
-        :param image_frame: ImageFrame
+        :param imageset: ImageSet
         :param memory_type: string, DRAM or PMEM
                             If it's DRAM, will cache dataset into dynamic random-access memory
                             If it's PMEM, will cache dataset into Intel Optane DC Persistent Memory
+                            If it's a Int number n, will cache dataset into disk, and only hold 1/n
+                              of the data into memory during the training. After going through the
+                              1/n, we will release the current cache, and load another 1/n into
+                              memory.
         :param bigdl_type: numeric type
         :return: A feature set
         """
         jvalue = callBigDlFunc(bigdl_type, "createFeatureSetFromImageFrame",
                                imageset.to_image_frame(), memory_type)
+        return cls(jvalue=jvalue)
+
+    @classmethod
+    def sample_rdd(cls, rdd, memory_type="DRAM", bigdl_type="float"):
+        """
+        Create FeatureSet from RDD[Sample].
+        :param rdd: A RDD[Sample]
+        :param memory_type: string, DRAM or PMEM
+                            If it's DRAM, will cache dataset into dynamic random-access memory
+                            If it's PMEM, will cache dataset into Intel Optane DC Persistent Memory
+                            If it's a Int number n, will cache dataset into disk, and only hold 1/n
+                              of the data into memory during the training. After going through the
+                              1/n, we will release the current cache, and load another 1/n into
+                              memory.
+        :param bigdl_type:numeric type
+        :return: A feature set
+        """
+        jvalue = callBigDlFunc(bigdl_type, "createSampleFeatureSetFromRDD", rdd, memory_type)
         return cls(jvalue=jvalue)
 
     @classmethod
