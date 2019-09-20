4048d882cf21920b764e2df230370c3bf3969bff
Yuhao Yang <yuhao.yang@intel.com>
2019-09-19 17:13:36 -0700

    NNFrames: support caching training data on Disk (#1588)
    * disk cache for NNFrames
    
    * update doc and example
    
    * update train method
    
    * fix style
    
    * fix ut
    
    * fix ut

--- python/dllib/src/test/bigdl/nnframes/test_nn_classifier.py
+++ python/dllib/src/test/bigdl/nnframes/test_nn_classifier.py
@@ -185,6 +185,29 @@ class TestNNClassifer():
         estimator.setGradientClippingByL2Norm(1.2)
         estimator.fit(df)
 
+    def test_nnEstimator_fit_with_Cache_Disk(self):
+        model = Sequential().add(Linear(2, 2))
+        criterion = MSECriterion()
+        estimator = NNEstimator(model, criterion, SeqToTensor([2]), ArrayToTensor([2])) \
+            .setBatchSize(1).setLearningRate(0.2).setMaxEpoch(2) \
+            .setDataCacheLevel("DISK_AND_DRAM", 2)
+
+        data = self.sc.parallelize([
+            ((2.0, 1.0), (1.0, 2.0)),
+            ((1.0, 2.0), (2.0, 1.0)),
+            ((2.0, 1.0), (1.0, 2.0)),
+            ((1.0, 2.0), (2.0, 1.0)),
+            ((2.0, 1.0), (1.0, 2.0)),
+            ((1.0, 2.0), (2.0, 1.0)),
+            ((2.0, 1.0), (1.0, 2.0)),
+            ((1.0, 2.0), (2.0, 1.0))])
+
+        schema = StructType([
+            StructField("features", ArrayType(DoubleType(), False), False),
+            StructField("label", ArrayType(DoubleType(), False), False)])
+        df = self.sqlContext.createDataFrame(data, schema)
+        estimator.fit(df)
+
     def test_nnEstimator_fit_with_non_default_featureCol(self):
         model = Sequential().add(Linear(2, 2))
         criterion = MSECriterion()
