1053fa96629cfc351dbd4b9877ab160a0dfb3593
Leicong Li <leicong.li@intel.com>
2019-09-25 18:49:35 +0800

    onnx dev

---
+++ pyspark/bigdl/contrib/onnx2bigdl/translation_utils.py
@@ -0,0 +1,24 @@
+
+import math
+import numpy as np
+
+def calc_output_shape(input, kernel, padding = 0, stride = 1, dilation = 1, ceil_mode = False):
+	def dilated_kernel_size(kernel, dilation):
+		return kernel + (kernel - 1) * (dilation - 1)
+	rounding = math.ceil if ceil_mode else math.floor
+	out = (input + 2 * padding - dilated_kernel_size(kernel, dilation)) / stride + 1
+	out = int(rounding(out))
+	print(input, kernel, padding, stride, dilation, ceil_mode, out)
+	return out
+
+def parse_tensor_data(tensor_proto):
+	try:
+		from onnx.numpy_helper import to_array
+	except ImportError:
+		raise ImportError("Onnx and protobuf need to be installed.")
+	if len(tuple(tensor_proto.dims)) > 0:
+		np_array = to_array(tensor_proto).reshape(tuple(tensor_proto.dims))
+	else:
+		# If it is a scalar tensor
+		np_array = np.array([to_array(tensor_proto)])
+	return np_array
