021c1df2e76c658cf7171152039a887e314ebdf5
Menooker <Menooker@users.noreply.github.com>
2019-09-25 15:41:09 +0800

    Update RoiLabel class and add RoiImageFeatureToBatch (#2913)
    * add MeanAveragePrecision validation method
    
    * Add MAP basic code for object detection
    
    * update tests
    
    * bug fixes based on results of former MAP validation method
    
    * update documents
    
    * add python binding
    
    * typo fix, style change, change calculateAP to private
    
    * update comments
    
    * update RoiLabel, add RoiImageFeatureToBatch
    
    * fix typo in class name
    
    * updates by suggestions
    
    * minor updates
    
    * Move RoiMiniBatch to MTImageFeatureToBatch.scala
    
    * mask in RoiLabel now have Floats not Bytes
    
    * use IndexedSeq for RoiLabel
    
    * style fix
    
    * add isCrowd and origSize to final target table
    
    * style fix
    
    * isCrowd change to float, add doc
    
    * add tests and bug fixes
    
    * add util getting RoiLabels from ImageFeatures
    
    * add util getting RoiLabels from Table
    
    * comment out the tests
    
    * rename utils in RoiLabel

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/transform/vision/image/label/roi/RoiLabel.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/transform/vision/image/label/roi/RoiLabel.scala
@@ -25,26 +25,41 @@ import com.intel.analytics.bigdl.utils.{T, Table}
  * @param classes N (class labels) or 2 * N, the first row is class labels,
  * the second line is difficults
  * @param bboxes N * 4
+ * @param masks the array of annotation masks of the targets
  */
-case class RoiLabel(classes: Tensor[Float], bboxes: Tensor[Float]) {
+case class RoiLabel(classes: Tensor[Float], bboxes: Tensor[Float],
+  masks: Array[Tensor[Float]] = null) {
   def copy(target: RoiLabel): Unit = {
     classes.resizeAs(target.classes).copy(target.classes)
     bboxes.resizeAs(target.bboxes).copy(target.bboxes)
+    require(target.masks == null, "Copying RoiLabels with masks not supported")
   }
 
   if (classes.dim() == 1) {
-    require(classes.size(1) == bboxes.size(1), "the number of classes should be" +
-      " equal to the number of bounding box numbers")
+    require(classes.size(1) == bboxes.size(1), s"the number of classes ${classes.size(1)} should " +
+      s"be equal to the number of bounding box numbers ${bboxes.size(1)}")
+    if (masks != null) {
+      require(classes.size(1) == masks.length, s"the number of classes ${classes.size(1)} should " +
+        s"be equal to the number of mask array ${masks.length}")
+    }
   } else if (classes.nElement() > 0 && classes.dim() == 2) {
     require(classes.size(2) == bboxes.size(1), s"the number of classes ${ classes.size(2) }" +
       s"should be equal to the number of bounding box numbers ${ bboxes.size(1) }")
+    if (masks != null) {
+      require(classes.size(2) == masks.length, s"the number of classes ${classes.size(2)}" +
+        s"should be equal to the number of bounding box numbers ${masks.length}")
+    }
   }
 
 
   def toTable: Table = {
     val table = T()
-    table.insert(classes)
-    table.insert(bboxes)
+    if (masks != null) {
+      table(RoiLabel.MASKS) = masks
+    }
+    table(RoiLabel.CLASSES) = classes
+    table(RoiLabel.BBOXES) = bboxes
+    table
   }
 
   def size(): Int = {
@@ -53,6 +68,28 @@ case class RoiLabel(classes: Tensor[Float], bboxes: Tensor[Float]) {
 }
 
 object RoiLabel {
+  val CLASSES = "classes"
+  val BBOXES = "bboxes"
+  val MASKS = "masks"
+  // ISCROWD and ORIGSIZE are stored in ImageFeature
+  val ISCROWD = "is_crowd"
+  val ORIGSIZE = "size"
+
+
+  def getClasses(tab: Table): Tensor[Float] = tab[Tensor[Float]](CLASSES)
+  def getBBoxes(tab: Table): Tensor[Float] = tab[Tensor[Float]](BBOXES)
+  def getMasks(tab: Table): Array[Tensor[Float]] =
+    tab[Array[Tensor[Float]]](MASKS)
+  def getIsCrowd(tab: Table): Tensor[Float] =
+    tab[Tensor[Float]](ISCROWD)
+
+  /**
+   * @return (height, width, channel)
+   */
+  def getOrigSize(tab: Table): (Int, Int, Int) =
+    tab[(Int, Int, Int)](ORIGSIZE)
+
+
   def fromTensor(tensor: Tensor[Float]): RoiLabel = {
     val label = tensor.narrow(2, 1, 2).transpose(1, 2).contiguous()
     val rois = tensor.narrow(2, 3, 4)
