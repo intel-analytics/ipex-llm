af7a508d2a1b09bc207cf73e612b82ad7cb78ba2
Xiao <lingxiao1989@gmail.com>
2019-10-14 14:18:08 +0800

    Onnx support: add a dim parameter to ops.Gather (#2920)
    * add dim parameter to ops.Gather
    
    * improve and simplify code
    
    * improve and simplify code
    
    * improve and simplify code
    
    * improve and simplify code

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/ops/Gather.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/nn/ops/Gather.scala
@@ -26,7 +26,8 @@ import scala.reflect.ClassTag
  * Input should be two tensors, the first one is the tensor which to gather values;
  * the second one is Index tensor.
  */
-class Gather[T: ClassTag, D: ClassTag]()(implicit ev: TensorNumeric[T], ev2: TensorNumeric[D])
+class Gather[T: ClassTag, D: ClassTag](
+  var dim: Int = 1)(implicit ev: TensorNumeric[T], ev2: TensorNumeric[D])
   extends Operation[Table, Tensor[D], T]{
   output = Tensor[D]()
 
@@ -44,34 +45,39 @@ class Gather[T: ClassTag, D: ClassTag]()(implicit ev: TensorNumeric[T], ev2: Ten
       intBuffer
     }
     val inputSizes = inputTensor.size()
-
-    if (indices.isScalar) {
-      val index = indices.value()
-      require(index < inputSizes(0),
-        s"index should smaller than ${inputSizes(0)}, but got $index")
-      val theOutput = inputTensor.select(1, index + 1)
-      inputSizes(0) = 1
-      this.output.resize(inputSizes).copy(theOutput)
-    } else {
-      val indicesSize = indices.size()
-      val outputSizes = indicesSize ++ inputSizes.slice(1, inputSizes.length)
-
-      output.resize(Array(indices.nElement()) ++ inputSizes.slice(1, inputSizes.length))
-      indices.resize(indices.nElement())
-      var i = 0
-      while (i < indices.nElement()) {
-        val index = indices.valueAt(i + 1)
-        require(index < inputSizes(0),
-          s"index should smaller than ${inputSizes(0)}, but got $index")
-        output.select(1, i + 1).copy(inputTensor.select(1, index + 1))
-        i += 1
-      }
-
-      indices.resize(indicesSize)
-      output.resize(outputSizes)
+    val inputDim = inputTensor.dim() // data batch dim
+    dim = if (dim <= 0) {
+      inputDim + dim
     }
+    else dim
+    require(dim >= 1 && dim <= inputDim, s"Invalid position: $dim. " +
+      s"input:dim() is $inputTensor, input feature map dim (numInputDims) is $inputDim.")
 
-    output
+    // set output shape
+    val indicesSize = indices.size()
+    val outputSizes = if (indices.isScalar) {
+      inputSizes.slice(0, dim-1) ++ Array(1) ++ inputSizes.slice(dim, inputSizes.length)
+    } else {
+      inputSizes.slice(0, dim-1) ++ indicesSize ++ inputSizes.slice(dim, inputSizes.length)
+    }
+    // set the insert position in output to one-dim array
+    output.resize(inputSizes.slice(0, dim-1)++
+      Array(indices.nElement())++
+      inputSizes.slice(dim, inputSizes.length))
+
+    // copy selected element to the insert position
+    indices.resize(indices.nElement())
+    var i = 0
+    while (i < indices.nElement()) {
+      val index = indices.valueAt(i + 1)
+      require(index < inputSizes(dim - 1),
+        s"index should smaller than ${inputSizes(dim - 1)}, but got $index")
+      output.select(dim, i + 1).copy(inputTensor.select(dim, index + 1))
+      i += 1
+    }
+    // resize the output to expected shape
+    indices.resize(indicesSize)
+    output.resize(outputSizes)
   }
 
   override def getClassTagNumerics() : (Array[ClassTag[_]], Array[TensorNumeric[_]]) = {
@@ -88,7 +94,8 @@ class Gather[T: ClassTag, D: ClassTag]()(implicit ev: TensorNumeric[T], ev2: Ten
 }
 
 object Gather {
-  def apply[T: ClassTag, D: ClassTag]()(implicit ev: TensorNumeric[T], ev2: TensorNumeric[D]):
-  Gather[T, D] = new Gather()
-
+  def apply[T: ClassTag, D: ClassTag](
+    dim: Int = 1
+  )(implicit ev: TensorNumeric[T], ev2: TensorNumeric[D]):
+  Gather[T, D] = new Gather(dim)
 }
