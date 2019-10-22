ed9cec8e88882173e6efbcf21b654a796eacb15d
zhangxiaoli73 <380761639@qq.com>
2019-10-22 12:54:23 +0800

    Add resize for segmentation (#2923)
    * add resize for segmentation
    
    * meet pr comments

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/transform/vision/image/augmentation/ResizeSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/transform/vision/image/augmentation/ResizeSpec.scala
@@ -17,7 +17,7 @@
 package com.intel.analytics.bigdl.transform.vision.image.augmentation
 
 import com.intel.analytics.bigdl.transform.vision.image.opencv.OpenCVMat
-import com.intel.analytics.bigdl.transform.vision.image.{BytesToMat, ImageFrame, LocalImageFrame}
+import com.intel.analytics.bigdl.transform.vision.image.{BytesToMat, ImageFeature, ImageFrame, LocalImageFrame}
 import org.opencv.imgcodecs.Imgcodecs
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -77,5 +77,14 @@ class ResizeSpec extends FlatSpec with Matchers {
     val (height, width) = AspectScale.getHeightWidthAfterRatioScale(img, 600, 1000, 1)
     height should be (600)
     width should be (800)
+  }
+
+  "scaleResize without roi" should "be ok" in {
+    val data = ImageFrame.read(resource.getFile)
+    val transformer = ScaleResize(minSize = 100, maxSize = 120, resizeROI = false)
+    val transformed = transformer(data)
+    val imageFeature = transformed.asInstanceOf[LocalImageFrame].array(0)
+    imageFeature.getHeight() should be(90)
+    imageFeature.getWidth() should be(120)
   }
 }
