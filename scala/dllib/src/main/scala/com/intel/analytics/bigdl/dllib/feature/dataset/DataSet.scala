ef1adb4af0c84e558ee4046e1c105849017bc37d
Menooker <Menooker@users.noreply.github.com>
2019-10-28 10:07:45 +0800

    COCO Seq file reader: grey to bgr (#2942)
    * grey to bgr
    
    * refactor isGrayScaleImage
    
    * simplify grey scale image checking

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/feature/dataset/DataSet.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/feature/dataset/DataSet.scala
@@ -26,7 +26,8 @@ import com.intel.analytics.bigdl.tensor.Tensor
 import com.intel.analytics.bigdl.transform.vision.image.label.roi.RoiLabel
 import com.intel.analytics.bigdl.transform.vision.image.{DistributedImageFrame, ImageFeature, ImageFrame, LocalImageFrame}
 import com.intel.analytics.bigdl.utils.{Engine, RandomGenerator, T}
-import java.awt.image.DataBufferByte
+import java.awt.Color
+import java.awt.image.{BufferedImage, DataBufferByte}
 import java.io.ByteArrayInputStream
 import javax.imageio.ImageIO
 import org.apache.hadoop.io.{BytesWritable, Text}
@@ -589,6 +590,12 @@ object DataSet {
       ImageFrame.rdd(rawData)
     }
 
+    private def isSingleChannelImage(image: BufferedImage): Boolean = {
+      if (image.getType == BufferedImage.TYPE_BYTE_GRAY) return true
+      if (image.getType == BufferedImage.TYPE_USHORT_GRAY) return true
+      if (image.getRaster.getNumBands == 1) return true
+      false
+    }
     /**
      * Extract hadoop sequence files from an HDFS path as ImageFrame
      * @param url sequence files folder path
@@ -620,8 +627,19 @@ object DataSet {
           require(metaBytes.getInt == COCODataset.MAGIC_NUM, "Corrupted metadata")
 
           val inputStream = new ByteArrayInputStream(data._2.getBytes)
-          val image = ImageIO.read(inputStream)
+          val image = {
+            val img = ImageIO.read(inputStream)
+            if (isSingleChannelImage(img)) {
+              val imageBuff: BufferedImage =
+                new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_3BYTE_BGR)
+              imageBuff.getGraphics.drawImage(img, 0, 0, new Color(0, 0, 0), null)
+              imageBuff
+            } else {
+              img
+            }
+          }
           val rawdata = image.getRaster.getDataBuffer.asInstanceOf[DataBufferByte].getData()
+          require(rawdata.length == height * width * 3)
           val imf = ImageFeature(rawdata, RoiLabel(labelClasses, bboxes, masks), fileName)
           imf(ImageFeature.originalSize) = (height, width, 3)
           imf(RoiLabel.ISCROWD) = isCrowd
