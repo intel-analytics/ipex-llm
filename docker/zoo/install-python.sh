f2a10649e494b9acfb52799906d02ea886f6ce5e
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2019-11-27 15:38:42 +0800

    update Dockerfile python (#1755)
    * update Dockerfile python version
    
    * update tf version

--- docker/zoo/install-python.sh
+++ docker/zoo/install-python.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PY_VERSION_SUFFIX=${PY_VERSION_3:+3}
+PY_VERSION_SUFFIX=3
 PYTHON=python${PY_VERSION_SUFFIX}
 PIP=pip${PY_VERSION_SUFFIX}
 
@@ -12,18 +12,13 @@ apt-get install -y build-essential ${PYTHON} ${PYTHON}-setuptools ${PYTHON}-dev 
 ${PIP} install --upgrade setuptools
 ${PIP} install numpy scipy
 ${PIP} install --no-binary pandas -I pandas
-${PIP} install scikit-learn matplotlib seaborn jupyter wordcloud moviepy requests h5py opencv-python tensorflow==1.10.0
+${PIP} install scikit-learn matplotlib seaborn jupyter wordcloud moviepy requests h5py opencv-python tensorflow==1.15.0
 ${PIP} install torch==1.1.0 torchvision==0.3.0 -f https://download.pytorch.org/whl/torch_stable.html
 
-if [[ "$PYTHON" != "python3" ]]; then
-    ${PYTHON} -m pip install -U ipykernel
-    ${PYTHON} -m ipykernel install --user
-else
-    ln -s /usr/bin/python3 /usr/bin/python
-    ln -s /usr/bin/pip3 /usr/local/bin/pip
-    #Fix tornado await process
-    pip uninstall -y -q tornado
-    pip install tornado==5.1.1
-fi
+ln -s /usr/bin/python3 /usr/bin/python
+ln -s /usr/bin/pip3 /usr/local/bin/pip
+#Fix tornado await process
+pip uninstall -y -q tornado
+pip install tornado==5.1.1
 
 ${PYTHON} -m ipykernel.kernelspec
