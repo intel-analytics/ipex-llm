68f87589ba13e2c8e71984777b2f1ce85c5907d8
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2019-12-06 14:48:39 +0800

    update inference-flink-example (#1826)

--- apps/model-inference-examples/model-inference-flink/src/main/scala/com/intel/analytics/zoo/apps/model/inference/flink/Resnet50ImageClassification/Resnet50InferenceModel.scala
+++ apps/model-inference-examples/model-inference-flink/src/main/scala/com/intel/analytics/zoo/apps/model/inference/flink/Resnet50ImageClassification/Resnet50InferenceModel.scala
@@ -2,8 +2,6 @@ package com.intel.analytics.zoo.apps.model.inference.flink.Resnet50ImageClassifi
 
 import com.intel.analytics.zoo.pipeline.inference.InferenceModel
 
-class Resnet50InferenceModel(var concurrentNum: Int = 1, modelType: String, modelBytes: Array[Byte], inputShape: Array[Int], ifReverseInputChannels: Boolean, meanValues: Array[Float], scale: Float) extends InferenceModel(concurrentNum) with Serializable {
-  doLoadTF(null, modelType, modelBytes, inputShape, ifReverseInputChannels, meanValues, scale)
-  println(this)
+class Resnet50InferenceModel(var concurrentNum: Int = 1, modelType: String, checkpointBytes: Array[Byte], inputShape: Array[Int], ifReverseInputChannels: Boolean, meanValues: Array[Float], scale: Float) extends InferenceModel(concurrentNum) with Serializable {
+  doLoadTF(null, modelType, checkpointBytes, inputShape, ifReverseInputChannels, meanValues, scale)
 }
-
