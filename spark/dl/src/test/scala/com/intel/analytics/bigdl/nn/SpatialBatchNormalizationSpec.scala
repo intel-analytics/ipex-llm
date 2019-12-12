3ae86e6902347cc352d56a80becb3eeeb4508ffa
Yanzhang Wang <i8run15@gmail.com>
2019-12-12 21:00:08 +0800

    fix: bn nhwc error, the channel should be the last dim (#2981)

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/nn/SpatialBatchNormalizationSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/nn/SpatialBatchNormalizationSpec.scala
@@ -219,6 +219,27 @@ class SpatialBatchNormalizationSpec extends FlatSpec with Matchers with BeforeAn
     bnNCHW.gradWeight.almostEqual(bnNHWC.gradWeight, 1e-5)
     bnNCHW.gradBias.almostEqual(bnNHWC.gradBias, 1e-5)
   }
+
+  "bn with NHWC" should "return correct extra parameters" in {
+    val bn1 = SpatialBatchNormalization[Float](16, dataFormat = DataFormat.NCHW)
+    val bn2 = SpatialBatchNormalization[Float](16, dataFormat = DataFormat.NHWC)
+
+    bn2.parameters()._1.zip(bn1.parameters()._1).foreach {
+      case (bn2Para, bn1Para) => bn2Para.copy(bn1Para)
+    }
+
+    val input1 = Tensor[Float](4, 16, 3, 3).rand(-1, 1)
+    bn1.forward(input1)
+
+    val input2 = input1.transpose(2, 4).contiguous()
+    bn2.forward(input2)
+
+    bn1.getExtraParameter().zip(bn2.getExtraParameter()).foreach {
+      case (p1, p2) =>
+        p1.size() should be (p2.size())
+        p1 should be (p2)
+    }
+  }
 }
 
 class SpatialBatchNormalizationSerialTest extends ModuleSerializationTest {
