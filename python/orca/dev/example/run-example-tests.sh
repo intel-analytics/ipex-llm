542c21ef8587ab3243c1d2044ecbcb4808212c99
Qiyuan Gong <qiyuan.gong@intel.com>
2020-03-10 10:11:09 +0800

    Modify optimizer based OpenVINO examples (#2016)
    * Remove VNNI OpenVINO PrepareOpenVINOResNet.scala.
    * Modify Python OpenVINO faster_rcnn_resnet101_coco example and auto test.
    * Modify document.

--- python/orca/dev/example/run-example-tests.sh
+++ python/orca/dev/example/run-example-tests.sh
@@ -425,13 +425,14 @@ time10=$((now-start))
 echo "#11 start example test for openvino"
 #timer
 start=$(date "+%s")
-if [ -f analytics-zoo-models/faster_rcnn_resnet101_coco_2018_01_28.tar.gz ]
+if [ -f analytics-zoo-models/faster_rcnn_resnet101_coco.xml ]
 then
    echo "analytics-zoo-models/faster_rcnn_resnet101_coco already exists."
 else
-   wget -nv $FTP_URI/analytics-zoo-models/openvino/faster_rcnn_resnet101_coco_2018_01_28.tar.gz \
+   wget -nv $FTP_URI/analytics-zoo-models/openvino/2018_R5/faster_rcnn_resnet101_coco.xml \
     -P analytics-zoo-models
-   tar zxf analytics-zoo-models/faster_rcnn_resnet101_coco_2018_01_28.tar.gz -C analytics-zoo-models/
+   wget -nv $FTP_URI/analytics-zoo-models/openvino/2018_R5/faster_rcnn_resnet101_coco.bin \
+    -P analytics-zoo-models
 fi
 if [ -d analytics-zoo-data/data/object-detection-coco ]
 then
@@ -446,7 +447,7 @@ ${ANALYTICS_ZOO_HOME}/bin/spark-submit-python-with-zoo.sh \
     --executor-memory 10g \
     ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/openvino/predict.py \
     --image analytics-zoo-data/data/object-detection-coco \
-    --model analytics-zoo-models/faster_rcnn_resnet101_coco_2018_01_28
+    --model analytics-zoo-models/faster_rcnn_resnet101_coco.xml
 now=$(date "+%s")
 time11=$((now-start))
 
