bdb79d9d702f1c562bf1aea17cc6e58ecd66f092
Kai Huang <huangkaivision@gmail.com>
2020-04-13 19:09:49 +0800

    Add doc for MXNetTrainer and some code refactor (#2198)
    * add doc and some code refactor
    
    * minor
    
    * update
    
    * minor

--- python/orca/src/bigdl/orca/ray/mxnet/mxnet_runner.py
+++ python/orca/src/bigdl/orca/ray/mxnet/mxnet_runner.py
@@ -18,21 +18,23 @@ import os
 import time
 import logging
 import subprocess
-import socket
 import ray.services
 import mxnet as mx
 from mxnet import gluon
-from contextlib import closing
-from dmlc_tracker.tracker import get_host_ip
+from zoo.ray.mxnet.utils import find_free_port
 
 
 class MXNetRunner(object):
     """Manages a MXNet model for training."""
+
     def setup_distributed(self, env, config, data_creator, model_creator,
                           loss_creator=None, metrics_creator=None):
         logging.basicConfig(level=logging.INFO)  # This can print log messages to console.
         self.logger = logging.getLogger()
-        self.config = config  # TODO: add check for config keys
+        assert isinstance(config, dict), "config must be a dict"
+        for param in ["batch_size", "optimizer", "optimizer_params", "log_interval"]:
+            assert param in config, param + " must be specified in config"
+        self.config = config
         self.data_creator = data_creator
         self.model_creator = model_creator
         self.loss_creator = loss_creator
@@ -117,9 +119,8 @@ class MXNetRunner(object):
                         self.trainer.step(batch.data[0].shape[0])
                         if self.metrics:
                             self.metrics.update(label, outputs)
-                        if "log_interval" in self.config and \
-                                not (i + 1) % self.config["log_interval"]:
-                            # This would print on driver for each pid.
+                        if not (i + 1) % self.config["log_interval"]:
+                            # This would be logged on driver for each worker process.
                             print_output = ""
                             print_output \
                                 += 'Epoch[%d] Batch[%d]  Speed: %f samples/sec %s=%f' \
@@ -135,6 +136,7 @@ class MXNetRunner(object):
                                     print_output += ' %s=%f' % (name, acc)
                             self.logger.info(print_output)
                         batch_start_time = time.time()
+                        # TODO: save checkpoints
                 if self.metrics:
                     names, accs = self.metrics.get()
                     if not isinstance(names, list):
@@ -144,19 +146,21 @@ class MXNetRunner(object):
                         stats[name] = acc
             else:  # Symbolic API
                 # TODO: seems no history (i.e. validation accuracy) returned by fit?
+                if "init" not in self.config:
+                    from mxnet.initializer import Uniform
+                    self.config["init"] = Uniform(0.01)  # This is the default value for MXNet
                 self.model.fit(train_data=self.train_data,
                                num_epoch=nb_epoch,
                                initializer=self.config["init"],
                                kvstore=self.kv,
                                optimizer=self.config["optimizer"],
                                optimizer_params=self.config["optimizer_params"],
+                               eval_data=self.val_data,
                                # TODO: eval and validation metrics could be different
                                eval_metric=self.metrics,
                                validation_metric=self.metrics,
-                               eval_data=self.val_data,
-                               batch_end_callback=None if "log_interval" not in self.config
-                               else mx.callback.Speedometer(self.config["batch_size"],
-                                                            self.config["log_interval"]),
+                               batch_end_callback=mx.callback.Speedometer(
+                                   self.config["batch_size"], self.config["log_interval"]),
                                epoch_end_callback=None if "model" not in self.config
                                else mx.callback.do_checkpoint(self.config["model"]))
             epoch_time = time.time() - start_time
@@ -182,105 +186,3 @@ class MXNetRunner(object):
     def find_free_port(self):
         """Finds a free port on the current node."""
         return find_free_port()
-
-
-def find_free_port():
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-        s.bind(("", 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        return s.getsockname()[1]
-
-
-class MXNetTrainer(object):
-    # TODO: Add documentation.
-    def __init__(self,
-                 config,  # Pass in some config, including initializer, batch_size, etc.
-                 data_creator,
-                 # Return a MXNET model defined with either symbolic or gluon API.
-                 model_creator,
-                 # No need for symbolic API. Loss is already defined as model output.
-                 loss_creator=None,
-                 metrics_creator=None,
-                 # Specify cpu resources for actors if necessary.
-                 runner_cores=None):
-        self.config = config
-        self.data_creator = data_creator
-        self.model_creator = model_creator
-        self.loss_creator = loss_creator
-        self.metrics_creator = metrics_creator
-        self.num_workers = config["num_workers"]
-        self.num_servers = config["num_servers"] if "num_servers" in self.config \
-            else self.num_workers
-
-        # Generate actor class
-        # Add a dummy custom resource to diff worker from server if runner_cores is specified
-        # so that we can place one worker and one server on a node for better performance.
-        Worker = ray.remote(num_cpus=runner_cores, resources={"_mxnet_worker": 1})(MXNetRunner) \
-            if runner_cores else ray.remote(MXNetRunner)
-        Server = ray.remote(num_cpus=runner_cores, resources={"_mxnet_server": 1})(MXNetRunner) \
-            if runner_cores else ray.remote(MXNetRunner)
-
-        # Start runners: workers followed by servers
-        self.runners = [
-            Worker.remote()
-            for i in range(self.num_workers)
-        ]
-        self.runners += [
-            Server.remote()
-            for i in range(self.num_servers)
-        ]
-
-        # Compute URL for initializing distributed setup
-        ips = ray.get(
-            [runner.get_node_ip.remote() for runner in self.runners])
-        ports = ray.get(
-            [runner.find_free_port.remote() for runner in self.runners])
-        logger = logging.getLogger()
-        logger.info(ips)
-        logger.info(ports)
-
-        env = {
-            "DMLC_PS_ROOT_URI": str(get_host_ip()),
-            "DMLC_PS_ROOT_PORT": str(find_free_port()),
-            "DMLC_NUM_SERVER": str(self.num_servers),
-            "DMLC_NUM_WORKER": str(self.num_workers),
-        }
-        envs = []
-        for i in range(self.num_workers):
-            current_env = env.copy()
-            current_env['DMLC_ROLE'] = 'worker'
-            envs.append(current_env)
-        for i in range(self.num_servers):
-            current_env = env.copy()
-            current_env['DMLC_ROLE'] = 'server'
-            envs.append(current_env)
-
-        env['DMLC_ROLE'] = 'scheduler'
-        modified_env = os.environ.copy()
-        modified_env.update(env)
-        # Need to contain system env to run bash
-        # TODO: Need to kill this process manually?
-        subprocess.Popen("python -c 'import mxnet'", shell=True, env=modified_env)
-
-        ray.get([
-            runner.setup_distributed.remote(envs[i], self.config,
-                self.data_creator,
-                self.model_creator,
-                self.loss_creator,
-                self.metrics_creator)
-            for i, runner in enumerate(self.runners)
-        ])
-
-    def train(self, nb_epoch=1):
-        """Trains an MXNet model for several epochs."""
-        stats = ray.get([w.train.remote(nb_epoch) for w in self.runners])
-        return stats
-
-    def shutdown(self):
-        """Shuts down runners and releases resources."""
-        for runner in self.runners:
-            runner.shutdown.remote()
-            runner.__ray_terminate__.remote()
-
-# TODO: add model save and restore
-# TODO: add predict, evaluate
