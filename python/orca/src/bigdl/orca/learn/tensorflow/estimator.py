0d1ea3cb25ed5b43c5f9c2e2c90f7d7b3e0942a3
Yang Wang <yang3.wang@intel.com>
2020-05-26 15:49:22 +0800

    Add orca tensorflow estimator (#2330)
    * add orca tensorflow estimator
    
    * support pre-built graph
    
    * add tests
    
    * use ndarray
    
    * simplify
    
    * adjust for rebase
    
    * move optimizer to constructor
    
    * simplify
    
    * support only ndarray
    
    * address comments
    
    * address comments
    
    * address comments
    
    * fix style

---
+++ python/orca/src/bigdl/orca/learn/tensorflow/estimator.py
@@ -0,0 +1,198 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from bigdl.optim.optimizer import MaxIteration, SGD
+from zoo.orca.data.shard import SparkXShards
+from zoo.tfpark import TFEstimator, TFOptimizer, TFPredictor, TFNet
+import pandas as pd
+import tensorflow as tf
+
+from zoo.tfpark.tf_dataset import TFDataset
+from zoo.util import nest
+
+
+class Estimator(object):
+    def fit(self, data, steps, **kwargs):
+        pass
+
+    def predict(self, data, **kwargs):
+        pass
+
+    @staticmethod
+    def from_graph(*, inputs, outputs=None,
+                   labels=None, loss=None, optimizer=None,
+                   metrics=None, updates=None,
+                   sess=None, model_dir=None, backend="spark"):
+        assert backend == "spark", "only spark backend is supported for now"
+        if sess is None:
+            sess = tf.Session()
+            sess.run(tf.global_variables_initializer())
+
+        return TFOptimizerWrapper(inputs=inputs,
+                                  outputs=outputs,
+                                  labels=labels,
+                                  loss=loss,
+                                  optimizer=optimizer,
+                                  metrics=metrics, updates=updates,
+                                  sess=sess,
+                                  model_dir=model_dir)
+
+
+def _xshards_to_tf_dataset(data_shard,
+                           batch_size=-1, batch_per_thread=-1,
+                           validation_data_shard=None):
+    # todo data_shard.head ?
+    import numpy as np
+
+    def check_data_type_and_to_list(data):
+        result = {}
+        assert isinstance(data, dict), "each shard should be an dict"
+        assert "x" in data, "key x should in each shard"
+        x = data["x"]
+        if isinstance(x, np.ndarray):
+            new_x = [x]
+        elif isinstance(x, list) and all([isinstance(xi, np.ndarray) for xi in x]):
+            new_x = x
+        else:
+            raise ValueError("value of x should be a ndarray or a list of ndarrays")
+        result["x"] = new_x
+        if "y" in data:
+            y = data["y"]
+            if isinstance(y, np.ndarray):
+                new_y = [y]
+            elif isinstance(y, list) and all([isinstance(yi, np.ndarray) for yi in y]):
+                new_y = y
+            else:
+                raise ValueError("value of x should be a ndarray or a list of ndarrays")
+            result["y"] = new_y
+        return result
+
+    def get_spec(data):
+        data = check_data_type_and_to_list(data)
+        feature_spec = [(feat.dtype, feat.shape[1:])
+                        for feat in data["x"]]
+        if "y" in data:
+            label_spec = [(label.dtype, label.shape[1:])
+                          for label in data["y"]]
+        else:
+            label_spec = None
+        return (feature_spec, label_spec)
+
+    (feature_spec, label_spec) = data_shard.rdd.map(get_spec).first()
+
+    feature_spec = [(tf.dtypes.as_dtype(spec[0]), spec[1]) for spec in feature_spec]
+    label_spec = [(tf.dtypes.as_dtype(spec[0]), spec[1]) for spec in label_spec] \
+        if label_spec is not None else None
+
+    assert batch_size != -1 or batch_per_thread != -1, \
+        "one of batch_size and batch_per_thread should be specified"
+
+    # todo this might be very slow
+    def flatten(data):
+        data = check_data_type_and_to_list(data)
+        features = data["x"]
+
+        has_label = "y" in data
+        labels = data["y"] if has_label else None
+        length = features[0].shape[0]
+
+        for i in range(length):
+            fs = [feat[i] for feat in features]
+            if has_label:
+                ls = [l[i] for l in labels]
+                yield (fs, ls)
+            else:
+                yield (fs,)
+
+    val_rdd = None if validation_data_shard is None \
+        else validation_data_shard.rdd.flatMap(flatten)
+
+    dataset = TFDataset.from_rdd(data_shard.rdd.flatMap(flatten),
+                                 features=feature_spec,
+                                 labels=label_spec,
+                                 batch_size=batch_size,
+                                 batch_per_thread=batch_per_thread,
+                                 val_rdd=val_rdd)
+
+    return dataset
+
+
+class TFOptimizerWrapper(Estimator):
+
+    def __init__(self, *, inputs, outputs, labels, loss,
+                 optimizer,
+                 metrics,
+                 updates, sess,
+                 model_dir):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.labels = labels
+        self.loss = loss
+        self.optimizer = optimizer
+        self.metrics = metrics
+        self.updates = updates
+        self.sess = sess
+        self.model_dir = model_dir
+
+    def fit(self, data_shard, steps,
+            batch_size=32,
+            validation_data_shard=None,
+            feed_dict=None,
+            session_config=None):
+        import bigdl
+
+        assert self.labels is not None, \
+            "labels is None; it should not be None in training"
+        assert self.loss is not None, \
+            "loss is None; it should not be None in training"
+        assert self.optimizer is not None, \
+            "optimizer is None; it not None in training"
+        assert isinstance(self.optimizer, bigdl.optim.optimizer.OptimMethod), \
+            "optimizer is of type {}, ".format(type(self.optimizer)) + \
+            "it should be an instance of bigdl.optim.optimizer.OptimMethod"
+
+        dataset = _xshards_to_tf_dataset(data_shard,
+                                         batch_size=batch_size,
+                                         validation_data_shard=validation_data_shard)
+
+        if feed_dict is not None:
+            tensor_with_value = {key: (value, value) for key, value in feed_dict.items()}
+        else:
+            tensor_with_value = None
+
+        optimizer = TFOptimizer.from_loss(
+            loss=self.loss,
+            optim_method=self.optimizer,
+            inputs=(self.inputs, self.labels),
+            dataset=dataset,
+            metrics=self.metrics,
+            updates=self.updates, session=self.sess,
+            tensor_with_value=tensor_with_value,
+            session_config=session_config,
+            model_dir=self.model_dir)
+
+        optimizer.optimize(end_trigger=MaxIteration(steps))
+        return self
+
+    def predict(self, data_shard, batch_size=32):
+        assert self.outputs is not None, \
+            "output is None, it should not be None in prediction"
+        dataset = _xshards_to_tf_dataset(data_shard,
+                                         batch_per_thread=batch_size)
+
+        flat_inputs = nest.flatten(self.inputs)
+        flat_outputs = nest.flatten(self.outputs)
+        tfnet = TFNet.from_session(sess=self.sess, inputs=flat_inputs, outputs=flat_outputs)
+        return tfnet.predict(dataset)
---
+++ python/orca/test/bigdl/orca/learn/spark/test_estimator_for_spark.py
@@ -0,0 +1,139 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import tensorflow as tf
+
+
+from zoo.orca.learn.tensorflow.estimator import Estimator
+
+resource_path = os.path.join(os.path.split(__file__)[0], "../../../resources")
+
+
+class SimpleModel(object):
+
+    def __init__(self):
+        self.user = tf.placeholder(dtype=tf.int32, shape=(None,))
+        self.item = tf.placeholder(dtype=tf.int32, shape=(None,))
+        self.label = tf.placeholder(dtype=tf.int32, shape=(None,))
+
+        feat = tf.stack([self.user, self.item], axis=1)
+        self.logits = tf.layers.dense(tf.to_float(feat), 2)
+
+        self.loss = tf.reduce_mean(tf.losses.sparse_softmax_cross_entropy(logits=self.logits,
+                                                                          labels=self.label))
+
+
+def test_estimator_graph(estimator_for_spark_fixture):
+    from bigdl.optim.optimizer import SGD
+    import zoo.orca.data.pandas
+
+    sc = estimator_for_spark_fixture
+
+    model = SimpleModel()
+    file_path = os.path.join(resource_path, "orca/learn/ncf.csv")
+    data_shard = zoo.orca.data.pandas.read_csv(file_path, sc)
+
+    def transform(df):
+        result = {
+            "x": [df['user'].to_numpy(), df['item'].to_numpy()],
+            "y": df['label'].to_numpy()
+        }
+        return result
+
+    data_shard = data_shard.transform_shard(transform)
+
+    est = Estimator.from_graph(
+        inputs=[model.user, model.item],
+        labels=[model.label],
+        outputs=[model.logits],
+        loss=model.loss,
+        optimizer=SGD(),
+        metrics={"loss": model.loss})
+    est.fit(data_shard=data_shard,
+            batch_size=8,
+            steps=10,
+            validation_data_shard=data_shard)
+
+    data_shard = zoo.orca.data.pandas.read_csv(file_path, sc)
+
+    def transform(df):
+        result = {
+            "x": [df['user'].to_numpy(), df['item'].to_numpy()],
+        }
+        return result
+
+    data_shard = data_shard.transform_shard(transform)
+    predictions = est.predict(data_shard).collect()
+    print(predictions)
+
+
+def test_estimator_graph_fit(estimator_for_spark_fixture):
+    from bigdl.optim.optimizer import SGD
+    import zoo.orca.data.pandas
+
+    model = SimpleModel()
+    sc = estimator_for_spark_fixture
+    file_path = os.path.join(resource_path, "orca/learn/ncf.csv")
+    data_shard = zoo.orca.data.pandas.read_csv(file_path, sc)
+
+    def transform(df):
+        result = {
+            "x": [df['user'].to_numpy(), df['item'].to_numpy()],
+            "y": df['label'].to_numpy()
+        }
+        return result
+
+    data_shard = data_shard.transform_shard(transform)
+
+    est = Estimator.from_graph(
+        inputs=[model.user, model.item],
+        labels=[model.label],
+        loss=model.loss,
+        optimizer=SGD(),
+        metrics={"loss": model.loss})
+    est.fit(data_shard=data_shard,
+            batch_size=8,
+            steps=10,
+            validation_data_shard=data_shard)
+
+
+def test_estimator_graph_predict(estimator_for_spark_fixture):
+    import zoo.orca.data.pandas
+
+    sc = estimator_for_spark_fixture
+
+    model = SimpleModel()
+    file_path = os.path.join(resource_path, "orca/learn/ncf.csv")
+    data_shard = zoo.orca.data.pandas.read_csv(file_path, sc)
+
+    est = Estimator.from_graph(
+        inputs=[model.user, model.item],
+        outputs=[model.logits])
+
+    def transform(df):
+        result = {
+            "x": [df['user'].to_numpy(), df['item'].to_numpy()],
+        }
+        return result
+
+    data_shard = data_shard.transform_shard(transform)
+    predictions = est.predict(data_shard).collect()
+    print(predictions)
+
+
+if __name__ == "__main__":
+    import pytest
+    pytest.main([__file__])
