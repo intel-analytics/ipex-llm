af2dd8d454e0b4c592f8e849a26c8b58bf4727af
Yina Chen <33650826+cyita@users.noreply.github.com>
2020-05-28 12:40:25 +0800

    Orca MXNetTrainer XShards support (#2348)
    * migrate mxnet_trainer to estimator
    
    * fix
    
    * newline
    
    * indent
    
    * fix test path
    
    * fix
    
    * ignore mxnet estimator test in spark2.4-
    
    * estimator to trainer
    
    * init
    
    * change api
    
    * MXNetTrainer xshard UT
    
    * fix
    
    * add list of ndarray support
    
    * delete data
    
    * style fix
    
    * divide eval metrics & validation metrics
    
    * fix
    
    * fix

--- python/orca/src/bigdl/orca/learn/mxnet/mxnet_runner.py
+++ python/orca/src/bigdl/orca/learn/mxnet/mxnet_runner.py
@@ -20,26 +20,30 @@ import logging
 import subprocess
 import ray.services
 import mxnet as mx
+import numpy as np
 from mxnet import gluon
+from functools import reduce
 from zoo.ray.utils import to_list
 from zoo.orca.learn.mxnet.utils import find_free_port
+from zoo.orca.data.shard import XShards
 
 
 class MXNetRunner(object):
     """Manages a MXNet model for training."""
 
-    def setup_distributed(self, env, config, data_creator, model_creator,
-                          loss_creator=None, metrics_creator=None):
+    def setup_distributed(self, env, config, train_data, model_creator, loss_creator=None,
+                          validation_metrics_creator=None, test_data=None, train_resize_size=None,
+                          eval_metrics_creator=None):
         logging.basicConfig(level=logging.INFO)  # This can print log messages to console.
         self.logger = logging.getLogger()
         assert isinstance(config, dict), "config must be a dict"
         for param in ["batch_size", "optimizer", "optimizer_params", "log_interval"]:
             assert param in config, param + " must be specified in config"
         self.config = config
-        self.data_creator = data_creator
         self.model_creator = model_creator
         self.loss_creator = loss_creator
-        self.metrics_creator = metrics_creator
+        self.validation_metrics_creator = validation_metrics_creator
+        self.eval_metircs_creator = eval_metrics_creator
         self.is_worker = False
         env["DMLC_NODE_HOST"] = self.get_node_ip()
         if env["DMLC_ROLE"] == "worker":
@@ -51,28 +55,58 @@ class MXNetRunner(object):
             # Set seed so that the model on each worker is initialized with the same weights
             if "seed" in self.config:
                 mx.random.seed(self.config["seed"])
-            data = self.data_creator(self.config, self.kv)
-            if isinstance(data, tuple):
-                assert len(data) == 1 or len(data) == 2, \
-                    "data_creator should return either train_data only or a tuple of " \
-                    "(train_data, val_data), which can be directly fed to model training"
-                if len(data) == 1:
-                    self.train_data, self.val_data = data[0], None
+
+            if isinstance(train_data, XShards):
+                # XShard input
+                # retrieve train data
+                train_data = train_data.get_partitions()
+                train_partition_data = ray.get(train_data[self.kv.rank].get_data())
+                data, label = get_data_label(train_partition_data)
+                self.train_data = mx.io.NDArrayIter(data=data, label=label,
+                                                    batch_size=config["batch_size"],
+                                                    shuffle=True)
+                if train_resize_size is not None:
+                    self.train_data = mx.io.ResizeIter(self.train_data, train_resize_size)
+                # retrieve val data
+                if test_data is None:
+                    self.val_data = None
                 else:
-                    self.train_data, self.val_data = data
-            else:  # Only return one object, supposed to be train data.
-                self.train_data, self.val_data = data, None
+                    assert isinstance(test_data, XShards), "Test data should be an instance of " \
+                                                           "XShards, please check your input"
+                    test_data = test_data.get_partitions()
+                    val_partition_data = ray.get(test_data[self.kv.rank].get_data())
+                    val_data, val_label = get_data_label(val_partition_data)
+                    self.val_data = mx.io.NDArrayIter(data=val_data,
+                                                      label=val_label,
+                                                      batch_size=config["batch_size"],
+                                                      shuffle=True)
+            else:
+                # data_creator
+                assert callable(train_data), "Train data should be an instance of xShards or " \
+                                             "a callable function, please check your input"
+                self.train_data = train_data(self.config, self.kv)
+                if test_data is not None:
+                    assert callable(test_data), "Test data should be an instance of xShards or " \
+                                                "a callable function, please check your input"
+                    self.val_data = test_data(self.config, self.kv)
+                else:
+                    self.val_data = None
+
             self.model = self.model_creator(self.config)
             if self.loss_creator:
                 self.loss = self.loss_creator(self.config)
             else:
                 self.loss = None
-            if self.val_data:
-                assert self.metrics_creator, \
-                    "Metrics not defined for validation, please specify metrics_creator"
-                self.metrics = self.metrics_creator(self.config)
+            if self.eval_metircs_creator:
+                self.eval_metrics = self.eval_metircs_creator(self.config)
             else:
-                self.metrics = None
+                self.eval_metrics = None
+            if self.val_data:
+                assert self.validation_metrics_creator, \
+                    "Metrics not defined for validation, please specify metrics_creator"
+                self.val_metrics = self.validation_metrics_creator(self.config)
+            else:
+                self.val_metrics = None
             # For BaseModule, use symbolic API. Otherwise, use imperative API.
             # TODO: change to Estimator API?
             if not isinstance(self.model, mx.module.BaseModule):
@@ -98,8 +132,8 @@ class MXNetRunner(object):
             if self.trainer:  # Imperative API
                 for epoch in range(nb_epoch):
                     self.train_data.reset()
-                    if self.metrics:
-                        self.metrics.reset()  # metrics will accumulate for one batch
+                    if self.eval_metrics:
+                        self.eval_metrics.reset()  # metrics will accumulate for one batch
                     batch_start_time = time.time()
                     epoch_start_time = time.time()
                     for i, batch in enumerate(self.train_data):
@@ -119,8 +153,8 @@ class MXNetRunner(object):
                                 outputs.append(z)
                             ag.backward(Ls)
                         self.trainer.step(batch.data[0].shape[0])
-                        if self.metrics:
-                            self.metrics.update(label, outputs)
+                        if self.eval_metrics:
+                            self.eval_metrics.update(label, outputs)
                         if not (i + 1) % self.config["log_interval"]:
                             # This would be logged on driver for each worker process.
                             iteration_log = \
@@ -128,8 +162,8 @@ class MXNetRunner(object):
                                 % (epoch, i,
                                    self.config["batch_size"] / (time.time() - batch_start_time),
                                    "loss", Ls[0].asnumpy().mean())
-                            if self.metrics:
-                                names, accs = self.metrics.get()
+                            if self.eval_metrics:
+                                names, accs = self.eval_metrics.get()
                                 names, accs = to_list(names), to_list(accs)
                                 for name, acc in zip(names, accs):
                                     iteration_log += "  %s=%f" % (name, acc)
@@ -139,16 +173,16 @@ class MXNetRunner(object):
                     self.logger.info("[Epoch %d] time cost: %f" %
                                      (epoch, time.time() - epoch_start_time))
                     # Epoch metrics log on train data
-                    if self.metrics:
+                    if self.eval_metrics:
                         epoch_train_log = "[Epoch %d] training: " % epoch
-                        names, accs = self.metrics.get()
+                        names, accs = self.eval_metrics.get()
                         names, accs = to_list(names), to_list(accs)
                         for name, acc in zip(names, accs):
                             epoch_train_log += "%s=%f  " % (name, acc)
                         self.logger.info(epoch_train_log)
                     # Epoch metrics log on validation data if any:
                     if self.val_data:
-                        self.metrics.reset()
+                        self.val_metrics.reset()
                         self.val_data.reset()
                         for batch in self.val_data:
                             data = gluon.utils.split_and_load(
@@ -158,16 +192,16 @@ class MXNetRunner(object):
                                 batch.label[0].astype("float32", copy=False),
                                 ctx_list=[mx.cpu()], batch_axis=0)
                             outputs = [self.model(X) for X in data]
-                            self.metrics.update(label, outputs)
+                            self.val_metrics.update(label, outputs)
                         epoch_val_log = "[Epoch %d] validation: " % epoch
-                        names, accs = self.metrics.get()
+                        names, accs = self.val_metrics.get()
                         names, accs = to_list(names), to_list(accs)
                         for name, acc in zip(names, accs):
                             epoch_val_log += "%s=%f  " % (name, acc)
                         self.logger.info(epoch_val_log)
                     # TODO: save checkpoints
-                if self.metrics:
-                    names, accs = self.metrics.get()
+                if self.eval_metrics:
+                    names, accs = self.eval_metrics.get()
                     names, accs = to_list(names), to_list(accs)
                     for name, acc in zip(names, accs):
                         stats[name] = acc
@@ -176,6 +210,8 @@ class MXNetRunner(object):
                 if "init" not in self.config:
                     from mxnet.initializer import Uniform
                     self.config["init"] = Uniform(0.01)  # This is the default value for MXNet
+                if self.eval_metrics is None:
+                    self.eval_metrics = 'acc'
                 self.model.fit(train_data=self.train_data,
                                num_epoch=nb_epoch,
                                initializer=self.config["init"],
@@ -183,9 +219,8 @@ class MXNetRunner(object):
                                optimizer=self.config["optimizer"],
                                optimizer_params=self.config["optimizer_params"],
                                eval_data=self.val_data,
-                               # TODO: eval and validation metrics could be different
-                               eval_metric=self.metrics,
-                               validation_metric=self.metrics,
+                               eval_metric=self.eval_metrics,
+                               validation_metric=self.val_metrics,
                                batch_end_callback=mx.callback.Speedometer(
                                    self.config["batch_size"], self.config["log_interval"]),
                                epoch_end_callback=None if "model" not in self.config
@@ -213,3 +248,33 @@ class MXNetRunner(object):
     def find_free_port(self):
         """Finds a free port on the current node."""
         return find_free_port()
+
+
+def get_data_label(partition_data):
+    def combine_dict(dict1, dict2):
+        return {key: np.concatenate((value, dict2[key]), axis=0)
+                for (key, value) in dict1.items()}
+
+    def combine_list(list1, list2):
+        return [np.concatenate((list1[index], list2[index]), axis=0)
+                for index in range(0, len(list1))]
+
+    data_list = [data['x'] for data in partition_data]
+    label_list = [data['y'] for data in partition_data]
+    if isinstance(partition_data[0]['x'], dict):
+        data = reduce(lambda dict1, dict2: combine_dict(dict1, dict2), data_list)
+    elif isinstance(partition_data[0]['x'], np.ndarray):
+        data = reduce(lambda array1, array2: np.concatenate((array1, array2), axis=0),
+                      data_list)
+    elif isinstance(partition_data[0]['x'], list):
+        data = reduce(lambda list1, list2: combine_list(list1, list2), data_list)
+
+    if isinstance(partition_data[0]['y'], dict):
+        label = reduce(lambda dict1, dict2: combine_dict(dict1, dict2), label_list)
+    elif isinstance(partition_data[0]['y'], np.ndarray):
+        label = reduce(lambda array1, array2: np.concatenate((array1, array2), axis=0),
+                       label_list)
+    elif isinstance(partition_data[0]['y'], list):
+        label = reduce(lambda list1, list2: combine_list(list1, list2), data_list)
+
+    return data, label
--- python/orca/test/bigdl/orca/learn/mxnet/conftest.py
+++ python/orca/test/bigdl/orca/learn/mxnet/conftest.py
@@ -23,9 +23,14 @@ ray_ctx = None
 def rayonspark_fixture():
     from zoo import init_spark_on_local
     from zoo.ray import RayContext
+    global ray_ctx
     sc = init_spark_on_local(cores=8, spark_log_level="INFO")
     ray_ctx = RayContext(sc=sc, object_store_memory="1g")
     ray_ctx.init()
     yield
     ray_ctx.stop()
     sc.stop()
+
+
+def get_ray_ctx():
+    return ray_ctx
