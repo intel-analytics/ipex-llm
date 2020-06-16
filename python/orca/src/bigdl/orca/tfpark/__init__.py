902c680b6fa96ad1e4f9f7decfd404111464161e
Yang Wang <yang3.wang@intel.com>
2020-06-16 23:06:34 +0800

    Add tensorflow version check (#2459)
    * add tensorflow version check
    
    * remove disable check and add message if tf not installed
    
    * fix style

--- python/orca/src/bigdl/orca/tfpark/__init__.py
+++ python/orca/src/bigdl/orca/tfpark/__init__.py
@@ -14,6 +14,30 @@
 # limitations under the License.
 #
 
+
+def check_tf_version():
+    import logging
+    try:
+        import tensorflow as tf
+    except Exception as e:
+        raise RuntimeError("Importing TensorFlow failed, please install tensorflow 1.15.0.", e)
+
+    v_str = tf.__version__
+    major, minor, patch = v_str.split(".")
+    if v_str != "1.15.0":
+        if int(major) == 1:
+            logging.warning("\n######################### WARNING ##########################\n"
+                            "\nAnalytics Zoo TFPark has only been tested on TensorFlow 1.15.0,"
+                            " but your current TensorFlow installation is {}.".format(v_str) +
+                            "\nYou may encounter some version incompatibility issues. "
+                            "\n##############################################################")
+        else:
+            message = "Currently Analytics Zoo TFPark only supports TensorFlow 1.15.0, " + \
+                      "but your current TensorFlow installation is {}".format(v_str)
+            raise RuntimeError(message)
+
+check_tf_version()
+
 from .model import KerasModel
 from .estimator import TFEstimator
 from .tf_optimizer import TFOptimizer
