560b1dc6eb1947775d9cb160e484502778121dfd
Qiyuan Gong <qiyuan.gong@intel.com>
2020-06-29 17:42:26 +0800

    OpenVINO int8 model check support for version 10 (#2511)
    * OpenVINO changes int8 related keyword from `statistics` into `FakeQuantize` in model version 10. Added support with this change.

--- scala/orca/src/main/scala/com/intel/analytics/bigdl/orca/inference/OpenVinoInferenceSupportive.scala
+++ scala/orca/src/main/scala/com/intel/analytics/bigdl/orca/inference/OpenVinoInferenceSupportive.scala
@@ -97,7 +97,11 @@ object OpenVinoInferenceSupportive extends InferenceSupportive with Serializable
       val modelBytes = Files.readAllBytes(Paths.get(modelFilePath))
       val weightBytes = Files.readAllBytes(Paths.get(weightFilePath))
       val buffer = Source.fromBytes(modelBytes)
-      val isInt8 = buffer.getLines().count(_ matches ".*statistics.*") > 0
+      // For OpenVINO model version 9 or previous, check statistics keyword
+      // For OpenVINO model version 10 or later, check FakeQuantize keyword
+      val isInt8 = buffer
+        .getLines()
+        .count(_ matches ".*statistics.*|.*FakeQuantize.*") > 0
       buffer.close()
       new OpenVINOModel(new OpenVINOModelHolder(modelBytes, weightBytes),
         isInt8, batchSize, deviceType)
@@ -110,7 +114,11 @@ object OpenVinoInferenceSupportive extends InferenceSupportive with Serializable
                      batchSize: Int): OpenVINOModel = {
     timing("load OpenVINO IR") {
       val buffer = Source.fromBytes(modelBytes)
-      val isInt8 = buffer.getLines().count(_ matches ".*statistics.*") > 0
+      // For OpenVINO model version 9 or previous, check statistics keyword
+      // For OpenVINO model version 10 or later, check FakeQuantize keyword
+      val isInt8 = buffer
+        .getLines()
+        .count(_ matches ".*statistics.*|.*FakeQuantize.*") > 0
       buffer.close()
       new OpenVINOModel(new OpenVINOModelHolder(modelBytes, weightBytes),
         isInt8, batchSize, deviceType)
