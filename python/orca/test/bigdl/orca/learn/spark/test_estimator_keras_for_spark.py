0d8f27819df564ef645347584a4f5a3958315bac
jenniew <jenniewang123@gmail.com>
2020-08-11 12:24:30 -0700

    Predict result as xshard (#2590)
    * predict result as xshard
    
    * merge2
    
    * update predict result
    
    * update test
    
    * fix style

--- python/orca/test/bigdl/orca/learn/spark/test_estimator_keras_for_spark.py
+++ python/orca/test/bigdl/orca/learn/spark/test_estimator_keras_for_spark.py
@@ -22,6 +22,7 @@ import tensorflow as tf
 from bigdl.optim.optimizer import SeveralIteration
 from zoo.orca.learn.tf.estimator import Estimator
 from zoo.common.nncontext import *
+from zoo.orca.learn.tf.utils import convert_predict_to_dataframe
 import zoo.orca.data.pandas
 
 
@@ -70,6 +71,8 @@ class TestEstimatorForKeras(TestCase):
     def test_estimator_keras_xshards(self):
         import zoo.orca.data.pandas
 
+        tf.reset_default_graph()
+
         model = self.create_model()
         file_path = os.path.join(self.resource_path, "orca/learn/ncf.csv")
         data_shard = zoo.orca.data.pandas.read_csv(file_path)
@@ -104,10 +107,12 @@ class TestEstimatorForKeras(TestCase):
 
         data_shard = data_shard.transform_shard(transform)
         predictions = est.predict(data_shard).collect()
-        assert len(predictions[0]) == 2
+        assert predictions[0]['prediction'].shape[1] == 2
 
     def test_estimator_keras_xshards_options(self):
         import zoo.orca.data.pandas
+
+        tf.reset_default_graph()
 
         model = self.create_model()
         file_path = os.path.join(self.resource_path, "orca/learn/ncf.csv")
@@ -157,6 +162,8 @@ class TestEstimatorForKeras(TestCase):
     def test_estimator_keras_xshards_clip(self):
         import zoo.orca.data.pandas
 
+        tf.reset_default_graph()
+
         model = self.create_model_with_clip()
         file_path = os.path.join(self.resource_path, "orca/learn/ncf.csv")
         data_shard = zoo.orca.data.pandas.read_csv(file_path)
@@ -179,6 +186,8 @@ class TestEstimatorForKeras(TestCase):
 
     def test_estimator_keras_xshards_checkpoint(self):
         import zoo.orca.data.pandas
+
+        tf.reset_default_graph()
 
         import tensorflow.keras.backend as K
         K.clear_session()
@@ -227,6 +236,9 @@ class TestEstimatorForKeras(TestCase):
         shutil.rmtree(temp)
 
     def test_estimator_keras_dataframe(self):
+
+        tf.reset_default_graph()
+
         model = self.create_model()
         sc = init_nncontext()
         sqlcontext = SQLContext(sc)
@@ -253,6 +265,9 @@ class TestEstimatorForKeras(TestCase):
         assert len(predictions) == 10
 
     def test_estimator_keras_dataframe_no_fit(self):
+
+        tf.reset_default_graph()
+
         model = self.create_model()
         sc = init_nncontext()
         sqlcontext = SQLContext(sc)
@@ -271,6 +286,23 @@ class TestEstimatorForKeras(TestCase):
         assert 'prediction' in prediction_df.columns
         predictions = prediction_df.collect()
         assert len(predictions) == 10
+
+    def test_convert_predict_list_of_array(self):
+
+        tf.reset_default_graph()
+
+        sc = init_nncontext()
+        sqlcontext = SQLContext(sc)
+        rdd = sc.parallelize([(1, 2, 3), (4, 5, 6), (7, 8, 9)])
+        df = rdd.toDF(["feature", "label", "c"])
+        predict_rdd = df.rdd.map(lambda row: [np.array([1, 2]), np.array(0)])
+        resultDF = convert_predict_to_dataframe(df, predict_rdd)
+        resultDF.printSchema()
+        print(resultDF.collect()[0])
+        predict_rdd = df.rdd.map(lambda row: np.array(1))
+        resultDF = convert_predict_to_dataframe(df, predict_rdd)
+        resultDF.printSchema()
+        print(resultDF.collect()[0])
 
 
 if __name__ == "__main__":
