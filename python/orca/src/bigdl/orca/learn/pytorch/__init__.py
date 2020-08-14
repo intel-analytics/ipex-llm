d3d3492707d768721d6a47772a9e52de2776a8ac
Yina Chen <33650826+cyita@users.noreply.github.com>
2020-08-14 17:15:25 +0800

    Add orca spark pytorch estimator wrapper (#2683)
    * add orca spark pytorch estimator wrapper
    
    * add testcase
    
    * add DataLoader
    
    * add dataloader testcase
    
    * evaluate add dataloader
    
    * add jep tests
    
    * meet comments
    
    * add evaluateminibatch
    
    * remove evaluate & add creator testcase
    
    * style fix
    
    * fix style
    
    * add trainer
    
    * remove Trainer in init
    
    * fix testcase
    
    * fix
    
    * divide testcase
    
    * pandas backend

--- python/orca/src/bigdl/orca/learn/pytorch/__init__.py
+++ python/orca/src/bigdl/orca/learn/pytorch/__init__.py
@@ -13,8 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-from .pytorch_trainer import PyTorchTrainer
 from .training_operator import TrainingOperator
-from .pytorch_horovod_estimator import PyTorchHorovodEstimator
 from .estimator import Estimator
