630b9f03013979740b49fdbb65d107d28658898e
Yina Chen <33650826+cyita@users.noreply.github.com>
2020-08-25 15:09:56 +0800

    Orca PyTorchEstimatorLeNet MNIST Example (#2766)
    * orca pytorch mnist lenet example
    
    * add backend
    
    * lenet5
    
    * add readme
    
    * add evaluate
    
    * fix readme
    
    * fix readme
    
    * fix evaluate

--- python/orca/src/bigdl/orca/learn/pytorch/estimator.py
+++ python/orca/src/bigdl/orca/learn/pytorch/estimator.py
@@ -229,7 +229,7 @@ class PytorchSparkEstimatorWrapper(Estimator):
             val_feature_set = FeatureSet.sample_rdd(data.rdd.flatMap(to_sample))
             return self.estimator.evaluate(val_feature_set, validation_methods, batch_size)
         elif isinstance(data, DataLoader) or callable(data):
-            val_feature_set = FeatureSet.pytorch_dataloader(data, "", "")
+            val_feature_set = FeatureSet.pytorch_dataloader(data)
             return self.estimator.evaluate_minibatch(val_feature_set, validation_methods)
         else:
             raise ValueError("Data should be a SparkXShards, a DataLoader or a callable "
