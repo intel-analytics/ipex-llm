fbcc04845f642ba1a796288a0e99b20d2d664f10
Yang Wang <yang3.wang@intel.com>
2020-09-03 10:47:57 +0800

    fix duplicated ray worker logs (#2799)
    * fix multiple ray worker logs
    
    * refine comments
    
    * Update raycontext.py

--- python/orca/src/bigdl/orca/ray/raycontext.py
+++ python/orca/src/bigdl/orca/ray/raycontext.py
@@ -48,6 +48,32 @@ class JVMGuard:
             raise err
 
 
+def kill_redundant_log_monitors(redis_address):
+
+    """
+    Killing redundant log_monitor.py processes.
+    If multiple ray nodes are started on the same machine,
+    there will be multiple ray log_monitor.py processes
+    monitoring the same log dir. As a result, the logs
+    will be replicated multiple times and forwarded to driver.
+    See issue https://github.com/ray-project/ray/issues/10392
+    """
+
+    import psutil
+    import subprocess
+    log_monitor_processes = []
+    for proc in psutil.process_iter(["name", "cmdline"]):
+        cmdline = subprocess.list2cmdline(proc.cmdline())
+        is_log_monitor = "log_monitor.py" in cmdline
+        is_same_redis = "--redis-address={}".format(redis_address)
+        if is_log_monitor and is_same_redis in cmdline:
+            log_monitor_processes.append(proc)
+
+    if len(log_monitor_processes) > 1:
+        for proc in log_monitor_processes[1:]:
+            proc.kill()
+
+
 class RayServiceFuncGenerator(object):
     """
     This should be a pickable class.
@@ -204,6 +230,7 @@ class RayServiceFuncGenerator(object):
                             object_store_memory=self.object_store_memory,
                             extra_params=self.extra_params),
                         tag="raylet")
+                    kill_redundant_log_monitors(redis_address=redis_address)
 
             yield process_info
         return _start_ray_services
@@ -418,6 +445,9 @@ class RayContext(object):
             else:
                 self._start_cluster()
                 self._address_info = self._start_driver(num_cores=driver_cores)
+
+            print(self._address_info)
+            kill_redundant_log_monitors(self._address_info["redis_address"])
             self.initialized = True
         return self._address_info
 
