d798dc7cf2a75533e993329d5583e915ead87e4d
Le-Zheng <alettazheng@hotmail.com>
2020-09-28 09:37:26 +0800

    add spark3.0 support

--- scripts/deploy-spark2.sh
+++ scripts/deploy-spark2.sh
@@ -54,10 +54,13 @@ cd spark
 mv dl/pom.xml dl/pom.xml.origin
 cat dl/pom.xml.origin | sed 's/<artifactId>bigdl<\/artifactId>/<artifactId>bigdl-${SPARK_PLATFORM}<\/artifactId>/' | \
     sed 's/<artifactId>${spark-version.project}<\/artifactId>/<artifactId>${spark-version.project}-${SPARK_PLATFORM}<\/artifactId>/' > dl/pom.xml
-mv spark-version/1.5-plus/pom.xml spark-version/1.5-plus/pom.xml.origin
-cat spark-version/1.5-plus/pom.xml.origin | sed 's/<artifactId>1.5-plus<\/artifactId>/<artifactId>1.5-plus-${SPARK_PLATFORM}<\/artifactId>/' > spark-version/1.5-plus/pom.xml
+#mv spark-version/1.5-plus/pom.xml spark-version/1.5-plus/pom.xml.origin
+#cat spark-version/1.5-plus/pom.xml.origin | sed 's/<artifactId>1.5-plus<\/artifactId>/<artifactId>1.5-plus-${SPARK_PLATFORM}<\/artifactId>/' > spark-version/1.5-plus/pom.xml
 mv spark-version/2.0/pom.xml spark-version/2.0/pom.xml.origin
 cat spark-version/2.0/pom.xml.origin | sed 's/<artifactId>2.0<\/artifactId>/<artifactId>2.0-${SPARK_PLATFORM}<\/artifactId>/' > spark-version/2.0/pom.xml
+
+mv spark-version/3.0/pom.xml spark-version/3.0/pom.xml.origin
+cat spark-version/3.0/pom.xml.origin | sed 's/<artifactId>3.0<\/artifactId>/<artifactId>3.0-${SPARK_PLATFORM}<\/artifactId>/' > spark-version/3.0/pom.xml
 
 function deploy {
     mvn clean install -DskipTests -P sign -Dspark.version=$1 -DSPARK_PLATFORM=$2 $3
@@ -69,8 +72,10 @@ function deploy {
 deploy 2.1.1 SPARK_2.1 '-P spark_2.x'
 deploy 2.2.0 SPARK_2.2 '-P spark_2.x'
 deploy 2.3.1 SPARK_2.3 '-P spark_2.x'
-deploy 2.4.0 SPARK_2.4 '-P spark_2.x'
+deploy 2.4.3 SPARK_2.4 '-P spark_2.x'
+deploy 3.0.0 SPARK_3.0 '-P spark_3.x'
 
 mv dl/pom.xml.origin dl/pom.xml
-mv spark-version/1.5-plus/pom.xml.origin spark-version/1.5-plus/pom.xml
+#mv spark-version/1.5-plus/pom.xml.origin spark-version/1.5-plus/pom.xml
 mv spark-version/2.0/pom.xml.origin spark-version/2.0/pom.xml
+mv spark-version/3.0/pom.xml.origin spark-version/3.0/pom.xml
