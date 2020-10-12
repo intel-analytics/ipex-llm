a3007376dbf495a6dc44c19ecb801eb869d65a0b
dding3 <ding.ding@intel.com>
2020-10-12 13:18:24 -0700

    disable mac xgb ut (#2950)

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/nnframes/XgboostTrainSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/nnframes/XgboostTrainSpec.scala
@@ -39,29 +39,31 @@ class XgboostTrainSpec extends ZooSpecHelper {
   }
 
   "XGBRegressor train" should "work" in {
-    val spark = SparkSession.builder().getOrCreate()
-    import spark.implicits._
+    if (!(scala.util.Properties.isMac || scala.util.Properties.isWin)) {
+      val spark = SparkSession.builder().getOrCreate()
+      import spark.implicits._
 
-    val df = Seq(
-      (1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 1.0f, 2.0f, 4.0f, 8.0f, 3.0f, 116.3668f),
-      (1.0f, 3.0f, 8.0f, 6.0f, 5.0f, 9.0f, 5.0f, 6.0f, 7.0f, 4.0f, 116.367f),
-      (2.0f, 1.0f, 5.0f, 7.0f, 6.0f, 7.0f, 4.0f, 1.0f, 2.0f, 3.0f, 116.367f),
-      (2.0f, 1.0f, 4.0f, 3.0f, 6.0f, 1.0f, 3.0f, 2.0f, 1.0f, 3.0f, 116.3668f)
-    ).toDF("f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "label")
+      val df = Seq(
+        (1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 1.0f, 2.0f, 4.0f, 8.0f, 3.0f, 116.3668f),
+        (1.0f, 3.0f, 8.0f, 6.0f, 5.0f, 9.0f, 5.0f, 6.0f, 7.0f, 4.0f, 116.367f),
+        (2.0f, 1.0f, 5.0f, 7.0f, 6.0f, 7.0f, 4.0f, 1.0f, 2.0f, 3.0f, 116.367f),
+        (2.0f, 1.0f, 4.0f, 3.0f, 6.0f, 1.0f, 3.0f, 2.0f, 1.0f, 3.0f, 116.3668f)
+      ).toDF("f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "label")
 
-    val vectorAssembler = new VectorAssembler()
-      .setInputCols(Array("f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10"))
-      .setOutputCol("features")
-    val assembledDf = vectorAssembler.transform(df).select("features", "label").cache()
+      val vectorAssembler = new VectorAssembler()
+        .setInputCols(Array("f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10"))
+        .setOutputCol("features")
+      val assembledDf = vectorAssembler.transform(df).select("features", "label").cache()
 
-    val xgbRf0 = new XGBRegressor()
-    xgbRf0.setNthread(1)
-    val xgbRegressorModel0 = xgbRf0.fit(assembledDf)
-    val y0 = xgbRegressorModel0.transform(assembledDf)
+      val xgbRf0 = new XGBRegressor()
+      xgbRf0.setNthread(1)
+      val xgbRegressorModel0 = xgbRf0.fit(assembledDf)
+      val y0 = xgbRegressorModel0.transform(assembledDf)
 
-    xgbRegressorModel0.save("/tmp/test")
-    val model = XGBRegressorModel.load("/tmp/test")
-    val y0_0 = model.transform(assembledDf)
-    assert(y0_0.except(y0).count()==0)
+      xgbRegressorModel0.save("/tmp/test")
+      val model = XGBRegressorModel.load("/tmp/test")
+      val y0_0 = model.transform(assembledDf)
+      assert(y0_0.except(y0).count()==0)
+    }
   }
 }
