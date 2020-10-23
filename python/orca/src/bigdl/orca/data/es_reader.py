29f3ab04f8696b6ebdcb6a2ca10ca2613204d458
dding3 <ding.ding@intel.com>
2020-10-23 12:06:34 -0700

    Support read/write from es (#2984)
    * add support read/save to ES

---
+++ python/orca/src/bigdl/orca/data/es_reader.py
@@ -0,0 +1,70 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from pyspark import SparkContext
+from pyspark.sql.types import *
+from pyspark.sql import SQLContext
+
+
+class ESReader:
+    """
+    Primary DataFrame-based loading data from elastic search interface,
+    defining API to read data from ES to DataFrame.
+    """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def readData(esConfig, esResource, schema=None):
+        """
+        Read the data from elastic search into DataFrame.
+        :param esConfig Dictionary which represents configuration for
+               elastic search(eg. ip, port etc).
+        :param esResource resource file in elastic search.
+        :param schema Optional. Defines the schema of Spark dataframe.
+                If each column in Es is single value, don't need set schema.
+        :return Spark DataFrame. Each row represents a document in ES.
+        """
+        sc = SparkContext.getOrCreate()
+        sqlContext = SQLContext.getOrCreate(sc)
+        spark = sqlContext.sparkSession
+
+        reader = spark.read.format("org.elasticsearch.spark.sql")
+
+        for key in esConfig:
+            reader.option(key, esConfig[key])
+        if schema:
+            reader.schema(schema)
+
+        df = reader.load(esResource)
+        return df
+
+    @staticmethod
+    def save2ES(esConfig, esResource, df):
+        """
+        Read the data from elastic search into DataFrame.
+        :param esConfig Dictionary which represents configuration for
+               elastic search(eg. ip, port etc).
+        :param esResource resource file in elastic search.
+        :param df Spark DataFrame that will be saved.
+        """
+        wdf = df.write.format("org.elasticsearch.spark.sql")\
+            .option("es.resource", esResource)
+
+        for key in esConfig:
+            wdf.option(key, esConfig[key])
+
+        wdf.save()
