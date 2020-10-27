b027377c68cb6b2f8844307c2530516fca5738eb
Le-Zheng <alettazheng@hotmail.com>
2020-10-27 09:38:47 +0800

    support jdk11 in Spark3 & spark2.4

--- spark/dl/src/main/scala/com/intel/analytics/bigdl/models/maskrcnn/Utils.scala
+++ spark/dl/src/main/scala/com/intel/analytics/bigdl/models/maskrcnn/Utils.scala
@@ -21,7 +21,7 @@ import breeze.linalg.{*, dim, max}
 import com.intel.analytics.bigdl.nn.ResizeBilinear
 import com.intel.analytics.bigdl.nn.abstractnn.DataFormat
 import com.intel.analytics.bigdl.tensor.Tensor
-import com.sun.xml.internal.bind.v2.TODO
+//import com.sun.xml.internal.bind.v2.TODO
 
 import scala.collection.mutable.ArrayBuffer
 
