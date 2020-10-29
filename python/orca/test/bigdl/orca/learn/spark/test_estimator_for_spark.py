bd4822ecb5c488345dd5a325998cd9a064a7e673
Yang Wang <yang3.wang@intel.com>
2020-10-29 15:39:39 +0800

    Disable predict on TFDataDataset to prevent returning incorrect result silently (#3008)
    * Disable predict on TFDataDataset to prevent returning incorrect result silently
    
    * fix tests
    
    * fix style

--- python/orca/test/bigdl/orca/learn/spark/test_estimator_for_spark.py
+++ python/orca/test/bigdl/orca/learn/spark/test_estimator_for_spark.py
@@ -425,10 +425,6 @@ class TestEstimatorForGraph(TestCase):
 
         dataset = dataset_creator()
 
-        dataset = tf.data.Dataset.from_tensor_slices((np.random.randint(0, 200, size=(100,)),
-                                                      np.random.randint(0, 50, size=(100,)),
-                                                      np.ones(shape=(100,), dtype=np.int32)))
-
         est = Estimator.from_graph(
             inputs=[model.user, model.item],
             labels=[model.label],
@@ -443,12 +439,6 @@ class TestEstimatorForGraph(TestCase):
 
         result = est.evaluate(dataset, batch_size=4)
         assert 'loss' in result
-
-        predict_dataset = tf.data.Dataset.from_tensor_slices((
-            np.random.randint(0, 200, size=(20,)),
-            np.random.randint(0, 50, size=(20,))))
-        predictions = est.predict(predict_dataset).collect()
-        assert predictions[0]['prediction'].shape[1] == 2
 
     def test_estimator_graph_tf_dataset(self):
 
