c651ad5c2de429d9cb82ed475cda8244c59f31d7
Le-Zheng <alettazheng@hotmail.com>
2020-12-02 16:36:38 +0800

    update

--- pyspark/setup.py
+++ pyspark/setup.py
@@ -95,9 +95,7 @@ def setup_package():
         package_data={"bigdl.share": ['bigdl/share/lib', 'bigdl/share/conf', 'bigdl/share/bin']},
         classifiers=[
             'License :: OSI Approved :: Apache Software License',
-            'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
