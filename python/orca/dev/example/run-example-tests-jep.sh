92106a9e53031785877804890ac6476b9e8d9601
Yina Chen <33650826+cyita@users.noreply.github.com>
2020-12-03 15:37:46 +0800

    add orca pytorch & tf examples to jenkins (#3161)

--- python/orca/dev/example/run-example-tests-jep.sh
+++ python/orca/dev/example/run-example-tests-jep.sh
@@ -17,3 +17,17 @@ fi
 
 python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/pytorch/train/mnist/main.py --dir analytics-zoo-data/data
 
+echo "start example for orca MNIST"
+#timer
+start=$(date "+%s")
+if [ -f analytics-zoo-data/data/MNIST ]
+then
+    echo "MNIST already exists"
+else
+    wget -nv $FTP_URI/analytics-zoo-data/mnist/train-labels-idx1-ubyte.gz -P analytics-zoo-data/data/MNIST/raw
+    wget -nv $FTP_URI/analytics-zoo-data/mnist/train-images-idx3-ubyte.gz -P analytics-zoo-data/data/MNIST/raw
+    wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-labels-idx1-ubyte.gz -P analytics-zoo-data/data/MNIST/raw
+    wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-images-idx3-ubyte.gz -P analytics-zoo-data/data/MNIST/raw
+fi
+
+python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/pytorch/mnist/lenet_mnist.py --dir analytics-zoo-data/data
