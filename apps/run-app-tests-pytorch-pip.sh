3ad8cf01ec6c4e81cf59ab00dc44557b19af0679
GavinGu07 <55721214+GavinGu07@users.noreply.github.com>
2020-12-07 15:17:39 +0800

    add pytorch app tests (#3125)
    * add pytorch app tests
    
    * update script
    
    * fix ANALYTICS_ZOO_HOME
    
    * fix ANALYTICS_ZOO_HOME
    
    * delete python app test

---
+++ apps/run-app-tests-pytorch-pip.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+export ANALYTICS_ZOO_HOME=${ANALYTICS_ZOO_ROOT}/dist
+
+clear_up () {
+    echo "Clearing up environment. Uninstalling analytics-zoo"
+    pip uninstall -y analytics-zoo
+    pip uninstall -y bigdl
+    pip uninstall -y pyspark
+}
+
+chmod +x ${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh
+
+set -e
+
+echo "#start app test for pytorch face-generation"
+#timer
+start=$(date "+%s")
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/apps/pytorch/face_generation
+sed -i '/get_ipython()/d' ${ANALYTICS_ZOO_HOME}/apps/pytorch/face_generation.py
+sed -i '/plt./d' ${ANALYTICS_ZOO_HOME}/apps/pytorch/face_generation.py
+python ${ANALYTICS_ZOO_HOME}/apps/pytorch/face_generation.py
+exit_status=$?
+if [ $exit_status -ne 0 ];
+then
+    clear_up
+    echo "pytorch face-generation failed"
+    exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now-start))
+echo "#pytorch face-generation time used:$time seconds"
