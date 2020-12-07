5a05a7ebd9bf1941dde6a227889ddc96bc41e682
Yang Wang <yang3.wang@intel.com>
2020-12-07 13:50:18 +0800

    add tf2 mnist test (#3179)
    * add tf2 mnist test
    
    * add to ray test
    
    * add horovod tf test6
    
    * remove test

---
+++ python/orca/dev/example/run-example-test-ray-horovod-tf.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export SPARK_HOME=$SPARK_HOME
+export MASTER=local[4]
+export FTP_URI=$FTP_URI
+export ANALYTICS_ZOO_ROOT=$ANALYTICS_ZOO_ROOT
+export ANALYTICS_ZOO_HOME=$ANALYTICS_ZOO_ROOT/dist
+export ANALYTICS_ZOO_JAR=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
+export ANALYTICS_ZOO_PYZIP=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*python-api.zip"`
+export ANALYTICS_ZOO_CONF=${ANALYTICS_ZOO_HOME}/conf/spark-analytics-zoo.conf
+export PYTHONPATH=${ANALYTICS_ZOO_PYZIP}:$PYTHONPATH
+export BIGDL_JARS=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
+
+set -e
+
+echo "Start ray horovod tf example tests"
+
+echo "#1 Start tf2 estimator lenet"
+start=$(date "+%s")
+python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/tf2/mnist/lenet_mnist_keras.py --cluster_mode local --max_epoch 1
+now=$(date "+%s")
+time1=$((now-start))
+
+echo "Ray example tests finished"
+echo "#1 tf2 estimator lenet used:$time1 seconds"
