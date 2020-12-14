841296c353b74b15666a902ad98c197b33ddfd36
Yang Wang <yang3.wang@intel.com>
2020-12-14 10:41:12 +0800

    add ray resnet 50 example test (#3217)
    * add ray resnet 50 example test
    
    * use smaller batch size

--- python/orca/dev/example/run-example-test-ray-horovod-tf.sh
+++ python/orca/dev/example/run-example-test-ray-horovod-tf.sh
@@ -15,11 +15,18 @@ set -e
 
 echo "Start ray horovod tf example tests"
 
-echo "#1 Start tf2 estimator lenet"
+echo "#1 tf2 estimator resnet 50 example"
 start=$(date "+%s")
-python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/tf2/mnist/lenet_mnist_keras.py --cluster_mode local --max_epoch 1
+python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/tf2/resnet/resnet-50-imagenet.py --use_dummy_data --benchmark --batch_size_per_worker 4
 now=$(date "+%s")
 time1=$((now-start))
 
+echo "#2 Start tf2 estimator lenet"
+start=$(date "+%s")
+python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/learn/tf2/mnist/lenet_mnist_keras.py --cluster_mode local --max_epoch 1
+now=$(date "+%s")
+time2=$((now-start))
+
 echo "Ray example tests finished"
-echo "#1 tf2 estimator lenet used:$time1 seconds"
+echo "#1 tf2 estimator resnet 50 time used:$time1 seconds"
+echo "#2 tf2 estimator lenet used:$time2 seconds"
