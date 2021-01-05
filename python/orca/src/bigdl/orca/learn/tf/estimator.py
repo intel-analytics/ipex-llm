c67d22ad287dc1452548e2f86514ec37009312ea
jenniew <jenniewang123@gmail.com>
2021-01-05 13:18:25 -0800

    Add memory type support for Orca tf estimator (#3280)
    * add mem type for dataframe dataset
    
    * add ZooTrigger support
    
    * update mem type
    
    * add test
    
    * update
    
    * update mem type and orca context
    
    * update orca context docs
    
    * update orca context with comments
    
    * fix style

--- python/orca/src/bigdl/orca/learn/tf/estimator.py
+++ python/orca/src/bigdl/orca/learn/tf/estimator.py
@@ -17,11 +17,9 @@ from pyspark.sql import DataFrame
 
 from bigdl.optim.optimizer import MaxEpoch
 
-from zoo.tfpark.tf_dataset import TFNdarrayDataset
-from zoo.tfpark.tf_dataset import _standarize_feature_label_dataset
 from zoo.tfpark.tf_dataset import _standardize_keras_target_data
-
 from zoo.common.utils import load_from_file
+from zoo.orca import OrcaContext
 from zoo.orca.data.tf.data import Dataset, TFDataDataset2
 from zoo.orca.data import SparkXShards
 from zoo.orca.learn.tf.utils import *
@@ -256,7 +254,7 @@ def is_tf_data_dataset(data):
 
 def to_dataset(data, batch_size, batch_per_thread, validation_data,
                feature_cols, labels_cols, hard_code_batch_size,
-               sequential_order, shuffle, auto_shard_files):
+               sequential_order, shuffle, auto_shard_files, memory_type="DRAM"):
     # todo wrap argument into kwargs
     if validation_data:
         if isinstance(data, SparkXShards):
@@ -278,6 +276,7 @@ def to_dataset(data, batch_size, batch_per_thread, validation_data,
                                         batch_per_thread,
                                         validation_data,
                                         hard_code_batch_size=hard_code_batch_size,
+                                        memory_type=memory_type,
                                         sequential_order=sequential_order,
                                         shuffle=shuffle)
     elif isinstance(data, Dataset):
@@ -290,6 +289,7 @@ def to_dataset(data, batch_size, batch_per_thread, validation_data,
                                            batch_per_thread,
                                            hard_code_batch_size,
                                            validation_data,
+                                           memory_type,
                                            sequential_order,
                                            shuffle
                                            )
@@ -422,12 +422,14 @@ class TensorFlowEstimator(Estimator):
         if checkpoint_trigger is not None:
             checkpoint_trigger = Trigger.convert_trigger(checkpoint_trigger)
 
+        memory_type = OrcaContext.train_data_store
         dataset = to_dataset(data, batch_size=batch_size, batch_per_thread=-1,
                              validation_data=validation_data,
                              feature_cols=feature_cols, labels_cols=labels_cols,
                              hard_code_batch_size=hard_code_batch_size,
                              sequential_order=False, shuffle=True,
-                             auto_shard_files=auto_shard_files
+                             auto_shard_files=auto_shard_files,
+                             memory_type=memory_type
                              )
 
         if feed_dict is not None:
@@ -607,7 +609,7 @@ class KerasEstimator(Estimator):
             hard_code_batch_size=False,
             session_config=None,
             checkpoint_trigger=None,
-            auto_shard_files=False,
+            auto_shard_files=True
             ):
         """
         Train this keras model with train data.
@@ -655,15 +657,14 @@ class KerasEstimator(Estimator):
             data = data.map(_standardize_keras_target_data)
             validation_data = validation_data.map(_standardize_keras_target_data)
 
+        memory_type = OrcaContext.train_data_store
         dataset = to_dataset(data, batch_size=batch_size, batch_per_thread=-1,
                              validation_data=validation_data,
                              feature_cols=feature_cols, labels_cols=labels_cols,
                              hard_code_batch_size=hard_code_batch_size,
                              sequential_order=False, shuffle=True,
-                             auto_shard_files=auto_shard_files)
-
-        if isinstance(dataset, TFNdarrayDataset):
-            dataset = _standarize_feature_label_dataset(dataset, self.model.model)
+                             auto_shard_files=auto_shard_files,
+                             memory_type=memory_type)
 
         self.tf_optimizer = TFOptimizer.from_keras(self.model.model, dataset,
                                                    model_dir=self.model.model_dir,
