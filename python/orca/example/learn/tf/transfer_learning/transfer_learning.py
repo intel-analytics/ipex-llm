76b6399e9079648da76473bdb50a318770e55115
Chirs_Leo <597714572@qq.com>
2021-01-18 11:55:04 +0800

    Add tensorflow license to transfer_learning (#3313)
    * fix docs
    
    * fix
    
    * fix
    
    * license
    
    * @title

--- python/orca/example/learn/tf/transfer_learning/transfer_learning.py
+++ python/orca/example/learn/tf/transfer_learning/transfer_learning.py
@@ -13,8 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+#
+# Copyright 2019 The TensorFlow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # This example shows how to classify cats vs dogs images
-# by using transfer learning from a pre-trained network on Orca.
+# by using transfer learning from a pre-trained network on Orca,
+# and is adapted from
 # https://github.com/tensorflow/docs/blob/master/site/en/r1/tutorials/images/transfer_learning.ipynb
 
 import os
