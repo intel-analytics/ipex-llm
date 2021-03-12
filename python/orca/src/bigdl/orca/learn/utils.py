83945842e1bf20ad2ceca021a8fefc43f7faf78e
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-03-12 04:39:56 +0000

    fit with xshards of pandas df (#3551)

--- python/orca/src/bigdl/orca/learn/utils.py
+++ python/orca/src/bigdl/orca/learn/utils.py
@@ -228,6 +228,30 @@ def arrays2dict(iter, feature_cols, label_cols, shard_size=None):
         yield generate_output(feature_lists, label_lists)
 
 
+def transform_to_shard_dict(data, feature_cols, label_cols=None):
+    def to_shard_dict(df):
+        result = dict()
+        result["x"] = tuple([df[feature_col].to_numpy() for feature_col in feature_cols])
+        if label_cols:
+            result["y"] = df[label_cols[0]].to_numpy()
+        return result
+    data = data.transform_shard(to_shard_dict)
+    return data
+
+
+def process_xshards_of_pandas_dataframe(data, feature_cols, label_cols=None, validation_data=None,
+                                        mode=None):
+    data = transform_to_shard_dict(data, feature_cols, label_cols)
+    if mode == "fit":
+        if validation_data:
+            assert validation_data._get_class_name() == 'pandas.core.frame.DataFrame',\
+                "train data and validation data should be both XShards of Pandas DataFrame"
+            validation_data = transform_to_shard_dict(validation_data, feature_cols, label_cols)
+        return data, validation_data
+    else:
+        return data
+
+
 def _dataframe_to_xshards(data, feature_cols, label_cols=None):
     from zoo.orca import OrcaContext
     schema = data.schema
