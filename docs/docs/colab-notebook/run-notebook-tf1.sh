5600fab8a36566a4afa977d21626fddb1ddc9632
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-03-18 03:21:48 +0000

    update quickstart notebook test (#3605)

--- docs/docs/colab-notebook/run-notebook-tf1.sh
+++ docs/docs/colab-notebook/run-notebook-tf1.sh
@@ -1,7 +1,19 @@
 # run with ipython
 
 echo "#1 start test for tf_lenet_mnist.ipynb "
+#replace '!pip install --pre' to '#pip install --pre', here we test pr with built whl package. In nightly-build job, we test only use "ipython notebook" for pre-release Analytics Zoo
+start=$(date "+%s")
+sed -i 's/\!/#/g' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/tf_lenet_mnist.ipynb
 ipython ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/tf_lenet_mnist.ipynb
+now=$(date "+%s")
+time1=$((now - start))
 
 echo "#2 start test for keras_lenet_mnist.ipynb "
+start=$(date "+%s")
+sed -i 's/\!/#/g' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/keras_lenet_mnist.ipynb
 ipython ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/keras_lenet_mnist.ipynb
+now=$(date "+%s")
+time2=$((now - start))
+
+echo "#1 tf_lenet_mnist time used: $time1 seconds"
+echo "#2 keras_lenet_mnist time used: $time2 seconds"
--- docs/docs/colab-notebook/run-notebook-tf2.sh
+++ docs/docs/colab-notebook/run-notebook-tf2.sh
@@ -1,4 +1,19 @@
 # run with ipython
 
-echo "#1 start test for tf2_lenet_mnist.ipynb "
+echo "#1 start test for tf2_lenet_mnist.ipynb"
+#replace '!pip install --pre' to '#pip install --pre', here we test pr with built whl package. In nightly-build job, we test only use "ipython notebook" for pre-release Analytics Zoo
+start=$(date "+%s")
+sed -i 's/\!/#/g' $ANALYTICS_ZOO_HOME/docs/docs/colab-notebook/orca/quickstart/tf2_keras_lenet_mnist.ipynb
 ipython ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/tf2_keras_lenet_mnist.ipynb
+now=$(date "+%s")
+time1=$((now - start))
+
+echo "#2 start test for ncf_dataframe.ipynb"
+start=$(date "+%s")
+sed -i 's/\!/#/g' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/ncf_dataframe.ipynb
+ipython ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/ncf_dataframe.ipynb
+now=$(date "+%s")
+time2=$((now - start))
+
+echo "#1 tf2_keras_lenet_mnist time used: $time1 seconds"
+echo "#2 ncf_dataframe time used: $time2 seconds"
