0cdc018f3ae13ca8e621216cb132e1dea4d730cc
Qiyuan Gong <qiyuan.gong@intel.com>
2021-03-18 16:36:28 +0800

    Enable Cluster Serving message encrytpion (#3560)
    * Add Python and Scala AES CBC & GCM methods.
    * Add Cluster Serving message encryption feature.
    * Fix typo in encryption unit test.

--- scala/orca/src/test/scala/com/intel/analytics/bigdl/orca/inference/EncryptSpec.scala
+++ scala/orca/src/test/scala/com/intel/analytics/bigdl/orca/inference/EncryptSpec.scala
@@ -18,14 +18,17 @@
 package com.intel.analytics.zoo.pipeline.inference
 
 import java.io.File
-
 import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
+
+import java.util.Base64
+import scala.util.Random
 
 class EncryptSpec extends FunSuite with Matchers with BeforeAndAfterAll
   with InferenceSupportive with EncryptSupportive {
 
-  val plain = "hello world, hello scala, hello encrypt, come on UNITED!!!"
-  val secrect = "analytics-zoo"
+  // val plain = "hello world, hello scala, hello encrypt, come on UNITED!!!"
+  val plain = Random.nextString(224 * 224 * 3 * 16)
+  val secret = "analytics-zoo"
   val salt = "intel-analytics"
 
   var tempDir: File = _
@@ -39,20 +42,56 @@ class EncryptSpec extends FunSuite with Matchers with BeforeAndAfterAll
     tempDir.delete()
   }
 
-  test("plain text should be encrypted") {
-    val encrypted = encryptWithAES256(plain, secrect, salt)
-    val decrypted = decryptWithAES256(encrypted, secrect, salt)
+  test("AES CBC plain text should be encrypted") {
+    var encrypted = timing(s"CBC 128 encryption") {
+      encryptWithAESCBC(plain, secret, salt)
+    }
+    var decrypted = timing(s"CBC 128 decryption") {
+      decryptWithAESCBC(encrypted, secret, salt)
+    }
+    println("CBC128 Text increase: " + encrypted.length * 100.0/ plain.length)
     decrypted should be (plain)
+    encrypted = timing(s"CBC 256 encryption") {
+      encryptWithAESCBC(plain, secret, salt, 256)
+    }
+    println("CBC256 Text increase: " + encrypted.length * 100.0/ plain.length)
+    decrypted = timing(s"CBC 256 decryption") {
+      decryptWithAESCBC(encrypted, secret, salt, 256)
+    }
   }
 
-  test("plain file should be encrypted") {
+  test("AES GCM plain text should be encrypted") {
+    var encrypted = timing(s"GCM 128 encryption") {
+      encryptWithAESGCM(plain, secret, salt)
+    }
+    println("GCM128 Text increase: " + encrypted.length * 100.0 / plain.length)
+    // println(encrypted)
+    var decrypted = timing(s"GCM 128 decryption") {
+      decryptWithAESGCM(encrypted, secret, salt)
+    }
+    val bytes = Base64.getDecoder.decode(encrypted)
+    timing(s"GCM 128 bytes decryption") {
+      decryptBytesWithAESGCM(bytes, secret, salt, 128)
+    }
+    decrypted should be (plain)
+    encrypted = timing(s"GCM 256 encryption") {
+      encryptWithAESGCM(plain, secret, salt, 256)
+    }
+    println("GCM256 Text increase: " + encrypted.length * 100.0 / plain.length )
+    // println(encrypted)
+    decrypted = timing(s"GCM 256 decryption") {
+      decryptWithAESGCM(encrypted, secret, salt, 256)
+    }
+  }
+
+  test("AES CBC plain file should be encrypted") {
     val file = getClass.getResource("/application.conf")
     val encryptedFile = tempDir.getAbsolutePath + "/" + file.getFile.split("/").last + ".encrpyted"
-    encryptFileWithAES256(file.getFile, secrect, salt, encryptedFile)
+    encryptFileWithAESCBC(file.getFile, secret, salt, encryptedFile)
     new File(encryptedFile).exists() should be (true)
-    val decrypted = decryptFileWithAES256(encryptedFile, secrect, salt)
+    val decrypted = decryptFileWithAESCBC(encryptedFile, secret, salt)
     val decryptedFile = encryptedFile + ".decrypted"
-    decryptFileWithAES256(encryptedFile, secrect, salt, decryptedFile)
+    decryptFileWithAESCBC(encryptedFile, secret, salt, decryptedFile)
     new File(decryptedFile).exists() should be (true)
     val source = scala.io.Source.fromFile(file.getFile)
     val plain = try source.mkString finally source.close()
@@ -66,8 +105,8 @@ class EncryptSpec extends FunSuite with Matchers with BeforeAndAfterAll
     val weightFile = s"$dir/resnet_v1_50.bin"
     val decryptedModelFile = modelFile + ".encrpyted"
     val decryptedWeightFile = weightFile + ".encrpyted"
-    // encryptFileWithAES256(modelFile, secrect, salt, decryptedModelFile, "ISO-8859-1")
-    // encryptFileWithAES256(weightFile, secrect, salt, decryptedWeightFile, "ISO-8859-1")
+    // encryptFileWithAESCBC(modelFile, secrect, salt, decryptedModelFile, "ISO-8859-1")
+    // encryptFileWithAESCBC(weightFile, secrect, salt, decryptedWeightFile, "ISO-8859-1")
     val model = new InferenceModel(1)
     model.doLoadEncryptedOpenVINO(decryptedModelFile, decryptedWeightFile, secrect, salt)
   }
