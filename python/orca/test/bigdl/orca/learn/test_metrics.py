100763d25aef456a222910971254b8b9a74ca5d2
yaxinchen666 <51110939+yaxinchen666@users.noreply.github.com>
2021-03-24 16:37:08 +0800

    Fix Orca pytorch metrics MAE and MSE (#3634)
    * modify MAE and MSE in pytorch metrics
    
    Signed-off-by: yaxinchen666 <leepace666666@sjtu.edu.cn>
    
    * modify MAE and MSE in pytorch metrics
    
    Signed-off-by: yaxinchen666 <leepace666666@sjtu.edu.cn>
    
    * change one unit test to tensors with different dimensions
    
    Signed-off-by: yaxinchen666 <leepace666666@sjtu.edu.cn>
    
    * minimize fault tolerance and add comment
    
    Signed-off-by: yaxinchen666 <leepace666666@sjtu.edu.cn>
    
    * fix typo
    
    Signed-off-by: yaxinchen666 <leepace666666@sjtu.edu.cn>

--- python/orca/test/bigdl/orca/learn/test_metrics.py
+++ python/orca/test/bigdl/orca/learn/test_metrics.py
@@ -94,6 +94,14 @@ def test_torch_MAE():
     target = torch.tensor([[0, 1], [0, 1]])
     m(pred, target)
     assert m.compute() == 0.875
+    pred = torch.tensor([[1.5, 2.5], [1.0, 1.0]])
+    target = torch.tensor([[0.2, 1.1], [0.5, 1.0]])
+    m(pred, target)
+    assert abs(m.compute() - 0.85) < 1e-7   # add fault tolerance for floating point precision
+    pred = torch.tensor([[1.5, 2.5, 1.5, 2.5], [1.8, 2.0, 0.5, 4.5]])
+    target = torch.tensor([[0, 1, 0, 0], [0, 1, 2, 2]])
+    m(pred, target)
+    assert abs(m.compute() - 1.2) < 1e-7
 
 
 def test_torch_MSE():
@@ -107,6 +115,14 @@ def test_torch_MSE():
     target = torch.tensor([[1, 1], [0, 1]])
     m(pred, target)
     assert m.compute() == 1.25
+    pred = torch.tensor([[1.3, 1.0], [0.2, 1.0]])
+    target = torch.tensor([[1.1, 1.0], [0.0, 1.0]])
+    m(pred, target)
+    assert abs(m.compute() - 0.84) < 1e-7
+    pred = torch.tensor([[1.2, 1.2, 1.2, 1.8], [0.2, 0.8, 0.9, 1.1]])
+    target = torch.tensor([[1, 1, 1, 2], [0, 1, 1, 1]])
+    m(pred, target)
+    assert abs(m.compute() - 0.517) < 1e-7
 
 
 def test_torch_BinaryCrossEntropy():
