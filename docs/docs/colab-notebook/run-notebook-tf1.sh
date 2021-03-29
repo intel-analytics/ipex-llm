8091f0cfa3206bd8f30f2197793efaa3fdb321b5
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-03-29 02:32:39 +0100

    update quickstart test (#3650)

--- docs/docs/colab-notebook/run-notebook-tf1.sh
+++ docs/docs/colab-notebook/run-notebook-tf1.sh
@@ -1,17 +1,43 @@
-# run with ipython
+#!/usr/bin/env bash
+clear_up() {
+  echo "Clearing up environment. Uninstalling analytics-zoo"
+  pip uninstall -y analytics-zoo
+  pip uninstall -y bigdl
+  pip uninstall -y pyspark
+}
+
+set -e 
 
 echo "#1 start test for tf_lenet_mnist.ipynb "
 #replace '!pip install --pre' to '#pip install --pre', here we test pr with built whl package. In nightly-build job, we test only use "ipython notebook" for pre-release Analytics Zoo
 start=$(date "+%s")
-sed -i 's/\!/#/g' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/tf_lenet_mnist.ipynb
-ipython ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/tf_lenet_mnist.ipynb
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/tf_lenet_mnist
+sed -i '/get_ipython/s/^/#/' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/tf_lenet_mnist.py
+python ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/tf_lenet_mnist.py
+
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "tf_lenet_mnist failed"
+  exit $exit_status
+fi
+
 now=$(date "+%s")
 time1=$((now - start))
 
 echo "#2 start test for keras_lenet_mnist.ipynb "
 start=$(date "+%s")
-sed -i 's/\!/#/g' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/keras_lenet_mnist.ipynb
-ipython ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/keras_lenet_mnist.ipynb
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/keras_lenet_mnist
+sed -i '/get_ipython/s/^/#/' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/keras_lenet_mnist.py
+python ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart/keras_lenet_mnist.py
+
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "keras_lenet_mnist failed"
+  exit $exit_status
+fi
+
 now=$(date "+%s")
 time2=$((now - start))
 
