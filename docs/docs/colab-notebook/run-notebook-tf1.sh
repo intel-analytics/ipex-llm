95dd57cc72044210e42883e43a21691d1e83f2cd
Yu Shan <shan.yu@intel.com>
2021-03-29 16:00:28 +0800

    add autotest for zouwu quick start notebook (#3675)

--- docs/docs/colab-notebook/run-notebook-tf1.sh
+++ docs/docs/colab-notebook/run-notebook-tf1.sh
@@ -6,7 +6,7 @@ clear_up() {
   pip uninstall -y pyspark
 }
 
-set -e 
+set -e
 
 echo "#1 start test for tf_lenet_mnist.ipynb "
 #replace '!pip install --pre' to '#pip install --pre', here we test pr with built whl package. In nightly-build job, we test only use "ipython notebook" for pre-release Analytics Zoo
@@ -41,5 +41,25 @@ fi
 now=$(date "+%s")
 time2=$((now - start))
 
+echo "#3 start test for zouwu_autots_nyc_taxi.ipynb "
+if [ ! -f nyc_taxi.csv ]; then
+  wget https://raw.githubusercontent.com/numenta/NAB/v1.0/data/realKnownCause/nyc_taxi.csv
+fi
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/zouwu/zouwu_autots_nyc_taxi
+sed -i '/get_ipython/s/^/#/' ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/zouwu/zouwu_autots_nyc_taxi.py
+start=$(date "+%s")
+python ${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/zouwu/zouwu_autots_nyc_taxi.py
+
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "keras_lenet_mnist failed"
+  exit $exit_status
+fi
+
+now=$(date "+%s")
+time3=$((now - start))
+
 echo "#1 tf_lenet_mnist time used: $time1 seconds"
 echo "#2 keras_lenet_mnist time used: $time2 seconds"
+echo "#3 zouwu_autots_nyc_taxi time used: $time3 seconds"
