63ccd01cee4522772a17d370ea14438e608b7b14
Zhenhao Gu <47915715+GZHoffie@users.noreply.github.com>
2021-03-30 10:12:16 +0800

    Bug Fix in Colab Notebook Test Script (#3672)
    * bug fix in test script
    
    * Adjustment in the script
    
    * Adjustment in the script
    
    * Adjust script
    
    * Delete comments

--- docs/docs/colab-notebook/run-notebook-pytorch.sh
+++ docs/docs/colab-notebook/run-notebook-pytorch.sh
@@ -3,7 +3,10 @@
 dir=${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/quickstart
 pytorchFiles=("pytorch_lenet_mnist_data_creator_func" "pytorch_lenet_mnist" "pytorch_distributed_lenet_mnist")
 index=1
-for f in $pytorchFiles
+
+set -e
+
+for f in "${pytorchFiles[@]}"
 do
    
 	filename="${dir}/${f}"
@@ -18,17 +21,15 @@ do
 	sed -i 's/^[^#].*environ*/#&/g' ${filename}.py
 	sed -i 's/^[^#].*__future__ */#&/g' ${filename}.py
 	sed -i "s/_ = (sys.path/#_ = (sys.path/g" ${filename}.py
+	sed -i "s/.append/#.append/g" ${filename}.py
 	sed -i 's/^[^#].*site-packages*/#&/g' ${filename}.py
-
-	wget -nv $FTP_URI/analytics-zoo-data/mnist/train-labels-idx1-ubyte.gz -P ./dataset
-	wget -nv $FTP_URI/analytics-zoo-data/mnist/train-images-idx3-ubyte.gz -P ./dataset
-	wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-labels-idx1-ubyte.gz -P ./dataset
-	wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-images-idx3-ubyte.gz -P ./dataset
+	sed -i 's/version_info/#version_info/g' ${filename}.py
+	sed -i 's/python_version/#python_version/g' ${filename}.py
 
 	python ${filename}.py
 
 	now=$(date "+%s")
 	time=$((now-start))
-	index=$((index+1))
 	echo "Complete #${index} with time ${time} seconds"	
+	index=$((index+1))
 done
