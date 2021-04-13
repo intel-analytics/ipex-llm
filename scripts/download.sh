6ba01a445fd8cc71cac40e4222b52fb41a4cf40b
Le-Zheng <alettazheng@hotmail.com>
2021-04-13 14:45:46 +0800

    flip version to 0.12.2

--- scripts/download.sh
+++ scripts/download.sh
@@ -23,7 +23,7 @@
 HDFS_HOST=$1                                                                                               
 set -e
 
-BIGDL_VERSION=0.12.2-SNAPSHOT
+BIGDL_VERSION=0.12.2
 
 if [ -d "images" ]
 then
