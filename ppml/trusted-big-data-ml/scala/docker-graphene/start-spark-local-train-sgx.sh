f1cf8cb609cd7e2e618f3ee9a82852c3d0243232
Xin Qiu <qiuxin2012@users.noreply.github.com>
2021-04-20 08:41:37 +0800

    add ppml-spark-submit.sh (#3802)
    * add ppml-spark-submit.sh
    
    * some update

--- ppml/trusted-big-data-ml/scala/docker-graphene/start-spark-local-train-sgx.sh
+++ ppml/trusted-big-data-ml/scala/docker-graphene/start-spark-local-train-sgx.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-set -x
-
-SGX=1 ./pal_loader /opt/jdk8/bin/java \
-        -cp '/ppml/trusted-big-data-ml/work/bigdl-jar-with-dependencies.jar:/ppml/trusted-big-data-ml/work/spark-2.4.3/conf/:/ppml/trusted-big-data-ml/work/spark-2.4.3/jars/*' \
-        -Xmx10g \
-        -Dbigdl.mklNumThreads=1 \
-        -XX:ActiveProcessorCount=24 \
-        org.apache.spark.deploy.SparkSubmit \
+bash ppml-spark-submit.sh \
         --master 'local[4]' \
         --conf spark.driver.port=10027 \
         --conf spark.scheduler.maxRegisteredResourcesWaitingTime=5000000 \
@@ -21,9 +14,7 @@ SGX=1 ./pal_loader /opt/jdk8/bin/java \
         --conf spark.driver.blockManager.port=10026 \
         --conf spark.io.compression.codec=lz4 \
         --class com.intel.analytics.bigdl.models.lenet.Train \
-        --executor-cores 4 \
-        --total-executor-cores 4 \
-        --executor-memory 10G \
+        --driver-memory 10G \
         /ppml/trusted-big-data-ml/work/bigdl-jar-with-dependencies.jar \
         -f /ppml/trusted-big-data-ml/work/data \
         -b 64 \
--- ppml/trusted-big-data-ml/scala/docker-graphene/start-spark-standalone-driver-sgx.sh
+++ ppml/trusted-big-data-ml/scala/docker-graphene/start-spark-standalone-driver-sgx.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 
-set -x
-
 spark_master=$SPARK_MASTER
 driver_port=$SPARK_DRIVER_PORT
 block_manager_port=$SPARK_BLOCK_MANAGER_PORT
 driver_host=$SPARK_DRIVER_IP
 driver_block_manager_port=$SPARK_DRIVER_BLOCK_MANAGER_PORT
-secure_password=`openssl rsautl -inkey /ppml/trusted-big-data-ml/work/password/key.txt -decrypt </ppml/trusted-big-data-ml/work/password/output.bin`
+export secure_password=`openssl rsautl -inkey /ppml/trusted-big-data-ml/work/password/key.txt -decrypt </ppml/trusted-big-data-ml/work/password/output.bin`
 
 export SPARK_HOME=/ppml/trusted-big-data-ml/work/spark-2.4.3
 
-SGX=1 ./pal_loader /opt/jdk8/bin/java \
-    -cp "/ppml/trusted-big-data-ml/work/bigdl-jar-with-dependencies.jar:/ppml/trusted-big-data-ml/work/spark-2.4.3/conf/:/ppml/trusted-big-data-ml/work/spark-2.4.3/jars/*" \
-    -Dspark.authenticate=true \
-    -Dspark.authenticate.secret=$secure_password \
-    -Dspark.network.crypto.enabled=true \
-    -Dspark.network.crypto.keyLength=128 \
-    -Dspark.network.crypto.keyFactoryAlgorithm=PBKDF2WithHmacSHA1 \
-    -Dspark.io.encryption.enabled=true \
-    -Dspark.io.encryption.keySizeBits=128 \
-    -Dspark.io.encryption.keygen.algorithm=HmacSHA1 \
-    -Dspark.ssl.enabled=true \
-    -Dspark.ssl.port=8043 \
-    -Dspark.ssl.keyPassword=$secure_password \
-    -Dspark.ssl.keyStore=/ppml/trusted-big-data-ml/work/keys/keystore.jks \
-    -Dspark.ssl.keyStorePassword=$secure_password \
-    -Dspark.ssl.keyStoreType=JKS \
-    -Dspark.ssl.trustStore=/ppml/trusted-big-data-ml/work/keys/keystore.jks \
-    -Dspark.ssl.trustStorePassword=$secure_password \
-    -Dspark.ssl.trustStoreType=JKS \
-    -Xmx10g \
-    org.apache.spark.deploy.SparkSubmit \
+bash ppml-spark-submit.sh \
     --master $spark_master \
     --conf spark.driver.port=$driver_port \
     --conf spark.scheduler.maxRegisteredResourcesWaitingTime=5000000 \
