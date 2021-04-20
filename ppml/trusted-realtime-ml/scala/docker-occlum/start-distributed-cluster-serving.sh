bfb02307367c5d66ce1ea7f0bf1de8f3c084064e
Colossus Chang <60425643+ColossusChang@users.noreply.github.com>
2021-04-20 10:07:47 +0800

    [PPML] Separated Flink scripts from other scripts of distributed-cluster-serving in Occlum (#3814)
    - Separated the scripts to start and stop Flink service from the rest of distributed-cluster-serving in Occlum
    - Incorporated the changes in the scripts into README.md
    - In README.md, prompted the user to increase `vm.max_map_count`
    - Fixed distributed cluster serving by starting AESM manually

--- ppml/trusted-realtime-ml/scala/docker-occlum/start-distributed-cluster-serving.sh
+++ ppml/trusted-realtime-ml/scala/docker-occlum/start-distributed-cluster-serving.sh
@@ -43,56 +43,8 @@ while ! ssh root@$MASTER "nc -z $MASTER 6379"; do
 done
 echo ">>> $MASTER, redis started successfully."
 
-echo ">>> $MASTER, start flink-jobmanager"
-ssh root@$MASTER "docker run -itd \
-      --privileged \
-      --net=host \
-      --cpuset-cpus="3-5" \
-      --oom-kill-disable \
-      -v $KEYS_PATH:/opt/keys \
-      -v $SECURE_PASSWORD_PATH:/opt/password \
-      --name=flink-job-manager \
-      -e FLINK_JOB_MANAGER_IP=$MASTER \
-      -e FLINK_JOB_MANAGER_REST_PORT=8081 \
-      -e FLINK_JOB_MANAGER_RPC_PORT=6123 \
-      -e CORE_NUM=3 \
-      $TRUSTED_CLUSTER_SERVING_DOCKER bash -c 'cd /opt && ./start-flink-jobmanager.sh'"
-while ! ssh root@$MASTER "nc -z $MASTER 8081"; do
-  sleep 10
-done
-echo ">>> $MASTER, flink-jobmanager started successfully."
-
-for worker in ${WORKERS[@]}
-  do
-    echo ">>> $worker"
-    ssh root@$worker "docker run -itd \
-        --privileged \
-        --net=host \
-        --cpuset-cpus="6-30" \
-        --oom-kill-disable \
-        --device=/dev/sgx \
-        -v $KEYS_PATH:/opt/keys \
-        -v $SECURE_PASSWORD_PATH:/opt/password \
-        --name=flink-task-manager-$worker \
-        -e SGX_MEM_SIZE=64G \
-        -e FLINK_JOB_MANAGER_IP=$MASTER \
-        -e FLINK_JOB_MANAGER_REST_PORT=8081 \
-        -e FLINK_JOB_MANAGER_RPC_PORT=6123 \
-        -e FLINK_TASK_MANAGER_IP=$worker \
-        -e FLINK_TASK_MANAGER_DATA_PORT=6124 \
-        -e FLINK_TASK_MANAGER_RPC_PORT=6125 \
-        -e FLINK_TASK_MANAGER_TASKSLOTS_NUM=1 \
-        -e CORE_NUM=25 \
-        $TRUSTED_CLUSTER_SERVING_DOCKER bash -c 'cd /opt && ./init-occlum-taskmanager.sh && ./start-flink-taskmanager.sh'"
-  done
-
-for worker in ${WORKERS[@]}
-  do
-    while ! ssh root@$worker "nc -z $worker 6124"; do
-      sleep 10
-    done
-    echo ">>> $worker, flink-taskmanager-$worker started successfully."
-  done
+# Script for starting flink job manager and task manager is in the following file:
+bash ./deploy-flink.sh
 
 echo ">>> $MASTER, start http-frontend"
 ssh root@$MASTER "docker run -itd \
