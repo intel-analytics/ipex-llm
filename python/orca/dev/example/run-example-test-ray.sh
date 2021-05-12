f9fcbe94406d5a64450cf08a2aecb01b7a474761
liangs6212 <80952198+liangs6212@users.noreply.github.com>
2021-05-12 17:15:11 +0800

    new example for ut:test_autoestimator_pytorch (#3834)
    * create new example for ut
    
    * add four argparse and mse,such as loss,num_samples,optimizer,training_iteration,best_model_mse
    
    * use accuracy instead of mse,remove dict.get method,delete "return best_model".
    
    * Solve the proposed needs, except for remove "all_available_features"
    
    * remove all_available_features
    
    * remove multioutput="raw_values",add auto test fot the example
    
    * for jenkins.
    
    * remove redundant symbols in ln[144]
    
    * autopep8

--- python/orca/dev/example/run-example-test-ray.sh
+++ python/orca/dev/example/run-example-test-ray.sh
@@ -135,6 +135,16 @@ now=$(date "+%s")
 time10=$((now-start))
 
 
+echo "#11 start example for orca autoestimator-pytorch"
+start=$(date "+%s")
+
+python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/examples/orca/automl/autoestimator/autoestimator_pytorch.py \
+    --trials 5 --epochs 2
+
+now=$(date "+%s")
+time11=$((now-start))
+
+
 echo "Ray example tests finished"
 echo "#1 orca rl_pong time used:$time1 seconds"
 echo "#2 orca async_parameter_server time used:$time2 seconds"
@@ -146,3 +156,4 @@ echo "#7 orca super-resolution example time used:$time7 seconds"
 echo "#8 orca cifar10 example time used:$time8 seconds"
 echo "#9 orca auto-xgboost-classifier time used:$time9 seconds"
 echo "#10 orca auto-xgboost-regressor time used:$time10 seconds"
+echo "#11 orca autoestimator-pytorch time used:$time11 second"
