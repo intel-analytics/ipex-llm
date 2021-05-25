42d7418b4fd2d264cea171d01108bb18d64d8c62
DiegoCao <945003692@qq.com>
2021-05-25 09:52:37 +0800

    Add Utest about dividing zero

--- spark/dl/src/test/scala/com/intel/analytics/bigdl/dataset/DataSetSpec.scala
+++ spark/dl/src/test/scala/com/intel/analytics/bigdl/dataset/DataSetSpec.scala
@@ -470,4 +470,13 @@ class DataSetSpec extends SparkContextLifeCycle with Matchers {
     img.content((100 + 100 * 213) * 3 + 1) should be(30 / 255f)
     img.content((100 + 100 * 213) * 3) should be(36 / 255f)
   }
+
+  "small RDD" should "throw exception" in {
+    intercept[Exception] {
+      val data = Array.empty[Int]
+      val dataset = DataSet.rdd(sc.parallelize(data, 3)).data(train = true)
+      dataset.mapPartitions(iter => { Iterator.single(iter.next())}).collect()(0)
+    }
+  }
 }
+
