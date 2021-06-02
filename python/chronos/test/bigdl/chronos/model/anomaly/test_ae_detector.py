2faf059a0e248862e13a02a1a921d98503dcf908
Shane Huang <shengsheng.huang@intel.com>
2021-06-02 17:25:36 +0800

    Add AutoEncoder Anomaly Detector (#4027)
    * add implementations of AutoEncoder anomaly detector
    
    * fix pep8

---
+++ python/chronos/test/bigdl/chronos/model/anomaly/test_ae_detector.py
@@ -0,0 +1,47 @@
+#
+# Copyright 2018 Analytics Zoo Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import pytest
+import numpy as np
+import pandas as pd
+from test.zoo.pipeline.utils.test_utils import ZooTestCase
+
+from zoo.chronos.model.anomaly.ae_detector import AEDetector
+
+
+class TestAEDetector(ZooTestCase):
+
+    def setup_method(self, method):
+        pass
+
+    def teardown_method(self, method):
+        pass
+
+    def create_data(self):
+        cycles = 10
+        time = np.arange(0, cycles * np.pi, 0.01)
+        data = np.sin(time)
+        data[600:800] = 10
+        return data
+
+    def test_ae_fit_score_rolled(self):
+        y = self.create_data()
+        ad = AEDetector(roll_len=314)
+        ad.fit(y)
+        anomaly_scores = ad.score()
+        assert len(anomaly_scores) == len(y)
+        anomaly_indexes = ad.anomaly_indexes()
+        assert len(anomaly_indexes) == int(ad.ratio * len(y))
