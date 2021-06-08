5ecdb9b189568d2fad5c85c3124cccc5351ebc67
Dongjie Shi <glorysdj@gmail.com>
2021-06-08 16:53:35 +0800

    [PPML] Add python on graphene (#3939)
    * add python on graphene
    
    * update python manifest
    
    * update Dockerfile
    
    * add java to bash
    
    * update makefile and init
    
    * add examples and fix proc and pyspark driver no result issues
    
    * rm examples
    
    * update manifest

---
+++ ppml/trusted-big-data-ml/python/docker-graphene/build-docker-image.sh
@@ -0,0 +1,17 @@
+export HTTP_PROXY_HOST=your_http_proxy_host
+export HTTP_PROXY_PORT=your_http_proxy_port
+export HTTPS_PROXY_HOST=your_https_proxy_host
+export HTTPS_PROXY_PORT=your_https_proxy_port
+export JDK_URL=http://your-http-url-to-download-jdk
+
+sudo docker build \
+    --build-arg http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT \
+    --build-arg https_proxy=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT \
+    --build-arg HTTP_PROXY_HOST=$HTTP_PROXY_HOST \
+    --build-arg HTTP_PROXY_PORT=$HTTP_PROXY_PORT \
+    --build-arg HTTPS_PROXY_HOST=$HTTPS_PROXY_HOST \
+    --build-arg HTTPS_PROXY_PORT=$HTTPS_PROXY_PORT \
+    --build-arg JDK_VERSION=8u192 \
+    --build-arg JDK_URL=$JDK_URL \
+    --build-arg no_proxy=x.x.x.x \
+    -t intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-python-graphene:0.11-SNAPSHOT -f ./Dockerfile .
