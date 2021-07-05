9e20632e40768315750ab4ca8f6cdcd48b1f6440
Junwei Deng <35031544+TheaperDeng@users.noreply.github.com>
2021-07-05 14:55:11 +0800

    Rename experimental AutoTSTrainer to AutoTSEstimator and corr uts (#4214)

--- python/chronos/src/bigdl/chronos/autots/experimental/__init__.py
+++ python/chronos/src/bigdl/chronos/autots/experimental/__init__.py
@@ -13,3 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+from .autotsestimator import AutoTSEstimator
+from .tspipeline import TSPipeline
