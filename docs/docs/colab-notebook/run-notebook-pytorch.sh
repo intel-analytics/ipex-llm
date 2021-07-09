44a03a4244fb4b866b9ced45ef6826a9f6deb4d5
SGwhat <38711238+sgwhat@users.noreply.github.com>
2021-07-09 15:43:35 +0800

    fashion_mnist colab example (#4218)
    * fashion_mnist bigdl
    
    * fashion_mnist pytorch version
    
    * update the log dir
    
    * remove original python file
    
    * create new file orca/examples and move fashion_mnist notebooks in
    
    * update comment
    
    * update pytorch comment
    
    * reomve last updated comment
    
    * romove the last updated comment for bigdl
    
    * new update, move notebook files into orca/example
    
    * fix python file cannot read logdir error in test script
    
    * update comments for bigdl version
    
    * update for bigdl version(tensorboard could show more information)
    
    * remove useless cell in torch_distributed version
    
    * merge logdir in the bigdl into one file
    
    * merge logdir into one output file
    
    * add test script
    
    * test script for notebook
    
    * delete generated python file by test script
    
    * update bigdl comments
    
    * new update
    
    * update comments
    
    * remove torch_distributed adn modify script
    
    * update comments

--- docs/docs/colab-notebook/run-notebook-pytorch.sh
+++ docs/docs/colab-notebook/run-notebook-pytorch.sh
@@ -84,3 +84,40 @@ do
 	echo "Complete #${index} with time ${time} seconds"	
 	index=$((index+1))
 done
+
+# orca example test
+echo "orca examples test start"
+
+dir=${ANALYTICS_ZOO_HOME}/docs/docs/colab-notebook/orca/examples
+pytorchFiles=("fashion_mnist_bigdl")
+index=1
+
+set -e
+
+for f in "${pytorchFiles[@]}"
+do
+   
+	filename="${dir}/${f}"
+	echo "#${index} start example for ${f}"
+	#timer
+	start=$(date "+%s")
+
+	${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${filename}
+	sed -i "s/get_ipython()/#/g"  ${filename}.py
+	sed -i "s/import os/#import os/g" ${filename}.py
+	sed -i "s/import sys/#import sys/g" ${filename}.py
+	sed -i 's/^[^#].*environ*/#&/g' ${filename}.py
+	sed -i 's/^[^#].*__future__ */#&/g' ${filename}.py
+	sed -i "s/_ = (sys.path/#_ = (sys.path/g" ${filename}.py
+	sed -i "s/.append/#.append/g" ${filename}.py
+	sed -i 's/^[^#].*site-packages*/#&/g' ${filename}.py
+	sed -i 's/version_info/#version_info/g' ${filename}.py
+	sed -i 's/python_version/#python_version/g' ${filename}.py
+
+	python ${filename}.py
+
+	now=$(date "+%s")
+	time=$((now-start))
+	echo "Complete #${index} with time ${time} seconds"	
+	index=$((index+1))
+done
