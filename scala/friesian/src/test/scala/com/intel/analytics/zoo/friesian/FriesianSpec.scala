b7408a0e0ff14b06d7a91ab612c33bfe29c69c43
Guoqiong Song <guoqiongsong@gmail.com>
2021-07-08 18:22:36 -0700

    Support different data types in FeatureTable methods (#3932)
    support different data types in FeatureTable methods

--- scala/friesian/src/test/scala/com/intel/analytics/zoo/friesian/FriesianSpec.scala
+++ scala/friesian/src/test/scala/com/intel/analytics/zoo/friesian/FriesianSpec.scala
@@ -124,7 +124,7 @@ class FriesianSpec extends ZooSpecHelper {
     assert(stringIdxList.get(1).count == 1)
   }
 
-  "mask" should "work properly" in {
+  "mask Int" should "work properly" in {
     val data = sc.parallelize(Seq(
       Row("jack", Seq(1, 2, 3, 4, 5)),
       Row("alice", Seq(4, 5, 6, 7, 8)),
@@ -140,49 +140,183 @@ class FriesianSpec extends ZooSpecHelper {
     assert(dfmasked.filter("size(history_mask) = 2").count() == 0)
   }
 
-  "postpad" should "work properly" in {
+  "mask Long" should "work properly" in {
+    val data = sc.parallelize(Seq(
+      Row("jack", Seq(1L, 2L, 3L, 4L, 5L)),
+      Row("alice", Seq(4L, 5L, 6L, 7L, 8L)),
+      Row("rose", Seq(1L, 2L))))
+    val schema = StructType(Array(
+      StructField("name", StringType, true),
+      StructField("history", ArrayType(LongType), true)
+    ))
+    val df = sqlContext.createDataFrame(data, schema)
+    val dfmasked = friesian.mask(df, Array("history").toList.asJava, 4)
+    assert(dfmasked.columns.contains("history_mask"))
+    assert(dfmasked.filter("size(history_mask) = 4").count() == 3)
+    assert(dfmasked.filter("size(history_mask) = 2").count() == 0)
+  }
+
+  "mask Double" should "work properly" in {
+    val data: RDD[Row] = sc.parallelize(Seq(
+      Row("jack", Seq(1.0, 2.0, 3.0, 4.0, 5.0)),
+      Row("alice", Seq(4.0, 5.0, 6.0, 7.0, 8.0)),
+      Row("rose", Seq(1.0, 2.0))))
+    val schema = StructType(Array(
+      StructField("name", StringType, true),
+      StructField("history", ArrayType(DoubleType), true)
+    ))
+    val df = sqlContext.createDataFrame(data, schema)
+    df.show(100, false)
+    df.printSchema()
+    val dfmasked = friesian.mask(df, Array("history").toList.asJava, 4)
+    dfmasked.show(100, false)
+
+    assert(dfmasked.columns.contains("history_mask"))
+    assert(dfmasked.filter("size(history_mask) = 4").count() == 3)
+    assert(dfmasked.filter("size(history_mask) = 2").count() == 0)
+  }
+
+
+  "mask Long and Int" should "work properly" in {
+    val data = sc.parallelize(Seq(
+      Row("jack", Seq(1, 2, 3, 4, 5), Seq(1L, 2L, 3L, 4L, 5L)),
+      Row("alice", Seq(4, 5, 6, 7, 8), Seq(4L, 5L, 6L, 7L, 8L)),
+      Row("rose", Seq(1, 2), Seq(1L, 2L))))
+    val schema = StructType(Array(
+      StructField("name", StringType, true),
+      StructField("history", ArrayType(IntegerType), true),
+      StructField("history1", ArrayType(LongType), true)
+    ))
+    val df = sqlContext.createDataFrame(data, schema)
+    val dfmasked = friesian.mask(df, Array("history", "history1").toList.asJava, 4)
+    assert(dfmasked.columns.contains("history_mask"))
+    assert(dfmasked.columns.contains("history1_mask"))
+    assert(dfmasked.filter("size(history_mask) = 4").count() == 3)
+    assert(dfmasked.filter("size(history_mask) = 2").count() == 0)
+    assert(dfmasked.filter("size(history1_mask) = 4").count() == 3)
+    assert(dfmasked.filter("size(history1_mask) = 2").count() == 0)
+  }
+
+
+  "postpad Int" should "work properly" in {
     val data = sc.parallelize(Seq(
       Row("jack", Seq(1, 2, 3, 4, 5), Seq(Seq(1, 2, 3), Seq(1, 2, 3))),
       Row("alice", Seq(4, 5, 6, 7, 8), Seq(Seq(1, 2, 3), Seq(1, 2, 3))),
       Row("rose", Seq(1, 2), Seq(Seq(1, 2, 3), Seq(1, 2, 3)))))
-    val schema = StructType( Array(
+    val schema = StructType(Array(
       StructField("name", StringType, true),
       StructField("history", ArrayType(IntegerType), true),
       StructField("history_list", ArrayType(ArrayType(IntegerType)), true)
     ))
     val df = sqlContext.createDataFrame(data, schema)
     val dft = friesian.postPad(df, Array("history", "history_list").toList.asJava, 4)
-    dft.show(10, false)
+    dft.schema.fields.map(x => x.dataType).foreach(println(_))
+
     assert(dft.filter("size(history) = 4").count() == 3)
     assert(dft.filter("size(history_list) = 4").count() == 3)
     assert(dft.filter(dft("name") === "rose").select("history").collect()(0)(0).toString()
       == "WrappedArray(1, 2, 0, 0)")
   }
 
-  "addHisSeq" should "work properly" in {
+  "postpad Double" should "work properly" in {
     val data = sc.parallelize(Seq(
-      Row("jack", 1, "2019-07-01 12:01:19.000"),
-      Row("jack", 2, "2019-08-01 12:01:19.000"),
-      Row("jack", 3, "2019-09-01 12:01:19.000"),
-      Row("jack", 4, "2019-10-01 12:01:19.000"),
-      Row("jack", 5, "2019-11-01 12:01:19.000"),
-      Row("jack", 6, "2019-12-01 12:01:19.000"),
-      Row("jack", 7, "2019-12-02 12:01:19.000"),
-      Row("alice", 4, "2019-09-01 12:01:19.000"),
-      Row("alice", 5, "2019-10-01 12:01:19.000"),
-      Row("alice", 6, "2019-11-01 12:01:19.000")))
+      Row("jack", Seq(1.0, 2.0, 3.0, 4.0, 5.0), Seq(Seq(1.0, 2.0, 3.0), Seq(1.0, 2.0, 3.0))),
+      Row("alice", Seq(4.0, 5.0, 6.0, 7.0, 8.0), Seq(Seq(1.0, 2.0, 3.0), Seq(1.0, 2.0, 3.0))),
+      Row("rose", Seq(1.0, 2.0), Seq(Seq(1.0, 2.0, 3.0), Seq(1.0, 2.0, 3.0)))))
     val schema = StructType(Array(
       StructField("name", StringType, true),
+      StructField("history", ArrayType(DoubleType), true),
+      StructField("history_list", ArrayType(ArrayType(DoubleType)), true)
+    ))
+    val df = sqlContext.createDataFrame(data, schema)
+    val dft = friesian.postPad(df, Array("history", "history_list").toList.asJava, 4)
+    dft.schema.fields.map(x => x.dataType).foreach(println(_))
+
+    assert(dft.filter("size(history) = 4").count() == 3)
+    assert(dft.filter("size(history_list) = 4").count() == 3)
+    assert(dft.filter(dft("name") === "rose").select("history").collect()(0)(0).toString()
+      == "WrappedArray(1.0, 2.0, 0.0, 0.0)")
+  }
+
+  "postpad Long" should "work properly" in {
+    val data = sc.parallelize(Seq(
+      Row("jack", Seq(1L, 2L, 3L, 4L, 5L), Seq(Seq(1L, 2L, 3L), Seq(1L, 2L, 3L))),
+      Row("alice", Seq(4L, 5L, 6L, 7L, 8L), Seq(Seq(1L, 2L, 3L), Seq(1L, 2L, 3L))),
+      Row("rose", Seq(1L, 2L), Seq(Seq(1L, 2L, 3L), Seq(1L, 2L, 3L)))))
+    val schema = StructType(Array(
+      StructField("name", StringType, true),
+      StructField("history", ArrayType(LongType), true),
+      StructField("history_list", ArrayType(ArrayType(LongType)), true)
+    ))
+    val df = sqlContext.createDataFrame(data, schema)
+    val dft = friesian.postPad(df, Array("history", "history_list").toList.asJava, 4)
+//    dft.schema.fields.map(x => x.dataType).foreach(println(_))
+    assert(dft.filter("size(history) = 4").count() == 3)
+    assert(dft.filter("size(history_list) = 4").count() == 3)
+    assert(dft.filter(dft("name") === "rose").select("history").collect()(0)(0).toString()
+      == "WrappedArray(1, 2, 0, 0)")
+  }
+
+  "addHisSeq int and float" should "work properly" in {
+    val data = sc.parallelize(Seq(
+      Row("rose", "A", 1, 2.0f, "2019-07-01 12:01:19.000"),
+      Row("jack", "B", 1, 2.0f, "2019-07-01 12:01:19.000"),
+      Row("jack", "A", 2, 2.0f, "2019-08-01 12:01:19.000"),
+      Row("jack", "C", 3, 2.0f, "2019-09-01 12:01:19.000"),
+      Row("jack", "D", 4, 1.0f, "2019-10-01 12:01:19.000"),
+      Row("jack", "A", 5, 1.0f, "2019-11-01 12:01:19.000"),
+      Row("jack", "E", 6, 1.0f, "2019-12-01 12:01:19.000"),
+      Row("jack", "F", 7, 0.0f, "2019-12-02 12:01:19.000"),
+      Row("alice", "G", 4, 0.0f, "2019-09-01 12:01:19.000"),
+      Row("alice", "H", 5, 1.0f, "2019-10-01 12:01:19.000"),
+      Row("alice", "I", 6, 0.0f, "2019-11-01 12:01:19.000")))
+    val schema = StructType(Array(
+      StructField("name", StringType, true),
+      StructField("category", StringType, true),
       StructField("item", IntegerType, true),
+      StructField("other", FloatType, true),
       StructField("time", StringType, true)
     ))
     val df = sqlContext.createDataFrame(data, schema)
-           .withColumn("ts", col("time").cast("timestamp").cast("long"))
-    val dft = friesian.addHistSeq(df, "name", Array("item").toList.asJava, "ts", 1, 4)
+      .withColumn("ts", col("time").cast("timestamp").cast("long"))
+
+    df.show(false)
+    val dft = friesian.addHistSeq(df, Array("item", "other").toList.asJava, "name", "ts", 1, 4)
     assert(dft.count() == 8)
     assert(dft.filter(df("name") === "alice").count() == 2)
     assert(dft.filter(df("name") === "jack").count() == 6)
     assert(dft.columns.contains("item_hist_seq"))
+    assert(dft.columns.contains("other_hist_seq"))
+  }
+
+  "addHisSeq double long" should "work properly" in {
+    val data = sc.parallelize(Seq(
+      Row("rose", 1L, 2.0, "2019-07-01 12:01:19.000"),
+      Row("jack", 1L, 2.0, "2019-07-01 12:01:19.000"),
+      Row("jack", 2L, 2.0, "2019-08-01 12:01:19.000"),
+      Row("jack", 3L, 2.0, "2019-09-01 12:01:19.000"),
+      Row("jack", 4L, 1.0, "2019-10-01 12:01:19.000"),
+      Row("jack", 5L, 1.0, "2019-11-01 12:01:19.000"),
+      Row("jack", 6L, 1.0, "2019-12-01 12:01:19.000"),
+      Row("jack", 7L, 0.0, "2019-12-02 12:01:19.000"),
+      Row("alice", 4L, 0.0, "2019-09-01 12:01:19.000"),
+      Row("alice", 5L, 1.0, "2019-10-01 12:01:19.000"),
+      Row("alice", 6L, 0.0, "2019-11-01 12:01:19.000")))
+    val schema = StructType(Array(
+      StructField("name", StringType, true),
+      StructField("item", LongType, true),
+      StructField("other", DoubleType, true),
+      StructField("time", StringType, true)
+    ))
+    val df = sqlContext.createDataFrame(data, schema)
+      .withColumn("ts", col("time").cast("timestamp").cast("long"))
+
+    val dft = friesian.addHistSeq(df, Array("item", "other").toList.asJava, "name", "ts", 1, 4)
+    assert(dft.count() == 8)
+    assert(dft.filter(df("name") === "alice").count() == 2)
+    assert(dft.filter(df("name") === "jack").count() == 6)
+    assert(dft.columns.contains("item_hist_seq"))
+    assert(dft.columns.contains("other_hist_seq"))
   }
 
   "addNegSamples" should "work properly" in {
@@ -199,9 +333,9 @@ class FriesianSpec extends ZooSpecHelper {
       StructField("time", StringType, true)
     ))
     val df = sqlContext.createDataFrame(data, schema)
-    val dft = friesian.addNegSamples(df, 10)
+    val dft = friesian.addNegSamples(df, 10, negNum = 2)
     assert(dft.filter(col("label") === 1).count() == 6)
-    assert(dft.filter(col("label") === 0).count() == 6)
+    assert(dft.filter(col("label") === 0).count() == 12)
   }
 
   "addNegHisSeq" should "work properly" in {
@@ -209,36 +343,16 @@ class FriesianSpec extends ZooSpecHelper {
       Row("jack", Seq(1, 2, 3, 4, 5)),
       Row("alice", Seq(4, 5, 6, 7, 8)),
       Row("rose", Seq(1, 2))))
-    val items: RDD[Row] = sc.parallelize((0  to 2).map(i => Row(i, 0))
-      ++ (0  to 2).map(i => Row(i + 3, 1)) ++  (0  to 2).map(i => Row(i + 6, 2)))
 
-    val schema = StructType(Array(
-    StructField("name", StringType, true),
-    StructField("history", ArrayType(IntegerType), true)
-    ))
-    val df = sqlContext.createDataFrame(data, schema)
-    val dft = friesian.addNegHisSeq(df, 9, "history", 4)
-    assert(dft.select("neg_item_hist_seq").collect().length == 3)
-    assert(dft.select("neg_item_hist_seq").rdd.map(r =>
-      r.getAs[mutable.WrappedArray[mutable.WrappedArray[Int]]](0)).collect()(0).length == 5)
-  }
-
-  "addLength" should "work properly" in {
-    val r = scala.util.Random
-    val data: RDD[Row] = sc.parallelize(Seq(
-      Row("jack", Seq(1, 2, 3, 4, 5)),
-      Row("alice", Seq(4, 5, 6, 7, 8)),
-      Row("rose", Seq(1, 2))))
     val schema = StructType(Array(
       StructField("name", StringType, true),
       StructField("history", ArrayType(IntegerType), true)
     ))
     val df = sqlContext.createDataFrame(data, schema)
-    val dft = friesian.addLength(df, "history")
-
-    assert(dft.columns.contains("history_length"))
-    assert(dft.filter("history_length = 2").count() == 1)
-    assert(dft.filter("history_length = 5").count() == 2)
+    val dft = friesian.addNegHisSeq(df, 9, "history", 4)
+    assert(dft.select("neg_history").collect().length == 3)
+    assert(dft.select("neg_history").rdd.map(r =>
+      r.getAs[mutable.WrappedArray[mutable.WrappedArray[Int]]](0)).collect()(0).length == 5)
   }
 
   "Fill median" should "work properly" in {
