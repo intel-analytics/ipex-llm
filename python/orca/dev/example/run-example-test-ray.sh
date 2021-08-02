b831a53d2cfb8bd274a86fc2fecb69d8451ffb60
liangs6212 <80952198+liangs6212@users.noreply.github.com>
2021-08-02 10:17:18 +0800

    delete the csv file in chronos/quickstart (#4361)
    * remove nyc_taxi.csv,change to pd.read_csv(url)
    
    * change dataset download
    
    * fix jenkins error message
    
    * add args.url
    
    * fix path error
    
    * fix error

--- python/orca/dev/example/run-example-test-ray.sh
+++ python/orca/dev/example/run-example-test-ray.sh
@@ -156,11 +156,38 @@ time11=$((now-start))
 echo "#12 start example for chronos autolstm_nyc_taxi"
 start=$(date "+%s")
 
+if [ -f ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/lstm/nyc_taxi.csv ]
+then
+    echo "nyc_taxi.csv already exists"
+else
+    wget -nv $FTP_URI/analytics-zoo-data/apps/nyc-taxi/nyc_taxi.csv -P \
+    ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/lstm/
+fi
+
 python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/lstm/autolstm_nyc_taxi.py \
- --datadir ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/nyc_taxi.csv
+    --datadir ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/lstm/nyc_taxi.csv
 
 now=$(date "+%s")
 time12=$((now-start))
+
+
+echo "#13 start example for chronos autots_nyc_taxi"
+start=$(date "+%s")
+
+if [ -f ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/nyc_taxi.csv ]
+then
+    echo "nyc_taxi.csv already exists"
+else
+    wget -nv $FTP_URI/analytics-zoo-data/apps/nyc-taxi/nyc_taxi.csv -P \
+    ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/
+fi
+
+python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/autots_nyc_taxi.py \
+    --datadir ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/nyc_taxi.csv
+
+now=$(date "+%s")
+time13=$((now-start))
+
 
 echo "Ray example tests finished"
 echo "#1 orca rl_pong time used:$time1 seconds"
@@ -175,3 +202,4 @@ echo "#9 orca auto-xgboost-classifier time used:$time9 seconds"
 echo "#10 orca auto-xgboost-regressor time used:$time10 seconds"
 echo "#11 orca autoestimator-pytorch time used:$time11 seconds"
 echo "#12 chronos autolstm_nyc_taxi time used:$time12 seconds"
+echo "#13 chronos autots_nyc_taxi time used:$time13 seconds"
