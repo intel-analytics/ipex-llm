a949b1e068756508964d6b116a79a3baa1806a86
dding3 <ding.ding@intel.com>
2021-08-16 21:01:18 -0700

    update test scripts

--- python/bigdl/version.py
+++ python/bigdl/version.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 #
 # Copyright 2016 The BigDL Authors.
 #
@@ -16,27 +14,4 @@
 # limitations under the License.
 #
 
-# run: run-keras.sh python2.7
-. `dirname $0`/prepare_env.sh
-
-if (( $# < 1)); then
-  echo "Bad parameters. Usage: run-keras.sh python2.7"
-  exit -1
-fi
-
-cd "`dirname $0`"
-p=$1  # executable python 
-export DL_CORE_NUMBER=4
-
-echo "${cyan}Using python version: $p${reset}"
-export PYTHON_EXECUTABLE=$p
-export PYSPARK_PYTHON=$p
-export PYSPARK_DRIVER_PYTHON=$p
-$p -m pytest -v  ../../../python/test/bigdl/keras
-
-exit_status=$?
-if [ $exit_status -ne 0 ];
-then
-    exit $exit_status
-fi
-
+__version__ = "2.0.0.dev0"
