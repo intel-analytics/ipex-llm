49926ce6d2047258c5dc0860744ca9a1762a9938
Chirs_Leo <597714572@qq.com>
2021-08-17 16:28:53 +0800

    Bigdl-nano Trainer (#4488)
    * basic structure
    
    * refine
    
    * orca_lite -> nano
    
    * nano -> bigdl.nano
    
    * refactor
    
    * init structure
    
    * sync with https://github.com/analytics-zoo/orca-lite-poc/pull/159
    
    * conda build done
    
    * remove unused
    
    * pytorch trainer
    
    * tensorflow related
    
    * no lite
    
    * mv ipex tensor saver
    
    * tailing duplicated blank lines
    
    * liscense and restructure
    
    * licenses
    
    * restructure trainer
    
    * plugins init

--- python/nano/src/bigdl/nano/common/common.py
+++ python/nano/src/bigdl/nano/common/common.py
@@ -1,12 +1,40 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
 import os
 import sys
 import subprocess
+import logging
 import warnings
 from typing import Union, Dict, List, Optional
 import numpy as np
 import re
 
 from bigdl.nano.common.cpu_schedule import schedule_workers
+
+
+def check_avx512():
+    cmd = "lscpu | grep avx512"
+    try:
+        subprocess.check_output(cmd, shell=True)
+        return True
+    except subprocess.CalledProcessError:
+        logging.warning("avx512 disabled, fall back to non-ipex mode.")
+        return False
 
 
 def _env_variable_is_set(variable: str,
--- python/nano/src/bigdl/nano/tf/keras/Model.py
+++ python/nano/src/bigdl/nano/tf/keras/Model.py
@@ -16,14 +16,8 @@
 
 
 import tensorflow as tf
-from bigdl.nano.common.cpu_schedule import schedule_workers, get_cpu_info
 
-proc_list = schedule_workers(1)
-_, get_socket = get_cpu_info()
 
-num_sockets = len(set(get_socket.values()))
-num_threads = len(proc_list[0]) // num_sockets
-
-tf.config.threading.set_inter_op_parallelism_threads(num_sockets)
-tf.config.threading.set_intra_op_parallelism_threads(num_threads)
-tf.config.set_soft_device_placement(enabled=True)
+class Model(tf.keras.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
