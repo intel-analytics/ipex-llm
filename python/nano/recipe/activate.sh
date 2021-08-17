f2d8a1016413fae2ca947ab30c2edd867c1cb41d
Yang Wang <yang3.wang@intel.com>
2021-08-17 19:27:49 +0800

    Support pip install in bigdl-nano (#4492)
    * enable pip install in nano
    
    * fix ipex
    
    * conda
    
    * rename to bigdl-nano-init

--- python/nano/recipe/activate.sh
+++ python/nano/recipe/activate.sh
@@ -4,9 +4,9 @@ echo "=====activate===="
 is_tf=$(conda list bigdl-nano.tf | grep bigdl-nano.tf) || is_tf="no"
 
 if [ "${is_tf}" = "no" ];then
-    echo "Use bigdl-nano-run support for pytorch"
-    bigdl-nano-run
+    echo "Use bigdl-nano-init support for pytorch"
+    bigdl-nano-init
 else
-    echo "find bigdl-nano-run support for tensorflow"
-    bigdl-nano-run -t
+    echo "find bigdl-nano-init support for tensorflow"
+    bigdl-nano-init -t
 fi
