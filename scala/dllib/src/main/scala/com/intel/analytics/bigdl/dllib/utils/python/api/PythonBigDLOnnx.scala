abee05c0ae34fc187f1973f6780952d5cc306ef2
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-08-18 16:31:18 +0800

    deploy dllib  (#4493)

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/utils/python/api/PythonBigDLOnnx.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/utils/python/api/PythonBigDLOnnx.scala
@@ -1,40 +1,40 @@
-///*
-// * Copyright 2016 The BigDL Authors.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 //
-//package com.intel.analytics.bigdl.dllib.python.api
+// package com.intel.analytics.bigdl.dllib.python.api
 //
-//import scala.reflect.ClassTag
-//import scala.collection.JavaConverters._
-//import java.util.{ArrayList => JArrayList, HashMap => JHashMap, List => JList, Map => JMap}
+// import scala.reflect.ClassTag
+// import scala.collection.JavaConverters._
+// import java.util.{ArrayList => JArrayList, HashMap => JHashMap, List => JList, Map => JMap}
 //
-//import com.intel.analytics.bigdl.dllib.nn
-//import com.intel.analytics.bigdl.dllib.nn.onnx._
-//import com.intel.analytics.bigdl.dllib.tensor.TensorNumericMath.TensorNumeric
+// import com.intel.analytics.bigdl.dllib.nn
+// import com.intel.analytics.bigdl.dllib.nn.onnx._
+// import com.intel.analytics.bigdl.dllib.tensor.TensorNumericMath.TensorNumeric
 //
 //
-//private[bigdl] object PythonBigDLOnnx {
+// private[bigdl] object PythonBigDLOnnx {
 //
 //  def ofFloat(): PythonBigDLOnnx[Float] = new PythonBigDLOnnx[Float]()
 //
 //  def ofDouble(): PythonBigDLOnnx[Double] = new PythonBigDLOnnx[Double]()
 //
-//}
+// }
 //
 //
-//class PythonBigDLOnnx[T: ClassTag](implicit ev: TensorNumeric[T]) extends PythonBigDL[T] {
+// class PythonBigDLOnnx[T: ClassTag](implicit ev: TensorNumeric[T]) extends PythonBigDL[T] {
 //
 //  def createConstant(value: JTensor): nn.tf.Const[T, T] = {
 //    nn.tf.Const[T, T](toTensor(value))
@@ -64,4 +64,4 @@
 //    Shape[T]()
 //  }
 //
-//}
+// }
