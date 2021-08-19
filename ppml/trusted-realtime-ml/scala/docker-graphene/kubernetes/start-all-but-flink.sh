63aa1c98322c1a4bc2a2619186d8a215db3f8a40
Colossus Chang <60425643+ColossusChang@users.noreply.github.com>
2021-08-19 14:23:31 +0800

    [PPML] Scripts for Graphene on k8s (#4478)
    * fix realtime occlum
    
    * added scripts for graphene on k8s
    
    * Refined scripts, written readme
    
    * Updated scripts
    
    * Updated readme with port forwarding
    
    * Added more files to sgx
    
    * changed directory name
    
    * delete directory

---
+++ ppml/trusted-realtime-ml/scala/docker-graphene/kubernetes/start-all-but-flink.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -x
+export FLINK_JOB_MANAGER_IP=flink-jobmanager
+cd /ppml/trusted-realtime-ml/redis
+export SGX_MEM_SIZE=16G
+./init-redis.sh
+echo "redis initiated"
+
+
+cd /ppml/trusted-realtime-ml/java
+export SGX_MEM_SIZE=32G
+./init-java.sh
+echo "java initiated"
+
+
+export REDIS_HOST=$LOCAL_IP
+./init-cluster-serving.sh
+echo "cluster serving initiated"
+
+
+cd /ppml/trusted-realtime-ml/redis
+./start-redis.sh &
+
+
+while ! nc -z $REDIS_HOST $REDIS_PORT; do
+  sleep 5
+done
+echo "redis started"
+
+
+cd ../java
+./start-http-frontend.sh &
+echo "http-frontend started"
+
+
+while ! nc -z $LOCAL_IP 10020; do
+  sleep 1
+done
+./start-cluster-serving-job.sh &
+echo "cluster-serving-job started"
+
+
+bash /ppml/trusted-realtime-ml/check-status.sh
