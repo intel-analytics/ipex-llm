71766202ecb9fa97a6918803293a46e943f70ae7
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-08-20 10:26:41 +0800

    change integration.torch ut path for testing (#4521)
    * change ut path for testing

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/integration/torch/PowerSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/integration/torch/PowerSpec.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.intel.analytics.bigdl.integration.torch
+package com.intel.analytics.bigdl.dllib.integration.torch
 
 import com.intel.analytics.bigdl.dllib.nn.{Power}
 import com.intel.analytics.bigdl.dllib.tensor.Tensor
