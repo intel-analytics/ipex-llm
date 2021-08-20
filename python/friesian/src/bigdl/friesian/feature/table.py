59674953ed34cef44a17af08c7b366fdf816b2d3
jenniew <jenniewang123@gmail.com>
2021-08-20 12:25:18 -0700

    Friesian table add cache support (#4523)
    * add cache
    
    * update
    
    * update

--- python/friesian/src/bigdl/friesian/feature/table.py
+++ python/friesian/src/bigdl/friesian/feature/table.py
@@ -18,6 +18,7 @@ import os
 import hashlib
 import numpy as np
 from functools import reduce
+from py4j.protocol import Py4JError
 
 import pyspark.sql.functions as F
 from pyspark.sql import Row, Window
@@ -682,6 +683,29 @@ class Table:
         spark.conf.set("spark.sql.execution.arrow.enabled", "true")
         sparkDF = spark.createDataFrame(pandas_df)
         return Table(sparkDF)
+
+    def cache(self):
+        """
+        Persist this table in memory
+
+        :return: this Table
+        """
+        self.df.cache()
+        return self
+
+    def uncache(self):
+        """
+
+        Make this table as non-persistent, and remove all blocks for it from memory
+
+        :return: this Table
+        """
+        if self.df.is_cached:
+            try:
+                self.df.unpersist()
+            except Py4JError:
+                print("Try to unpersist an uncached table")
+        return self
 
 
 class FeatureTable(Table):
