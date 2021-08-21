5d100ba07da6585c2e2de314bcd538ef79b335b5
dding3 <ding.ding@intel.com>
2021-08-20 19:54:43 -0700

    resolve python conflicts II

--- python/bigdl/dllib/models/ml_pipeline/dl_classifier.py
+++ python/bigdl/dllib/models/ml_pipeline/dl_classifier.py
@@ -1,10 +1,6 @@
 from pyspark.ml.pipeline import Estimator, Model
 from pyspark.ml.param.shared import *
-<<<<<<< HEAD
 from bigdl.dllib.utils.common import *
-=======
-from bigdl.utils.common import *
->>>>>>> upstream_bigdl-2.0
 import sys
 
 if sys.version >= '3':
