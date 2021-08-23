b85b6b983e348ef2b2064aaa535d938dde561f6f
Chirs_Leo <597714572@qq.com>
2021-08-23 09:50:30 +0800

    Nano unit test (#4503)
    * nano datasets
    
    * lisence
    
    * restructure
    
    * fix import
    
    * rm transform seg
    
    * ut
    
    * jenkins
    
    * rm debug info
    
    * lincense
    
    * rm unused codes
    
    * shell print
    
    * license
    
    * codestyle check
    
    * diff
    
    * type check
    
    * torch vision whl

---
+++ python/nano/test/run-nano-codestyle-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
+export NANO_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/test
+export PYTHONPATH=${NANO_HOME}:${PYTHONPATH}
+
+pycodestyle ${NANO_HOME} --config=${NANO_TEST_DIR}/tox.ini
