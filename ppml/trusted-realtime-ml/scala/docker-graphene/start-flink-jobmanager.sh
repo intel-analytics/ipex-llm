94a3a6f1dbccf0f96c55e2b6c28aab3164cc5b0e
Colossus Chang <60425643+ColossusChang@users.noreply.github.com>
2021-08-25 17:05:28 +0800

    [PPML] Putting environment variables in configmap (#4562)
    * fix realtime occlum
    
    * update flink script and readme
    
    * put env in configmap

--- ppml/trusted-realtime-ml/scala/docker-graphene/start-flink-jobmanager.sh
+++ ppml/trusted-realtime-ml/scala/docker-graphene/start-flink-jobmanager.sh
@@ -10,12 +10,14 @@ secure_password=`openssl rsautl -inkey /ppml/trusted-realtime-ml/java/work/passw
 flink_home=$FLINK_HOME
 flink_version=$FLINK_VERSION
 blob_server_port=$FLINK_BLOB_SERVER_PORT
+blob_server_port=$FLINK_BLOB_SERVER_PORT
+xmx_size=$XMX_SIZE
 
 echo "### Launching Flink Jobmanager ###"
 
 SGX=1 ./pal_loader /opt/jdk8/bin/java \
     -Xms5g \
-    -Xmx10g \
+    -Xmx${xmx_size} \
     -XX:ActiveProcessorCount=${core_num} \
     -Djdk.tls.client.protocols="TLSv1,TLSv1.1,TLSv1.2" \
     -Dorg.apache.flink.shaded.netty4.io.netty.tryReflectionSetAccessible=true \
@@ -26,7 +28,7 @@ SGX=1 ./pal_loader /opt/jdk8/bin/java \
     -Dlogback.configurationFile=file:${flink_home}/conf/logback.xml \
     -classpath ${flink_home}/lib/flink-csv-${flink_version}.jar:${flink_home}/lib/flink-dist_2.11-${flink_version}.jar:${flink_home}/lib/flink-json-${flink_version}.jar:${flink_home}/lib/flink-shaded-zookeeper-3.4.14.jar:${flink_home}/lib/flink-table_2.11-${flink_version}.jar:${flink_home}/lib/flink-table-blink_2.11-${flink_version}.jar:${flink_home}/lib/log4j-1.2-api-2.12.1.jar:${flink_home}/lib/log4j-api-2.12.1.jar:${flink_home}/lib/log4j-core-2.12.1.jar:${flink_home}/lib/log4j-slf4j-impl-2.12.1.jar::: org.apache.flink.runtime.entrypoint.StandaloneSessionClusterEntrypoint \
     --configDir ${flink_home}/conf \
-    -D rest.bind-address=${job_manager_host} \
+    -D rest.bind-address=0.0.0.0 \
     -D rest.bind-port=${job_manager_rest_port} \
     -D blob.server.port=${blob_server_port} \
     -D jobmanager.rpc.address=${job_manager_host} \
