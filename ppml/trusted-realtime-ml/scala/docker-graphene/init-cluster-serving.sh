8e548d1e00669a7ebda84c46ab9a77824c5bec74
Colossus Chang <60425643+ColossusChang@users.noreply.github.com>
2021-08-25 10:18:12 +0800

    [PPML] Updating Graphene cluster serving scripts (#4549)
    * fix realtime occlum
    
    * update flink script and readme
    
    * improved scripts
    
    * deleted unwanted line
    
    * Updated scripts

--- ppml/trusted-realtime-ml/scala/docker-graphene/init-cluster-serving.sh
+++ ppml/trusted-realtime-ml/scala/docker-graphene/init-cluster-serving.sh
@@ -5,7 +5,7 @@ core_num=$CORE_NUM
 secure_password=`openssl rsautl -inkey /ppml/trusted-realtime-ml/redis/work/password/key.txt -decrypt </ppml/trusted-realtime-ml/redis/work/password/output.bin`
 redis_host=$REDIS_HOST
 
-sed -i "s#core_number:#core_number: ${core_num}#" config.yaml
-sed -i "s#secure_enabled:#secure_enabled: true#" config.yaml
-sed -i "s#secure_struct_store_password:#secure_struct_store_password: ${secure_password}#" config.yaml
-sed -i "s#src:#src: ${redis_host}:6379#" config.yaml
+sed -i "/coreNumberPerMachine:/c \ \ coreNumberPerMachine: ${​​​​​​​core_num}​​​​​​​" config.yaml
+sed -i "/secureEnabled:/c \ \ secureEnabled: true" config.yaml
+sed -i "/secureStructStorePassword:/c \ \ secureStructStorePassword: ${​​​​​​​secure_password}​​​​​​​" config.yaml
+sed -i "/redisUrl:/c \ \ redisUrl: ${​​​​​​​redis_host}​​​​​​​:6379" config.yaml
