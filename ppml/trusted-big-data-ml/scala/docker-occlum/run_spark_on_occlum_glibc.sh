c5b2038a9702b96a6c8734cfa0b55c03d04e9e55
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-08-26 09:30:26 +0800

    [PPML] Add ResNet&CIFAR-10 example in spark local (#4559)
    * Add ResNet&CIFAR-10 example in spark local for occlum

--- ppml/trusted-big-data-ml/scala/docker-occlum/run_spark_on_occlum_glibc.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/run_spark_on_occlum_glibc.sh
@@ -16,14 +16,13 @@ init_instance() {
     cd occlum_instance_$postfix
     occlum init
     new_json="$(jq '.resource_limits.user_space_size = "64000MB" |
-        .resource_limits.max_num_of_threads = 256 |
+        .resource_limits.max_num_of_threads = 512 |
         .process.default_heap_size = "128MB" |
         .resource_limits.kernel_space_heap_size="256MB" |
         .process.default_mmap_size = "50000MB" |
         .entry_points = [ "/usr/lib/jvm/java-11-openjdk-amd64/bin" ] |
         .env.default = [ "LD_LIBRARY_PATH=/usr/lib/jvm/java-11-openjdk-amd64/lib/server:/usr/lib/jvm/java-11-openjdk-amd64/lib:/usr/lib/jvm/java-11-openjdk-amd64/../lib:/lib","SPARK_CONF_DIR=/bin/conf","SPARK_ENV_LOADED=1","PYTHONHASHSEED=0","SPARK_HOME=/bin","SPARK_SCALA_VERSION=2.12","SPARK_JARS_DIR=/bin/jars","LAUNCH_CLASSPATH=/bin/jars/*",""]' Occlum.json)" && \
     echo "${new_json}" > Occlum.json
-
 }
 
 build_spark() {
@@ -43,7 +42,7 @@ build_spark() {
     cp -rf /etc/group image/etc/
     cp -rf /etc/java-11-openjdk image/etc/
     cp -rf ../bigdl-${BIGDL_VERSION}-jar-with-dependencies.jar image/bin/jars
-    cp -rf ../data image/bin/
+    cp -rf ../cifar image/bin/
     /opt/occlum/start_aesm.sh
     occlum build
 }
@@ -55,7 +54,7 @@ run_spark_test() {
     echo -e "${BLUE}logfile=$log${NC}"
     occlum run /usr/lib/jvm/java-11-openjdk-amd64/bin/java \
                 -XX:-UseCompressedOops -XX:MaxMetaspaceSize=256m \
-                -XX:ActiveProcessorCount=2 \
+                -XX:ActiveProcessorCount=192 \
                 -Divy.home="/tmp/.ivy" \
                 -Dos.name="Linux" \
                 -cp '/bin/conf/:/bin/jars/*' -Xmx10g org.apache.spark.deploy.SparkSubmit --jars /bin/examples/jars/spark-examples_2.11-2.4.6.jar,/bin/examples/jars/scopt_2.11-3.7.0.jar --class org.apache.spark.examples.SparkPi spark-internal
@@ -92,6 +91,38 @@ run_spark_bigdl(){
                 -e 1 | tee spark.local.sgx.log
 }
 
+run_spark_resnet_cifar(){
+    init_instance spark
+    build_spark
+    echo -e "${BLUE}occlum run spark${NC}"
+    echo -e "${BLUE}logfile=$log${NC}"
+    occlum run /usr/lib/jvm/java-11-openjdk-amd64/bin/java \
+                -XX:-UseCompressedOops -XX:MaxMetaspaceSize=256m \
+                -XX:ActiveProcessorCount=4 \
+                -Divy.home="/tmp/.ivy" \
+                -Dos.name="Linux" \
+                -cp '/bin/conf/:/bin/jars/*'  -Xmx10g org.apache.spark.deploy.SparkSubmit --jars /bin/examples/jars/spark-examples_2.11-2.4.6.jar,/bin/examples/jars/scopt_2.11-3.7.0.jar \
+                --master 'local[4]' \
+                --conf spark.driver.port=10027 \
+                --conf spark.scheduler.maxRegisteredResourcesWaitingTime=5000000 \
+                --conf spark.worker.timeout=600 \
+                --conf spark.executor.extraClassPath=/bin/jars/bigdl-0.13.0-jar-with-dependencies.jar \
+                --conf spark.driver.extraClassPath=/bin/jars/bigdl-0.13.0-jar-with-dependencies.jar \
+                --conf spark.starvation.timeout=250000 \
+                --conf spark.rpc.askTimeout=600 \
+                --conf spark.blockManager.port=10025 \
+                --conf spark.driver.host=127.0.0.1 \
+                --conf spark.driver.blockManager.port=10026 \
+                --conf spark.io.compression.codec=lz4 \
+                --class com.intel.analytics.bigdl.models.resnet.TrainCIFAR10 \
+                --driver-memory 10G \
+                /bin/jars/bigdl-0.13.0-jar-with-dependencies.jar \
+                -f /bin/cifar \
+                --batchSize 400 --optnet true --depth 20 --classes 10 --shortcutType A --nEpochs 156 \
+                --learningRate 0.1 | tee spark.local.sgx.log
+}
+
+
 id=$([ -f "$pid" ] && echo $(wc -l < "$pid") || echo "0")
 
 arg=$1
@@ -100,13 +131,18 @@ case "$arg" in
         run_spark_test
         cd ../
         ;;
-    task)
-        run_task
+    bigdl)
+        run_spark_bigdl
         cd ../
         ;;
-    bigdl)
-	run_spark_bigdl
-	cd ../
-	;;
+    cifar)
+        run_spark_resnet_cifar
+        cd ../
+        ;;
+    spark)
+        init_instance spark
+        build_spark
+        run_spark
+        cd ../
+        ;;
 esac
-
