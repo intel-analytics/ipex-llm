9be527d46e0be003c55e29432f3b832bfd90699c
Colossus Chang <60425643+ColossusChang@users.noreply.github.com>
2021-08-26 16:37:17 +0800

    [PPML] Added configurable sgx mode (#4570)
    * fix realtime occlum
    
    * update flink script and readme
    
    * put env in configmap
    
    * rpc port
    
    * tm ip
    
    * tm ip
    
    * configmap added quotes
    
    * more edits for scripts
    
    * delete unwanted config lines
    
    * updating no sgx mode
    
    * updating no sgx mode
    
    * updating no sgx mode
    
    * updating no sgx mode

--- ppml/trusted-realtime-ml/scala/docker-graphene/start-flink-jobmanager.sh
+++ ppml/trusted-realtime-ml/scala/docker-graphene/start-flink-jobmanager.sh
@@ -12,10 +12,13 @@ flink_version=$FLINK_VERSION
 blob_server_port=$FLINK_BLOB_SERVER_PORT
 blob_server_port=$FLINK_BLOB_SERVER_PORT
 xmx_size=$XMX_SIZE
+sgx_mode=$SGX_MODE
+
+if [[ $sgx_mode == "sgx" || $sgx_mode == "SGX" ]];then cmd_prefix="SGX=1 ./pal_loader"; fi
 
 echo "### Launching Flink Jobmanager ###"
 
-SGX=1 ./pal_loader /opt/jdk8/bin/java \
+eval ${cmd_prefix} /opt/jdk8/bin/java \
     -Xms5g \
     -Xmx${xmx_size} \
     -XX:ActiveProcessorCount=${core_num} \
@@ -23,7 +26,7 @@ SGX=1 ./pal_loader /opt/jdk8/bin/java \
     -Dorg.apache.flink.shaded.netty4.io.netty.tryReflectionSetAccessible=true \
     -Dorg.apache.flink.shaded.netty4.io.netty.eventLoopThreads=${core_num} \
     -Dcom.intel.analytics.zoo.shaded.io.netty.tryReflectionSetAccessible=true \
-    -Dlog.file=${flink_home}/log/flink-sgx-standalonesession-1-sgx-ICX-LCC.log \
+    -Dlog.file=${flink_home}/log/flink-standalonesession-1-${sgx_mode}.log \
     -Dlog4j.configurationFile=file:${flink_home}/conf/log4j.properties \
     -Dlogback.configurationFile=file:${flink_home}/conf/logback.xml \
     -classpath ${flink_home}/lib/flink-csv-${flink_version}.jar:${flink_home}/lib/flink-dist_2.11-${flink_version}.jar:${flink_home}/lib/flink-json-${flink_version}.jar:${flink_home}/lib/flink-shaded-zookeeper-3.4.14.jar:${flink_home}/lib/flink-table_2.11-${flink_version}.jar:${flink_home}/lib/flink-table-blink_2.11-${flink_version}.jar:${flink_home}/lib/log4j-1.2-api-2.12.1.jar:${flink_home}/lib/log4j-api-2.12.1.jar:${flink_home}/lib/log4j-core-2.12.1.jar:${flink_home}/lib/log4j-slf4j-impl-2.12.1.jar::: org.apache.flink.runtime.entrypoint.StandaloneSessionClusterEntrypoint \
@@ -40,4 +43,4 @@ SGX=1 ./pal_loader /opt/jdk8/bin/java \
     -D security.ssl.internal.keystore-password=${secure_password} \
     -D security.ssl.internal.truststore-password=${secure_password} \
     -D security.ssl.internal.key-password=${secure_password} \
-    --executionMode cluster | tee ./flink-jobmanager-sgx.log
+    --executionMode cluster | tee ./flink-jobmanager-${sgx_mode}.log
