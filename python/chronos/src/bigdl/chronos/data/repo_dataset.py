5f786a9d015eb460e9a01e7fed8f97fbbadef2b7
liangs6212 <80952198+liangs6212@users.noreply.github.com>
2021-08-26 08:44:44 +0800

    Chronos: add other dataset for repo_dataset (#4344)
    * add AIOps dataset for repo_dataset
    
    * add AIOps,fsi,nyc_taxi
    
    * change name
    
    * add **kwargs for tsdata
    
    * fix type error
    
    * sync dtype np.float32
    
    * remove default name param,add name.lower
    
    * fix name,path assert error

--- python/chronos/src/bigdl/chronos/data/repo_dataset.py
+++ python/chronos/src/bigdl/chronos/data/repo_dataset.py
@@ -16,7 +16,7 @@
 from zoo.chronos.data.utils.public_dataset import PublicDataset
 
 
-def get_public_dataset(name="network_traffic", path='~/.chronos/dataset', redownload=False):
+def get_public_dataset(name, path='~/.chronos/dataset', redownload=False, **kwargs):
     """
     Get public dataset.
 
@@ -26,20 +26,44 @@ def get_public_dataset(name="network_traffic", path='~/.chronos/dataset', redown
     :param name: str, public dataset name, e.g. "network traffic".
     :param path: str, download path, the value defatults to "~/.chronos/dataset/".
     :param redownload: bool, if redownload the raw dataset file(s).
+    :param **kwargs: dict, extra arguments passed to initialize the tsdataset,
+                     including with_split, val_ratio and test_ratio.
     """
-    assert isinstance(name, str) or isinstance(path, str),\
+    assert isinstance(name, str) and isinstance(path, str),\
         "Name and path must be string."
 
-    public_dataset = PublicDataset(name=name, path=path, redownload=redownload)\
-        .get_public_data()
-    if name == 'network_traffic':
-        return public_dataset.preprocess_network_traffic()\
-            .get_tsdata(dt_col='StartTime', target_col=['AvgRate', 'total'])
-    elif name == 'AIOps':
-        raise NotImplementedError('The AIOps is not currently supported.')
-    elif name == 'fsi':
-        raise NotImplementedError('The fsi is not currently supported.')
-    elif name == 'nyc_taxi':
-        raise NotImplementedError('The nyc_taxi is not currently supported.')
+    if name.lower().strip() == 'network_traffic':
+        return PublicDataset(name='network_traffic',
+                             path=path,
+                             redownload=redownload,
+                             **kwargs).get_public_data()\
+                                      .preprocess_network_traffic()\
+                                      .get_tsdata(dt_col='StartTime',
+                                                  target_col=['AvgRate', 'total'])
+    elif name.lower().strip() == 'aiops':
+        return PublicDataset(name='AIOps',
+                             path=path,
+                             redownload=redownload,
+                             **kwargs).get_public_data()\
+                                      .preprocess_AIOps()\
+                                      .get_tsdata(dt_col='time_step',
+                                                  target_col=['cpu_usage'])
+    elif name.lower().strip() == 'fsi':
+        return PublicDataset(name='fsi',
+                             path=path,
+                             redownload=redownload,
+                             **kwargs).get_public_data()\
+                                      .preprocess_fsi()\
+                                      .get_tsdata(dt_col='ds',
+                                                  target_col=['y'])
+    elif name.lower().strip() == 'nyc_taxi':
+        return PublicDataset(name='nyc_taxi',
+                             path=path,
+                             redownload=redownload,
+                             **kwargs).get_public_data()\
+                                      .preprocess_nyc_taxi()\
+                                      .get_tsdata(dt_col='timestamp',
+                                                  target_col=['value'])
     else:
-        raise NameError('The dataset is not currently supported.')
+        raise NameError(f'Only "network_traffic", "AIOps", "fsi", "nyc_taxi"'
+                        'are supported in Chronos built-in dataset, while get {name}.')
