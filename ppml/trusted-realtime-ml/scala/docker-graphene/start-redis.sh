8cd8bd6582ca5dfa6f0d7c848916163721eea5db
Colossus Chang <60425643+ColossusChang@users.noreply.github.com>
2021-08-27 10:06:24 +0800

    [PPML] fix typo and config (#4575)
    * fix realtime occlum
    
    * update flink script and readme
    
    * put env in configmap
    
    * rpc port
    
    * tm ip
    
    * tm ip
    
    * configmap added quotes
    
    * more edits for scripts
    
    * delete unwanted config lines
    
    * updating no sgx mode
    
    * updating no sgx mode
    
    * updating no sgx mode
    
    * updating no sgx mode
    
    * yabai's patch

--- ppml/trusted-realtime-ml/scala/docker-graphene/start-redis.sh
+++ ppml/trusted-realtime-ml/scala/docker-graphene/start-redis.sh
@@ -7,7 +7,7 @@ if [[ $sgx_mode == "sgx" || $sgx_mode == "SGX" ]];then cmd_prefix="SGX=1 ./pal_l
 echo "### Launching Redis ###"
 
 cd /ppml/trusted-realtime-ml/redis
-eval ${cmd_prefix}  redis-server --tls-port $REDIS_PORT --port 0 \
+eval ${cmd_prefix}  ./redis-server --tls-port $REDIS_PORT --port 0 \
     --tls-cert-file /ppml/trusted-realtime-ml/redis/work/keys/server.crt \
     --tls-key-file /ppml/trusted-realtime-ml/redis/work/keys/server.key \
     --tls-ca-cert-file /ppml/trusted-realtime-ml/redis/work/keys/server.crt \
