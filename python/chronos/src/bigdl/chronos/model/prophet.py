5b696c0035b85a3af49764b3e02b8b8fe867e6f3
Junwei Deng <35031544+TheaperDeng@users.noreply.github.com>
2021-08-30 18:34:56 +0800

    Chronos: overhaul on prophet forecaster and autoprophet (#4496)
    * overhaul on prophet forecaster and autoprophet
    
    * pep8 fix
    
    * fix bug
    
    * add cv option
    
    * pep8 change
    
    * add notebook changes and bug fixes
    
    * fix doc

--- python/chronos/src/bigdl/chronos/model/prophet.py
+++ python/chronos/src/bigdl/chronos/model/prophet.py
@@ -15,8 +15,11 @@
 
 import json
 import pandas as pd
+import numpy as np
 from prophet import Prophet
 from prophet.serialize import model_to_json, model_from_json
+from prophet.diagnostics import performance_metrics
+from prophet.diagnostics import cross_validation
 
 from zoo.automl.common.metrics import Evaluator
 from zoo.automl.model.abstract import BaseModel, ModelBuilder
@@ -50,7 +53,10 @@ class ProphetModel(BaseModel):
                              changepoint_range=changepoint_range,
                              seasonality_mode=seasonality_mode)
 
-    def fit_eval(self, data, validation_data, **config):
+    def fit_eval(self,
+                 data,
+                 validation_data,
+                 **config):
         """
         Fit on the training data from scratch.
 
@@ -58,6 +64,11 @@ class ProphetModel(BaseModel):
             and 2 columns, with column 'ds' indicating date and column 'y' indicating target
             and Td is the time dimension
         :param validation_data: validation data, should be the same type as data.
+        :param cross_validation: bool, if the eval result comes from cross_validation.
+               The value is set to False by default. Setting this option to true may
+               significantly slow down the process.
+        :param expected_horizon: int or pandas interval str, only effective when
+               cross_validation is set to True.
         :return: the evaluation metric value
         """
         if not self.model_init:
@@ -65,25 +76,38 @@ class ProphetModel(BaseModel):
             self.model_init = True
 
         self.model.fit(data)
-        val_metric = self.evaluate(target=validation_data,
-                                   metrics=[self.metric])[0].item()
-        return {self.metric: val_metric}
 
-    def predict(self, data=None, horizon=24):
+        cross_validation = config.get('cross_validation', False)
+        expected_horizon = config.get('expect_horizon', int(0.1*len(data)))
+        if cross_validation:
+            return self._eval_cross_validation(expected_horizon)
+        else:
+            val_metric = self.evaluate(target=validation_data,
+                                       metrics=[self.metric])[0].item()
+            return {self.metric: val_metric}
+
+    def _eval_cross_validation(self, expected_horizon):
+        df_cv = cross_validation(self.model, horizon=expected_horizon)
+        df_p = performance_metrics(df_cv, metrics=[self.metric], rolling_window=1)
+        return {self.metric: np.mean(df_p[self.metric].values)}  # here we use the mean metrics
+
+    def predict(self, ds_data=None, horizon=24, freq="D"):
         """
         Predict horizon time-points ahead the input data in fit_eval
         :param data: Prophet predicts the horizon steps foreward from the training data.
             So data should be None as it is not used.
         :param horizon: horizon length to predict
+        :param freq: the freqency of the predicted dataframe, defaulted to day("D"),
+               the frequency can be anything from the pandas list of frequency strings.
+
         :return: predicted result of length horizon
         """
-        if data is not None:
-            raise ValueError("We don't support input data currently")
         if self.model is None:
             raise Exception("Needs to call fit_eval or restore first before calling predict")
-        future = self.model.make_future_dataframe(periods=horizon)
+        if ds_data is not None:
+            return self.model.predict(ds_data)
+        future = self.model.make_future_dataframe(periods=horizon, freq=freq)
         out = self.model.predict(future)[-horizon:]
-
         return out
 
     def evaluate(self, target, data=None, metrics=['mse']):
@@ -93,7 +117,8 @@ class ProphetModel(BaseModel):
         target.
         :param data: Prophet predicts the horizon steps foreward from the training data.
             So data should be None as it is not used.
-        :param target: target for evaluation.
+        :param target: target for evaluation. A dataframe with 2 columns, where column 'ds'
+               indicating date and column 'y' indicating target.
         :param metrics: a list of metrics in string format
         :return: a list of metric evaluation results
         """
@@ -104,10 +129,8 @@ class ProphetModel(BaseModel):
         if self.model is None:
             raise Exception("Needs to call fit_eval or restore first before calling evaluate")
 
-        horizon = len(target)
-        future = self.model.make_future_dataframe(periods=horizon)
-        target_pred = self.predict(horizon=horizon)[['yhat']]
-        return [Evaluator.evaluate(m, target[['y']].values, target_pred.values) for m in metrics]
+        target_pred = self.model.predict(target)
+        return [Evaluator.evaluate(m, target.y.values, target_pred.yhat.values) for m in metrics]
 
     def save(self, checkpoint):
         if self.model is None:
