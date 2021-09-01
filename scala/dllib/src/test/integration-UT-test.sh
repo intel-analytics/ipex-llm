eedb7cf0b582e2a25a68d05dc4debea5f643d2ce
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-09-01 08:10:40 +0100

    hotfix integration ut (#4612)

--- scala/dllib/src/test/integration-UT-test.sh
+++ scala/dllib/src/test/integration-UT-test.sh
@@ -1,1 +1,1 @@
-mvn clean test -Dsuites=com.intel.analytics.bigdl.dllib.optim.OptimPredictorShutdownSpec -DhdfsMaster=${hdfs_272_master} -P integration-test -DforkMode=never
+mvn -f ./scala clean test -Dsuites=com.intel.analytics.bigdl.dllib.optim.OptimPredictorShutdownSpec -DhdfsMaster=${hdfs_272_master} -P integration-test -DforkMode=never
