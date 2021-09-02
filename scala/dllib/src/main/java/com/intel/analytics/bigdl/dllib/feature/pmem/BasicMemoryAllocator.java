e79dc1bf37007fef44af16e25a3ef50368adadf3
dding3 <ding.ding@intel.com>
2021-09-01 23:56:10 -0700

    migrate pmem java code

--- scala/dllib/src/main/java/com/intel/analytics/bigdl/dllib/feature/pmem/BasicMemoryAllocator.java
+++ scala/dllib/src/main/java/com/intel/analytics/bigdl/dllib/feature/pmem/BasicMemoryAllocator.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.intel.analytics.zoo.pmem;
+package com.intel.analytics.bigdl.dllib.feature.pmem;
 
 public interface BasicMemoryAllocator {
     long allocate(long size);
