3fd73c04bbdb62963665a70c3a8fab6be175c5b5
Colossus Chang <60425643+ColossusChang@users.noreply.github.com>
2021-09-03 16:32:06 +0800

    [PPML] Escaped the quotes in sgx mode script (#4638)
    * fix realtime occlum
    
    * update flink script and readme
    
    * fix sgx mode script

--- ppml/trusted-realtime-ml/scala/docker-graphene/start-redis.sh
+++ ppml/trusted-realtime-ml/scala/docker-graphene/start-redis.sh
@@ -7,8 +7,8 @@ if [[ $sgx_mode == "sgx" || $sgx_mode == "SGX" ]];then cmd_prefix="graphene-sgx 
 echo "### Launching Redis ###"
 
 cd /ppml/trusted-realtime-ml/redis
-eval ${cmd_prefix}bash -c "  ./redis-server --tls-port $REDIS_PORT --port 0 \
+eval ${cmd_prefix}bash -c \"  ./redis-server --tls-port $REDIS_PORT --port 0 \
     --tls-cert-file /ppml/trusted-realtime-ml/redis/work/keys/server.crt \
     --tls-key-file /ppml/trusted-realtime-ml/redis/work/keys/server.key \
     --tls-ca-cert-file /ppml/trusted-realtime-ml/redis/work/keys/server.crt \
-    --protected-mode no --maxmemory 10g " | tee ./redis-${sgx_mode}.log
+    --protected-mode no --maxmemory 10g \" | tee ./redis-${sgx_mode}.log
