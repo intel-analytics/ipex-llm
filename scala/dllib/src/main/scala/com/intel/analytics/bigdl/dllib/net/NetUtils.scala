9f24acd196e738b9c99fbc67f16c94762672083e
Le-Zheng <alettazheng@hotmail.com>
2021-09-03 12:12:41 +0800

    migrate NetUtils.scala

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/net/NetUtils.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/net/NetUtils.scala
@@ -13,22 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.intel.analytics.zoo.pipeline.api.net
+package com.intel.analytics.bigdl.dllib.net
 
 import com.esotericsoftware.kryo.{Kryo, KryoSerializable}
 import com.esotericsoftware.kryo.io.{Input, Output}
 import com.intel.analytics.bigdl.Module
-import com.intel.analytics.bigdl.nn.Graph.ModuleNode
-import com.intel.analytics.bigdl.nn.abstractnn.{AbstractModule, Activity}
-import com.intel.analytics.bigdl.nn.keras.KerasLayer
-import com.intel.analytics.bigdl.nn.{Container, Graph, StaticGraph}
-import com.intel.analytics.bigdl.serialization.Bigdl.BigDLModule
-import com.intel.analytics.bigdl.tensor.Tensor
+import com.intel.analytics.bigdl.dllib.nn.Graph.ModuleNode
+import com.intel.analytics.bigdl.dllib.nn.abstractnn.{AbstractModule, Activity}
+import com.intel.analytics.bigdl.dllib.keras.KerasLayer
+import com.intel.analytics.bigdl.dllib.nn.{Container, Graph, StaticGraph}
+import com.intel.analytics.bigdl.dllib.serialization.Bigdl.BigDLModule
+import com.intel.analytics.bigdl.dllib.tensor.Tensor
 import com.intel.analytics.bigdl.tensor.TensorNumericMath.TensorNumeric
 import com.intel.analytics.bigdl.utils.serializer._
 import com.intel.analytics.zoo.pipeline.api.Predictable
-import com.intel.analytics.zoo.pipeline.api.keras.layers.KerasLayerWrapper
-import com.intel.analytics.zoo.pipeline.api.keras.layers.utils.KerasUtils
+import com.intel.analytics.bigdl.dllib.keras.layers.KerasLayerWrapper
+import com.intel.analytics.bigdl.dllib.keras.layers.utils.KerasUtils
 import org.apache.spark.utils.SparkUtils
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
