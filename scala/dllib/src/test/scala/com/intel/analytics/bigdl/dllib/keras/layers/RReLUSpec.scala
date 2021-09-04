ae4ce8f55df7f4e892d2290734f3d910590dc816
Le-Zheng <alettazheng@hotmail.com>
2021-09-04 19:52:40 +0800

    migrate keras layers

--- scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/keras/layers/RReLUSpec.scala
+++ scala/dllib/src/test/scala/com/intel/analytics/bigdl/dllib/keras/layers/RReLUSpec.scala
@@ -14,12 +14,12 @@
  * limitations under the License.
  */
 
-package com.intel.analytics.zoo.pipeline.api.keras.layers
+package com.intel.analytics.bigdl.dllib.keras.layers
 
-import com.intel.analytics.bigdl.nn.{RReLU => BRReLU}
+import com.intel.analytics.bigdl.dllib.nn.{RReLU => BRReLU}
 import com.intel.analytics.zoo.pipeline.api.keras.layers.{RReLU => ZRReLU}
-import com.intel.analytics.bigdl.tensor.Tensor
-import com.intel.analytics.bigdl.utils.Shape
+import com.intel.analytics.bigdl.dllib.tensor.Tensor
+import com.intel.analytics.bigdl.dllib.utils.Shape
 import com.intel.analytics.zoo.pipeline.api.keras.ZooSpecHelper
 import com.intel.analytics.zoo.pipeline.api.keras.serializer.ModuleSerializationTest
 
