dfcec3a7ba0cf3f4011b2a7dd54ee8150b9753a0
Yu Shan <shan.yu@intel.com>
2021-09-06 17:28:21 +0800

    Add sub-folder ray_tune in zoo.automl.search and move ray_tune_search_engine there (#4648)
    * add subfolder ray_tune and mv ray_tune_search_engine
    
    * move ray tune utils
    
    * rename search.util to search.utils

--- python/chronos/src/bigdl/chronos/regression/base_predictor.py
+++ python/chronos/src/bigdl/chronos/regression/base_predictor.py
@@ -20,7 +20,7 @@ from abc import abstractmethod
 
 from zoo.automl.metrics import Evaluator
 from zoo.chronos.pipeline.time_sequence import TimeSequencePipeline
-from zoo.automl.search.util import process
+from zoo.automl.search.utils import process
 from zoo.chronos.config.recipe import *
 from zoo.ray import RayContext
 from zoo.orca.automl.auto_estimator import AutoEstimator
