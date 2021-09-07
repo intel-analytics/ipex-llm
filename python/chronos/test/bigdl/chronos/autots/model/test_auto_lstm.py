483ac93fefd321c4f0429bc3f8fb1c9cd4b8d579
Junwei Deng <35031544+TheaperDeng@users.noreply.github.com>
2021-09-07 09:55:15 +0800

    Chronos: add base model and post fit methods to automodel(autotcn, autos2s, autolstm) (#4629)
    * add basemodel and post fit methods
    
    * fix bugs and add uts
    
    * fix pep8

--- python/chronos/test/bigdl/chronos/autots/model/test_auto_lstm.py
+++ python/chronos/test/bigdl/chronos/autots/model/test_auto_lstm.py
@@ -18,6 +18,8 @@ import torch
 import numpy as np
 from unittest import TestCase
 import pytest
+import tempfile
+
 from zoo.chronos.autots.model.auto_lstm import AutoLSTM
 from zoo.orca.automl import hp
 
@@ -30,7 +32,7 @@ future_seq_len = 1
 def get_x_y(size):
     x = np.random.randn(size, past_seq_len, input_feature_dim)
     y = np.random.randn(size, future_seq_len, output_feature_dim)
-    return x, y
+    return x.astype(np.float32), y.astype(np.float32)
 
 
 class RandomDataset(Dataset):
@@ -58,6 +60,23 @@ def valid_dataloader_creator(config):
                       shuffle=True)
 
 
+def get_auto_estimator():
+    auto_lstm = AutoLSTM(input_feature_num=input_feature_dim,
+                         output_target_num=output_feature_dim,
+                         past_seq_len=5,
+                         optimizer='Adam',
+                         loss=torch.nn.MSELoss(),
+                         metric="mse",
+                         hidden_dim=hp.grid_search([32, 64]),
+                         layer_num=hp.randint(1, 3),
+                         lr=hp.choice([0.001, 0.003, 0.01]),
+                         dropout=hp.uniform(0.1, 0.2),
+                         logs_dir="/tmp/auto_lstm",
+                         cpus_per_trial=2,
+                         name="auto_lstm")
+    return auto_lstm
+
+
 class TestAutoLSTM(TestCase):
     def setUp(self) -> None:
         from zoo.orca import init_orca_context
@@ -68,25 +87,12 @@ class TestAutoLSTM(TestCase):
         stop_orca_context()
 
     def test_fit_np(self):
-        auto_lstm = AutoLSTM(input_feature_num=input_feature_dim,
-                             output_target_num=output_feature_dim,
-                             past_seq_len=5,
-                             optimizer='Adam',
-                             loss=torch.nn.MSELoss(),
-                             metric="mse",
-                             hidden_dim=hp.grid_search([32, 64]),
-                             layer_num=hp.randint(1, 3),
-                             lr=hp.choice([0.001, 0.003, 0.01]),
-                             dropout=hp.uniform(0.1, 0.2),
-                             logs_dir="/tmp/auto_lstm",
-                             cpus_per_trial=2,
-                             name="auto_lstm")
+        auto_lstm = get_auto_estimator()
         auto_lstm.fit(data=get_x_y(size=1000),
                       epochs=1,
                       batch_size=hp.choice([32, 64]),
                       validation_data=get_x_y(size=400),
-                      n_sampling=1,
-                      )
+                      n_sampling=1)
         assert auto_lstm.get_best_model()
         best_config = auto_lstm.get_best_config()
         assert 0.1 <= best_config['dropout'] <= 0.2
@@ -94,32 +100,68 @@ class TestAutoLSTM(TestCase):
         assert 1 <= best_config['layer_num'] < 3
 
     def test_fit_data_creator(self):
-        auto_lstm = AutoLSTM(input_feature_num=input_feature_dim,
-                             output_target_num=output_feature_dim,
-                             past_seq_len=5,
-                             optimizer='Adam',
-                             loss=torch.nn.MSELoss(),
-                             metric="mse",
-                             hidden_dim=hp.grid_search([32, 64]),
-                             layer_num=hp.randint(1, 3),
-                             lr=hp.choice([0.001, 0.003, 0.01]),
-                             dropout=hp.uniform(0.1, 0.2),
-                             logs_dir="/tmp/auto_lstm",
-                             cpus_per_trial=2,
-                             name="auto_lstm")
-
+        auto_lstm = get_auto_estimator()
         auto_lstm.fit(data=train_dataloader_creator,
                       epochs=1,
                       batch_size=hp.choice([32, 64]),
                       validation_data=valid_dataloader_creator,
-                      n_sampling=1,
-                      )
+                      n_sampling=1)
         assert auto_lstm.get_best_model()
         best_config = auto_lstm.get_best_config()
         assert 0.1 <= best_config['dropout'] <= 0.2
         assert best_config['batch_size'] in (32, 64)
         assert 1 <= best_config['layer_num'] < 3
 
+    def test_predict_evaluation(self):
+        auto_lstm = get_auto_estimator()
+        auto_lstm.fit(data=train_dataloader_creator(config={"batch_size": 64}),
+                      epochs=1,
+                      validation_data=valid_dataloader_creator(config={"batch_size": 64}),
+                      n_sampling=1)
+        test_data_x, test_data_y = get_x_y(size=100)
+        auto_lstm.predict(test_data_x)
+        auto_lstm.evaluate((test_data_x, test_data_y))
+
+    def test_onnx_methods(self):
+        auto_lstm = get_auto_estimator()
+        auto_lstm.fit(data=train_dataloader_creator(config={"batch_size": 64}),
+                      epochs=1,
+                      validation_data=valid_dataloader_creator(config={"batch_size": 64}),
+                      n_sampling=1)
+        test_data_x, test_data_y = get_x_y(size=100)
+        pred = auto_lstm.predict(test_data_x)
+        eval_res = auto_lstm.evaluate((test_data_x, test_data_y))
+        try:
+            import onnx
+            import onnxruntime
+            pred_onnx = auto_lstm.predict_with_onnx(test_data_x)
+            eval_res_onnx = auto_lstm.evaluate_with_onnx((test_data_x, test_data_y))
+            np.testing.assert_almost_equal(pred, pred_onnx, decimal=5)
+            np.testing.assert_almost_equal(eval_res, eval_res_onnx, decimal=5)
+        except ImportError:
+            pass
+
+    def test_save_load(self):
+        auto_lstm = get_auto_estimator()
+        auto_lstm.fit(data=train_dataloader_creator(config={"batch_size": 64}),
+                      epochs=1,
+                      validation_data=valid_dataloader_creator(config={"batch_size": 64}),
+                      n_sampling=1)
+        with tempfile.TemporaryDirectory() as tmp_dir_name:
+            auto_lstm.save(tmp_dir_name)
+            auto_lstm.load(tmp_dir_name)
+        test_data_x, test_data_y = get_x_y(size=100)
+        pred = auto_lstm.predict(test_data_x)
+        eval_res = auto_lstm.evaluate((test_data_x, test_data_y))
+        try:
+            import onnx
+            import onnxruntime
+            pred_onnx = auto_lstm.predict_with_onnx(test_data_x)
+            eval_res_onnx = auto_lstm.evaluate_with_onnx((test_data_x, test_data_y))
+            np.testing.assert_almost_equal(pred, pred_onnx, decimal=5)
+            np.testing.assert_almost_equal(eval_res, eval_res_onnx, decimal=5)
+        except ImportError:
+            pass
 
 if __name__ == "__main__":
     pytest.main([__file__])
