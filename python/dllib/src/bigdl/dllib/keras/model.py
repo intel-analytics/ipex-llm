2fdc272ccb3adcc9581a65cb1d09ab45901e1092
Le-Zheng <alettazheng@hotmail.com>
2021-09-08 10:23:49 +0800

    rm unwanted

--- python/dllib/src/bigdl/dllib/keras/model.py
+++ python/dllib/src/bigdl/dllib/keras/model.py
@@ -49,7 +49,7 @@ class Sequential(KerasNet):
             return False
 
     def add(self, model):
-        from zoo.pipeline.api.autograd import Lambda
+        from bigdl.dllib.autograd import Lambda
         if (isinstance(model, Lambda)):
             if not self.is_built():
                 if not model.input_shape:
