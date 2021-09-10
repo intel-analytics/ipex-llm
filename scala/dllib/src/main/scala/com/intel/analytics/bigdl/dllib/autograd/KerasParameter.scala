591c137afa60554bf3e421242b001833909d7508
Le-Zheng <alettazheng@hotmail.com>
2021-09-10 09:12:46 +0800

    move nn.keras to keras

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/autograd/KerasParameter.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/autograd/KerasParameter.scala
@@ -18,7 +18,7 @@
 package com.intel.analytics.bigdl.dllib.autograd
 
 import com.intel.analytics.bigdl.dllib.nn.abstractnn._
-import com.intel.analytics.bigdl.dllib.nn.keras.KerasLayer
+import com.intel.analytics.bigdl.dllib.keras.KerasLayer
 import com.intel.analytics.bigdl.dllib.nn.tf.InternalWithoutInput
 import com.intel.analytics.bigdl.dllib.nn.{InitializationMethod, RandomUniform, VariableFormat}
 import com.intel.analytics.bigdl.dllib.tensor.Tensor
