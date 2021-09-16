503ae6c22b69be173c54866f2e3602944826123a
Yina Chen <33650826+cyita@users.noreply.github.com>
2021-09-16 10:34:22 +0800

    Orca openvino estimator fix (#4728)
    * fix openvino estimator
    
    * delete useless

--- python/orca/src/bigdl/orca/learn/openvino/estimator.py
+++ python/orca/src/bigdl/orca/learn/openvino/estimator.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import math
+
 from zoo.pipeline.inference import InferenceModel
 from zoo.orca.data import SparkXShards
 from zoo.orca.learn.spark_estimator import Estimator as SparkEstimator
@@ -53,9 +55,9 @@ class OpenvinoEstimator(SparkEstimator):
                 raise ValueError("Invalid openVINO IR xml file, please check your model_path")
             self.batch_size = int(shape_item.text)
         self.model = InferenceModel(supported_concurrent_num=self.core_num)
-        self.model.load_openvino(model_path=model_path,
-                                 weight_path=model_path[:model_path.rindex(".")] + ".bin",
-                                 batch_size=self.batch_size)
+        self.model.load_openvino_ng(model_path=model_path,
+                                    weight_path=model_path[:model_path.rindex(".")] + ".bin",
+                                    batch_size=self.batch_size)
 
     def fit(self, data, epochs, batch_size=32, feature_cols=None, label_cols=None,
             validation_data=None, checkpoint_trigger=None):
@@ -87,7 +89,7 @@ class OpenvinoEstimator(SparkEstimator):
             assert "x" in dict_data, "key x should in each shard"
             feature_data = dict_data["x"]
             if isinstance(feature_data, np.ndarray):
-                assert feature_data.shape[1] <= batch_size, \
+                assert feature_data.shape[0] <= batch_size, \
                     "The batch size of input data (the second dim) should be less than the model " \
                     "batch size, otherwise some inputs will be ignored."
             elif isinstance(feature_data, list):
@@ -95,13 +97,13 @@ class OpenvinoEstimator(SparkEstimator):
                     assert isinstance(elem, np.ndarray), "Each element in the x list should be " \
                                                          "a ndarray, but get " + \
                                                          elem.__class__.__name__
-                    assert elem.shape[1] <= batch_size, "The batch size of each input data (the " \
+                    assert elem.shape[0] <= batch_size, "The batch size of each input data (the " \
                                                         "second dim) should be less than the " \
                                                         "model batch size, otherwise some inputs " \
                                                         "will be ignored."
             else:
                 raise ValueError("x in each shard should be a ndarray or a list of ndarray.")
-            return dict_data["x"]
+            return feature_data
 
         sc = init_nncontext()
 
@@ -114,32 +116,34 @@ class OpenvinoEstimator(SparkEstimator):
                                               mode="predict")
             transformed_data = xshards.transform_shard(predict_transform, self.batch_size)
             result_rdd = self.model.distributed_predict(transformed_data.rdd, sc)
+
+            def delete_useless_result(data):
+                shard, y = data
+                data_length = len(shard["x"])
+                return y[: data_length]
+            result_rdd = xshards.rdd.zip(result_rdd).map(delete_useless_result)
             return convert_predict_rdd_to_dataframe(data, result_rdd.flatMap(lambda data: data))
         elif isinstance(data, SparkXShards):
-            from zoo.orca.learn.utils import convert_predict_rdd_to_xshard
             transformed_data = data.transform_shard(predict_transform, self.batch_size)
             result_rdd = self.model.distributed_predict(transformed_data.rdd, sc)
 
             def update_shard(data):
                 shard, y = data
-                shard["prediction"] = y
+                data_length = len(shard["x"])
+                shard["prediction"] = y[: data_length]
                 return shard
             return SparkXShards(data.rdd.zip(result_rdd).map(update_shard))
         elif isinstance(data, (np.ndarray, list)):
-            total_core_num = self.core_num * self.node_num
             if isinstance(data, np.ndarray):
-                assert data.shape[1] <= self.batch_size, "The batch size of input data (the " \
-                                                         "second dim) should be less than the " \
-                                                         "model batch size, otherwise some " \
-                                                         "inputs will be ignored."
-                split_num = min(total_core_num, data.shape[0])
+                split_num = math.ceil(len(data)/self.batch_size)
                 arrays = np.array_split(data, split_num)
+                data_length_list = list(map(lambda arr: len(arr), arrays))
                 data_rdd = sc.parallelize(arrays, numSlices=split_num)
             elif isinstance(data, list):
                 flattened = nest.flatten(data)
                 data_length = len(flattened[0])
                 data_to_be_rdd = []
-                split_num = min(total_core_num, flattened[0].shape[0])
+                split_num = math.ceil(flattened[0].shape[0]/self.batch_size)
                 for i in range(split_num):
                     data_to_be_rdd.append([])
                 for x in flattened:
@@ -149,19 +153,18 @@ class OpenvinoEstimator(SparkEstimator):
                     assert len(x) == data_length, \
                         "the ndarrays in data must all have the same size in first dimension" \
                         ", got first ndarray of size {} and another {}".format(data_length, len(x))
-                    assert x.shape[1] <= self.batch_size, "The batch size of each input data (" \
-                                                          "the second dim) should be less than " \
-                                                          "the model batch size, otherwise some " \
-                                                          "inputs will be ignored."
                     x_parts = np.array_split(x, split_num)
                     for idx, x_part in enumerate(x_parts):
                         data_to_be_rdd[idx].append(x_part)
+                        data_length_list = list(map(lambda arr: len(arr), x_part))
 
                 data_to_be_rdd = [nest.pack_sequence_as(data, shard) for shard in data_to_be_rdd]
                 data_rdd = sc.parallelize(data_to_be_rdd, numSlices=split_num)
 
             result_rdd = self.model.distributed_predict(data_rdd, sc)
             result_arr_list = result_rdd.collect()
+            for i in range(0, len(result_arr_list)):
+                result_arr_list[i] = result_arr_list[i][:data_length_list[i]]
             result_arr = np.concatenate(result_arr_list, axis=0)
             return result_arr
         else:
