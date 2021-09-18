a24ffca75348ad2785def323ebe679fc0b4200bf
Qiyuan Gong <qiyuan.gong@intel.com>
2021-09-18 18:40:20 +0800

    [PPML] Refine Spark Occlum Kubernetes PI example (#4759)
    * Refine Occlum Kubernetes PI example
    * Remove unnecessary doc
    * Fix image name

--- ppml/trusted-big-data-ml/scala/docker-occlum/kubernetes/run_spari_pi.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/kubernetes/run_spari_pi.sh
@@ -6,10 +6,10 @@ ${SPARK_HOME}/bin/spark-submit \
     --name spark-pi \
     --class org.apache.spark.examples.SparkPi \
     --conf spark.executor.instances=1 \
-	--conf spark.rpc.netty.dispatcher.numThreads=32 \
-    --conf spark.kubernetes.container.image=occlum_spark:latest \
-	--conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
-	--conf spark.kubernetes.executor.deleteOnTermination=false \
-	--conf spark.kubernetes.driver.podTemplateFile=./executor.yaml \
-	--conf spark.kubernetes.executor.podTemplateFile=./executor.yaml \
+    --conf spark.rpc.netty.dispatcher.numThreads=32 \
+    --conf spark.kubernetes.container.image=intelanalytics/analytics-zoo-ppml-trusted-big-data-ml-scala-occlum-k8s:0.11-SNAPSHOT \
+    --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+    --conf spark.kubernetes.executor.deleteOnTermination=false \
+    --conf spark.kubernetes.driver.podTemplateFile=./executor.yaml \
+    --conf spark.kubernetes.executor.podTemplateFile=./executor.yaml \
     local:/bin/examples/jars/spark-examples_2.12-3.0.0.jar
