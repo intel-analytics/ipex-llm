a665a5a20469fe68687eb43a9ddde34978d25a7f
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-09-23 02:46:01 +0100

    Migrate local estimator dependencies in dllib (#4788)

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/estimator/EstimateSupportive.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/estimator/EstimateSupportive.scala
@@ -15,60 +15,135 @@
  */
 
 package com.intel.analytics.bigdl.dllib.estimator
-// TODO call InferenceSupportive from orca or move local Estimator in orca
-// import com.intel.analytics.bigdl.orca.inference.InferenceSupportive
+import com.intel.analytics.bigdl.Module
+import com.intel.analytics.bigdl.dllib.nn.abstractnn.{AbstractModule, Activity}
+import com.intel.analytics.bigdl.dllib.tensor.Tensor
+import com.intel.analytics.bigdl.dllib.tensor.TensorNumericMath.TensorNumeric
 import org.slf4j.LoggerFactory
+
+import scala.reflect.ClassTag
 
 /**
  * EstimateSupportive trait to provide some AOP methods as utils
  *
  */
-// trait EstimateSupportive extends InferenceSupportive {
-//
-//  /**
-//   * calculate and log the time and throughput
-//   *
-//   * @param name    the name of the process
-//   * @param batch   the number of the batch
-//   * @param f       the process function
-//   * @tparam T      the return of the process function
-//   * @return        the result of the process function
-//   */
-//  def throughputing[T](name: String, batch: Int)(f: => T): T = {
-//    val begin = System.currentTimeMillis
-//    val result = f
-//    val end = System.currentTimeMillis
-//    val cost = (end - begin)
-//    val throughput = batch.toDouble / cost * 1000
-//    EstimateSupportive.logger.info(
-//      s"$name time elapsed [${cost / 1000} s, ${cost % 1000} ms], " +
-//        s"throughput: ${throughput} records/second.")
-//    result
-//  }
-//
-//  /**
-//   * calculate and log the time and throughput and loss
-//   *
-//   * @param name  the name of the process
-//   * @param batch the number of the batch
-//   * @param f     the process function
-//   * @tparam T    the return of the process function
-//   * @return      the result of the process function
-//   */
-//  def throughputingWithLoss[T](name: String, batch: Int)(f: => T): T = {
-//    val begin = System.currentTimeMillis
-//    val result = f
-//    val end = System.currentTimeMillis
-//    val cost = (end - begin)
-//    val throughput = batch.toDouble / cost * 1000
-//    EstimateSupportive.logger.info(
-//      s"$name time elapsed [${cost / 1000} s, ${cost % 1000} ms], " +
-//        s"throughput: ${throughput} records/second, loss: ${result}.")
-//    result
-//  }
-//
-// }
-//
-// object EstimateSupportive {
-//  val logger = LoggerFactory.getLogger(getClass)
-// }
+ trait EstimateSupportive {
+
+  def timing[T](name: String)(f: => T): T = {
+    val begin = System.currentTimeMillis
+    val result = f
+    val end = System.currentTimeMillis
+    val cost = (end - begin)
+    EstimateSupportive.logger.info(s"$name time elapsed [${cost / 1000} s, ${cost % 1000} ms].")
+    result
+  }
+
+  /**
+   * calculate and log the time and throughput
+   *
+   * @param name    the name of the process
+   * @param batch   the number of the batch
+   * @param f       the process function
+   * @tparam T      the return of the process function
+   * @return        the result of the process function
+   */
+  def throughputing[T](name: String, batch: Int)(f: => T): T = {
+    val begin = System.currentTimeMillis
+    val result = f
+    val end = System.currentTimeMillis
+    val cost = (end - begin)
+    val throughput = batch.toDouble / cost * 1000
+    EstimateSupportive.logger.info(
+      s"$name time elapsed [${cost / 1000} s, ${cost % 1000} ms], " +
+        s"throughput: ${throughput} records/second.")
+    result
+  }
+
+  /**
+   * calculate and log the time and throughput and loss
+   *
+   * @param name  the name of the process
+   * @param batch the number of the batch
+   * @param f     the process function
+   * @tparam T    the return of the process function
+   * @return      the result of the process function
+   */
+  def throughputingWithLoss[T](name: String, batch: Int)(f: => T): T = {
+    val begin = System.currentTimeMillis
+    val result = f
+    val end = System.currentTimeMillis
+    val cost = (end - begin)
+    val throughput = batch.toDouble / cost * 1000
+    EstimateSupportive.logger.info(
+      s"$name time elapsed [${cost / 1000} s, ${cost % 1000} ms], " +
+        s"throughput: ${throughput} records/second, loss: ${result}.")
+    result
+  }
+
+  def clearWeightBias(model: Module[Float]): Unit = {
+    model.reset()
+    val weightBias = model.parameters()._1
+    val clonedWeightBias = model.parameters()._1.map(tensor => {
+      val newTensor = Tensor[Float]().resizeAs(tensor)
+      newTensor.copy(tensor)
+    })
+    val localWeightBias = model.parameters()._1
+    var i = 0
+    while (i < localWeightBias.length) {
+      if (localWeightBias(i) != null) {
+        localWeightBias(i).set(clonedWeightBias(i))
+      }
+      i += 1
+    }
+    releaseTensors(model.parameters()._1)
+    releaseTensors(model.parameters()._2)
+  }
+
+  def makeMetaModel(original: AbstractModule[Activity, Activity, Float]):
+  AbstractModule[Activity, Activity, Float] = {
+    val metaModel = original.cloneModule()
+    releaseWeightBias(metaModel)
+    metaModel
+  }
+
+  def releaseWeightBias(model: Module[Float]): Unit = {
+    model.reset()
+    releaseTensors(model.parameters()._1)
+    releaseTensors(model.parameters()._2)
+  }
+
+  private def releaseTensors[T: ClassTag](tensors: Array[Tensor[T]])
+                                         (implicit ev: TensorNumeric[T]) = {
+    var i = 0
+    while (i < tensors.length) {
+      if (tensors(i) != null) {
+        tensors(i).set()
+      }
+      i += 1
+    }
+  }
+
+  def makeUpModel(clonedModel: Module[Float], weightBias: Array[Tensor[Float]]):
+  AbstractModule[Activity, Activity, Float] = {
+    putWeightBias(clonedModel, weightBias)
+    clonedModel.evaluate()
+    clonedModel
+  }
+
+  private def putWeightBias(target: Module[Float], weightBias: Array[Tensor[Float]]):
+  Module[Float] = {
+    val localWeightBias = target.parameters()._1
+    var i = 0
+    while (i < localWeightBias.length) {
+      if (localWeightBias(i) != null) {
+        localWeightBias(i).set(weightBias(i))
+      }
+      i += 1
+    }
+    target
+  }
+ }
+
+ object EstimateSupportive {
+  val logger = LoggerFactory.getLogger(getClass)
+ }
