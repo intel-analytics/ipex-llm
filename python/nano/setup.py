b2a471b6995a8f435ed70c64b0d3f7d47e1ac473
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-09-23 09:07:27 +0100

    update version to 0.14.0.dev0 (#4761)

--- python/nano/setup.py
+++ python/nano/setup.py
@@ -140,7 +140,7 @@ def setup_package():
 
     metadata = dict(
         name='bigdl-nano',
-        version='0.0.1.dev0',
+        version='0.14.0.dev0',
         description='',
         author='',
         author_email='',
