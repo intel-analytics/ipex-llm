ed0adf0dadc8cae98a01a6a29db98e61754f0a94
Song Jiaming <litchy233@gmail.com>
2021-09-27 01:41:32 +0000

    Migrate serving scala code with history (#4803)

---
+++ scala/serving/src/main/scala/com/intel/analytics/bigdl/serving/utils/ClusterServingHelper.scala
@@ -0,0 +1,287 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.intel.analytics.bigdl.serving.utils
+
+import java.nio.file.{Files, Path, Paths}
+import com.intel.analytics.bigdl.orca.inference.InferenceModel
+import com.intel.analytics.bigdl.serving.ClusterServing
+import redis.clients.jedis.Jedis
+import scala.beans.BeanProperty
+
+/**
+ * The helper of Cluster Serving
+ * by default, all parameters are loaded by config including model directory
+ * However, in some condition, models are distributed to remote machine
+ * and locate in tmp directory, but other configs are still needed.
+ * Thus model directory could be passed and overwrite that in config YAML
+ */
+class ClusterServingHelper
+  extends Serializable {
+  // BeanProperty store attributes read from config file
+  @BeanProperty var modelPath = ""
+  @BeanProperty var jobName = Conventions.SERVING_STREAM_DEFAULT_NAME
+  @BeanProperty var postProcessing = ""
+
+  // utils attributes
+  @BeanProperty var imageResize = ""
+
+  // performance attributes
+  @BeanProperty var inputAlreadyBatched = false
+  @BeanProperty var coreNumberPerMachine = -1
+  @BeanProperty var modelParallelism = 1
+  @BeanProperty var threadPerModel = 1
+
+  // specific attributes
+  @BeanProperty var flinkRestUrl = "localhost:8081"
+  @BeanProperty var redisUrl = "localhost:6379"
+  @BeanProperty var redisMaxMemory = "4g"
+  @BeanProperty var redisTimeout = 5000
+
+  // secure attributes
+  @BeanProperty var redisSecureEnabled = false
+  @BeanProperty var redisSecureTrustStorePath = ""
+  @BeanProperty var redisSecureTrustStorePassword = ""
+  @BeanProperty var modelEncrypted = false
+  @BeanProperty var recordEncrypted = false
+
+  var configPath: String = "config.yaml"
+  var redisHost: String = _
+  var redisPort: Int = _
+  var blasFlag: Boolean = false
+  var chwFlag: Boolean = true
+  var resize: Boolean = false
+  var modelType: String = _
+  var weightPath: String = _
+  var defPath: String = _
+
+  def parseConfigStrings(): Unit = {
+    redisHost = redisUrl.split(":").head.trim
+    redisPort = redisUrl.split(":").last.trim.toInt
+  }
+  /**
+   * Load inference model
+   * The concurrent number of inference model depends on
+   * backend engine type
+   * @return
+   */
+  def loadInferenceModel(concurrentNum: Int = 0): InferenceModel = {
+    if (modelPath != "") {
+      parseModelType(modelPath)
+    }
+    if (modelType.startsWith("tensorflow")) {
+      chwFlag = false
+    }
+    // Allow concurrent number overwrite
+    if (concurrentNum > 0) {
+      modelParallelism = concurrentNum
+    }
+    ClusterServing.logger.info(
+      s"Cluster Serving load Inference Model with Parallelism $modelParallelism")
+    val model = new InferenceModel(modelParallelism)
+
+    // Used for Tensorflow Model, it could not have intraThreadNum > 2^8
+    // in some models, thus intraThreadNum should be limited
+
+    var secret: String = null
+    var salt: String = null
+    if (modelEncrypted) {
+      val jedis = new Jedis(redisHost, redisPort)
+      while (secret == null || salt == null) {
+        secret = jedis.hget(Conventions.MODEL_SECURED_KEY, Conventions.MODEL_SECURED_SECRET)
+        salt = jedis.hget(Conventions.MODEL_SECURED_KEY, Conventions.MODEL_SECURED_SALT)
+        ClusterServing.logger.info("Waiting for Model Encrypted Secret and Salt in Redis," +
+          "please put them in model_secured -> secret and " +
+          "model_secured -> salt")
+        ClusterServing.logger.info("Retrying in 3 seconds...")
+        Thread.sleep(3000)
+      }
+
+    }
+    modelType match {
+      case "caffe" => model.doLoadCaffe(defPath, weightPath, blas = blasFlag)
+      case "bigdl" => model.doLoadBigDL(weightPath, blas = blasFlag)
+      case "tensorflowFrozenModel" =>
+        model.doLoadTensorflow(weightPath, "frozenModel", 1, 1, true)
+      case "tensorflowSavedModel" =>
+        model.doLoadTensorflow(weightPath, "savedModel", null, null)
+      case "pytorch" => model.doLoadPyTorch(weightPath)
+      case "keras" => logError("Keras currently not supported in Cluster Serving," +
+        "consider transform it to Tensorflow")
+      case "openvino" => modelEncrypted match {
+        case true => model.doLoadEncryptedOpenVINO(
+          defPath, weightPath, secret, salt, threadPerModel)
+        case false => model.doLoadOpenVINONg(defPath, weightPath, threadPerModel)
+      }
+      case _ => logError("Invalid model type, please check your model directory")
+    }
+    model
+  }
+
+  /**
+   * To check if there already exists detected defPath or weightPath
+   * @param defPath Boolean, true means need to check if it is not null
+   * @param weightPath Boolean, true means need to check if it is not null
+   */
+  def throwOneModelError(modelType: Boolean,
+                         defPath: Boolean, weightPath: Boolean): Unit = {
+
+    if ((modelType && this.modelType != null) ||
+        (defPath && this.defPath != null) ||
+        (weightPath && this.weightPath != null)) {
+      logError("Only one model is allowed to exist in " +
+        "model folder, please check your model folder to keep just" +
+        "one model in the directory")
+
+    }
+  }
+
+  /**
+   * Log error message to local log file
+   * @param msg
+   */
+  def logError(msg: String): Unit = {
+    ClusterServing.logger.error(msg)
+    throw new Error(msg)
+  }
+
+
+  /**
+   * Infer the model type in model directory
+   * Try every file in the directory, infer which are the
+   * model definition file and model weight file
+   * @param location
+   */
+  def parseModelType(location: String): Unit = {
+    /**
+     * Download file to local if the scheme is remote
+     * Currently support hdfs, s3
+     */
+    val scheme = location.split(":").head
+    val localModelPath = if (scheme == "file" || location.split(":").length <= 1) {
+      location.split("file://").last
+    } else {
+      // hdfs and s3 are supported by Flink natively
+      null
+    }
+
+    /**
+     * Initialize all relevant parameters at first
+     */
+    modelType = null
+    weightPath = null
+    defPath = null
+
+    var variablesPathExist = false
+
+    import java.io.File
+    val f = new File(localModelPath)
+    val fileList = f.listFiles
+
+    if (fileList == null) {
+      logError("Your model path provided in config is empty, please check your model path.")
+    }
+    // model type is always null, not support pass model type currently
+    if (modelType == null) {
+
+      for (file <- fileList) {
+        val fName = file.getName
+        val fPath = new File(localModelPath, fName).toString
+        if (fName.endsWith("caffemodel")) {
+          throwOneModelError(true, false, true)
+          weightPath = fPath
+          modelType = "caffe"
+        }
+        else if (fName.endsWith("prototxt")) {
+          throwOneModelError(false, true, false)
+          defPath = fPath
+        }
+        // ckpt seems not supported
+        else if (fName.endsWith("pb")) {
+          throwOneModelError(true, false, true)
+          weightPath = localModelPath
+          if (variablesPathExist) {
+            modelType = "tensorflowSavedModel"
+          } else {
+            modelType = "tensorflowFrozenModel"
+          }
+        }
+        else if (fName.endsWith("pt")) {
+          throwOneModelError(true, false, true)
+          weightPath = fPath
+          modelType = "pytorch"
+        }
+        else if (fName.endsWith("model")) {
+          throwOneModelError(true, false, true)
+          weightPath = fPath
+          modelType = "bigdl"
+        }
+        else if (fName.endsWith("keras")) {
+          throwOneModelError(true, false, true)
+          weightPath = fPath
+          modelType = "keras"
+        }
+        else if (fName.endsWith("bin")) {
+          throwOneModelError(true, false, true)
+          weightPath = fPath
+          modelType = "openvino"
+        }
+        else if (fName.endsWith("xml")) {
+          throwOneModelError(false, true, false)
+          defPath = fPath
+        }
+        else if (fName.equals("variables")) {
+          if (modelType != null && modelType.equals("tensorflowFrozenModel")) {
+            modelType = "tensorflowSavedModel"
+          } else {
+            variablesPathExist = true
+          }
+        }
+
+      }
+      if (modelType == null) logError("There is no model detected in your directory." +
+        "Please refer to document for supported model types.")
+    }
+    else {
+      modelType = modelType.toLowerCase
+    }
+    // auto set parallelism if coreNumberPerMachine is set
+    if (coreNumberPerMachine > 0) {
+      if (modelType == "openvino") {
+        threadPerModel = coreNumberPerMachine
+      } else {
+        threadPerModel = 1
+        modelParallelism = coreNumberPerMachine
+      }
+    }
+  }
+
+}
+object ClusterServingHelper {
+  /**
+   * Method wrapped for external use only
+   * @param modelDir directory of model
+   * @param concurrentNumber model concurrent number
+   * @return
+   */
+  def loadModelfromDir(modelDir: String, concurrentNumber: Int = 1): (InferenceModel, String) = {
+    val helper = new ClusterServingHelper()
+    helper.modelPath = modelDir
+    helper.parseModelType(modelDir)
+    (helper.loadInferenceModel(concurrentNumber), helper.modelType)
+  }
+}
---
+++ scala/serving/src/test/scala/com/intel/analytics/bigdl/serving/CorrectnessSpec.scala
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.serving
+
+import java.awt._
+import java.awt.image.BufferedImage
+import java.io.{ByteArrayOutputStream, File}
+import java.util.Base64
+
+import com.intel.analytics.bigdl.dllib.feature.image.OpenCVMethod
+import com.intel.analytics.bigdl.dllib.nn.abstractnn.Activity
+import com.intel.analytics.bigdl.serving.engine.ClusterServingInference
+import com.intel.analytics.bigdl.serving.http.{Instances, JsonUtil}
+import com.intel.analytics.bigdl.serving.postprocessing.PostProcessing
+import com.intel.analytics.bigdl.serving.utils.{ClusterServingHelper, DeprecatedUtils}
+import javax.imageio.ImageIO
+import org.apache.commons.io.FileUtils
+import org.apache.log4j.Logger
+import org.opencv.core._
+import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgproc.Imgproc
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.io.Source
+import scala.sys.process._
+
+class CorrectnessSpec extends FlatSpec with Matchers {
+  val configPath = "/tmp/config.yaml"
+//  val configPath = "/home/litchy/pro/analytics-zoo/config.yaml"
+  var redisHost: String = "localhost"
+  var redisPort: Int = 6379
+  val logger = Logger.getLogger(getClass)
+  def resize(p: String): String = {
+    val source = ImageIO.read(new File(p))
+    val outputImage: BufferedImage = new BufferedImage(224, 224, source.getType)
+    val graphics2D: Graphics2D = outputImage.createGraphics
+
+    graphics2D.drawImage(source, 0, 0, 224, 224, null)
+    graphics2D.dispose()
+
+    val byteStream = new ByteArrayOutputStream()
+    ImageIO.write(outputImage, "jpg", byteStream)
+
+//    val f = new File("/home/litchy/tmp/034.jpg")
+//    ImageIO.write(outputImage, "jpg", f)
+    val dataStr = Base64.getEncoder.encodeToString(byteStream.toByteArray)
+    dataStr
+  }
+  def getBase64FromPath(path: String): String = {
+
+    val b = FileUtils.readFileToByteArray(new File(path))
+    val img = OpenCVMethod.fromImageBytes(b, Imgcodecs.CV_LOAD_IMAGE_COLOR)
+    Imgproc.resize(img, img, new Size(224, 224))
+    val matOfByte = new MatOfByte()
+    Imgcodecs.imencode(".jpg", img, matOfByte)
+    val dataStr = Base64.getEncoder.encodeToString(matOfByte.toArray)
+    dataStr
+  }
+
+
+//  def runServingBg(): Future[Unit] = Future {
+//    ClusterServing.run(configPath, redisHost, redisPort)
+//  }
+  "Cluster Serving result" should "be correct" in {
+
+    ("wget -O /tmp/serving_val.tar http://10.239.45.10:8081" +
+     "/repository/raw/analytics-zoo-data/imagenet_1k.tar").!
+    "tar -xvf /tmp/serving_val.tar -C /tmp/".!
+    ClusterServing.helper = new ClusterServingHelper()
+    val helper = ClusterServing.helper
+    helper.configPath = configPath
+    DeprecatedUtils.loadConfig(helper)
+//    helper.dataShape = Array(Array(3, 224, 224))
+    val model = helper.loadInferenceModel()
+    val imagePath = "/tmp/imagenet_1k"
+    val lsCmd = "ls " + imagePath
+
+    val totalNum = (lsCmd #| "wc").!!.split(" +").filter(_ != "").head.toInt
+
+    // enqueue image
+    val f = new File(imagePath)
+    val fileList = f.listFiles
+    logger.info(s"${fileList.size} images about to enqueue...")
+
+    val clusterServingInference = new ClusterServingInference()
+    var predictMap = Map[String, String]()
+
+    for (file <- fileList) {
+      val dataStr = getBase64FromPath(file.getAbsolutePath)
+      val instancesJson =
+       s"""{
+         |"instances": [
+         |   {
+         |     "img": "${dataStr}"
+         |   }
+         |]
+         |}
+         |""".stripMargin
+      val instances = JsonUtil.fromJson(classOf[Instances], instancesJson)
+      val inputBase64 = new String(java.util.Base64.getEncoder
+       .encode(instances.toArrow()))
+      val input = clusterServingInference.preProcessing.decodeArrowBase64("", inputBase64)
+      val bInput = clusterServingInference.batchInput(Seq(("", input)), 1, true, false)
+      val result = model.doPredict(bInput)
+      val value = PostProcessing(result.toTensor[Float], "topN(1)", 1)
+      val clz = value.split(",")(0).stripPrefix("[[")
+      predictMap = predictMap + (file.getName -> clz)
+    }
+    ("rm -rf /tmp/" + imagePath + "*").!
+    "rm -rf /tmp/serving_val_*".!
+    "rm -rf /tmp/config.yaml".!
+
+    // start check with txt file
+
+    var cN = 0f
+    var tN = 0f
+    for (line <- Source.fromFile(imagePath + ".txt").getLines()) {
+     val key = line.split(" ").head
+     val cls = line.split(" ").tail(0)
+     try {
+       if (predictMap(key) == cls) {
+         cN += 1
+       }
+       tN += 1
+     }
+     catch {
+       case _ => None
+     }
+    }
+    val acc = cN / tN
+    logger.info(s"Top 1 Accuracy of serving, Openvino ResNet50 Model on ImageNet is ${acc}")
+    assert(acc > 0.71)
+
+  }
+
+  "Cluster Serving batch inference result" should "be correct" in {
+
+    ("wget -O /tmp/serving_val.tar http://10.239.45.10:8081" +
+      "/repository/raw/analytics-zoo-data/imagenet_1k.tar").!
+    "tar -xvf /tmp/serving_val.tar -C /tmp/".!
+    ClusterServing.helper = new ClusterServingHelper()
+val helper = ClusterServing.helper
+    helper.configPath = configPath
+    DeprecatedUtils.loadConfig(helper)
+    //    helper.dataShape = Array(Array(3, 224, 224))
+    val model = helper.loadInferenceModel()
+    val imagePath = "/tmp/imagenet_1k"
+    val lsCmd = "ls " + imagePath
+
+    val totalNum = (lsCmd #| "wc").!!.split(" +").filter(_ != "").head.toInt
+
+    // enqueue image
+    val f = new File(imagePath)
+    val fileList = f.listFiles
+    logger.info(s"${fileList.size} images about to enqueue...")
+
+
+    val clusterServingInference = new ClusterServingInference()
+    var predictMap = Map[String, String]()
+
+    var batchInputs = Seq[(String, Activity)]()
+
+    for (file <- fileList) {
+      val dataStr = getBase64FromPath(file.getAbsolutePath)
+      val instancesJson =
+        s"""{
+           |"instances": [
+           |   {
+           |     "img": "${dataStr}"
+           |   }
+           |]
+           |}
+           |""".stripMargin
+      val instances = JsonUtil.fromJson(classOf[Instances], instancesJson)
+      val inputBase64 = new String(java.util.Base64.getEncoder
+        .encode(instances.toArrow()))
+      val input = clusterServingInference.preProcessing.decodeArrowBase64("", inputBase64)
+
+      if (batchInputs.length < 4) {
+        batchInputs = batchInputs :+ (file.getName(), input)
+      }
+      if (batchInputs.length == 4) {
+        val bInput = clusterServingInference.batchInput(batchInputs, 4, true, false)
+        val result = model.doPredict(bInput)
+        var i = 0
+        for ( i <- 1 to 4) {
+          val value = PostProcessing(result.toTensor[Float], "topN(1)", i)
+          val clz = value.split(",")(0).stripPrefix("[[")
+          predictMap = predictMap + (batchInputs(i-1)._1 -> clz)
+        }
+
+        batchInputs = Seq[(String, Activity)]()
+      }
+    }
+    ("rm -rf /tmp/" + imagePath + "*").!
+    "rm -rf /tmp/serving_val_*".!
+    "rm -rf /tmp/config.yaml".!
+
+    // start check with txt file
+
+    var cN = 0f
+    var tN = 0f
+    for (line <- Source.fromFile(imagePath + ".txt").getLines()) {
+      val key = line.split(" ").head
+      val cls = line.split(" ").tail(0)
+      try {
+        if (predictMap(key) == cls) {
+          cN += 1
+        }
+        tN += 1
+      }
+      catch {
+        case _ => None
+      }
+    }
+    val acc = cN / tN
+    logger.info(s"Top 1 Accuracy of serving, Openvino ResNet50 Model on ImageNet is ${acc}")
+    assert(acc > 0.71)
+
+  }
+}
