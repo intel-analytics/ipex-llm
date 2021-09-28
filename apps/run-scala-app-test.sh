cf994fa6df3173e40bea5b362de2a262eee20bb4
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-09-28 02:45:39 +0100

    remove fraud detection from test script - zoo master (#4849)

--- apps/run-scala-app-test.sh
+++ apps/run-scala-app-test.sh
@@ -8,58 +8,8 @@ export ANALYTICS_ZOO_HOME=${ANALYTICS_ZOO_ROOT}/dist
 export ANALYTICS_ZOO_JAR=`find ${ANALYTICS_ZOO_HOME}/lib -type f -name "analytics-zoo*jar-with-dependencies.jar"`
 export ANALYTICS_ZOO_JAR_AND_SPARK=`find ${ANALYTICS_ZOO_ROOT}/zoo/target -type f -name "analytics-zoo*serving.jar"`
 
-echo "#1 start fraud-detection scala app test"
-#timer
-start=$(date "+%s")
-
-FILENAME="${ANALYTICS_ZOO_HOME}/apps/fraud-detection/creditcard.csv"
-if [ -f "$FILENAME" ]
-then
-   echo "$FILENAME already exists."
-else
-	echo "Downloading creditcard.csv"
-	wget -P ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/ $FTP_URI/analytics-zoo-data/apps/fraudDetection/creditcard.csv
-	echo "Finished"
-fi
-
-#convert notebook to scala script
-jupyter nbconvert --to script ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/fraud-detection.ipynb
-
-#add object frauddetection extends App{}
-sed -i '/import com.intel.analytics.bigdl.utils.LoggerFilter/ a\ object frauddetection extends App{' ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/fraud-detection.scala
-sed -i '/evaluateModel(predictDF)/ a\ }' ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/fraud-detection.scala
-sed -i 's@"/tmp/datasets/creditcard.csv"@sys.env("ANALYTICS_ZOO_HOME")+"/apps/fraud-detection/creditcard.csv"@g' ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/fraud-detection.scala
-
-mkdir ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/bin
-
-#compile scala to .class
-scalac -classpath "$ANALYTICS_ZOO_JAR_AND_SPARK:${ANALYTICS_ZOO_HOME}/apps/fraud-detection/fraud-1.0.1-SNAPSHOT.jar:$SPARK_HOME/jars/*" \
-	-d ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/bin \
-    ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/fraud-detection.scala
-echo "compile scala to .class finished"
-
-#pack classes to fraud.jar
-cd ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/bin
-jar cvf fraud.jar *
-echo "pack classes to fraud.jar finished"
-
-master=local[*]
-$SPARK_HOME/bin/spark-submit \
-	--verbose --master $master \
-    --conf spark.executor.cores=1 \
-    --total-executor-cores 4 \
-    --driver-memory 200g \
-    --executor-memory 200g \
-    --class frauddetection \
-    --jars $ANALYTICS_ZOO_JAR_AND_SPARK,${ANALYTICS_ZOO_HOME}/apps/fraud-detection/fraud-1.0.1-SNAPSHOT.jar \
-    ${ANALYTICS_ZOO_HOME}/apps/fraud-detection/bin/fraud.jar
-
-now=$(date "+%s")
-time1=$((now-start))
-echo "#1 fraud-detection time used:$time1 seconds"
-
 echo "App[Model-inference-example] Test"
-echo "# Test 2 text-classification-training"
+echo "# Test 1 text-classification-training"
 
 cd ${ANALYTICS_ZOO_ROOT}/apps/model-inference-examples/
 mkdir "models"
@@ -99,16 +49,16 @@ ${ANALYTICS_ZOO_HOME}/bin/spark-shell-with-zoo.sh \
 --modelSaveDirPath "models/text-classification.bigdl"
 
 now=$(date "+%s")
-time2=$((now-start))
-echo "#App[Model-inference-example] Test 2: text-classification-training time used:$time2 seconds"
+time1=$((now-start))
+echo "#App[Model-inference-example] Test 1: text-classification-training time used:$time1 seconds"
 
-echo "# Test Apps -- 3.text-classification-inference"
+echo "# Test Apps -- 2.text-classification-inference"
 
 cd ${ANALYTICS_ZOO_ROOT}/apps/model-inference-examples/text-classification-inference
 mvn clean
 mvn clean package
 
-echo "# Test 3.1 text-classification-inference:SimpleDriver"
+echo "# Test 2.1 text-classification-inference:SimpleDriver"
 #timer
 start=$(date "+%s")
 
@@ -118,10 +68,10 @@ java -cp target/text-classification-inference-0.1.0-SNAPSHOT-jar-with-dependenci
 com.intel.analytics.zoo.apps.textclassfication.inference.SimpleDriver
 
 now=$(date "+%s")
-time3=$((now-start))
-echo "#App[Model-inference-example] Test 3.1: text-classification-inference:SimpleDriver time used:$time3 seconds"
+time2=$((now-start))
+echo "#App[Model-inference-example] Test 3.1: text-classification-inference:SimpleDriver time used:$time2 seconds"
 
-echo "# Test 3.2 text-classification-inference:WebServiceDriver"
+echo "# Test 2.2 text-classification-inference:WebServiceDriver"
 #timer
 start=$(date "+%s")
 
@@ -140,10 +90,10 @@ fi
 done
 
 now=$(date "+%s")
-time4=$((now-start))
-echo "#App[Model-inference-example] Test 3.2: text-classification-inference:WebServiceDriver time used:$time4 seconds"
+time3=$((now-start))
+echo "#App[Model-inference-example] Test 3.2: text-classification-inference:WebServiceDriver time used:$time3 seconds"
 
-echo "# Test 4.recommendation-inference"
+echo "# Test 3.recommendation-inference"
 
 #recommendation
 cd ${ANALYTICS_ZOO_ROOT}/apps/model-inference-examples/recommendation-inference
@@ -157,7 +107,7 @@ then
 else
     wget $FTP_URI/analytics-zoo-models/recommendation/ncf.bigdl -P analytics-zoo-models/recommendation/
 fi
-echo "# Test 4.1 recommendation-inference:SimpleScalaDriver"
+echo "# Test 3.1 recommendation-inference:SimpleScalaDriver"
 #timer
 start=$(date "+%s")
 
@@ -166,8 +116,8 @@ java -cp ./recommendation-inference/target/recommendation-inference-0.1.0-SNAPSH
 com.intel.analytics.zoo.apps.recommendation.inference.SimpleScalaDriver
 
 now=$(date "+%s")
-time5=$((now-start))
-echo "#App[Model-inference-example] Test 4.1: recommendation-inference:SimpleScalaDriver time used:$time5 seconds"
+time4=$((now-start))
+echo "#App[Model-inference-example] Test 3.1: recommendation-inference:SimpleScalaDriver time used:$time4 seconds"
 
 echo "# Test 4.2 recommendation-inference:SimpleDriver[Java]"
 #timer
@@ -178,8 +128,8 @@ java -cp ./recommendation-inference/target/recommendation-inference-0.1.0-SNAPSH
 com.intel.analytics.zoo.apps.recommendation.inference.SimpleDriver
 
 now=$(date "+%s")
-time6=$((now-start))
-echo "#App[Model-inference-example] Test 4.2: recommendation-inference:SimpleDriver time used:$time6 seconds"
+time5=$((now-start))
+echo "#App[Model-inference-example] Test 4.2: recommendation-inference:SimpleDriver time used:$time5 seconds"
 
 echo "# Test 5.model-inference-flink"
 
@@ -249,7 +199,6 @@ echo "#App[Model-inference-example] Test 5.1: model-inference-flink: Image Class
 
 ./flink-1.7.2/bin/stop-cluster.sh
 
-echo "#1 fraud-detection time used:$time1 seconds"
 echo "#2 text-classification-training time used:$time2 seconds"
 echo "#3.1 text-classification-inference:SimpleDriver time used:$time3 seconds"
 echo "#3.2 text-classification-inference:WebServiceDriver time used:$time4 seconds"
