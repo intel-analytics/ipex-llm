8126063563c30641e7b20f90de3a8f658d0e18b9
liangs6212 <80952198+liangs6212@users.noreply.github.com>
2021-09-29 19:21:06 +0800

    Chronos: AutoTSEstimator notebook (for customized model) (#4838)
    * init 3rd party model
    
    * add notebook test
    
    * fix grammatical errors
    
    * fix get_public_dataset
    
    * fix test script error
    
    * emmmm, fix syntax error
    
    * fix varibale error
    
    * emmmm,emmmm

--- apps/run-app-tests-pip.sh
+++ apps/run-app-tests-pip.sh
@@ -947,6 +947,43 @@ now=$(date "+%s")
 time25=$((now-start))
 echo "#25 chronos-network-traffic-autots-forecasting time used:$time25 seconds"
 
+echo "#26 start app test for chronos-network-traffic-autots-customized-model"
+#timer
+time26=$(date "+%s")
+${ANALYTICS_ZOO_HOME}/apps/ipynb2py.sh ${ANALYTICS_ZOO_HOME}/../pyzoo/zoo/chronos/use-case/network_traffic/network_traffic_autots_customized_model
+
+sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${ANALYTICS_ZOO_HOME}/../pyzoo/zoo/chronos/use-case/network_traffic/network_traffic_autots_customized_model.py
+sed -i 's/epochs=5/epochs=1/g' ${ANALYTICS_ZOO_HOME}/../pyzoo/zoo/chronos/use-case/network_traffic/network_traffic_autots_customized_model.py
+
+if [ -f ~/.chronos/dataset/network_traffic/network_traffic_data.csv ]
+then
+    echo "network_traffic_data.csv exists."
+else
+
+    echo "Download network traffic data."    
+    wget $FTP_URI/analytics-zoo-data/network-traffic/data/data.csv -P ~/.chronos/dataset/network_traffic/
+
+    cd ~/.chronos/dataset/network_traffic/
+    mv data.csv network_traffic_data.csv
+
+    echo "Finished downloading network_traffic_data.csv"
+
+    cd -
+fi
+
+python ${ANALYTICS_ZOO_HOME}/../pyzoo/zoo/chronos/use-case/network_traffic/network_traffic_autots_customized_model.py
+
+exit_status=$?
+if [ $exit_status -ne 0 ];
+then
+    clear_up
+    echo "chronos network_traffic_autots_customized_model failed."
+    exit $exit_status
+fi
+now=$(date "+%s")
+time26=$((now-start))
+echo "#26 network_traffic_autots_customized_model time used:$time26 seconds"
+
 fi
 
 # This should be done at the very end after all tests finish.
