5ccd49d8e97ed60ca9d1415d889c2d8757b918b5
liangs6212 <80952198+liangs6212@users.noreply.github.com>
2021-09-29 13:34:12 +0800

    Chronos: del quickstart example(autotstrainer) (#4855)
    * remove autots example
    
    * fix test script error

--- python/orca/dev/example/run-example-test-ray.sh
+++ python/orca/dev/example/run-example-test-ray.sh
@@ -186,24 +186,7 @@ python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/auto_model/autoprophet_n
 now=$(date "+%s")
 time13=$((now-start))
 
-echo "#14 start example for chronos autots_nyc_taxi"
-start=$(date "+%s")
-
-if [ -f ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/nyc_taxi.csv ]
-then
-    echo "nyc_taxi.csv already exists"
-else
-    wget -nv $FTP_URI/analytics-zoo-data/apps/nyc-taxi/nyc_taxi.csv -P \
-    ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/
-fi
-
-python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/autots_nyc_taxi.py \
-    --datadir ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/quickstart/nyc_taxi.csv
-
-now=$(date "+%s")
-time14=$((now-start))
-
-echo "#15 start example for chronos simulator-dpgansimulator-wwt"
+echo "#14 start example for chronos simulator-dpgansimulator-wwt"
 start=$(date "+%s")
 
 if [ -f ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/simulator/data_train_small.npz ]
@@ -219,9 +202,9 @@ python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/simulator/dpgansimulator
     --epoch 1 --plot_figures False
 
 now=$(date "+%s")
-time15=$((now-start))
+time14=$((now-start))
 
-echo "#16 start example for chronos distributed_training_network_traffic"
+echo "#15 start example for chronos distributed_training_network_traffic"
 start=$(date "+%s")
 
 if [ -f ~/.chronos/dataset/network_traffic/network_traffic_data.csv ]
@@ -235,9 +218,9 @@ fi
 python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/distributed/distributed_training_network_traffic.py
 
 now=$(date "+%s")
-time16=$((now-start))
+time15=$((now-start))
 
-echo "#17 start example for chronos onnx_autotsestimator_nyc_taxi"
+echo "#16 start example for chronos onnx_autotsestimator_nyc_taxi"
 start=$(date "+%s")
 
 if [ ! -f ~/.chronos/dataset/nyc_taxi/nyc_taxi_data.csv ]
@@ -254,9 +237,9 @@ sed -i '/onnx/d' ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/onnx/onnx_auto
 python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/onnx/onnx_autotsestimator_nyc_taxi.py
 
 now=$(date "+%s")
-time17=$((now-start))
+time16=$((now-start))
 
-echo "#18 start example for chronos onnx_autotsestimator_nyc_taxi"
+echo "#17 start example for chronos onnx_autotsestimator_nyc_taxi"
 start=$(date "+%s")
 
 if [ ! -f ~/.chronos/dataset/network_traffic/network_traffic_data.csv ]
@@ -273,7 +256,7 @@ sed -i '/onnx/d' ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/onnx/onnx_fore
 python ${ANALYTICS_ZOO_ROOT}/pyzoo/zoo/chronos/examples/onnx/onnx_forecaster_network_traffic.py
 
 now=$(date "+%s")
-time18=$((now-start))
+time17=$((now-start))
 
 echo "Ray example tests finished"
 echo "#1 orca rl_pong time used:$time1 seconds"
@@ -289,8 +272,7 @@ echo "#10 orca auto-xgboost-regressor time used:$time10 seconds"
 echo "#11 orca autoestimator-pytorch time used:$time11 seconds"
 echo "#12 chronos autolstm_nyc_taxi time used:$time12 seconds"
 echo "#13 chronos autoprophet_nyc_taxi time used:$time13 seconds"
-echo "#14 chronos autots_nyc_taxi time used:$time14 seconds"
-echo "#15 chronos simulator-dpgansimulator-wwt time used:$time15 seconds"
-echo "#16 chronos distributed_training_network_traffic time used:$time16 seconds"
-echo "#17 chronos onnx_autotsestimator_nyc_taxi time used:$time17 seconds"
-echo "#18 chronos onnx_forecaster_network_traffic time used:$time18 seconds"
+echo "#14 chronos simulator-dpgansimulator-wwt time used:$time14 seconds"
+echo "#15 chronos distributed_training_network_traffic time used:$time15 seconds"
+echo "#16 chronos onnx_autotsestimator_nyc_taxi time used:$time16 seconds"
+echo "#17 chronos onnx_forecaster_network_traffic time used:$time17 seconds"
