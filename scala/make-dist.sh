45628f66422dfb4df31ef2b214daa3d395faa91c
Kai Huang <huangkaivision@gmail.com>
2021-10-09 14:47:48 +0800

    add make dist and fix python xml

--- scala/make-dist.sh
+++ scala/make-dist.sh
@@ -75,4 +75,6 @@ cp -r $BASEDIR/dllib/target/bigdl-dllib-*-jar-with-dependencies.jar $DIST_DIR/li
 cp -r $BASEDIR/dllib/target/bigdl-dllib-*-python-api.zip $DIST_DIR/lib
 cp -r $BASEDIR/orca/target/bigdl-orca-*-jar-with-dependencies.jar $DIST_DIR/lib
 cp -r $BASEDIR/orca/target/bigdl-orca-*-python-api.zip $DIST_DIR/lib
+cp -r $BASEDIR/friesian/target/bigdl-friesian-*-jar-with-dependencies.jar $DIST_DIR/lib
+cp -r $BASEDIR/friesian/target/bigdl-friesian-*-python-api.zip $DIST_DIR/lib
 cp -r $BASEDIR/dllib/src/main/resources/spark-bigdl.conf $DIST_DIR/conf
