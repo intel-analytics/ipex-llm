343e2be697658f6482672531fc5e311c1d466900
sgwhat <njusongge@gmail.com>
2021-10-09 16:06:39 +0800

    move examples into source file

---
+++ python/orca/src/bigdl/orca/example/tfpark/tf_optimizer/evaluate.py
@@ -0,0 +1,77 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from optparse import OptionParser
+import tensorflow as tf
+from bigdl.dllib.utils.nncontext import init_nncontext
+from bigdl.orca.tfpark import TFDataset, TFPredictor
+import numpy as np
+import sys
+
+from bigdl.dataset import mnist
+
+sys.path.append("/tmp/models/slim")  # add the slim library
+from nets import lenet
+
+slim = tf.contrib.slim
+
+
+def main(options, data_num):
+
+    data_path = '/tmp/mnist' if not options.data_path else options.data_path
+    sc = init_nncontext()
+
+    # get data, pre-process and create TFDataset
+    (images_data, labels_data) = mnist.read_data_sets(data_path, "test")
+    images_data = (images_data[:data_num] - mnist.TRAIN_MEAN) / mnist.TRAIN_STD
+    labels_data = labels_data[:data_num].astype(np.int32)
+    dataset = TFDataset.from_ndarrays((images_data, labels_data), batch_per_thread=20)
+
+    # construct the model from TFDataset
+    images, labels = dataset.tensors
+
+    labels = tf.squeeze(labels)
+
+    with slim.arg_scope(lenet.lenet_arg_scope()):
+        logits, end_points = lenet.lenet(images, num_classes=10, is_training=False)
+
+    predictions = tf.to_int32(tf.argmax(logits, axis=1))
+    correct = tf.expand_dims(tf.to_int32(tf.equal(predictions, labels)), axis=1)
+
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, "/tmp/lenet/model")
+
+        predictor = TFPredictor(sess, [correct])
+
+        accuracy = predictor.predict().mean()
+
+        print("predict accuracy is %s" % accuracy)
+
+
+if __name__ == '__main__':
+
+    data_num = 10000
+
+    if len(sys.argv) > 1:
+        data_num = int(sys.argv[1])
+
+    parser = OptionParser()
+    parser.add_option("--data_path", dest="data_path")
+    (options, args) = parser.parse_args(sys.argv)
+
+    main(options, data_num)
---
+++ python/orca/src/bigdl/orca/example/torchmodel/train/mnist/main.py
@@ -0,0 +1,126 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from bigdl.orca.torch import TorchModel, TorchLoss, TorchOptim
+from bigdl.dllib.estimator import *
+from bigdl.optim.optimizer import SGD, Adam
+from bigdl.dllib.utils.nncontext import *
+from bigdl.dllib.feature.common import FeatureSet
+from bigdl.dllib.keras.metrics import Accuracy
+from bigdl.dllib.utils.utils import detect_conda_env_name
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--dir', default='/tmp/data', metavar='N',
+                        help='the folder store mnist data')
+    parser.add_argument('--batch-size', type=int, default=256, metavar='N',
+                        help='input batch size for training per executor(default: 256)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing per executor(default: 1000)')
+    parser.add_argument('--epochs', type=int, default=2, metavar='N',
+                        help='number of epochs to train (default: 2)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+
+    torch.manual_seed(args.seed)
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(args.dir, train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(args.dir, train=False,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=False)
+
+    # init on yarn when HADOOP_CONF_DIR and ZOO_CONDA_NAME is provided.
+    if os.environ.get('HADOOP_CONF_DIR') is None:
+        sc = init_spark_on_local(cores=1, conf={"spark.driver.memory": "20g"})
+    else:
+        num_executors = 2
+        num_cores_per_executor = 4
+        hadoop_conf_dir = os.environ.get('HADOOP_CONF_DIR')
+        zoo_conda_name = detect_conda_env_name()  # auto detect current conda env name
+        sc = init_spark_on_yarn(
+            hadoop_conf=hadoop_conf_dir,
+            conda_name=zoo_conda_name,
+            num_executors=num_executors,
+            executor_cores=num_cores_per_executor,
+            executor_memory="2g",
+            driver_memory="10g",
+            driver_cores=1,
+            conf={"spark.rpc.message.maxSize": "1024",
+                  "spark.task.maxFailures":  "1",
+                  "spark.driver.extraJavaOptions": "-Dbigdl.failure.retryTimes=1"})
+
+    model = Net()
+    model.train()
+    criterion = nn.NLLLoss()
+
+    adam = torch.optim.Adam(model.parameters(), lr=args.lr)
+    zoo_model = TorchModel.from_pytorch(model)
+    zoo_criterion = TorchLoss.from_pytorch(criterion)
+    zoo_optim = TorchOptim.from_pytorch(adam)
+    zoo_estimator = Estimator(zoo_model, optim_methods=zoo_optim)
+    train_featureset = FeatureSet.pytorch_dataloader(train_loader)
+    test_featureset = FeatureSet.pytorch_dataloader(test_loader)
+    from bigdl.optim.optimizer import MaxEpoch, EveryEpoch
+    zoo_estimator.train_minibatch(train_featureset, zoo_criterion,
+                                  end_trigger=MaxEpoch(args.epochs),
+                                  checkpoint_trigger=EveryEpoch(),
+                                  validation_set=test_featureset,
+                                  validation_method=[Accuracy()])
+
+
+if __name__ == '__main__':
+    main()
