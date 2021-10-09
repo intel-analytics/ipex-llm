da9d94355bdaf227fc8bef8c03e99c3d9632520b
sgwhat <njusongge@gmail.com>
2021-10-09 16:06:39 +0800

    move examples into source file

---
+++ python/orca/src/bigdl/orca/example/tfpark/estimator/estimator_dataset.py
@@ -0,0 +1,78 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import tensorflow as tf
+import numpy as np
+from bigdl.dllib.utils.nncontext import init_nncontext
+from bigdl.orca.tfpark import TFDataset, TFEstimator
+from bigdl.orca.tfpark import ZooOptimizer
+
+
+def get_data(dataset):
+    from bigdl.dataset import mnist
+    (images_data, labels_data) = mnist.read_data_sets("/tmp/mnist", dataset)
+    images_data = (images_data - mnist.TRAIN_MEAN) / mnist.TRAIN_STD
+    return (images_data, labels_data.astype(np.int32))
+
+
+def main():
+    sc = init_nncontext()
+
+    def model_fn(features, labels, mode):
+        from nets import lenet
+        slim = tf.contrib.slim
+        with slim.arg_scope(lenet.lenet_arg_scope()):
+            logits, end_points = lenet.lenet(features, num_classes=10, is_training=True)
+
+        if mode == tf.estimator.ModeKeys.EVAL or mode == tf.estimator.ModeKeys.TRAIN:
+            loss = tf.reduce_mean(
+                tf.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels))
+
+            optimizer = ZooOptimizer(tf.train.AdamOptimizer())
+            train_op = optimizer.minimize(loss)
+            return tf.estimator.EstimatorSpec(mode, predictions=logits,
+                                              loss=loss, train_op=train_op)
+        else:
+            return tf.estimator.EstimatorSpec(mode, predictions=logits)
+
+    def input_fn(mode):
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            training_data = get_data("train")
+            dataset = TFDataset.from_ndarrays(training_data, batch_size=320)
+        elif mode == tf.estimator.ModeKeys.EVAL:
+            testing_data = get_data("test")
+            dataset = TFDataset.from_ndarrays(testing_data, batch_per_thread=80)
+        else:
+            images, _ = get_data("test")
+            dataset = TFDataset.from_ndarrays(images, batch_per_thread=80)
+
+        return dataset
+    estimator = TFEstimator.from_model_fn(model_fn, model_dir="/tmp/estimator")
+
+    estimator.train(input_fn, steps=10)
+
+    metrics = estimator.evaluate(input_fn, ["acc"])
+    print(metrics)
+
+    predictions = estimator.predict(input_fn)
+
+    print(predictions.first())
+    print("finished...")
+    sc.stop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ python/orca/src/bigdl/orca/example/tfpark/tf_optimizer/train.py
@@ -0,0 +1,82 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import tensorflow as tf
+from bigdl.dllib.utils.nncontext import init_nncontext
+from bigdl.orca.tfpark import TFOptimizer, TFDataset
+from bigdl.optim.optimizer import *
+import numpy as np
+import sys
+
+from bigdl.dataset import mnist
+from bigdl.dataset.transformer import *
+
+sys.path.append("/tmp/models/slim")  # add the slim library
+from nets import lenet
+
+slim = tf.contrib.slim
+
+
+def accuracy(logits, labels):
+    predictions = tf.argmax(logits, axis=1, output_type=labels.dtype)
+    is_correct = tf.cast(tf.equal(predictions, labels), dtype=tf.float32)
+    return tf.reduce_mean(is_correct)
+
+
+def main(max_epoch, data_num):
+    sc = init_nncontext()
+
+    # get data, pre-process and create TFDataset
+    (train_images_data, train_labels_data) = mnist.read_data_sets("/tmp/mnist", "train")
+    (test_images_data, test_labels_data) = mnist.read_data_sets("/tmp/mnist", "train")
+
+    train_images_data = (train_images_data[:data_num] - mnist.TRAIN_MEAN) / mnist.TRAIN_STD
+    train_labels_data = train_labels_data[:data_num].astype(np.int)
+    test_images_data = (test_images_data[:data_num] - mnist.TRAIN_MEAN) / mnist.TRAIN_STD
+    test_labels_data = (test_labels_data[:data_num]).astype(np.int)
+    dataset = TFDataset.from_ndarrays((train_images_data, train_labels_data),
+                                      batch_size=360,
+                                      val_tensors=(test_images_data, test_labels_data))
+
+    # construct the model from TFDataset
+    images, labels = dataset.tensors
+
+    with slim.arg_scope(lenet.lenet_arg_scope()):
+        logits, end_points = lenet.lenet(images, num_classes=10, is_training=True)
+
+    loss = tf.reduce_mean(tf.losses.sparse_softmax_cross_entropy(logits=logits, labels=labels))
+
+    acc = accuracy(logits, labels)
+
+    # create a optimizer
+    optimizer = TFOptimizer.from_loss(loss, Adam(1e-3),
+                                      metrics={"acc": acc},
+                                      model_dir="/tmp/lenet/")
+    # kick off training
+    optimizer.optimize(end_trigger=MaxEpoch(max_epoch))
+
+    saver = tf.train.Saver()
+    saver.save(optimizer.sess, "/tmp/lenet/model")
+
+
+if __name__ == '__main__':
+
+    max_epoch = 5
+    data_num = 60000
+
+    if len(sys.argv) > 1:
+        max_epoch = int(sys.argv[1])
+        data_num = int(sys.argv[2])
+    main(max_epoch, data_num)
