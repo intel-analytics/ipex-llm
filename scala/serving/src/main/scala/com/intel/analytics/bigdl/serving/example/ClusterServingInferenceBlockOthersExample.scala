be760d17b65cf93e0f563dd2015c367c58d10e42
Song Jiaming <litchy233@gmail.com>
2021-10-12 02:08:02 +0000

    serving example (#4924)

---
+++ scala/serving/src/main/scala/com/intel/analytics/bigdl/serving/example/ClusterServingInferenceBlockOthersExample.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018 BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.serving.example
+
+import org.apache.flink.api.common.functions.RichMapFunction
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
+import org.apache.flink.streaming.api.functions.source.{RichParallelSourceFunction, SourceFunction}
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.log4j.{Level, Logger}
+
+object ClusterServingInferenceBlockOthersExample {
+  class MySource extends RichParallelSourceFunction[String] {
+    var cnt = 0
+    @volatile var isRunning = true
+
+    override def open(parameters: Configuration): Unit = {
+      println("Source opened")
+    }
+    override def run(ctx: SourceFunction.SourceContext[String]): Unit =
+      while (isRunning) {
+        cnt += 1
+        ctx.collect(cnt.toString)
+      }
+
+    override def cancel(): Unit = {
+    }
+  }
+  class MyMap extends RichMapFunction[String, String] {
+    override def map(value: String): String = {
+      println(s"preprocessing")
+      Model.synchronized {
+        while (Model.queueing != 0) {
+          println("waiting during preprocessing")
+          Model.wait()
+        }
+      }
+      Thread.sleep(50)
+      println(s"predicting")
+      var res = ""
+      var holder: String = null
+      Model.synchronized {
+        Model.queueing += 1
+        while (Model.model == null) {
+          println("waiting during predict")
+          Model.wait()
+        }
+        holder = Model.model
+        Model.model = null
+      }
+      println(s"Model taken, name is: $holder")
+      res = holder + ": " + value
+      Thread.sleep(200)
+      Model.synchronized {
+        Model.model = "MyModel"
+        Model.queueing -= 1
+        Model.notifyAll()
+      }
+      res
+    }
+  }
+  class MySink extends RichSinkFunction[String] {
+    override def invoke(value: String, context: SinkFunction.Context[_]): Unit = {
+      println(s"value $value written to sink.")
+    }
+  }
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  def main(args: Array[String]): Unit = {
+    val serving = StreamExecutionEnvironment.getExecutionEnvironment
+    serving.addSource(new MySource())
+      .map(new MyMap())
+      .addSink(new MySink())
+    serving.execute()
+  }
+}
+
+object Model {
+  var queueing: Int = 0
+  var model: String = "MyModel"
+}
---
+++ scala/serving/src/main/scala/com/intel/analytics/bigdl/serving/example/ClusterServingOperatorUsageExample.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2018 BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.serving.example
+
+import com.intel.analytics.bigdl.dllib.nn.abstractnn.Activity
+import com.intel.analytics.bigdl.serving.operator.{ClusterServingInferenceOperator, ClusterServingInput}
+import com.intel.analytics.bigdl.serving.serialization.ArrowDeserializer
+import com.intel.analytics.bigdl.serving.utils.Conventions
+import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
+import org.apache.flink.streaming.api.functions.source.{RichParallelSourceFunction, SourceFunction}
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.log4j.{Level, Logger}
+import scopt.OptionParser
+
+object ClusterServingOperatorUsageExample {
+  case class ExampleParams(modelPath: String = null)
+  val parser = new OptionParser[ExampleParams]("Cluster Serving Operator Usage Example") {
+    opt[String]('m', "modelPath")
+      .text("Model Path of Cluster Serving")
+      .action((x, params) => params.copy(modelPath = x))
+      .required()
+  }
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  def main(args: Array[String]): Unit = {
+    val arg = parser.parse(args, ExampleParams()).head
+
+    val serving = StreamExecutionEnvironment.getExecutionEnvironment
+    // Use Flink distributed cache to copy model to every executors
+    serving.registerCachedFile(arg.modelPath, Conventions.SERVING_MODEL_TMP_DIR)
+    serving.addSource(new MySource()).setParallelism(1)
+        .map(new ClusterServingInferenceOperator()).setParallelism(1)
+        .addSink(new MySink()).setParallelism(1)
+    serving.execute()
+  }
+
+}
+class MySource extends RichParallelSourceFunction[List[(String, Activity)]] {
+  var cnt = 0
+  @volatile var isRunning = true
+  override def run(ctx: SourceFunction.SourceContext[List[(String, Activity)]]): Unit =
+    while (isRunning) {
+      cnt += 1
+      val valueArr = new Array[Float](128)
+      (0 until 128).foreach(i => valueArr(i) = i)
+      val input = ClusterServingInput(cnt.toString, valueArr)
+      ctx.collect(List((cnt.toString, input)))
+  }
+
+  override def cancel(): Unit = {
+  }
+}
+class MySink extends RichSinkFunction[List[(String, String)]] {
+  override def invoke(value: List[(String, String)], context: SinkFunction.Context[_]): Unit = {
+    value.foreach(kv => {
+      println(s"Id ${kv._1} Cluster Serving inference result is ${ArrowDeserializer(kv._2)}")
+    })
+  }
+}
