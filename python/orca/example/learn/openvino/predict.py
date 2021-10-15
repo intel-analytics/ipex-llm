45d7aa0321ee8cf26759759f0e62594966841492
Yina Chen <33650826+cyita@users.noreply.github.com>
2021-10-15 10:18:37 +0800

    [bigdl2.0] Orca openvino est to 2.0 (#4974)
    * update orca openvino est
    
    * update readme

---
+++ python/orca/example/learn/openvino/predict.py
@@ -0,0 +1,78 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import print_function
+import argparse
+import numpy as np
+import time
+import cv2
+import glob
+import math
+from bigdl.orca.learn.openvino import Estimator
+
+from bigdl.orca import init_orca_context, stop_orca_context
+
+
+def crop(img, w, h):
+    center = np.array(img.shape) / 2
+    x = center[1] - w / 2
+    y = center[0] - h / 2
+    crop_img = img[int(y):int(y + h), int(x):int(x + w)]
+    crop_img = np.transpose(crop_img, (2, 0, 1))
+    return crop_img
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='PyTorch Tensorboard Example')
+    parser.add_argument('--cluster_mode', type=str, default="local",
+                        help='The cluster mode, such as local, yarn.')
+    parser.add_argument('--model_path', type=str, default="./model.xml",
+                        help="Path to the OpenVINO model file")
+    parser.add_argument('--image_folder', type=str, default="./",
+                        help="The path to the folder where the images are stored.")
+    parser.add_argument('--core_num', type=int, default=4,
+                        help="The number of cpu cores you want to use on each node. "
+                             "You can change it depending on your own cluster setting.")
+    parser.add_argument('--executor_num', type=int, default=2,
+                        help="The number of executors when cluster_mode=yarn.")
+    parser.add_argument('--data_num', type=int, default=12, help="The number of dummy data.")
+    parser.add_argument('--batch_size', type=int, default=4, help="The batch size of inference.")
+    parser.add_argument('--memory', type=str, default="2g", help="The executor memory size.")
+    args = parser.parse_args()
+
+    if args.cluster_mode == "local":
+        init_orca_context(cores=args.core_num, memory=args.memory)
+    elif args.cluster_mode == "yarn":
+        init_orca_context(cluster_mode=args.cluster_mode, cores=args.core_num,
+                          num_nodes=args.executor_num, memory=args.memory)
+
+    images = [cv2.imread(file) for file in
+              glob.glob(args.image_folder + "/*.jpg")]
+    images = [crop(img, 416, 416) for img in images]
+    image_num = len(images)
+    copy_time = math.ceil(args.data_num/image_num)
+    images = images * copy_time
+    images = np.array(images[:args.data_num])
+    est = Estimator.from_openvino(model_path=args.model_path)
+    start = time.time()
+    result = est.predict(images, batch_size=args.batch_size)
+    end = time.time()
+    print("Throughput: ", args.data_num / (end - start))
+    assert isinstance(result, list)
+    assert result[0].shape == (args.data_num, 255, 13, 13)
+    assert result[1].shape == (args.data_num, 255, 26, 26)
+    assert result[2].shape == (args.data_num, 255, 52, 52)
+    stop_orca_context()
--- python/orca/src/bigdl/orca/learn/openvino/estimator.py
+++ python/orca/src/bigdl/orca/learn/openvino/estimator.py
@@ -14,50 +14,36 @@
 # limitations under the License.
 #
 import math
+import os.path
 
-from bigdl.orca.inference import InferenceModel
+from pyspark.sql import DataFrame
+
 from bigdl.orca.data import SparkXShards
 from bigdl.orca.learn.spark_estimator import Estimator as SparkEstimator
 from bigdl.dllib.utils.common import get_node_and_core_number
 from bigdl.dllib.utils import nest
 from bigdl.dllib.utils.nncontext import init_nncontext
 
+from openvino.inference_engine import IECore
 import numpy as np
 
 
 class Estimator(object):
     @staticmethod
-    def from_openvino(*, model_path, batch_size=0):
+    def from_openvino(*, model_path):
         """
         Load an openVINO Estimator.
 
         :param model_path: String. The file path to the OpenVINO IR xml file.
-        :param batch_size: Int. Set batch Size, default is 0 (use default batch size).
         """
-        return OpenvinoEstimator(model_path=model_path, batch_size=batch_size)
+        return OpenvinoEstimator(model_path=model_path)
 
 
 class OpenvinoEstimator(SparkEstimator):
     def __init__(self,
                  *,
-                 model_path,
-                 batch_size=0):
-        self.node_num, self.core_num = get_node_and_core_number()
-        self.path = model_path
-        if batch_size != 0:
-            self.batch_size = batch_size
-        else:
-            import xml.etree.ElementTree as ET
-            tree = ET.parse(model_path)
-            root = tree.getroot()
-            shape_item = root.find('./layers/layer/output/port/dim[1]')
-            if shape_item is None:
-                raise ValueError("Invalid openVINO IR xml file, please check your model_path")
-            self.batch_size = int(shape_item.text)
-        self.model = InferenceModel(supported_concurrent_num=self.core_num)
-        self.model.load_openvino_ng(model_path=model_path,
-                                    weight_path=model_path[:model_path.rindex(".")] + ".bin",
-                                    batch_size=self.batch_size)
+                 model_path):
+        self.load(model_path)
 
     def fit(self, data, epochs, batch_size=32, feature_cols=None, label_cols=None,
             validation_data=None, checkpoint_trigger=None):
@@ -66,10 +52,11 @@ class OpenvinoEstimator(SparkEstimator):
         """
         raise NotImplementedError
 
-    def predict(self, data, feature_cols=None):
+    def predict(self, data, feature_cols=None, batch_size=4):
         """
         Predict input data
 
+        :param batch_size: Int. Set batch Size, default is 4.
         :param data: data to be predicted. XShards, Spark DataFrame, numpy array and list of numpy
                arrays are supported. If data is XShards, each partition is a dictionary of  {'x':
                feature}, where feature(label) is a numpy array or a list of numpy arrays.
@@ -82,7 +69,55 @@ class OpenvinoEstimator(SparkEstimator):
                  If the input data is numpy arrays or list of numpy arrays, the predict result is
                  a numpy array or a list of numpy arrays.
         """
-        from pyspark.sql import DataFrame
+        sc = init_nncontext()
+        model_bytes_broadcast = sc.broadcast(self.model_bytes)
+        weight_bytes_broadcast = sc.broadcast(self.weight_bytes)
+
+        def partition_inference(partition):
+            model_bytes = model_bytes_broadcast.value
+            weight_bytes = weight_bytes_broadcast.value
+            partition = list(partition)
+            data_num = len(partition)
+            ie = IECore()
+            config = {'CPU_THREADS_NUM': str(self.core_num)}
+            ie.set_config(config, 'CPU')
+            net = ie.read_network(model=model_bytes,
+                                  weights=weight_bytes, init_from_buffer=True)
+            net.batch_size = batch_size
+            local_model = ie.load_network(network=net, device_name="CPU",
+                                          num_requests=data_num)
+            inputs = list(iter(local_model.requests[0].input_blobs))
+            outputs = list(iter(local_model.requests[0].output_blobs))
+            assert len(outputs) != 0, "The number of model outputs should not be 0."
+
+            def add_elem(d):
+                d_len = len(d)
+                if d_len < batch_size:
+                    rep_time = [1] * (d_len - 1)
+                    rep_time.append(batch_size - d_len + 1)
+                    return np.repeat(d, rep_time, axis=0), d_len
+                else:
+                    return d, d_len
+
+            results = []
+            for idx, batch_data in enumerate(partition):
+                infer_request = local_model.requests[idx]
+                input_dict = dict()
+                elem_num = 0
+                if isinstance(batch_data, list):
+                    for i, input in enumerate(inputs):
+                        input_dict[input], elem_num = add_elem(batch_data[i])
+                else:
+                    input_dict[inputs[0]], elem_num = add_elem(batch_data)
+                infer_request.infer(input_dict)
+                if len(outputs) == 1:
+                    results.append(infer_request.output_blobs[outputs[0]].buffer[:elem_num])
+                else:
+                    results.append(list(map(lambda output:
+                                            infer_request.output_blobs[output].buffer[:elem_num],
+                                            outputs)))
+
+            return results
 
         def predict_transform(dict_data, batch_size):
             assert isinstance(dict_data, dict), "each shard should be an dict"
@@ -105,8 +140,6 @@ class OpenvinoEstimator(SparkEstimator):
                 raise ValueError("x in each shard should be a ndarray or a list of ndarray.")
             return feature_data
 
-        sc = init_nncontext()
-
         if isinstance(data, DataFrame):
             from bigdl.orca.learn.utils import dataframe_to_xshards, convert_predict_rdd_to_dataframe
             xshards, _ = dataframe_to_xshards(data,
@@ -114,36 +147,30 @@ class OpenvinoEstimator(SparkEstimator):
                                               feature_cols=feature_cols,
                                               label_cols=None,
                                               mode="predict")
-            transformed_data = xshards.transform_shard(predict_transform, self.batch_size)
-            result_rdd = self.model.distributed_predict(transformed_data.rdd, sc)
-
-            def delete_useless_result(data):
-                shard, y = data
-                data_length = len(shard["x"])
-                return y[: data_length]
-            result_rdd = xshards.rdd.zip(result_rdd).map(delete_useless_result)
+            transformed_data = xshards.transform_shard(predict_transform, batch_size)
+            result_rdd = transformed_data.rdd.mapPartitions(lambda iter: partition_inference(iter))
             return convert_predict_rdd_to_dataframe(data, result_rdd.flatMap(lambda data: data))
         elif isinstance(data, SparkXShards):
-            transformed_data = data.transform_shard(predict_transform, self.batch_size)
-            result_rdd = self.model.distributed_predict(transformed_data.rdd, sc)
+            transformed_data = data.transform_shard(predict_transform, batch_size)
+            result_rdd = transformed_data.rdd.mapPartitions(lambda iter: partition_inference(iter))
 
-            def update_shard(data):
+            def update_result_shard(data):
                 shard, y = data
-                data_length = len(shard["x"])
-                shard["prediction"] = y[: data_length]
+                shard["prediction"] = y
                 return shard
-            return SparkXShards(data.rdd.zip(result_rdd).map(update_shard))
+            return SparkXShards(data.rdd.zip(result_rdd).map(update_result_shard))
         elif isinstance(data, (np.ndarray, list)):
             if isinstance(data, np.ndarray):
-                split_num = math.ceil(len(data)/self.batch_size)
+                split_num = math.ceil(len(data)/batch_size)
                 arrays = np.array_split(data, split_num)
-                data_length_list = list(map(lambda arr: len(arr), arrays))
-                data_rdd = sc.parallelize(arrays, numSlices=split_num)
+                num_slices = min(split_num, self.node_num)
+                data_rdd = sc.parallelize(arrays, numSlices=num_slices)
             elif isinstance(data, list):
                 flattened = nest.flatten(data)
                 data_length = len(flattened[0])
                 data_to_be_rdd = []
-                split_num = math.ceil(flattened[0].shape[0]/self.batch_size)
+                split_num = math.ceil(flattened[0].shape[0]/batch_size)
+                num_slices = min(split_num, self.node_num)
                 for i in range(split_num):
                     data_to_be_rdd.append([])
                 for x in flattened:
@@ -156,16 +183,19 @@ class OpenvinoEstimator(SparkEstimator):
                     x_parts = np.array_split(x, split_num)
                     for idx, x_part in enumerate(x_parts):
                         data_to_be_rdd[idx].append(x_part)
-                        data_length_list = list(map(lambda arr: len(arr), x_part))
 
                 data_to_be_rdd = [nest.pack_sequence_as(data, shard) for shard in data_to_be_rdd]
-                data_rdd = sc.parallelize(data_to_be_rdd, numSlices=split_num)
+                data_rdd = sc.parallelize(data_to_be_rdd, numSlices=num_slices)
 
-            result_rdd = self.model.distributed_predict(data_rdd, sc)
+            print("Partition number: ", data_rdd.getNumPartitions())
+            result_rdd = data_rdd.mapPartitions(lambda iter: partition_inference(iter))
             result_arr_list = result_rdd.collect()
-            for i in range(0, len(result_arr_list)):
-                result_arr_list[i] = result_arr_list[i][:data_length_list[i]]
-            result_arr = np.concatenate(result_arr_list, axis=0)
+            result_arr = None
+            if isinstance(result_arr_list[0], list):
+                result_arr = [np.concatenate([r[i] for r in result_arr_list], axis=0)
+                              for i in range(len(result_arr_list[0]))]
+            elif isinstance(result_arr_list[0], np.ndarray):
+                result_arr = np.concatenate(result_arr_list, axis=0)
             return result_arr
         else:
             raise ValueError("Only XShards, Spark DataFrame, a numpy array and a list of numpy arr"
@@ -189,30 +219,21 @@ class OpenvinoEstimator(SparkEstimator):
         """
         raise NotImplementedError
 
-    def load(self, model_path, batch_size=0):
+    def load(self, model_path):
         """
         Load an openVINO model.
 
         :param model_path: String. The file path to the OpenVINO IR xml file.
-        :param batch_size: Int. Set batch Size, default is 0 (use default batch size).
         :return:
         """
         self.node_num, self.core_num = get_node_and_core_number()
-        self.path = model_path
-        if batch_size != 0:
-            self.batch_size = batch_size
-        else:
-            import xml.etree.ElementTree as ET
-            tree = ET.parse(model_path)
-            root = tree.getroot()
-            shape_item = root.find('./layers/layer/output/port/dim[1]')
-            if shape_item is None:
-                raise ValueError("Invalid openVINO IR xml file, please check your model_path")
-            self.batch_size = int(shape_item.text)
-        self.model = InferenceModel(supported_concurrent_num=self.core_num)
-        self.model.load_openvino(model_path=model_path,
-                                 weight_path=model_path[:model_path.rindex(".")] + ".bin",
-                                 batch_size=batch_size)
+        assert isinstance(model_path, str), "The model_path should be string."
+        assert os.path.exists(model_path), "The model_path should be exist."
+        with open(model_path, 'rb') as file:
+            self.model_bytes = file.read()
+
+        with open(model_path[:model_path.rindex(".")] + ".bin", 'rb') as file:
+            self.weight_bytes = file.read()
 
     def set_tensorboard(self, log_dir, app_name):
         """
