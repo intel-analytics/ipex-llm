85567fca0f7be223670f9f46a3ac8e9b79dbde99
qiuxin2012 <qiuxin2012cs@gmail.com>
2021-10-21 17:04:53 +0800

    some update

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -10,7 +10,7 @@ echo "#3 start test for dllib textclassifier"
 start=$(date "+%s")
 #run the example
 rm -rf /tmp/news20
-${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/text_data /tmp/news20
+${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/news20 /tmp/news20
 ls /tmp/news20
 python ${BIGDL_ROOT}/python/dllib/src/bigdl/dllib/models/textclassifier/textclassifier.py --on-yarn --max_epoch 3 --model cnn
 exit_status=$?
