af8a64134ca3bf772cdde05522001f60d3e45d2e
cyita <yitastudy@gmail.com>
2021-10-22 11:14:18 +0800

    update

--- docs/readthedocs/source/conf.py
+++ docs/readthedocs/source/conf.py
@@ -20,7 +20,7 @@ import urllib
 
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, '.')
-sys.path.insert(0, os.path.abspath('.'))sys.path.insert(0, os.path.abspath("../../../python/chronos/src/"))
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath("../../../python/friesian/src/"))
 sys.path.insert(0, os.path.abspath("../../../python/orca/src/"))
 
