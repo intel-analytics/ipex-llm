2a7782dee02eb6f8b679d7e454cec6ef09e7d815
leonardozcm <597714572@qq.com>
2021-10-22 09:28:48 +0800

    update libtf-zoo

--- python/tflibs/dev/release.sh
+++ python/tflibs/dev/release.sh
@@ -49,12 +49,13 @@ if [ "$platform" ==  "mac" ]; then
     # Todo: append dylibs
 elif [ "$platform" == "linux" ]; then
     verbose_pname="manylinux2010_x86_64"
-    FRAMEWORK_SO="$(readlink -f linux-x86_64/libtensorflow_framework.so)"
-    rm linux-x86_64/libtensorflow_framework.so
-    rm linux-x86_64/libtensorflow_framework.so.1
-    mv "${FRAMEWORK_SO}" "linux-x86_64/libtensorflow_framework-zoo.so"
-    sudo patchelf --set-soname libtensorflow_framework-zoo.so linux-x86_64/libtensorflow_framework-zoo.so
-    sudo patchelf --replace-needed libtensorflow_framework.so.1 libtensorflow_framework-zoo.so linux-x86_64/libtensorflow_jni.so
+    SO_PREFIX=${BIGDL_PYTHON_DIR}/bigdl/share/tflibs/linux-x86_64
+    FRAMEWORK_SO="$(readlink -f ${SO_PREFIX}/libtensorflow_framework.so)"
+    rm ${SO_PREFIX}/libtensorflow_framework.so
+    rm ${SO_PREFIX}/libtensorflow_framework.so.1
+    mv "${FRAMEWORK_SO}" "${SO_PREFIX}/libtensorflow_framework-zoo.so"
+    patchelf --set-soname libtensorflow_framework-zoo.so ${SO_PREFIX}/libtensorflow_framework-zoo.so
+    patchelf --replace-needed libtensorflow_framework.so.1 libtensorflow_framework-zoo.so ${SO_PREFIX}/libtensorflow_jni.so
 else
     echo "Unsupported platform"
 fi
