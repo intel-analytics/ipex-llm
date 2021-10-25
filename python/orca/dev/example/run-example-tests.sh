2c728a944eba2fdec2f1ccdaf6d9e7930dc71659
sgwhat <ge.song@intel.com>
2021-10-25 11:34:09 +0800

    new update

--- python/orca/dev/example/run-example-tests.sh
+++ python/orca/dev/example/run-example-tests.sh
@@ -2,122 +2,122 @@
 
 set -e
 
-# echo "#1 start example test for openvino"
-# #timer
-# start=$(date "+%s")
-# if [ -f models/faster_rcnn_resnet101_coco.xml ]; then
-#   echo "models/faster_rcnn_resnet101_coco already exists."
-# else
-#   wget -nv $FTP_URI/analytics-zoo-models/openvino/2018_R5/faster_rcnn_resnet101_coco.xml \
-#     -P models
-#   wget -nv $FTP_URI/analytics-zoo-models/openvino/2018_R5/faster_rcnn_resnet101_coco.bin \
-#     -P models
-# fi
-# if [ -d tmp/data/object-detection-coco ]; then
-#   echo "tmp/data/object-detection-coco already exists"
-# else
-#   wget -nv $FTP_URI/analytics-zoo-data/data/object-detection-coco.zip -P tmp/data
-#   unzip -q tmp/data/object-detection-coco.zip -d tmp/data
-# fi
-# python ${BIGDL_ROOT}/python/orca/example/openvino/predict.py \
-#   --image tmp/data/object-detection-coco \
-#   --model models/faster_rcnn_resnet101_coco.xml
-# now=$(date "+%s")
-# time1=$((now - start))
+echo "#1 start example test for openvino"
+#timer
+start=$(date "+%s")
+if [ -f models/faster_rcnn_resnet101_coco.xml ]; then
+  echo "models/faster_rcnn_resnet101_coco already exists."
+else
+  wget -nv $FTP_URI/analytics-zoo-models/openvino/2018_R5/faster_rcnn_resnet101_coco.xml \
+    -P models
+  wget -nv $FTP_URI/analytics-zoo-models/openvino/2018_R5/faster_rcnn_resnet101_coco.bin \
+    -P models
+fi
+if [ -d tmp/data/object-detection-coco ]; then
+  echo "tmp/data/object-detection-coco already exists"
+else
+  wget -nv $FTP_URI/analytics-zoo-data/data/object-detection-coco.zip -P tmp/data
+  unzip -q tmp/data/object-detection-coco.zip -d tmp/data
+fi
+python ${BIGDL_ROOT}/python/orca/example/openvino/predict.py \
+  --image tmp/data/object-detection-coco \
+  --model models/faster_rcnn_resnet101_coco.xml
+now=$(date "+%s")
+time1=$((now - start))
 
-# echo "#2 start example for vnni/openvino"
-# #timer
-# start=$(date "+%s")
-# if [ -d models/vnni ]; then
-#   echo "models/resnet_v1_50.xml already exists."
-# else
-#   wget -nv $FTP_URI/analytics-zoo-models/openvino/vnni/resnet_v1_50.zip \
-#     -P models
-#   unzip -q models/resnet_v1_50.zip -d models/vnni
-# fi
-# if [ -d tmp/data/object-detection-coco ]; then
-#   echo "tmp/data/object-detection-coco already exists"
-# else
-#   wget -nv $FTP_URI/analytics-zoo-data/data/object-detection-coco.zip -P tmp/data
-#   unzip -q tmp/data/object-detection-coco.zip -d tmp/data
-# fi
-# python ${BIGDL_ROOT}/python/orca/example/vnni/openvino/predict.py \
-#   --model models/vnni/resnet_v1_50.xml \
-#   --image tmp/data/object-detection-coco
-# now=$(date "+%s")
-# time2=$((now - start))
+echo "#2 start example for vnni/openvino"
+#timer
+start=$(date "+%s")
+if [ -d models/vnni ]; then
+  echo "models/resnet_v1_50.xml already exists."
+else
+  wget -nv $FTP_URI/analytics-zoo-models/openvino/vnni/resnet_v1_50.zip \
+    -P models
+  unzip -q models/resnet_v1_50.zip -d models/vnni
+fi
+if [ -d tmp/data/object-detection-coco ]; then
+  echo "tmp/data/object-detection-coco already exists"
+else
+  wget -nv $FTP_URI/analytics-zoo-data/data/object-detection-coco.zip -P tmp/data
+  unzip -q tmp/data/object-detection-coco.zip -d tmp/data
+fi
+python ${BIGDL_ROOT}/python/orca/example/vnni/openvino/predict.py \
+  --model models/vnni/resnet_v1_50.xml \
+  --image tmp/data/object-detection-coco
+now=$(date "+%s")
+time2=$((now - start))
 
-# echo "#3 start example test for tensorflow"
-# #timer
-# start=$(date "+%s")
-# if [ -f models/resnet_50_saved_model.zip ]; then
-#   echo "-models/resnet_50_saved_model.zip already exists."
-# else
-#   wget -nv $FTP_URI/analytics-zoo-models/tensorflow/resnet_50_saved_model.zip \
-#     -P models
-#   unzip models/resnet_50_saved_model.zip -d models/resnet_50_saved_model
-# fi
+echo "#3 start example test for tensorflow"
+#timer
+start=$(date "+%s")
+if [ -f models/resnet_50_saved_model.zip ]; then
+  echo "-models/resnet_50_saved_model.zip already exists."
+else
+  wget -nv $FTP_URI/analytics-zoo-models/tensorflow/resnet_50_saved_model.zip \
+    -P models
+  unzip models/resnet_50_saved_model.zip -d models/resnet_50_saved_model
+fi
 
-# echo "start example test for TFPark freeze saved model 9"
-# python ${BIGDL_ROOT}/python/orca/example/freeze_saved_model/freeze.py \
-#   --saved_model_path models/resnet_50_saved_model \
-#   --output_path models/resnet_50_tfnet
+echo "start example test for TFPark freeze saved model 9"
+python ${BIGDL_ROOT}/python/orca/example/freeze_saved_model/freeze.py \
+  --saved_model_path models/resnet_50_saved_model \
+  --output_path models/resnet_50_tfnet
 
-# now=$(date "+%s")
-# time3=$((now - start))
+now=$(date "+%s")
+time3=$((now - start))
 
-# echo "#4 start example test for orca data"
-# if [ -f tmp/data/NAB/nyc_taxi/nyc_taxi.csv ]; then
-#   echo "tmp/data/NAB/nyc_taxi/nyc_taxi.csv already exists"
-# else
-#   wget -nv $FTP_URI/analytics-zoo-data/data/NAB/nyc_taxi/nyc_taxi.csv \
-#     -P tmp/data/NAB/nyc_taxi/
-# fi
-# #timer
-# start=$(date "+%s")
-# python ${BIGDL_ROOT}/python/orca/example/data/spark_pandas.py \
-#   -f tmp/data/NAB/nyc_taxi/nyc_taxi.csv
+echo "#4 start example test for orca data"
+if [ -f tmp/data/NAB/nyc_taxi/nyc_taxi.csv ]; then
+  echo "tmp/data/NAB/nyc_taxi/nyc_taxi.csv already exists"
+else
+  wget -nv $FTP_URI/analytics-zoo-data/data/NAB/nyc_taxi/nyc_taxi.csv \
+    -P tmp/data/NAB/nyc_taxi/
+fi
+#timer
+start=$(date "+%s")
+python ${BIGDL_ROOT}/python/orca/example/data/spark_pandas.py \
+  -f tmp/data/NAB/nyc_taxi/nyc_taxi.csv
 
-# now=$(date "+%s")
-# time4=$((now - start))
+now=$(date "+%s")
+time4=$((now - start))
 
-# echo "#5 start test for orca tf imagesegmentation"
-# #timer
-# start=$(date "+%s")
-# # prepare data
-# if [ -f tmp/data/carvana ]; then
-#   echo "tmp/data/carvana already exists"
-# else
-#   wget $FTP_URI/analytics-zoo-data/data/carvana/train.zip \
-#     -P tmp/data/carvana/
-#   wget $FTP_URI/analytics-zoo-data/data/carvana/train_masks.zip \
-#     -P tmp/data/carvana/
-#   wget $FTP_URI/analytics-zoo-data/data/carvana/train_masks.csv.zip \
-#     -P tmp/data/carvana/
-# fi
+echo "#5 start test for orca tf imagesegmentation"
+#timer
+start=$(date "+%s")
+# prepare data
+if [ -f tmp/data/carvana ]; then
+  echo "tmp/data/carvana already exists"
+else
+  wget $FTP_URI/analytics-zoo-data/data/carvana/train.zip \
+    -P tmp/data/carvana/
+  wget $FTP_URI/analytics-zoo-data/data/carvana/train_masks.zip \
+    -P tmp/data/carvana/
+  wget $FTP_URI/analytics-zoo-data/data/carvana/train_masks.csv.zip \
+    -P tmp/data/carvana/
+fi
 
-# # Run the example
-# python ${BIGDL_ROOT}/python/orca/example/learn/tf/image_segmentation/image_segmentation.py \
-#   --file_path tmp/data/carvana --epochs 1 --non_interactive
-# exit_status=$?
-# if [ $exit_status -ne 0 ]; then
-#   echo "orca tf imagesegmentation failed"
-#   exit $exit_status
-# fi
-# now=$(date "+%s")
-# time5=$((now - start))
+# Run the example
+python ${BIGDL_ROOT}/python/orca/example/learn/tf/image_segmentation/image_segmentation.py \
+  --file_path tmp/data/carvana --epochs 1 --non_interactive
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  echo "orca tf imagesegmentation failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time5=$((now - start))
 
-# echo "#6 start test for orca tf transfer_learning"
-# #timer
-# start=$(date "+%s")
-# python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py
-# exit_status=$?
-# if [ $exit_status -ne 0 ]; then
-#   echo "orca tf transfer_learning failed"
-#   exit $exit_status
-# fi
-# now=$(date "+%s")
-# time6=$((now - start))
+echo "#6 start test for orca tf transfer_learning"
+#timer
+start=$(date "+%s")
+python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  echo "orca tf transfer_learning failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time6=$((now - start))
 
 echo "#7 start test for orca tf basic_text_classification"
 #timer
