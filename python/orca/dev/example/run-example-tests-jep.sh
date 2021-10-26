3e04220061622622f264c112c4e528c5dc0b4395
sgwhat <ge.song@intel.com>
2021-10-26 20:33:34 +0800

    new test

--- python/orca/dev/example/run-example-tests-jep.sh
+++ python/orca/dev/example/run-example-tests-jep.sh
@@ -2,77 +2,77 @@
 
 set -e
 
-echo "#1 start example for MNIST"
-#timer
-start=$(date "+%s")
-if [ -f tmp/data/MNIST ]; then
-  echo "MNIST already exists"
-else
-  wget -nv $FTP_URI/analytics-zoo-data/mnist/train-labels-idx1-ubyte.gz -P tmp/data/MNIST/raw
-  wget -nv $FTP_URI/analytics-zoo-data/mnist/train-images-idx3-ubyte.gz -P tmp/data/MNIST/raw
-  wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-labels-idx1-ubyte.gz -P tmp/data/MNIST/raw
-  wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-images-idx3-ubyte.gz -P tmp/data/MNIST/raw
-fi
+# echo "#1 start example for MNIST"
+# #timer
+# start=$(date "+%s")
+# if [ -f tmp/data/MNIST ]; then
+#   echo "MNIST already exists"
+# else
+#   wget -nv $FTP_URI/analytics-zoo-data/mnist/train-labels-idx1-ubyte.gz -P tmp/data/MNIST/raw
+#   wget -nv $FTP_URI/analytics-zoo-data/mnist/train-images-idx3-ubyte.gz -P tmp/data/MNIST/raw
+#   wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-labels-idx1-ubyte.gz -P tmp/data/MNIST/raw
+#   wget -nv $FTP_URI/analytics-zoo-data/mnist/t10k-images-idx3-ubyte.gz -P tmp/data/MNIST/raw
+# fi
 
-python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/mnist/main.py --dir tmp/data
+# python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/mnist/main.py --dir tmp/data
 
-now=$(date "+%s")
-time1=$((now - start))
+# now=$(date "+%s")
+# time1=$((now - start))
 
-echo "#2 start example for orca Cifar10"
-#timer
-start=$(date "+%s")
-if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/data ]; then
-  echo "Cifar10 already exists"
-else
-  wget -nv $FTP_URI/analytics-zoo-data/cifar10.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10
-  unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.zip
-fi
+# echo "#2 start example for orca Cifar10"
+# #timer
+# start=$(date "+%s")
+# if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/data ]; then
+#   echo "Cifar10 already exists"
+# else
+#   wget -nv $FTP_URI/analytics-zoo-data/cifar10.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10
+#   unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.zip
+# fi
 
-sed "s/epochs=2/epochs=1/g;s/batch_size=4/batch_size=256/g" \
-  ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.py \
-  >${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10_tmp.py
+# sed "s/epochs=2/epochs=1/g;s/batch_size=4/batch_size=256/g" \
+#   ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.py \
+#   >${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10_tmp.py
 
-python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10_tmp.py
+# python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10_tmp.py
 
-now=$(date "+%s")
-time2=$((now - start))
+# now=$(date "+%s")
+# time2=$((now - start))
 
-echo "#3 start example for orca Fashion-MNIST"
-#timer
-start=$(date "+%s")
-if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/data ]
-then
-    echo "fashion-mnist dataset already exists"
-else
-    wget -nv $FTP_URI/analytics-zoo-data/data/fashion-mnist.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/
-    unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion-mnist.zip
-fi
+# echo "#3 start example for orca Fashion-MNIST"
+# #timer
+# start=$(date "+%s")
+# if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/data ]
+# then
+#     echo "fashion-mnist dataset already exists"
+# else
+#     wget -nv $FTP_URI/analytics-zoo-data/data/fashion-mnist.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/
+#     unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion-mnist.zip
+# fi
 
-sed "s/epochs=5/epochs=1/g;s/batch_size=4/batch_size=256/g" \
-    ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist.py \
-    > ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py
+# sed "s/epochs=5/epochs=1/g;s/batch_size=4/batch_size=256/g" \
+#     ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist.py \
+#     > ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py
 
-python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py
+# python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py
 
-now=$(date "+%s")
-time3=$((now - start))
+# now=$(date "+%s")
+# time3=$((now - start))
 
-echo "#4 start example for orca Super Resolution"
-#timer
-start=$(date "+%s")
-if [ ! -f BSDS300-images.tgz ]; then
-  wget -nv $FTP_URI/analytics-zoo-data/BSDS300-images.tgz
-fi
-if [ ! -d dataset/BSDS300/images ]; then
-  mkdir dataset
-  tar -xzf BSDS300-images.tgz -C dataset
-fi
+# echo "#4 start example for orca Super Resolution"
+# #timer
+# start=$(date "+%s")
+# if [ ! -f BSDS300-images.tgz ]; then
+#   wget -nv $FTP_URI/analytics-zoo-data/BSDS300-images.tgz
+# fi
+# if [ ! -d dataset/BSDS300/images ]; then
+#   mkdir dataset
+#   tar -xzf BSDS300-images.tgz -C dataset
+# fi
 
-python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py
+# python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py
 
-now=$(date "+%s")
-time4=$((now - start))
+# now=$(date "+%s")
+# time4=$((now - start))
 
 echo "#5 start test for orca bigdl resnet-finetune"
 #timer
@@ -93,7 +93,7 @@ else
   wget $FTP_URI/analytics-zoo-models/pytorch/resnet18-5c106cde.pth -P ${HOME}/.cache/torch/hub/checkpoints
 fi
 #run the example
-python ${ANALYTICS_ZOO_ROOT}/python/orca/example/torchmodel/resnet_finetune/resnet_finetune.py --imagePath tmp/data/cats_and_dogs_filtered/samples
+python ${ANALYTICS_ZOO_ROOT}/python/orca/example/torchmodel/train/resnet_finetune/resnet_finetune.py --imagePath tmp/data/cats_and_dogs_filtered/samples
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
   clear_up
