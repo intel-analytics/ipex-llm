d6378280bb45f7e2fbab4e58dd5d892a954839a9
leonardozcm <597714572@qq.com>
2021-11-02 01:18:14 +0800

    update type check

--- python/nano/test/run-nano-type-test.sh
+++ python/nano/test/run-nano-type-test.sh
@@ -4,4 +4,4 @@ export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
 export NANO_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/test
 
-mypy --config-file ${NANO_TEST_DIR}/mypy.ini ${ANALYTICS_ZOO_ROOT}/python/nano
+mypy --config-file ${NANO_TEST_DIR}/mypy.ini ${ANALYTICS_ZOO_ROOT}/python/nano/src
