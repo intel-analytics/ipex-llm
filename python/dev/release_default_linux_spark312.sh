080ac4e8df3f0751822f1540978cd3c35d0ff68f
Le-Zheng <alettazheng@hotmail.com>
2021-11-03 16:24:55 +0800

    add mvn parma for spark3 release script

--- python/dev/release_default_linux_spark312.sh
+++ python/dev/release_default_linux_spark312.sh
@@ -29,7 +29,7 @@ if (( $# < 3)); then
   echo "Usage: release_default_linux_spark312.sh version quick_build upload mvn_parameters"
   echo "Usage example: bash release_default_linux_spark312.sh default false true"
   echo "Usage example: bash release_default_linux_spark312.sh 0.14.0.dev1 false false"
-  echo "Usage example: bash release_default_linux_spark246.sh 0.14.0.dev1 false false -Ddata-store-url=.."
+  echo "Usage example: bash release_default_linux_spark312.sh 0.14.0.dev1 false false -Ddata-store-url=.."
   exit -1
 fi
 
