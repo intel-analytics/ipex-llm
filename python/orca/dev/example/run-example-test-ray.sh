e167606b616c38261e6056b93b61d84fa5174793
sgwhat <ge.song@intel.com>
2021-11-09 17:31:32 +0800

    update for after part test

--- python/orca/dev/example/run-example-test-ray.sh
+++ python/orca/dev/example/run-example-test-ray.sh
@@ -68,57 +68,57 @@ execute_ray_test(){
 # now=$(date "+%s")
 # time5=$((now-start))
 
-echo "#6 Start fashion_mnist example with Tensorboard visualization"
-start=$(date "+%s")
+# echo "#6 Start fashion_mnist example with Tensorboard visualization"
+# start=$(date "+%s")
 
-if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/data ]
-then
-    echo "fashion-mnist already exists"
-else
-    wget -nv $FTP_URI/analytics-zoo-data/data/fashion-mnist.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/
-    unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion-mnist.zip
-fi
+# if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/data ]
+# then
+#     echo "fashion-mnist already exists"
+# else
+#     wget -nv $FTP_URI/analytics-zoo-data/data/fashion-mnist.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/
+#     unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion-mnist.zip
+# fi
 
-sed "s/epochs=5/epochs=1/g;s/batch_size=4/batch_size=256/g" \
-    ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist.py \
-    > ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py
+# sed "s/epochs=5/epochs=1/g;s/batch_size=4/batch_size=256/g" \
+#     ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist.py \
+#     > ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py
 
-python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py --backend torch_distributed
-now=$(date "+%s")
-time6=$((now-start))
-
-
-echo "#7 start example for orca super-resolution"
-start=$(date "+%s")
-
-if [ ! -f BSDS300-images.tgz ]; then
-  wget -nv $FTP_URI/analytics-zoo-data/BSDS300-images.tgz
-fi
-if [ ! -d dataset/BSDS300/images ]; then
-  mkdir dataset
-  tar -xzf BSDS300-images.tgz -C dataset
-fi
-
-python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py --backend torch_distributed
-
-now=$(date "+%s")
-time7=$((now-start))
+# python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py --backend torch_distributed
+# now=$(date "+%s")
+# time6=$((now-start))
 
 
-echo "#8 start example for orca cifar10"
-start=$(date "+%s")
+# echo "#7 start example for orca super-resolution"
+# start=$(date "+%s")
 
-if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/data ]; then
-  echo "Cifar10 already exists"
-else
-  wget -nv $FTP_URI/analytics-zoo-data/cifar10.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10
-  unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.zip
-fi
+# if [ ! -f BSDS300-images.tgz ]; then
+#   wget -nv $FTP_URI/analytics-zoo-data/BSDS300-images.tgz
+# fi
+# if [ ! -d dataset/BSDS300/images ]; then
+#   mkdir dataset
+#   tar -xzf BSDS300-images.tgz -C dataset
+# fi
 
-python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.py --backend torch_distributed
+# python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py --backend torch_distributed
 
-now=$(date "+%s")
-time8=$((now-start))
+# now=$(date "+%s")
+# time7=$((now-start))
+
+
+# echo "#8 start example for orca cifar10"
+# start=$(date "+%s")
+
+# if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/data ]; then
+#   echo "Cifar10 already exists"
+# else
+#   wget -nv $FTP_URI/analytics-zoo-data/cifar10.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10
+#   unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.zip
+# fi
+
+# python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.py --backend torch_distributed
+
+# now=$(date "+%s")
+# time8=$((now-start))
 
 execute_ray_test auto-estimator-pytorch "${BIGDL_ROOT}/python/orca/example/automl/autoestimator/autoestimator_pytorch.py --trials 5 --epochs 2"
 time9=$?
