e5adbffb53d7698a27225ac50657bc5153f700e0
sgwhat <ge.song@intel.com>
2021-11-09 19:42:00 +0800

    update

--- python/orca/dev/example/run-example-test-ray.sh
+++ python/orca/dev/example/run-example-test-ray.sh
@@ -4,6 +4,51 @@ set -e
 
 ray stop -f
 
+clear_up () {
+    echo "Clearing up environment. Uninstalling BigDL"
+    pip uninstall -y bigdl-orca
+    pip uninstall -y bigdl-dllib
+    pip uninstall -y pyspark
+}
+
+execute_ray_test(){
+    echo "start example $1"
+    start=$(date "+%s")
+    python $2
+    exit_status=$?
+    if [ $exit_status -ne 0 ];
+    then
+        clear_up
+        echo "$1 failed"
+        exit $exit_status
+    fi
+    now=$(date "+%s")
+    return $((now-start))
+}
+
+execute_ray_test auto-estimator-pytorch "${BIGDL_ROOT}/python/orca/example/automl/autoestimator/autoestimator_pytorch.py --trials 5 --epochs 2"
+time1=$?
+
+if [ -f ${BIGDL_ROOT}/data/airline_14col.data ]
+then
+    echo "airline_14col.data already exists"
+else
+    wget -nv $FTP_URI/analytics-zoo-data/airline_14col.data -P ${BIGDL_ROOT}/data/
+fi
+
+execute_ray_test auto-xgboost-classifier "${BIGDL_ROOT}/python/orca/example/automl/autoxgboost/AutoXGBoostClassifier.py -p ${BIGDL_ROOT}/data/airline_14col.data"
+time2=$?
+
+if [ -f ${BIGDL_ROOT}/data/incd.csv ]
+then
+    echo "incd.csv already exists"
+else
+    wget -nv $FTP_URI/analytics-zoo-data/incd.csv -P ${BIGDL_ROOT}/data/
+fi
+
+execute_ray_test auto-xgboost-regressor "${BIGDL_ROOT}/python/orca/example/automl/autoxgboost/AutoXGBoostRegressor.py -p ${BIGDL_ROOT}/data/incd.csv"
+time3=$?
+
 echo "#start orca ray example tests"
 echo "#1 Start rl_pong example"
 start=$(date "+%s")
@@ -97,51 +142,6 @@ python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.py --back
 
 now=$(date "+%s")
 time8=$((now-start))
-
-clear_up () {
-    echo "Clearing up environment. Uninstalling BigDL"
-    pip uninstall -y bigdl-orca
-    pip uninstall -y bigdl-dllib
-    pip uninstall -y pyspark
-}
-
-execute_ray_test(){
-    echo "start example $1"
-    start=$(date "+%s")
-    python $2
-    exit_status=$?
-    if [ $exit_status -ne 0 ];
-    then
-        clear_up
-        echo "$1 failed"
-        exit $exit_status
-    fi
-    now=$(date "+%s")
-    return $((now-start))
-}
-
-execute_ray_test auto-estimator-pytorch "${BIGDL_ROOT}/python/orca/example/automl/autoestimator/autoestimator_pytorch.py --trials 5 --epochs 2"
-time1=$?
-
-if [ -f ${BIGDL_ROOT}/data/airline_14col.data ]
-then
-    echo "airline_14col.data already exists"
-else
-    wget -nv $FTP_URI/analytics-zoo-data/airline_14col.data -P ${BIGDL_ROOT}/data/
-fi
-
-execute_ray_test auto-xgboost-classifier "${BIGDL_ROOT}/python/orca/example/automl/autoxgboost/AutoXGBoostClassifier.py -p ${BIGDL_ROOT}/data/airline_14col.data"
-time2=$?
-
-if [ -f ${BIGDL_ROOT}/data/incd.csv ]
-then
-    echo "incd.csv already exists"
-else
-    wget -nv $FTP_URI/analytics-zoo-data/incd.csv -P ${BIGDL_ROOT}/data/
-fi
-
-execute_ray_test auto-xgboost-regressor "${BIGDL_ROOT}/python/orca/example/automl/autoxgboost/AutoXGBoostRegressor.py -p ${BIGDL_ROOT}/data/incd.csv"
-time3=$?
 
 
 echo "Ray example tests finished"
