c0c351c5358673869ed34ff5e258533759dba414
Zhentao Chen <90437536+zhentaocc@users.noreply.github.com>
2021-11-11 20:48:41 +0800

    Add pytorch-lightning decorator to nano (#3181)
    * added decorator to create a pytorch lightning model from torch
    
    * added unit test for pytorch lightning decorator
    
    * refactoring - renaming, adding hints and docstring
    
    * moved lightning extension to nano/pytorch
    
    * remove loss, optim creator and directly pass loss and optimizer to initiate
    
    * added another implementation for pytorch to lightning
    
    * use LightningModuleFromTorch to create lightning module from pytorch
    
    * remove temporary change
    
    * remove redundant part
    
    * added trainer.compile to convert pytorch to pytorch-lightning
    
    * added unit test for trainer.compile
    
    * fixed return when input is pl model
    
    * added type hint for LightningModuleFromTorch.copy
    
    * Renamed copy as _copy
    
    * Modified comment of compile
    
    * added input checking
    
    * refactored docstring
    
    * Reformat docstring
    
    * Tiny changes
    
    * reformat
    
    * correct the import
    
    * type check and
    
    * assign model as a member variable
    
    * override load_state_dict
    
    * fix test_trainer_compile
    
    * fix test_lightning
    
    * try lightning module and then self.model
    
    * rename _forward as forward
    
    * type check
    
    * optimize imports
    
    Co-authored-by: Yang Wang <yang3.wang@intel.com>

---
+++ python/nano/src/bigdl/nano/pytorch/lightning.py
@@ -0,0 +1,69 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from collections import OrderedDict
+
+from pytorch_lightning import LightningModule
+from torch import nn, Tensor
+from torch.nn.modules.loss import _Loss
+from torch.optim import Optimizer
+
+
+class LightningModuleFromTorch(LightningModule):
+    def __init__(self, model: nn.Module, loss: _Loss, optimizer: Optimizer):
+        """
+        Integrate pytorch modules, loss, optimizer to pytorch-lightning model.
+
+        :param model:       Pytorch model to be converted.
+        :param loss:        A torch loss function.
+        :param optimizer:   A torch optimizer.
+        """
+        super().__init__()
+        self.model = model
+        self.loss = loss
+        self.optimizer = optimizer
+
+    def forward(self, batch):
+        # Handle different numbers of input for various models
+        nargs = self.model.forward.__code__.co_argcount
+        return self.model(*(batch[:nargs - 1]))
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(batch)
+        loss = self.loss(y_hat, y)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(batch)
+        loss = self.loss(y_hat, y)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(batch)
+        loss = self.loss(y_hat, y)
+        return loss
+
+    def configure_optimizers(self):
+        return self.optimizer
+
+    def load_state_dict(self, state_dict: 'OrderedDict[str, Tensor]',
+                        strict: bool = True):
+        try:
+            super().load_state_dict(state_dict)
+        except RuntimeError:
+            self.model.load_state_dict(state_dict)
