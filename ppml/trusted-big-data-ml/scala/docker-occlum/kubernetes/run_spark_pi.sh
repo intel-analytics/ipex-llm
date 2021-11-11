400abf6056d3ea90ee3c07a14c15cfcd5dc1e300
Qiyuan Gong <qiyuan.gong@intel.com>
2021-11-11 15:11:55 +0800

    [PPML] Refine Scripts in Occlum Spark (#3454)
    * Upgrade to Occlum 0.25.0
    * Add SGX_MEM_SIZE to script and env
    * Add /etc/nsswitch.conf

--- ppml/trusted-big-data-ml/scala/docker-occlum/kubernetes/run_spark_pi.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/kubernetes/run_spark_pi.sh
@@ -12,4 +12,4 @@ ${SPARK_HOME}/bin/spark-submit \
     --conf spark.kubernetes.executor.deleteOnTermination=false \
     --conf spark.kubernetes.driver.podTemplateFile=./executor.yaml \
     --conf spark.kubernetes.executor.podTemplateFile=./executor.yaml \
-    local:/bin/examples/jars/spark-examples_2.12-3.1.2.jar
+    local:/opt/spark/examples/jars/spark-examples_2.12-3.1.2.jar
