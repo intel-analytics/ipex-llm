a92ff3e28d4704349d13c7c6f2704f12049db698
Qiyuan Gong <qiyuan.gong@intel.com>
2021-11-12 20:18:04 +0800

    [PPML] Occlum Spasrk fix hadoop so (#3464)
    * Fix libhadoop path
    * Copy /opt/data into image/opt/data

--- ppml/trusted-big-data-ml/scala/docker-occlum/run_spark_on_occlum_glibc.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/run_spark_on_occlum_glibc.sh
@@ -8,7 +8,8 @@ occlum_glibc=/opt/occlum/glibc/lib
 init_instance() {
     # Init Occlum instance
     cd /opt
-    rm -rf occlum_spark && mkdir occlum_spark
+    # check if occlum_spark exists
+    [[ -d occlum_spark ]] || mkdir occlum_spark
     cd occlum_spark
     occlum init
     new_json="$(jq '.resource_limits.user_space_size = "SGX_MEM_SIZE" |
@@ -37,6 +38,8 @@ build_spark() {
     cp $occlum_glibc/libdl.so.2 image/$occlum_glibc
     cp $occlum_glibc/librt.so.1 image/$occlum_glibc
     cp $occlum_glibc/libm.so.6 image/$occlum_glibc
+    # Copy libhadoop
+    cp /opt/libhadoop.so image/$occlum_glibc
     # Prepare Spark
     mkdir -p image/opt/spark
     cp -rf $SPARK_HOME/* image/opt/spark/
@@ -53,7 +56,7 @@ build_spark() {
     mkdir -p image/bin/jars
     cp -f $BIGDL_HOME/jars/* image/bin/jars
     # Copy Data dir
-    cp -rf /opt/data image/bin/
+    cp -rf /opt/data image/opt/
     occlum build
 }
 
