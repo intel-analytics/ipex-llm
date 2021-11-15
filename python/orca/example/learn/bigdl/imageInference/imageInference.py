e88c59fee0eb19098008c5bde4d27f543b3c6f6e
sgwhat <ge.song@intel.com>
2021-11-15 23:29:29 +0800

    update

--- python/orca/example/learn/bigdl/imageInference/imageInference.py
+++ python/orca/example/learn/bigdl/imageInference/imageInference.py
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     cluster_mode = options.cluster_mode
     if cluster_mode == "local":
         sc = init_orca_context(memory="3g")
-    elif cluster_mode.starswith("yarn"):
+    elif cluster_mode.startswith("yarn"):
         if cluster_mode == "yarn-client":
             sc = init_orca_context(cluster_mode="yarn-client", num_nodes=2, memory="3g")
         else:
