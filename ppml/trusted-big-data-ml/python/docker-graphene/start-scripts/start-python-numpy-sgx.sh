c23e4547fc6f01c95d433494f814d5bfcd59eaca
shaojie <shaojiex.bai@intel.com>
2021-11-17 13:43:23 +0800

    refactor(start-scripts): 1. update start-python-helloworld-sgx.sh 2. update start-python-numpy-sgx.sh

--- ppml/trusted-big-data-ml/python/docker-graphene/start-scripts/start-python-numpy-sgx.sh
+++ ppml/trusted-big-data-ml/python/docker-graphene/start-scripts/start-python-numpy-sgx.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-SGX=1 ./pal_loader bash -c "python ./work/examples/test-numpy.py" | tee test-numpy-sgx.log
+cd /ppml/trusted-big-data-ml
+SGX=1 ./pal_loader bash -c "python ./work/examples/test-numpy.py" | tee test-numpy-sgx.log && \
+	cat test-numpy-sgx.log | egrep -a "dot"
