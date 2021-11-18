f965c59eed9be8fa483780b89a2e5644f4eb337d
piaolaidelangman <707056129@qq.com>
2021-11-18 11:09:54 +0800

    update example

--- python/orca/example/torchmodel/train/resnet_finetune/resnet_finetune.py
+++ python/orca/example/torchmodel/train/resnet_finetune/resnet_finetune.py
@@ -114,4 +114,4 @@ if __name__ == '__main__':
     predictionDF.sample(False, 0.1).show()
 
     # expecting: accuracy around 95%
-    print("Validation accuracy = {}, correct {},  total {}".format(accuracy, correct, overall)
+    print("Validation accuracy = {}, correct {},  total {}".format(accuracy, correct, overall))
