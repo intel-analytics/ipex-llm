dfd8caa837a9a73be3f733bd25228ac0710e50dd
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-11-18 23:38:37 -0500

    Update run-example-tests-yarn-integration.sh

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -4,7 +4,7 @@ clear_up() {
   pip uninstall -y bigdl-dllib
 }
 
-:<<!
+: '
 echo "#1 start test for dllib lenet5"
 
 #timer
@@ -137,7 +137,8 @@ fi
 now=$(date "+%s")
 time=$((now - start))
 echo "#7 Total time cost ${time} seconds"
-！
+
+'
 
 echo "#9 start test for orca learn/tf/image_segmentation/image_segmentation.py"
 #timer
