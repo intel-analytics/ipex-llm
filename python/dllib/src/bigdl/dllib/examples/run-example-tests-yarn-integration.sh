19bf471d1a0e3ee29a9b32adb7d0d65a3ba128fe
piaolaidelangman <707056129@qq.com>
2021-11-19 08:14:50 +0800

    update integratest.sh

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -199,6 +199,7 @@ echo "#14 start test for pytorch super_resolution"
 #timer
 start=$(date "+%s")
 #run the example
+ls /tmp/super_resolution_data
 rm -rf /tmp/super_resolution_data
 python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py  --cluster_mode 'yarn-client' --data_dir '/tmp/super_resolution_data'
 exit_status=$?
@@ -217,10 +218,13 @@ echo "#15 start test for torchmodel imagenet"
 start=$(date "+%s")
 #run the example
 rm -rf /tmp/imagenet
-wget $FTP_URI/imagenet   -P /tmp/imagenet
+# wget $FTP_URI/imagenet   -P /tmp/imagenet
+${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet-mini /tmp/imagenet-mini
+${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet-small /tmp/imagenet-small
 echo "##############imagenet files"
-ls /tmp/imagenet
-python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/imagenet/main.py  /tmp/imagenet  --max_epochs 1  --batch-size 256  --deploy_mode 'yarn-client'
+ls /tmp/imagenet-small
+ls /tmp/imagenet-mini
+python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/imagenet/main.py  /tmp/imagenet-small  --max_epochs 1  --batch-size 256  --deploy_mode 'yarn-client'
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
   #clear_up
@@ -254,7 +258,7 @@ echo "#17 start test for torchmodel resnet_finetune"
 start=$(date "+%s")
 #run the example
 rm -rf /tmp/dogs_cats/samples
-wget $FTP_URI/dogs_cats/samples   -P /tmp/dogs_cats/samples
+${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/dogs_cats/samples /tmp/dogs_cats/samples
 ls /tmp/dogs_cats/samples
 python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/resnet_finetune/resnet_finetune.py /tmp/dogs_cats/samples
 exit_status=$?
