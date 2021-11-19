afc8ec0105b196ffdddcb09738b003ae97e27854
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-11-19 00:33:47 -0500

    Update run-example-tests-yarn-integration.sh

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -4,7 +4,7 @@ clear_up() {
   pip uninstall -y bigdl-dllib
 }
 
-:<<!
+: '
 echo "#1 start test for dllib lenet5"
 
 #timer
@@ -137,7 +137,8 @@ fi
 now=$(date "+%s")
 time=$((now - start))
 echo "#7 Total time cost ${time} seconds"
-！
+
+'
 
 echo "#14 start test for orca yolov3"
 #timer
@@ -150,7 +151,7 @@ python ${BIGDL_ROOT}/python/orca/example/learn/tf2/yolov3/yoloV3.py  \
   --checkpoint /bigdl2.0/data/yolov3/yolov3.tf \
   --class_num 20 \
   --names /bigdl2.0/data/yolov3/voc2012.names \
-  --epochs 1 ---cluster-mode yarn-client
+  --epochs 1 ---cluster_mode yarn-client
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
   clear_up
