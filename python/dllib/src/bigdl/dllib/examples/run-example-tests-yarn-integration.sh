81bc3d61c00ece57697644d5ef1a5a70daf9a7f0
piaolaidelangman <707056129@qq.com>
2021-11-19 16:20:02 +0800

    test imagenet example

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -195,23 +195,23 @@ clear_up() {
 # echo "#13 Total time cost ${time} seconds"
 
 
-echo "#14 start test for pytorch super_resolution"
-#timer
-start=$(date "+%s")
-#run the example
-# ls /tmp/super_resolution_data
-# rm -rf /tmp/super_resolution_data
-rm -rf /home/super_resolution_data
-python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py  --cluster_mode 'yarn-client' --data_dir /home/super_resolution_data
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-  #clear_up
-  echo "####################pytorch super_resolution failed"
-  #exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#14 Total time cost ${time} seconds"
+# echo "#14 start test for pytorch super_resolution"
+# #timer
+# start=$(date "+%s")
+# #run the example
+# # ls /tmp/super_resolution_data
+# # rm -rf /tmp/super_resolution_data
+# rm -rf /home/super_resolution_data
+# python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py  --cluster_mode 'yarn-client' --data_dir /home/super_resolution_data
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+#   #clear_up
+#   echo "####################pytorch super_resolution failed"
+#   #exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#14 Total time cost ${time} seconds"
 
 
 echo "#15 start test for torchmodel imagenet"
