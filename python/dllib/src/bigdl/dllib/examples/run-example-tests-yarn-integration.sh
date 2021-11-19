ec6ff3550952e6dd7bb10df87ba974384ca2c42a
sgwhat <ge.song@intel.com>
2021-11-19 22:24:51 +0800

    update

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -213,9 +213,7 @@ echo "#9 start test for orca ros async"
 #timer
 start=$(date "+%s")
 #run the example
-python ${BIGDL_ROOT}/python/orca/example/ray_on_spark/parameter_server/async_parameter_server.py \
-  --iterations 5 \
-  --cluster_mode yarn-client
+python ${BIGDL_ROOT}/python/orca/example/ray_on_spark/parameter_server/async_parameter_server.py --iterations 20 --num_workers 2 --cluster_mode yarn
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
   clear_up
--- python/orca/example/ray_on_spark/parameter_server/async_parameter_server.py
+++ python/orca/example/ray_on_spark/parameter_server/async_parameter_server.py
@@ -97,7 +97,7 @@ def worker_task(ps, worker_index, batch_size=50):
 if __name__ == "__main__":
     args = parser.parse_args()
     cluster_mode = args.cluster_mode
-    if cluster_mode.startswith("yarn"):
+    if cluster_mode == "yarn":
         sc = init_orca_context(cluster_mode=cluster_mode,
                                cores=args.executor_cores,
                                memory=args.executor_memory,
@@ -143,4 +143,3 @@ if __name__ == "__main__":
         time.sleep(1)
     ray_ctx.stop()
     stop_orca_context()
-
