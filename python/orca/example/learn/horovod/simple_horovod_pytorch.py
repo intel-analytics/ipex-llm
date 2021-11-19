7c2f10fdab15087bf90cf3b2ef817e1a35bd6639
sgwhat <ge.song@intel.com>
2021-11-19 17:23:40 +0800

    simple_horovod.py client

--- python/orca/example/learn/horovod/simple_horovod_pytorch.py
+++ python/orca/example/learn/horovod/simple_horovod_pytorch.py
@@ -68,6 +68,12 @@ def run_horovod():
     # Horovod: limit # of CPU threads to be used per worker.
     torch.set_num_threads(4)
 
+    new_mirror = 'https://ossci-datasets.s3.amazonaws.com/mnist'
+    datasets.MNIST.resources = [
+    ('/'.join([new_mirror, url.split('/')[-1]]), md5)
+    for url, md5 in datasets.MNIST.resources
+    ]
+
     kwargs = {}
     train_dataset = \
         datasets.MNIST('data-%d' % hvd.rank(), train=True, download=True,
