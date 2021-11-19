47108e2ea50fcb136c099b0c177a9acdd4d78050
piaolaidelangman <707056129@qq.com>
2021-11-19 09:02:44 +0800

    update integratest.sh add resnet example

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -195,45 +195,46 @@ clear_up() {
 # echo "#13 Total time cost ${time} seconds"
 
 
-echo "#14 start test for pytorch super_resolution"
-#timer
-start=$(date "+%s")
-#run the example
-ls /tmp/super_resolution_data
-rm -rf /tmp/super_resolution_data
-python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py  --cluster_mode 'yarn-client' --data_dir '/tmp/super_resolution_data'
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-  #clear_up
-  echo "####################pytorch super_resolution failed"
-  #exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#14 Total time cost ${time} seconds"
+# echo "#14 start test for pytorch super_resolution"
+# #timer
+# start=$(date "+%s")
+# #run the example
+# # ls /tmp/super_resolution_data
+# # rm -rf /tmp/super_resolution_data
+# python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py  --cluster_mode 'yarn-client' 
+# #--data_dir '/tmp/super_resolution_data'
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+#   #clear_up
+#   echo "####################pytorch super_resolution failed"
+#   #exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#14 Total time cost ${time} seconds"
 
 
-echo "#15 start test for torchmodel imagenet"
-#timer
-start=$(date "+%s")
-#run the example
-rm -rf /tmp/imagenet
-# wget $FTP_URI/imagenet   -P /tmp/imagenet
-${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet-mini /tmp/imagenet-mini
-${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet-small /tmp/imagenet-small
-echo "##############imagenet files"
-ls /tmp/imagenet-small
-ls /tmp/imagenet-mini
-python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/imagenet/main.py  /tmp/imagenet-small  --max_epochs 1  --batch-size 256  --deploy_mode 'yarn-client'
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-  #clear_up
-  echo "####################torchmodel imagenet failed"
-  #exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#15 Total time cost ${time} seconds"
+# echo "#15 start test for torchmodel imagenet"
+# #timer
+# start=$(date "+%s")
+# #run the example
+# rm -rf /tmp/imagenet
+# # wget $FTP_URI/imagenet   -P /tmp/imagenet
+# ${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet-mini /tmp/imagenet-mini
+# ${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet-small /tmp/imagenet-small
+# echo "##############imagenet files"
+# ls /tmp/imagenet-small
+# ls /tmp/imagenet-mini
+# python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/imagenet/main.py  /tmp/imagenet-small  --max_epochs 1  --batch-size 256  --deploy_mode 'yarn-client'
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+#   #clear_up
+#   echo "####################torchmodel imagenet failed"
+#   #exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#15 Total time cost ${time} seconds"
 
 
 # echo "#16 start test for torchmodel mnist"##
@@ -258,9 +259,10 @@ echo "#17 start test for torchmodel resnet_finetune"
 start=$(date "+%s")
 #run the example
 rm -rf /tmp/dogs_cats/samples
-${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/dogs_cats/samples /tmp/dogs_cats/samples
+${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/dogs_cats/ /tmp/dogs_cats/
+echo "#########################samples"
 ls /tmp/dogs_cats/samples
-python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/resnet_finetune/resnet_finetune.py /tmp/dogs_cats/samples
+python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/resnet_finetune/resnet_finetune.py ${HDFS_URI}/dogs_cats/samples
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
   #clear_up
