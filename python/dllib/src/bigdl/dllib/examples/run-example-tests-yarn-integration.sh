e95d41e750885ccaaed88c51c92f090eee443c06
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-11-22 16:28:14 -0500

    update yarn cluster

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -138,8 +138,6 @@ now=$(date "+%s")
 time=$((now - start))
 echo "#7 Total time cost ${time} seconds"
 
-'
-
 echo "#11 start test for orca transfer learning"
 #timer
 start=$(date "+%s")
@@ -155,3 +153,22 @@ fi
 now=$(date "+%s")
 time=$((now - start))
 echo "#11 Total time cost ${time} seconds"
+
+'
+
+
+echo "#11-2 start test for orca transfer learning"
+#timer
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py \
+  --data_dir ${HDFS_URI}/datasets --cluster_mode yarn-cluster
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca transfer learning failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#11-2 Total time cost ${time} seconds"
