8f1954a1ed86da4633de0df2687c49adc001c19a
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-11-22 17:07:25 -0500

    Update run-example-tests-yarn-integration.sh

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -138,8 +138,6 @@ now=$(date "+%s")
 time=$((now - start))
 echo "#7 Total time cost ${time} seconds"
 
-'
-
 echo "#12 start test for orca tf2 resnet-50-imagenet"
 #timer
 start=$(date "+%s")
@@ -158,3 +156,24 @@ fi
 now=$(date "+%s")
 time=$((now - start))
 echo "#12 Total time cost ${time} seconds"
+
+'
+
+echo "#12-2 start test for orca tf2 resnet-50-imagenet"
+#timer
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/learn/tf2/resnet/resnet-50-imagenet.py  \
+  --worker_num 8 --cores 17 \
+  --data_dir /bigdl2.0/data/imagenet_to_tfrecord --use_bf16 \
+  --enable_numa_binding \
+  --cluster_mode yarn-cluster
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca tf2 resnet-50-imagenet failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#12-2 Total time cost ${time} seconds"
