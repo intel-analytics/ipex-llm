3dd3aed886ab9885818c5e0e9111015673b830d6
Uxito-Ada <414416158@qq.com>
2021-11-23 09:30:11 +0800

    Modify scripts

--- ppml/trusted-realtime-ml/scala/docker-occlum/init-cluster-serving.sh
+++ ppml/trusted-realtime-ml/scala/docker-occlum/init-cluster-serving.sh
@@ -2,12 +2,10 @@
 set -x
 
 core_num=$CORE_NUM
-secure_password=`openssl rsautl -inkey /opt/password/key.txt -decrypt </opt/password/output.bin`
 redis_host=$REDIS_HOST
 
 sed -i "/modelPath:/c \ \ modelPath: /opt/resnet50" config.yaml
 sed -i "/coreNumberPerMachine:/c \ \ coreNumberPerMachine: ${core_num}" config.yaml
-sed -i "/redisSecureEnabled:/c \ \ redisSecureEnabled: true" config.yaml
-sed -i "/redisSecureTrustStorePath:/c \ \ redisSecureTrustStorePath: /opt/redis/work/keys/keystore.jks" config.yaml
-sed -i "/redisSecureTrustStorePassword:/c \ \ redisSecureTrustStorePassword: ${secure_password}" config.yaml
+sed -i "/redisSecureEnabled:/c \ \ redisSecureEnabled: false" config.yaml
+sed -i "/recordEncrypted:/c \ \ recordEncrypted: false" config.yaml
 sed -i "/redisUrl:/c \ \ redisUrl: ${redis_host}:6379" config.yaml
