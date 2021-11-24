46f9df905256bb75a327ac3469e99c63d478faf9
sgwhat <ge.song@intel.com>
2021-11-24 18:37:36 +0800

    update

--- python/orca/example/ray_on_spark/parameter_server/async_parameter_server.py
+++ python/orca/example/ray_on_spark/parameter_server/async_parameter_server.py
@@ -110,8 +110,8 @@ if __name__ == "__main__":
                                driver_memory=args.driver_memory,
                                driver_cores=args.driver_cores,
                                extra_executor_memory_for_ray=args.extra_executor_memory_for_ray,
-                               extra_python_lib=args.extra_python_lib,
                                object_store_memory=args.object_store_memory,
+                               extra_python_lib=args.extra_python_lib,
                                additional_archive="MNIST_data.zip#MNIST_data")
         ray_ctx = OrcaContext.get_ray_context()
     elif cluster_mode == "local":
