938bb0e850cfc6ad7e0ecb89de847a106b88a2d1
piaolaidelangman <707056129@qq.com>
2021-11-25 12:05:47 +0800

    test except imagenet

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -137,22 +137,115 @@ clear_up() {
 # time=$((now - start))
 # echo "#7 Total time cost ${time} seconds"
 
-echo "#20 start test for orca torchmodel imagenet"
+# echo "#20 start test for orca torchmodel imagenet"
+# #timer
+# start=$(date "+%s")
+# #run the example
+# rm -rf /tmp/imagenet2012
+# ${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet2012 /tmp/imagenet2012
+# python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/imagenet/main.py  /tmp/imagenet2012 --batch-size 8
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+#   clear_up
+#   echo "orca torchmodel imagenet failed"
+#   exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#20 Total time cost ${time} seconds"
+
+echo "#21 start test for orca torchmodel mnist"
 #timer
 start=$(date "+%s")
 #run the example
-rm -rf /tmp/imagenet2012
-${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet2012 /tmp/imagenet2012
-python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/imagenet/main.py  /tmp/imagenet2012 --batch-size 8
+rm -rf /tmp/test_mnist
+${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/test_mnist /tmp/test_mnist
+python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/mnist/main.py  --deploy-mode 'yarn-client' --dir /tmp/test_mnist
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
   clear_up
-  echo "orca torchmodel imagenet failed"
+  echo "orca torchmodel mnist failed"
   exit $exit_status
 fi
 now=$(date "+%s")
 time=$((now - start))
-echo "#20 Total time cost ${time} seconds"
+echo "#21 Total time cost ${time} seconds"
+
+echo "#22 start test for orca torchmodel resnet_finetune"
+#timer
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/resnet_finetune/resnet_finetune.py ${HDFS_URI}/dogs_cats/samples
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca torchmodel resnet_finetune failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#22 Total time cost ${time} seconds"
+
+echo "##23 start test for orca data spark_pandas"
+#timer
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/data/spark_pandas.py --deploy-mode 'yarn-client'  -f ${HDFS_URI}/nyc_taxi.csv
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca data spark_pandas failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#23 Total time cost ${time} seconds"
+
+echo "#24 start test for pytorch cifar10"
+#timer
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.py --cluster_mode 'yarn-client' --epochs 1  --batch_size 256 --data_dir /tmp/cifar10_data
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca pytorch cifar10 failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#24 Total time cost ${time} seconds"
+
+echo "#25 start test for pytorch fashion_mnist"
+#timer
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist.py --cluster_mode 'yarn-client'   --epochs 1  --batch_size 256
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca pytorch fashion_mnist failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#25 Total time cost ${time} seconds"
+
+echo "#26 start test for pytorch super_resolution"
+#timer
+start=$(date "+%s")
+#run the example
+rm -rf /tmp/resolution_data
+python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py  --cluster_mode 'yarn-client' --data_dir /tmp/resolution_data
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca pytorch super_resolution failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#26 Total time cost ${time} seconds"
 
 # echo "##21 start test for data spark_pandas"
 # #timer
