091f85bec4557b045b5f8bdaebe8dfde35754aa4
TheaperDeng <theaperdeng@outlook.com>
2021-11-26 08:08:32 +0800

    add test script update

--- python/nano/test/run-nano-test-ipex.sh
+++ python/nano/test/run-nano-test-ipex.sh
@@ -6,7 +6,7 @@ export NANO_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/test
 
 wget -nv ${FTP_URI}/analytics-zoo-data/cifar-10-python.tar.gz -P ${NANO_TEST_DIR}/data
 
-source bigdl-nano-init
+# source bigdl-nano-init
 
 set -e
 echo "#Start bigdl-nano ipex tests"
