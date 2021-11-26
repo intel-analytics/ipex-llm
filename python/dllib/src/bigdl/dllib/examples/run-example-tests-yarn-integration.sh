bab600e859b13d48300f8feb1c4e07f983ded358
piaolaidelangman <707056129@qq.com>
2021-11-26 15:32:18 +0800

    test super data pre

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration.sh
@@ -212,46 +212,45 @@ clear_up() {
 # time=$((now - start))
 # echo "#25 Total time cost ${time} seconds"
 
-echo "#20 start test for orca torchmodel imagenet"
-#timer
-start=$(date "+%s")
-#run the example
-# rm -rf /home/imagenet2012
-# ${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet2012 /data/imagenet2012
-ls /data
-ls /data/imagenet2012
-cat /etc/hostname
-echo "###################"
-python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/imagenet/main.py  /data/imagenet2012 --batch-size 8 --max_epochs 1 --deploy_mode yarn-client
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-  clear_up
-  echo "orca torchmodel imagenet failed"
-  exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#20 Total time cost ${time} seconds"
-
-# echo "#26 start test for pytorch super_resolution"
+# echo "#20 start test for orca torchmodel imagenet"
 # #timer
 # start=$(date "+%s")
 # #run the example
-# rm -rf /home/resolution_data 
-# python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py  --cluster_mode 'yarn-client' --data_dir /home/resolution_data 
-# exit_status=$?
-# ls /home
-# echo "test resolution###############"
-# ls /home/resolution_data
+# # rm -rf /home/imagenet2012
+# # ${HADOOP_HOME}/bin/hadoop fs -get ${HDFS_URI}/imagenet2012 /data/imagenet2012
+# ls /data
+# ls /data/imagenet2012
 # cat /etc/hostname
+# echo "###################"
+# python ${BIGDL_ROOT}/python/orca/example/torchmodel/train/imagenet/main.py  /data/imagenet2012 --batch-size 8 --max_epochs 1 --deploy_mode yarn-client
+# exit_status=$?
 # if [ $exit_status -ne 0 ]; then
 #   clear_up
-#   echo "orca pytorch super_resolution failed"
+#   echo "orca torchmodel imagenet failed"
 #   exit $exit_status
 # fi
 # now=$(date "+%s")
 # time=$((now - start))
-# echo "#26 Total time cost ${time} seconds"
+# echo "#20 Total time cost ${time} seconds"
+
+echo "#26 start test for pytorch super_resolution"
+#timer
+start=$(date "+%s")
+#run the example
+ls /data
+echo "test resolution###############"
+ls /data/dataset
+cat /etc/hostname
+python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py  --cluster_mode 'yarn-client' --data_dir /data/dataset 
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca pytorch super_resolution failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#26 Total time cost ${time} seconds"
 
 # echo "##21 start test for data spark_pandas"
 # #timer
