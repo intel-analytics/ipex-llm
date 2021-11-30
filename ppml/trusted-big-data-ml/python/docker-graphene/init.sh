d63d6bbd15c26a9c04e0ba78156b30582c9e8cdd
glorysdj <glorysdj@gmail.com>
2021-11-29 18:09:02 -0800

    update init for new kernels

--- ppml/trusted-big-data-ml/python/docker-graphene/init.sh
+++ ppml/trusted-big-data-ml/python/docker-graphene/init.sh
@@ -5,4 +5,32 @@ set -x
 local_ip=$LOCAL_IP
 sgx_mem_size=$SGX_MEM_SIZE
 
+if [ -c "/dev/sgx/enclave" ]; then
+    echo "/dev/sgx/enclave is ready"
+elif [ -c "/dev/sgx_enclave" ]; then
+    echo "/dev/sgx/enclave not ready, try to link to /dev/sgx_enclave"
+    if [ -c "/dev/sgx/enclave" ]; then
+        rm /dev/sgx/enclave
+    fi
+    mkdir -p /dev/sgx
+    ln -s /dev/sgx_enclave /dev/sgx/enclave
+else
+    echo "both /dev/sgx/enclave /dev/sgx_enclave are not ready, please check the kernel and driver"
+fi
+
+if [ -c "/dev/sgx/provision" ]; then
+    echo "/dev/sgx/provision is ready"
+elif [ -c "/dev/sgx_provision" ]; then
+    echo "/dev/sgx/provision not ready, try to link to /dev/sgx_provision"
+    if [ -c "/dev/sgx/provision" ]; then
+        rm /dev/sgx/provision
+    fi
+    mkdir -p /dev/sgx
+    ln -s /dev/sgx_provision /dev/sgx/provision
+else
+    echo "both /dev/sgx/provision /dev/sgx_provision are not ready, please check the kernel and driver"
+fi
+
+ls -al /dev/sgx
+
 make SGX=1 GRAPHENEDIR=/graphene THIS_DIR=/ppml/trusted-big-data-ml  SPARK_LOCAL_IP=$local_ip SPARK_USER=root G_SGX_SIZE=$sgx_mem_size
