4e2e24596bcd0f3866355777da8166469bdba178
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2021-11-30 08:08:02 +0000

    update BigDL docker dockerfile and readme (#3607)

--- docker/bigdl/start-notebook.sh
+++ docker/bigdl/start-notebook.sh
@@ -71,7 +71,7 @@ ${SPARK_HOME}/bin/pyspark \
   --executor-memory ${RUNTIME_EXECUTOR_MEMORY} \
   --total-executor-cores ${RUNTIME_TOTAL_EXECUTOR_CORES} \
   --properties-file ${BIGDL_HOME}/conf/spark-bigdl.conf \
-  --py-files local://${BIGDL_HOME}/python/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-serving-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip \
+  --py-files local://${BIGDL_HOME}/python/bigdl-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip \
   --jars local://${BIGDL_HOME}/jars/* \
   --conf spark.driver.extraClassPath=local://${BIGDL_HOME}/jars/* \
   --conf spark.executor.extraClassPath=local://${BIGDL_HOME}/jars/* \
