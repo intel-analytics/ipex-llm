40db5892b9af4a71a73915c1e2e2ea90204fe566
CharlieJ107 <charlie_j107@outlook.com>
2021-12-01 11:19:09 +0800

    Create Callback class

---
+++ python/orca/src/bigdl/orca/learn/pytorch/callbacks.py
@@ -0,0 +1,57 @@
+class Callback(object):
+    def __init__(self):
+        pass
+
+    def on_batch_begin(self):
+        pass
+
+    def on_batch_end(self):
+        pass
+
+    def on_epoch_begin(self):
+        pass
+
+    def on_epoch_end(self):
+        pass
+
+    def on_predict_batch_begin(self):
+        pass
+
+    def on_predict_batch_end(self):
+        pass
+
+    def on_predict_begin(self):
+        pass
+
+    def on_predict_end(self):
+        pass
+
+    def on_test_batch_begin(self):
+        pass
+
+    def on_test_batch_end(self):
+        pass
+
+    def on_test_begin(self):
+        pass
+
+    def on_test_end(self):
+        pass
+
+    def on_train_batch_begin(self):
+        pass
+
+    def on_train_batch_end(self):
+        pass
+
+    def on_train_begin(self):
+        pass
+
+    def on_train_end(self):
+        pass
+
+    def set_model(self):
+        pass
+
+    def set_param(self):
+        pass
