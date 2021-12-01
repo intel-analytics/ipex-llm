56753de9a4c5d47b2cf0197ead0f69eb8c68e920
Jiaming <litchilitchy@qq.com>
2021-12-01 16:06:26 +0800

    sp

--- scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/utils/DataFrameUtils.scala
+++ scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/utils/DataFrameUtils.scala
@@ -25,7 +25,7 @@ import org.apache.spark.sql.types.FloatType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object DataFrameUtils {
-  def dataFrameToSample(df: DataFrame, batchSize: Int = 4): bigdl.DataSet[MiniBatch[Float]] = {
+  def dataFrameToSample(df: DataFrame, isTrain: Boolean = true, batchSize: Int = 4): bigdl.DataSet[MiniBatch[Float]] = {
     val spark = VflContext.getSparkSession()
     import spark.implicits._
     var fDf: DataFrame = df
@@ -34,10 +34,16 @@ object DataFrameUtils {
     })
     val samples = fDf.rdd.map(r => {
       val arr = (0 until r.size).map(i => r.getAs[Float](i)).toArray
-      val featureNum = r.size - 1
-      val features = Tensor[Float](arr.slice(0, featureNum), Array(featureNum))
-      val target = Tensor[Float](Array(arr(featureNum)), Array(1))
-      Sample(features, target)
+      if (isTrain) {
+        val featureNum = r.size - 1
+        val features = Tensor[Float](arr.slice(0, featureNum), Array(featureNum))
+        val target = Tensor[Float](Array(arr(featureNum)), Array(1))
+        Sample(features, target)
+      } else {
+        val featureNum = r.size
+        val features = Tensor[Float](arr.slice(0, featureNum), Array(featureNum))
+        Sample(features)
+      }
     })
     DataSet.array(samples.collect()) ->
       SampleToMiniBatch(batchSize, parallelizing = false)
