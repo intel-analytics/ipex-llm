62ef2eab58a373f89ea4c467081e1b6bfd2a2e11
Chen, Zhentao <zhentao.chen@intel.com>
2021-12-02 14:23:21 +0800

    code style check

--- python/nano/src/bigdl/nano/quantization/quantization_inc.py
+++ python/nano/src/bigdl/nano/quantization/quantization_inc.py
@@ -24,17 +24,20 @@ class QuantizationINC:
                             onnxrt_integer, onnxrt_qlinear or mxnet; allow new framework backend
                             extension. Default: pytorch_fx. Consistent with Intel Neural Compressor
                             Quantization.
-        :param conf:        A path to conf yaml file for quantization. Default: None, use default config.
-        :param approach:    post_training_static_quant, post_training_dynamic_quant, quant_aware_training.
-                            Default: post_training_static_quant.
+        :param conf:        A path to conf yaml file for quantization.
+                            Default: None, use default config.
+        :param approach:    post_training_static_quant, post_training_dynamic_quant,
+                            quant_aware_training. Default: post_training_static_quant.
         :param strategy:    bayesian, basic, mse, sigopt. Default: bayesian.
         :param accuracy_criterion:  Tolerable accuracy drop.
-                                    accuracy_criterion = {'relative': 0.1, higher_is_better=True} allows relative
+                                    accuracy_criterion = {'relative': 0.1, higher_is_better=True}
+                                     allows relative
                                     accuracy loss: 1%. accuracy_criterion = {'absolute': 0.99,
                                     higher_is_better=Flase} means accuracy < 0.99 must be satisfied.
         :param timeout:     Tuning timeout (seconds). Default: 0,  which means early stop.
                             combine with max_trials field to decide when to exit.
-        :param max_trials:  Max tune times. Default: 1. combine with timeout field to decide when to exit.
+        :param max_trials:  Max tune times. Default: 1.
+                            combine with timeout field to decide when to exit.
         :param inputs:      For tensorflow to specify names of inputs. e.g. inputs=['img',]
         :param outputs:     For tensorflow to specify names of outputs. e.g. outputs=['logits',]
         """
