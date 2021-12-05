49c53565250ceffa1e71177e19c5bd92a4d03527
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-12-05 16:50:59 -0500

    Update run-example-tests-yarn-integration-tf.sh

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration-tf.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration-tf.sh
@@ -13,7 +13,7 @@ cat /etc/hostname
 
 python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py \
   --batch_size 4 \
-  --data_dir ${HDFS_URI}/ \
+  --data_dir /opt/work/manfei/BigDL/python/orca/example/learn/tf/transfer_learning \
   --cluster_mode yarn-client
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
