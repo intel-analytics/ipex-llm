663be51abe35e43905a7c477d37e6c0e04c1c5e2
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-12-05 17:46:37 -0500

    Update transfer_learning.py

--- python/orca/example/learn/tf/transfer_learning/transfer_learning.py
+++ python/orca/example/learn/tf/transfer_learning/transfer_learning.py
@@ -50,7 +50,7 @@ print("&&&&&&&&&&&&&&&&&&Current path&&&&&&&&&&&&&&&&&&&&&&")
 print(os.getcwd())
 for filepath,dirnames,filenames in os.walk('/opt/work/manfei/BigDL/python/orca/example/learn/tf/transfer_learning'):
     for filename in filenames:
-        print(os.path.join(filepath,filename))
+        #print(os.path.join(filepath,filename))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cluster_mode', type=str, default="local",
