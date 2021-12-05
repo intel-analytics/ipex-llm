90891ea623823767c0d21419f6d8091e785ee8cb
Manfei <41607353+ManfeiBai@users.noreply.github.com>
2021-12-05 16:25:48 -0500

    Update run-example-tests-yarn-integration-tf.sh

--- python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration-tf.sh
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration-tf.sh
@@ -4,6 +4,7 @@ clear_up() {
   pip uninstall -y bigdl-dllib
 }
 
+
 echo "#11 start test for orca transfer learning"
 #timer
 start=$(date "+%s")
@@ -12,7 +13,7 @@ cat /etc/hostname
 
 python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py \
   --batch_size 4 \
-  --data_dir /opt/work/manfei/BigDL/python/orca/example/learn/tf/transfer_learning/cats_and_dogs_filtered \
+  --data_dir --data_dir ${HDFS_URI}/datasets \
   --cluster_mode yarn-client
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
@@ -23,3 +24,27 @@ fi
 now=$(date "+%s")
 time=$((now - start))
 echo "#11 Total time cost ${time} seconds"
+
+: '
+
+echo "#11 start test for orca transfer learning"
+#timer
+start=$(date "+%s")
+#run the example
+cat /etc/hostname
+
+python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py \
+  --batch_size 4 \
+  --data_dir /opt/work/manfei/BigDL/python/orca/example/learn/tf/transfer_learning/cats_and_dogs_filtered \
+  --cluster_mode local
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca transfer learning failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#11 Total time cost ${time} seconds"
+
+'
