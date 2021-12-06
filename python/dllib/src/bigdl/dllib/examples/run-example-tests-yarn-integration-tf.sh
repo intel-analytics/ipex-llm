3ac1ba05fd8182f24fcfeb6d055803a17951a204
piaolaidelangman <707056129@qq.com>
2021-12-06 09:35:49 +0800

    test-transfer

---
+++ python/dllib/src/bigdl/dllib/examples/run-example-tests-yarn-integration-tf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear_up() {
+  echo "Clearing up environment. Uninstalling dllib"
+  pip uninstall -y bigdl-dllib
+}
+
+echo "#15 start test for orca tf transfer_learning "
+#timer   
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py --batch_size 32 --cluster_mode yarn --data_dir /data/diankun
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  clear_up
+  echo "orca torchmodel mnist failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#15 Total time cost ${time} seconds"
+
