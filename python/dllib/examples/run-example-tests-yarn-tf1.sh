6ae87412f3cbdd1a314044471de3f57c510a0190
piaolaidelangman <707056129@qq.com>
2021-12-13 15:06:13 +0800

    test tf

--- python/dllib/examples/run-example-tests-yarn-tf1.sh
+++ python/dllib/examples/run-example-tests-yarn-tf1.sh
@@ -132,36 +132,36 @@
 # time=$((now - start))
 # echo "#7 Total time cost ${time} seconds"
 
-echo "#8 start test for orca learn transfer_learning"
-#timer 
-start=$(date "+%s")
-#run the example
-python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py --data_dir /data --cluster_mode yarn-client
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-#   clear_up
-  echo "orca learning transfer_learning failed"
-  exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#8 Total time cost ${time} seconds"
+# echo "#8 start test for orca learn transfer_learning"
+# #timer 
+# start=$(date "+%s")
+# #run the example
+# python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py --data_dir /data --cluster_mode yarn-client
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+# #   clear_up
+#   echo "orca learning transfer_learning failed"
+#   exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#8 Total time cost ${time} seconds"
 
-echo "#20 start test for orca tf basic_text_classification basic_text_classification"
-#timer
-start=$(date "+%s")
-#run the example
-python ${BIGDL_ROOT}/python/orca/example/learn/tf/basic_text_classification/basic_text_classification.py \
-  --cluster_mode yarn-client
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-  #clear_up
-  echo "orca basic_text_classification failed"
-  exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#20 Total time cost ${time} seconds"
+# echo "#20 start test for orca tf basic_text_classification basic_text_classification"
+# #timer
+# start=$(date "+%s")
+# #run the example
+# python ${BIGDL_ROOT}/python/orca/example/learn/tf/basic_text_classification/basic_text_classification.py \
+#   --cluster_mode yarn-client
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+#   #clear_up
+#   echo "orca basic_text_classification failed"
+#   exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#20 Total time cost ${time} seconds"
 
 echo "#21 start test for orca tf image_segmentation image_segmentation.py"
 #timer
@@ -228,7 +228,7 @@ echo "#22 Total time cost ${time} seconds"
 #   --weights /data/yolov3/yolov3.weights \
 #   --class_num 20 \
 #   --names /bigdl2.0/data/yolov3/voc2012.names \
-#   --epochs 1 ---cluster_mode yarn-client
+#   --epochs 1 --cluster_mode yarn-client
 # exit_status=$?
 # if [ $exit_status -ne 0 ]; then
 #   clear_up
