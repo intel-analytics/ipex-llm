a76d2b8747536fe56e964f6d30c2e7c8ae55204d
glorysdj <glorysdj@gmail.com>
2021-12-14 15:14:01 +0800

    upgrade log4j to 2.15.0 and also eliminate 1.x

--- scala/friesian.serving/src/main/java/com/intel/analytics/bigdl/friesian/serving/ranking/RankingServer.java
+++ scala/friesian.serving/src/main/java/com/intel/analytics/bigdl/friesian/serving/ranking/RankingServer.java
@@ -31,8 +31,10 @@ import io.grpc.stub.StreamObserver;
 import io.prometheus.client.exporter.HTTPServer;
 import me.dinowernli.grpc.prometheus.Configuration;
 import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import java.io.IOException;
 import java.util.Base64;
@@ -41,7 +43,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class RankingServer extends GrpcServerBase {
-    private static final Logger logger = Logger.getLogger(RankingServer.class.getName());
+    private static final Logger logger = LogManager.getLogger(RankingServer.class.getName());
 
     /**
      * One Server could support multiple services.
@@ -51,7 +53,7 @@ public class RankingServer extends GrpcServerBase {
     public RankingServer(String[] args) {
         super(args);
         configPath = "config_ranking.yaml";
-        Logger.getLogger("org").setLevel(Level.ERROR);
+        Configurator.setLevel("org", Level.ERROR);
     }
 
     @Override
