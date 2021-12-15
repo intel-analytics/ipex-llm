4e5e0112d21d613d96ee18cda33cfc4f936f654b
piaolaidelangman <707056129@qq.com>
2021-12-15 10:29:52 +0800

    delete transfer learning

--- python/dllib/examples/run-example-tests-yarn-tf1.sh
+++ python/dllib/examples/run-example-tests-yarn-tf1.sh
@@ -222,20 +222,20 @@
 
 ###################### cluster
 
-echo "#8 start test for orca learn transfer_learning"
-#timer 
-start=$(date "+%s")
-#run the example
-python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py --data_dir /data --cluster_mode yarn-cluster
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-#   clear_up
-  echo "orca learning transfer_learning failed"
-#  exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#8 Total time cost ${time} seconds"
+# echo "#8 start test for orca learn transfer_learning"
+# #timer 
+# start=$(date "+%s")
+# #run the example
+# python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py --data_dir /data --cluster_mode yarn-cluster
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+# #   clear_up
+#   echo "orca learning transfer_learning failed"
+# #  exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#8 Total time cost ${time} seconds"
 
 echo "#20 start test for orca tf basic_text_classification basic_text_classification"
 #timer
