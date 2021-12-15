f6cfe3405ec44829de418ad3ce140883d56e55c1
piaolaidelangman <707056129@qq.com>
2021-12-15 12:06:56 +0800

    test basic text client and cluster

--- python/dllib/examples/run-example-tests-yarn-tf1.sh
+++ python/dllib/examples/run-example-tests-yarn-tf1.sh
@@ -148,21 +148,20 @@
 # time=$((now - start))
 # echo "#8 Total time cost ${time} seconds"
 
-# echo "#20 start test for orca tf basic_text_classification basic_text_classification"
-# #timer
-# start=$(date "+%s")
-# #run the example
-# python ${BIGDL_ROOT}/python/orca/example/learn/tf/basic_text_classification/basic_text_classification.py \
-#   --cluster_mode yarn-client
-# exit_status=$?
-# if [ $exit_status -ne 0 ]; then
-#   #clear_up
-#   echo "orca basic_text_classification failed"
-#   exit $exit_status
-# fi
-# now=$(date "+%s")
-# time=$((now - start))
-# echo "#20 Total time cost ${time} seconds"
+echo "#20 start test for orca tf basic_text_classification basic_text_classification"
+#timer
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/learn/tf/basic_text_classification/basic_text_classification.py --cluster_mode yarn-client --data_dir /data
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  #clear_up
+  echo "orca basic_text_classification failed"
+  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#20 Total time cost ${time} seconds"
 
 # echo "#21 start test for orca tf image_segmentation image_segmentation.py"
 # #timer
@@ -241,7 +240,7 @@ echo "#20 start test for orca tf basic_text_classification basic_text_classifica
 #timer
 start=$(date "+%s")
 #run the example
-python ${BIGDL_ROOT}/python/orca/example/learn/tf/basic_text_classification/basic_text_classification.py --cluster_mode yarn-cluster
+python ${BIGDL_ROOT}/python/orca/example/learn/tf/basic_text_classification/basic_text_classification.py --cluster_mode yarn-cluster --data_dir /data
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
   #clear_up
@@ -252,40 +251,41 @@ now=$(date "+%s")
 time=$((now - start))
 echo "#20 Total time cost ${time} seconds"
 
-echo "#21 start test for orca tf image_segmentation image_segmentation.py"
-#timer
-start=$(date "+%s")
-#run the example
-python ${BIGDL_ROOT}/python/orca/example/learn/tf/image_segmentation/image_segmentation.py \
-  --batch_size 64 \
-  --file_path /data/carvana \
-  --non_interactive --epochs 1 --cluster_mode yarn-cluster
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-  #clear_up
-  echo "orca image_segmentation failed"
-#  exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#21 Total time cost ${time} seconds"
+####################### passed
+# echo "#21 start test for orca tf image_segmentation image_segmentation.py"
+# #timer
+# start=$(date "+%s")
+# #run the example
+# python ${BIGDL_ROOT}/python/orca/example/learn/tf/image_segmentation/image_segmentation.py \
+#   --batch_size 64 \
+#   --file_path /data/carvana \
+#   --non_interactive --epochs 1 --cluster_mode yarn-cluster
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+#   #clear_up
+#   echo "orca image_segmentation failed"
+# #  exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#21 Total time cost ${time} seconds"
 
-echo "#22 start test for orca inception inception"
-#timer
-start=$(date "+%s")
-#run the example
-python ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception.py  \
-  --imagenet ${HDFS_URI}/imagenettfrecord \
-  -b 128 --cluster_mode yarn-cluster
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-  clear_up
-  echo "orca inception failed"
-#  exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#22 Total time cost ${time} seconds"
+# echo "#22 start test for orca inception inception"
+# #timer ModuleNotFoundError: No module named 'inception_preprocessing'
+# start=$(date "+%s")
+# #run the example
+# python ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception.py  \
+#   --imagenet ${HDFS_URI}/imagenettfrecord \
+#   -b 128 --cluster_mode yarn-cluster
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+#   clear_up
+#   echo "orca inception failed"
+# #  exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#22 Total time cost ${time} seconds"
 
 ###########################
 
--- python/orca/example/learn/tf/basic_text_classification/basic_text_classification.py
+++ python/orca/example/learn/tf/basic_text_classification/basic_text_classification.py
@@ -58,6 +58,9 @@ import tensorflow as tf
 
 from tensorflow import keras
 
+import numpy as np
+from keras.utils.data_utils import get_file
+
 import argparse
 
 from bigdl.orca import init_orca_context, stop_orca_context
@@ -67,8 +70,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--cluster_mode', type=str, default="local",
                     help='The mode for the Spark cluster. local, yarn or spark-submit.')
 parser.add_argument('--epochs', type=int, default=2, help='number of epochs to train for')
+parser.add_argument('--download', type=bool, default=False, help='download dataset or not')
+parser.add_argument('--data_dir', type=str, default="./dataset", help='The path of datesets where includes imdb.npz if download ')
 args = parser.parse_args()
 cluster_mode = args.cluster_mode
+download = args.download
 if cluster_mode == "local":
     init_orca_context(cluster_mode="local", cores=4, memory="3g")
 elif cluster_mode.startswith("yarn"):
@@ -83,7 +89,69 @@ print(tf.__version__)
 
 imdb = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+if download == True:
+    (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+else:
+    import numpy as np
+    from tensorflow.keras.utils import get_file
+    path = os.path.join(args.data_dir, 'imdb.npz')
+    num_words=None
+    skip_top=0
+    maxlen=None
+    seed=113
+    start_char=1
+    oov_char=2
+    index_from=3
+    with np.load(path, allow_pickle=True) as f:  # pylint: disable=unexpected-keyword-arg
+        x_train, labels_train = f['x_train'], f['y_train']
+        x_test, labels_test = f['x_test'], f['y_test']
+
+    rng = np.random.RandomState(seed)
+    indices = np.arange(len(x_train))
+    rng.shuffle(indices)
+    x_train = x_train[indices]
+    labels_train = labels_train[indices]
+
+    indices = np.arange(len(x_test))
+    rng.shuffle(indices)
+    x_test = x_test[indices]
+    labels_test = labels_test[indices]
+
+    if start_char is not None:
+        x_train = [[start_char] + [w + index_from for w in x] for x in x_train]
+        x_test = [[start_char] + [w + index_from for w in x] for x in x_test]
+    elif index_from:
+        x_train = [[w + index_from for w in x] for x in x_train]
+        x_test = [[w + index_from for w in x] for x in x_test]
+
+    if maxlen:
+        x_train, labels_train = _remove_long_seq(maxlen, x_train, labels_train)
+        x_test, labels_test = _remove_long_seq(maxlen, x_test, labels_test)
+    if not x_train or not x_test:
+        raise ValueError('After filtering for sequences shorter than maxlen='
+                        f'{str(maxlen)}, no sequence was kept. Increase maxlen.')
+
+    xs = x_train + x_test
+    labels = np.concatenate([labels_train, labels_test])
+
+    if not num_words:
+        num_words = max(max(x) for x in xs)
+
+    # by convention, use 2 as OOV word
+    # reserve 'index_from' (=3 by default) characters:
+    # 0 (padding), 1 (start), 2 (OOV)
+    if oov_char is not None:
+        xs = [
+            [w if (skip_top <= w < num_words) else oov_char for w in x] for x in xs
+        ]
+    else:
+        xs = [[w for w in x if skip_top <= w < num_words] for x in xs]
+
+    idx = len(x_train)
+    x_train, y_train = np.array(xs[:idx], dtype='object'), labels[:idx]
+    x_test, y_test = np.array(xs[idx:], dtype='object'), labels[idx:]
+    (train_data, train_labels), (test_data, test_labels) = (x_train, y_train), (x_test, y_test)
+    print(type(test_data))
 
 print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
 
