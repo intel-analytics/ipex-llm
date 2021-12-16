8c8f41d4ec4a8cea2652f5ac43bae809697886bd
piaolaidelangman <707056129@qq.com>
2021-12-16 09:51:09 +0800

    test inception

--- python/dllib/examples/run-example-tests-yarn-tf1.sh
+++ python/dllib/examples/run-example-tests-yarn-tf1.sh
@@ -133,20 +133,20 @@
 # echo "#7 Total time cost ${time} seconds"
 
 ############################# client
-echo "#8 start test for orca learn transfer_learning"
-#timer 
-start=$(date "+%s")
-#run the example
-python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py --data_dir /data --cluster_mode yarn-client --download False
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-#   clear_up
-  echo "orca learning transfer_learning failed"
-  #exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#8 Total time cost ${time} seconds"
+# echo "#8 start test for orca learn transfer_learning"
+# #timer 
+# start=$(date "+%s")
+# #run the example
+# python ${BIGDL_ROOT}/python/orca/example/learn/tf/transfer_learning/transfer_learning.py --data_dir /data --cluster_mode yarn-client --download False
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+# #   clear_up
+#   echo "orca learning transfer_learning failed"
+#   #exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#8 Total time cost ${time} seconds"
 
 # echo "#20 start test for orca tf basic_text_classification basic_text_classification"
 # #timer
@@ -181,22 +181,22 @@ echo "#8 Total time cost ${time} seconds"
 # time=$((now - start))
 # echo "#21 Total time cost ${time} seconds"
 
-# echo "#22 start test for orca inception inception"
-# #timer
-# start=$(date "+%s")
-# #run the example
-# python ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception.py  \
-#   --imagenet /data/imagenettfrecord \
-#   -b 128 --cluster_mode yarn-client
-# exit_status=$?
-# if [ $exit_status -ne 0 ]; then
-#   #clear_up
-#   echo "orca inception failed"
-#   #exit $exit_status
-# fi
-# now=$(date "+%s")
-# time=$((now - start))
-# echo "#22 Total time cost ${time} seconds"
+echo "#22 start test for orca inception inception"
+#timer
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception.py  \
+  --imagenet /data/imagenettfrecord \
+  -b 128 --cluster_mode yarn-client
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  #clear_up
+  echo "orca inception failed"
+  #exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#22 Total time cost ${time} seconds"
 
 # echo "#9 start test for orca learn tf2 resnet"
 # #timer
@@ -269,22 +269,22 @@ echo "#8 Total time cost ${time} seconds"
 # time=$((now - start))
 # echo "#21 Total time cost ${time} seconds"
 
-# echo "#22 start test for orca inception inception"
-# #timer ModuleNotFoundError: No module named 'inception_preprocessing'
-# start=$(date "+%s")
-# #run the example
-# python ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception.py  \
-#   --imagenet /data/imagenettfrecord \
-#   -b 128 --cluster_mode yarn-cluster
-# exit_status=$?
-# if [ $exit_status -ne 0 ]; then
-#   #clear_up
-#   echo "orca inception failed"
-# #  exit $exit_status
-# fi
-# now=$(date "+%s")
-# time=$((now - start))
-# echo "#22 Total time cost ${time} seconds"
+echo "#22 start test for orca inception inception"
+#timer ModuleNotFoundError: No module named 'inception_preprocessing'
+start=$(date "+%s")
+#run the example
+python ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception.py  \
+  --imagenet /data/imagenettfrecord \
+  -b 128 --cluster_mode yarn-cluster
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+  #clear_up
+  echo "orca inception failed"
+#  exit $exit_status
+fi
+now=$(date "+%s")
+time=$((now - start))
+echo "#22 Total time cost ${time} seconds"
 
 # echo "#9 start test for orca learn tf2 resnet"
 # #timer  success
