94a11685ca12cca9f2053847f381f4c9164ed6bb
yizerozhuang <yzhu8075@uni.sydney.edu.au>
2021-12-16 10:10:32 +0800

    update

--- python/friesian/dev/test/run-example-tests-tf2.6.0.sh
+++ python/friesian/dev/test/run-example-tests-tf2.6.0.sh
@@ -74,8 +74,6 @@ else
 fi
 
 python ../../example/deep_fm/deepFM_train.py \
-    --executor_cores 4 \
-    --executor_memory 10g \
     --data_dir ./data/input_2tower \
     --model_dir ./result/model \
     --frequency_limit 2 
