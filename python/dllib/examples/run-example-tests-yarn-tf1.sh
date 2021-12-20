a5b6544dec89a1b160c2fa88b25628772c23c7ab
piaolaidelangman <707056129@qq.com>
2021-12-20 12:15:47 +0800

    inception cluster

--- python/dllib/examples/run-example-tests-yarn-tf1.sh
+++ python/dllib/examples/run-example-tests-yarn-tf1.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 
-echo "#3 start test for orca inception inception"
-#timer
-start=$(date "+%s")
-#run the example
-python ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception.py  \
-  --imagenet /data/imagenettfrecord \
-  --py_files ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception_preprocessing.py \
-  -b 128 --cluster_mode yarn-client
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-  echo "orca inception failed"
-  exit $exit_status
-fi
-now=$(date "+%s")
-time=$((now - start))
-echo "#3 Total time cost ${time} seconds"
+# echo "#3 start test for orca inception inception"
+# #timer
+# start=$(date "+%s")
+# #run the example
+# python ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception.py  \
+#   --imagenet /data/imagenettfrecord \
+#   --py_files ${BIGDL_ROOT}/python/orca/example/learn/tf/inception/inception_preprocessing.py \
+#   -b 128 --cluster_mode yarn-client
+# exit_status=$?
+# if [ $exit_status -ne 0 ]; then
+#   echo "orca inception failed"
+#   exit $exit_status
+# fi
+# now=$(date "+%s")
+# time=$((now - start))
+# echo "#3 Total time cost ${time} seconds"
 
 echo "#7 start test for orca inception inception"
 #timer
