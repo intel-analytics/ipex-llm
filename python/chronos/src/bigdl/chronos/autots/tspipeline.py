d81f43b89c1798a2a30a94ce257b2f87266e5433
liangs6212 <liangx.shen@intel.com>
2021-12-21 00:50:13 +0800

    add dataloader support first version.

--- python/chronos/src/bigdl/chronos/autots/tspipeline.py
+++ python/chronos/src/bigdl/chronos/autots/tspipeline.py
@@ -15,9 +15,11 @@
 #
 
 import os
+import numpy
 import torch
 import types
 
+from torch.utils.data import DataLoader
 from bigdl.chronos.data import TSDataset
 from bigdl.chronos.metric.forecast_metrics import Evaluator
 
@@ -82,13 +84,23 @@ class TSPipeline:
                effective when data is a TSDataset. The values defaults to 32.
         '''
         # predict
-        x, y = self._tsdataset_to_numpy(data, is_predict=False)
-        yhat = self._best_model.inference(torch.from_numpy(x),
-                                          batch_size=batch_size,
-                                          backend=None).numpy()
-        yhat = self._tsdataset_unscale(yhat)
-        # unscale
-        y = self._tsdataset_unscale(y)
+        if isinstance(data, TSDataset):
+            x, y = self._tsdataset_to_numpy(data, is_predict=False)
+            yhat = self._best_model.inference(torch.from_numpy(x),
+                                              batch_size=batch_size,
+                                              backend=None).numpy()
+            yhat = self._tsdataset_unscale(yhat)
+            # unscale
+            y = self._tsdataset_unscale(y)
+        elif isinstance(data, DataLoader):
+            x, y = self._data_loader(data)
+            yhat = self._best_model.inference(x if isinstance(x, numpy)
+                                                else torch.from_numpy(x),
+                                              batch_size=batch_size,
+                                              backend=None).numpy()
+        else:
+            pass
+
         # evaluate
         aggregate = 'mean' if multioutput == 'uniform_average' else None
         eval_result = Evaluator.evaluate(metrics, y, yhat, aggregate=aggregate)
@@ -134,11 +146,19 @@ class TSPipeline:
                if batch_size is small while cost less memory.  The param is only
                effective when data is a TSDataset. The values defaults to 32.
         '''
-        x, _ = self._tsdataset_to_numpy(data, is_predict=True)
-        yhat = self._best_model.inference(torch.from_numpy(x),
-                                          batch_size=batch_size,
-                                          backend=None)
-        yhat = self._tsdataset_unscale(yhat)
+        if isinstance(data, TSDataset):
+            x, _ = self._tsdataset_to_numpy(data, is_predict=True)
+            yhat = self._best_model.inference(torch.from_numpy(x),
+                                              batch_size=batch_size,
+                                              backend=None)
+            yhat = self._tsdataset_unscale(yhat)
+        elif isinstance(data, DataLoader):
+            x, _ = self._data_loader(data)
+            yhat = self._best_model.inference(x,
+                                              batch_size=batch_size,
+                                              backward=None)
+        else:
+            pass
         return yhat
 
     def predict_with_onnx(self, data, batch_size=32):
@@ -152,11 +172,19 @@ class TSPipeline:
                if batch_size is small while cost less memory.  The param is only
                effective when data is a TSDataset. The values defaults to 32.
         '''
-        x, _ = self._tsdataset_to_numpy(data, is_predict=True)
-        yhat = self._best_model.inference(x,
-                                          batch_size=batch_size,
-                                          backend="onnx")
-        yhat = self._tsdataset_unscale(yhat)
+        if isinstance(data, TSDataset):
+            x, _ = self._tsdataset_to_numpy(data, is_predict=True)
+            yhat = self._best_model.inference(x,
+                                              batch_size=batch_size,
+                                              backend="onnx")
+            yhat = self._tsdataset_unscale(yhat)
+        elif isinstance(data, DataLoader):
+            x, _ = self._data_loader(data)
+            yhat = self._best_model.inference(x,
+                                              batch_size=batch_size,
+                                              backend="onnx")
+        else:
+            pass
         return yhat
 
     def fit(self,
@@ -187,17 +215,21 @@ class TSPipeline:
         from bigdl.nano.pytorch.trainer import Trainer
 
         train_loader = None
-        vaild_loader = None
-        if batch_size is None:
-            batch_size = self._best_config["batch_size"]
-        train_loader = self._tsdataset_to_loader(data, batch_size=batch_size)
-        if validation_data is not None:
-            vaild_loader = self._tsdataset_to_loader(validation_data, batch_size=batch_size)
+        valid_loader = None
+        if isinstance(data, TSDataset):
+            if batch_size is None:
+                batch_size = self._best_config["batch_size"]
+            train_loader = self._tsdataset_to_loader(data, batch_size=batch_size)
+            if validation_data is not None:
+                valid_loader = self._tsdataset_to_loader(validation_data, batch_size=batch_size)
+        else:
+            # data creator
+            train_loader, valid_loader = data, validation_data
 
         self.trainer = Trainer(max_epochs=epochs, **kwargs)
         self.trainer.fit(self._best_model,
                          train_dataloaders=train_loader,
-                         val_dataloaders=vaild_loader)
+                         val_dataloaders=valid_loader)
 
     def save(self, file_path):
         '''
@@ -297,3 +329,17 @@ class TSPipeline:
             from bigdl.chronos.data.utils.scale import unscale_timeseries_numpy
             y = unscale_timeseries_numpy(y, self._scaler, self._scaler_index)
         return y
+
+    def _data_loader(self, data):
+        from torch import Tensor
+        data_type_check = next(iter(data))[0]
+        if isinstance(data_type_check, numpy):
+            x = numpy.concatenate([x for x, _ in data], axis=0)
+            y = numpy.concatenate([y for _, y in data], axis=0)
+        elif isinstance(data_type_check, Tensor):
+            x = torch.cat([x for x, _ in data], dim=0)
+            y = torch.cat([y for _, y in data], dim=0)
+        else:
+            raise TypeError("We only support numpy.ndarray or numpy, "
+                            f"but found {type(data_type_check)}")
+        return x, y
