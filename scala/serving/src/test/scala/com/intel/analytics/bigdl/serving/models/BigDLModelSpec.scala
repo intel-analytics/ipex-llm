7e38ba94d6c75ac7f6acbb428b0c0357bc681049
Jiaming <litchilitchy@qq.com>
2021-12-27 13:44:12 +0800

    add addr

--- scala/serving/src/test/scala/com/intel/analytics/bigdl/serving/models/BigDLModelSpec.scala
+++ scala/serving/src/test/scala/com/intel/analytics/bigdl/serving/models/BigDLModelSpec.scala
@@ -3,14 +3,18 @@ package com.intel.analytics.bigdl.serving.models
 import com.intel.analytics.bigdl.dllib.tensor.Tensor
 import com.intel.analytics.bigdl.serving.{ClusterServing, ClusterServingHelper}
 import org.scalatest.{FlatSpec, Matchers}
+import scala.sys.process._
 
 class BigDLModelSpec extends FlatSpec with Matchers {
   "BigDL NNModel" should "work" in {
+    ("wget --no-check-certificate -O /tmp/linear.model https://sourceforge.net/" +
+      "projects/analytics-zoo/files/analytics-zoo-data/linear.model").!
     ClusterServing.helper = new ClusterServingHelper()
     val helper = ClusterServing.helper
     helper.modelType = "bigdl"
-    helper.weightPath = "/home/litchy/models/linear.model"
+    helper.weightPath = "/tmp/linear.model"
     ClusterServing.model = helper.loadInferenceModel()
+    "rm /tmp/linear.model".!
     val tensor = Tensor[Float](1, 2).rand()
     val result = ClusterServing.model.doPredict(tensor)
     require(result.toTensor[Float].size().sameElements(Array(1, 1)), "shape error")
