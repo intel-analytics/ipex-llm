1725d1156dd992b057a61c0d16ddd4bdfeef1003
Yu Shan <shan.yu@intel.com>
2021-12-27 10:42:17 +0800

    Update multi-agent to the latest for ray upgrade (#3729)
    * update mutli agent to the latest
    
    * comment out async parameter server due to segmentation fault
    
    * comment out aync_parameter and sync_parameter
    
    * remove comment out
    
    * change num_workers to 0 in examples
    
    * remove comment out
    
    * remove comment out
    
    * change num_workers in super_resolution

--- python/orca/example/ray_on_spark/rllib/multi_agent_two_trainers.py
+++ python/orca/example/ray_on_spark/rllib/multi_agent_two_trainers.py
@@ -1,29 +1,10 @@
-# This file is adapted from https://github.com/ray-project/ray/blob
-# /master/python/ray/rllib/examples/multiagent_two_trainers.py
-#
-# Copyright 2016 The BigDL Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 """Example of using two different training methods at once in multi-agent.
+
 Here we create a number of CartPole agents, some of which are trained with
 DQN, and some of which are trained with PPO. We periodically sync weights
 between the two trainers (note that no such syncing is needed when using just
 a single training method).
+
 For a simpler example, see also: multiagent_cartpole.py
 """
 
@@ -31,17 +12,13 @@ import argparse
 import gym
 import os
 
-from ray.rllib.agents.dqn.dqn import DQNTrainer
-from ray.rllib.agents.dqn.dqn_tf_policy import DQNTFPolicy
-from ray.rllib.agents.ppo.ppo import PPOTrainer
-from ray.rllib.agents.ppo.ppo_tf_policy import PPOTFPolicy
-from ray.rllib.tests.test_multi_agent_env import MultiAgentCartPole
+from ray.rllib.agents.dqn import DQNTrainer, DQNTFPolicy, DQNTorchPolicy
+from ray.rllib.agents.ppo import PPOTrainer, PPOTFPolicy, PPOTorchPolicy
+from ray.rllib.examples.env.multi_agent import MultiAgentCartPole
 from ray.tune.logger import pretty_print
 from ray.tune.registry import register_env
 from bigdl.orca import init_orca_context, stop_orca_context
 from bigdl.orca import OrcaContext
-
-os.environ["LANG"] = "C.UTF-8"
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cluster_mode', type=str, default="local",
@@ -68,9 +45,36 @@ parser.add_argument("--extra_executor_memory_for_ray", type=str, default="20g",
 parser.add_argument("--object_store_memory", type=str, default="4g",
                     help="The memory to store data on local."
                          "You can change it depending on your own cluster setting.")
+# Use torch for both policies.
+parser.add_argument(
+    "--framework",
+    choices=["tf", "tf2", "tfe", "torch"],
+    default="tf",
+    help="The DL framework specifier.")
+parser.add_argument(
+    "--as-test",
+    action="store_true",
+    help="Whether this script should be run as a test: --stop-reward must "
+    "be achieved within --stop-timesteps AND --stop-iters.")
+parser.add_argument(
+    "--stop-iters",
+    type=int,
+    default=10,
+    help="Number of iterations to train.")
+parser.add_argument(
+    "--stop-timesteps",
+    type=int,
+    default=100000,
+    help="Number of timesteps to train.")
+parser.add_argument(
+    "--stop-reward",
+    type=float,
+    default=50.0,
+    help="Reward at which we stop training.")
 
 if __name__ == "__main__":
     args = parser.parse_args()
+
     cluster_mode = args.cluster_mode
     if cluster_mode.startswith("yarn"):
         if cluster_mode == "yarn-client":
@@ -105,47 +109,64 @@ if __name__ == "__main__":
               + cluster_mode)
 
     # Simple environment with 4 independent cartpole entities
-    register_env("multi_cartpole", lambda _: MultiAgentCartPole({"num_agents": 4}))
-    single_env = gym.make("CartPole-v0")
-    obs_space = single_env.observation_space
-    act_space = single_env.action_space
+    register_env("multi_agent_cartpole",
+                 lambda _: MultiAgentCartPole({"num_agents": 4}))
+    single_dummy_env = gym.make("CartPole-v0")
+    obs_space = single_dummy_env.observation_space
+    act_space = single_dummy_env.action_space
 
     # You can also have multiple policies per trainer, but here we just
     # show one each for PPO and DQN.
     policies = {
-        "ppo_policy": (PPOTFPolicy, obs_space, act_space, {}),
-        "dqn_policy": (DQNTFPolicy, obs_space, act_space, {}),
+        "ppo_policy": (PPOTorchPolicy if args.framework == "torch" else
+                       PPOTFPolicy, obs_space, act_space, {}),
+        "dqn_policy": (DQNTorchPolicy if args.framework == "torch" else
+                       DQNTFPolicy, obs_space, act_space, {}),
     }
 
-    def policy_mapping_fn(agent_id):
+    def policy_mapping_fn(agent_id, episode, worker, **kwargs):
         if agent_id % 2 == 0:
             return "ppo_policy"
         else:
             return "dqn_policy"
 
     ppo_trainer = PPOTrainer(
-        env="multi_cartpole",
+        env="multi_agent_cartpole",
         config={
             "multiagent": {
                 "policies": policies,
                 "policy_mapping_fn": policy_mapping_fn,
                 "policies_to_train": ["ppo_policy"],
             },
+            "model": {
+                "vf_share_layers": True,
+            },
+            "num_sgd_iter": 6,
+            "vf_loss_coeff": 0.01,
             # disable filters, otherwise we would need to synchronize those
             # as well to the DQN agent
-            "observation_filter": "NoFilter",
+            "observation_filter": "MeanStdFilter",
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+            "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+            "framework": args.framework,
         })
 
     dqn_trainer = DQNTrainer(
-        env="multi_cartpole",
+        env="multi_agent_cartpole",
         config={
             "multiagent": {
                 "policies": policies,
                 "policy_mapping_fn": policy_mapping_fn,
                 "policies_to_train": ["dqn_policy"],
             },
+            "model": {
+                "vf_share_layers": True,
+            },
             "gamma": 0.95,
             "n_step": 3,
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+            "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
+            "framework": args.framework,
         })
 
     # You should see both the printed X and Y approach 200 as this trains:
@@ -153,20 +174,33 @@ if __name__ == "__main__":
     #   policy_reward_mean:
     #     dqn_policy: X
     #     ppo_policy: Y
-    for i in range(args.iterations):
+    for i in range(args.stop_iters):
         print("== Iteration", i, "==")
 
         # improve the DQN policy
         print("-- DQN --")
-        print(pretty_print(dqn_trainer.train()))
+        result_dqn = dqn_trainer.train()
+        print(pretty_print(result_dqn))
 
         # improve the PPO policy
         print("-- PPO --")
-        print(pretty_print(ppo_trainer.train()))
+        result_ppo = ppo_trainer.train()
+        print(pretty_print(result_ppo))
+
+        # Test passed gracefully.
+        if args.as_test and \
+                result_dqn["episode_reward_mean"] > args.stop_reward and \
+                result_ppo["episode_reward_mean"] > args.stop_reward:
+            print("test passed (both agents above requested reward)")
+            quit(0)
 
         # swap weights to synchronize
         dqn_trainer.set_weights(ppo_trainer.get_weights(["ppo_policy"]))
         ppo_trainer.set_weights(dqn_trainer.get_weights(["dqn_policy"]))
 
+    # Desired reward not reached.
+    if args.as_test:
+        raise ValueError("Desired reward ({}) not reached!".format(
+            args.stop_reward))
     ray_ctx.stop()
     stop_orca_context()
