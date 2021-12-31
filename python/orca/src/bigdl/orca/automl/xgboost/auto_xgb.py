8b24919407fa4fa7a3aea7c2623128aa5a045f4b
Yu Shan <shan.yu@intel.com>
2021-12-31 15:57:21 +0800

    Add Pyspark DataFrame support for AutoXGBoost (#3822)
    * add Spark DataFrame input for AutoXGBoost
    
    * typo

--- python/orca/src/bigdl/orca/automl/xgboost/auto_xgb.py
+++ python/orca/src/bigdl/orca/automl/xgboost/auto_xgb.py
@@ -63,6 +63,77 @@ class AutoXGBClassifier(AutoEstimator):
                          resources_per_trial=resources_per_trial,
                          name=name)
 
+    def fit(self,
+            data,
+            epochs=1,
+            validation_data=None,
+            metric=None,
+            metric_mode=None,
+            metric_threshold=None,
+            n_sampling=1,
+            search_space=None,
+            search_alg=None,
+            search_alg_params=None,
+            scheduler=None,
+            scheduler_params=None,
+            feature_cols=None,
+            target_cols=None,
+            ):
+        """
+        Automatically fit the model and search for the best hyperparameters.
+
+        :param data: A Spark DataFrame, a tuple of ndarrays or a function.
+            If data is a tuple of ndarrays, it should be in the form of (x, y),
+            where x is training input data and y is training target data.
+            If data is a function, it should takes config as argument and returns a tuple of
+            ndarrays in the form of (x, y).
+        :param epochs: Max number of epochs to train in each trial. Defaults to 1.
+            If you have also set metric_threshold, a trial will stop if either it has been
+            optimized to the metric_threshold or it has been trained for {epochs} epochs.
+        :param validation_data: Validation data. Validation data type should be the same as data.
+        :param metric: String or customized evaluation metric function.
+            If string, metric is the evaluation metric name to optimize, e.g. "mse".
+            If callable function, it signature should be func(y_true, y_pred), where y_true and
+            y_pred are numpy ndarray. The function should return a float value as evaluation result.
+        :param metric_mode: One of ["min", "max"]. "max" means greater metric value is better.
+            You have to specify metric_mode if you use a customized metric function.
+            You don't have to specify metric_mode if you use the built-in metric in
+            bigdl.orca.automl.metrics.Evaluator.
+        :param metric_threshold: a trial will be terminated when metric threshold is met
+        :param n_sampling: Number of times to sample from the search_space. Defaults to 1.
+            If hp.grid_search is in search_space, the grid will be repeated n_sampling of times.
+            If this is -1, (virtually) infinite samples are generated
+            until a stopping condition is met.
+        :param search_space: a dict for search space
+        :param search_alg: str, all supported searcher provided by ray tune
+               (i.e."variant_generator", "random", "ax", "dragonfly", "skopt",
+               "hyperopt", "bayesopt", "bohb", "nevergrad", "optuna", "zoopt" and
+               "sigopt")
+        :param search_alg_params: extra parameters for searcher algorithm besides search_space,
+            metric and searcher mode
+        :param scheduler: str, all supported scheduler provided by ray tune
+        :param scheduler_params: parameters for scheduler
+        :param feature_cols: feature column names if data is Spark DataFrame.
+        :param target_cols: target column names if data is Spark DataFrame.
+        """
+        data, validation_data = _maybe_convert_spark_df_to_ndarray(data,
+                                                                   validation_data,
+                                                                   feature_cols,
+                                                                   target_cols)
+        super().fit(data=data,
+                    epochs=epochs,
+                    validation_data=validation_data,
+                    metric=metric,
+                    metric_mode=metric_mode,
+                    metric_threshold=metric_threshold,
+                    n_sampling=n_sampling,
+                    search_space=search_space,
+                    search_alg=search_alg,
+                    search_alg_params=search_alg_params,
+                    scheduler=scheduler,
+                    scheduler_params=scheduler_params,
+                    )
+
 
 class AutoXGBRegressor(AutoEstimator):
     def __init__(self,
@@ -108,3 +179,112 @@ class AutoXGBRegressor(AutoEstimator):
                          logs_dir=logs_dir,
                          resources_per_trial=resources_per_trial,
                          name=name)
+
+    def fit(self,
+            data,
+            epochs=1,
+            validation_data=None,
+            metric=None,
+            metric_mode=None,
+            metric_threshold=None,
+            n_sampling=1,
+            search_space=None,
+            search_alg=None,
+            search_alg_params=None,
+            scheduler=None,
+            scheduler_params=None,
+            feature_cols=None,
+            target_cols=None,
+            ):
+        """
+        Automatically fit the model and search for the best hyperparameters.
+
+        :param data: A Spark DataFrame, a tuple of ndarrays or a function.
+            If data is a tuple of ndarrays, it should be in the form of (x, y),
+            where x is training input data and y is training target data.
+            If data is a function, it should takes config as argument and returns a tuple of
+            ndarrays in the form of (x, y).
+        :param epochs: Max number of epochs to train in each trial. Defaults to 1.
+            If you have also set metric_threshold, a trial will stop if either it has been
+            optimized to the metric_threshold or it has been trained for {epochs} epochs.
+        :param validation_data: Validation data. Validation data type should be the same as data.
+        :param metric: String or customized evaluation metric function.
+            If string, metric is the evaluation metric name to optimize, e.g. "mse".
+            If callable function, it signature should be func(y_true, y_pred), where y_true and
+            y_pred are numpy ndarray. The function should return a float value as evaluation result.
+        :param metric_mode: One of ["min", "max"]. "max" means greater metric value is better.
+            You have to specify metric_mode if you use a customized metric function.
+            You don't have to specify metric_mode if you use the built-in metric in
+            bigdl.orca.automl.metrics.Evaluator.
+        :param metric_threshold: a trial will be terminated when metric threshold is met
+        :param n_sampling: Number of times to sample from the search_space. Defaults to 1.
+            If hp.grid_search is in search_space, the grid will be repeated n_sampling of times.
+            If this is -1, (virtually) infinite samples are generated
+            until a stopping condition is met.
+        :param search_space: a dict for search space
+        :param search_alg: str, all supported searcher provided by ray tune
+               (i.e."variant_generator", "random", "ax", "dragonfly", "skopt",
+               "hyperopt", "bayesopt", "bohb", "nevergrad", "optuna", "zoopt" and
+               "sigopt")
+        :param search_alg_params: extra parameters for searcher algorithm besides search_space,
+            metric and searcher mode
+        :param scheduler: str, all supported scheduler provided by ray tune
+        :param scheduler_params: parameters for scheduler
+        :param feature_cols: feature column names if data is Spark DataFrame.
+        :param target_cols: target column names if data is Spark DataFrame.
+        """
+        data, validation_data = _maybe_convert_spark_df_to_ndarray(data,
+                                                                   validation_data,
+                                                                   feature_cols,
+                                                                   target_cols)
+        super().fit(data=data,
+                    epochs=epochs,
+                    validation_data=validation_data,
+                    metric=metric,
+                    metric_mode=metric_mode,
+                    metric_threshold=metric_threshold,
+                    n_sampling=n_sampling,
+                    search_space=search_space,
+                    search_alg=search_alg,
+                    search_alg_params=search_alg_params,
+                    scheduler=scheduler,
+                    scheduler_params=scheduler_params,
+                    )
+
+
+def _maybe_convert_spark_df_to_ndarray(data,
+                                       validation_data,
+                                       feature_cols,
+                                       target_cols):
+    def convert_df_to_ndarray(data, feature_cols, target_cols):
+        df = data.toPandas()
+        X = df[feature_cols]
+        y = df[target_cols]
+        arrays = (X, y)
+        return arrays
+
+    def check_cols(cols, cols_name):
+        if not cols:
+            raise ValueError(f"You must input valid {cols_name} for Spark DataFrame data input")
+        if isinstance(cols, list):
+            return cols
+        if not isinstance(cols, str):
+            raise ValueError(f"{cols_name} should be a string or a list of strings, "
+                             f"but got {type(cols)}")
+        return [cols]
+
+    from pyspark.sql import DataFrame
+    if isinstance(data, DataFrame):
+        feature_cols = check_cols(feature_cols, cols_name="feature_cols")
+        target_cols = check_cols(target_cols, cols_name="target_cols")
+        train_arrays = convert_df_to_ndarray(data, feature_cols, target_cols)
+        if validation_data:
+            if not isinstance(validation_data, DataFrame):
+                raise ValueError(f"data and validation_data should be both Spark DataFrame, "
+                                 f"but got validation_data of type {type(data)}")
+            val_arrays = convert_df_to_ndarray(validation_data, feature_cols, target_cols)
+        else:
+            val_arrays = None
+        return train_arrays, val_arrays
+    else:
+        return data, validation_data
