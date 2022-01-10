011983f1ae8645d9d2630d3b1619d50d30477d2f
piaolaidelangman <707056129@qq.com>
2022-01-10 09:06:01 +0800

    Graphene java makefile and sgx start

--- scala/ppml/demo/runFlserver.sh
+++ scala/ppml/demo/runFlserver.sh
@@ -1,5 +1,3 @@
-java -cp /opt/bigdl-0.14.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar com.intel.analytics.bigdl.ppml.FLServer
-#/opt/spark/bin/spark-submit \
-#  --master local[2] \
-#  --class com.intel.analytics.bigdl.ppml.FLServer \
-#  /opt/bigdl-0.14.0-SNAPSHOT/jars/bigdl-ppml-spark_2.4.6-0.14.0-SNAPSHOT-jar-with-dependencies.jar
+cd /opt && \
+make SGX=1 DEBUG=1 && \
+graphene-sgx java -cp /opt/bigdl-0.14.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar com.intel.analytics.bigdl.ppml.FLServer
