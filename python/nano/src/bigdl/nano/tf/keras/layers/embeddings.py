d897f390d41d6e6d73a2e10a93b199a47a87bded
Wang, Yang <yang3.wang@intel.com>
2022-01-10 02:34:20 +0000

    fix style

--- python/nano/src/bigdl/nano/tf/keras/layers/embeddings.py
+++ python/nano/src/bigdl/nano/tf/keras/layers/embeddings.py
@@ -16,6 +16,7 @@
 
 import tensorflow as tf
 
+
 class Embedding(tf.keras.layers.Embedding):
 
     def __init__(self,
@@ -27,40 +28,41 @@ class Embedding(tf.keras.layers.Embedding):
                  mask_zero=False,
                  input_length=None,
                  **kwargs):
-    '''
-    This is a slightly modified version of tf.keras.Embedding,
-    which only apply regularizer to the output of the embedding
-    layers, such that the gradient to embeddings is sparse. 
 
-    :param input_sample: torch.Tensor or a list for the model tracing.
-    :param file_path: The path to save onnx model file.
-    :param sess_options: ortsess options in ort.SessionOptions type
-    :param **kwargs: will be passed to torch.onnx.export function.
+        '''
+        This is a slightly modified version of tf.keras.Embedding,
+        which only apply regularizer to the output of the embedding
+        layers, such that the gradient to embeddings is sparse.
 
-    :param input_dim: Integer. Size of the vocabulary,
-        i.e. maximum integer index + 1.
-    :param output_dim: Integer. Dimension of the dense embedding.
-    :param embeddings_initializer: Initializer for the `embeddings`
-        matrix (see `keras.initializers`).
-    :param regularizer: Regularizer function applied to
-        the output tensor after looking up the `embeddings` matrix.
-    :param embeddings_constraint: Constraint function applied to
-        the `embeddings` matrix (see `keras.constraints`).
-    :param mask_zero: Boolean, whether or not the input value 0 is a special "padding"
-        value that should be masked out.
-        This is useful when using recurrent layers
-        which may take variable length input.
-        If this is `True`, then all subsequent layers
-        in the model need to support masking or an exception will be raised.
-        If mask_zero is set to True, as a consequence, index 0 cannot be
-        used in the vocabulary (input_dim should equal size of
-        vocabulary + 1).
-    param: input_length: Length of input sequences, when it is constant.
-        This argument is required if you are going to connect
-        `Flatten` then `Dense` layers upstream
-        (without it, the shape of the dense outputs cannot be computed).
-    '''
-    
+        :param input_sample: torch.Tensor or a list for the model tracing.
+        :param file_path: The path to save onnx model file.
+        :param sess_options: ortsess options in ort.SessionOptions type
+        :param **kwargs: will be passed to torch.onnx.export function.
+
+        :param input_dim: Integer. Size of the vocabulary,
+            i.e. maximum integer index + 1.
+        :param output_dim: Integer. Dimension of the dense embedding.
+        :param embeddings_initializer: Initializer for the `embeddings`
+            matrix (see `keras.initializers`).
+        :param regularizer: Regularizer function applied to
+            the output tensor after looking up the `embeddings` matrix.
+        :param embeddings_constraint: Constraint function applied to
+            the `embeddings` matrix (see `keras.constraints`).
+        :param mask_zero: Boolean, whether or not the input value 0 is a special "padding"
+            value that should be masked out.
+            This is useful when using recurrent layers
+            which may take variable length input.
+            If this is `True`, then all subsequent layers
+            in the model need to support masking or an exception will be raised.
+            If mask_zero is set to True, as a consequence, index 0 cannot be
+            used in the vocabulary (input_dim should equal size of
+            vocabulary + 1).
+        param: input_length: Length of input sequences, when it is constant.
+            This argument is required if you are going to connect
+            `Flatten` then `Dense` layers upstream
+            (without it, the shape of the dense outputs cannot be computed).
+        '''
+
     super(Embedding).__init__(input_dim=input_dim,
                               output_dim=output_dim,
                               embeddings_initializer=embeddings_initializer,
