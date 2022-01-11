88ba25d31196476ab33f4fd1bab86a70b719cf92
piaolaidelangman <707056129@qq.com>
2022-01-11 09:04:24 +0800

    success start flserver in graphene but client still fail

---
+++ scala/ppml/demo/runFlServer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+SGX=1 graphene-sgx bash -c "/opt/jdk8/bin/java -cp bigdl-0.14.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar com.intel.analytics.bigdl.ppml.FLServer"
+
--- scala/ppml/demo/runVflClient1.sh
+++ scala/ppml/demo/runVflClient1.sh
@@ -1,2 +1,3 @@
-java -cp /opt/bigdl-0.14.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar \
-com.intel.analytics.bigdl.ppml.example.VflLogisticRegression -d /opt/data/diabetes-vfl-1.csv
+#!/bin/bash
+SGX=1 graphene-sgx bash -c "/opt/jdk8/bin/java -cp /opt/bigdl-0.14.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar \
+com.intel.analytics.bigdl.ppml.example.VflLogisticRegression -d /opt/data/diabetes-vfl-1.csv"
