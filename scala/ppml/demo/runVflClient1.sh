95626feb4ff20c98b6979c916965bcd004f6914f
piaolaidelangman <707056129@qq.com>
2022-01-11 09:07:35 +0800

    typo and format

--- scala/ppml/demo/runVflClient1.sh
+++ scala/ppml/demo/runVflClient1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SGX=1 graphene-sgx bash -c "/opt/jdk8/bin/java -cp /opt/bigdl-0.14.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar \
-com.intel.analytics.bigdl.ppml.example.VflLogisticRegression -d /opt/data/diabetes-vfl-1.csv"
+    com.intel.analytics.bigdl.ppml.example.VflLogisticRegression -d /opt/data/diabetes-vfl-1.csv"
--- scala/ppml/demo/runVflClient2.sh
+++ scala/ppml/demo/runVflClient2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SGX=1 graphene-sgx bash -c "/opt/jdk8/bin/java -cp /opt/bigdl-0.14.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar \
-com.intel.analytics.bigdl.ppml.example.VflLogisticRegression -d /opt/data/diabetes-vfl-2.csv --hasLabel false"
+    com.intel.analytics.bigdl.ppml.example.VflLogisticRegression -d /opt/data/diabetes-vfl-2.csv --hasLabel false"
