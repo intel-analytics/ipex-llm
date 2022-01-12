783440f5afe00f01bd259c17835fc2d85ef41ebc
Chen, Zhentao <zhentao.chen@intel.com>
2022-01-12 14:30:05 +0800

    add header

--- python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
+++ python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import warnings
 from logging import warning
 from typing import Any, List, Optional
 
