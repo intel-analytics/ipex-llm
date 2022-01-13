445f12ef3bfb440cc97a8db35980972f0a200584
Uxito-Ada <414416158@qq.com>
2022-01-13 09:38:04 +0800

    Modify codes according to reviews

--- ppml/kms-client/DecryptColumnsWithCSV.py
+++ ppml/kms-client/DecryptColumnsWithCSV.py
@@ -1,17 +1,29 @@
 # Import kms-client, csv And Decryption APIs To Get Datakey
 import sys
-sys.path.append('/ppml/trusted-big-data-ml/work/kms-client')
 from GetDataKeyPlaintext import decrypt_data_key as get_data_key_plaintext
 import csv
 from cryptography.fernet import Fernet
 import os
 from glob import glob
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('-path', '--path', type=str, help='path of the column encrypted CSV file', required=True)
-args = parser.parse_args()
+import time
 
+start = time.time()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-ip', '--ip', type=str, help='ip address of the ehsm_kms_server', required=True)
+parser.add_argument('-port', '--port', type=str, help='port of the ehsm_kms_server',default='3000', required=False)
+parser.add_argument('-path', '--path', type=str, help='path of the column encrypted CSV file', required=True)
+parser.add_argument('-pkp', '--pkp', type=str, help='path of the primary key storage file', required=True)
+parser.add_argument('-dkp', '--dkp', type=str, help='path of the data key storage file', required=True)
+
+args = parser.parse_args()
+ip = args.ip
+port = args.port
 path = args.path # CSV File Path
+pkp = args.pkp
+dkp = args.dkp
+
 EXT = "*.csv"
 all_csv_files = [file
                  for p, subdir, files in os.walk(path)
@@ -28,7 +40,7 @@ print('[INFO] If The First Line In Input CSV Is Not A Headr, The 18th Line Shoul
 # If The First Line In Input CSV Is The Header, Write The Header Directly. Otherwise, The Below Line Should Be Annotated
 csvWriter.writerow(next(data))
 
-data_key = get_data_key_plaintext("192.168.0.112","3000","/ppml/trusted-big-data-ml/encrypted_primary_key","/ppml/trusted-big-data-ml/encrypted_data_key") # Request Data Key For Decryption
+data_key = get_data_key_plaintext(ip, port, pkp, dkp) # Request Data Key For Decryption
 
 fernet = Fernet(data_key) # Generate Decryption Method
 
@@ -38,13 +50,12 @@ for row in data:
         if i == 3:
             write_buffer.append(row[i])
         else:
-            #print(i)
-            #print(row[i])
             plaintext = fernet.decrypt(row[i].encode('ascii')).decode("utf-8")
-            #print(plaintext)
             dpt = fernet.decrypt(plaintext.encode('ascii')).decode("utf-8")
-            #print(dpt)
-            write_buffer.append(dpt)
+        write_buffer.append(dpt)
     csvWriter.writerow(write_buffer)
 
+end = time.time()
+
+print('[INFO] Total Elapsed Time For Columns Decrytion: ' + str(end - start) + ' s')
 print('[INFO] Decryption Finished. The Output Is ' + path + '.col_decrypted')
