1ad441c15e98449b99105b9ac5fa0795c633de5f
yizerozhuang <48541207+yizerozhuang@users.noreply.github.com>
2022-01-13 23:52:03 +0800

    Update deepFM_train.py

--- python/friesian/example/deep_fm/deepFM_train.py
+++ python/friesian/example/deep_fm/deepFM_train.py
@@ -138,8 +138,8 @@ if __name__ == '__main__':
 
     train = FeatureTable.read_parquet(args.data_dir + "/train_parquet")
     test = FeatureTable.read_parquet(args.data_dir + "/test_parquet")
-    print(train.df.rdd.flatMap(lambda x: x).collect())
-    print(test.df.rdd.flatMap(lambda x: x).collect())
+    print(train.df.rdd.collect())
+    print(test.df.rdd.collect())
     
     test_user_ids = test.select("engaged_with_user_id").cast("engaged_with_user_id", "str").\
         to_list("engaged_with_user_id")
