e4778f4447718c6139bfcd0c1d80f44916605289
piaolaidelangman <707056129@qq.com>
2022-01-17 22:56:39 +0800

    Format

--- scala/ppml/demo/runFlServer.sh
+++ scala/ppml/demo/runFlServer.sh
@@ -2,4 +2,3 @@
 
 SGX=1 ./pal_loader bash -c "/opt/jdk8/bin/java -Xms8g -Xmx8g -cp /ppml/trusted-big-data-ml/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar \
  com.intel.analytics.bigdl.ppml.FLServer"
-
