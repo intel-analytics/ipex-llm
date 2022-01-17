5d6f5e7a51dbfb3bd9b1ff9b02a57bd46ab211a3
piaolaidelangman <707056129@qq.com>
2022-01-17 22:56:39 +0800

    Format

--- scala/ppml/demo/runVflClient2.sh
+++ scala/ppml/demo/runVflClient2.sh
@@ -2,4 +2,3 @@
 
 SGX=1 ./pal_loader bash -c "/opt/jdk8/bin/java -Xms2g -Xmx2g -cp $SPARK_HOME/jars/*:$BIGDL_HOME/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar \
   com.intel.analytics.bigdl.ppml.example.VflLogisticRegression -d data/diabetes-vfl-2.csv --hasLabel false"
-
