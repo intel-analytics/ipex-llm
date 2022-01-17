de50ddf0e2b25d15a3c94a935379ff0e37b21e45
sgwhat <ge.song@intel.com>
2022-01-17 15:17:09 +0800

    add ray_ctx test script

---
+++ python/orca/dev/test/run-pytests-ray-ctx.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# bigdl orca test only support pip, you have to install orca whl before running the script.
+#. `dirname $0`/prepare_env.sh
+
+set -ex
+
+cd "`dirname $0`"
+cd ../..
+
+export PYSPARK_PYTHON=python
+export PYSPARK_DRIVER_PYTHON=python
+
+ray stop -f
+
+echo "Running RayContext tests"
+python -m pytest -v test/bigdl/orca/ray/ray_ctx
+exit_status_1=$?
+if [ $exit_status_1 -ne 0 ];
+then
+    exit $exit_status_1
+fi
+
+ray stop -f
