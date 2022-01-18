fc336aed8baca48fd681073baf08b95a412982e3
piaolaidelangman <707056129@qq.com>
2022-01-18 09:29:07 +0800

    Change jvm-mem to 2g in vfl

--- scala/ppml/demo/runVflClient1.sh
+++ scala/ppml/demo/runVflClient1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-SGX=1 ./pal_loader bash -c "/opt/jdk8/bin/java -Xms8g -Xmx8g -cp $SPARK_HOME/jars/*:$BIGDL_HOME/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar \
+SGX=1 ./pal_loader bash -c "/opt/jdk8/bin/java -Xms2g -Xmx2g -cp $SPARK_HOME/jars/*:$BIGDL_HOME/jars/bigdl-ppml-spark_3.1.2-0.14.0-SNAPSHOT-jar-with-dependencies.jar \
   com.intel.analytics.bigdl.ppml.example.VflLogisticRegression -d data/diabetes-vfl-1.csv"
