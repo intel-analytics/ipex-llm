720138f569f85955fd21e41acb3af0cb9941caf6
liangs6212 <liangx.shen@intel.com>
2022-01-19 00:40:52 +0800

    add auto-nbeats API and ut

--- python/chronos/src/bigdl/chronos/autots/model/auto_nbeats.py
+++ python/chronos/src/bigdl/chronos/autots/model/auto_nbeats.py
@@ -14,10 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from .base_automodel import BasePytorchAutomodel
+
 from bigdl.orca.automl.auto_estimator import AutoEstimator
 from bigdl.chronos.model.nbeats_pytorch import model_creator
 from bigdl.orca.automl.model.base_pytorch_model import PytorchModelBuilder
+
+from .base_automodel import BasePytorchAutomodel
 
 
 class AutoNbeats(BasePytorchAutomodel):
@@ -33,38 +35,69 @@ class AutoNbeats(BasePytorchAutomodel):
                  optimizer,
                  loss,
                  metric,
-                 lr=0.01,
+                 input_feature_num=1,
+                 output_target_num=1,
+                 lr=0.001,
+                 dropout=0.2,
                  backend="torch",
                  logs_dir="/tmp/auto_nbeats",
                  cpus_per_trial=1,
                  name="auto_nbeats",
                  remote_dir=None):
+        """
+        :param past_seq_len: Specify the history time steps (i.e. lookback).
+        :param future_seq_len: Specify the output time steps (i.e. horizon).
+        :param stack_types: Specifies the architecture of the stack,
+               This value defaults to ("generic", "generic").
+        :param nb_blocks_per_stack: Blocks contained in the stack,
+               This value defaults to 3.
+        :param thetas_dim: Number of fully connected layers
+               with ReLu activation per block.
+        :param share_weigets_in_stack: Shared weights between stacks,
+               This value defaults to False.
+        :param hidden_layer_units: The number of layers in a fully
+               connected neural network, This values defaults to 256.
+        :param nb_harmonics: Number of fully connected layers
+               with ReLu activation per block.
+        :param dropout: Specify the dropout close possibility
+               (i.e. the close possibility to a neuron). This value defaults to 0.001.
+        :param optimizer: Specify the optimizer used for training. This value
+               defaults to "Adam".
+        :param loss: Specify the loss function used for training. This value
+               defaults to "mse". You can choose from "mse", "mae" and
+               "huber_loss".
+        :param metric: A list contains metrics for evaluating the quality of
+               forecasting. You may only choose from "mse" and "mae" for a
+               distributed forecaster. You may choose from "mse", "mae",
+               "rmse", "r2", "mape", "smape", for a non-distributed forecaster.
+        """
         super().__init__()
 
         if backend != "torch":
             raise ValueError(f"We only support backend as torch. Got {backend}")
 
-        self.search_space = dict(stack_types,
-                                 nb_blocks_per_stack,
-                                 thetas_dim,
-                                 share_weigets_in_stack,
-                                 hidden_layer_units,
-                                 nb_harmonics,
-                                 past_seq_len,
-                                 future_seq_len,
-                                 lr,
-                                 input_feature_num=1,
-                                 output_feature_num=1)
+        if isinstance(input_feature_num, int) and input_feature_num != 1:
+            raise ValueError(f"NBeat only supports univariate prediction.")
+
+        self.search_space = dict(share_weigets_in_stack=share_weigets_in_stack,
+                                 hidden_layer_units=hidden_layer_units,
+                                 stack_types=stack_types,
+                                 thetas_dim=thetas_dim,
+                                 nb_blocks_per_stack=nb_blocks_per_stack,
+                                 nb_harmonics=nb_harmonics,
+                                 lr=lr,
+                                 dropout=dropout,
+                                 past_seq_len=past_seq_len,
+                                 future_seq_len=future_seq_len,
+                                 input_feature_num=input_feature_num,
+                                 output_feature_num=output_target_num)
 
         self.metric = metric
         model_builder = PytorchModelBuilder(model_creator=model_creator,
                                             optimizer_creator=optimizer,
                                             loss_creator=loss)
-        self.auto_nbeats = AutoEstimator(model_builder=model_builder,
-                                         logs_dir=logs_dir,
-                                         resources_per_trial={'cpu': cpus_per_trial},
-                                         remote_dir=remote_dir,
-                                         name=name)
-
-
-
+        self.auto_est = AutoEstimator(model_builder=model_builder,
+                                      logs_dir=logs_dir,
+                                      resources_per_trial={'cpu': cpus_per_trial},
+                                      remote_dir=remote_dir,
+                                      name=name)
