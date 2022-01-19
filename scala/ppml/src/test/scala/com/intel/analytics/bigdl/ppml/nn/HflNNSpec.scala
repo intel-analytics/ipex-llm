ecddb41eb4cd67c95531659db1a2157306752c48
Jiaming <litchilitchy@qq.com>
2022-01-19 12:59:48 +0800

    refactor

--- scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/nn/HflNNSpec.scala
+++ scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/nn/HflNNSpec.scala
@@ -1,8 +1,24 @@
-package com.intel.analytics.bigdl.ppml.hfl
+/*
+ * Copyright 2021 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.intel.analytics.bigdl.ppml.{FLContext, FLServer}
+package com.intel.analytics.bigdl.ppml.nn
+
 import com.intel.analytics.bigdl.ppml.algorithms.hfl.LogisticRegression
 import com.intel.analytics.bigdl.ppml.example.DebugLogger
+import com.intel.analytics.bigdl.ppml.{FLContext, FLServer}
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 class MockAnotherParty(algorithm: String, clientID: String = "mock") extends Thread {
@@ -15,7 +31,6 @@ class MockAnotherParty(algorithm: String, clientID: String = "mock") extends Thr
   }
   def runLogisticRegression(): Unit = {
     val spark = FLContext.getSparkSession()
-    import spark.implicits._
     val df = spark.read.option("header", "true")
       .csv(this.getClass.getClassLoader.getResource("diabetes-test.csv").getPath)
     val lr = new LogisticRegression(df.columns.size - 1)
@@ -28,7 +43,6 @@ class NNSpec extends FlatSpec with Matchers with BeforeAndAfter with DebugLogger
     flServer.build()
     flServer.start()
     val spark = FLContext.getSparkSession()
-    import spark.implicits._
     val trainDf = spark.read.option("header", "true")
       .csv(this.getClass.getClassLoader.getResource("diabetes-test.csv").getPath)
     val testDf = trainDf.drop("Outcome")
