77a95aeb8db3f15aab714c8c06b6254bd8ca1423
Chen, Zhentao <zhentao.chen@intel.com>
2022-01-21 13:14:00 +0800

    added inc non-installed test

--- python/nano/test/pytorch/tests/bare/test_missing_module.py
+++ python/nano/test/pytorch/tests/bare/test_missing_module.py
@@ -13,3 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+from unittest import TestCase
+
+import pytest
+import torchvision
+
+from bigdl.nano.pytorch.trainer import Trainer
+from pytorch.utils._train_torch_lightning import create_data_loader
+
+batch_size = 256
+num_workers = 0
+data_dir = os.path.join(os.path.dirname(__file__), "../../data")
+
+
+class TestLightningModuleFromTorch(TestCase):
+    resnet18 = torchvision.models.resnet18()
+    trainer = Trainer()
+
+    def test_quantize_without_inc(self):
+        pl_model = Trainer.compile(self.resnet18)
+        dataloader = create_data_loader(data_dir, batch_size, num_workers, None)
+        with pytest.raises(ImportError,
+                           match="Intel Neural Compressor must be installed to use quantization."
+                                 "Please install INC by: pip install neural-compressor."):
+            self.trainer.quantize(pl_model, dataloader)
