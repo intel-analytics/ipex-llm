5b3d2fa3ddfe60c9f46386e225876ba3966325a1
cyita <yitastudy@gmail.com>
2022-01-27 18:20:20 +0800

    fix style

--- scala/friesian/src/main/scala/com/intel/analytics/bigdl/friesian/nearline/feature/FeatureNearlineUtils.scala
+++ scala/friesian/src/main/scala/com/intel/analytics/bigdl/friesian/nearline/feature/FeatureNearlineUtils.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intel.analytics.bigdl.friesian.nearline.feature
 
 import com.intel.analytics.bigdl.friesian.nearline.utils.NearlineUtils
@@ -24,7 +40,8 @@ object FeatureNearlineUtils {
       logger.info("Start inserting user features...")
       val colNames = NearlineUtils.helper.userFeatureColArr.mkString(",")
       redis.setSchema("user", colNames)
-      val userFeatureColumns = NearlineUtils.helper.userIDColumn +: NearlineUtils.helper.userFeatureColArr
+      val userFeatureColumns = NearlineUtils.helper.userIDColumn +:
+        NearlineUtils.helper.userFeatureColArr
       divideFileAndLoad(spark, NearlineUtils.helper.initialUserDataPath, userFeatureColumns,
         "user")
     }
@@ -35,7 +52,8 @@ object FeatureNearlineUtils {
       logger.info("Start inserting item features...")
       val colNames = NearlineUtils.helper.itemFeatureColArr.mkString(",")
       redis.setSchema("item", colNames)
-      val itemFeatureColumns = NearlineUtils.helper.itemIDColumn +: NearlineUtils.helper.itemFeatureColArr
+      val itemFeatureColumns = NearlineUtils.helper.itemIDColumn +:
+        NearlineUtils.helper.itemFeatureColArr
       divideFileAndLoad(spark, NearlineUtils.helper.initialItemDataPath, itemFeatureColumns,
         "item")
     }
