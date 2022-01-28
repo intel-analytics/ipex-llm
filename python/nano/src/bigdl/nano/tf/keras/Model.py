959fb04d5d6705fe60d093f34f046d3ddd0683b2
charlieJ107 <charlie_j107@outlook.com>
2022-01-28 03:20:37 +0000

    Fix codestyle

--- python/nano/src/bigdl/nano/tf/keras/Model.py
+++ python/nano/src/bigdl/nano/tf/keras/Model.py
@@ -51,26 +51,27 @@ class Model(tf.keras.Model):
             assert issubclass(
                 type(x), tf.data.Dataset), "Data object currently only support tf.data.dataset"
             dataList = (
-                ("Data Pipeline", 100,  x.take(1).cache().repeat(100), callbacks),
+                ("Data Pipeline", 100, x.take(1).cache().repeat(100), callbacks),
                 ("Batch Size / 2", 200,
-                 x.take(1).cache().repeat(100).unbatch().batch(int(batch_size/2)), callbacks),
+                 x.take(1).cache().repeat(100).unbatch().batch(int(batch_size / 2)), callbacks),
                 ("Batch Size * 2", 50,
-                 x.take(1).cache().repeat(100).unbatch().batch(int(batch_size*2)), callbacks),
+                 x.take(1).cache().repeat(100).unbatch().batch(int(batch_size * 2)), callbacks),
                 ("Clear Callback", 100, x.take(1).cache().repeat(100), None)
             )
             for data in dataList:
                 start = time()
                 a = data[2]
                 super(Model, self).fit(
-                    a, y, batch_size, epochs, verbose, data[
-                        3], validation_split, validation_data, shuffle, class_weight,
-                    sample_weight, initial_epoch, steps_per_epoch, validation_steps, validation_batch_size, validation_freq,
+                    a, y, batch_size, epochs, verbose, data[3], validation_split, validation_data,
+                    shuffle, class_weight, sample_weight, initial_epoch, steps_per_epoch,
+                    validation_steps, validation_batch_size, validation_freq,
                     max_queue_size, workers, use_multiprocessing)
                 end = time()
-                print(f"Throughput of {data[0]}: ", data[1]/(end-start))
+                print(f"Throughput of {data[0]}: ", data[1] / (end - start))
 
         else:
-            super(Model, self).fit(
-                x, y, batch_size, epochs, verbose, callbacks, validation_split, validation_data, shuffle, class_weight,
-                sample_weight, initial_epoch, steps_per_epoch, validation_steps, validation_batch_size, validation_freq,
-                max_queue_size, workers, use_multiprocessing)
+                super(Model, self).fit(
+                    x, y, batch_size, epochs, verbose, callbacks, validation_split, validation_data,
+                    shuffle, class_weight, sample_weight, initial_epoch, steps_per_epoch,
+                    validation_steps, validation_batch_size, validation_freq,
+                    max_queue_size, workers, use_multiprocessing)
