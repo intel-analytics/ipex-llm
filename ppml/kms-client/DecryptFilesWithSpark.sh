44b543fb064e09d1d03271625f301fa824b28d0c
Uxito-Ada <414416158@qq.com>
2022-02-07 11:40:58 +0800

    [PPML] Move the Job of Key-request into Scala Jar

--- ppml/kms-client/DecryptFilesWithSpark.sh
+++ ppml/kms-client/DecryptFilesWithSpark.sh
@@ -1,11 +1,14 @@
 # set -x
 SPARK_DECRYPT_JAR_PATH=/ppml/trusted-big-data-ml/work/spark-3.1.2/examples/jars/spark-encrypt-io-0.1-SNAPSHOT.jar
-INPUT_DIR_PATH=$1
+INPUT_PATH=$1
 ENCRYPT_KEYS_PATH=$2
 KMS_SERVER_IP=$3
-LOCAL_IP=$4
+KMS_SERVER_PORT=$4
+LOCAL_IP=$5
+OUTPUT_PATH=$6
+
 secure_password=`openssl rsautl -inkey /ppml/trusted-big-data-ml/work/password/key.txt -decrypt </ppml/trusted-big-data-ml/work/password/output.bin`
-data_key=$(python /ppml/trusted-big-data-ml/work/kms-client/KMS_Client.py -api get_data_key_plaintext -ip $KMS_SERVER_IP -pkp $ENCRYPT_KEYS_PATH/encrypted_primary_key -dkp $ENCRYPT_KEYS_PATH/encrypted_data_key)
+#data_key=$(python /ppml/trusted-big-data-ml/work/kms-client/KMS_Client.py -api get_data_key_plaintext -ip $KMS_SERVER_IP -pkp $ENCRYPT_KEYS_PATH/encrypted_primary_key -dkp $ENCRYPT_KEYS_PATH/encrypted_data_key)
 
 /opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*:/ppml/trusted-big-data-ml/work/spark-3.1.2/examples/jars/spark-example-sql-e2e.jar' \
@@ -26,8 +29,10 @@ data_key=$(python /ppml/trusted-big-data-ml/work/kms-client/KMS_Client.py -api g
   --conf spark.ssl.trustStore=/ppml/trusted-big-data-ml/work/keys/keystore.jks \
   --conf spark.ssl.trustStorePassword=$secure_password \
   --conf spark.ssl.trustStoreType=JKS \
-  --class com.intel.analytics.bigdl.ppml.e2e.examples.SimpleEncryptIO \
+  --class com.intel.analytics.bigdl.ppml.e2e.examples.SimpleEncryptIOV2 \
   $SPARK_DECRYPT_JAR_PATH \
-  $INPUT_DIR_PATH \
-  $data_key \
-  $INPUT_DIR_PATH.col_encrypted
+  $INPUT_PATH \
+  $KMS_SERVER_IP \
+  $KMS_SERVER_PORT \
+  $ENCRYPT_KEYS_PATH \
+  $OUTPUT_PATH
