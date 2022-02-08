a2cd22eacb4b476de91ebd38405e24fdec0c65b6
dding3 <ding.ding@intel.com>
2022-02-08 09:26:31 -0800

    change keeporder to shuffle

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/keras/models/Topology.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/keras/models/Topology.scala
@@ -316,7 +316,7 @@ abstract class KerasNet[T](implicit val tag: ClassTag[T], implicit val ev: Tenso
   private def toDataSet(x: RDD[Sample[T]], batchSize: Int,
     featurePaddingParam: PaddingParam[T] = null,
     labelPaddingParam: PaddingParam[T] = null,
-    keepOrder: Boolean = false,
+    shuffleData: Boolean = false,
     groupSize: Int = 1): DataSet[MiniBatch[T]] = {
     val _featurePaddingParam = if (featurePaddingParam != null) {
       Some(featurePaddingParam)
@@ -325,7 +325,7 @@ abstract class KerasNet[T](implicit val tag: ClassTag[T], implicit val ev: Tenso
       Some(labelPaddingParam)
     } else None
 
-    if (x != null) DataSet.rdd(x, keepOrder = keepOrder, groupSize = groupSize) ->
+    if (x != null) DataSet.rdd(x, shuffleData = shuffleData, groupSize = groupSize) ->
       SampleToMiniBatch[T](batchSize, _featurePaddingParam, _labelPaddingParam)
     else null
   }
@@ -435,19 +435,19 @@ abstract class KerasNet[T](implicit val tag: ClassTag[T], implicit val ev: Tenso
    * @param validationData RDD of Sample, or null if validation is not configured. Default is null.
    */
   def fit(
-      x: RDD[Sample[T]],
-      batchSize: Int = 32,
-      nbEpoch: Int = 10,
-      validationData: RDD[Sample[T]] = null,
-      featurePaddingParam: PaddingParam[T] = null,
-      labelPaddingParam: PaddingParam[T] = null,
-      keepOrder: Boolean = false,
-      groupSize: Int = 1)(implicit ev: TensorNumeric[T]): Unit = {
+           x: RDD[Sample[T]],
+           batchSize: Int = 32,
+           nbEpoch: Int = 10,
+           validationData: RDD[Sample[T]] = null,
+           featurePaddingParam: PaddingParam[T] = null,
+           labelPaddingParam: PaddingParam[T] = null,
+           shuffleData: Boolean = false,
+           groupSize: Int = 1)(implicit ev: TensorNumeric[T]): Unit = {
     KerasUtils.validateBatchSize(batchSize)
     val trainData = toDataSet(x, batchSize, featurePaddingParam, labelPaddingParam,
-      keepOrder, groupSize)
+      shuffleData, groupSize)
     val valData = toDataSet(validationData, batchSize, featurePaddingParam,
-      labelPaddingParam, keepOrder, groupSize)
+      labelPaddingParam, shuffleData, groupSize)
     this.fit(trainData, nbEpoch, valData)
 
     releaseDataSets(Array(trainData, valData))
