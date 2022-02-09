5fe40594f22f6867ba4231760cfbf1d8f924b907
Wang <yiming.wang@intel.com>
2022-02-09 15:42:55 +0800

    Copare different results when applied lazyadam

--- python/nano/test/pytorch/tests/test_optim.py
+++ python/nano/test/pytorch/tests/test_optim.py
@@ -14,18 +14,16 @@
 # ==============================================================================
 
 import numpy as np
-# from bigdl.nano.pytorch.optim import LazyAdam
-from lazyadam import LazyAdam
+from bigdl.nano.pytorch.optim import LazyAdam
 import torch
 from torch import nn
-from torch.nn import Embedding, Sequential, MSELoss
 
 
 class Example18(nn.Module):
     def __init__(self, vocab_size, embedding_dim, sparse=False):
         super(Example18, self).__init__()
-        self.model = Sequential()
-        self.embedding = Embedding(vocab_size, embedding_dim, sparse=sparse)
+        self.model = nn.Sequential()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, sparse=sparse)
 
     def forward(self, inputs):
         embeds = self.embedding(inputs)
@@ -33,38 +31,60 @@ class Example18(nn.Module):
 
 
 def test_optim_lazyadam():
-    model = Example18(30, 16)
-    loss_function = MSELoss()
-    optimizer = LazyAdam(model.parameters())
+    # # Test different results of LazyAdam
+    # # when applied in sparse= False and sparse=True
+    # model1: sparse=False
+    # model2: sparse=True
+    model1 = Example18(30, 16, sparse=False)
+    model2 = Example18(30, 16, sparse=True)
 
-    N_EPOCHES = 1
+    loss_function = nn.MSELoss()
+
+    optimizer1 = LazyAdam(model1.parameters())
+    optimizer2 = LazyAdam(model2.parameters())
+
+    nums_epochs = 1
+    nums_batches = 2
     input = torch.LongTensor([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]])
-    labels_np = np.random.randint(30, size=model(input).shape)
+    labels_np = np.random.randint(30, size=model1(input).shape)
     labels = torch.FloatTensor(labels_np)
 
-    for epoch in range(N_EPOCHES):
+    sparse_dic = {}
+
+    for epoch in range(nums_epochs):
+        for i in range(nums_batches):
+            x = input[i]
+            y = labels[i]
+
+            model1.train()
+            total_loss = torch.Tensor([0])
+            model1.zero_grad()
+            result = model1(x)
+            loss = loss_function(result, y)
+            loss.backward()
+            optimizer1.step()
+            total_loss += loss.data
+
+            sparse_dic["result{0}".format(i)] = model1.state_dict()['embedding.weight'][0:5, :].clone()
+
+    for epoch in range(nums_epochs):
         for i in range(2):
             x = input[i]
             y = labels[i]
 
-            model.train()
-
+            model2.train()
             total_loss = torch.Tensor([0])
-            model.zero_grad()
-            result = model(x)
+            model2.zero_grad()
+            result = model2(x)
             loss = loss_function(result, y)
             loss.backward()
-            optimizer.step()
+            optimizer2.step()
             total_loss += loss.data
-            print("first:", model.state_dict()['embedding.weight'][0:10, :])
-    # before_weights = model.get_weights()
-    # model.fit(input_array, labels, epochs=4, batch_size=2)
-    # after_weights = model.get_weights()
-    #
-    # # check if only the 0-10 cows of weights
-    # # have changed after training
-    # assert ((before_weights[0][0:10, :] != after_weights[0][0:10, :]).all())
-    # assert ((before_weights[0][10:30, :] == after_weights[0][10:30, :]).all())
 
+            sparse_dic["result{0}".format(i + 2)] = model2.state_dict()['embedding.weight'][0:5, :].clone()
 
-test_optim_lazyadam()
+    # check if the 0-5 cows of model1 weights have changed after training
+    # check if the 0-5 cows of model2 weights remain unchanged after training
+    assert ((sparse_dic['result0'] != sparse_dic['result1']).all())
+    assert ((sparse_dic['result2'] == sparse_dic['result3']).all())
+
