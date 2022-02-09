a3401cef992b05546ed0220f132d869647b9d2c8
Chen, Zhentao <zhentao.chen@intel.com>
2022-02-09 10:06:48 +0800

    add quantization to example

--- python/nano/example/pytorch/quantization/inc/resnet18_cifar.py
+++ python/nano/example/pytorch/quantization/inc/resnet18_cifar.py
@@ -34,17 +34,20 @@
 
 # mypy: ignore-errors
 import os
+from time import time
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchmetrics import Accuracy
 import torchvision
-from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning import LightningModule, seed_everything
+from bigdl.nano.pytorch.trainer import Trainer
 from pytorch_lightning.callbacks import LearningRateMonitor
 from pytorch_lightning.loggers import TensorBoardLogger
 from torch.optim.lr_scheduler import OneCycleLR
 from torchmetrics.functional import accuracy
-
+import copy
 
 from pl_bolts.datamodules import CIFAR10DataModule
 from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
@@ -52,7 +55,7 @@ from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
 
 seed_everything(7)
 
-PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
+PATH_DATASETS = os.path.dirname(os.path.abspath(__file__))
 BATCH_SIZE = 64
 NUM_WORKERS = 0  # Multi-thread run sometimes raise quantization error
 
@@ -158,4 +161,52 @@ trainer = Trainer(
 )
 
 trainer.fit(model, cifar10_dm)
-trainer.test(model, datamodule=cifar10_dm)
+
+fp32_model = model
+# FP32 testing
+start = time()
+outputs = trainer.test(fp32_model, datamodule=cifar10_dm)
+time1 = time() - start
+log_dict = outputs[0]
+print("FP32 model size: %.3f MB" % fp32_model.model_size)
+print("Throughput: %d s" % time1)
+print("Accuracy: top1=%.2f%%" % (log_dict['test_acc'] * 100))
+
+# Save FP32 checkpoint
+trainer.save_checkpoint('ResNet18_on_CIFAR_FP32_CKPT.pth')
+
+# Run post-training quantization
+quantized = trainer.quantize(fp32_model, calib_dataloader=cifar10_dm.train_dataloader(),
+                             val_dataloader=cifar10_dm.test_dataloader(),
+                             metric=Accuracy(num_classes=10),
+                             # recommend 'pytorch_fx' instead of 'pytorch' to enable automatic
+                             # fusion
+                             framework='pytorch_fx',
+                             approach='static',
+                             tuning_strategy='bayesian',
+                             accuracy_criterion={
+                                 'higher_is_better': True, 'relative': 0.1},
+                             timeout=0,  # Limited time(seconds)
+                             max_trials=10,  # Maximum traverse to get best model
+                             raw_return=True)
+
+# Wrap quantized model as a Pytorch-Lightning Module for testing
+int8_model = copy.deepcopy(fp32_model)
+int8_model.model = quantized
+
+# Testing on quantized INT8 model
+start = time()
+outputs = trainer.test(int8_model, cifar10_dm)
+time2 = time() - start
+log_dict = outputs[0]
+print("INT8 model size: %.3f MB" % int8_model.model_size)
+print("Throughput: %d s" % time2)
+print("Accuracy: top1=%.2f%%" % (log_dict['test_acc'] * 100))
+
+# Save INT8 checkpoint
+trainer.save_checkpoint('ResNet18_on_CIFAR_INT8_CKPT.pth')
+
+print("After quantization, the model size reduces %d%%, throughput improves %d%%" % (
+     (fp32_model.model_size - int8_model.model_size) / fp32_model.model_size * 100,
+     (time1 - time2)/time1 * 100
+))
