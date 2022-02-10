ce645787666d653c343332317652b3f05ac8505b
piaolaidelangman <707056129@qq.com>
2022-02-10 14:16:26 +0800

    Merge preprocess to train

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/example/nnframes/xgboost/xgbClassifierTrainingExampleOnCriteoClickLogsDataset.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/example/nnframes/xgboost/xgbClassifierTrainingExampleOnCriteoClickLogsDataset.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intel.analytics.bigdl.dllib.examples.nnframes.xgboost
 
 import ml.dmlc.xgboost4j.scala.spark.TrackerConf
@@ -16,12 +32,16 @@ object xgbClassifierTrainingExampleOnCriteoClickLogsDataset {
 
     val sc = NNContext.initNNContext()
     val spark = SQLContext.getOrCreate(sc)
+    val task = new Task()
 
     val input_path = args(0) // path to data
     val modelsave_path = args(1) // save model to this path
     val num_threads = args(2).toInt // xgboost threads
 
-    var df = spark.read.option("header", "false").option("inferSchema", "true").option("delimiter", " ").csv(input_path)
+    // var df = spark.read.option("header", "false").option("inferSchema", "true").option("delimiter", " ").csv(input_path)
+    var df = spark.read.option("header", "false").option("inferSchema", "true").option("delimiter", "\t").csv(input_path)
+    df = df.rdd.map(task.rowToLibsvm).toDF
+
 
     val stringIndexer = new StringIndexer()
       .setInputCol("_c0")
