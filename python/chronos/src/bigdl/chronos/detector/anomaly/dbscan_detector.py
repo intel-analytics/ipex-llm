720b92c92997f1a19634bcd08afbcf474b7b7b9e
liangs6212 <liangx.shen@intel.com>
2022-02-11 00:14:09 +0800

    remove param algorithm_list

--- python/chronos/src/bigdl/chronos/detector/anomaly/dbscan_detector.py
+++ python/chronos/src/bigdl/chronos/detector/anomaly/dbscan_detector.py
@@ -57,21 +57,19 @@ class DBScanDetector(AnomalyDetector):
         if len(arr.shape) > 1:
             raise ValueError("Only univariate time series is supported")
 
-    def fit(self, y, use_sklearnex=True, algorithm_list=['DBSCAN']):
+    def fit(self, y, use_sklearnex=True):
         """
         Fit the model
 
         :param y: the input time series. y must be 1-D numpy array.
         :param use_sklearnex: bool, If scikit-learn-intelex is not installed,
                DBScanDetector will fallback to use stack sklearn.
-        :param algorithm_list: list, Select the accelerated algorithm name,
-               This default value is ['DBSCAN'].
         """
         self.check_data(y)
         self.anomaly_scores_ = np.zeros_like(y)
 
         with INTEL_EXT_DBSCAN(use_sklearnex=use_sklearnex,
-                              algorithm_list=algorithm_list) as DBSCAN:
+                              algorithm_list=['DBSCAN']) as DBSCAN:
             clusters = DBSCAN(eps=self.eps, min_samples=self.min_samples)\
                 .fit(y.reshape(-1, 1), **self.argv)
         labels = clusters.labels_
