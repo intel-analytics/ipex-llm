157f9aa6a2bea32cc58213e765773e1e98f6731e
liangs6212 <80952198+liangs6212@users.noreply.github.com>
2022-02-16 10:37:14 +0800

    Chronos:  Added sklearnex support in `DBSCAN` (#3904)
    * support sklearnex
    
    * fit typo, change warning message
    
    * change description
    
    * remove param algorithm_list
    
    * fix typo

--- python/chronos/src/bigdl/chronos/detector/anomaly/util.py
+++ python/chronos/src/bigdl/chronos/detector/anomaly/util.py
@@ -15,6 +15,40 @@
 #
 
 import numpy as np
+from logging import warning
+
+
+def sklearn_install_check():
+    try:
+        from sklearnex import patch_sklearn, unpatch_sklearn
+        sklearnex_available = True
+    except ImportError:
+        sklearnex_available = False
+    return sklearnex_available
+
+
+class INTEL_EXT_DBSCAN:
+    __slots__ = 'use_sklearnex', 'algorithm_list', 'DBSCAN', 'sklearnex_available'
+
+    def __init__(self, use_sklearnex, algorithm_list):
+        self.algorithm_list = algorithm_list
+        self.sklearnex_available = sklearn_install_check()
+        if not self.sklearnex_available and use_sklearnex:
+            warning("If you want to use sklearnex, please install scikit-learn-intelex first.")
+        self.use_sklearnex = use_sklearnex if self.sklearnex_available else False
+
+    def __enter__(self):
+        if self.use_sklearnex:
+            from sklearnex import patch_sklearn
+            patch_sklearn(self.algorithm_list)
+        from sklearn.cluster import DBSCAN
+        self.DBSCAN = DBSCAN
+        return self.DBSCAN
+
+    def __exit__(self, *args, **kwargs):
+        if self.use_sklearnex:
+            from sklearnex import unpatch_sklearn
+            unpatch_sklearn(self.algorithm_list)
 
 
 def roll_arr(arr, stride):
