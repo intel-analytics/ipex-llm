a93bb18710eb6436ec305026fca153d865fbfc7a
Yang Wang <yang3.wang@intel.com>
2022-02-16 08:46:38 +0800

    Fix keras multiprocessing (#4032)
    * fix keras multi processing
    
    * fix typo

--- python/nano/example/tensorflow/keras_multi_processing_demo.py
+++ python/nano/example/tensorflow/keras_multi_processing_demo.py
@@ -1,10 +1,5 @@
 import numpy as np
-import os
-import PIL
-from bigdl.nano.common.multiprocessing.ray_backend import RayBackend
-from bigdl.nano.tf.keras.distributed_utils import distributed_train_keras
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras import layers
 from bigdl.nano.tf.keras import Sequential
 
@@ -19,20 +14,20 @@ img_height = 180
 img_width = 180
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+    data_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 class_names = train_ds.class_names
 
@@ -43,16 +38,16 @@ train_ds = train_ds.cache().shuffle(100).prefetch(buffer_size=AUTOTUNE)
 num_classes = len(class_names)
 
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+    layers.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
 ])
 
 model.compile(optimizer='adam',
@@ -60,6 +55,4 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 model.fit(train_ds, epochs=3, validation_data=val_ds, nprocs=2)
-model.evaluate(train_ds)
 model.evaluate(val_ds)
-
