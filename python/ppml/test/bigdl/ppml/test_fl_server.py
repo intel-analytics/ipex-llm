28c452ceae479cf6dc278f13a4f91e1e6d53e556
Jiaming <litchilitchy@qq.com>
2022-02-17 16:54:16 +0800

    start flserver

--- python/ppml/test/bigdl/ppml/test_fl_server.py
+++ python/ppml/test/bigdl/ppml/test_fl_server.py
@@ -5,6 +5,8 @@ from bigdl.ppml import FLServer
 class MyTestCase(unittest.TestCase):
     def test_fl_server_default_config(self):
         fl_server = FLServer()
+        fl_server.build()
+        fl_server.start()
 
     def test_fl_server_custom_config(self):
         pass
