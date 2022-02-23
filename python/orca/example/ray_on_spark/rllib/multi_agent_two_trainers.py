64a6dfc0d0706e9b14856fb3a6ab246681dce622
Shan Yu <shan.yu@intel.com>
2022-02-23 17:23:29 +0800

    Fix license check doesn't take effect (#4093)
    * fix license check doesn't take effect
    
    * add license
    
    * add license
    
    * add license
    
    * add license
    
    * add license
    
    * add license

--- python/orca/example/ray_on_spark/rllib/multi_agent_two_trainers.py
+++ python/orca/example/ray_on_spark/rllib/multi_agent_two_trainers.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 """Example of using two different training methods at once in multi-agent.
 
 Here we create a number of CartPole agents, some of which are trained with
