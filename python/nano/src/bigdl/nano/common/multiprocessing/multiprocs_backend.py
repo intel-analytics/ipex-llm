434c8364c9912cfb596e414e7a9cfd47acf8590d
lwf <xtu_fan@163.com>
2022-02-24 09:59:20 +0800

    dump args into a single file

--- python/nano/src/bigdl/nano/common/multiprocessing/multiprocs_backend.py
+++ python/nano/src/bigdl/nano/common/multiprocessing/multiprocs_backend.py
@@ -14,11 +14,8 @@
 # limitations under the License.
 #
 
-import copy
 import os
-from itertools import product
-import multiprocessing
-from multiprocessing import Process, Pool
+from tempfile import TemporaryDirectory
 
 from bigdl.nano.common.multiprocessing.backend import Backend
 
@@ -47,33 +44,25 @@ class MultiprocessingBackend(Backend):
         import subprocess
         import sys
 
-        temp_dir = args[0]
-        with open(os.path.join(temp_dir, "train_ds_def.pkl"), 'wb') as f:
-            pickle.dump(args[1], f)
-        with open(os.path.join(temp_dir, "train_elem_spec.pkl"), 'wb') as f:
-            pickle.dump(args[2], f)
-        with open(os.path.join(temp_dir, "val_ds_def.pkl"), 'wb') as f:
-            pickle.dump(args[3], f)
-        with open(os.path.join(temp_dir, "val_elem_spec.pkl"), 'wb') as f:
-            pickle.dump(args[4], f)
-        with open(os.path.join(temp_dir, "fit_kwargs.pkl"), 'wb') as f:
-            pickle.dump(args[5], f)
-        with open(os.path.join(temp_dir, "target.pkl"), 'wb') as f:
-            pickle.dump(target, f)
+        with TemporaryDirectory() as temp_dir:
+            with open(os.path.join(temp_dir, "args.pkl"), 'wb') as f:
+                pickle.dump(args, f)
+            with open(os.path.join(temp_dir, "target.pkl"), 'wb') as f:
+                pickle.dump(target, f)
 
-        ex_list = []
-        cwd_path = os.path.split(os.path.realpath(__file__))[0]
-        for i in range(nprocs):
-            for key, val in os.environ.items():
-                if key not in envs[i]:
-                    envs[i][key] = val
-            ex_list.append(subprocess.Popen([sys.executable, f"{cwd_path}/worker.py", temp_dir],
-                                            env=envs[i]))
-        for _, ex in enumerate(ex_list):
-            ex.wait()
+            ex_list = []
+            cwd_path = os.path.split(os.path.realpath(__file__))[0]
+            for i in range(nprocs):
+                for key, val in os.environ.items():
+                    if key not in envs[i]:
+                        envs[i][key] = val
+                ex_list.append(subprocess.Popen([sys.executable, f"{cwd_path}/worker.py", temp_dir],
+                                                env=envs[i]))
+            for _, ex in enumerate(ex_list):
+                ex.wait()
 
-        results = []
-        for i in range(nprocs):
-            with open(os.path.join(temp_dir, f"history_{i}"), "rb") as f:
-                results.append(pickle.load(f))
+            results = []
+            for i in range(nprocs):
+                with open(os.path.join(temp_dir, f"history_{i}"), "rb") as f:
+                    results.append(pickle.load(f))
         return results
