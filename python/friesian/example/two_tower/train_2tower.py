a99758a1c6ab598f98724dd3dbf9f0db96e883c4
Kai Huang <huangkaivision@gmail.com>
2022-02-24 15:41:15 +0800

    add error report

--- python/friesian/example/two_tower/train_2tower.py
+++ python/friesian/example/two_tower/train_2tower.py
@@ -200,6 +200,10 @@ if __name__ == '__main__':
                                init_ray_on_spark=True)
     elif args.cluster_mode == "spark-submit":
         sc = init_orca_context("spark-submit")
+    else:
+        raise ValueError(
+            "cluster_mode should be one of 'local', 'yarn', 'standalone' and 'spark-submit'"
+            ", but got " + args.cluster_mode)
 
     num_cols = ["enaging_user_follower_count", 'enaging_user_following_count',
                 "engaged_with_user_follower_count", "engaged_with_user_following_count",
