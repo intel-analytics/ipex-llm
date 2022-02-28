e29ce2264df70bf75813524ce64820fed616b417
Wang, Yang <yang3.wang@intel.com>
2022-02-28 05:31:01 +0000

    fix nano tf init threading config

--- python/nano/src/bigdl/nano/tf/__init__.py
+++ python/nano/src/bigdl/nano/tf/__init__.py
@@ -14,16 +14,9 @@
 # limitations under the License.
 #
 import platform
+import os
 if platform.system() != "Darwin":
     import tensorflow as tf
-    from bigdl.nano.common.cpu_schedule import schedule_workers, get_cpu_info
-
-    proc_list = schedule_workers(1)
-    _, get_socket = get_cpu_info()
-
-    num_sockets = len(set(get_socket.values()))
-    num_threads = len(proc_list[0]) // num_sockets
-
-    tf.config.threading.set_inter_op_parallelism_threads(num_sockets)
-    tf.config.threading.set_intra_op_parallelism_threads(num_threads)
+    tf.config.threading.set_inter_op_parallelism_threads(int(os.environ["NANO_TF_INTER_OP"]))
+    tf.config.threading.set_intra_op_parallelism_threads(int(os.environ["OMP_NUM_THREADS"]))
     tf.config.set_soft_device_placement(enabled=True)
