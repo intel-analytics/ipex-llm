fdb21fd9f648bc542d12eab4e868ce18e326a905
liangs6212 <80952198+liangs6212@users.noreply.github.com>
2022-02-28 15:57:22 +0800

    Chronos: Remove deprecated API in `MTNet` ut (#4122)
    * remove deprecated API in MTNet
    
    * remote redundant import
    
    * pep8
    
    * use TSDataset API in mtnet ut
    
    * change name, del self.y_test

--- python/chronos/test/bigdl/chronos/forecaster/test_mtnet_forecaster.py
+++ python/chronos/test/bigdl/chronos/forecaster/test_mtnet_forecaster.py
@@ -16,57 +16,59 @@
 
 import pytest
 import numpy as np
-from bigdl.chronos.autots.deprecated.feature.time_sequence import TimeSequenceFeatureTransformer
 import tensorflow as tf
 import pandas as pd
 
 from bigdl.chronos.forecaster.mtnet_forecaster import MTNetForecaster
+from bigdl.chronos.data import TSDataset
 from unittest import TestCase
 
+
+def create_data():
+    lookback = 5
+    horizon = 1
+    def get_data(num_samples):
+        values = np.random.randn(num_samples)
+        df = pd.DataFrame({'timestep': pd.date_range(start='2010-01-01',
+                                                     freq='m',
+                                                     periods=num_samples),
+                           'value 1': values,
+                           'value 2': values,
+                           'value 3': values,
+                           'value 4': values})
+        return df
+    tsdata_train = TSDataset.from_pandas(get_data(64),
+                                         target_col=['value 1', 'value 2', 'value 3', 'value 4'],
+                                         dt_col='timestep',
+                                         with_split=False) 
+    tsdata_test = TSDataset.from_pandas(get_data(16),
+                                        target_col=['value 1', 'value 2', 'value 3', 'value 4'],
+                                        dt_col='timestep',
+                                        with_split=False)
+    for tsdata in [tsdata_train, tsdata_test]:
+        tsdata.roll(lookback=lookback, horizon=horizon)
+    return tsdata_train, tsdata_test
 
 class TestChronosModelMTNetForecaster(TestCase):
 
     def setUp(self):
         tf.keras.backend.clear_session()
-        self.ft = TimeSequenceFeatureTransformer()
-        self.create_data()
 
     def tearDown(self):
         pass
 
-    def create_data(self):
-        def gen_train_sample(data, past_seq_len, future_seq_len):
-            data = pd.DataFrame(data)
-            x, y = self.ft._roll_train(data,
-                                       past_seq_len=past_seq_len,
-                                       future_seq_len=future_seq_len
-                                       )
-            return x, y
-
-        def gen_test_sample(data, past_seq_len):
-            test_data = pd.DataFrame(data)
-            x = self.ft._roll_test(test_data, past_seq_len=past_seq_len)
-            return x
-
-        self.long_num = 4
-        self.time_step = 1
-        look_back = (self.long_num + 1) * self.time_step
-        look_forward = 1
-        self.x_train, self.y_train = gen_train_sample(data=np.random.randn(
-            64, 4), past_seq_len=look_back, future_seq_len=look_forward)
-        self.x_val, self.y_val = gen_train_sample(data=np.random.randn(16, 4),
-                                                  past_seq_len=look_back,
-                                                  future_seq_len=look_forward)
-        self.x_test = gen_test_sample(data=np.random.randn(16, 4),
-                                      past_seq_len=look_back)
-
     def test_forecast_mtnet(self):
         # TODO hacking to fix a bug
-        target_dim = 1
-        model = MTNetForecaster(target_dim=target_dim,
+        train_data, test_data = create_data()
+        self.x_train, y_train = train_data.to_numpy()
+        self.y_train = y_train[:, :, 0]
+        self.x_val, y_val = test_data.to_numpy()
+        self.y_val = y_val[:, :, 0]
+        self.x_test, _ = test_data.to_numpy()
+        model = MTNetForecaster(target_dim=1,
                                 feature_dim=self.x_train.shape[-1],
-                                long_series_num=self.long_num,
-                                series_length=self.time_step
+                                long_series_num=4,
+                                series_length=1
                                 )
         x_train_long, x_train_short = model.preprocess_input(self.x_train)
         x_val_long, x_val_short = model.preprocess_input(self.x_val)
@@ -79,7 +81,7 @@ class TestChronosModelMTNetForecaster(TestCase):
                   distributed=False)
         assert model.evaluate([x_val_long, x_val_short], self.y_val)
         predict_result = model.predict([x_test_long, x_test_short])
-        assert predict_result.shape == (self.x_test.shape[0], target_dim)
+        assert predict_result.shape == (self.x_test.shape[0], 1)
 
 
 if __name__ == "__main__":
--- python/chronos/test/bigdl/chronos/model/test_mtnet.py
+++ python/chronos/test/bigdl/chronos/model/test_mtnet.py
@@ -19,22 +19,52 @@ import pytest
 
 from bigdl.orca.test_zoo_utils import ZooTestCase
 from bigdl.chronos.model.MTNet_keras import MTNetKeras
-from bigdl.chronos.autots.deprecated.feature.time_sequence import TimeSequenceFeatureTransformer
+from bigdl.chronos.data import TSDataset
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 from numpy.testing import assert_array_almost_equal
 
 
+def create_data():
+    lookback = 3
+    horizon = 1
+    def get_data(num_samples):
+        values = np.random.randn(num_samples)
+        df = pd.DataFrame({'timestep': pd.date_range(start='2010-01-01',
+                                                     freq='m',
+                                                     periods=num_samples),
+                           'value 1': values,
+                           'value 2': values,
+                           'value 3': values,
+                           'value 4': values})
+        return df
+    tsdata_train = TSDataset.from_pandas(get_data(32),
+                                         target_col=['value 1', 'value 2', 'value 3', 'value 4'],
+                                         dt_col='timestep',
+                                         with_split=False) 
+    tsdata_test = TSDataset.from_pandas(get_data(16),
+                                        target_col=['value 1', 'value 2', 'value 3', 'value 4'],
+                                        dt_col='timestep',
+                                        with_split=False)
+    for tsdata in [tsdata_train, tsdata_test]:
+        tsdata.roll(lookback=lookback, horizon=horizon)
+    return tsdata_train, tsdata_test
+
+
 class TestMTNetKeras(ZooTestCase):
 
     def setup_method(self, method):
         tf.keras.backend.clear_session()
-        self.ft = TimeSequenceFeatureTransformer()
-        self.create_data()
+        train_data, test_data = create_data()
+        self.x_train, y_train = train_data.to_numpy()
+        self.y_train = y_train[:, :, 0]
+        self.x_val, y_val = test_data.to_numpy()
+        self.y_val = y_val[:, :, 0]
+        self.x_test, _ = test_data.to_numpy()
         self.model = MTNetKeras()
-        self.config = {"long_num": self.long_num,
-                       "time_step": self.time_step,
+        self.config = {"long_num": 2,
+                       "time_step": 1,
                        "ar_window": 1, # np.random.randint(1, 3),
                        "cnn_height": 1, # np.random.randint(1, 3),
                        "cnn_hid_size": 2,
@@ -43,32 +73,6 @@ class TestMTNetKeras(ZooTestCase):
 
     def teardown_method(self, method):
         pass
-
-    def create_data(self):
-        def gen_train_sample(data, past_seq_len, future_seq_len):
-            data = pd.DataFrame(data)
-            x, y = self.ft._roll_train(data,
-                                       past_seq_len=past_seq_len,
-                                       future_seq_len=future_seq_len
-                                       )
-            return x, y
-
-        def gen_test_sample(data, past_seq_len):
-            test_data = pd.DataFrame(data)
-            x = self.ft._roll_test(test_data, past_seq_len=past_seq_len)
-            return x
-
-        self.long_num = 2
-        self.time_step = 1
-        look_back = (self.long_num + 1) * self.time_step
-        look_forward = 1
-        self.x_train, self.y_train = gen_train_sample(data=np.random.randn(
-            32, 4), past_seq_len=look_back, future_seq_len=look_forward)
-        self.x_val, self.y_val = gen_train_sample(data=np.random.randn(16, 4),
-                                                  past_seq_len=look_back,
-                                                  future_seq_len=look_forward)
-        self.x_test = gen_test_sample(data=np.random.randn(16, 4),
-                                      past_seq_len=look_back)
 
     def test_fit_evaluate(self):
         self.model.fit_eval(data=(self.x_train, self.y_train),
