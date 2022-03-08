c19fc193c5f4e1f1889ea27d311aee0fbae48fd4
Jiaming <litchy233@gmail.com>
2022-03-08 10:12:02 +0800

    ppml python api

---
+++ python/ppml/src/bigdl/ppml/algorithms/hfl_nn.py
@@ -0,0 +1,50 @@
+#
+# Copyright 2021 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from bigdl.dllib.utils.common import JavaValue
+
+
+class HflNN(JavaValue):
+    """
+    HFL NN class, users could build custom NN structure in this class
+    """
+    def __init__(self, jvalue, *args):
+        bigdl_type = "float"
+        super(JavaValue, self).__init__(jvalue, bigdl_type, *args)
+
+    def fit(self, x, y, epochs):
+        """
+        :param x: data, could be Numpy NdArray or Pandas DataFrame
+        :param y: label, could be Numpy NdArray or Pandas DataFrame
+        :param epochs: training epochs
+        :return:
+        """
+        pass
+
+    def evaluate(self, x, y):
+        """
+        :param x: data, could be Numpy NdArray or Pandas DataFrame
+        :param y: label, could be Numpy NdArray or Pandas DataFrame
+        :return:
+        """
+        pass
+
+    def predict(self, x):
+        """
+        :param x: data, could be Numpy NdArray or Pandas DataFrame
+        :return:
+        """
+        pass
