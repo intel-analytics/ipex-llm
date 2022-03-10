7871d6eb9aec1cdff09b50c1048826b067c5527d
sgwhat <ge.song@intel.com>
2022-03-10 19:22:11 +0800

    support ray dataset in pytorch_ray_worker

--- python/orca/src/bigdl/orca/learn/pytorch/pytorch_ray_worker.py
+++ python/orca/src/bigdl/orca/learn/pytorch/pytorch_ray_worker.py
@@ -112,14 +112,17 @@ class PytorchRayWorker(TorchRunner):
         self._create_schedulers_if_available()
         self._create_loss()
 
-    def predict(self, data_creator, batch_size=32, profile=False):
+    def predict(self, data_creator, batch_size=32, profile=False, label_cols=None):
         """Evaluates the model on the validation data set."""
         config = self.config.copy()
         self._toggle_profiling(profile=profile)
 
         shards_ref = data_creator(config, batch_size)
-        if not isinstance(shards_ref, ray.ObjectID):
-            raise ValueError("Only xshards is supported for predict")
-
-        partition = ray.get(shards_ref)
-        return super().predict(partition=partition, batch_size=batch_size, profile=profile)
+        if isinstance(shards_ref, ray.data.Dataset):
+            partition = shards_ref
+        else:
+            if not isinstance(shards_ref, ray.ObjectID):
+                raise ValueError("Only xshards is supported for predict")
+            partition = ray.get(shards_ref)
+        return super().predict(partition=partition, batch_size=batch_size, profile=profile,
+                               label_cols=label_cols)
