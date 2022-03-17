136fac9474f9af4b79a3e078fb572e8d04087894
Qiyuan Gong <qiyuan.gong@intel.com>
2022-03-17 13:44:12 +0800

    Add README & API

---
+++ scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/attestation/GramineQuoteGeneratorImpl.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2021 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.ppml.attestation
+
+import org.apache.logging.log4j.LogManager
+
+class GramineQuoteGeneratorImpl extends QuoteGenerator {
+
+  val logger = LogManager.getLogger(getClass)
+
+  override def getQuote(): Unit = {
+    // TODO Gramine Quote https://graphene.readthedocs.io/en/latest/attestation.html
+    logger.debug("Write user report to /dev/attestation/user_report_data")
+    logger.debug("Read quote from /dev/attestation/quote")
+  }
+}
