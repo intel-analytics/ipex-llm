231e268e9ab13ffc077befa216ffbd50f7528e8d
Chen, Zhentao <zhentao.chen@intel.com>
2022-03-22 16:30:24 +0800

    apply quantization

--- python/nano/example/quantization/neural-compressor/pytorch/mnist/mnist.py
+++ python/nano/example/quantization/neural-compressor/pytorch/mnist/mnist.py
@@ -54,7 +54,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-
+from bigdl.nano.pytorch.trainer import Trainer
+from torchmetrics import Accuracy
 
 class Net(nn.Module):
     def __init__(self):
@@ -80,42 +81,6 @@ class Net(nn.Module):
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
-
-
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
-            if args.dry_run:
-                break
-
-
-def test(model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    test_loss /= len(test_loader.dataset)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
 
 
 def main():
@@ -170,12 +135,15 @@ def main():
 
     model = Net().to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
-
+    loss = nn.NLLLoss(reduction='sum')
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
-        scheduler.step()
+    trainer = Trainer(max_epochs=args.epochs + 1)
+    pl_model = trainer.compile(model, loss=loss, optimizer=optimizer,
+                               scheduler=scheduler, metrics=[Accuracy()])
+    trainer.fit(pl_model, train_loader)
+    test_out = trainer.test(pl_model, test_loader)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {:.2f}%\n'.format(
+        test_out[0]['test/loss'], test_out[0]['test/Accuracy']*100))
 
     if args.save_model:
         torch.save(model.state_dict(), "mnist_cnn.pt")
