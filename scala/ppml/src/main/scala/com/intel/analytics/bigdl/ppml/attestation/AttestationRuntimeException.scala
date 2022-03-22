a02dc37f113455d4712e79809d20701380391d5d
Qiyuan Gong <qiyuan.gong@intel.com>
2022-03-22 23:42:32 +0800

    Fix  GramineQuoteGeneratorImpl code style

--- scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/attestation/AttestationRuntimeException.scala
+++ scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/attestation/AttestationRuntimeException.scala
@@ -16,5 +16,6 @@
 
 package com.intel.analytics.bigdl.ppml.attestation
 
-class AttestationRuntimeException(message: String, cause: Throwable = null) extends Exception(message, cause) {
+class AttestationRuntimeException(message: String, cause: Throwable = null)
+  extends Exception(message, cause) {
 }
