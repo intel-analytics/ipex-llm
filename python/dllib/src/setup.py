2118e06f116b67f1551d65be6024ec0bdcb67a67
qiuxin2012 <qiuxin2012cs@gmail.com>
2022-03-23 15:26:34 +0800

    update

--- python/dllib/src/setup.py
+++ python/dllib/src/setup.py
@@ -96,7 +96,9 @@ def setup_package():
         url='https://github.com/intel-analytics/BigDL',
         packages=get_bigdl_packages(),
         scripts=scripts,
-        install_requires=['numpy>=1.19.5', 'pyspark==2.4.6', 'six>=1.10.0', 'bigdl-core==2.1.0b20220321'],
+        install_requires=[
+            'numpy>=1.19.5', 'pyspark==2.4.6',
+            'six>=1.10.0', 'bigdl-core==2.1.0b20220321'],
         dependency_links=['https://d3kbcqa49mib13.cloudfront.net/spark-2.0.0-bin-hadoop2.7.tgz'],
         include_package_data=True,
         package_data={"bigdl.share.dllib": ['lib/bigdl-dllib*.jar', 'conf/*',
