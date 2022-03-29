2d82a2e70ea6350f11cd02098ccd64ad2a9f8a2c
sgwhat <ge.song@intel.com>
2022-03-29 14:43:28 +0800

    code style

--- python/orca/src/bigdl/orca/learn/tf2/ray_estimator.py
+++ python/orca/src/bigdl/orca/learn/tf2/ray_estimator.py
@@ -33,6 +33,7 @@ from bigdl.orca.ray import RayContext
 
 logger = logging.getLogger(__name__)
 
+
 class TensorFlow2Estimator(OrcaRayEstimator):
     def __init__(self,
                  model_creator,
