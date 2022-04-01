d4e576bccece185866fbda2b5fece2ee4491f7bd
DirkFi <1394997184@qq.com>
2022-04-01 11:54:10 +0800

    Changes to be committed: 	modified:   python/nano/test/run-nano-pytorch-ray-tests.sh

--- python/nano/test/run-nano-pytorch-ray-tests.sh
+++ python/nano/test/run-nano-pytorch-ray-tests.sh
@@ -2,14 +2,14 @@
 
 export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
-export PYTORCH_NANO_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/test/pytorch
+export PYTORCH_NANO_RAY_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/test/ray
 
 set -e
 
 # ipex is not installed here. Any tests needs ipex should be moved to next pytest command.
 echo "# Start testing"
 start=$(date "+%s")
-python -m pytest ./ray/test_ray_trainer.py 
+python -m pytest -s ${PYTORCH_NANO_RAY_TEST_DIR}/test_ray_trainer.py 
 
 now=$(date "+%s")
 time=$((now-start))
