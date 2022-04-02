8c57e58740c1040d4348a2e75950a9be11b253fa
Kai Huang <huangkaivision@gmail.com>
2022-04-02 08:25:19 +0800

    revert sh

--- scripts/spark-submit-with-dllib.sh
+++ scripts/spark-submit-with-dllib.sh
@@ -12,26 +12,29 @@ if [ -z "${SPARK_HOME}" ]; then
 fi
 
 #setup paths
-export BIGDL_JARS="${BIGDL_HOME}/jars/*" | sed 's/ /,/g'
 export BIGDL_CONF=${BIGDL_HOME}/conf/spark-bigdl.conf
 export BIGDL_PY_ZIP=`find ${BIGDL_HOME}/python -name bigdl-spark_*-python-api.zip`
 
 # Check files
 if [ ! -f ${BIGDL_CONF} ]; then
-    echo "Cannot find ${BIGDL_CONF}"
+    "Cannot find BigDL configuration file, please check your BigDL download"
     exit 1
 fi
 
-#if [ ! -f $BIGDL_JAR ]; then
-#    echo "Cannot find $BIGDL_JAR"
-#    exit 1
-#fi
+if [ ! -f ${BIGDL_PY_ZIP} ]; then
+    "Cannot find BigDL python zip file, please check your BigDL download"
+    exit 1
+fi
+
+if [ ! -d ${BIGDL_HOME}/jars ]; then
+    echo "Cannot find BigDL jar files, please check your BigDL download"
+    exit 1
+fi
 
 
 ${SPARK_HOME}/bin/spark-submit \
   --properties-file ${BIGDL_CONF} \
   --py-files ${BIGDL_PY_ZIP} \
-  --jars ${BIGDL_JARS} \
   --conf spark.driver.extraClassPath=${BIGDL_HOME}/jars/* \
   --conf spark.executor.extraClassPath=${BIGDL_HOME}/jars/* \
   $*
