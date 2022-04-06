40b065838da794869d99a9a43de6a4d79e2531e1
sgwhat <ge.song@intel.com>
2022-04-06 13:42:51 +0800

    add test script for cifar10 and super_resolution

--- python/orca/dev/example/run-example-test-ray-runtime.sh
+++ python/orca/dev/example/run-example-test-ray-runtime.sh
@@ -9,8 +9,11 @@ clear_up () {
 
 set -e
 
-echo "#1start orca pytorch example tests"
+ray stop -f
+ray start --head
 
+echo "#start orca pytorch example tests"
+echo "#1 start example for orca fashion-mnist"
 start=$(date "+%s")
 
 if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/data ]
@@ -29,3 +32,41 @@ python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mni
     --runtime ray --backend torch_distributed --batch_size=256
 now=$(date "+%s")
 time1=$((now-start))
+
+
+echo "#2 start example for orca cifar10"
+start=$(date "+%s")
+
+if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/data ]; then
+  echo "Cifar10 already exists"
+else
+  wget -nv $FTP_URI/analytics-zoo-data/cifar10.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10
+  unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.zip
+fi
+
+python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.py \
+    --runtime ray --backend torch_distributed --batch_size=256
+
+now=$(date "+%s")
+time2=$((now-start))
+
+ray stop -f
+ray start --head
+
+echo "#3 start example for orca super-resolution"
+start=$(date "+%s")
+
+if [ ! -f BSDS300-images.tgz ]; then
+  wget -nv $FTP_URI/analytics-zoo-data/BSDS300-images.tgz
+fi
+if [ ! -d dataset/BSDS300/images ]; then
+  mkdir dataset
+  tar -xzf BSDS300-images.tgz -C dataset
+fi
+
+python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py --runtime ray --backend torch_distributed
+
+now=$(date "+%s")
+time3=$((now-start))
+
+ray stop -f
