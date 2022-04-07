210a7f10390f8cd4747bc1bfc2111366b0437c01
lwf <xtu_fan@163.com>
2022-04-07 15:02:36 +0800

    change download link of CIFAR10

--- python/nano/test/run-nano-pytorch-ray-tests.sh
+++ python/nano/test/run-nano-pytorch-ray-tests.sh
@@ -3,7 +3,8 @@
 export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
 export NANO_RAY_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/test/ray
-export PYTHONPATH=$PYTHONPATH:/home/wym/my_ray/BigDL/python/nano
+
+wget -nv ${FTP_URI}/analytics-zoo-data/cifar-10-python.tar.gz -P ${NANO_RAY_TEST_DIR}/pytorch/data
 
 set -e
 
