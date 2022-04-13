b169528d346d5348fcc341b25dd24761368c25a9
lwf <xtu_fan@163.com>
2022-04-13 12:07:51 +0800

    fix bug

--- python/nano/setup.py
+++ python/nano/setup.py
@@ -66,11 +66,13 @@ def download_libs(url: str):
 
 def setup_package():
 
+
     if VERSION.endswith("Mac"):
         require_tensorflow = "tensorflow==2.7.0"
         version = VERSION.strip("-Mac")
     else:
         require_tensorflow = "intel-tensorflow==2.7.0"
+        version = VERSION
 
     tensorflow_requires = [require_tensorflow,
                            "keras==2.7.0",
