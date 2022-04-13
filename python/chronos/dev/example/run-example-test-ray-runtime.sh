d639332d2730de2310571f31fef6df00fc6d7306
sgwhat <ge.song@intel.com>
2022-04-13 20:19:23 +0800

    add chronos test script

---
+++ python/chronos/dev/example/run-example-test-ray-runtime.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+clear_up () {
+    echo "Clearing up environment. Uninstalling bigdl"
+    pip uninstall -y bigdl-chronos
+    pip uninstall -y bigdl-orca
+    pip uninstall -y bigdl-dllib
+    pip uninstall -y pyspark
+}
+#if image exist this two dependency, remove below
+execute_ray_test(){
+    echo "start example $1"
+    start=$(date "+%s")
+    python $2
+    exit_status=$?
+    if [ $exit_status -ne 0 ];
+    then
+        clear_up
+        echo "$1 failed"
+        exit $exit_status
+    fi
+    now=$(date "+%s")
+    return $((now-start))
+}
+
+if [ ! -f ~/.chronos/dataset/network_traffic/network_traffic_data.csv ]; then
+  wget -nv $FTP_URI/analytics-zoo-data/network-traffic/data/data.csv -P ~/.chronos/dataset/network_traffic/
+  mv ~/.chronos/dataset/network_traffic/data.csv ~/.chronos/dataset/network_traffic/network_traffic_data.csv
+fi
+
+execute_ray_test distributed_training_network_traffic "${BIGDL_ROOT}/python/chronos/example/distributed/distributed_training_network_traffic.py"
+time1=$?
