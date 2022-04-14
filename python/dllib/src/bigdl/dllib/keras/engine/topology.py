943accc894d8c6efd74c6fde4e89514cd1d0ff35
dding3 <ding.ding@intel.com>
2022-04-14 16:12:16 -0700

    return string for summary

--- python/dllib/src/bigdl/dllib/keras/engine/topology.py
+++ python/dllib/src/bigdl/dllib/keras/engine/topology.py
@@ -500,10 +500,12 @@ class KerasNet(ZooKerasLayer):
                    If the field has a larger length, the remaining part will be trimmed.
                    If the field has a smaller length, the remaining part will be white spaces.
         """
-        callZooFunc(self.bigdl_type, "zooKerasNetSummary",
+        res = callZooFunc(self.bigdl_type, "zooKerasNetSummary",
                     self.value,
                     line_length,
                     [float(p) for p in positions])
+        print(res)
+        return res
 
     def to_model(self):
         from bigdl.dllib.keras.models import Model
