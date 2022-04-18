eeb7900a7be568cca878b118cd8cd7ee6b6a3a57
Yang Wang <yang3.wang@intel.com>
2022-04-18 16:56:20 +0800

    Downgrade torchmetrics in setup.py (#4435)

--- python/nano/test/run-nano-pytorch-tests-ipex.sh
+++ python/nano/test/run-nano-pytorch-tests-ipex.sh
@@ -5,8 +5,6 @@ export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
 export PYTORCH_NANO_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/test/pytorch
 
 wget -nv ${FTP_URI}/analytics-zoo-data/cifar-10-python.tar.gz -P ${PYTORCH_NANO_TEST_DIR}/data
-# temporary fix on torchmetrics, downgrade to 0.7.2
-pip install torchmetrics==0.7.2 --upgrade
 
 set -e
 
