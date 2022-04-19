31443f34934fc0ccfff5ed0de3b4b3dfaf74e4d3
sgwhat <ge.song@intel.com>
2022-04-19 10:43:33 +0800

    update code format

--- python/orca/dev/example/run-example-test-ray-runtime.sh
+++ python/orca/dev/example/run-example-test-ray-runtime.sh
@@ -29,14 +29,12 @@ ray stop -f
 # else
 #   wget -nv $FTP_URI/analytics-zoo-data/yolov3/coco.names -P tmp
 # fi
-
 # if [ -f tmp/VOCdevkit.zip ]; then
 #   echo "tmp/VOCdevkit.zip already exists."
 # else
 #   wget -nv $FTP_URI/analytics-zoo-data/yolov3/VOCdevkit.zip -P tmp
 #   unzip -q tmp/VOCdevkit.zip -d tmp/VOCdevkit
 # fi
-
 # if [ -f models/checkpoints.zip ]; then
 #   echo "models/checkpoints already exists."
 # else
@@ -44,23 +42,21 @@ ray stop -f
 #     -P models
 #   unzip -q models/checkpoints.zip -d models
 # fi
-
 # echo "yolov3 predict"
 # start=$(date "+%s")
 # python ${BIGDL_ROOT}/python/orca/example/learn/tf2/yolov3/predict.py \
 #   --checkpoint models/checkpoints/yolov3.tf \
 #   --names tmp/coco.names --class_num 80 \
 #   --image tmp/VOCdevkit/VOCdevkit/VOC2007/JPEGImages/000005.jpg
-
 # now=$(date "+%s")
 # time2=$((now-start))
 
 # ray stop -f
 ray start --head
+
 echo "#start orca pytorch example tests"
 echo "#3 start example for orca fashion-mnist"
 start=$(date "+%s")
-
 if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/data ]
 then
     echo "fashion-mnist already exists"
@@ -68,7 +64,6 @@ else
     wget -nv $FTP_URI/analytics-zoo-data/data/fashion-mnist.zip -P ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/
     unzip ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion-mnist.zip
 fi
-
 sed "s/epochs=5/epochs=1/g;s/batch_size=4/batch_size=256/g" \
     ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist.py \
     > ${BIGDL_ROOT}/python/orca/example/learn/pytorch/fashion_mnist/fashion_mnist_tmp.py
@@ -79,9 +74,7 @@ now=$(date "+%s")
 time3=$((now-start))
 
 echo "#4 start example for orca cifar10"
-
 start=$(date "+%s")
-
 if [ -d ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/data ]; then
   echo "Cifar10 already exists"
 else
@@ -91,16 +84,11 @@ fi
 
 python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/cifar10/cifar10.py \
     --runtime ray --address localhost:6379 --backend ray --batch_size 256 --epochs 1
-
 now=$(date "+%s")
 time4=$((now-start))
 
-ray stop -f
-ray start --head
-
 echo "#5 start example for orca super-resolution"
 start=$(date "+%s")
-
 if [ ! -f BSDS300-images.tgz ]; then
   wget -nv $FTP_URI/analytics-zoo-data/BSDS300-images.tgz
 fi
@@ -108,12 +96,9 @@ if [ ! -d dataset/BSDS300/images ]; then
   mkdir dataset
   tar -xzf BSDS300-images.tgz -C dataset
 fi
-
 python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/super_resolution/super_resolution.py \
   --runtime ray --address localhost:6379 --backend ray
-
 now=$(date "+%s")
 time5=$((now-start))
 
 ray stop -f
-
