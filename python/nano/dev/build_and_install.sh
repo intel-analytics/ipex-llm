0c58ff6daaeca4db5db15af575577629253afc01
lwf <xtu_fan@163.com>
2022-04-19 10:04:06 +0800

    add build_and_install.sh

---
+++ python/nano/dev/build_and_install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+RUN_SCRIPT_DIR=$(cd $(dirname $0) ; pwd)
+echo $RUN_SCRIPT_DIR
+BIGDL_DIR="$(cd ${RUN_SCRIPT_DIR}/../../..; pwd)"
+echo $BIGDL_DIR
+WHL_DIR="$(cd ${BIGDL_DIR}/python/nano/src; pwd)"
+echo WHL_DIR
+
+if (( $# < 4)); then
+  echo "Usage: release.sh platform version upload framework"
+  echo "Usage example: bash release.sh linux default true pytorch"
+  echo "Usage example: bash release.sh mac 0.14.0.dev1 false tensorflow"
+  exit -1
+fi
+
+platform=$1
+version=$2
+upload=$3  # Whether to upload the whl to pypi
+
+bash ${RUN_SCRIPT_DIR}/release.sh ${platform} ${version} ${upload}
+
+cd ${WHL_DIR}
+
+whl_name="ls dist";pip install ${whl_name}[${framework}]
+
