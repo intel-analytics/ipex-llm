00da39e196c38d0862c3dd593ee5f9d734593abb
Weifan <xtu_fan@163.com>
2022-04-20 15:40:26 +0800

    add pip install options for build_and_install.sh (#4453)
    * add nano typecheck
    
    * add nano dependency
    
    * fix " invalid command 'bdist_wheel' "
    
    * fix
    
    * try to fix
    
    * add tensorflow typecheck
    
    * fix style
    
    * uninstall bigdl-nao
    
    * try
    
    * recovery
    
    * add build_and_install.sh
    
    * fix whl_name
    
    * fix command
    
    * modify github action
    
    * A try for chronos error
    
    * A try for chronos error
    
    * A try for chronos error
    
    * add install options
    
    * add pip install options
    
    * add pip install options
    
    * using pip3
    
    * del pip3

--- python/nano/dev/build_and_install.sh
+++ python/nano/dev/build_and_install.sh
@@ -17,15 +17,12 @@
 
 set -e
 RUN_SCRIPT_DIR=$(cd $(dirname $0) ; pwd)
-echo $RUN_SCRIPT_DIR
 BIGDL_DIR="$(cd ${RUN_SCRIPT_DIR}/../../..; pwd)"
-echo $BIGDL_DIR
 WHL_DIR="$(cd ${BIGDL_DIR}/python/nano/src; pwd)"
-echo WHL_DIR
 
 if (( $# < 4)); then
-  echo "Usage: build_and_install.sh platform version upload framework"
-  echo "Usage example: bash build_and_install.sh linux default true pytorch"
+  echo "Usage: build_and_install.sh platform version upload framework pip_install_options[optional]"
+  echo "Usage example: bash build_and_install.sh linux default true pytorch --force-reinstall"
   echo "Usage example: bash build_and_install.sh mac 0.14.0.dev1 false tensorflow"
   exit -1
 fi
@@ -34,10 +31,12 @@ platform=$1
 version=$2
 upload=$3  # Whether to upload the whl to pypi
 framework=$4
+install_options=${@:5:$((${#@}))}
 
 bash ${RUN_SCRIPT_DIR}/release.sh ${platform} ${version} ${upload}
 
 cd ${WHL_DIR}
+whl_name=`ls dist`
+pip install $install_options dist/${whl_name}[${framework}]
 
-whl_name=`ls dist`;pip install dist/${whl_name}[${framework}]
 
