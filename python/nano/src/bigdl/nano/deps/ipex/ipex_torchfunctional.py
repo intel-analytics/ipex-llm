18457a9b9c117a46c312b9ac31f0f7539db19bc7
Django-Jiang <1205034803@qq.com>
2022-04-21 14:28:08 +0800

    rm ipex save import

--- python/nano/src/bigdl/nano/deps/ipex/ipex_torchfunctional.py
+++ python/nano/src/bigdl/nano/deps/ipex/ipex_torchfunctional.py
@@ -28,14 +28,14 @@ _torch_save = torch.save
 
 # To replace torch.save in ipex, you need to import and exec their __init__.py first.
 # And then you can replace torch.save with your customized function.
-try:
-    from intel_pytorch_extension.ops.save import *
-except ImportError:
-    warning("IPEXAccelerator requires intel_pytorch_extension installed, \
-    please run `pip install torch_ipex -f https://software.intel.com/ipex-whl-stable` \
-    to get IPEX ready.")
-    # process needs to stop here
-    raise ImportError
+# try:
+#     from intel_pytorch_extension.ops.save import *
+# except ImportError:
+#     warning("IPEXAccelerator requires intel_pytorch_extension installed, \
+#     please run `pip install torch_ipex -f https://software.intel.com/ipex-whl-stable` \
+#     to get IPEX ready.")
+#     # process needs to stop here
+#     raise ImportError
 
 
 def replace_torch_function(function_name: str, replace_func: Callable):
