2a28837aed3e7fffb45391f6fba8ea522772f4e8
Django-Jiang <1205034803@qq.com>
2022-04-21 16:00:24 +0800

    compile refactor

--- python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
+++ python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
@@ -42,6 +42,7 @@ from bigdl.nano.deps.neural_compressor.inc_api import QuantizationINC, PytorchQu
     check_pytorch_dataloaders, load_inc_model
 
 
+
 distributed_backends = ["spawn", "ray", "subprocess"]
 
 
@@ -175,12 +176,11 @@ class Trainer(pl.Trainer):
                 "Loss and optimizer should be None if model is a pytorch-lightning model."
             pl_model = model
         else:
-            if use_ipex:
+            if use_ipex: 
                 if enable_bf16:
-                    model, optimizer = ipex_optimize(model, optimizer=optimizer,
-                                                     dtype=torch.bfloat16)
+                    model, optimizer= ipex_optimize(model, optimizer=optimizer, dtype=torch.bfloat16)
                 else:
-                    model, optimizer = ipex_optimize(model, optimizer=optimizer)
+                    model, optimizer= ipex_optimize(model, optimizer=optimizer)
             pl_model = LightningModuleFromTorch(model, loss, optimizer, scheduler, metrics)
 
         return pl_model
