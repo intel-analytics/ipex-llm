b88efa9b4fab2d299546d1563eb77d2a824aa87e
Diankun An <54262787+piaolaidelangman@users.noreply.github.com>
2022-04-26 16:49:33 +0800

    [PPML] Add Occlum Attestation to BigDL (#4507)
    * Update `Dockerfile` to build attestation dependency.
    * Update `README.md`.
    * Add start server scripts.
    * Update `run_spark_on_occlum_glibc.sh` for attestation.

--- ppml/trusted-big-data-ml/scala/docker-occlum/start-occlum-attestation-server.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/start-occlum-attestation-server.sh
@@ -1,24 +1,18 @@
 # Clean up old container
-sudo docker rm -f bigdl-ppml-trusted-big-data-ml-scala-occlum
+sudo docker rm -f bigdl-ppml-trusted-big-data-ml-scala-occlum-attestation-server
 
 # Run new command in container
 sudo docker run -it \
 	--net=host \
-	--name=bigdl-ppml-trusted-big-data-ml-scala-occlum \
-	--cpuset-cpus 10-14 \
+	--name=bigdl-ppml-trusted-big-data-ml-scala-occlum-attestation-server \
+	--cpuset-cpus 5-7 \
 	--device=/dev/sgx/enclave \
 	--device=/dev/sgx/provision \
 	-v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
-	-v data:/opt/occlum_spark/data \
 	-e LOCAL_IP=$LOCAL_IP \
-	-e SGX_MEM_SIZE=24GB \
-	-e SGX_THREAD=512 \
-	-e SGX_HEAP=512MB \
-	-e SGX_KERNEL_HEAP=1GB \
-	-e SGX_MMAP=10GB \
+	-e SGX_MEM_SIZE=8GB \
 	-e PCCS_URL=$PCCS_URL \
-	-e ATTESTATION=false \
 	-e ATTESTATION_SERVER_IP=$ATTESTATION_SERVER_IP \
 	-e ATTESTATION_SERVER_PORT=$ATTESTATION_SERVER_PORT \
 	intelanalytics/bigdl-ppml-trusted-big-data-ml-scala-occlum:2.1.0-SNAPSHOT \
-	bash /opt/run_spark_on_occlum_glibc.sh $1 && tail -f /dev/null
+	bash /root/demos/remote_attestation/init_ra_flow/run_attestation_server.sh
