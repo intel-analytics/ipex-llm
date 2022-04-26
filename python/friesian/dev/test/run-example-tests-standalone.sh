e4a1372eee5889865b3c29a89df017dd51024a4e
pinggao187 <ping.gao3@pactera.com>
2022-04-26 15:08:56 +0800

    add friesian example test standalone

---
+++ python/friesian/dev/test/run-example-tests-standalone.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+export FTP_URI=$FTP_URI
+
+
+set -e
+mkdir -p result
+echo "#1 start example test for dien preprocessing inference"
+#timer
+start=$(date "+%s")
+if [ -f data/test.json ]; then
+  echo "data/test.json already exists"
+else
+  wget -nv $FTP_URI/analytics-zoo-data/test.json -P data
+fi
+if [ -f data/test ]; then
+  echo "data/test already exists"
+else
+  wget -nv $FTP_URI/analytics-zoo-data/test -P data
+fi
+
+python ../../example/dien/preprocessing_inference.py \
+    --cluster mode=standalone \
+    --executor_cores 6 \
+    --executor_memory 50g \
+    --input_meta ./data/test \
+    --input_transaction ./data/test.json \
+    --output ./result/
+
+now=$(date "+%s")
+time1=$((now - start))
+
+rm -rf data
+rm -rf result
+
+echo "#1 dien preprocessing inference time used: $time1 seconds"
