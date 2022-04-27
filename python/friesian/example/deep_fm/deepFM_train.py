7a919dbddf01f43b38fa4b8b49a7e294a64e9ec1
Kai Huang <huangkaivision@gmail.com>
2022-04-27 20:01:18 +0800

    move model

--- python/friesian/example/deep_fm/deepFM_train.py
+++ python/friesian/example/deep_fm/deepFM_train.py
@@ -14,14 +14,11 @@
 # limitations under the License.
 #
 
-# from deepctr_torch.inputs import SparseFeat, DenseFeat, get_feature_names
-# from deepctr_torch.models.deepfm import *
-# from deepctr_torch.models.basemodel import *
-from bigdl.friesian.feature import FeatureTable
+import argparse
 from bigdl.orca import init_orca_context, stop_orca_context
 from bigdl.orca.learn.pytorch import Estimator
 from bigdl.orca.learn.metrics import Accuracy, AUC
-import argparse
+from bigdl.friesian.feature import FeatureTable
 
 spark_conf = {"spark.network.timeout": "10000000",
               "spark.sql.broadcastTimeout": "7200",
@@ -38,56 +35,6 @@ spark_conf = {"spark.network.timeout": "10000000",
               "spark.eventLog.enabled": "true",
               "spark.app.name": "recsys-2tower",
               "spark.executor.memoryOverhead": "120g"}
-
-
-# class DeepFM(BaseModel):
-#     def __init__(self,
-#                  linear_feature_columns, dnn_feature_columns, use_fm=True,
-#                  dnn_hidden_units=(1024, 512, 128), l2_reg_linear=0.00001, l2_reg_embedding=0.00001,
-#                  l2_reg_dnn=0, init_std=0.0001, seed=1024, dnn_dropout=0, dnn_activation='relu',
-#                  dnn_use_bn=False, task='binary', device='cpu', gpus=None):
-#
-#         super(DeepFM, self).__init__(linear_feature_columns, dnn_feature_columns,
-#                                      l2_reg_linear=l2_reg_linear, l2_reg_embedding=l2_reg_embedding,
-#                                      init_std=init_std, seed=seed, task=task,
-#                                      device=device, gpus=gpus)
-#
-#         self.use_fm = use_fm
-#         self.use_dnn = len(dnn_feature_columns) > 0 and len(dnn_hidden_units) > 0
-#         if use_fm:
-#             self.fm = FM()
-#
-#         if self.use_dnn:
-#             self.dnn = DNN(self.compute_input_dim(dnn_feature_columns), dnn_hidden_units,
-#                            activation=dnn_activation, l2_reg=l2_reg_dnn, dropout_rate=dnn_dropout,
-#                            use_bn=dnn_use_bn, init_std=init_std, device=device)
-#             self.dnn_linear = nn.Linear(dnn_hidden_units[-1], 1, bias=False).to(device)
-#
-#             self.add_regularization_weight(
-#                 filter(lambda x: 'weight' in x[0] and 'bn' not in x[0],
-#                        self.dnn.named_parameters()), l2=l2_reg_dnn)
-#             self.add_regularization_weight(self.dnn_linear.weight, l2=l2_reg_dnn)
-#         self.to(device)
-#
-#     def forward(self, X):
-#         sparse_embedding_list, dense_value_list = self\
-#             .input_from_feature_columns(X, self.dnn_feature_columns, self.embedding_dict)
-#         logit = self.linear_model(X)
-#
-#         if self.use_fm and len(sparse_embedding_list) > 0:
-#             fm_input = torch.cat(sparse_embedding_list, dim=1)
-#             logit += self.fm(fm_input)
-#
-#         if self.use_dnn:
-#             dnn_input = combined_dnn_input(
-#                 sparse_embedding_list, dense_value_list)
-#             dnn_output = self.dnn(dnn_input)
-#             dnn_logit = self.dnn_linear(dnn_output)
-#             logit += dnn_logit
-#
-#         y_pred = self.out(logit)
-#
-#         return y_pred
 
 
 if __name__ == '__main__':
@@ -116,13 +63,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.cluster_mode == "local":
-        sc = init_orca_context("local")
+        sc = init_orca_context("local", cores=args.executor_cores, memory=args.executor_memory)
     elif args.cluster_mode == "yarn":
         sc = init_orca_context("yarn-client", cores=args.executor_cores,
                                num_nodes=args.num_executor, memory=args.executor_memory,
                                driver_cores=args.driver_cores, driver_memory=args.driver_memory,
-                               conf=spark_conf, object_store_memory="40g", init_ray_on_spark=True,
-                               extra_python_lib="evaluation.py")
+                               conf=spark_conf, object_store_memory="40g",
+                               extra_python_lib="model.py,evaluation.py")
     elif args.cluster_mode == "spark-submit":
         sc = init_orca_context("spark-submit", object_store_memory="40g")
     else:
@@ -172,6 +119,7 @@ if __name__ == '__main__':
 
     import torch
     def model_creator(config):
+        from model import DeepFM
         model = DeepFM(linear_feature_columns=config["linear_feature_columns"],
                        dnn_feature_columns=config["dnn_feature_columns"],
                        task='binary', l2_reg_embedding=1e-1)
