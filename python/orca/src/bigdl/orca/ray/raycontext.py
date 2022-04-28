5a873f75571de520a514160b583b94fd5b9a003f
SONG Ge <38711238+sgwhat@users.noreply.github.com>
2022-04-28 17:09:57 +0800

    [Orca] Support Handle Cores Number in RayonSpark Context (#4436)
    * update ray_ctx to handle cores specified
    
    * support handle cores_num in rayonspark

--- python/orca/src/bigdl/orca/ray/raycontext.py
+++ python/orca/src/bigdl/orca/ray/raycontext.py
@@ -36,17 +36,18 @@ class RayContext(object):
             from bigdl.orca.ray import RayOnSparkContext
             self._ray_on_spark_context = RayOnSparkContext(**kwargs)
             self.is_local = self._ray_on_spark_context.is_local
+            self.num_ray_nodes = self._ray_on_spark_context.num_ray_nodes
+            self.ray_node_cpu_cores = self._ray_on_spark_context.ray_node_cpu_cores
 
         elif runtime == "ray":
             self.is_local = False
             ray_args = kwargs.copy()
             self.ray_args = ray_args
+            self.num_ray_nodes = num_nodes
+            self.ray_node_cpu_cores = cores
         else:
             raise ValueError(f"Unsupported runtime: {runtime}. "
                              f"Runtime must be spark or ray")
-
-        self.num_ray_nodes = num_nodes
-        self.ray_node_cpu_cores = cores
 
         RayContext._active_ray_context = self
 
