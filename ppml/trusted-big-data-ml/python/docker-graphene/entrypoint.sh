ad444887505e1f1617645f8c33e7b18316c8f23c
Qiyuan Gong <qiyuan.gong@intel.com>
2022-04-29 17:23:54 +0800

    Fix entrypoint

--- ppml/trusted-big-data-ml/python/docker-graphene/entrypoint.sh
+++ ppml/trusted-big-data-ml/python/docker-graphene/entrypoint.sh
@@ -79,21 +79,22 @@ if [ -z "$ATTESTATION" ]; then
 elif [ "$ATTESTATION" = "true" ]; then
   echo "[INFO] Attestation is enabled!"
   # Build ATTESTATION_COMMAND
-  if [ -z "$ATTESTATION_SERVICE_URL" ]; then
-    echo "[ERROR] Attestation is enabled, but ATTESTATION_SERVICE_URL is empty!"
+  if [ -z "$ATTESTATION_URL" ]; then
+    echo "[ERROR] Attestation is enabled, but ATTESTATION_URL is empty!"
     echo "[INFO] PPML Application Exit!"
     exit 1
+  fi
   if [ -z "$ATTESTATION_ID" ]; then
     echo "[ERROR] Attestation is enabled, but ATTESTATION_ID is empty!"
     echo "[INFO] PPML Application Exit!"
     exit 1
-  if
+  fi
   if [ -z "$ATTESTATION_KEY" ]; then
     echo "[ERROR] Attestation is enabled, but ATTESTATION_KEY is empty!"
     echo "[INFO] PPML Application Exit!"
     exit 1
-  if
-  ATTESTATION_COMMAND="/opt/jdk8/bin/java -Xmx1g -cp $SPARK_HOME/examples/jars/spark-encrypt-io.jar com.intel.analytics.bigdl.ppml.attestation.AttestationCLI -u ${ATTESTATION_SERVICE_URL} -i ${ATTESTATION_ID}  -k ${ATTESTATION_KEY}"
+  fi
+  ATTESTATION_COMMAND="/opt/jdk8/bin/java -Xmx1g -cp $SPARK_HOME/examples/jars/spark-encrypt-io.jar com.intel.analytics.bigdl.ppml.attestation.AttestationCLI -u ${ATTESTATION_URL} -i ${ATTESTATION_ID}  -k ${ATTESTATION_KEY}"
 fi
 
 
