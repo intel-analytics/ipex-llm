da2f5a9ddcf7eb62fdb6fe0c8ab378cf7db321c3
LouieBHLu <lmx00518@qq.com>
2022-04-29 12:33:27 +0800

    add license

--- python/orca/example/learn/pytorch/brainMRI/dataset.py
+++ python/orca/example/learn/pytorch/brainMRI/dataset.py
@@ -1,29 +1,27 @@
-import os
-from os.path import exists
-from os import makedirs
-import argparse
-import matplotlib.pyplot as plt
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import numpy as np
-import glob
 import random
 import cv2
-from PIL import Image
-import pandas as pd
 
 import torch
-import torchvision
-import torchvision.transforms as transforms
-from torchvision.utils import make_grid
 import torchvision.transforms as tt
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 import torch.utils.data as data
-from torch.utils.data import DataLoader
 
-from mpl_toolkits.axes_grid1 import ImageGrid
-import albumentations as A
-from tqdm import tqdm
 
 def set_seed(seed = 0):
     torch.backends.cudnn.deterministic = True
