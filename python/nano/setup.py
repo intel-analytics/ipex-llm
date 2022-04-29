f5dd9c8443579f45906194995befc3319da871f2
Django-Jiang <1205034803@qq.com>
2022-04-29 11:37:45 +0800

    setup revise

--- python/nano/setup.py
+++ python/nano/setup.py
@@ -74,7 +74,7 @@ def setup_package():
                         "torchvision==0.12.0",
                         "pytorch_lightning==1.6.2", # attempt upgrade
                         "neural_compressor==1.11.0", # for INC test  
-                        "torchmetrics==0.7.2", # remove if not pl 1.4.2
+                        # "torchmetrics==0.7.2", # remove if not pl 1.4.2
                         "intel_extension_for_pytorch==1.11.0",
                         "opencv-python-headless",
                         "PyTurboJPEG",
