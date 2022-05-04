a691acbb0ef270d8ef1dcabd8cf59af96763e7fa
piaolaidelangman <707056129@qq.com>
2022-05-04 19:52:36 +0800

    update

--- scala/ppml/demo/scripts/runVflClient1.sh
+++ scala/ppml/demo/scripts/runVflClient1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 /graphene/Tools/argv_serializer bash -c "/opt/jdk8/bin/java -Xms2g -Xmx2g -cp $SPARK_HOME/jars/*:$BIGDL_HOME/jars/bigdl-ppml-spark_$SPARK_VERSION-$BIGDL_VERSION-jar-with-dependencies.jar \
-  com.intel.analytics.bigdl.ppml.fl.example.VFLLogisticRegression -d /ppml/trusted-big-data-ml/work/data/diabetes-vfl-1.csv" > /ppml/trusted-big-data-ml/secured-argvs
+  com.intel.analytics.bigdl.ppml.fl.example.VFLLogisticRegression -d /ppml/trusted-big-data-ml/work/data/diabetes-vfl-1.csv -c 1" > /ppml/trusted-big-data-ml/secured-argvs
 bash /ppml/trusted-big-data-ml/init.sh
 SGX=1 /ppml/trusted-big-data-ml/pal_loader bash
