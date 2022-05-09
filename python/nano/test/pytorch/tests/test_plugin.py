2a6f84c0d1c66d457ab5253f3da1c52304b3a757
Django-Jiang <1205034803@qq.com>
2022-05-09 20:05:02 +0800

    enable subprocess test

--- python/nano/test/pytorch/tests/test_plugin.py
+++ python/nano/test/pytorch/tests/test_plugin.py
@@ -57,15 +57,15 @@ class TestPlugin(TestCase):
         trainer.fit(pl_model, self.data_loader, self.data_loader)
         trainer.test(pl_model, self.data_loader)
 
-    # def test_trainer_subprocess_plugin(self):
-    #     pl_model = LightningModuleFromTorch(
-    #         self.model, self.loss, self.optimizer,
-    #         metrics=[torchmetrics.F1Score(num_classes), torchmetrics.Accuracy(num_classes=10)]
-    #     )
-    #     trainer = Trainer(num_processes=2, distributed_backend="subprocess",
-    #                       max_epochs=4)
-    #     trainer.fit(pl_model, self.data_loader, self.data_loader)
-    #     trainer.test(pl_model, self.data_loader)
+    def test_trainer_subprocess_plugin(self):
+        pl_model = LightningModuleFromTorch(
+            self.model, self.loss, self.optimizer,
+            metrics=[torchmetrics.F1Score(num_classes), torchmetrics.Accuracy(num_classes=10)]
+        )
+        trainer = Trainer(num_processes=2, distributed_backend="subprocess",
+                          max_epochs=4)
+        trainer.fit(pl_model, self.data_loader, self.data_loader)
+        trainer.test(pl_model, self.data_loader)
 
 
 if __name__ == '__main__':
