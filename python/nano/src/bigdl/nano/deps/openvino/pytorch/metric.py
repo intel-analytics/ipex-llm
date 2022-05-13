c1fdaac666bc6981ba25ace8cbdfb3a1d2ed298f
Chen, Zhentao <zhentao.chen@intel.com>
2022-05-13 12:28:05 +0800

    add metric

---
+++ python/nano/src/bigdl/nano/deps/openvino/pytorch/metric.py
@@ -0,0 +1,36 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from ..core.metric import BaseOpenVINOMetric
+import torch
+
+
+class PytorchOpenVINOMetric(BaseOpenVINOMetric):
+    def stack(self, output):
+        return torch.stack(output)
+
+    def update(self, output, target):
+        def numpy_to_tensors(np_arrays):
+            tensors = tuple(map(lambda x: torch.from_numpy(x), np_arrays))
+            return tensors
+        output = numpy_to_tensors(output)
+        target = numpy_to_tensors(target)
+        return super().update(output, target)
+
+    def reset(self):
+        self._scores = []
+
+    def to_scalar(self, score):
+        return score.item()
