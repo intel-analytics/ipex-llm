c3b5e31e6812b2de1f06bb411c669c84743c1c4a
jenniew <jenniewang123@gmail.com>
2022-05-17 15:19:36 -0700

    update

--- ppml/scripts/generate-enclave-key-az.sh
+++ ppml/scripts/generate-enclave-key-az.sh
@@ -2,6 +2,6 @@
 
 $VAULT_NAME=$1
 
-base64result=$( openssl genrsa -3 3072 | base64 -w 0 )
+key=$(openssl genrsa -3 3072)
 
-az keyvault secret set --vault-name $VAULT_NAME --name "enclave-key-pem" --value $base64result
+az keyvault secret set --vault-name $VAULT_NAME --name "enclave-key-pem" --value $key
