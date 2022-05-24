c92d72d7d440bb50c13dcefd7dadd1582f43ef54
Rebekk <1373597091@qq.com>
2022-05-24 09:47:12 +0800

    inherit base_forecaster and modify fit

--- python/chronos/src/bigdl/chronos/forecaster/autoformer_forecaster.py
+++ python/chronos/src/bigdl/chronos/forecaster/autoformer_forecaster.py
@@ -14,34 +14,9 @@
 # limitations under the License.
 #
 
-# MIT License
-
-# Copyright (c) 2021 THUML @ Tsinghua University
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# code adapted from https://github.com/thuml/Autoformer
-#
-
 import torch
 from bigdl.chronos.forecaster.base_forecaster import BasePytorchForecaster
-from bigdl.chronos.model.autoformer import model_creator
+from bigdl.chronos.model.VanillaLSTM_pytorch import model_creator
 
 
 class AutoformerForecaster(BasePytorchForecaster):
@@ -49,34 +24,97 @@ class AutoformerForecaster(BasePytorchForecaster):
                  seq_len,
                  label_len,
                  pred_len,
-                 output_attention,
+                 output_attention=False,
+                 moving_avg=25,
                  enc_in,
+                 d_model=512,
+                 embed='timeF',
                  freq,
+                 dropout=0.05,
                  dec_in,
-                 c_out):
-        self.model_config = {
+                 factor=3,
+                 n_head=8,
+                 d_ff=2048,
+                 activation='gelu',
+                 e_layer=2,
+                 c_out,
+                 d_layers=1):
+        # config setting
+        self.config = {
             "seq_len": seq_len,
             "label_len": label_len,
             "pred_len": pred_len,
             "output_attention": output_attention,
+            "moving_avg": moving_avg,
             "enc_in": enc_in,
+            "d_model": d_model,
+            "embed": embed,
             "freq": freq,
+            "dropout": dropout,
             "dec_in": dec_in,
-            "c_out": c_out
+            "factor": factor,
+            "n_head": n_head,
+            "d_ff": d_ff,
+            "activation": activation,
+            "e_layer": e_layer,
+            "c_out": c_out,
+            "d_layers": d_layers
         }
 
-        self.internal = AutoFormer()
+        self.model_creator = model_creator
 
         super().__init__()
+    
+    def fit(self, data, epochs=1, batch_size=32):
+        # input transform
+        if isinstance(data, DataLoader) and self.distributed:
+            data = loader_to_creator(data)
+        if isinstance(data, tuple) and self.distributed:
+            data = np_to_creator(data)
+        try:
+            from bigdl.orca.data.shard import SparkXShards
+            if isinstance(data, SparkXShards) and not self.distributed:
+                warnings.warn("Xshards is collected to local since the "
+                              "forecaster is non-distribued.")
+                data = xshard_to_np(data)
+        except ImportError:
+            pass
 
-    def fit(self, batch, batch_idx):
-        return self.internal.training_step(self, batch, batch_idx)
+        # fit on internal
+        if self.distributed:
+            # for cluster mode
+            from bigdl.orca.common import OrcaContext
+            sc = OrcaContext.get_spark_context().getConf()
+            num_nodes = 1 if sc.get('spark.master').startswith('local') \
+                else int(sc.get('spark.executor.instances'))
+            if batch_size % self.workers_per_node != 0:
+                raise RuntimeError("Please make sure that batch_size can be divisible by "
+                                   "the product of worker_per_node and num_nodes, "
+                                   f"but 'batch_size' is {batch_size}, 'workers_per_node' "
+                                   f"is {self.workers_per_node}, 'num_nodes' is {num_nodes}")
+            batch_size //= (self.workers_per_node * num_nodes)
+            return self.internal.fit(data=data,
+                                     epochs=epochs,
+                                     batch_size=batch_size)
+        else:
+            from bigdl.chronos.pytorch import TSTrainer as Trainer
 
-    def evaluate(self, batch, batch_idx):
-        return self.internal.validation_step(self, batch, batch_idx)
+            # # numpy data shape checking
+            # if isinstance(data, tuple):
+            #     check_data(data[0], data[1], self.data_config)
+            # else:
+            #     warnings.warn("Data shape checking is not supported by dataloader input.")
 
-    def predict(self, batch, batch_idx):
-        return self.internal.predict_step(self, batch, batch_idx)
+            # data transformation
+            if isinstance(data, tuple):
+                data = DataLoader(TensorDataset(torch.from_numpy(data[0]),
+                                                torch.from_numpy(data[1]),
+                                                torch.from_numpy(data[2]),
+                                                torch.from_numpy(data[3]),),
+                                  batch_size=batch_size,
+                                  shuffle=True)
 
-    def configure_optimizers(self):
-        return self.internal.configure_optimizers(self)
+            # Trainer init and fitting
+            self.trainer = Trainer(max_epochs=epochs)
+            self.trainer.fit(self.internal, data)
+            self.fitted = True
