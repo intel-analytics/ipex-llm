eaae428af51f617765a1ed309f93f4ad5323ec2e
Jiaming <litchy233@gmail.com>
2022-05-24 11:21:01 +0800

    add moved code

---
+++ python/ppml/src/bigdl/ppml/fl/nn/fl_server.py
@@ -0,0 +1,49 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from concurrent import futures
+import grpc
+from bigdl.ppml.fl import *
+from bigdl.ppml.fl.nn.generated.nn_service_pb2_grpc import *
+from bigdl.ppml.fl.nn.nn_service import NNServiceImpl
+
+
+class FLServer(object):
+    def __init__(self, client_num=1):
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+        self.port = 8980 # TODO: set from config file
+        self.client_num = client_num
+
+    def build(self):
+        add_NNServiceServicer_to_server(
+            NNServiceImpl(client_num=self.client_num),            
+            self.server)
+        self.server.add_insecure_port(f'[::]:{self.port}')
+        logging.info(f'gRPC server starts listening port: {self.port}')
+
+    def start(self):
+        self.server.start()
+        # self.server.wait_for_termination()
+
+    def stop(self):
+        self.server.stop(None)
+
+    
+if __name__ == '__main__':
+    fl_server = FLServer()
+    fl_server.build()
+    fl_server.start()
+    fl_server.block_until_shutdown()
