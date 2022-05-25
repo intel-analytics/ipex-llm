a549289e963ef9f788007c58c0342c8ccbbc5aa5
dding3 <ding.ding@intel.com>
2022-05-25 09:42:23 -0700

    fix style check failure

--- python/chronos/src/bigdl/chronos/forecaster/tf/tcn_forecaster.py
+++ python/chronos/src/bigdl/chronos/forecaster/tf/tcn_forecaster.py
@@ -113,10 +113,11 @@ class TCNForecaster(BaseTF2Forecaster):
         # self.distributed = distributed
         # self.distributed_backend = distributed_backend
         # self.workers_per_node = workers_per_node
+        from bigdl.nano.utils.log4Error import invalidInputError
         if distributed:
-            raise NotImplementedError("We will add distributed support in subsequent releases, "
-                                      "the feature is currently unavailable, "
-                                      "Please set distributed=False.")
+            invalidInputError(False, "We will add distributed support in subsequent releases, "
+                                     "the feature is currently unavailable, "
+                                     "Please set distributed=False.")
 
         # other settings
         self.lr = lr
