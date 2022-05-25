c064be01bb626eebc4b4df77e51d39d48b4682e1
Chen, Zhentao <zhentao.chen@intel.com>
2022-05-25 14:05:06 +0800

    add attribute validation and errors

--- python/nano/src/bigdl/nano/deps/neural_compressor/core/quantization.py
+++ python/nano/src/bigdl/nano/deps/neural_compressor/core/quantization.py
@@ -57,13 +57,17 @@ class BaseQuantization(Quantization):
         :param outputs:     For tensorflow to specify names of outputs. e.g. outputs=['logits',]
         """
         qconf = Quantization_Conf(conf)
+        self.validate_config(framework=framework, approach=approach,
+                             tuning_strategy=tuning_strategy,
+                             accuracy_criterion=accuracy_criterion,
+                             timeout=timeout, max_trials=max_trials,
+                             inputs=inputs, outputs=outputs)
         cfg = qconf.usr_cfg
         # Override default config
         cfg.model.framework = framework
         cfg.quantization.approach = approach
         cfg.tuning.strategy.name = tuning_strategy
-        if accuracy_criterion:
-            cfg.tuning.accuracy_criterion = accuracy_criterion
+        cfg.tuning.accuracy_criterion = accuracy_criterion
         cfg.tuning.exit_policy.timeout = timeout
         cfg.tuning.exit_policy.max_trials = max_trials
         cfg.model.inputs = inputs
@@ -71,7 +75,7 @@ class BaseQuantization(Quantization):
         super().__init__(qconf)
 
     def post_training_quantize(self, model, calib_dataloader=None, metric=None):
-        self.check(calib_dataloader, metric)
+        self.sanity_check_before_execution(calib_dataloader, metric)
         self.model = common.Model(model)
 
         if self.cfg.quantization.approach == 'post_training_static_quant':
@@ -87,7 +91,7 @@ class BaseQuantization(Quantization):
             invalidInputError(False,
                               "Found no quantized model satisfying accuracy criterion.")
 
-    def check(self, calib_dataloader, metric):
+    def sanity_check_before_execution(self, calib_dataloader, metric):
         """
         Call before self.__call__() to check if the object is well-initialized
         for quantization.
@@ -107,3 +111,74 @@ class BaseQuantization(Quantization):
             invalidInputError(False,
                               "calib_dataloader must be specified for evaluation"
                               " when metric is not None.")
+
+    @property
+    def valid_frameworks(self):
+        return ('pytorch_fx', 'pytorch', 'pytorch_ipex', 'tensorflow', 'onnxrt_integer',
+                'onnxrt_qlinear')
+
+    @property
+    def valid_approaches(self):
+        return 'post_training_static_quant', 'post_training_dynamic_quant'
+
+    @property
+    def valid_tuning_strategies(self):
+        return 'basic', 'bayesian', 'mse', 'sigopt'
+
+    @property
+    def valid_drop_type(self):
+        return 'relative', 'absolute'
+
+    def validate_config(self, **kwargs):
+        invalidInputError(
+            kwargs['approach'] in self.valid_approaches,
+            errMsg="{} is invalid.".format(kwargs['approach']),
+            fixMsg="Choose from {}.".format(self.valid_approaches)
+        )
+        invalidInputError(
+            kwargs['framework'] in self.valid_frameworks,
+            errMsg="{} is invalid.".format(kwargs['framework']),
+            fixMsg="Choose from {}.".format(self.valid_frameworks)
+        )
+        invalidInputError(
+            kwargs['tuning_strategy'] in self.valid_tuning_strategies,
+            errMsg="{} is invalid.".format(kwargs['tuning_strategy']),
+            fixMsg="Choose from {}.".format(self.valid_tuning_strategies)
+        )
+
+        invalidInputError(
+            isinstance(kwargs['accuracy_criterion'], dict),
+            errMsg="accuracy_criterion should be a dictionary."
+        )
+        is_drop_type_defined = False
+        for k, v in kwargs['accuracy_criterion'].items():
+            if k in self.valid_drop_type:
+                # either of 'relative'/'absolute' can be in the dict
+                invalidInputError(
+                    not is_drop_type_defined,
+                    errMsg="Drop type should be defined only once.",
+                    fixMsg="There multiple 'relative'/'absolute' in "
+                           "accuracy_criterion, only one is required."
+                )
+                is_drop_type_defined = True
+                invalidInputError(
+                    0 <= v < 1,
+                    errMsg="The value of {} for {} drop is invalid.".format(v, k),
+                    fixMsg="Value should be within [0, 1)."
+                )
+            else:
+                invalidInputError(
+                    k == 'higher_is_better',
+                    errMsg="Key {} is not valid".format(k),
+                    fixMsg="Keys for accuracy_criterion should be"
+                           " 'relative'/'absolute' and 'higher_is_better'."
+                )
+
+        invalidInputError(
+            isinstance(kwargs['timeout'], int) and kwargs['timeout'] >= 0,
+            errMsg="Argument timeout should be an integer >= 0"
+        )
+        invalidInputError(
+            isinstance(kwargs['max_trials'], int) and kwargs['max_trials'] >= 0,
+            errMsg="Argument max_trials should be an integer >= 0"
+        )
