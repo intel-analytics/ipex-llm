653ced5e626357e4e380464e4bd377a229d38951
piaolaidelangman <707056129@qq.com>
2022-05-26 08:09:30 +0800

    Cancel aesm change

--- ppml/trusted-big-data-ml/scala/docker-occlum/start-occlum-attestation-server.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/start-occlum-attestation-server.sh
@@ -8,7 +8,7 @@ sudo docker run -it \
 	--cpuset-cpus 5-7 \
 	--device=/dev/sgx/enclave \
 	--device=/dev/sgx/provision \
-	-v /var/run/aesmd/:/var/run/aesmd/ \
+	-v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
 	-e LOCAL_IP=$LOCAL_IP \
 	-e SGX_MEM_SIZE=8GB \
 	-e PCCS_URL=$PCCS_URL \
