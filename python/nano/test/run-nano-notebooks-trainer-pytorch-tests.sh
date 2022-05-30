8e3f1c1ec379296dab38d7fb7b457b001beaaf85
Mingzhi Hu <49382651+y199387@users.noreply.github.com>
2022-05-30 08:03:30 +0800

    Add nano notebooks unit test

--- python/nano/test/run-nano-notebooks-trainer-pytorch-tests.sh
+++ python/nano/test/run-nano-notebooks-trainer-pytorch-tests.sh
@@ -3,7 +3,7 @@
 export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
 export PYTORCH_NANO_NOTEBOOKS_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/notebooks/pytorch
-export FIT_EPOCHS=2
+export MAX_STEPS=10
 
 wget -nv ${FTP_URI}/analytics-zoo-data/cifar-10-python.tar.gz -P ${PYTORCH_NANO_NOTEBOOKS_DIR}/cifar10/
 
