c364d88ecbbbec29de6be0d089448b90c80b1eac
Xin Qiu <qiuxin2012@users.noreply.github.com>
2022-05-30 21:10:29 +0800

    Refine ppml crypto  (#4719)
    * update
    
    * update
    
    * update scala
    
    * update API
    
    * rename
    
    * fix typo
    
    * fix ut
    
    * fix style

--- scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/crypto/Crypto.scala
+++ scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/crypto/Crypto.scala
@@ -16,25 +16,108 @@
 
 package com.intel.analytics.bigdl.ppml.crypto
 
+import com.intel.analytics.bigdl.dllib.nn.NormMode.Value
 import com.intel.analytics.bigdl.ppml.utils.Supportive
+import org.apache.spark.input.PortableDataStream
+
+import java.io.{DataInputStream, DataOutputStream}
+import javax.crypto.Cipher
 
 trait Crypto extends Supportive with Serializable {
-  def encryptFile(sourceFilePath: String, saveFilePath: String, dataKeyPlaintext: String)
-  def decryptFile(sourceFilePath: String, saveFilePath: String, dataKeyPlaintext: String)
-  def encryptBytes(sourceBytes: Array[Byte], dataKeyPlaintext: String): Array[Byte]
-  def decryptBytes(sourceBytes: Array[Byte], dataKeyPlaintext: String): Array[Byte]
+  def init(cryptoMode: CryptoMode, mode: OperationMode, dataKeyPlaintext: String): Unit
+
+  def decryptBigContent(ite: Iterator[(String, PortableDataStream)]): Iterator[String]
+
+  def genFileHeader(): Array[Byte]
+
+  def verifyFileHeader(header: Array[Byte]): Unit
+
+  def update(content: Array[Byte]): Array[Byte]
+
+  def update(content: Array[Byte], offset: Int, len: Int): Array[Byte]
+
+  def doFinal(content: Array[Byte]): (Array[Byte], Array[Byte])
+
+  def doFinal(content: Array[Byte], offset: Int, len: Int): (Array[Byte], Array[Byte])
+
+  def encryptStream(inputStream: DataInputStream, outputStream: DataOutputStream): Unit
+
+  def decryptStream(inputStream: DataInputStream, outputStream: DataOutputStream): Unit
+
+  def decryptFile(binaryFilePath: String, savePath: String): Unit
+
+  def encryptFile(binaryFilePath: String, savePath: String): Unit
 }
 
-object CryptoMode extends Enumeration {
-  type CryptoMode = Value
-  val PLAIN_TEXT = value("plain_text", "plain_text")
-  val AES_CBC_PKCS5PADDING = value("AES/CBC/PKCS5Padding", "AES/CBC/PKCS5Padding")
-  val UNKNOWN = value("UNKNOWN", "UNKNOWN")
-  class EncryptModeEnumVal(name: String, val value: String) extends Val(nextId, name)
-  protected final def value(name: String, value: String): EncryptModeEnumVal = {
-    new EncryptModeEnumVal(name, value)
-  }
-  def parse(s: String): Value = {
-    values.find(_.toString.toLowerCase() == s.toLowerCase).getOrElse(CryptoMode.UNKNOWN)
+object Crypto {
+  def apply(cryptoMode: CryptoMode): Crypto = {
+    cryptoMode match {
+      case AES_CBC_PKCS5PADDING =>
+        new BigDLEncrypt()
+      case default =>
+        throw new EncryptRuntimeException("No such crypto mode!")
+    }
   }
 }
+
+// object OperationMode extends Enumeration {
+//   type OperationMode = Value
+//   val ENCRYPT, DECRYPT = Value
+// }
+sealed trait OperationMode extends Serializable {
+  def opmode: Int
+}
+
+case object ENCRYPT extends OperationMode {
+  override def opmode: Int = Cipher.ENCRYPT_MODE
+}
+case object DECRYPT extends OperationMode {
+  override def opmode: Int = Cipher.DECRYPT_MODE
+}
+
+trait CryptoMode extends Serializable {
+  def encryptionAlgorithm: String
+  def signingAlgorithm: String
+  def secretKeyAlgorithm: String
+}
+
+object CryptoMode {
+   def parse(s: String): CryptoMode = {
+     s.toLowerCase() match {
+       case "aes/cbc/pkcs5padding" =>
+         AES_CBC_PKCS5PADDING
+       case "plain_text" =>
+         PLAIN_TEXT
+     }
+   }
+}
+
+case object AES_CBC_PKCS5PADDING extends CryptoMode {
+  override def encryptionAlgorithm: String = "AES/CBC/PKCS5Padding"
+
+  override def signingAlgorithm: String = "HmacSHA256"
+
+  override def secretKeyAlgorithm: String = "AES"
+}
+
+case object PLAIN_TEXT extends CryptoMode {
+  override def encryptionAlgorithm: String = "plain_text"
+
+  override def signingAlgorithm: String = ""
+
+  override def secretKeyAlgorithm: String = ""
+}
+
+// object CryptoMode extends Enumeration {
+//  type CryptoMode = Value
+//  val PLAIN_TEXT = value("plain_text", "plain_text")
+//  val AES_CBC_PKCS5PADDING = value("AES/CBC/PKCS5Padding", "AES/CBC/PKCS5Padding")
+//  val UNKNOWN = value("UNKNOWN", "UNKNOWN")
+//  class EncryptModeEnumVal(name: String, val value: String) extends Val(nextId, name)
+//  protected final def value(name: String, value: String): EncryptModeEnumVal = {
+//    new EncryptModeEnumVal(name, value)
+//  }
+//  def parse(s: String): Value = {
+//    values.find(_.toString.toLowerCase() == s.toLowerCase).getOrElse(CryptoMode.UNKNOWN)
+//  }
+// }
