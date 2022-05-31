cbaa984011bf1b250c65a5aa0d40bdbdfec5f995
Wang, Yang <yang3.wang@intel.com>
2022-05-31 00:27:20 +0000

    fix tests

--- python/nano/test/pytorch/utils/_train_torch_lightning.py
+++ python/nano/test/pytorch/utils/_train_torch_lightning.py
@@ -81,15 +81,13 @@ def create_test_data_loader(dir, batch_size, num_workers, transform, subset=50):
 
 
 def train_with_linear_top_layer(model_without_top, batch_size, num_workers, data_dir,
-                                use_ipex=False, use_orca_lite_trainer=False):
+                                use_ipex=False):
     model = Net(model_without_top)
     train_torch_lightning(model, batch_size, num_workers,
-                          data_dir, use_ipex=use_ipex,
-                          use_orca_lite_trainer=use_orca_lite_trainer)
+                          data_dir, use_ipex=use_ipex)
 
 
-def train_torch_lightning(model, batch_size, num_workers, data_dir, use_ipex=False,
-                          use_orca_lite_trainer=False):
+def train_torch_lightning(model, batch_size, num_workers, data_dir, use_ipex=False):
     orig_parameters = deepcopy(model.state_dict())
     # list to store the right key of dict
     orig_parameters_list = deepcopy(list(model.named_parameters()))
@@ -97,12 +95,8 @@ def train_torch_lightning(model, batch_size, num_workers, data_dir, use_ipex=Fal
     train_loader = create_data_loader(
         data_dir, batch_size, num_workers, data_transform)
 
-    if use_orca_lite_trainer:
-        from bigdl.nano.pytorch import Trainer
-
-        trainer = Trainer(max_epochs=1)
-    else:
-        trainer = pl.Trainer(max_epochs=1, use_ipex=use_ipex)
+    from bigdl.nano.pytorch import Trainer
+    trainer = Trainer(max_epochs=1, use_ipex=use_ipex)
 
     trainer.fit(model, train_loader)
 
