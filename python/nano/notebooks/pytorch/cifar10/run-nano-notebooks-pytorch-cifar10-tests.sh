edb68b5872e271dd2c1a28a6629fa58a9134d8cb
Mingzhi Hu <mingzhi.hu@intel.com>
2022-06-02 11:33:15 +0800

    refactor nano notebooks unit test

---
+++ python/nano/notebooks/pytorch/cifar10/run-nano-notebooks-pytorch-cifar10-tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
+export PYTORCH_NANO_NOTEBOOKS_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/notebooks/pytorch/cifar10
+export LIMIT_BATCHES=0.02
+export FIT_EPOCHS=3
+
+wget -nv ${FTP_URI}/analytics-zoo-data/cifar-10-python.tar.gz -P ${PYTORCH_NANO_NOTEBOOKS_DIR}/
+
+set -e
+
+echo "# Start Testing ${train_notebooks}"
+start=$(date "+%s")
+
+python -m pytest --nbmake --nbmake-timeout=180 --nbmake-kernel=python3 ${PYTORCH_NANO_NOTEBOOKS_DIR}/nano-trainer-example.ipynb
+
+now=$(date "+%s")
+time=$((now-start))
+
+echo "# Start Testing ${inference_notebooks}"
+start=$(date "+%s")
+
+python -m pytest --nbmake --nbmake-timeout=180 --nbmake-kernel=python3 ${PYTORCH_NANO_NOTEBOOKS_DIR}/nano-inference-example.ipynb
+
+now=$(date "+%s")
+time=$((now-start))
+
