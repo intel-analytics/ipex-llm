a5b03a2d9aa356521186a66fbb2ee5605a8e53a5
guoyao <guoyao.li@intel.com>
2022-06-02 15:28:28 +0800

    change resume search behavior

--- python/nano/src/bigdl/nano/automl/tf/mixin.py
+++ python/nano/src/bigdl/nano/automl/tf/mixin.py
@@ -15,6 +15,7 @@
 #
 
 import copy
+import warnings
 
 from .objective import Objective
 from bigdl.nano.automl.hpo.backend import create_hpo_backend
@@ -229,6 +230,20 @@ class HPOMixin:
             and search backend arguments (e.g. n_trials, pruner, etc.)
             are allowed in kwargs.
         """
+
+        do_create = True
+        if resume:
+            if 'storage' not in kwargs.keys() or kwargs['storage'].strip() == "":
+                if self.study is None:
+                    warnings.warn(
+                        'You did not specify storage path and your study is None. \
+                            Creating a new search study', UserWarning)
+                else:
+                    do_create = False
+        
+        if kwargs['storage'].strip() == "":
+            del kwargs['storage']
+
         search_kwargs = kwargs or {}
         self.target_metric = self._fix_target_metric(target_metric, kwargs)
 
@@ -246,7 +261,7 @@ class HPOMixin:
                             self.backend)
 
         # create study
-        if not resume or search_kwargs['storage'] != "":
+        if do_create:
             self.study = _create_study(resume, self.create_kwargs, self.backend)
 
         isprune = True if self.create_kwargs.get('pruner', None) else False
