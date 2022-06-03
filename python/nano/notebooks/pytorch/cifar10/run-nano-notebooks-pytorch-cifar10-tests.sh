5ba17597ebd43d49f1d0e4c10e1cbf4e1ae76532
Mingzhi Hu <49382651+y199387@users.noreply.github.com>
2022-06-03 09:31:23 +0800

    reduce example training time

--- python/nano/notebooks/pytorch/cifar10/run-nano-notebooks-pytorch-cifar10-tests.sh
+++ python/nano/notebooks/pytorch/cifar10/run-nano-notebooks-pytorch-cifar10-tests.sh
@@ -3,7 +3,7 @@
 export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
 export PYTORCH_NANO_NOTEBOOKS_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/notebooks/pytorch/cifar10
-export LIMIT_BATCHES=0.02
+export LIMIT_BATCHES=0.005
 export FIT_EPOCHS=3
 
 wget -nv ${FTP_URI}/analytics-zoo-data/cifar-10-python.tar.gz -P ${PYTORCH_NANO_NOTEBOOKS_DIR}/
