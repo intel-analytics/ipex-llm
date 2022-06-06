54ee0afc83fbfc2c7de3ab814aa2778a74804996
Xin Qiu <qiuxin2012@users.noreply.github.com>
2022-06-06 14:38:23 +0800

    PPMLContext write csv (#4746)
    * write support
    
    * wirter
    
    * update
    
    * fix ut
    
    * some update

--- scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/crypto/dataframe/EncryptedDataFrameWriter.scala
+++ scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/crypto/dataframe/EncryptedDataFrameWriter.scala
@@ -16,14 +16,97 @@
 
 package com.intel.analytics.bigdl.ppml.crypto.dataframe
 
-import com.intel.analytics.bigdl.ppml.crypto.CryptoMode
-import org.apache.spark.sql.SparkSession
+import com.intel.analytics.bigdl.ppml.crypto.dataframe.EncryptedDataFrameWriter.writeCsv
+import com.intel.analytics.bigdl.ppml.crypto.{AES_CBC_PKCS5PADDING, Crypto, CryptoMode, ENCRYPT, PLAIN_TEXT}
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SerializableWritable, SparkContext, TaskContext}
+import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
+
+import java.util.Locale
 
 class EncryptedDataFrameWriter(
       sparkSession: SparkSession,
+      df: DataFrame,
       encryptMode: CryptoMode,
       dataKeyPlainText: String) {
+  protected val extraOptions = new scala.collection.mutable.HashMap[String, String]
+
+  def option(key: String, value: String): this.type = {
+    this.extraOptions += (key -> value)
+    this
+  }
+
+  def option(key: String, value: Boolean): this.type = {
+    this.extraOptions += (key -> value.toString)
+    this
+  }
+
+  private var mode: SaveMode = SaveMode.ErrorIfExists
+
+  def mode(saveMode: String): this.type = {
+    this.mode = saveMode.toLowerCase(Locale.ROOT) match {
+      case "overwrite" => SaveMode.Overwrite
+      case "append" => SaveMode.Append
+      case "ignore" => SaveMode.Ignore
+      case "error" | "errorifexists" | "default" => SaveMode.ErrorIfExists
+      case _ => throw new IllegalArgumentException(s"Unknown save mode: $saveMode. " +
+        "Accepted save modes are 'overwrite', 'append', 'ignore', 'error', 'errorifexists'.")
+    }
+    this
+  }
+
+  def csv(path: String): Unit = {
+    encryptMode match {
+      case PLAIN_TEXT =>
+        df.write.options(extraOptions).csv(path)
+      case AES_CBC_PKCS5PADDING =>
+        writeCsv(df.rdd, sparkSession.sparkContext, path, encryptMode, dataKeyPlainText)
+      case _ =>
+        throw new IllegalArgumentException("unknown EncryptMode " + CryptoMode.toString)
+    }
+  }
 
 
+}
 
+object EncryptedDataFrameWriter {
+  protected def writeCsv(rdd: RDD[Row],
+                         sc: SparkContext,
+                         path: String,
+                         encryptMode: CryptoMode,
+                         dataKeyPlainText: String): Unit = {
+    val confBroadcast = sc.broadcast(
+      new SerializableWritable(sc.hadoopConfiguration)
+    )
+    rdd.mapPartitions{ rows => {
+      if (rows.nonEmpty) {
+        val hadoopConf = confBroadcast.value.value
+        val fs = FileSystem.get(hadoopConf)
+        val partId = TaskContext.getPartitionId()
+        // TODO
+        val output = fs.create(new Path(path + "/part-" + partId), true)
+        val cypto = Crypto(cryptoMode = encryptMode)
+        cypto.init(encryptMode, ENCRYPT, dataKeyPlainText)
+        val header = cypto.genHeader()
+
+        output.write(header)
+        var row = rows.next()
+        while (rows.hasNext) {
+          val line = row.toSeq.mkString(",") + "\n"
+          output.write(cypto.update(line.getBytes))
+//          print(rows.hasNext)
+          row = rows.next()
+        }
+        val lastLine = row.toSeq.mkString(",")
+        val (lBytes, hmac) = cypto.doFinal(lastLine.getBytes)
+        output.write(lBytes)
+        output.write(hmac)
+        output.flush()
+        output.close()
+      }
+      Iterator.single(1)
+    }}.count()
+
+  }
 }
---
+++ scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/crypto/dataframe/DataFrameHelper.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.ppml.crypto.dataframe
+
+import com.intel.analytics.bigdl.dllib.common.zooUtils
+import com.intel.analytics.bigdl.ppml.crypto.{AES_CBC_PKCS5PADDING, BigDLEncrypt, ENCRYPT}
+import com.intel.analytics.bigdl.ppml.kms.SimpleKeyManagementService
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+import java.io.FileWriter
+import scala.util.Random
+
+class DataFrameHelper extends FlatSpec with Matchers with BeforeAndAfter {
+  val repeatedNum = 100000
+  val totalNum = repeatedNum * 3
+  val (appid, appkey) = generateKeys()
+  val simpleKms = SimpleKeyManagementService(appid, appkey)
+  val dir = zooUtils.createTmpDir("PPMLUT", "rwx------").toFile()
+
+  val primaryKeyPath = dir + "/primary.key"
+  val dataKeyPath = dir + "/data.key"
+  var dataKeyPlaintext: String = null
+  simpleKms.retrievePrimaryKey(primaryKeyPath)
+  simpleKms.retrieveDataKey(primaryKeyPath, dataKeyPath)
+  val (plainFileName, encryptFileName, data) = generateCsvData()
+
+  def generateKeys(): (String, String) = {
+    val appid: String = (1 to 12).map(x => Random.nextInt(10)).mkString
+    val appkey: String = (1 to 12).map(x => Random.nextInt(10)).mkString
+    (appid, appkey)
+  }
+
+  def generateCsvData(): (String, String, String) = {
+    val fileName = dir + "/people.csv"
+    val encryptFileName = dir + "/en_people.csv"
+    val fw = new FileWriter(fileName)
+    val data = new StringBuilder()
+    data.append(s"name,age,job\n")
+    (0 until repeatedNum).foreach {i =>
+      data.append(s"yvomq,$i,Developer\ngdni,$i,Engineer\npglyal,$i,Engineer\n")
+    }
+    fw.append(data)
+    fw.close()
+
+    val crypto = new BigDLEncrypt()
+    dataKeyPlaintext = simpleKms.retrieveDataKeyPlainText(primaryKeyPath, dataKeyPath)
+    crypto.init(AES_CBC_PKCS5PADDING, ENCRYPT, dataKeyPlaintext)
+    crypto.doFinal(fileName, encryptFileName)
+    (fileName, encryptFileName, data.toString())
+  }
+
+}
