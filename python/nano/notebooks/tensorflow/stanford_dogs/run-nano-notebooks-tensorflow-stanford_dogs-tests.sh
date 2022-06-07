ee07c6218495150478d219f2f4779dc27443712b
Hu Mingzhi <mingzhi.hu@intel.com>
2022-06-07 08:55:43 +0000

    add nano unit test for tensorflow notebooks

---
+++ python/nano/notebooks/tensorflow/stanford_dogs/run-nano-notebooks-tensorflow-stanford_dogs-tests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
+export TENSORFLOW_NANO_NOTEBOOKS_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/notebooks/tensorflow/stanford_dogs
+export FIT_STEPS=5
+export VAL_STEPS=2
+export FREEZE_EPOCHS=2
+export UNFREEZE_EPOCHS=1
+export TEST_STEPS=2
+
+set -e
+
+echo "# Start Testing stanford_dogs Fit Notebook"
+
+python -m pytest --nbmake --nbmake-timeout=300 --nbmake-kernel=python3 ${TENSORFLOW_NANO_NOTEBOOKS_DIR}/nano_tensorflow_fit_example.ipynb
+
+echo "# Start Testing stanford_dogs Inference Notebook"
+
+jupyter nbconvert --to script ${TENSORFLOW_NANO_NOTEBOOKS_DIR}/nano_tensorflow_inference_example.ipynb
+python ${TENSORFLOW_NANO_NOTEBOOKS_DIR}/nano_tensorflow_inference_example.py
