9850ae1f17feb0b4c79f2d2c8553433c45dec376
Django-Jiang <1205034803@qq.com>
2022-06-08 08:50:04 +0800

    [nano] distribtued training test - add subprocess check

--- python/nano/src/bigdl/nano/pytorch/plugins/ddp_subprocess.py
+++ python/nano/src/bigdl/nano/pytorch/plugins/ddp_subprocess.py
@@ -130,7 +130,7 @@ class DDPSubprocessPlugin(DDPSpawnPlugin):
                 process.wait()
 
             for _, process in enumerate(processes):
-                print(">>>>>>>>>>>>>>returncode: ", process.returncode)
+                assert process.returncode == 0, "Subprocess incorrectly exit, check the trainer configure or usage"
 
             with open(os.path.join(temp_dir, "results.pkl"), "rb") as f:
                 queue_list = cloudpickle.load(f)
