0c3a46feffa6178f8d221772c95679ccb6123b79
Mingzhi Hu <49382651+y199387@users.noreply.github.com>
2022-06-08 08:26:28 +0800

    add nano unit test for notebooks (#4706)
    * add nano unit test for notebooks
    
    * Update workflow and shell for Notebook unit test
    
    - decrease train epochs
    - delete some pip install
    
    * Add nano notebooks unit test
    
    * Add requirements.txt
    
    * Update requirements.txt
    
    - Add dependence on torchvision 0.10.0+cpu
    
    * Update requirements.txt
    
    * Update nano notebooks unit test
    
    - Update nano notebooks unit test shell
    
    * Update run-nano-notebooks-pytorch-tests.sh
    
    * Change structure of nano notebook unit test
    - put requirements file in cifar10 directory
    
    * refactor nano notebooks unit test
    
    * refactor nano notebooks unit test
    
    * reduce example training time
    
    * reduce nano notebook's inference time
    
    * reduce notebook trainning time

---
+++ python/nano/notebooks/pytorch/cifar10/run-nano-notebooks-pytorch-cifar10-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
+export PYTORCH_NANO_NOTEBOOKS_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/notebooks/pytorch/cifar10
+export LIMIT_TRAIN_BATCHES=0.005
+export LIMIT_VAL_BATCHES=0.01
+export FIT_EPOCHS=3
+export SUBSET=50
+
+wget -nv ${FTP_URI}/analytics-zoo-data/cifar-10-python.tar.gz -P ${PYTORCH_NANO_NOTEBOOKS_DIR}/
+
+set -e
+
+echo "# Start Testing cifar10 train notebook"
+start=$(date "+%s")
+
+python -m pytest --nbmake --nbmake-timeout=180 --nbmake-kernel=python3 ${PYTORCH_NANO_NOTEBOOKS_DIR}/nano-trainer-example.ipynb
+
+now=$(date "+%s")
+time=$((now-start))
+
+echo "Bigdl-nano cifar10 train notebook test finished"
+echo "Time used:$time seconds"
+
+echo "# Start Testing cifar10 inference notebook"
+start=$(date "+%s")
+
+python -m pytest --nbmake --nbmake-timeout=180 --nbmake-kernel=python3 ${PYTORCH_NANO_NOTEBOOKS_DIR}/nano-inference-example.ipynb
+
+now=$(date "+%s")
+time=$((now-start))
+
+echo "Bigdl-nano cifar10 inference notebook tests finished"
+echo "Time used:$time seconds"
