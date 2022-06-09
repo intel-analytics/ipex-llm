7e6fe467c911d82d796bb8cecc88634276e9a95d
Zhou <jian.zhou@intel.com>
2022-06-09 14:30:23 +0800

    add read csv ut

--- scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/python/PPMLContextWrapperTest.scala
+++ scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/python/PPMLContextWrapperTest.scala
@@ -3,14 +3,42 @@ package com.intel.analytics.bigdl.ppml.python
 import com.intel.analytics.bigdl.ppml.PPMLContext
 import org.scalatest.FunSuite
 
+import java.util
+
 class PPMLContextWrapperTest extends FunSuite {
   val ppmlContextWrapper: PPMLContextWrapper[Float] = PPMLContextWrapper.ofFloat
 
-  var sc: PPMLContext = null
-
-  test("init with app name") {
-    sc = ppmlContextWrapper.createPPMLContext("test")
+  def initArgs(): util.Map[String, String] = {
+    val args = new util.HashMap[String, String]()
+    args.put("spark.bigdl.kms.type", "SimpleKeyManagementService")
+    args.put("spark.bigdl.kms.simple.id", "465227134889")
+    args.put("spark.bigdl.kms.simple.key", "799072978028")
+    args
   }
 
+  test("init PPMLContext with app name") {
+    val appName = "test"
+    ppmlContextWrapper.createPPMLContext(appName)
+  }
+
+  test("init PPMLContext with app name & args") {
+    val appName = "test"
+    val args = initArgs()
+    ppmlContextWrapper.createPPMLContext(appName, args)
+  }
+
+  test("read plain text csv file") {
+    val appName = "test"
+    val args = initArgs()
+    val cryptoMode = "plain_text"
+    val path = this.getClass.getClassLoader.getResource("people.csv").getPath
+    
+    val sc = ppmlContextWrapper.createPPMLContext(appName, args)
+    val encryptedDataFrameReader = ppmlContextWrapper.read(sc, cryptoMode)
+    ppmlContextWrapper.option(encryptedDataFrameReader, "header", "true")
+    val df = ppmlContextWrapper.csv(encryptedDataFrameReader, path)
+
+    assert(df.count() == 100)
+  }
 
 }
