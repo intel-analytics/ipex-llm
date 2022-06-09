e7138b41273e68d1dce451de6fca6dcc57db7bad
Hu Mingzhi <mingzhi.hu@intel.com>
2022-06-09 02:53:34 +0000

    add nano unit test for tensorflow notebooks

--- python/nano/notebooks/tensorflow/stanford_dogs/run-nano-notebooks-tensorflow-stanford_dogs-tests.sh
+++ python/nano/notebooks/tensorflow/stanford_dogs/run-nano-notebooks-tensorflow-stanford_dogs-tests.sh
@@ -3,19 +3,34 @@
 export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
 export TENSORFLOW_NANO_NOTEBOOKS_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/notebooks/tensorflow/stanford_dogs
-export FIT_STEPS=4
+export FIT_STEPS=2
 export VAL_STEPS=2
 export FREEZE_EPOCHS=2
 export UNFREEZE_EPOCHS=1
-export TEST_STEPS=2
+export BATCH_SIZE=2
+export TEST_STEPS=128
 
 set -e
 
 echo "# Start Testing stanford_dogs Fit Notebook"
+start=$(date "+%s")
 
 python -m pytest --nbmake --nbmake-timeout=300 --nbmake-kernel=python3 ${TENSORFLOW_NANO_NOTEBOOKS_DIR}/nano_tensorflow_fit_example.ipynb
 
-echo "# Start Testing stanford_dogs Inference Notebook"
+now=$(date "+%s")
+time=$((now-start))
 
-jupyter nbconvert --to script ${TENSORFLOW_NANO_NOTEBOOKS_DIR}/nano_tensorflow_inference_example.ipynb
-python ${TENSORFLOW_NANO_NOTEBOOKS_DIR}/nano_tensorflow_inference_example.py
+echo "Bigdl-nano cifar10 train notebook test finished"
+echo "Time used:$time seconds"
+
+echo "# Start Testing stanford_dogs Inference Notebook"
+start=$(date "+%s")
+
+jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name python3 --execute ${TENSORFLOW_NANO_NOTEBOOKS_DIR}/nano_tensorflow_inference_example.ipynb
+
+now=$(date "+%s")
+time=$((now-start))
+
+echo "Bigdl-nano cifar10 train notebook test finished"
+echo "Time used:$time seconds"
+
