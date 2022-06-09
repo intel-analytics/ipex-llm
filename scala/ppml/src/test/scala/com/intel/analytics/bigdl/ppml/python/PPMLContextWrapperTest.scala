11e6fdc78003c3c12fb11a3bbb370469e3178c47
Zhou <jian.zhou@intel.com>
2022-06-09 11:50:07 +0800

    update

--- scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/python/PPMLContextWrapperTest.scala
+++ scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/python/PPMLContextWrapperTest.scala
@@ -3,7 +3,7 @@ package com.intel.analytics.bigdl.ppml.python
 import com.intel.analytics.bigdl.ppml.PPMLContext
 import org.scalatest.FunSuite
 
-class PPMLContextWrapperSpec extends FunSuite {
+class PPMLContextWrapperTest extends FunSuite {
   val ppmlContextWrapper: PPMLContextWrapper[Float] = PPMLContextWrapper.ofFloat
 
   var sc: PPMLContext = null
