1b17f939651aa40a64593c4f405feeebc494d93f
Chen, Zhentao <zhentao.chen@intel.com>
2022-06-09 13:30:02 +0800

    enable tf trace

---
+++ python/nano/src/bigdl/nano/tf/keras/inference_utils.py
@@ -0,0 +1,26 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import tensorflow as tf
+from bigdl.nano.deps.openvino.openvino_api import KerasOpenVINOModel
+
+
+class InferenceUtils:
+    """A mixedin class for nano keras Sequential and Model, adding more functions."""
+
+    def trace(self, accelerator=None, input_sample=None):
+        if accelerator == 'openvino':
+            return KerasOpenVINOModel(self, input_sample)
---
+++ python/nano/test/openvino/tf/test_openvino.py
@@ -0,0 +1,38 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from unittest import TestCase
+import tensorflow as tf
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+import numpy as np
+from bigdl.nano.tf.keras import Model
+
+
+class TestOpenVINO(TestCase):
+    def test_model_trace_openvino(self):
+        model = MobileNetV2(weights=None, input_shape=[40, 40, 3], classes=10)
+        model = Model(inputs=model.inputs, outputs=model.outputs)
+        train_examples = np.random.random((100, 40, 40, 3))
+        train_labels = np.random.randint(0, 10, size=(100,))
+        train_dataset = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
+
+        # trace a torch model
+        openvino_model = model.trace(accelerator='openvino')
+        y_hat = openvino_model(train_examples[:3])
+        assert y_hat.shape == (3, 10)
+        y_hat = openvino_model(train_examples[:10])
+        assert y_hat.shape == (10, 10)
+
+        openvino_model.predict(train_dataset)
