da789c56e624e70943e899b6095954c4b98557d6
jenniew <jenniewang123@gmail.com>
2022-06-10 14:00:03 -0700

    license

--- scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/examples/tpch/Q12.scala
+++ scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/examples/tpch/Q12.scala
@@ -12,7 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */
+
+/*
  * This file is copied from:
  * https://github.com/ssavvides/tpch-spark/blob/master/src/main/scala/Q12.scala
  *
--- scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/examples/tpch/TpchQuery.scala
+++ scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/examples/tpch/TpchQuery.scala
@@ -12,7 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */
+
+/*
  * This file is adapted from
  * https://github.com/ssavvides/tpch-spark/blob/master/src/main/scala/TpchQuery.scala
  *
