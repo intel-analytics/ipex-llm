e26431727f2b4c60bbfd373b7fc78a290ab7c5dd
Wang, Yang <yang3.wang@intel.com>
2022-06-15 07:50:01 +0000

    add license and add ipex

--- python/nano/benchmark/pytorch_resnet50_finetune.sh
+++ python/nano/benchmark/pytorch_resnet50_finetune.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
 
@@ -6,6 +21,8 @@ $CONDA/bin/conda create -n benchmark-resnet-50 -y python==3.7.10 setuptools==58.
 $CONDA/bin/activate benchmark-resnet-50
 
 bash $ANALYTICS_ZOO_ROOT/python/nano/dev/build_and_install.sh linux default false pytorch
+python -m pip install torch_ipex==1.9.0 -f https://software.intel.com/ipex-whl-stable
+python -m pip install torchvision==0.10.0+cpu -f https://download.pytorch.org/whl/torch_stable.html
 
 echo "Nano_Perf: Running PyTorch Baseline"
 python pytorch-cat-dog.py --batch_size 32 --name "PyTorch Baseline"
