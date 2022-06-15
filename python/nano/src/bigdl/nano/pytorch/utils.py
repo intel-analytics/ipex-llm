f54611caeba095131b45d52b01d0e93701acb547
Hu Mingzhi <mingzhi.hu@intel.com>
2022-06-15 03:47:31 +0000

    fix yml

--- python/nano/src/bigdl/nano/pytorch/utils.py
+++ python/nano/src/bigdl/nano/pytorch/utils.py
@@ -18,4 +18,4 @@ import operator
 from pytorch_lightning.utilities.imports import _compare_version
 
 TORCH_VERSION_LESS_1_10 = _compare_version("torch", operator.lt, "1.10")
-TORCH_VERSION_LESS_1_11 = _compare_version("torch", operator.lt. "1.11")
+TORCH_VERSION_LESS_1_11 = _compare_version("torch", operator.lt, "1.11")
