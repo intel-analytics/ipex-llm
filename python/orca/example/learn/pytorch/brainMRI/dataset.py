c451e055c1fd147a80a8d611f2fff36d125f04de
yaxinchen666 <leepace666666@sjtu.edu.cn>
2022-06-15 16:54:24 +0800

    delete some package not used and the content about the bigdl backend.

--- python/orca/example/learn/pytorch/brainMRI/dataset.py
+++ python/orca/example/learn/pytorch/brainMRI/dataset.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 import numpy as np
 import random
@@ -23,7 +22,7 @@ import torchvision.transforms as tt
 import torch.utils.data as data
 
 
-def set_seed(seed = 0):
+def set_seed(seed=0):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     torch.manual_seed(seed)
@@ -31,36 +30,34 @@ def set_seed(seed = 0):
     np.random.seed(seed)
     random.seed(seed)
 
+
 def diagnosis(mask_path):
     return 1 if np.max(cv2.imread(mask_path)) > 0 else 0
-    
+
 
 class BrainDataset(data.Dataset):
     def __init__(self, df, transform=None):
         self.df = df
         self.transform = transform
-        
+
     def __len__(self):
         return len(self.df)
-    
+
     def __getitem__(self, idx):
         image = cv2.imread(self.df.iloc[idx, 0])
-        image = np.array(image)/255.
+        image = np.array(image) / 255.
         mask = cv2.imread(self.df.iloc[idx, 1], 0)
-        mask = np.array(mask)/255.
-        
+        mask = np.array(mask) / 255.
+
         if self.transform is not None:
             aug = self.transform(image=image, mask=mask)
             image = aug['image']
             mask = aug['mask']
-        
-        image = image.transpose((2,0,1))
+
+        image = image.transpose((2, 0, 1))
         image = torch.from_numpy(image).type(torch.float32)
         image = tt.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))(image)
-        mask = np.expand_dims(mask, axis=-1).transpose((2,0,1))
+        mask = np.expand_dims(mask, axis=-1).transpose((2, 0, 1))
         mask = torch.from_numpy(mask).type(torch.float32)
-    
+
         return image, mask
-
-
-
