1e79bf269bef2a3ad3c39d5365a891810d4b51fb
MeouSker77 <MeouSker77@outlook.com>
2022-06-16 17:33:57 +0800

    update ddp_subprocess for ipex==1.11

--- python/nano/src/bigdl/nano/pytorch/plugins/ddp_subprocess.py
+++ python/nano/src/bigdl/nano/pytorch/plugins/ddp_subprocess.py
@@ -37,19 +37,15 @@ import os
 import subprocess
 import sys
 import copy
-from typing import Any, List, Optional, Callable
+from typing import Any, Optional, Callable
 from tempfile import TemporaryDirectory
-
-import torch
 
 import pytorch_lightning as pl
 from pytorch_lightning.strategies.launchers import _Launcher
-from pytorch_lightning.strategies import Strategy, DDPSpawnStrategy
-from pytorch_lightning.plugins.environments import LightningEnvironment
+from pytorch_lightning.strategies import Strategy
 
+from bigdl.nano.pytorch.plugins.ddp_spawn import DDPSpawnStrategy
 from bigdl.nano.common.cpu_schedule import schedule_workers
-from bigdl.nano.deps.ipex.ipex_api import ipex_device, ipex_optimize, create_IPEXAccelerator, create_IPEXAccelerator_1_9
-from bigdl.nano.pytorch.utils import TORCH_VERSION_LESS_1_10
 
 import logging
 
@@ -93,24 +89,22 @@ class _DDPSubprocessLauncher(_Launcher):
         """
         os.environ["MASTER_PORT"] = str(self._strategy.cluster_environment.main_port)
 
-        strategy = self._strategy
-        cpu_procs = schedule_workers(strategy.num_processes)
-
-        if strategy.is_global_zero and not torch.distributed.is_initialized():
-            log.info("-" * 100)
-            log.info(f"distributed_backend={strategy.distributed_backend}")
-            log.info(f"All DDP processes registered. Starting ddp with {strategy.world_size} processes")
-            log.info("-" * 100)
+        if self._strategy.cpu_for_each_process is None:
+            cpu_procs = schedule_workers(self._strategy.num_processes)
+        else:
+            cpu_procs = self._strategy.cpu_for_each_process
 
         with TemporaryDirectory() as temp_dir:
-            with open(os.path.join(temp_dir, "args.pkl"), 'wb') as f:
-                args = strategy
-                cloudpickle.dump(args, f)
+            with open(os.path.join(temp_dir, "strategy.pkl"), "wb") as f:
+                cloudpickle.dump(self._strategy, f)
+            with open(os.path.join(temp_dir, "args.pkl"), "wb") as f:
+                cloudpickle.dump((args, kwargs), f)
+            with open(os.path.join(temp_dir, "function.pkl"), 'wb') as f:
+                cloudpickle.dump(function, f)
 
             processes = []
             cwd_path = os.path.split(os.path.realpath(__file__))[0]
-            for i in range(strategy.num_processes):
-
+            for i in range(self._strategy.num_processes):
                 env = copy.deepcopy(os.environ)
 
                 env.update({
@@ -119,6 +113,8 @@ class _DDPSubprocessLauncher(_Launcher):
                     "OMP_NUM_THREADS": str(len(cpu_procs[i])),
                     "PROCESS_IDX": str(i),
                 })
+                log.debug(f"[Process {i}]: using KMP_AFFINITY: {env['KMP_AFFINITY']}")
+                log.debug(f"[Process {i}]: using OMP_NUM_THREADS: {env['OMP_NUM_THREADS']}")
 
                 processes.append(subprocess.Popen([sys.executable, f"{cwd_path}/worker.py",
                                                 temp_dir], env=env))
@@ -130,115 +126,10 @@ class _DDPSubprocessLauncher(_Launcher):
                 assert process.returncode == 0, "Subprocess incorrectly exit, \
                                                 check the trainer configure or usage"
 
-            with open(os.path.join(temp_dir, "results.pkl"), "rb") as f:
-                queue_list = cloudpickle.load(f)
-                strategy.mp_queue = queue_loader(queue_list)
-
 
 class DDPSubprocessStrategy(DDPSpawnStrategy):
 
-    distributed_backend = "ddp_subprocess"
-
-    def __init__(
-        self,
-        num_processes: int = 1,
-        cpu_for_each_process: Optional[List[List[int]]] = None,
-        use_ipex=False,
-        enable_bf16=False,
-        **kwargs: Any
-    ):
-        """Create a DDPSpawnStrategy, adding a cpu_for_each_process parameter."""
-        device = ipex_device() if use_ipex and TORCH_VERSION_LESS_1_10 else 'cpu'
-        parallel_devices = [torch.device(device) for _ in range(num_processes)]
-        cluster_environment = LightningEnvironment()
-
-        if use_ipex and TORCH_VERSION_LESS_1_10 and 'accelerator' not in kwargs:
-            super().__init__(accelerator=create_IPEXAccelerator_1_9(),
-                             parallel_devices=parallel_devices,
-                             cluster_environment=cluster_environment, **kwargs)
-        else:
-            super().__init__(parallel_devices=parallel_devices,
-                             cluster_environment=cluster_environment, **kwargs)
-        self.cpu_for_each_process = cpu_for_each_process
-        self.use_ipex = use_ipex
-        self.enable_bf16 = enable_bf16
+    strategy_name = "ddp_subprocess"
 
     def _configure_launcher(self):
         self._launcher = _DDPSubprocessLauncher(self)
-
-
-
-
-# class DDPSubprocessPlugin(DDPSpawnPlugin):
-#     """
-#     Extending DDPSpawnPlugin to support launch subprocesses with optimized env variables.
-
-#     Instead of using python multiprocessing.spawn, this plugin use subprocess.Popen to start
-#     a new process in order to run mulit-instance training in a jupyter notebook.
-#     """
-
-#     distributed_backend = "ddp_subprocess"
-
-#     def _run_subprocess(self, tmpdir):
-#         from bigdl.nano.common.cpu_schedule import schedule_workers
-
-#         cpu_procs = schedule_workers(self.num_processes)
-
-#         processes = []
-#         cwd_path = os.path.split(os.path.realpath(__file__))[0]
-#         for i in range(self.num_processes):
-
-#             env = copy.deepcopy(os.environ)
-
-#             env.update({
-#                 "KMP_AFFINITY": f"granularity=fine,proclist"
-#                                 f"=[{','.join([str(i) for i in cpu_procs[i]])}],explicit",
-#                 "OMP_NUM_THREADS": str(len(cpu_procs[i])),
-#                 "PROCESS_IDX": str(i),
-#             })
-
-#             processes.append(subprocess.Popen([sys.executable, f"{cwd_path}/worker.py",
-#                                                tmpdir], env=env))
-
-#         return processes
-
-#     def start_training(self, trainer):
-#         """Run training in multiple processes."""
-#         self.model._ortsess = None
-#         self._execution_loop()
-#         trainer.optimizers = []
-
-#     def start_evaluating(self, trainer):
-#         """Run evaluating in multiple processes."""
-#         print("evaluate")
-#         self._execution_loop()
-
-#     def start_predicting(self, trainer):
-#         """Run predicting in multiple processes."""
-#         print("predict")
-#         self._execution_loop()
-
-#     def _execution_loop(self):
-#         if self.is_global_zero and not torch.distributed.is_initialized():
-#             log.info("-" * 100)
-#             log.info(f"distributed_backend={self.distributed_backend}")
-#             log.info(f"All DDP processes registered. Starting ddp with {self.world_size} processes")
-#             log.info("-" * 100)
-
-#         with TemporaryDirectory() as temp_dir:
-#             with open(os.path.join(temp_dir, "args.pkl"), 'wb') as f:
-#                 args = self
-#                 cloudpickle.dump(args, f)
-
-#             processes = self._run_subprocess(temp_dir)
-
-#             for _, process in enumerate(processes):
-#                 process.wait()
-
-#             for _, process in enumerate(processes):
-#                 assert process.returncode == 0, "Subprocess incorrectly exit, \
-#                                                 check the trainer configure or usage"
-
-#             with open(os.path.join(temp_dir, "results.pkl"), "rb") as f:
-#                 queue_list = cloudpickle.load(f)
-#                 self.mp_queue = queue_loader(queue_list)
--- python/nano/src/bigdl/nano/pytorch/plugins/worker.py
+++ python/nano/src/bigdl/nano/pytorch/plugins/worker.py
@@ -14,67 +14,28 @@
 # limitations under the License.
 #
 
-import multiprocessing
 import os
 import sys
 
 import cloudpickle
 
-from pytorch_lightning.utilities.seed import reset_seed
-
-from bigdl.nano.pytorch.plugins.ddp_subprocess import queue_dumper
-from bigdl.nano.pytorch.utils import TORCH_VERSION_LESS_1_10
-from bigdl.nano.deps.ipex.ipex_api import ipex_device, ipex_optimize
-import torch
-import warnings
-
 
 if __name__ == '__main__':
     temp_dir = sys.argv[1]
 
-    with open(os.path.join(temp_dir, "args.pkl"), 'rb') as f:
-        args = cloudpickle.load(f)
+    with open(os.path.join(temp_dir, "strategy.pkl"), 'rb') as f:
+        strategy = cloudpickle.load(f)
+    with open(os.path.join(temp_dir, "args.pkl"), "rb") as f:
+        (args, kwargs) = cloudpickle.load(f)
+    with open(os.path.join(temp_dir, "function.pkl"), 'rb') as f:
+        function = cloudpickle.load(f)
 
-    plugin = args
-    trainer = plugin.lightning_module.trainer
-    plugin.mp_queue = multiprocessing.SimpleQueue()
     process_idx = int(os.environ["PROCESS_IDX"])
 
-    # reset_seed()
-    # plugin.set_world_ranks(process_idx)
-    # rank_zero_only.rank = plugin.global_rank
+    strategy._worker_setup(process_idx)
 
-    plugin._worker_setup(process_idx)
+    results = function(*args, **kwargs)
 
-    # plugin.init_ddp_connection(plugin.global_rank, plugin.world_size)
-
-    # plugin.dist.rank = plugin.global_rank
-    # plugin.dist.device = plugin.root_device
-
-    if plugin.use_ipex and not TORCH_VERSION_LESS_1_10:
-        dtype = torch.bfloat16 if plugin.enable_bf16 else None
-        num_optimizers = len(plugin.optimizers)
-        if num_optimizers == 1:
-            optimizer = plugin.optimizers[0]
-            ipex_optimize(plugin.model, optimizer=optimizer,
-                          inplace=True, dtype=dtype)
-        elif num_optimizers == 0:
-            plugin.model.eval()
-            ipex_optimize(plugin.model, inplace=True, dtype=dtype)
-        else:
-            warnings.warn(f"IPEX currently only support single optimizers, "
-                          f"but got {num_optimizers}. Skip IPEX")
-
-    plugin.configure_ddp()
-
-    plugin.model_to_device()
-
-    plugin.barrier()
-    results = trainer.run_stage()
-
-    plugin.transfer_distrib_spawn_state_on_fit_end(results)
-    if plugin.global_rank == 0:
-        with open(os.path.join(temp_dir,
-                               "results.pkl"), "wb") as f:
-            results = queue_dumper(plugin.mp_queue)
+    if strategy.global_rank == 0:
+        with open(os.path.join(temp_dir, "results.pkl"), "wb") as f:
             cloudpickle.dump(results, f)
