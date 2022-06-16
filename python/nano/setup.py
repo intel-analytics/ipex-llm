a9fc1e744d8620484d3b796bf8a41e8c1d69a673
Hu Mingzhi <mingzhi.hu@intel.com>
2022-06-16 08:50:04 +0000

    fix setup.py

--- python/nano/setup.py
+++ python/nano/setup.py
@@ -72,12 +72,12 @@ def setup_package():
 
     pytorch_requires = ["torch==1.11.0",
                         "torchvision==0.12.0",
-                       "intel_extension_for_pytorch",
                         "pytorch_lightning==1.4.2",
                         "torchmetrics==0.7.2",
                         "opencv-python-headless",
                         "PyTurboJPEG",
-                        "opencv-transforms"]
+                        "opencv-transforms",
+                        "intel_extension_for_pytorch"]
 
     install_requires = ["intel-openmp", "cloudpickle", "protobuf==3.20.1"]
 
