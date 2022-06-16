8a035930cc6720ca3d485867ad27c2d9059d1feb
Hu Mingzhi <mingzhi.hu@intel.com>
2022-06-16 08:07:33 +0000

    fix errors in setup.py

--- python/nano/setup.py
+++ python/nano/setup.py
@@ -72,7 +72,7 @@ def setup_package():
 
     pytorch_requires = ["torch==1.11.0",
                         "torchvision==0.12.0",
-                       "intel_extension_for_pytorch" 
+                       "intel_extension_for_pytorch",
                         "pytorch_lightning==1.4.2",
                         "torchmetrics==0.7.2",
                         "opencv-python-headless",
