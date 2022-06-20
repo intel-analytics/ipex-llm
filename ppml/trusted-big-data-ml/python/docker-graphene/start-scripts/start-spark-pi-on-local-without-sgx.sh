a1f4fb056f4f8df5bdb0bc3d09539127cde83942
Shaojun Liu <61072813+liu-shaojun@users.noreply.github.com>
2022-06-20 18:32:35 +0800

    Create start-spark-pi-on-local-without-sgx.sh

---
+++ ppml/trusted-big-data-ml/python/docker-graphene/start-scripts/start-spark-pi-on-local-without-sgx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ 
+export SGX_ENABLED=false
+unset SPARK_MODE
+bash bigdl-ppml-submit.sh \
+        --master local[2] \
+        --class org.apache.spark.examples.SparkPi \
+        --name spark-pi \
+        --verbose \
+        local:///ppml/trusted-big-data-ml/work/spark-3.1.2/examples/jars/spark-examples_2.12-3.1.2.jar 3000
