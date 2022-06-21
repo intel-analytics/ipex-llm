6d72d92179d9973c75a2a82ccbfbaaa70a209a01
Shaojun Liu <61072813+liu-shaojun@users.noreply.github.com>
2022-06-21 15:06:37 +0800

    Update start-spark-pi-on-local-sgx.sh

--- ppml/trusted-big-data-ml/python/docker-graphene/start-scripts/start-spark-pi-on-local-sgx.sh
+++ ppml/trusted-big-data-ml/python/docker-graphene/start-scripts/start-spark-pi-on-local-sgx.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
- 
-export SGX_ENABLED=true
-unset SPARK_MODE
+
 bash bigdl-ppml-submit.sh \
         --master local[2] \
+        --sgx-enabled true \
+        --sgx-log-level error \
+        --sgx-driver-memory 64g\
+        --sgx-driver-jvm-memory 12g\
+        --sgx-executor-memory 64g\
+        --sgx-executor-jvm-memory 12g\
+        --driver-memory 32g \
+        --driver-cores 8 \
+        --executor-memory 32g \
+        --executor-cores 8 \
+        --num-executors 2 \
         --class org.apache.spark.examples.SparkPi \
         --name spark-pi \
         --verbose \
