e8422ee247c5fdd147b4d417bf1f1f9ef8f10012
MeouSker77 <MeouSker77@outlook.com>
2022-06-23 08:58:38 +0800

    fix style check

--- python/nano/src/bigdl/nano/pytorch/plugins/worker.py
+++ python/nano/src/bigdl/nano/pytorch/plugins/worker.py
@@ -22,11 +22,10 @@ import multiprocessing
 from torch.multiprocessing.spawn import _wrap
 
 
-
 if __name__ == '__main__':
     temp_dir = sys.argv[1]
     process_idx = int(os.environ["PROCESS_IDX"])
-    
+
     # set the same `multiprocessing.current_process().authkey` as the main process
     # so that we can load the `args.pkl`
     authkey = bytes(os.environ['AUTHKEY'], encoding='utf-8')
