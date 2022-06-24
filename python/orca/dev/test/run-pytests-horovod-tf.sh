b386f652d25ea75e9d750db7301518d37f0aa43b
Yu Shan <shan.yu@intel.com>
2022-06-24 16:00:40 +0800

    rename test_tf2estimator_tf2_backend.py -> test_tf2estimator_ray_backend.py

--- python/orca/dev/test/run-pytests-horovod-tf.sh
+++ python/orca/dev/test/run-pytests-horovod-tf.sh
@@ -28,5 +28,5 @@ echo "Running RayOnSpark tests"
 python -m pytest -v test/bigdl/orca/learn/ray/tf/ \
     --ignore=test/bigdl/orca/learn/ray/tf/test_tf2estimator_ray_runtime.py \
     --ignore=test/bigdl/orca/learn/ray/tf/test_tf2estimator_ray_dataset.py \
-    --ignore=test/bigdl/orca/learn/ray/tf/test_tf2estimator_tf2_backend.py
+    --ignore=test/bigdl/orca/learn/ray/tf/test_tf2estimator_ray_backend.py
 python -m pytest -v test/bigdl/orca/data/test_read_parquet_images.py
