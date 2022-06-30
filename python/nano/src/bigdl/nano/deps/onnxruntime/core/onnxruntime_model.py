39a2077eb37384ce5bb983d747a0c239921219ef
ruoan1.wang <ruonan1.wang@intel.com>
2022-06-30 06:01:55 -0400

    modify as reviews

--- python/nano/src/bigdl/nano/deps/onnxruntime/core/onnxruntime_model.py
+++ python/nano/src/bigdl/nano/deps/onnxruntime/core/onnxruntime_model.py
@@ -30,9 +30,8 @@ class ONNXRuntimeModel:
         '''
         This function run through the onnxruntime forwarding step
         '''
-        if len(self._forward_args) < len(inputs):
-            invalidInputError(False, "The length of inputs is inconsistent with the length of "
-                                     "ONNXRuntime's inputs, there may be some redundant inputs.")
+        invalidInputError(len(self._forward_args) >= len(inputs), "The length of inputs is inconsistent with "
+                          "the length of ONNX Runtime session's inputs, there may be some redundant inputs.")
         inputs = dict(zip(self._forward_args, inputs))
         ort_outs = self.ortsess.run(None, inputs)
         return ort_outs
