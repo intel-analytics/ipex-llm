d6c18374b5a71c11e5c4a9f2ab7feb7413d5d00e
Django-Jiang <1205034803@qq.com>
2022-06-30 09:35:39 +0800

    fix style

--- python/nano/src/bigdl/nano/tf/keras/Model.py
+++ python/nano/src/bigdl/nano/tf/keras/Model.py
@@ -14,54 +14,10 @@
 # limitations under the License.
 #
 import tensorflow as tf
-from bigdl.nano.tf.keras.training_utils import TrainingUtils
-from bigdl.nano.tf.keras.inference_utils import InferenceUtils
-
-
-class Model(TrainingUtils, InferenceUtils, tf.keras.Model):
-    """A wrapper class for tf.keras.Model adding more functions for BigDL-Nano."""
-
-    def __init__(self, *args, **kwargs):
-        """Create a nano Sequential model, having the same arguments with tf.keras.Sequential."""
-        super().__init__(*args, **kwargs)
-
-
-#
-# Copyright 2016 The BigDL Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import tensorflow as tf
 from tensorflow.keras import Model as Model
 from bigdl.nano.tf.keras.training_utils import TrainingUtils
 from bigdl.nano.tf.keras.inference_utils import InferenceUtils
 from bigdl.nano.tf.keras.inheritance_utils import inject_function
-
-
-# class Model(TrainingUtils, InferenceUtils):
-#     """A wrapper class for tf.keras.Model adding more functions for BigDL-Nano."""
-
-#     def __init__(self, *args, **kwargs):
-#         """Create a nano Sequential model, having the same arguments with tf.keras.Sequential."""
-#         self.model = TFModel(*args, **kwargs)
-#         super().__init__()
-
-
-# def f_wrapper(f):
-#     """A wrapper function to overide desired method."""
-#     def wrapped(self, *args, **kwargs):
-#         return f(self.model, *args, **kwargs)
-#     return wrapped
 
 
 # override_method(Model, TFModel, f_wrapper)
--- python/nano/src/bigdl/nano/tf/keras/inheritance_utils.py
+++ python/nano/src/bigdl/nano/tf/keras/inheritance_utils.py
@@ -13,40 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# def extended_method(*classes):
-#     """A helper function to extract all extend method from base classes."""
-#     name_set = set()
-#     for extend_class in classes:
-#         for name in dir(extend_class):
-#             if not name.startswith("_"):
-#                 name_set.add(name)
-#     return name_set
-
-
-# def override_method(target_cls, inherited_cls, f_wrapper):
-#     """
-#     Override target cls with inherited class method to \
-#     avoid functional usage issue from tf.keras submodule.
-#     For example, to make nano.tf.keras.Model inherite tf.keras.Model
-#     without functional usage issue.
-#     :param target_cls:      nano extended class, e.g. nano.tf.keras.Model
-#     :param inherited_cls:   desired inherited class from tf.keras, e.g. tf.keras.Model
-#     :param f_wrapper:       wrapper function to map class method
-#     """
-#     extend_methods = extended_method(target_cls.__bases__)
-#     # map all public method inherited class from tf.keras
-#     for name in dir(inherited_cls):
-#         if name not in extend_methods and not name.startswith('_'):
-#             setattr(target_cls, name, f_wrapper(getattr(inherited_cls, name)))
-
-#     # map all public method from base class
-#     for cls in target_cls.__bases__:
-#         for name in dir(cls):
-#             if not name.startswith("_"):
-#                 setattr(target_cls, name, f_wrapper(getattr(cls, name)))
-
-
 def inject_function(target_cls, *injected_cls):
     """
     Inject function to base directly.
