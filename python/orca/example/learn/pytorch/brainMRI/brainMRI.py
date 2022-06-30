d42c8368bc74002625c91c79cbdf29e0b9ad242c
YingzhePeng <78460824+ForJadeForest@users.noreply.github.com>
2022-06-30 17:33:43 +0800

    Update BrainMRI example (#4848)
    * modify the readme, delete some error information and commands
    
    * delete some package not used and the content about the bigdl backend.
    
    * delete the error pip commands and add the citation of BigDL
    
    * delete the replace codes. add num_nodes parameter in init_orca_context.
    
    * delete the replace codes. add num_nodes parameter in init_orca_context.
    
    * Revert "delete the error pip commands and add the citation of BigDL"
    
    This reverts commit afbf29f6b3401ca8571a74a46d11673f15763039.
    
    * delete the error pip command and add the citation of the BidDL.
    
    * delete the path replacing codes. add num_nodes parameter in init_orca_context. add some help of args. add the thanks to S0MNATHS
    
    * add a memory args and a model_dir parameter in the Estimator to support the spark backend.
    
    * modify the website of the original code and add the help information of memory args.
    
    * modify the pip command, add the necessary packages and delete a package not used
    
    * add #
    
    * add the dataset preparation and the explanation of the parameters
    
    * add the model_dir parameter explanation and add the yarn-client with spark backend command
    
    * add the visual function. add the model_dir parameter. Delete the evaluation process. add the conf parameter of init_orca_mode when use yarn-client mode.
    
    * add bigdl backend
    
    * add detail information.
    
    * delete the memory parameter. set a global config. delete the special config when using spark backend and yarn-client cluster_mode
    
    * delete bigdl
    
    * delete some parameters description.
    
    * modify some description. add log4Error.
    
    * modify some description. add log4Error.
    
    * add tqdm requirement
    
    Co-authored-by: yaxinchen666 <leepace666666@sjtu.edu.cn>
    Co-authored-by: ForJadeForest <majorjadeforest@gmail.com>

--- python/orca/example/learn/pytorch/brainMRI/brainMRI.py
+++ python/orca/example/learn/pytorch/brainMRI/brainMRI.py
@@ -12,60 +12,46 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# ==============================================================================
+# Most of the pytorch code is adapted from S0MNATHS's notebook for
+# BrainMRI dataset.
+# https://www.kaggle.com/s0mnaths/brain-mri-unet-pytorch/notebook
 #
 
 from __future__ import print_function
-from UNET import UNet
-from dataset import *
-
-import os
-from os.path import exists
-from os import makedirs
-import argparse
-import matplotlib.pyplot as plt
-import numpy as np
-import glob
-import random
-import cv2
-from PIL import Image
-import pandas as pd
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import albumentations as A
+import pandas as pd
+import argparse
+import glob
+import os
 
-
-import torch
-import torchvision
-import torchvision.transforms as transforms
-from torchvision.utils import make_grid
-import torchvision.transforms as tt
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data as data
-from torch.utils.data import DataLoader
-from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import make_grid
 
 from bigdl.orca import init_orca_context, stop_orca_context
-from bigdl.orca.learn.pytorch import Estimator
 from bigdl.orca.learn.metrics import Accuracy
-from bigdl.orca.learn.trigger import EveryEpoch
+from bigdl.orca.learn.pytorch import Estimator
+from bigdl.dllib.utils.log4Error import invalidInputError
 
-import albumentations as A
+from Unet import UNet
+from dataset import *
 
-def dataset():
-    ROOT_PATH = '/home/mingxuan/bigdl/python/orca/example/learn/pytorch/brainMRI/data/kaggle_3m/'
-    mask_files = glob.glob(ROOT_PATH + '*/*_mask*')
+
+def dataset(root_path):
+    mask_files = glob.glob(root_path + '/*/*_mask*')
     image_files = [file.replace('_mask', '') for file in mask_files]
     files_df = pd.DataFrame({"image_path": image_files,
-                    "mask_path": mask_files,
-                    "diagnosis": [diagnosis(x) for x in mask_files]})
-
+                             "mask_path": mask_files,
+                             "diagnosis": [diagnosis(x) for x in mask_files]})
     train_df, test_df = train_test_split(files_df, stratify=files_df['diagnosis'], test_size=0.15, random_state=0)
     train_df = train_df.reset_index(drop=True)
     test_df = test_df.reset_index(drop=True)
 
     return train_df, test_df
-
-train_df, test_df = dataset()
 
 
 def dice_coef_metric(pred, label):
@@ -75,19 +61,36 @@ def dice_coef_metric(pred, label):
         return 1.
     return intersection / union
 
+
 def dice_coef_loss(pred, label):
     smooth = 1.0
     intersection = 2.0 * (pred * label).sum() + smooth
     union = pred.sum() + label.sum() + smooth
     return 1 - (intersection / union)
 
+
 def bce_dice_loss(pred, label):
     dice_loss = dice_coef_loss(pred, label)
     bce_loss = nn.BCELoss()(pred, label)
     return dice_loss + bce_loss
 
+
 def loss_creator(config):
     return bce_dice_loss
+
+
+def show_batch(dl):
+    for images, masks in dl:
+        fig1, ax1 = plt.subplots(figsize=(24, 24))
+        ax1.set_xticks([])
+        ax1.set_yticks([])
+        denorm_images = denormalize(images)
+        ax1.imshow(make_grid(denorm_images[:13], nrow=13).permute(1, 2, 0).clamp(0, 1))
+        fig2, ax2 = plt.subplots(figsize=(24, 24))
+        ax2.set_xticks([])
+        ax2.set_yticks([])
+        ax2.imshow(make_grid(masks[:13], nrow=13).permute(1, 2, 0).clamp(0, 1))
+        break
 
 
 def train_loader_creator(config, batch_size):
@@ -98,8 +101,8 @@ def train_loader_creator(config, batch_size):
         A.RandomRotate90(p=0.5),
         A.ShiftScaleRotate(shift_limit=0.01, scale_limit=0.04, rotate_limit=0, p=0.25),
     ])
-    
-    train_ds = BrainDataset(train_df, train_transform)
+
+    train_ds = BrainDataset(config['train'], train_transform)
     trainloader = torch.utils.data.DataLoader(train_ds, batch_size=batch_size,
                                               shuffle=True, num_workers=0)
     return trainloader
@@ -110,7 +113,7 @@ def test_loader_creator(config, batch_size):
         A.Resize(width=128, height=128, p=1.0)
     ])
 
-    test_ds = BrainDataset(test_df, test_transform)
+    test_ds = BrainDataset(config['test'], test_transform)
     testloader = torch.utils.data.DataLoader(test_ds, batch_size=batch_size,
                                              shuffle=False, num_workers=0)
     return testloader
@@ -127,96 +130,65 @@ def optim_creator(model, config):
                           momentum=config.get("momentum", 0.9))
     return optimizer
 
+
 def denormalize(images):
     means = torch.tensor([0.485, 0.456, 0.406]).reshape(1, 3, 1, 1)
     stds = torch.tensor([0.229, 0.224, 0.225]).reshape(1, 3, 1, 1)
     return images * stds + means
 
-def show_batch(dl):
-    for images, masks in dl:
-        fig1, ax1 = plt.subplots(figsize=(24, 24))
-        ax1.set_xticks([]); ax1.set_yticks([])
-        denorm_images = denormalize(images)
-        ax1.imshow(make_grid(denorm_images[:13], nrow=13).permute(1, 2, 0).clamp(0,1))
-        
-        fig2, ax2 = plt.subplots(figsize=(24, 24))
-        ax2.set_xticks([]); ax2.set_yticks([])
-        ax2.imshow(make_grid(masks[:13], nrow=13).permute(1, 2, 0).clamp(0,1))
-        break
-        
 
 parser = argparse.ArgumentParser(description='PyTorch brainMRI Example')
 parser.add_argument('--cluster_mode', type=str, default="local",
-                    help='The cluster mode, such as local, yarn-client, yarn-cluster, spark-submit or k8s.')
+                    help='The cluster mode, such as local, yarn-client, or spark-submit.')
 parser.add_argument('--backend', type=str, default="torch_distributed",
-                    help='The backend of PyTorch Estimator; '
-                        'bigdl, torch_distributed and spark are supported')
+                    help='The backend of PyTorch Estimator; torch_distributed and spark are supported')
 parser.add_argument('--batch_size', type=int, default=64, help='The training batch size')
 parser.add_argument('--epochs', type=int, default=2, help='The number of epochs to train for')
-parser.add_argument('--data_dir', type=str, default="./data", help='The path to dataset')
-parser.add_argument('--download', type=bool, default=False, help='Download dataset or not')
-parser.add_argument("--executor_memory", type=str, default="5g", help="executor memory")
-parser.add_argument("--driver_memory", type=str, default="5g", help="driver memory")
+parser.add_argument('--data_dir', type=str, default='./kaggle_3m', help='The path to the dataset')
+parser.add_argument('--additional_archive', type=str, default="kaggle_3m.zip#kaggle_3m",
+                    help='The zip dataset for yarn-client mode')
+parser.add_argument('--model_dir', type=str, default=os.getcwd(),
+                    help="The model save dir for spark backend")
 args = parser.parse_args()
 
 if args.cluster_mode == "local":
-    init_orca_context(memory="4g")
-elif args.cluster_mode.startswith("yarn"):
-    if args.cluster_mode == "yarn-client":
-        init_orca_context(cluster_mode="yarn-client")
-    elif args.cluster_mode == "yarn-cluster":
-        init_orca_context(cluster_mode="yarn-cluster", memory=args.executor_memory, driver_memory=args.driver_memory)
+    init_orca_context(memory='4g')
+elif args.cluster_mode.startswith('yarn'):
+    init_orca_context(cluster_mode="yarn-client", cores=2, num_nodes=2, additional_archive=args.additional_archive,
+                      extra_python_lib='dataset.py,Unet.py', num_executors=2, memory='4g')
 elif args.cluster_mode == "spark-submit":
     init_orca_context(cluster_mode="spark-submit")
+else:
+    invalidInputError(False,
+                      "cluster_mode should be one of 'local', 'yarn', 'standalone' and"
+                      " 'spark-submit', but got " + args.cluster_mode)
 
-tensorboard_dir = args.data_dir+"runs"
-writer = SummaryWriter(tensorboard_dir + '/brainMRI_experiment_1')
-
-criterion = nn.CrossEntropyLoss()
+train_df, test_df = dataset(args.data_dir)
 batch_size = args.batch_size
 epochs = args.epochs
-root_dir = args.data_dir
+config = {
+    'lr': 1e-3,
+    'train': train_df,
+    'test': test_df
+}
+train_loader = train_loader_creator(config=config, batch_size=batch_size)
 
-train_loader = train_loader_creator(config={"root": root_dir}, batch_size=batch_size)
-test_loader = test_loader_creator(config={"root": root_dir}, batch_size=batch_size)
+# plot some random training images.
+# You should use jupyter notebook to show the images.
+show_batch(train_loader)
 
-# plot some random training images
-# show_batch(train_loader)
-
-
-if args.backend == "bigdl":
-    net = model_creator(config={})
-    optimizer = optim_creator(model=net, config={"lr": 0.001})
-    orca_estimator = Estimator.from_torch(model=net,
-                                        optimizer=optimizer,
-                                        loss=criterion,
-                                        metrics=[Accuracy()],
-                                        backend="bigdl")
-
-    orca_estimator.fit(data=train_loader, epochs=args.epochs, validation_data=test_loader,
-                    checkpoint_trigger=EveryEpoch())
-
-    res = orca_estimator.evaluate(data=test_loader)
-    print("Accuracy of the network on the test images: %s" % res)
-elif args.backend in ["torch_distributed", "spark"]:
+if args.backend in ["torch_distributed", "spark"]:
     orca_estimator = Estimator.from_torch(model=model_creator,
-                                        optimizer=optim_creator,
-                                        loss=loss_creator,
-                                        metrics=[Accuracy()],
-                                        backend=args.backend,
-                                        config={"lr": 0.001,
-                                                "root": root_dir},
-                                        use_tqdm=True
-                                        )
+                                          optimizer=optim_creator,
+                                          loss=loss_creator,
+                                          model_dir=args.model_dir,
+                                          backend=args.backend,
+                                          config=config,
+                                          use_tqdm=True)
 
     orca_estimator.fit(data=train_loader_creator, epochs=args.epochs, batch_size=batch_size)
-
-    res = orca_estimator.evaluate(data=test_loader_creator)
-    for r in res:
-        print(r, ":", res[r])
 else:
-    raise NotImplementedError("Only bigdl and torch_distributed are supported as the backend,"
-                            " but got {}".format(args.backend))
+    raise NotImplementedError("Only torch_distributed and spark are supported as the backend,"
+                              " but got {}".format(args.backend))
 
 stop_orca_context()
-
