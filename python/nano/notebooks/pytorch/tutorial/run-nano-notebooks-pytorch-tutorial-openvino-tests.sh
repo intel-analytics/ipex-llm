447d71c8a29b77b240853da29e930d5958efac75
Hu Mingzhi <mingzhi.hu@intel.com>
2022-07-01 05:45:52 +0000

    add unit tests for tutorial notebooks

---
+++ python/nano/notebooks/pytorch/tutorial/run-nano-notebooks-pytorch-tutorial-openvino-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
+export NANO_TUTORIAL_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/notebooks/pytorch/tutorial
+export DEV_RUN=True
+
+set -e
+
+echo "# Start testing"
+start=$(date "+%s")
+python -m pytest -s --nbmake --nbmake-timeout=180 --nbmake-kernel=python3 ${NANO_TUTORIAL_TEST_DIR} -k 'openvino'
+
+now=$(date "+%s")
+time=$((now-start))
+
+echo "Bigdl-nano tests for tutorial finished(OpenVINO)"
+echo "Time used:$time seconds"
