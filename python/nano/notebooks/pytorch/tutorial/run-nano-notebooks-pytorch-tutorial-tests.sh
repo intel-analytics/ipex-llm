da91323caad8fe11fbbb25dffe8803d9e8b7c72f
Hu Mingzhi <mingzhi.hu@intel.com>
2022-07-03 23:37:10 +0000

    Add unit test for tutorial

--- python/nano/notebooks/pytorch/tutorial/run-nano-notebooks-pytorch-tutorial-tests.sh
+++ python/nano/notebooks/pytorch/tutorial/run-nano-notebooks-pytorch-tutorial-tests.sh
@@ -5,12 +5,18 @@ export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
 export NANO_TUTORIAL_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/notebooks/pytorch/tutorial
 export DEV_RUN=True
 
+openvino=$1
+
 set -e
 
 echo "# Start testing"
 start=$(date "+%s")
 python -m pytest -s --nbmake --nbmake-timeout=180 --nbmake-kernel=python3 ${NANO_TUTORIAL_TEST_DIR} -k 'not openvino'
 
+if [ ${openvino} == true ]; then
+    python -m pytest -s --nbmake --nbmake-timeout=180 --nbmake-kernel=python3 ${NANO_TUTORIAL_TEST_DIR} -k 'openvino'
+else
+    python -m pytest -s --nbmake --nbmake-timeout=180 --nbmake-kernel=python3 ${NANO_TUTORIAL_TEST_DIR} -k 'not openvino'
 now=$(date "+%s")
 time=$((now-start))
 
