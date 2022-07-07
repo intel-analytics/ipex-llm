17c1184305badc1b32e832bd84c270c35889de88
Yishuo Wang <MeouSker77@outlook.com>
2022-07-07 09:32:29 +0800

    add ray backend for pytorch_lightning 1.6

--- python/nano/src/bigdl/nano/pytorch/strategies/ray/__init__.py
+++ python/nano/src/bigdl/nano/pytorch/strategies/ray/__init__.py
@@ -13,8 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-from .ipex.ipex_api import create_IPEXStrategy
-from .ddp_spawn import DDPSpawnStrategy
-from .ddp_subprocess import DDPSubprocessStrategy
-from .ray.ray_distributed import RayStrategy
