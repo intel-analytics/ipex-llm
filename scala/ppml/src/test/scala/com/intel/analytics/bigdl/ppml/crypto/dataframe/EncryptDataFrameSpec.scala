0d58c17ca7a021f1be49c0f02a35cf74a360a2c9
qiuxin2012 <qiuxin2012cs@gmail.com>
2022-07-11 10:27:02 +0800

    fix ut

--- scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/crypto/dataframe/EncryptDataFrameSpec.scala
+++ scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/crypto/dataframe/EncryptDataFrameSpec.scala
@@ -16,6 +16,7 @@
 
 package com.intel.analytics.bigdl.ppml.crypto.dataframe
 
+import com.intel.analytics.bigdl.dllib.utils.LoggerFilter
 import com.intel.analytics.bigdl.ppml.PPMLContext
 import com.intel.analytics.bigdl.ppml.crypto.{AES_CBC_PKCS5PADDING, BigDLEncrypt, CryptoCodec, DECRYPT, ENCRYPT, PLAIN_TEXT}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -24,6 +25,9 @@ import org.apache.spark.sql.SparkSession
 import java.io.{File, FileWriter}
 
 class EncryptDataFrameSpec extends DataFrameHelper {
+  LoggerFilter.redirectSparkInfoLogs()
+
+  val (plainFileName, encryptFileName, data, dataKeyPlaintext) = generateCsvData()
 
   val ppmlArgs = Map(
       "spark.bigdl.kms.simple.id" -> appid,
@@ -157,7 +161,7 @@ class EncryptDataFrameSpec extends DataFrameHelper {
     val outFile = dir + "/plain_big_file.csv"
     val enFile = dir + "/en_big_file.csv" + CryptoCodec.getDefaultExtension()
     val fw = new FileWriter(bigFile)
-    val genNum = 40000000
+    val genNum = 400
     (0 until genNum).foreach {i =>
       fw.append(s"gdni,$i,Engineer\npglyal,$i,Engineer\nyvomq,$i,Developer\n")
     }
