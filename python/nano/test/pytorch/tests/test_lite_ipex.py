2e7e9830798ef1edfd6b7b65e0e7d5a2dd63e10d
Yishuo Wang <MeouSker77@outlook.com>
2022-07-12 16:18:08 +0800

    set new unit tests only run when using pl 1.6

--- python/nano/test/pytorch/tests/test_lite_ipex.py
+++ python/nano/test/pytorch/tests/test_lite_ipex.py
@@ -23,54 +23,56 @@ import torch
 from test.pytorch.utils._train_torch_lightning import create_data_loader, data_transform
 from torch import nn
 
-from bigdl.nano.pytorch.lite import LightningLite
-from bigdl.nano.pytorch.vision.models import vision
+from bigdl.nano.pytorch.utils import LIGHTNING_VERSION_LESS_1_6
+if not LIGHTNING_VERSION_LESS_1_6:
+    from bigdl.nano.pytorch.lite import LightningLite
+    from bigdl.nano.pytorch.vision.models import vision
 
-batch_size = 256
-num_workers = 0
-data_dir = os.path.join(os.path.dirname(__file__), "../data")
-
-
-class ResNet18(nn.Module):
-    def __init__(self, num_classes, pretrained=True, include_top=False, freeze=True):
-        super().__init__()
-        backbone = vision.resnet18(pretrained=pretrained, include_top=include_top, freeze=freeze)
-        output_size = backbone.get_output_size()
-        head = nn.Linear(output_size, num_classes)
-        self.model = nn.Sequential(backbone, head)
-
-    def forward(self, x):
-        return self.model(x)
+    batch_size = 256
+    num_workers = 0
+    data_dir = os.path.join(os.path.dirname(__file__), "../data")
 
 
-class Lite(LightningLite):
-    def run(self):
-        model = ResNet18(10, pretrained=False, include_top=False, freeze=True)
-        loss = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-        train_loader = create_data_loader(data_dir, batch_size, num_workers, data_transform)
+    class ResNet18(nn.Module):
+        def __init__(self, num_classes, pretrained=True, include_top=False, freeze=True):
+            super().__init__()
+            backbone = vision.resnet18(pretrained=pretrained, include_top=include_top, freeze=freeze)
+            output_size = backbone.get_output_size()
+            head = nn.Linear(output_size, num_classes)
+            self.model = nn.Sequential(backbone, head)
 
-        model, optimizer = self.setup(model, optimizer)
-        train_loader = self.setup_dataloaders(train_loader)
-        model.train()
-
-        max_epochs = 1
-        for _i in range(max_epochs):
-            total_loss, num = 0, 0
-            for X, y in train_loader:
-                optimizer.zero_grad()
-                l = loss(model(X), y)
-                self.backward(l)
-                optimizer.step()
-                
-                total_loss += l.sum()
-                num += 1
-            print(f'avg_loss: {total_loss / num}')
+        def forward(self, x):
+            return self.model(x)
 
 
-class TestLite(TestCase):
-    def test_lite(self):
-        Lite(use_ipex=True).run()
+    class Lite(LightningLite):
+        def run(self):
+            model = ResNet18(10, pretrained=False, include_top=False, freeze=True)
+            loss = nn.CrossEntropyLoss()
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+            train_loader = create_data_loader(data_dir, batch_size, num_workers, data_transform)
+
+            model, optimizer = self.setup(model, optimizer)
+            train_loader = self.setup_dataloaders(train_loader)
+            model.train()
+
+            max_epochs = 1
+            for _i in range(max_epochs):
+                total_loss, num = 0, 0
+                for X, y in train_loader:
+                    optimizer.zero_grad()
+                    l = loss(model(X), y)
+                    self.backward(l)
+                    optimizer.step()
+                    
+                    total_loss += l.sum()
+                    num += 1
+                print(f'avg_loss: {total_loss / num}')
+
+
+    class TestLite(TestCase):
+        def test_lite(self):
+            Lite(use_ipex=True).run()
 
 
 if __name__ == '__main__':
