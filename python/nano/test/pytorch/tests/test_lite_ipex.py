6ffd4e04435ad1f415c1c11c1c01ca1386b328e9
Yishuo Wang <MeouSker77@outlook.com>
2022-07-19 15:30:01 +0800

    small changes

--- python/nano/test/pytorch/tests/test_lite_ipex.py
+++ python/nano/test/pytorch/tests/test_lite_ipex.py
@@ -24,114 +24,112 @@ from torch.utils.data import DataLoader, TensorDataset
 
 from test.pytorch.utils._train_torch_lightning import create_data_loader, data_transform
 
-from bigdl.nano.pytorch.utils import LIGHTNING_VERSION_LESS_1_6
-if not LIGHTNING_VERSION_LESS_1_6:
-    from bigdl.nano.pytorch.lite import LightningLite
-    from bigdl.nano.pytorch.vision.models import vision
+from bigdl.nano.pytorch.lite import LightningLite
+from bigdl.nano.pytorch.vision.models import vision
 
-    batch_size = 256
-    num_workers = 0
-    data_dir = os.path.join(os.path.dirname(__file__), "../data")
+batch_size = 256
+num_workers = 0
+data_dir = os.path.join(os.path.dirname(__file__), "../data")
 
 
-    class ResNet18(nn.Module):
-        def __init__(self, num_classes, pretrained=True, include_top=False, freeze=True):
-            super().__init__()
-            backbone = vision.resnet18(pretrained=pretrained, include_top=include_top, freeze=freeze)
-            output_size = backbone.get_output_size()
-            head = nn.Linear(output_size, num_classes)
-            self.model = nn.Sequential(backbone, head)
+class ResNet18(nn.Module):
+    def __init__(self, num_classes, pretrained=True, include_top=False, freeze=True):
+        super().__init__()
+        backbone = vision.resnet18(pretrained=pretrained, include_top=include_top, freeze=freeze)
+        output_size = backbone.get_output_size()
+        head = nn.Linear(output_size, num_classes)
+        self.model = nn.Sequential(backbone, head)
 
-        def forward(self, x):
-            return self.model(x)
-
-
-    class Lite(LightningLite):
-        def run(self):
-            model = ResNet18(10, pretrained=False, include_top=False, freeze=True)
-            loss = nn.CrossEntropyLoss()
-            optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-            train_loader = create_data_loader(data_dir, batch_size, num_workers, data_transform)
-
-            model, optimizer = self.setup(model, optimizer)
-            train_loader = self.setup_dataloaders(train_loader)
-            model.train()
-
-            num_epochs = 1
-            for _i in range(num_epochs):
-                total_loss, num = 0, 0
-                for X, y in train_loader:
-                    optimizer.zero_grad()
-                    l = loss(model(X), y)
-                    self.backward(l)
-                    optimizer.step()
-                    
-                    total_loss += l.sum()
-                    num += 1
-                print(f'avg_loss: {total_loss / num}')
+    def forward(self, x):
+        return self.model(x)
 
 
-    class LinearModel(nn.Module):
-        def __init__(self):
-            super().__init__()
-            self.fc1 = nn.Linear(1, 1, bias=False)
-            self.fc1.weight.data.fill_(1.0)
+class Lite(LightningLite):
+    def run(self):
+        model = ResNet18(10, pretrained=False, include_top=False, freeze=True)
+        loss = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+        train_loader = create_data_loader(data_dir, batch_size, num_workers, data_transform)
 
-        def forward(self, input_):
-            return self.fc1(input_)
+        model, optimizer = self.setup(model, optimizer)
+        train_loader = self.setup_dataloaders(train_loader)
+        model.train()
 
-
-    class LiteCorrectness(LightningLite):
-        def run(self, lr):
-            dataset=TensorDataset(
-                torch.tensor([[0.0],[0.0],[1.0],[1.0]]),
-                torch.tensor([[0.0],[0.0],[0.0],[0.0]]),
-            )
-            train_loader = DataLoader(dataset=dataset, batch_size=2, shuffle=False)
-            origin_model = LinearModel()
-            loss = nn.MSELoss()
-            optimizer = torch.optim.SGD(origin_model.parameters(), lr=lr)
-
-            model, optimizer = self.setup(origin_model, optimizer)
-            train_loader = self.setup_dataloaders(train_loader)
-            model.train()
-
-            num_epochs = 2
-            for _i in range(num_epochs):
-                for X, y in train_loader:
-                    optimizer.zero_grad()
-                    l = loss(model(X), y)
-                    self.backward(l)
-                    optimizer.step()
-
-            assert origin_model.fc1.weight.data == 0.25, \
-                f"wrong weights: {origin_model.fc1.weight.data}"
+        num_epochs = 1
+        for _i in range(num_epochs):
+            total_loss, num = 0, 0
+            for X, y in train_loader:
+                optimizer.zero_grad()
+                l = loss(model(X), y)
+                self.backward(l)
+                optimizer.step()
+                
+                total_loss += l.sum()
+                num += 1
+            print(f'avg_loss: {total_loss / num}')
 
 
-    class TestLite(TestCase):
-        def test_lite(self):
-            Lite(use_ipex=True).run()
+class LinearModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(1, 1, bias=False)
+        self.fc1.weight.data.fill_(1.0)
 
-        def test_lite_spawn(self):
-            Lite(use_ipex=True, num_processes=2, strategy="spawn").run()
+    def forward(self, input_):
+        return self.fc1(input_)
 
-        def test_lite_subprocess(self):
-            Lite(use_ipex=True, num_processes=2, strategy="subprocess").run()
 
-        def test_lite_ray(self):
-            Lite(use_ipex=True, num_processes=2, strategy="ray").run()
+class LiteCorrectness(LightningLite):
+    def run(self, lr):
+        dataset=TensorDataset(
+            torch.tensor([[0.0],[0.0],[1.0],[1.0]]),
+            torch.tensor([[0.0],[0.0],[0.0],[0.0]]),
+        )
+        train_loader = DataLoader(dataset=dataset, batch_size=2, shuffle=False)
+        origin_model = LinearModel()
+        loss = nn.MSELoss()
+        optimizer = torch.optim.SGD(origin_model.parameters(), lr=lr)
 
-        def test_lite_correctness(self):
-            LiteCorrectness(use_ipex=True).run(0.25)
+        model, optimizer = self.setup(origin_model, optimizer)
+        train_loader = self.setup_dataloaders(train_loader)
+        model.train()
 
-        def test_lite_spawn_correctness(self):
-            LiteCorrectness(use_ipex=True, num_processes=2, strategy="spawn").run(0.5)
+        num_epochs = 2
+        for _i in range(num_epochs):
+            for X, y in train_loader:
+                optimizer.zero_grad()
+                l = loss(model(X), y)
+                self.backward(l)
+                optimizer.step()
 
-        def test_lite_subprocess_correctness(self):
-            LiteCorrectness(use_ipex=True, num_processes=2, strategy="subprocess").run(0.5)
+        assert origin_model.fc1.weight.data == 0.25, \
+            f"wrong weights: {origin_model.fc1.weight.data}"
 
-        def test_lite_ray_correctness(self):
-            LiteCorrectness(use_ipex=True, num_processes=2, strategy="ray").run(0.5)
+
+class TestLite(TestCase):
+    def test_lite(self):
+        Lite(use_ipex=True).run()
+
+    def test_lite_spawn(self):
+        Lite(use_ipex=True, num_processes=2, strategy="spawn").run()
+
+    def test_lite_subprocess(self):
+        Lite(use_ipex=True, num_processes=2, strategy="subprocess").run()
+
+    def test_lite_ray(self):
+        Lite(use_ipex=True, num_processes=2, strategy="ray").run()
+
+    def test_lite_correctness(self):
+        LiteCorrectness(use_ipex=True).run(0.25)
+
+    def test_lite_spawn_correctness(self):
+        LiteCorrectness(use_ipex=True, num_processes=2, strategy="spawn").run(0.5)
+
+    def test_lite_subprocess_correctness(self):
+        LiteCorrectness(use_ipex=True, num_processes=2, strategy="subprocess").run(0.5)
+
+    def test_lite_ray_correctness(self):
+        LiteCorrectness(use_ipex=True, num_processes=2, strategy="ray").run(0.5)
 
 
 if __name__ == '__main__':
