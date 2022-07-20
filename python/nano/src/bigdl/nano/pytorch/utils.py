6b3e4d029fedbecceba6b2b1bd1d874b988132d0
Mingzhi Hu <49382651+y199387@users.noreply.github.com>
2022-07-20 09:14:07 +0800

    Nano: Add support for channels_last (#5103)
    * Add support for channels_last
    
    * Add license
    
    * Fix code style
    
    * fix code style
    
    * fix code style and refactor
    
    * fix code style
    
    * fix code style
    
    * fix code style
    
    * add correctness test for channels last
    
    * Update datasets for correctness to 2 channels
    
    * remove comments
    
    * Add comment
    
    * fix tf inc metric format
    
    * undo commit
    
    * re-run actions

--- python/nano/src/bigdl/nano/pytorch/utils.py
+++ python/nano/src/bigdl/nano/pytorch/utils.py
@@ -15,10 +15,58 @@
 #
 
 import operator
-from bigdl.nano.common.compare_version import _compare_version
+from pytorch_lightning.utilities.imports import _compare_version
+from types import MethodType
+import pytorch_lightning as pl
+from typing import Optional
+import torch
 
 TORCH_VERSION_LESS_1_10 = _compare_version("torch", operator.lt, "1.10")
 TORCH_VERSION_LESS_1_11 = _compare_version("torch", operator.lt, "1.11")
 TORCH_VERSION_LESS_1_12 = _compare_version("torch", operator.lt, "1.12")
 
 LIGHTNING_VERSION_LESS_1_6 = _compare_version("pytorch_lightning", operator.lt, "1.6")
+
+
+def batch_call(func):
+    """
+    Decorator to extending hook of pl_module.
+
+    Extending behavior hook on_before_batch_transfer to convert data to channels_last
+    at each step.
+    """
+
+    def on_before_batch_transfer(self, batch, dataloader_idx):
+
+        def convert_channels_last(batch):
+            if isinstance(batch, torch.Tensor) and batch.dim() == 4:
+                batch = batch.to(memory_format=torch.channels_last)
+            elif isinstance(batch, list) or isinstance(batch, tuple):
+                batch = list(batch)
+                for index, t in enumerate(batch):
+                    batch[index] = convert_channels_last(t)
+            return batch
+        batch = func(batch, dataloader_idx)
+        batch = convert_channels_last(batch)
+        return batch
+    return on_before_batch_transfer
+
+
+class ChannelsLastCallback(pl.Callback):
+    """Custom pl.Callback for converting model and data to channels_last."""
+
+    def setup(self, trainer, pl_module, stage: Optional[str] = None) -> None:
+        """Override hook setup to convert model to channels_last and wrap DataHook."""
+        # TODO: Add check for module_states
+        fn_old = getattr(pl_module, "on_before_batch_transfer")
+        fn = batch_call(fn_old)
+        setattr(pl_module, "on_before_batch_transfer_origin", fn_old)
+        pl_module.on_before_batch_transfer = MethodType(fn, pl_module)
+        trainer.model = trainer.model.to(memory_format=torch.channels_last)
+        return super().setup(trainer, pl_module, stage)
+
+    def teardown(self, trainer, pl_module, stage: Optional[str] = None) -> None:
+        """Undo the changes to pl_module at end of fit, validate, tests, or predict."""
+        setattr(pl_module, "on_before_batch_transfer", pl_module.on_before_batch_transfer_origin)
+        delattr(pl_module, "on_before_batch_transfer_origin")
+        return super().teardown(trainer, pl_module, stage)
