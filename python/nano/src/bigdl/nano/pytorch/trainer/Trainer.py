16fe8f42f447c2627d1fc19a0bcbc951e072947d
Zhentao Chen <90437536+zhentaocc@users.noreply.github.com>
2022-07-20 11:05:12 +0800

    [Nano]Make OpenVINOModel as public API (#5115)
    * add device option to openvino model
    
    * add openvino api
    
    * reformat
    
    * add bigdl.nano.openvino

--- python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
+++ python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
@@ -37,7 +37,7 @@ from bigdl.nano.pytorch.plugins.ddp_subprocess import DDPSubprocessPlugin
 from bigdl.nano.deps.automl.hpo_api import create_hpo_searcher, check_hpo_status
 from bigdl.nano.deps.ray.ray_api import distributed_ray
 from bigdl.nano.deps.ipex.ipex_api import create_IPEXAccelerator, create_IPEXAccelerator_1_9
-from bigdl.nano.deps.openvino.openvino_api import PytorchOpenVINOModel, load_openvino_model
+from bigdl.nano.openvino import PytorchOpenVINOModel, load_openvino_model
 from bigdl.nano.deps.onnxruntime.onnxruntime_api import PytorchONNXRuntimeModel, \
     load_onnxruntime_model
 from bigdl.nano.deps.neural_compressor.inc_api import load_inc_model, quantize as inc_quantize
