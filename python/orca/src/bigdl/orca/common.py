80663272f43057840fbf39eb4f3249fd557a383b
Cengguang Zhang <potterguang101@gmail.com>
2022-07-26 17:26:43 +0800

    fix: set conf to None when using spark-submit

--- python/orca/src/bigdl/orca/common.py
+++ python/orca/src/bigdl/orca/common.py
@@ -250,6 +250,10 @@ def init_orca_context(cluster_mode=None, runtime="spark", cores=2, memory="2g", 
                 sc = init_spark_on_local(cores, **spark_args)
             elif cluster_mode == "spark-submit":
                 from bigdl.dllib.nncontext import init_nncontext
+                if "conf" in spark_args:
+                    warnings.warn("For Spark-submit mode, conf should be None, but got "
+                                  + repr(spark_args["conf"] + ", ignored", Warning)
+                    spark_args["conf"] = None
                 sc = init_nncontext(**spark_args)
             elif cluster_mode.startswith("yarn"):  # yarn, yarn-client or yarn-cluster
                 hadoop_conf = os.environ.get("HADOOP_CONF_DIR")
