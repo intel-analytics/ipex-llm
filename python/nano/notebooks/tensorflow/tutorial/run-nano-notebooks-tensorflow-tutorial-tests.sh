2981da7188347674bd393b683584bcbdcf84be32
Hu Mingzhi <mingzhi.hu@intel.com>
2022-07-28 06:57:37 +0000

    reduce ut time

--- python/nano/notebooks/tensorflow/tutorial/run-nano-notebooks-tensorflow-tutorial-tests.sh
+++ python/nano/notebooks/tensorflow/tutorial/run-nano-notebooks-tensorflow-tutorial-tests.sh
@@ -11,10 +11,10 @@ set -e
 echo "# Start testing"
 start=$(date "+%s")
 
-python -m pytest -s --nbmake --nbmake-timeout=180 --nbmake-kernel=python3 ${NANO_TUTORIAL_TEST_DIR} -k 'not quantization'
+python -m pytest -s --nbmake --nbmake-timeout=300 --nbmake-kernel=python3 ${NANO_TUTORIAL_TEST_DIR} -k 'not quantization'
 
 # meet error using nbmake
-jupyter nbconvert --ExecutePreprocessor.timeout=180 --to notebook --exectue ${NANO_TUTORIAL_TEST_DIR}/tensorflow_quantization.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=300 --to notebook --exectue ${NANO_TUTORIAL_TEST_DIR}/tensorflow_quantization.ipynb
 
 now=$(date "+%s")
 time=$((now-start))
