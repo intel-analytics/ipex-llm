2b9baa35c7cee260b9a984abbdba2d7c25fcc648
Shengsheng Huang <shengsheng.huang@intel.com>
2022-07-28 10:47:29 +0000

    rollback tensorflow 1.15 dependency

--- docs/readthedocs/source/conf.py
+++ docs/readthedocs/source/conf.py
@@ -18,7 +18,7 @@ import glob
 import shutil
 import urllib
 
-autodoc_mock_imports = ["openvino", "pytorch_lightning", "keras", "tensorflow"]
+autodoc_mock_imports = ["openvino", "pytorch_lightning", "keras"]
 
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, '.')
