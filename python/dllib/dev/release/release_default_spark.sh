b8cd6a987c6cdcf6969bf1efebb91650ebaa0b1a
Zhao Changmin <597714572@qq.com>
2022-07-29 15:11:21 +0800

    Enable Flexible Spark Versions in Release Shell Script (#5191)
    * Enable Flexible Spark Versions

--- python/dllib/dev/release/release_default_spark.sh
+++ python/dllib/dev/release/release_default_spark.sh
@@ -16,8 +16,7 @@
 # limitations under the License.
 #
 
-# This is the default script with maven parameters to release bigdl-dllib built on top of
-# Spark 3.1.2 for linux.
+# This is the default script with maven parameters to release bigdl-dllib built on top of Spark for linux or mac.
 # Note that if the maven parameters to build bigdl-dllib need to be changed,
 # make sure to change this file accordingly.
 # If you want to customize the release, please use release.sh and specify maven parameters instead.
@@ -31,25 +30,37 @@ DEV_DIR="$(cd ${DLLIB_DIR}/../dev/; pwd)"
 echo $DEV_DIR
 
 if (( $# < 4)); then
-  echo "Usage: release_default_linux_spark312.sh version quick_build upload suffix"
-  echo "Usage example: bash release_default_linux_spark312.sh default true true true"
-  echo "Usage example: bash release_default_linux_spark312.sh 0.14.0.dev1 false true false"
+  echo "Usage: release_default_spark.sh platform version quick_build upload spark_version suffix"
+  echo "Usage example: bash release_default_spark.sh linux default true true 3.1.2 true"
+  echo "Usage example: bash release_default_spark.sh mac 0.14.0.dev1 false true 2.4.6 false"
   exit -1
 fi
 
-version=$1
-quick=$2
-upload=$3
-suffix=$4
-profiles=${*:5}
+platform=$1
+version=$2
+quick=$3
+upload=$4
+spark_version=$5
+suffix=$6
+profiles=${*:7}
 
-sed -i "s/pyspark==2.4.6/pyspark==3.1.2/g" $DLLIB_DIR/src/setup.py
+version_array=(${spark_version//./ })
+spark_first_version=${version_array[0]}
+
+re='^[2-3]+$'
+if ! [[ $spark_first_version =~ $re ]] ; then
+   echo "error: Spark version is not a number like 3.1.2"
+   exit 1
+fi
+
+sed -i "s/pyspark==[0-9].[0-9]*.[0-9]*/pyspark==${spark_version}/g" $DLLIB_DIR/src/setup.py
+
 if [ ${suffix} == true ]; then
-    bash ${DEV_DIR}/add_suffix_spark3.sh $DLLIB_DIR/src/setup.py
-    bash ${DEV_DIR}/add_suffix_spark3.sh ${RUN_SCRIPT_DIR}/release.sh
+    bash ${DEV_DIR}/add_suffix_spark${spark_first_version}.sh $DLLIB_DIR/src/setup.py
+    bash ${DEV_DIR}/add_suffix_spark${spark_first_version}.sh ${RUN_SCRIPT_DIR}/release.sh
 else
     bash ${DEV_DIR}/remove_spark_suffix.sh $DLLIB_DIR/src/setup.py
     bash ${DEV_DIR}/remove_spark_suffix.sh ${RUN_SCRIPT_DIR}/release.sh
 fi
 
-bash ${RUN_SCRIPT_DIR}/release.sh linux ${version} ${quick} ${upload} -Dspark.version=3.1.2 -P spark_3.x ${profiles}
+bash ${RUN_SCRIPT_DIR}/release.sh ${platform} ${version} ${quick} ${upload} -Dspark.version=${spark_version} -P spark_${spark_first_version}.x ${profiles}
