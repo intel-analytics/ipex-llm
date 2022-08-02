2d704052051288f0d05c3335cbbbff7bf5055257
Mingzhi Hu <49382651+y199387@users.noreply.github.com>
2022-08-02 13:43:44 +0800

    Nano: Add automatically rescale learning rate (#5168)
    * Add scale_lr
    
    * Add unit test
    
    * update unit test for scale lr
    
    * Fix unit test in torch 1.9
    
    * Fix code style
    
    * Add support for torch 1.9
    
    * Fix code style
    
    * Fix code style
    
    * Refactor Nano auto config lr
    
    * Support torch1.9 and fix code style
    
    * Fix code style and update unit test
    
    * Fix code style w503 w504
    
    * re-run action
    
    * Update
    
    * refactor auto_lr
    
    * Update ut
    
    * Update
    
    * re-run action
    
    * refactor
    
    * Update
    
    * Update comments
    
    * Update
    
    * Fix code style
    
    * support ray
    
    * update
    
    * re-run action
    
    * update

--- python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
+++ python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
@@ -16,7 +16,7 @@
 import copy
 from logging import warning
 from pathlib import Path
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Union
 import pytorch_lightning as pl
 import torch
 from torch import nn
@@ -62,6 +62,7 @@ class Trainer(pl.Trainer):
                  cpu_for_each_process: Optional[List[List[int]]] = None,
                  use_hpo=False,
                  channels_last: bool = False,
+                 auto_lr: Union[int, bool] = True,
                  *args: Any, **kwargs: Any) -> None:
         """
         A pytorch lightning trainer that uses bigdl-nano optimization.
@@ -131,18 +132,21 @@ class Trainer(pl.Trainer):
                 strategy = DDPSpawnStrategy(num_processes=num_processes,
                                             cpu_for_each_process=cpu_for_each_process,
                                             use_ipex=self.use_ipex,
-                                            enable_bf16=enable_bf16)
+                                            enable_bf16=enable_bf16,
+                                            auto_lr=auto_lr)
             elif distributed_backend == "subprocess":
                 from bigdl.nano.pytorch.strategies import DDPSubprocessStrategy
                 strategy = DDPSubprocessStrategy(num_processes=num_processes,
                                                  cpu_for_each_process=cpu_for_each_process,
                                                  use_ipex=self.use_ipex,
-                                                 enable_bf16=enable_bf16)
+                                                 enable_bf16=enable_bf16,
+                                                 auto_lr=auto_lr)
             elif distributed_backend == "ray":
                 from bigdl.nano.pytorch.strategies import create_RayStrategy
                 strategy = create_RayStrategy(num_workers=num_processes,
                                               use_ipex=self.use_ipex,
-                                              enable_bf16=enable_bf16)
+                                              enable_bf16=enable_bf16,
+                                              auto_lr=auto_lr)
             kwargs["strategy"] = strategy
             super().__init__(*args, **kwargs)
 
