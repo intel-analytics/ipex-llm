773155dcfe6b3761c9fceb03c111796a9df41a04
Kai Huang <huangkaivision@gmail.com>
2022-08-12 20:19:11 +0800

    add license

--- python/friesian/example/deeprec/wdl.py
+++ python/friesian/example/deeprec/wdl.py
@@ -1,3 +1,19 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Original imports
 import time
 import argparse
@@ -8,7 +24,7 @@ import math
 import collections
 from tensorflow.python.client import timeline
 import json
-import logging
+
 from tensorflow.python.ops import partitioned_variables
 
 # Extra imports
