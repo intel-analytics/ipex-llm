c6557e3dcac66dd6398fbc6a4bcde77362f4e4cd
ruoan1.wang <ruonan1.wang@intel.com>
2022-08-14 13:10:36 -0400

    fix style

--- python/chronos/src/bigdl/chronos/model/autoformer/Autoformer.py
+++ python/chronos/src/bigdl/chronos/model/autoformer/Autoformer.py
@@ -60,7 +60,7 @@ class AutoFormer(pl.LightningModule):
     """
     def __init__(self, configs):
         super().__init__()
-        kwargs = {k : getattr(configs, k) for k in configs._fields}
+        kwargs = {k: getattr(configs, k) for k in configs._fields}
         self.save_hyperparameters(kwargs)
         pl.seed_everything(configs.seed, workers=True)
         self.seq_len = configs.seq_len
