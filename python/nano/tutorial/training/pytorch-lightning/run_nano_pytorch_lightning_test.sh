4929002e207d359dc86eefdefe00470d53f378fc
zhanglirong1999 <1695074375@qq.com>
2022-08-16 15:26:22 +0800

    fix

--- python/nano/tutorial/training/pytorch-lightning/run_nano_pytorch_lightning_test.sh
+++ python/nano/tutorial/training/pytorch-lightning/run_nano_pytorch_lightning_test.sh
@@ -1,6 +1,6 @@
 export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
-export NANO_TUTORIAL_TEST_DIR=${ANALYTICS_ZOO_ROOT}python/nano/tutorial/training/pytorch-lightning/
+export NANO_TUTORIAL_TEST_DIR=${ANALYTICS_ZOO_ROOT}python/nano/tutorial/training/pytorch-lightning
 
 set -e
 
