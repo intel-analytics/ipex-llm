8085011793910baa91119e7ef42b45c1b9860062
TheaperDeng <theaperdeng@outlook.com>
2022-08-21 14:22:10 +0800

    remove checking

--- python/chronos/src/bigdl/chronos/autots/__init__.py
+++ python/chronos/src/bigdl/chronos/autots/__init__.py
@@ -15,15 +15,7 @@
 #
 import warnings
 from bigdl.nano.utils.log4Error import invalidInputError
-import os
 
-
-if os.getenv("LD_PRELOAD", "null") != "null":
-    invalidInputError(False,
-                      errMsg="Users of `bigdl.chronos.autots` should "
-                             "unset bigdl-nano environment variables!",
-                      fixMsg="Please run `source bigdl-nano-unset-env` "
-                             "in your bash terminal")
 
 try:
     # TODO: make this a LazyImport
