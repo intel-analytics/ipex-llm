eb1231d87403fb6e8c203a7a9a2495ebada4b0ea
ruoan1.wang <ruonan1.wang@intel.com>
2022-08-22 07:06:25 -0400

    remove one line

--- python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
+++ python/nano/src/bigdl/nano/pytorch/trainer/Trainer.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from cmath import log
+
 import copy
 from logging import warning
 from pathlib import Path
