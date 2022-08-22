12565fdd0cf9bc769c372a29545cb93273c23f47
yansu <yan1.sun@intel.com>
2022-08-22 12:22:02 +0800

    add data processing tutorial

---
+++ python/orca/tutorial/xshards/titanic.py
@@ -0,0 +1,59 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This example is adapted from
+# https://www.kaggle.com/code/chuanguy/titanic-data-processing-with-python-0-813/notebook
+
+from bigdl.orca import init_orca_context, stop_orca_context, OrcaContext
+import bigdl.orca.data.pandas
+
+OrcaContext.pandas_read_backend = "pandas"
+init_orca_context(cluster_mode="local", cores=4, memory="3g")
+
+file_path = "train.csv"
+data_shard = bigdl.orca.data.pandas.read_csv(file_path)
+
+# drop
+def drop_func(df):
+    df = df.drop(['PassengerId'], axis=1)
+    return df
+data_shard = data_shard.transform_shard(drop_func)
+
+
+# fillna, apply, replace, map
+def process_cabin(df):
+    df['Cabin'] = df['Cabin'].fillna('X')
+    df['Cabin'] = df['Cabin'].apply(lambda x: str(x)[0])
+    df['Cabin'] = df['Cabin'].replace(['A', 'D', 'E', 'T'], 'M')
+    df['Cabin'] = df['Cabin'].replace(['B', 'C'], 'H')
+    df['Cabin'] = df['Cabin'].replace(['F', 'G'], 'L')
+    df['Cabin'] = df['Cabin'].map({'X': 0, 'L': 1, 'M': 2, 'H': 3})
+    df['Cabin'] = df['Cabin'].astype(int)
+    return df
+data_shard = data_shard.transform_shard(process_cabin)
+
+
+# astype, loc
+def change_val(data):
+    data['Sex'] = data['Sex'].map({'female': 1, 'male': 0})
+    data['Pclass'] = data['Pclass'].map({1: 3, 2: 2, 3: 1}).astype(int)
+    data.loc[data['Sex'] == 0, 'SexByPclass'] = data.loc[data['Sex'] == 0, 'Pclass']
+    data.loc[data['Sex'] == 1, 'SexByPclass'] = data.loc[data['Sex'] == 1, 'Pclass'] + 3
+    return data
+data_shard = data_shard.transform_shard(change_val)
+
+
+stop_orca_context()
+
