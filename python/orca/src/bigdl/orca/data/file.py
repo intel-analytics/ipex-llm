a53f4e7eb3491d0aeebc977b102feec3e1e819ef
Cengguang Zhang <potterguang101@gmail.com>
2022-08-24 12:01:59 +0800

    fix: fix code style.

--- python/orca/src/bigdl/orca/data/file.py
+++ python/orca/src/bigdl/orca/data/file.py
@@ -541,8 +541,12 @@ def get_remote_files_with_prefix_to_local(remote_path_prefix, local_dir):
 
 def multi_fs_load(load_func):
     """
-    Enable loading file or directory in multiple file systems. It supports local, hdfs, s3 file systems.
-    Note: this decorator is different from dllib decorator @enable_multi_fs_load. This decorator can load on each worker while @enable_multi_fs_load can only load on driver.
+
+    Enable loading file or directory in multiple file systems.
+    It supports local, hdfs, s3 file systems.
+    Note: this decorator is different from dllib decorator @enable_multi_fs_load. 
+          This decorator can load on each worker while @enable_multi_fs_load can only load on driver.
+
     :param load_func: load file or directory function
     :return: load file or directory funtion for specific file system
     """
