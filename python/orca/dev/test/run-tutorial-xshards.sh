445791499ffcabde8f5f368907e4a5867cb44c21
Nancy <51090416+yexinyinancy@users.noreply.github.com>
2022-08-24 17:51:34 +0800

    Add Xshards test to github actions (#5512)
    * Add Xshards test to github actions
    
    * add file path
    
    * fix yml
    
    * fix csv path
    
    * fix csv path
    
    * fix yml
    
    * add wget
    
    * fic typo
    
    * fix typo
    
    * change csv file path
    
    * update pwd
    
    * add new line
    
    * delete a line
    
    * add time
    
    Co-authored-by: yansu <yan1.sun@intel.com>

---
+++ python/orca/dev/test/run-tutorial-xshards.sh
@@ -0,0 +1,46 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+set -ex
+
+export FTP_URI=$FTP_URI
+export PYSPARK_PYTHON=python
+export PYSPARK_DRIVER_PYTHON=python
+
+ray stop -f
+
+cd "`dirname $0`"
+cd ../../tutorial/xshards
+
+echo "Running Xshards tests"
+
+#timer
+start=$(date "+%s")
+
+if [ -f ${BIGDL_ROOT}/python/orca/tutorial/xshards/train.csv ]
+then
+    echo "train.csv already exists"
+else
+    wget -nv $FTP_URI/analytics-zoo-data/xshards/train.csv -P ${BIGDL_ROOT}/python/orca/tutorial/xshards/
+fi
+
+python tabular_playground_series.py
+
+now=$(date "+%s")
+time1=$((now - start))
+
+echo "Running Xshards tests time used: $time1 seconds"
