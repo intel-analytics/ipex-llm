fe61b336704f07c29c952ba66d8beab1a5314de0
Zhou <jian.zhou@intel.com>
2022-08-26 10:17:43 +0800

    update ppml doc

--- python/ppml/src/bigdl/ppml/ppml_context.py
+++ python/ppml/src/bigdl/ppml/ppml_context.py
@@ -53,8 +53,8 @@ class PPMLContext(JavaValue):
                 conf["spark.bigdl.kms.key.primary"] = check(ppml_args, "primary_key_path")
                 conf["spark.bigdl.kms.key.data"] = check(ppml_args, "data_key_path")
             elif kms_type == "AzureKeyManagementService":
-                conf["spark.bigdl.kms.azure.vault"] = check(ppml_args, "kms_azure_vault")
-                conf["spark.bigdl.kms.azure.clientId"] = ppml_args.get("kms_azure_clientId", "")
+                conf["spark.bigdl.kms.azure.vault"] = check(ppml_args, "azure_vault")
+                conf["spark.bigdl.kms.azure.clientId"] = ppml_args.get("azure_client_id", "")
                 conf["spark.bigdl.kms.key.primary"] = check(ppml_args, "primary_key_path")
                 conf["spark.bigdl.kms.key.data"] = check(ppml_args, "data_key_path")
             else:
