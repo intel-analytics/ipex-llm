06a69d0323bec74283a20dfcbe1831af7310b3a2
leonardozcm <leonardo1997zcm@gmail.com>
2022-08-29 09:40:23 +0800

    rm sh

--- python/orca/dev/test/run-type-check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export ORCA_HOME=${BIGDL_HOME}/python/orca/
-export ORCA_TEST_DIR=${BIGDL_HOME}/python/orca/test
-# "mypy --install-types --non-interactive" is to automatically install missing types 
-mypy --install-types --non-interactive --config-file ${ORCA_TEST_DIR}/mypy.ini $ORCA_HOME/src/bigdl/orca/data/utils.py $ORCA_HOME/src/bigdl/orca/data/ray_xshards.py
