d946032f1b7fb77533ad771e1194c5032869c205
Yishuo Wang <yishuo.wang@intel.com>
2022-09-06 16:47:34 +0800

    remove tensorflow quantization example

--- python/nano/tutorial/inference/tensorflow/run-nano-tensorflow-inference-test.sh
+++
@@ -1,8 +0,0 @@
-export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
-export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
-export NANO_TUTORIAL_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/tutorial/inference/tensorflow
-
-set -e
-
-export NUM_SHARDS=4
-python $NANO_TUTORIAL_TEST_DIR/tensorflow_quantization.py
