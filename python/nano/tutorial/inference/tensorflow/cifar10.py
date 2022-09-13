b703a6f7d4c44bfc03e197a9e302e1026b442dce
mycprotein <mycprotein@gmail.com>
2022-09-13 10:20:52 +0800

    improve code organization

--- python/nano/tutorial/inference/tensorflow/cifar10.py
+++ python/nano/tutorial/inference/tensorflow/cifar10.py
@@ -60,75 +60,83 @@ print(x_test.shape[0], "test samples")
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = keras.Sequential()
 
-model.add(layers.Conv2D(32, (3,3), padding='same', activation='relu', input_shape=(32,32,3)))
-model.add(layers.BatchNormalization())
-model.add(layers.Conv2D(32, (3,3), padding='same', activation='relu'))
-model.add(layers.BatchNormalization())
-model.add(layers.MaxPooling2D(pool_size=(2,2)))
-model.add(layers.Dropout(0.3))
+def creat_model():
+    model = keras.Sequential()
 
-model.add(layers.Conv2D(64, (3,3), padding='same', activation='relu'))
-model.add(layers.BatchNormalization())
-model.add(layers.Conv2D(64, (3,3), padding='same', activation='relu'))
-model.add(layers.BatchNormalization())
-model.add(layers.MaxPooling2D(pool_size=(2,2)))
-model.add(layers.Dropout(0.5))
+    model.add(layers.Conv2D(32, (3, 3), padding='same',
+              activation='relu', input_shape=(32, 32, 3)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Dropout(0.3))
 
-model.add(layers.Conv2D(128, (3,3), padding='same', activation='relu'))
-model.add(layers.BatchNormalization())
-model.add(layers.Conv2D(128, (3,3), padding='same', activation='relu'))
-model.add(layers.BatchNormalization())
-model.add(layers.MaxPooling2D(pool_size=(2,2)))
-model.add(layers.Dropout(0.5))
+    model.add(layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Dropout(0.5))
 
-model.add(layers.Flatten())
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.BatchNormalization())
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(num_classes, activation='softmax'))  
+    model.add(layers.Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.Dropout(0.5))
 
-# this line is optional
-# model = Model(inputs=model.inputs, outputs=model.outputs)
-
-model.summary()
-
-batch_size = 128
-epochs = 20
-
-model.compile(loss="categorical_crossentropy",
-              optimizer="adam", metrics=["accuracy"])
-
-model.fit(x_train, y_train, batch_size=batch_size,
-          epochs=epochs, validation_split=0.1)
-
-score = model.evaluate(x_test, y_test, verbose=0)
-print("Test loss:", score[0])
-print("Test accuracy:", score[1])
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(num_classes, activation='softmax'))
+    return model
 
 
-# Quantization using Intel Neural Compressor
-# pip install 'neural-compressor'
+if __name__ == "__main__":
 
-# calib_dataset only accept tf.data.Dataset object
-tune_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    model = creat_model()
 
-# Execute quantization
-q_model = model.quantize(calib_dataset=tune_dataset)
+    # this line is optional
+    # model = Model(inputs=model.inputs, outputs=model.outputs)
 
-# Inference using quantized model
-y_test_hat = q_model(x_test)
+    model.summary()
 
-# Evaluate the quantized model
-loss = float(tf.reduce_mean(
-             losses.categorical_crossentropy(y_test, y_test_hat)))
-categorical_accuracy = metrics.CategoricalAccuracy()
-categorical_accuracy.update_state(y_test, y_test_hat)
-accuracy = categorical_accuracy.result().numpy()
+    batch_size = 128
+    epochs = 20
 
-print("Quantization test loss:", loss)
-print("Quantization test accuracy:", accuracy)
-# Note: accuracy decrease varies from different tasks and situations,
-# and sometimes the accuracy is even higher.
-# You can set a quantization threshold when making a quantization model.
+    model.compile(loss="categorical_crossentropy",
+                  optimizer="adam", metrics=["accuracy"])
+
+    model.fit(x_train, y_train, batch_size=batch_size,
+              epochs=epochs, validation_split=0.1)
+
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print("Test loss:", score[0])
+    print("Test accuracy:", score[1])
+
+    # Quantization using Intel Neural Compressor
+    # pip install 'neural-compressor'
+
+    # calib_dataset only accept tf.data.Dataset object
+    tune_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+
+    # Execute quantization
+    q_model = model.quantize(calib_dataset=tune_dataset)
+
+    # Inference using quantized model
+    y_test_hat = q_model(x_test)
+
+    # Evaluate the quantized model
+    loss = float(tf.reduce_mean(
+        losses.categorical_crossentropy(y_test, y_test_hat)))
+    categorical_accuracy = metrics.CategoricalAccuracy()
+    categorical_accuracy.update_state(y_test, y_test_hat)
+    accuracy = categorical_accuracy.result().numpy()
+
+    print("Quantization test loss:", loss)
+    print("Quantization test accuracy:", accuracy)
+    # Note: accuracy decrease varies from different tasks and situations,
+    # and sometimes the accuracy is even higher.
+    # You can set a quantization threshold when making a quantization model.
