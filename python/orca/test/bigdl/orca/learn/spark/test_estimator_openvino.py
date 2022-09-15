d5b232aa6b513012e1ba04cf0756bc72a6142f83
Yina Chen <33650826+cyita@users.noreply.github.com>
2022-09-15 14:38:52 +0800

    Orca OpenVINO estimator add input order param & dataframe input support multi-output (#5471)
    * update
    
    * df support multi-output
    
    * temp
    
    * update
    
    * update test
    
    * meet comments
    
    * meet comments
    
    * del pred
    
    * update testcase

--- python/orca/test/bigdl/orca/learn/spark/test_estimator_openvino.py
+++ python/orca/test/bigdl/orca/learn/spark/test_estimator_openvino.py
@@ -53,7 +53,8 @@ class TestEstimatorForOpenVINO(TestCase):
         pass
 
     def tearDown(self):
-        shutil.rmtree(local_path)
+        # shutil.rmtree(local_path)
+        pass
 
     def check_result(self, result, length=0):
         if length == 0:
@@ -89,6 +90,17 @@ class TestEstimatorForOpenVINO(TestCase):
         model_path = os.path.join(local_path, "roberta/model.xml")
         self.est = Estimator.from_openvino(model_path=model_path)
 
+    def load_multi_output_model(self):
+        os.makedirs(local_path, exist_ok=True)
+        model_url = data_url + "/analytics-zoo-data/ov_multi_output.tar"
+        model_path = maybe_download("ov_multi_output.tar",
+                                    local_path, model_url)
+        tar = tarfile.open(model_path)
+        tar.extractall(path=local_path)
+        tar.close()
+        model_path = os.path.join(local_path, "FP32/model_float32.xml")
+        self.est = Estimator.from_openvino(model_path=model_path)
+
     def test_openvino_predict_ndarray(self):
         self.load_resnet()
         input_data = np.array([self.input] * 22)
@@ -101,10 +113,18 @@ class TestEstimatorForOpenVINO(TestCase):
 
     def test_openvino_predict_ndarray_multi_input(self):
         self.load_roberta()
-        input_data = [np.zeros([32, 128])] * 3
+        input_data = [np.zeros([32, 128]), np.ones([32, 128]), np.zeros([32, 128])]
         result = self.est.predict(input_data, batch_size=5)
         assert isinstance(result, np.ndarray)
         assert result.shape == (32, 2)
+        with self.assertRaises(Exception):
+            self.est.predict(input_data, feature_cols=["feature"],
+                             input_cols=["a", 'input_ids', 'token_type_ids'])
+        result2 = self.est.predict(input_data,
+                                   input_cols=['input_ids', 'token_type_ids', 'attention_mask'])
+        assert isinstance(result2, np.ndarray)
+        assert result2.shape == (32, 2)
+        assert not np.allclose(result, result2)
 
     def test_openvino_predict_xshards(self):
         self.load_resnet()
@@ -122,11 +142,11 @@ class TestEstimatorForOpenVINO(TestCase):
         result = self.est.predict(shards)
         result_c = result.collect()
         assert isinstance(result, SparkXShards)
-        assert result_c[0]["prediction"].shape == (4, 1000)
-        assert result_c[1]["prediction"].shape == (3, 1000)
-        assert self.check_result(result_c[0]["prediction"], 4)
-        assert self.check_result(result_c[1]["prediction"], 2)
-        assert not self.check_result(result_c[1]["prediction"][2:], 1)
+        assert result_c[0].shape == (4, 1000)
+        assert result_c[1].shape == (3, 1000)
+        assert self.check_result(result_c[0], 4)
+        assert self.check_result(result_c[1], 2)
+        assert not self.check_result(result_c[1][2:], 1)
 
     def test_openvino_predict_spark_df(self):
         from pyspark.sql import SparkSession
@@ -135,18 +155,39 @@ class TestEstimatorForOpenVINO(TestCase):
         sc = init_nncontext()
         spark = SparkSession(sc)
         input_list = self.input.tolist()
-        rdd = sc.range(0, 18, numSlices=3)
-        input_df = rdd.map(lambda x: [input_list]).toDF(["feature"])
-        with self.assertRaises(Exception):
-            self.est.predict(input_df, feature_cols=["feature"])
         rdd = sc.range(0, 18, numSlices=5)
         input_df = rdd.map(lambda x: [input_list]).toDF(["feature"])
         result_df = self.est.predict(input_df, feature_cols=["feature"])
-        result = list(map(lambda row: np.array(row["prediction"]),
-                          result_df.select("prediction").collect()))
-        assert np.array(result_df.select("prediction").first()).shape == (1, 1000)
+        result = list(map(lambda row: np.array(row["resnet_v1_50/predictions/Softmax"]),
+                          result_df.select("resnet_v1_50/predictions/Softmax").collect()))
+        assert np.array(result_df.select("resnet_v1_50/predictions/Softmax").first()).shape \
+               == (1, 1, 1000)
         assert result_df.count() == 18
         assert self.check_result(result, 18)
+
+    def test_openvino_multi_output(self):
+        from pyspark.sql import SparkSession
+        from bigdl.orca.learn.utils import dataframe_to_xshards
+
+        self.load_multi_output_model()
+        sc = init_nncontext()
+        spark = SparkSession(sc)
+        rdd = sc.range(0, 2, numSlices=1)
+        df = rdd.map(lambda x: [np.random.rand(3, 550, 550).tolist()]).toDF(["input"])
+        result_df = self.est.predict(df, feature_cols=["input"])
+        df_c = result_df.rdd.map(lambda row: [row[1], row[2], row[3], row[4]]).collect()
+        df_c = [np.concatenate((np.array(df_c[0][i]), np.array(df_c[1][i]))) for i in range(4)]
+        shards, _ = dataframe_to_xshards(df,
+                                         validation_data=None,
+                                         feature_cols=["input"],
+                                         label_cols=None,
+                                         mode="predict")
+        result_shard = self.est.predict(shards, batch_size=4)
+        shard_c = result_shard.collect()[0]
+        nd_input = np.squeeze(np.array(df.select('input').collect()))
+        result_np = self.est.predict(nd_input)
+        assert np.all([np.allclose(r1, r2) for r1, r2 in zip(df_c, result_np)])
+        assert np.all([np.allclose(r1, r2) for r1, r2 in zip(shard_c, result_np)])
 
 
 if __name__ == "__main__":
