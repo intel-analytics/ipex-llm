606fa0d046d28c109a5e5b4c1b8c19a4038bcf29
Hu Mingzhi <mingzhi.hu@intel.com>
2022-09-16 01:40:53 +0000

    Test BF16 tutorial

--- python/nano/tutorial/training/pytorch/run_nano_pytorch_test.sh
+++ python/nano/tutorial/training/pytorch/run_nano_pytorch_test.sh
@@ -2,6 +2,8 @@ export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
 export NANO_TUTORIAL_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/tutorial/training/pytorch
 
+TORCH_VERSION=`python -c "from bigdl.nano.pytorch.utils import TORCH_VERSION_LESS_1_12; print(TORCH_VERSION_LESS_1_12)"`
+
 set -e
 
 echo " Start testing PyTorch CV data pipeline tutorial"
@@ -18,3 +20,12 @@ echo " Start testing PyTorch multi-instance training tutorial"
 sed -i s/:-val_size/:5/ $NANO_TUTORIAL_TEST_DIR/pytorch_train_multi_instance.py
 sed -i s/-val_size:/-5:/ $NANO_TUTORIAL_TEST_DIR/pytorch_train_multi_instance.py
 python $NANO_TUTORIAL_TEST_DIR/pytorch_train_multi_instance.py
+
+echo " Start testing PyTorch BFloat16 training tutorial"
+sed -i s/:-val_size/:5/ $NANO_TUTORIAL_TEST_DIR/pytorch_train_bf16.py
+sed -i s/-val_size:/-5:/ $NANO_TUTORIAL_TEST_DIR/pytorch_train_bf16.py
+if [ $TORCH_VERSION == True ]
+then
+    sed -i s/MyNano\(precision=\'bf16\'\)\.train\(\)//  $NANO_TUTORIAL_TEST_DIR/pytorch_train_bf16.py
+fi
+python $NANO_TUTORIAL_TEST_DIR/pytorch_train_bf16.py
