769720fab99cbb7cc9ff1936c7f8214a472733a7
Uxito-Ada <414416158@qq.com>
2022-09-17 19:55:24 +0800

    fix according to test

--- ppml/trusted-big-data-ml/python/docker-gramine/start-scripts/start-python-numpy-sgx.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/start-scripts/start-python-numpy-sgx.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /ppml/trusted-big-data-ml
 export spark_commnd="python ./work/examples/test-numpy.py"
-./init.sh
 gramine-sgx bash 2>&1 | tee test-numpy-sgx.log
 cat test-numpy-sgx.log | egrep -a "dot"
