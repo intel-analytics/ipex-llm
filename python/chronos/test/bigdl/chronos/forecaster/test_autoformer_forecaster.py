84f38598c4fe8ec9e735240c6fa6a22c33155d9e
Junwei Deng <35031544+TheaperDeng@users.noreply.github.com>
2022-09-19 19:47:50 +0800

    Chronos: More autoformer bug fix and enhancement (#5741)
    * midterm push for working in cafe:)
    
    * add updates
    
    * remove confusing comments
    
    * consider the conflict between tsdataset.label_len and label_len
    
    * update according to comments, fix pep8
    
    * add test cmt

--- python/chronos/test/bigdl/chronos/forecaster/test_autoformer_forecaster.py
+++ python/chronos/test/bigdl/chronos/forecaster/test_autoformer_forecaster.py
@@ -297,3 +297,26 @@ class TestChronosModelAutoformerForecaster(TestCase):
                                                   repetition_times=5)
         assert y_pred.shape == std.shape
         y_pred, std = forecaster.predict_interval(data=test_data)
+
+    def test_autoformer_forecaster_from_tsdataset(self):
+        df = get_ts_df()
+        target = ["value", "extra feature"]
+        tsdata_train, tsdata_val, tsdata_test =\
+            TSDataset.from_pandas(df, dt_col="datetime", target_col=target,
+                                with_split=True, test_ratio=0.1, val_ratio=0.1)
+        train_loader = tsdata_train.to_torch_data_loader(lookback=24, horizon=5, time_enc=True)
+        forecaster = AutoformerForecaster.from_tsdataset(tsdata_train)
+        forecaster.fit(train_loader, epochs=1, batch_size=32)
+
+        # conflict param
+        with pytest.raises(RuntimeError):
+            forecaster = AutoformerForecaster.from_tsdataset(tsdata_train, label_len=30)
+
+        # no data infer
+        tsdata_train, tsdata_val, tsdata_test =\
+            TSDataset.from_pandas(df, dt_col="datetime", target_col=target,
+                                with_split=True, test_ratio=0.1, val_ratio=0.1)
+        forecaster = AutoformerForecaster.from_tsdataset(tsdata_train,
+                                                         past_seq_len=24,
+                                                         future_seq_len=5)
+        forecaster.fit(train_loader, epochs=1, batch_size=32)
