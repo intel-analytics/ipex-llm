420567c29b5c36d45d545a37a37e40514fbb38e5
Le-Zheng <30695225+Le-Zheng@users.noreply.github.com>
2022-09-19 19:51:21 +0800

    flip bigdl-core/zoo-core pypi version (#5839)

--- python/orca/src/setup.py
+++ python/orca/src/setup.py
@@ -105,7 +105,7 @@ def setup_package():
         url='https://github.com/intel-analytics/BigDL',
         packages=get_bigdl_packages(),
         install_requires=['packaging', 'filelock',
-                          'bigdl-tf==2.1.0.dev3', 'bigdl-math==2.1.0.dev3',
+                          'bigdl-tf==2.1.0', 'bigdl-math==2.1.0',
                           'bigdl-dllib=='+VERSION, 'pyzmq'],
         extras_require={'ray': RAY_DEP,
                         'automl': AUTOML_DEP,
