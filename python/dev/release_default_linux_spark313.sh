7d62769b81b9578c735edc8b2e204052091911ad
pinggao187 <ping.gao3@pactera.com>
2022-09-22 10:18:13 +0800

    update

--- python/dev/release_default_linux_spark313.sh
+++ python/dev/release_default_linux_spark313.sh
@@ -26,10 +26,10 @@ BIGDL_DIR="$(cd ${RUN_SCRIPT_DIR}/../..; pwd)"
 echo $BIGDL_DIR
 
 if (( $# < 3)); then
-  echo "Usage: release_default_linux_spark313.sh version quick_build upload suffix mvn_pa                    rameters"
+  echo "Usage: release_default_linux_spark313.sh version quick_build upload suffix mvn_parameters"
   echo "Usage example: bash release_default_linux_spark313.sh default false true true"
-  echo "Usage example: bash release_default_linux_spark313.sh 0.14.0.dev1 false false tru                    e"
-  echo "Usage example: bash release_default_linux_spark313.sh 0.14.0.dev1 false false fal                    se -Ddata-store-url=.."
+  echo "Usage example: bash release_default_linux_spark313.sh 0.14.0.dev1 false false true"
+  echo "Usage example: bash release_default_linux_spark313.sh 0.14.0.dev1 false false false -Ddata-store-url=.."
   exit -1
 fi
 
@@ -44,4 +44,4 @@ else
   profiles=${*:5}
 fi
 
-bash ${RUN_SCRIPT_DIR}/release_default_spark.sh linux ${version} ${quick} ${upload} 3.1.3                     ${suffix} ${profiles}
+bash ${RUN_SCRIPT_DIR}/release_default_spark.sh linux ${version} ${quick} ${upload} 3.1.3 ${suffix} ${profiles}
