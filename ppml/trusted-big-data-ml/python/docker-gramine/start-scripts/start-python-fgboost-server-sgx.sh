054a8a56af81569a2b9bc4304c769fc606d9fd76
Jin Hanyu <476099001@qq.com>
2022-09-23 15:28:11 +0800

    Remove bigdl-ppml*-jar-with-dependencies.jar and replace pyspark with py4j.

--- ppml/trusted-big-data-ml/python/docker-gramine/start-scripts/start-python-fgboost-server-sgx.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/start-scripts/start-python-fgboost-server-sgx.sh
@@ -16,7 +16,7 @@ done
 cd /ppml/trusted-big-data-ml
 ./clean.sh
 gramine-argv-serializer bash -c "/opt/jdk8/bin/java\
-        -cp '/ppml/trusted-big-data-ml/work/bigdl-2.1.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-2.1.0-SNAPSHOT-jar-with-dependencies.jar:/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*'\
+        -cp '/ppml/trusted-big-data-ml/work/bigdl-2.1.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-2.1.0-SNAPSHOT.jar:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/commons-lang3-3.10.jar:/ppml/trusted-big-data-ml/work/bigdl-2.1.0-SNAPSHOT/jars/*:/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*'\
         -Xmx10g org.apache.spark.deploy.SparkSubmit\
         --master 'local[4]'\
         /ppml/trusted-big-data-ml/fl/start-fgboost-server.py --port $port --client_num $client_num" > secured_argvs
