824dae54ae3a46fc6cabb14a21681fe8202c682d
ShanSimu <shaojie.cui@intel.com>
2022-09-26 10:22:42 +0800

    add for loop in test

--- ppml/trusted-big-data-ml/python/docker-gramine/test-suites/pyspark-sql-ut.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/test-suites/pyspark-sql-ut.sh
@@ -6,22 +6,25 @@ cd /ppml/trusted-big-data-ml
 ./clean.sh
 for suite in `cat /ppml/trusted-big-data-ml/work/test-suites/pysparkSqlSuites.txt`
 do
-    gramine-argv-serializer bash -c "/opt/jdk8/bin/java -cp \
-                                    '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
-                                    -Xmx1g org.apache.spark.deploy.SparkSubmit --master 'local[4]' --conf spark.network.timeout=10000000 \
-                                    --conf spark.executor.heartbeatInterval=10000000 --conf spark.python.use.daemon=false \
-                                    --conf spark.python.worker.reuse=false \
-                                    /ppml/trusted-big-data-ml/work/spark-3.1.2/python/pyspark/sql/tests/$suite" \
-                                    > secured_argvs
-    ./init.sh
-    gramine-sgx bash 2>&1 | tee /ppml/trusted-big-data-ml/logs/pyspark/sql/$suite.log
-    echo "##########$suite Test:"
-    if [ -n "$(grep "FAILED" /ppml/trusted-big-data-ml/logs/pyspark/sql/$suite.log -H -o)" ]
-    then
-        echo "failed"
-        exit 1
-    else
-        echo "pass"
-    fi
+    while true
+        gramine-argv-serializer bash -c "/opt/jdk8/bin/java -cp \
+                                        '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
+                                        -Xmx1g org.apache.spark.deploy.SparkSubmit --master 'local[4]' --conf spark.network.timeout=10000000 \
+                                        --conf spark.executor.heartbeatInterval=10000000 --conf spark.python.use.daemon=false \
+                                        --conf spark.python.worker.reuse=false \
+                                        /ppml/trusted-big-data-ml/work/spark-3.1.2/python/pyspark/sql/tests/$suite" \
+                                        > secured_argvs
+        ./init.sh
+        gramine-sgx bash 2>&1 | tee /ppml/trusted-big-data-ml/logs/pyspark/sql/$suite.log
+        echo "##########$suite Test:"
+        if [ -n "$(grep "FAILED" /ppml/trusted-big-data-ml/logs/pyspark/sql/$suite.log -H -o)" ]
+        then
+            echo "failed"
+            rm /ppml/trusted-big-data-ml/logs/pyspark/sql/$suite.log
+        else
+            echo "pass"
+            break
+        fi
+    done
     echo "##########$suite Test Done"
 done
--- ppml/trusted-big-data-ml/python/docker-gramine/test-suites/spark-sql-ut.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/test-suites/spark-sql-ut.sh
@@ -7,20 +7,24 @@ cd /ppml/trusted-big-data-ml
 ./clean.sh
 for suite in `cat /ppml/trusted-big-data-ml/work/test-suites/sparkSqlSuites.txt`
 do
-    gramine-argv-serializer bash -c "/opt/jdk8/bin/java -cp '$SPARK_HOME/conf/:$SPARK_HOME/jars/*:$SPARK_HOME/test-jars/*:$SPARK_HOME/test-classes/' \
-                                    -Xmx8g -Dspark.testing=true -Djdk.lang.Process.launchMechanism=posix_spawn -XX:MaxMetaspaceSize=256m -Dos.name='Linux' \
-                                    -Dspark.test.home=/ppml/trusted-big-data-ml/work/spark-3.1.2 -Dspark.python.use.daemon=false -Dspark.python.worker.reuse=false \
-                                    -Dspark.driver.host=127.0.0.1 org.scalatest.tools.Runner -s ${suite} -fF /ppml/trusted-big-data-ml/logs/reporter/${suite}.txt" \
-                                    > /ppml/trusted-big-data-ml/secured_argvs
-    ./init.sh
-    gramine-sgx bash 2>&1 | tee /ppml/trusted-big-data-ml/logs/runtime/${suite}.log
-    echo "##########$suite Test:"
-    if [ -z "$(grep "All tests passed" /ppml/trusted-big-data-ml/logs/reporter/${suite}.txt)" ]
-    then
-        echo "failed"
-        exit 1
-    else
-        echo "pass"
-    fi
+    while true
+        gramine-argv-serializer bash -c "/opt/jdk8/bin/java -cp '$SPARK_HOME/conf/:$SPARK_HOME/jars/*:$SPARK_HOME/test-jars/*:$SPARK_HOME/test-classes/' \
+                                        -Xmx8g -Dspark.testing=true -Djdk.lang.Process.launchMechanism=posix_spawn -XX:MaxMetaspaceSize=256m -Dos.name='Linux' \
+                                        -Dspark.test.home=/ppml/trusted-big-data-ml/work/spark-3.1.2 -Dspark.python.use.daemon=false -Dspark.python.worker.reuse=false \
+                                        -Dspark.driver.host=127.0.0.1 org.scalatest.tools.Runner -s ${suite} -fF /ppml/trusted-big-data-ml/logs/reporter/${suite}.txt" \
+                                        > /ppml/trusted-big-data-ml/secured_argvs
+        ./init.sh
+        gramine-sgx bash 2>&1 | tee /ppml/trusted-big-data-ml/logs/runtime/${suite}.log
+        echo "##########$suite Test:"
+        if [ -z "$(grep "All tests passed" /ppml/trusted-big-data-ml/logs/reporter/${suite}.txt)" ]
+        then
+            echo "failed"
+            rm /ppml/trusted-big-data-ml/logs/reporter/${suite}.txt
+            rm /ppml/trusted-big-data-ml/logs/runtime/${suite}.log
+        else
+            echo "pass"
+            break
+        fi
+    done
     echo "##########$suite Test Done"
 done
