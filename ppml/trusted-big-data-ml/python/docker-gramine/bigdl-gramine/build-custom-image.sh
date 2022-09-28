55c0a079a27bc27e4ac1332e6f404ae268941b3d
Uxito-Ada <414416158@qq.com>
2022-09-28 14:32:53 +0800

    refine

--- ppml/trusted-big-data-ml/python/docker-gramine/bigdl-gramine/build-custom-image.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/bigdl-gramine/build-custom-image.sh
@@ -1,38 +1,18 @@
-export HTTP_PROXY_HOST=your_http_proxy_host
-export HTTP_PROXY_PORT=your_http_proxy_port
-export HTTPS_PROXY_HOST=your_https_proxy_host
-export HTTPS_PROXY_PORT=your_https_proxy_port
-export CUSTOM_IMAGE_TAG=your_custom_image_tag
+export CUSTOM_IMAGE_TAG=bigdl-ppmltrusted-big-data-ml-python-gramine-custom
 export SGX_MEM_SIZE=memory_size_of_sgx_in_custom_image
 export SGX_LOG_LEVEL=log_level_of_sgx_in_custom_image
 export BIGDL_IMAGE_NAME=bigdl-ppml-trusted-big-data-ml-python-gramine-base
 export BIGDL_IMAGE_VERSION=2.1.0-SNAPSHOT
 
-if [[ "$BIGDL_IMAGE_NAME" == "your_bigdl_base_image_name_used_to_build_custom_image" ]] || [[ "$BIGDL_IMAGE_VERSION" == "your_bigdl_base_image_version_used_to_build_custom_image" ]] || [[ "$CUSTOM_IMAGE_TAG" == "your_custom_image_tag" ]]
+if [[ "$SGX_MEM_SIZE" == "memory_size_of_sgx_in_custom_image" ]] || [[ "$SGX_LOG_LEVEL" == "log_level_of_sgx_in_custom_image" ]]
 then
-    echo "Please specific name and version of your bigdl base name, tag of your user image to use."
+    echo "Please specific SGX_MEM_SIZE and SGX_LOG_LEVEL."
 else
-    Proxy_Modified="sudo docker build \
-            --build-arg BIGDL_IMAGE_NAME=${BIGDL_IMAGE_NAME} \
-            --build-arg BIGDL_IMAGE_VERSION=${BIGDL_IMAGE_VERSION} \
-	    --build-arg http_proxy=http://${HTTP_PROXY_HOST}:${HTTP_PROXY_PORT} \
-            --build-arg https_proxy=http://${HTTPS_PROXY_HOST}:${HTTPS_PROXY_PORT} \
-	    --build-arg SGX_MEM_SIZE=${SGX_MEM_SIZE} \
-	    --build-arg SGX_LOG_LEVEL=${SGX_LOG_LEVEL} \
-            -t $CUSTOM_IMAGE_TAG -f ./Dockerfile ."
-    
-    No_Proxy_Modified="sudo docker build \
-	    --build-arg BIGDL_IMAGE_NAME=${BIGDL_IMAGE_NAME} \
-	    --build-arg BIGDL_IMAGE_VERSION=${BIGDL_IMAGE_VERSION} \
-	    --build-arg SGX_MEM_SIZE=${SGX_MEM_SIZE} \
-            --build-arg SGX_LOG_LEVEL=${SGX_LOG_LEVEL} \
-            -t $CUSTOM_IMAGE_TAG -f ./Dockerfile ."
-    if [[ "$HTTP_PROXY_HOST" == "your_http_proxy_host" ]] || [[ "$HTTP_PROXY_PORT" == "your_http_proxy_port" ]] || [[ "$HTTPS_PROXY_HOST" == "your_https_proxy_host" ]] || [[ "$HTTPS_PROXY_PORT" == "your_https_proxy_port" ]]
-    then
-        echo "If your environment don't need to set proxy, please ignore this notice information; if your environment need to set proxy, please delet the image just created and modify the proxy in the script, then rerun this script."
-        $No_Proxy_Modified
-    else
-        $Proxy_Modified
-    fi
+    sudo docker build \
+         --build-arg BIGDL_IMAGE_NAME=${BIGDL_IMAGE_NAME} \
+         --build-arg BIGDL_IMAGE_VERSION=${BIGDL_IMAGE_VERSION} \
+	 --build-arg SGX_MEM_SIZE=${SGX_MEM_SIZE} \
+	 --build-arg SGX_LOG_LEVEL=${SGX_LOG_LEVEL} \
+         -t $CUSTOM_IMAGE_TAG -f ./Dockerfile .
 fi
 
