f7941ed0d363ff134e9a9c9ddea6474b13777678
zhaojiehu <zhaojie.hu@intel.com>
2022-10-08 15:01:13 +0800

    add install options for chronos dockerfile

--- docker/chronos-nightly/install-python-env.sh
+++ docker/chronos-nightly/install-python-env.sh
@@ -1,5 +1,5 @@
-conda create -y -n chronos python=3.7 setuptools=58.0.4 && \
-source activate chronos && \
+conda create -y -n chronos python=3.7 setuptools=58.0.4
+source activate chronos
 pip install --no-cache-dir prophet==1.1.0 &&\
 pip install --no-cache-dir pmdarima==1.8.4 && \
 pip install --no-cache-dir neural_compressor==1.8.1 && \
@@ -17,6 +17,59 @@ pip install --no-cache-dir prometheus_pandas==0.3.1 && \
 pip install --no-cache-dir xgboost==1.2.0 && \
 pip install --no-cache-dir jupyter==1.0.0
 
+model=$1
+auto_tuning=$2
+hardware=$3
+options=()
+
+if [ $model == "pytorch" ] || [ $model == "tensorflow" ];
+then
+    options[0]=$model
+
+    if [ $auto_tuning == "y" ];
+    then
+        options[1]="automl"
+    fi
+
+    if [ $hardware == "cluster" ];
+    then
+        options[2]="distributed"
+    fi
+else
+    # prophet and arima
+
+    if [ $auto_tuning == "y" ];
+    then
+        options[0]="distributed"
+    fi
+
+    if [ $hardware == "cluster" ];
+    then
+        if [ ${#options[@]} == 0 ];
+        then
+            options[0]="distributed"
+        fi
+    fi
+
+    if [$model == "prophet"];
+    then
+        pip install --no-cache-dir prophet==1.1.0
+    else
+        # ARIMA
+        pip install --no-cache-dir pmdarima==1.8.5
+    fi
+fi
+
+if [ ${#options[@]} == 0 ];
+then
+    pip install --no-cache-dir --pre --upgrade bigdl-chronos
+else
+    echo $options
+    str=${options[*]}
+    opts=`echo $str | tr ' ' ','`
+    echo $opts
+    pip install --no-cache-dir --pre --upgrade bigdl-chronos[$opts]
+fi
 
 
 
