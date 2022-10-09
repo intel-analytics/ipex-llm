4d6e24d2c945501d3a68b27f96b0600a0d9b005c
Uxito-Ada <414416158@qq.com>
2022-10-09 10:38:36 +0800

    refine

--- ppml/trusted-big-data-ml/python/docker-gramine/base/test-suites/pyspark-sql-api-examples.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/base/test-suites/pyspark-sql-api-examples.sh
@@ -33,8 +33,7 @@ fi
 
 if [ $status_2_pyspark_sql_api_SQLContext -ne 0 ]; then
 echo "pysaprk sql api example.2 --- SQLContext"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
@@ -42,168 +41,147 @@ gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
     --conf spark.sql.broadcastTimeout=3000 \
-    /ppml/trusted-big-data-ml/work/examples/sql_context_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_context_example.py"
 gramine-sgx bash 2>&1 | tee test-sql-context-sgx.log
 status_2_pyspark_sql_api_SQLContext=$(echo $?)
 fi
 
 if [ $status_3_pyspark_sql_api_UDFRegistration -ne 0 ]; then
 echo "pysaprk sql api example.3 --- UDFRegistration"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/spark-sql_2.12-3.1.2.jar' \
   -Xmx1g \
     org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_UDFRegistration_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_UDFRegistration_example.py"
 gramine-sgx bash 2>&1 | tee test-sql-UDFRegistration.log
 status_3_pyspark_sql_api_UDFRegistration=$(echo $?)
 fi
 
 if [ $status_4_pyspark_sql_api_GroupedData -ne 0 ]; then
 echo "pysaprk sql api example.4 --- GroupedData"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_groupeddata_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_groupeddata_example.py"
 gramine-sgx bash 2>&1 | tee test-sql-groupeddata-sgx.log
 status_4_pyspark_sql_api_GroupedData=$(echo $?)
 fi
 
 if [ $status_5_pyspark_sql_api_Column -ne 0 ]; then
 echo "pysaprk sql api example.5 --- Column"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_column_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_column_example.py"
 gramine-sgx bash 2>&1 | tee test-sql-column-sgx.log
 status_5_pyspark_sql_api_Column=$(echo $?)
 fi
 
 if [ $status_6_pyspark_sql_api_Row_and_DataFrameNaFunctions -ne 0 ]; then
 echo "pysaprk sql api example.6 --- Row_and_DataFrameNaFunctions"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_row_func_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_row_func_example.py"
 gramine-sgx bash 2>&1 | tee test-sql-row-sgx.log
 status_6_pyspark_sql_api_Row_and_DataFrameNaFunctions=$(echo $?)
 fi
 
 if [ $status_7_pyspark_sql_api_Window -ne 0 ]; then
 echo "pysaprk sql api example.7 --- Window"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_window_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_window_example.py"
 gramine-sgx bash 2>&1 | tee test-window-sgx.log
 status_7_pyspark_sql_api_Window=$(echo $?)
 fi
 
 if [ $status_8_pyspark_sql_api_DataframeReader -ne 0 ]; then
 echo "pysaprk sql api example.8 --- DataframeReader"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_dataframe_reader_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_dataframe_reader_example.py"
 gramine-sgx bash 2>&1 | tee test-dataframe-reader-sgx.log
 status_8_pyspark_sql_api_DataframeReader=$(echo $?)
 fi
 
 if [ $status_9_pyspark_sql_api_DataframeWriter -ne 0 ]; then
 echo "pysaprk sql api example.9 --- DataframeWriter"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_dataframe_writer_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_dataframe_writer_example.py"
 gramine-sgx bash 2>&1 | tee test-dataframe-writer-sgx.log
 status_9_pyspark_sql_api_DataframeWriter=$(echo $?)
 fi
 
 if [ $status_10_pyspark_sql_api_HiveContext -ne 0 ]; then
 echo "pysaprk sql api example.10 --- HiveContext"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_hive_context_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_hive_context_example.py"
 gramine-sgx bash 2>&1 | tee sql_hive_context_example-sgx.log
 status_10_pyspark_sql_api_HiveContext=$(echo $?)
 fi
 
 if [ $status_11_pyspark_sql_api_Catalog -ne 0 ]; then
 echo "pysaprk sql api example.11 --- Catalog"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_catalog_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_catalog_example.py"
 gramine-sgx bash 2>&1 | tee sql_catalog_example-sgx.log
 status_11_pyspark_sql_api_Catalog=$(echo $?)
 fi
 
 if [ $status_12_pyspark_sql_types_module -ne 0 ]; then
 echo "pysaprk sql api example.12 --- types module"
-./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
   -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
   -Xmx1g \
   org.apache.spark.deploy.SparkSubmit \
     --master 'local[4]' \
     --conf spark.python.use.daemon=false \
     --conf spark.python.worker.reuse=false \
-    /ppml/trusted-big-data-ml/work/examples/sql_types_example.py" > secured_argvs
-./init.sh
+    /ppml/trusted-big-data-ml/work/examples/sql_types_example.py"
 gramine-sgx bash 2>&1 | tee sql_types_example-sgx.log
 status_12_pyspark_sql_types_module=$(echo $?)
 fi
@@ -212,16 +190,14 @@ if [ $status_13_pyspark_sql_functions_module -ne 0 ]; then
 echo "pysaprk sql api example.13 --- pyspark api functions"
   for f_num in {a..g}
   do
-    ./clean.sh
-gramine-argv-serializer bash -c "/opt/jdk8/bin/java \
+export spark_commnd="/opt/jdk8/bin/java \
       -cp '/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*' \
       -Xmx1g \
       org.apache.spark.deploy.SparkSubmit \
         --master 'local[4]' \
         --conf spark.python.use.daemon=false \
         --conf spark.python.worker.reuse=false \
-        /ppml/trusted-big-data-ml/work/examples/sql_functions_${f_num}_example.py" > secured_argvs
-./init.sh
+        /ppml/trusted-big-data-ml/work/examples/sql_functions_${f_num}_example.py"
 gramine-sgx bash 2>&1 | tee sql_functions_${f_num}_example.log
   done
 fi
