b71733e8eb5e0294a7c4bac5d5f977b88d914172
Juntao Luo <1072087358@qq.com>
2022-10-10 15:59:19 +0800

    update: add comments

--- docs/readthedocs/source/_static/js/custom.js
+++ docs/readthedocs/source/_static/js/custom.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 //    $('.btn.dropdown-toggle.nav-item').text('Libraries'); // change text for dropdown menu in header from More to Libraries
     $('.navbar-end-item.navbar-end__search-button-container').remove(); // remove search button from top bar manually
-    $('#ethical-ad-placement').css({
-        "transform":"scale(0.3)",
-        "position":"absolute",
+    $('#ethical-ad-placement').css({  //weanken the display effect of ads div
+        "transform":"scale(0.3)",   // make it smaller
+        "position":"absolute",      // modify its position
         "top":"-75px",
         "left":"-110px",
-        "opacity":"0.3"
+        "opacity":"0.3"             // modify its opacity
     });
     $('#ethical-ad-placement').parent().css({
-        "position":"relative",
-        "height":"60px"
+        "position":"relative",      // modify ads position
+        "height":"60px"             // give it a default height to prevent wrong display
     })
 })
 
