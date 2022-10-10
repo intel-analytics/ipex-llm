78e8c108ee89e3f5ac44d7b8f9992a34a1268314
Juntao Luo <1072087358@qq.com>
2022-10-10 15:37:07 +0800

    Test: weaken ads by js

--- docs/readthedocs/source/_static/js/custom.js
+++ docs/readthedocs/source/_static/js/custom.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
 //    $('.btn.dropdown-toggle.nav-item').text('Libraries'); // change text for dropdown menu in header from More to Libraries
     $('.navbar-end-item.navbar-end__search-button-container').remove(); // remove search button from top bar manually
+    $('#ethical-ad-placement').css({
+        "transform":"scale(0.3)",
+        "position":"absolute",
+        "top":"-75px",
+        "left":"15px",
+        "opacity":"0.3"
+    });
+    $('#ethical-ad-placement').parent().css({
+        "position":"relative",
+        "height":"60px"
+    })
 })
+
+// #ethical-ad-placement: transform:scale(0.3) position:absolute(前一个设置成relative height 60px) top:-75px left: -15px opacity:0.3
