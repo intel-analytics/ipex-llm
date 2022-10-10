975468a76ab0733ce06e301b09043a2a5bd3287f
Juntao Luo <1072087358@qq.com>
2022-10-10 15:59:19 +0800

    update: add comments

--- docs/readthedocs/source/_static/js/custom.js
+++ docs/readthedocs/source/_static/js/custom.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-//    $('.btn.dropdown-toggle.nav-item').text('Libraries'); // change text for dropdown menu in header from More to Libraries
+    $('.btn.dropdown-toggle.nav-item').text('Libraries'); // change text for dropdown menu in header from More to Libraries
+    $('.navbar-end-item.navbar-end__search-button-container').remove(); // remove search button from top bar manually
     $('#ethical-ad-placement').css({  //weanken the display effect of ads div
         "transform":"scale(0.3)",   // make it smaller
         "position":"absolute",      // modify its position
