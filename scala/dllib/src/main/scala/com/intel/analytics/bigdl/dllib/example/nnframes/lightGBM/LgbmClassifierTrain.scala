da767891af3ef241d405bf8ea49038e9467f621a
songhappy <guoqiongsong@gmail.com>
2022-10-09 22:28:22 -0700

    scala LGBM using iris data

--- scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/example/nnframes/lightGBM/LgbmClassifierTrain.scala
+++ scala/dllib/src/main/scala/com/intel/analytics/bigdl/dllib/example/nnframes/lightGBM/LgbmClassifierTrain.scala
@@ -17,12 +17,12 @@
 package com.intel.analytics.bigdl.dllib.example.nnframes.lightGBM
 
 import com.intel.analytics.bigdl.dllib.NNContext
-import com.intel.analytics.bigdl.dllib.nnframes.XGBClassifier
-import com.microsoft.azure.synapse.ml.lightgbm.{LightGBMClassifier => MLightGBMClassifier}
 import com.intel.analytics.bigdl.dllib.nnframes.LightGBMClassifier
-import com.intel.analytics.bigdl.dllib.utils.Engine
+import com.microsoft.azure.synapse.ml.lightgbm.{LightGBMClassifier => MLightGBMClassifier}
 import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, MulticlassClassificationEvaluator}
-import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 import scopt.OptionParser
 
 object LgbmClassifierTrain {
@@ -31,48 +31,52 @@ object LgbmClassifierTrain {
 
     val defaultParams = Utils.LGBMParams()
     Utils.parser.parse(args, defaultParams).foreach { params =>
-      // val spark = SparkSession.builder().appName("LGBM example").getOrCreate()
-
       val sc = NNContext.initNNContext("LGBM example")
       val spark = SQLContext.getOrCreate(sc)
-      val train_df = spark.read.parquet(params.inputPath + "/train").repartition(10)
-      val test_df = spark.read.parquet(params.inputPath + "/test").repartition(10)
 
-      train_df.show(10)
+      val schema = new StructType(Array(
+        StructField("sepal length", DoubleType, true),
+        StructField("sepal width", DoubleType, true),
+        StructField("petal length", DoubleType, true),
+        StructField("petal width", DoubleType, true),
+        StructField("class", StringType, true)))
+      val df = spark.read.schema(schema).csv(params.inputPath)
 
+      val stringIndexer = new StringIndexer()
+        .setInputCol("class")
+        .setOutputCol("classIndex")
+        .fit(df)
+      val labelTransformed = stringIndexer.transform(df).drop("class")
+      // compose all feature columns as vector
+      val vectorAssembler = new VectorAssembler().
+        setInputCols(Array("sepal length", "sepal width", "petal length", "petal width")).
+        setOutputCol("features")
+      val dfinput = vectorAssembler.transform(labelTransformed).select("features",
+        "classIndex")
+
+      val Array(train, test) = dfinput.randomSplit(Array(0.8, 0.2))
       val classifier = new LightGBMClassifier()
-      classifier.setObjective("binary")
       classifier.setNumIterations(params.numIterations)
-      val model = classifier.fit(train_df)
-      val predictions = model.transform(test_df)
+      classifier.setNumLeaves(params.numLeaves)
+      classifier.setMaxDepth(params.maxDepth)
+      classifier.setLambdaL1(params.lamda1)
+      classifier.setLambdaL2(params.lamda2)
+      classifier.setBaggingFreq(params.bagFreq)
+      classifier.setMaxBin(params.maxBin)
+      classifier.setNumIterations(params.numIterations)
 
-      val mclassifier = new MLightGBMClassifier()
-      mclassifier.setObjective("binary")
-      mclassifier.setNumIterations(params.numIterations)
-      val mmodel = mclassifier.fit(train_df)
-      val mpredictions = mmodel.transform(test_df)
+      val model = classifier.fit(train)
+      val predictions = model.transform(test)
 
       predictions.show(10)
-      mpredictions.show(10)
-
-      val evaluatorBin = new BinaryClassificationEvaluator()
-        .setLabelCol("label")
-        .setRawPredictionCol("rawPrediction")
-        .setMetricName("areaUnderROC")
 
       val evaluatorMulti = new MulticlassClassificationEvaluator()
         .setLabelCol("label")
         .setMetricName("accuracy")
 
-      val auc = evaluatorBin.evaluate(predictions)
       val acc = evaluatorMulti.evaluate(predictions)
-      val mauc = evaluatorBin.evaluate(mpredictions)
-      val macc = evaluatorMulti.evaluate(mpredictions)
 
-      println("auc: " + auc)
       println("acc: " + acc)
-      println("mauc: " + mauc)
-      println("macc: " + macc)
     }
   }
 }
@@ -81,6 +85,13 @@ private object Utils {
 
   case class LGBMParams(
     inputPath: String = "/Users/guoqiong/intelWork/data/tweet/xgb_processed",
+    numLeaves: Int = 10,
+    maxDepth: Int = 6,
+    lamda1: Double = 0.01,
+    lamda2: Double = 0.01,
+    bagFreq: Int = 5,
+    minDataInLeaf: Int = 20,
+    maxBin: Int = 255,
     numIterations: Int = 100,
     modelSavePath: String = "/tmp/lgbm/classifier")
 
@@ -88,6 +99,27 @@ private object Utils {
     opt[String]("inputPath")
       .text(s"inputPath")
       .action((x, c) => c.copy(inputPath = x))
+    opt[Int]("numLeaves")
+      .text(s"numLeaves")
+      .action((x, c) => c.copy(numLeaves = x))
+    opt[Int]("maxDepth")
+      .text(s"maxDepth")
+      .action((x, c) => c.copy(maxDepth = x))
+    opt[Double]("lamda1")
+      .text(s"lamda1")
+      .action((x, c) => c.copy(lamda1 = x))
+    opt[Double]("lamda2")
+      .text(s"lamda2")
+      .action((x, c) => c.copy(lamda2 = x))
+    opt[Int]("bagFreq")
+      .text(s"bagFreq")
+      .action((x, c) => c.copy(bagFreq = x))
+    opt[Int]("minDataInLeaf")
+      .text(s"minDataInLeaf")
+      .action((x, c) => c.copy(minDataInLeaf = x))
+    opt[Int]("maxBin")
+      .text(s"maxBin")
+      .action((x, c) => c.copy(maxBin = x))
     opt[Int]('n', "numIterations")
       .text(s"numIterations")
       .action((x, c) => c.copy(numIterations = x))
