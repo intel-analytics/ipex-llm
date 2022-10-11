f028a296e3e5cb5e5de73b002505c906cd03de19
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2022-10-11 11:08:45 +0800

    Update train_xshards.py

--- python/orca/tutorial/pytorch/NCF/train_xshards.py
+++ python/orca/tutorial/pytorch/NCF/train_xshards.py
@@ -15,63 +15,53 @@
 # ==============================================================================
 # Most of the pytorch code is adapted from guoyang9's NCF implementation for
 # ml-1m dataset.
-# guoyang9's source code: https://github.com/guoyang9/NCF
+# https://github.com/guoyang9/NCF
 #
 
-import os
-import time
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import scipy.sparse as sp
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
-import torch.nn.functional as F 
-from model import NCF
 
-#Step 1: Init Orca Context
+# Step 1: Init Orca Context
 
 from bigdl.orca import init_orca_context, stop_orca_context
-init_orca_context(cores=1, memory="8g")
+init_orca_context()
 
-#Step 2: Define Dataset
+# Step 2: Define Dataset
 
 from bigdl.orca.data import XShards
 from bigdl.orca.data.pandas import read_csv
 from sklearn.model_selection import train_test_split
 
-# prepare the train and test datasets
-user_num = 6040
-item_num = 3952
 
-def preprocess_data():   
+def preprocess_data():
     data_X = read_csv(
-        "ml-1m/ratings.dat", 
-        sep="::", header=None, names=['user', 'item'], 
-        usecols=[0, 1], dtype={0: np.int32, 1: np.int32})  
-    data_X = data_X.partition_by("user", data_X.num_partitions())
-    return data_X
+        "ml-1m/ratings.dat",
+        sep="::", header=None, names=['user', 'item'],
+        usecols=[0, 1], dtype={0: np.int64, 1: np.int64})
+    data_X = data_X.partition_by("user")
 
-data_X = preprocess_data()
+    user_set = set(data_X["user"].unique())
+    item_set = set(data_X["item"].unique())
+    user_num = max(user_set) + 1
+    item_num = max(item_set) + 1
+    return data_X, user_num, item_num
 
-# construct the train and test xshards
+
 def ng_sampling(data):
-    data["user"] = data["user"] - 1
-    data["item"] = data["item"] - 1
     data_X = data.values.tolist()
 
-    #calculate a dok matrix
+    # calculate a dok matrix
     train_mat = sp.dok_matrix((user_num, item_num), dtype=np.int64)
     for row in data_X:
         train_mat[row[0], row[1]] = 1
 
-    #negative sampling
+    # negative sampling
     features_ps = data_X
     features_ng = []
     for x in features_ps:
         u = x[0]
-        for t in range(4):# sample 4 negative items for training
+        for t in range(4):  # sample 4 negative items for training
             j = np.random.randint(item_num)
             while (u, j) in train_mat:
                 j = np.random.randint(item_num)
@@ -81,60 +71,77 @@ def ng_sampling(data):
     labels_ng = [0 for _ in range(len(features_ng))]
 
     features_fill = features_ps + features_ng
-    labels_fill = labels_ps + labels_ng      
+    labels_fill = labels_ps + labels_ng
     data_XY = pd.DataFrame(data=features_fill, columns=["user", "item"])
     data_XY["y"] = labels_fill
     return data_XY
 
-def transform_to_dict(data):
-    #split training set and testing set
-    train_data, test_data = train_test_split(data, test_size=0.2, random_state=100)
 
-    #transform dataset into dict
+def transform_to_dict(data):
+    # split training set and testing set
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=100)
+    # transform dataset into dict
     train_data = train_data.to_numpy()
     test_data = test_data.to_numpy()
-    train_data = {"x": train_data[:, :-1].astype(np.int64), "y": train_data[:, -1].astype(np.float)}
-    test_data = {"x": test_data[:, :-1].astype(np.int64), "y": test_data[:, -1].astype(np.float)}  
+    train_data = {"x": train_data[:, : -1].astype(np.int64),
+        "y": train_data[:, -1].astype(np.float)}
+    test_data = {"x": test_data[:, : -1].astype(np.int64),
+        "y": test_data[:, -1].astype(np.float)}
     return train_data, test_data
 
+# Prepare the train and test datasets
+data_X, user_num, item_num = preprocess_data()
+
+# Construct the train and test xshards
 data_X = data_X.transform_shard(ng_sampling)
 train_shards, test_shards = data_X.transform_shard(transform_to_dict).split()
 
-#Step 3: Define the Model
+# Step 3: Define the Model
 
-# create the model
+import torch.nn as nn
+import torch.optim as optim
+from model import NCF
+
+
 def model_creator(config):
-    model = NCF(user_num, item_num, factor_num=32, num_layers=3, dropout=0.0, model="NeuMF-end") # a torch.nn.Module
+    model = NCF(config['user_num'], config['item_num'], \
+        factor_num=32, num_layers=3, dropout=0.0, model="NeuMF-end")
     model.train()
     return model
 
-#create the optimizer
+
 def optimizer_creator(model, config):
     return optim.Adam(model.parameters(), lr=0.001)
 
-#define the loss function
 loss_function = nn.BCEWithLogitsLoss()
 
-#Step 4: Fit with Orca Estimator
+# Step 4: Fit with Orca Estimator
 
-from bigdl.orca.learn.pytorch import Estimator 
+from bigdl.orca.learn.pytorch import Estimator
 from bigdl.orca.learn.metrics import Accuracy, AUC
 
-# create the estimator
-est = Estimator.from_torch(model=model_creator, optimizer=optimizer_creator, loss=loss_function, metrics=[Accuracy(), AUC()], backend="spark")# backend="ray" or "spark"
+# Create the estimator
+backend = "spark"  # "ray" or "spark"
+est = Estimator.from_torch(model=model_creator,
+                        optimizer=optimizer_creator,
+                        loss=loss_function, 
+                        metrics=[Accuracy(), AUC()],
+                        config={'user_num': user_num, 'item_num': item_num},
+                        backend=backend)
 
-# fit the estimator
-est.fit(data=train_shards, epochs=5, batch_size=256, feature_cols=["x"], label_cols =["y"])
+# Fit the estimator
+est.fit(data=train_shards, epochs=1, batch_size=256,
+    feature_cols=["user", "item"], label_cols=["y"])
 
-#Step 5: Evaluate and save the Model
+# Step 5: Evaluate and save the Model
 
-# evaluate the model
+# Evaluate the model
 result = est.evaluate(data=test_shards)
 for r in result:
     print(r, ":", result[r])
 
-# save the model
-est.save("NCF_model") 
+# Save the model
+est.save("NCF_model")
 
-# stop orca context when program finishes
+# Stop orca context when program finishes
 stop_orca_context()
