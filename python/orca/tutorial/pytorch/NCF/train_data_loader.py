bc983d1fdd1502bd6d7f51a97d2d7da5f6275187
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2022-10-11 11:08:33 +0800

    Update train_data_loader.py

--- python/orca/tutorial/pytorch/NCF/train_data_loader.py
+++ python/orca/tutorial/pytorch/NCF/train_data_loader.py
@@ -15,60 +15,33 @@
 # ==============================================================================
 # Most of the pytorch code is adapted from guoyang9's NCF implementation for
 # ml-1m dataset.
-# guoyang9's source code: https://github.com/guoyang9/NCF
+# https://github.com/guoyang9/NCF
 #
 
-import os
-import time
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import scipy.sparse as sp
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as data
-import torch.nn.functional as F 
-from model import NCF
 
-#Step 1: Init Orca Context
+# Step 1: Init Orca Context
 
 from bigdl.orca import init_orca_context, stop_orca_context
-init_orca_context(cores=1, memory="8g") # 1 cpu core
+init_orca_context()
 
-#Step 2: Define Train Dataset
+# Step 2: Define Train Dataset
 
 from sklearn.model_selection import train_test_split
+import torch.utils.data as data
 
-def load_all():
-    """ We load all the files here to save time in each epoch. """
-    data_X = pd.read_csv(
-        "ml-1m/ratings.dat", 
-        sep="::", header=None, names=['user', 'item'], 
-        usecols=[0, 1], dtype={0: np.int32, 1: np.int32})
-    
-    user_num = data_X['user'].max() + 1
-    item_num = data_X['item'].max() + 1
-
-    data_X = data_X.values.tolist()
-
-    # load ratings as a dok matrix
-    train_mat = sp.dok_matrix((user_num, item_num), dtype=np.float32)
-    for x in data_X:
-        train_mat[x[0], x[1]] = 1.0
-        
-    train_data, test_data = train_test_split(data_X, test_size=0.1, random_state=100)
-    
-    return train_data, test_data, user_num, item_num, train_mat
 
 class NCFData(data.Dataset):
-    def __init__(self, features, 
-                num_item, train_mat=None, num_ng=4, is_training=None):
+    def __init__(self, features,
+                    item_num=0, train_mat=None, num_ng=4, is_training=False):
         super(NCFData, self).__init__()
-        """ Note that the labels are only useful when training, we thus 
+        """ Note that the labels are only useful when training, we thus
             add them in the ng_sample() function.
         """
         self.features_ps = features
-        self.num_item = num_item
+        self.item_num = item_num
         self.train_mat = train_mat
         self.num_ng = num_ng
         self.is_training = is_training
@@ -81,9 +54,9 @@ class NCFData(data.Dataset):
         for x in self.features_ps:
             u = x[0]
             for t in range(self.num_ng):
-                j = np.random.randint(self.num_item)
+                j = np.random.randint(self.item_num)
                 while (u, j) in self.train_mat:
-                    j = np.random.randint(self.num_item)
+                    j = np.random.randint(self.item_num)
                 self.features_ng.append([u, j])
 
         labels_ps = [1 for _ in range(len(self.features_ps))]
@@ -93,70 +66,105 @@ class NCFData(data.Dataset):
         self.labels_fill = labels_ps + labels_ng
 
     def __len__(self):
-        return (self.num_ng + 1) * len(self.labels)
-    
+        if self.is_training:
+            return (self.num_ng + 1) * len(self.labels)
+        return len(self.labels)
+
     def __getitem__(self, idx):
-        features =  self.features_fill if self.is_training else self.features_ps
-        labels =  self.labels_fill if self.is_training else self.labels
+        features = self.features_fill if self.is_training else self.features_ps
+        labels = self.labels_fill if self.is_training else self.labels
 
         user = features[idx][0]
         item = features[idx][1]
         label = float(labels[idx])
-        return [user, item] ,label
+        return user, item, label
 
-# prepare the train and test datasets
-train_data, test_data, user_num, item_num, train_mat = load_all()
 
-#Step 3: Define the Model
+def train_loader_func(config, batch_size):
+    data_X = pd.read_csv(
+        "ml-1m/ratings.dat",
+        sep="::", header=None, names=['user', 'item'],
+        usecols=[0, 1], dtype={0: np.int64, 1: np.int64})
+    user_num = data_X['user'].max() + 1
+    item_num = data_X['item'].max() + 1
 
-# create the model
+    data_X = data_X.values.tolist()
+
+    # load ratings as a dok matrix
+    train_mat = sp.dok_matrix((user_num, item_num), dtype=np.int64)
+    for x in data_X:
+        train_mat[x[0], x[1]] = 1
+
+    train_data, _ = train_test_split(data_X, test_size=0.1, random_state=100)
+
+    train_dataset = NCFData(train_data, item_num=item_num, train_mat=train_mat, num_ng=4, is_training=True)
+    train_loader = data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=0)
+    train_loader.dataset.ng_sample()  # sample negative items for training datasets
+    return train_loader
+
+
+def test_loader_func(config, batch_size):
+    data_X = pd.read_csv(
+        "ml-1m/ratings.dat",
+        sep="::", header=None, names=['user', 'item'],
+        usecols=[0, 1], dtype={0: np.int64, 1: np.int64})
+
+    data_X = data_X.values.tolist()
+
+    _, test_data = train_test_split(data_X, test_size=0.1, random_state=100)
+
+    test_dataset = NCFData(test_data)
+    test_loader = data.DataLoader(test_dataset, shuffle=False, num_workers=0)
+    return test_loader
+
+# Step 3: Define the Model
+
+from model import NCF
+import torch.nn as nn
+import torch.optim as optim
+
+
 def model_creator(config):
-    model = NCF(user_num, item_num, factor_num=32, num_layers=3, dropout=0.0, model="NeuMF-end") # a torch.nn.Module
+    data_X = pd.read_csv(
+        "ml-1m/ratings.dat",
+        sep="::", header=None, names=['user', 'item'],
+        usecols=[0, 1], dtype={0: np.int64, 1: np.int64})
+    user_num = data_X['user'].max() + 1
+    item_num = data_X['item'].max() + 1
+
+    model = NCF(user_num, item_num, \
+        factor_num=32, num_layers=3, dropout=0.0, model="NeuMF-end")
     model.train()
     return model
 
-#create the optimizer
+
 def optimizer_creator(model, config):
     return optim.Adam(model.parameters(), lr=0.001)
 
-#define the loss function
 loss_function = nn.BCEWithLogitsLoss()
 
-#Step 4: Fit with Orca Estimator
+# Step 4: Fit with Orca Estimator
 
-from bigdl.orca.learn.pytorch import Estimator 
+from bigdl.orca.learn.pytorch import Estimator
 from bigdl.orca.learn.metrics import Accuracy
 
-# create the estimator
-est = Estimator.from_torch(model=model_creator, optimizer=optimizer_creator,loss=loss_function, metrics=[Accuracy()],backend="ray")# backend="ray" or "spark"
+# Create the estimator
+backend = "ray"  # "ray" or "spark"
+est = Estimator.from_torch(model=model_creator, optimizer=optimizer_creator, \
+    loss=loss_function, metrics=[Accuracy()], backend=backend)
 
-# construct the train and test dataloader
-def train_loader_func(config, batch_size):
-    train_dataset = NCFData(train_data, item_num, train_mat, num_ng=4, is_training=True)
-    train_loader = data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=0)
-    train_loader.dataset.ng_sample()# sample negative items for training datasets
-    return train_loader
+# Fit the estimator
+est.fit(data=train_loader_func, epochs=1)
 
-def test_loader_func(config, batch_size):
-    test_dataset = NCFData(test_data, item_num, train_mat, num_ng=0, is_training=False)
-    test_loader = data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=0)
-    return test_loader
+# Step 5: Save and Load the Model
 
-# fit the estimator
-est.fit(data=train_loader_func, epochs=5)
-
-#Step 5: Save and Load the Model
-
-# save the model
-est.save("NCF_model")
-
-# load the model
-est.load("NCF_model")
-
-# evaluate the model
+# Evaluate the model
 result = est.evaluate(data=test_loader_func)
 for r in result:
     print(r, ":", result[r])
 
-# stop orca context when program finishes
+# Save the model
+est.save("NCF_model")
+
+# Stop orca context when program finishes
 stop_orca_context()
