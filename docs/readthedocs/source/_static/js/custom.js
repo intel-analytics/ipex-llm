1eb963a2f2dc7d3ebb71adf1773c8436af8aa7aa
Juntao Luo <1072087358@qq.com>
2022-10-14 12:48:50 +0800

    Update: delete my template and change files.

--- docs/readthedocs/source/_static/js/custom.js
+++ docs/readthedocs/source/_static/js/custom.js
@@ -1,22 +1,15 @@
 $(document).ready(function(){
-    $('.btn.dropdown-toggle.nav-item').text('Libraries'); // change text for dropdown menu in header from More to Libraries
-    $('.navbar-end-item.navbar-end__search-button-container').remove(); // remove search button from top bar manually
-    $('#ethical-ad-placement').css({  //weanken the display effect of ads div
-        // "transform":"scale(0.3)",   // make it smaller
-        // "position":"absolute",      // modify its position
-        // "top":"-75px",
-        // "left":"-110px",
-        // "opacity":"0.7"             // modify its opacity
+//  $('.btn.dropdown-toggle.nav-item').text('Libraries'); // change text for dropdown menu in header from More to Libraries
+
+    // hide the original left sidebar ads display
+    $('#ethical-ad-placement').css({
         "display":"none"
     });
-    // $('#ethical-ad-placement').parent().css({
-    //     "position":"relative",      // modify ads position
-    //     "height":"60px"             // give it a default height to prevent wrong display
-    // })
 
     // manually add the ads to the end of content
     $(".bd-article").append(
-        "<div style='display:flex;justify-content:center;'\
+        "<br />\
+        <div style='display:flex;justify-content:center;'\
         <div\
         id='ethical-ad-placement'\
         class='horizontal'\
