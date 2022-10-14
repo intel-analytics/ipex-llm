72c957328722083a3a6c45db9a58dc714abb2e13
Juntao Luo <1072087358@qq.com>
2022-10-14 11:17:57 +0800

    Update: try to change the position of ads.

--- docs/readthedocs/source/_static/js/custom.js
+++ docs/readthedocs/source/_static/js/custom.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
     $('.btn.dropdown-toggle.nav-item').text('Libraries'); // change text for dropdown menu in header from More to Libraries
     $('.navbar-end-item.navbar-end__search-button-container').remove(); // remove search button from top bar manually
     $('#ethical-ad-placement').css({  //weanken the display effect of ads div
-        "transform":"scale(0.3)",   // make it smaller
-        "position":"absolute",      // modify its position
-        "top":"-75px",
-        "left":"-110px",
-        "opacity":"0.7"             // modify its opacity
+        // "transform":"scale(0.3)",   // make it smaller
+        // "position":"absolute",      // modify its position
+        // "top":"-75px",
+        // "left":"-110px",
+        // "opacity":"0.7"             // modify its opacity
+        "display":"none"
     });
     $('#ethical-ad-placement').parent().css({
         "position":"relative",      // modify ads position
