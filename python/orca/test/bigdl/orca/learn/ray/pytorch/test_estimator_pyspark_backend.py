d91b2cfa57643088391ccf58f1801a5e5f10ca96
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2022-10-14 17:47:03 +0800

    Update test_estimator_pyspark_backend.py

--- python/orca/test/bigdl/orca/learn/ray/pytorch/test_estimator_pyspark_backend.py
+++ python/orca/test/bigdl/orca/learn/ray/pytorch/test_estimator_pyspark_backend.py
@@ -253,23 +253,21 @@ class TestPyTorchEstimator(TestCase):
         print(val_stats)
 
     def test_spark_xshards_not_dict(self):
-        from bigdl.dllib.nncontext import init_nncontext
-        from bigdl.orca.data import SparkXShards
-        estimator = get_estimator(workers_per_node=1,
-                                  model_fn=lambda config: MultiInputNet())
-        sc = init_nncontext()
-        x_rdd = sc.parallelize(np.random.rand(4000, 1, 50).astype(np.float32))
-        # torch 1.7.1+ requires target size same as output size, which is (batch, 1)
-        y_rdd = sc.parallelize(np.random.randint(0, 2, size=(4000, 1, 1)).astype(np.float32))
-        rdd = x_rdd.zip(y_rdd).map(lambda x_y: {'feature': x_y[0], 'label': x_y[1]})
-        train_rdd, val_rdd = rdd.randomSplit([0.9, 0.1])
-        train_xshards = SparkXShards(train_rdd)
-        val_xshards = SparkXShards(val_rdd)
-        train_stats = estimator.fit(train_xshards, validation_data=val_xshards,
-                                    batch_size=256, epochs=2, feature_cols=["feature"], label_cols=["label"])
-        print(train_stats)
-        val_stats = estimator.evaluate(val_xshards, batch_size=128)
-        print(val_stats)
+        OrcaContext.pandas_read_backend = "pandas"
+        file_path = os.path.join(resource_path, "orca/learn/ncf.csv")
+        data_shard = read_csv(file_path, usecols=[0, 1, 2], dtype={0: np.float32, 1: np.float32,
+                                                                   2: np.float32})
+        
+        estimator = get_estimator(model_fn=lambda config: SimpleModel())
+        estimator.fit(data_shard, batch_size=2, epochs=2,
+                      validation_data=data_shard,
+                      feature_cols=["user", "item"],
+                      label_cols=["label"])
+
+        estimator.evaluate(data_shard, batch_size=2, feature_cols=["user", "item"],
+                           label_cols=["label"])
+        result = estimator.predict(data_shard, batch_size=2, feature_cols=["user", "item"])
+        result.collect()
 
     def test_dataframe_train_eval(self):
 
