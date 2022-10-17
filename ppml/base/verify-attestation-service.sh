ba94891242c4aaf155597beedcbdcb0518495400
Xiangyu Tian <oveoveove142@outlook.com>
2022-10-17 15:10:38 +0800

    Init ehsm verification

--- ppml/base/verify-attestation-service.sh
+++ ppml/base/verify-attestation-service.sh
@@ -6,8 +6,7 @@ export ATTESTATION_TYPE=your_attestation_service_type
 export APP_ID=your_app_id
 export API_KEY=your_api_key
 export CHALLENGE=your_challenge_string
-export SPARK_HOME=$SPARK_HOME
-export BIGDL_PPML_JAR=$BIGDL_HOME/jars/*
+export JARS=your_verification_jars_path
 
 if [ "$ATTESTATION_URL" = "your_attestation_url" ]; then
     echo "[ERROR] ATTESTATION_URL is not set!"
@@ -42,7 +41,5 @@ if [ "$BIGDL_PPML_JAR" = "your_bigdl_ppml_jar" ]; then
     echo "[INFO] PPML Application Exit!"
     exit 1
 fi
-
-JARS="$SPARK_HOME/jars/*:$SPARK_HOME/examples/jars/*:$BIGDL_PPML_JAR"
 
 java -cp $JARS com.intel.analytics.bigdl.ppml.attestation.VerificationCLI -i $APP_ID -k $API_KEY -c $CHALLENGE -u $ATTESTATION_URL -t $ATTESTATION_TYPE
