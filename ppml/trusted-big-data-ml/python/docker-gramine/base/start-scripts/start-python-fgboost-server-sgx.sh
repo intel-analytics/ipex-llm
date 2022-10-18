ad6e3e126bbb5200d9fab118853764e3c532793b
Jin Hanyu <476099001@qq.com>
2022-10-18 16:33:28 +0800

    Change the bigdl and spark version to environment variables.

--- ppml/trusted-big-data-ml/python/docker-gramine/base/start-scripts/start-python-fgboost-server-sgx.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/base/start-scripts/start-python-fgboost-server-sgx.sh
@@ -15,7 +15,7 @@ do
 done
 cd /ppml/trusted-big-data-ml
 export sgx_command="/opt/jdk8/bin/java\
-        -cp '/ppml/trusted-big-data-ml/work/bigdl-2.2.0-SNAPSHOT/jars/bigdl-ppml-spark_3.1.2-2.2.0-SNAPSHOT.jar:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/commons-lang3-3.10.jar:/ppml/trusted-big-data-ml/work/bigdl-2.2.0-SNAPSHOT/jars/*:/ppml/trusted-big-data-ml/work/spark-3.1.2/conf/:/ppml/trusted-big-data-ml/work/spark-3.1.2/jars/*:/ppml/trusted-big-data-ml/work/spark-3.1.2/examples/jars/*'\
+        -cp '/ppml/trusted-big-data-ml/work/bigdl-${BIGDL_VERSION}/jars/bigdl-ppml-spark_${SPARK_VERSION}-${BIGDL_VERSION}.jar:/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/jars/commons-lang3-3.10.jar:/ppml/trusted-big-data-ml/work/bigdl-${BIGDL_VERSION}/jars/*:/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/conf/:/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/jars/*:/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/examples/jars/*'\
         -Xmx10g org.apache.spark.deploy.SparkSubmit\
         --master local[4]\
         /ppml/trusted-big-data-ml/fl/start-fgboost-server.py --port $port --client_num $client_num"
