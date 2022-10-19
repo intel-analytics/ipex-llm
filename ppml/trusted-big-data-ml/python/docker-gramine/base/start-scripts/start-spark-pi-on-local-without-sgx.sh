3a3b30aa05cd093cca20a62c627a82e1c3cf59ea
00-Luoshu <1372476112@qq.com>
2022-10-19 10:39:28 +0800

    Replace incorrect single quotes with double

--- ppml/trusted-big-data-ml/python/docker-gramine/base/start-scripts/start-spark-pi-on-local-without-sgx.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/base/start-scripts/start-spark-pi-on-local-without-sgx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 /opt/jdk8/bin/java \
-    -cp '/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/conf/:/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/jars/*:/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/examples/jars/*' -Xmx16g \
+    -cp "/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/conf/:/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/jars/*:/ppml/trusted-big-data-ml/work/spark-${SPARK_VERSION}/examples/jars/*" -Xmx16g \
     org.apache.spark.deploy.SparkSubmit \
     --master local[2] \
     --executor-memory 8g \
