f3791adcd1ed4cb51f8bdbdf5e9587a3a755a032
songhappy <guoqiongsong@gmail.com>
2022-10-20 16:32:26 -0700

    refactor test files

--- python/orca/test/bigdl/orca/data/test_spark_backend.py
+++ python/orca/test/bigdl/orca/data/test_spark_backend.py
@@ -29,7 +29,7 @@ from bigdl.orca.data.utils import *
 from bigdl.orca.data.transformer import *
 
 
-class TestSparkBackend(TestCase):
+class TestXShardsSparkBackend(TestCase):
     def setup_method(self, method):
         self.resource_path = os.path.join(os.path.split(__file__)[0], "../resources")
 
@@ -51,6 +51,83 @@ class TestSparkBackend(TestCase):
             file_path, header=None, names=["ID", "sale_price", "location"])
         df3 = data_shard.collect()[0]
         assert "sale_price" in df3.columns
+
+    def test_read_invalid_path(self):
+        file_path = os.path.join(self.resource_path, "abc")
+        with self.assertRaises(Exception) as context:
+            xshards = bigdl.orca.data.pandas.read_csv(file_path)
+        # This error is raised by pyspark.sql.utils.AnalysisException
+        self.assertTrue('Path does not exist' in str(context.exception))
+
+    def test_read_json(self):
+        file_path = os.path.join(self.resource_path, "orca/data/json")
+        data_shard = bigdl.orca.data.pandas.read_json(file_path)
+        data = data_shard.collect()
+        df = data[0]
+        assert "timestamp" in df.columns and "value" in df.columns
+        data_shard = bigdl.orca.data.pandas.read_json(file_path, names=["time", "value"])
+        data = data_shard.collect()
+        df2 = data[0]
+        assert "time" in df2.columns and "value" in df2.columns
+        data_shard = bigdl.orca.data.pandas.read_json(file_path, usecols=[0])
+        data = data_shard.collect()
+        df3 = data[0]
+        assert "timestamp" in df3.columns and "value" not in df3.columns
+        data_shard = bigdl.orca.data.pandas.read_json(file_path, dtype={"value": "float"})
+        data = data_shard.collect()
+        df4 = data[0]
+        assert df4.value.dtype == "float64"
+
+    def test_read_parquet(self):
+        file_path = os.path.join(self.resource_path, "orca/data/csv")
+        sc = init_nncontext()
+        from pyspark.sql.functions import col
+        spark = OrcaContext.get_spark_session()
+        df = spark.read.csv(file_path, header=True)
+        df = df.withColumn('sale_price', col('sale_price').cast('int'))
+        temp = tempfile.mkdtemp()
+        df.write.parquet(os.path.join(temp, "test_parquet"))
+        data_shard2 = bigdl.orca.data.pandas.read_parquet(os.path.join(temp, "test_parquet"))
+        assert data_shard2.num_partitions() == 2, "number of shard should be 2"
+        data = data_shard2.collect()
+        df = data[0]
+        assert "location" in df.columns
+
+        data_shard2 = bigdl.orca.data.pandas.read_parquet(os.path.join(temp, "test_parquet"),
+                                                          columns=['ID', 'sale_price'])
+        data = data_shard2.collect()
+        df = data[0]
+        assert len(df.columns) == 2
+
+        from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+        schema = StructType([StructField("ID", StringType(), True),
+                             StructField("sale_price", IntegerType(), True),
+                             StructField("location", StringType(), True)])
+        data_shard3 = bigdl.orca.data.pandas.read_parquet(os.path.join(temp, "test_parquet"),
+                                                          columns=['ID', 'sale_price'],
+                                                          schema=schema)
+        data = data_shard3.collect()
+        df = data[0]
+        assert str(df['sale_price'].dtype) == 'int64'
+
+        shutil.rmtree(temp)
+
+    def test_write_read_imagenet(self):
+        raw_data = os.path.join(self.resource_path, "imagenet_to_tfrecord")
+        temp_dir = tempfile.mkdtemp()
+        try:
+            write_tfrecord(format="imagenet", imagenet_path=raw_data, output_path=temp_dir)
+            data_dir = os.path.join(temp_dir, "train")
+            train_dataset = read_tfrecord(format="imagenet", path=data_dir, is_training=True)
+            train_dataset.take(1)
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_read_large_csv(self):
+        file_path = os.path.join(self.resource_path, "orca/data/10010.csv")
+        data_shard = bigdl.orca.data.pandas.read_csv(file_path)
+        res = data_shard.collect()
+        assert len(res[0]) == 10009, "number of records should be 10009"
 
     def test_usecols(self):
         file_path = os.path.join(self.resource_path, "orca/data/csv")
@@ -144,118 +221,6 @@ class TestSparkBackend(TestCase):
             data = data_shard.collect()
         self.assertTrue('column index to be set type is not in current dataframe'
                         in str(context.exception))
-
-    def test_read_invalid_path(self):
-        file_path = os.path.join(self.resource_path, "abc")
-        with self.assertRaises(Exception) as context:
-            xshards = bigdl.orca.data.pandas.read_csv(file_path)
-        # This error is raised by pyspark.sql.utils.AnalysisException
-        self.assertTrue('Path does not exist' in str(context.exception))
-
-    def test_read_json(self):
-        file_path = os.path.join(self.resource_path, "orca/data/json")
-        data_shard = bigdl.orca.data.pandas.read_json(file_path)
-        data = data_shard.collect()
-        df = data[0]
-        assert "timestamp" in df.columns and "value" in df.columns
-        data_shard = bigdl.orca.data.pandas.read_json(file_path, names=["time", "value"])
-        data = data_shard.collect()
-        df2 = data[0]
-        assert "time" in df2.columns and "value" in df2.columns
-        data_shard = bigdl.orca.data.pandas.read_json(file_path, usecols=[0])
-        data = data_shard.collect()
-        df3 = data[0]
-        assert "timestamp" in df3.columns and "value" not in df3.columns
-        data_shard = bigdl.orca.data.pandas.read_json(file_path, dtype={"value": "float"})
-        data = data_shard.collect()
-        df4 = data[0]
-        assert df4.value.dtype == "float64"
-
-    def test_read_parquet(self):
-        file_path = os.path.join(self.resource_path, "orca/data/csv")
-        sc = init_nncontext()
-        from pyspark.sql.functions import col
-        spark = OrcaContext.get_spark_session()
-        df = spark.read.csv(file_path, header=True)
-        df = df.withColumn('sale_price', col('sale_price').cast('int'))
-        temp = tempfile.mkdtemp()
-        df.write.parquet(os.path.join(temp, "test_parquet"))
-        data_shard2 = bigdl.orca.data.pandas.read_parquet(os.path.join(temp, "test_parquet"))
-        assert data_shard2.num_partitions() == 2, "number of shard should be 2"
-        data = data_shard2.collect()
-        df = data[0]
-        assert "location" in df.columns
-
-        data_shard2 = bigdl.orca.data.pandas.read_parquet(os.path.join(temp, "test_parquet"),
-                                                        columns=['ID', 'sale_price'])
-        data = data_shard2.collect()
-        df = data[0]
-        assert len(df.columns) == 2
-
-        from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-        schema = StructType([StructField("ID", StringType(), True),
-                             StructField("sale_price", IntegerType(), True),
-                             StructField("location", StringType(), True)])
-        data_shard3 = bigdl.orca.data.pandas.read_parquet(os.path.join(temp, "test_parquet"),
-                                                        columns=['ID', 'sale_price'],
-                                                        schema=schema)
-        data = data_shard3.collect()
-        df = data[0]
-        assert str(df['sale_price'].dtype) == 'int64'
-
-        shutil.rmtree(temp)
-
-    def test_write_read_imagenet(self):
-        raw_data = os.path.join(self.resource_path, "imagenet_to_tfrecord")
-        temp_dir = tempfile.mkdtemp()
-        try:
-            write_tfrecord(format="imagenet", imagenet_path=raw_data, output_path=temp_dir)
-            data_dir = os.path.join(temp_dir, "train")
-            train_dataset = read_tfrecord(format="imagenet", path=data_dir, is_training=True)
-            train_dataset.take(1)
-        finally:
-            shutil.rmtree(temp_dir)
-
-    def test_to_spark_df(self):
-        file_path = os.path.join(self.resource_path, "orca/data/csv")
-        data_shard = bigdl.orca.data.pandas.read_csv(file_path, header=0, names=['user', 'item'],
-                                                   usecols=[0, 1])
-        df = data_shard.to_spark_df()
-        df.show()
-
-    def test_read_large_csv(self):
-        file_path = os.path.join(self.resource_path, "orca/data/10010.csv")
-        data_shard = bigdl.orca.data.pandas.read_csv(file_path)
-        res = data_shard.collect()
-        assert len(res[0]) == 10009, "number of records should be 10009"
-
-    def test_spark_df_to_shards(self):
-        file_path = os.path.join(self.resource_path, "orca/data/csv")
-        from pyspark.sql import SparkSession
-        spark = SparkSession.builder.master("local[1]")\
-            .appName('test_spark_backend')\
-            .config("spark.driver.memory", "6g").getOrCreate()
-        df = spark.read.csv(file_path)
-        data_shards = spark_df_to_pd_sparkxshards(df)
-
-    def test_minmaxscale_shards(self):
-        file_path = os.path.join(self.resource_path, "orca/data/csv")
-        data_shard = bigdl.orca.data.pandas.read_csv(file_path)
-        scale = MinMaxScaler(inputCol=["sale_price"], outputCol="sale_price_scaled")
-        transformed_data_shard = scale.fit_transform(data_shard)
-
-    def test_standardscale_shards(self):
-        file_path = os.path.join(self.resource_path, "orca/data/csv")
-
-        data_shard = bigdl.orca.data.pandas.read_csv(file_path)
-        scale = StandardScaler(inputCol="sale_price", outputCol="sale_price_scaled")
-        transformed_data_shard = scale.fit_transform(data_shard)
-
-    def test_max_values(self):
-        file_path = os.path.join(self.resource_path, "orca/data/csv/morgage1.csv")
-        data_shard = bigdl.orca.data.pandas.read_csv(file_path)
-        max_value = data_shard.max_values('sale_price')
-        assert max_value == 475000, "max value of sale_price should be 2"
 
 
 if __name__ == "__main__":
