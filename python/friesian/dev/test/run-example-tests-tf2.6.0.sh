d96455f24f99126f5f6b8125d96ee5f1db100a34
ForJadeForest <1113480910@qq.com>
2022-10-20 15:54:24 +0800

    add download data from ftp

--- python/friesian/dev/test/run-example-tests-tf2.6.0.sh
+++ python/friesian/dev/test/run-example-tests-tf2.6.0.sh
@@ -117,9 +117,9 @@ if [ -d data/train_processed ]; then
   echo "data/train_processed already exists"
 else
   echo "download train_processed"
-  wget -nvr $FTP_URI/analytics-zoo-data/train_processed -P data
+  wget -nv -r $FTP_URI/analytics-zoo-data/train_processed -P data
    echo "download test_processed"
-  wget -nvr $FTP_URI/analytics-zoo-data/test_processed -P data
+  wget -nv -r $FTP_URI/analytics-zoo-data/test_processed -P data
 fi
 
 start=$(date "+%s")
