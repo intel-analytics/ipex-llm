c80a57aa103a35244d1daf37a8d58390c16016f5
dding3 <ding.ding@intel.com>
2022-10-24 11:03:10 -0700

    fix style check failure

--- python/orca/tutorial/xshards/answer_correctness_analysis.py
+++ python/orca/tutorial/xshards/answer_correctness_analysis.py
@@ -30,32 +30,35 @@ used_data_types_list = [
     'prior_question_had_explanation'
 ]
 data_shard = bigdl.orca.data.pandas.read_csv(path,
-                                             usecols = used_data_types_list,
-                                             index_col = 0)
+                                             usecols=used_data_types_list,
+                                             index_col=0)
+
 
 def get_feature(df):
     feature_df = df.iloc[:int(9/10 * len(df))]
     return feature_df
 feature_shard = data_shard.transform_shard(get_feature)
 
+
 def get_train_questions_only(df):
     train_questions_only_df = df[df['answered_correctly'] != -1]
     return train_questions_only_df
 train_questions_only_shard = feature_shard.transform_shard(get_train_questions_only)
 
-train_questions_only_shard = train_questions_only_shard.group_by(columns='user_id',
-                                                                 agg={"answered_correctly":
-                                                                          ['mean',
-                                                                           'count',
-                                                                           'stddev',
-                                                                           'skewness']},
-                                                                 join=True)
+train_questions_only_shard = \
+    train_questions_only_shard.group_by(columns='user_id', agg={"answered_correctly": ['mean',
+                                                                                       'count',
+                                                                                       'stddev',
+                                                                                       'skewness']
+                                                                }, join=True)
 target = 'answered_correctly'
+
 
 def filter_non_target(df):
     train_df = df[df[target] != -1]
     return train_df
 train_shard = train_questions_only_shard.transform_shard(filter_non_target)
+
 
 def fill_na(df, val):
     train_df = df.fillna(value=val)
