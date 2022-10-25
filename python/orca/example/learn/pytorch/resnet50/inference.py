8f25519b034f82e2671c6361baa8b92302f586c7
Kai Huang <huangkaivision@gmail.com>
2022-10-25 17:11:13 +0800

    override operator

--- python/orca/example/learn/pytorch/resnet50/inference.py
+++ python/orca/example/learn/pytorch/resnet50/inference.py
@@ -30,6 +30,7 @@ import torchvision.models as models
 
 from bigdl.dllib.utils.log4Error import *
 from bigdl.orca import init_orca_context, stop_orca_context
+from bigdl.orca.learn.pytorch import TrainingOperator
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Inference')
 parser.add_argument('data', metavar='DIR',
@@ -53,13 +54,103 @@ parser.add_argument('--hub', action='store_true', default=False,
                     help='use model with torch hub')
 parser.add_argument('--pretrained', dest='pretrained', action='store_true',
                     help='use pre-trained model')
+parser.add_argument('--steps', default=-1, type=int,
+                    help='steps for validation')
+parser.add_argument('--calibration', action='store_true', default=False,
+                    help='doing calibration step for int8 path')
+parser.add_argument('-w', '--warmup-iterations', default=100, type=int, metavar='N',
+                    help='number of warmup iterations to run')
+parser.add_argument('--configure-dir', default='configure.json', type=str, metavar='PATH',
+                    help = 'path to int8 configures, default file name is configure.json')
+
+
+class ResNetPerfOperator(TrainingOperator):
+    def validate(self, val_iterator, info, metrics, num_steps=None):
+        self.model.eval()
+        from bigdl.orca.learn.metrics import Metric
+        metrics = Metric.convert_metrics_dict(metrics, backend="pytorch")
+        losses = []
+        total_samples = 0
+
+        # TODO: not sure if this is needed
+        if self.config["ipex"] and self.config["int8"] and self.config["calibration"]:
+            print("running int8 calibration step\n")
+            import intel_extension_for_pytorch as ipex
+            from torch.ao.quantization import MinMaxObserver, PerChannelMinMaxObserver, QConfig
+            qconfig = QConfig(
+                activation=MinMaxObserver.with_args(qscheme=torch.per_tensor_symmetric, dtype=torch.qint8),
+                weight=PerChannelMinMaxObserver.with_args(dtype=torch.qint8, qscheme=torch.per_channel_symmetric))
+            x = torch.randn(1, 3, 224, 224)
+            prepared_model = ipex.quantization.prepare(self.model, qconfig, x, inplace=True)
+            with torch.no_grad():
+                for i, (images, target) in enumerate(val_iterator):
+                    images = images.contiguous(memory_format=torch.channels_last)
+                    prepared_model(images)
+                    if i == 4:
+                        print(i)
+                        break
+                prepared_model.save_qconf_summary(self.config["configure_dir"])
+                print(".........calibration step done..........")
+
+        if self.use_tqdm and self.world_rank == 0:
+            from tqdm import tqdm
+            _progress_bar = tqdm(
+                total=len(val_iterator),
+                unit="batch",
+                leave=False)
+
+        with torch.no_grad():
+            for batch_idx, batch in enumerate(val_iterator):
+                if num_steps and batch_idx == num_steps:
+                    break
+                batch_info = {"batch_idx": batch_idx}
+                batch_info.update(info)
+                output, target, loss = self.forward_batch(batch, batch_info)
+                if self.use_tqdm and self.world_rank == 0:
+                    _progress_bar.n = batch_idx + 1
+                    postfix = {}
+                    postfix.update(loss=loss)
+                    _progress_bar.set_postfix(postfix)
+                from bigdl.orca.learn.pytorch.utils import get_batchsize
+                num_samples = get_batchsize(target)
+                total_samples += num_samples
+                losses.append(loss.item() * num_samples)
+                for metric in metrics.values():
+                    metric(output, target)
+
+        result = {name: metric.compute() for name, metric in metrics.items()}
+        result["val_loss"] = sum(losses) / total_samples
+        result["num_samples"] = total_samples
+        return result
+
+
+    def forward_batch(self, batch, batch_info):
+        images, target = batch
+
+        # compute output
+        with self.timers.record("eval_fwd"):
+            if self.config["ipex"]:
+                images = images.contiguous(memory_format=torch.channels_last)
+            if self.config["bf16"]:
+                images = images.to(torch.bfloat16)
+            if not self.config["jit"] and self.config["bf16"]:
+                with torch.cpu.amp.autocast():
+                    output = self.model(images)
+            else:
+                output = self.model(images)
+
+        if self.config["bf16"]:
+            output = output.to(torch.float32)
+        loss = self.criterion(output, target)
+
+        return output, target, loss
 
 
 class DummyData(torch.utils.data.Dataset):
 
-    def __init__(self, use_ipex, use_bf16):
+    def __init__(self, size, use_ipex, use_bf16):
         super(DummyData, self).__init__()
-        self.len = 1000
+        self.len = size
         self.features = torch.randn(self.len, 3, 224, 224)
         self.labels = torch.arange(1, self.len + 1).long()
         if use_ipex:
@@ -110,8 +201,8 @@ def validate(args):
         else:
             # dummy data, always running channle last for fp32, bf16, int8
             val_loader = torch.utils.data.DataLoader(
-                DummyData(args.ipex, args.bf16),
-                batch_size=args.batch_size, shuffle=False,
+                DummyData(config["number_iter"]*batch_size, args.ipex, args.bf16),
+                batch_size=batch_size, shuffle=False,
                 num_workers=args.workers, pin_memory=True)
         return val_loader
 
@@ -123,10 +214,10 @@ def validate(args):
         else:
             # create model
             if args.pretrained:
-                print("=> using pre-trained model '{}'".format(args.arch))
+                print("=> using pre-trained model '{}'".format(arch))
                 model = models.__dict__[arch](pretrained=True)
             else:
-                print("=> creating model '{}'".format(args.arch))
+                print("=> creating model '{}'".format(arch))
                 model = models.__dict__[arch]()
 
         if args.ipex:
@@ -150,18 +241,6 @@ def validate(args):
                     y = model(x)
                     y = model(x)
                     print("running int8 evaluation step\n")
-
-                import torch.fx.experimental.optimization as optimization
-
-                model = optimization.fuse(model, inplace=True)
-                conf = ipex.quantization.QuantConf(args.configure_dir)
-                x = torch.randn(args.batch_size, 3, 224, 224) \
-                    .contiguous(memory_format=torch.channels_last)
-                model = ipex.quantization.convert(model, conf, x)
-                with torch.no_grad():
-                    y = model(x)
-                    print(model.graph_for(x))
-                print("running int8 evaluation step\n")
             else:
                 # for ipex path, always convert model to channels_last for bf16, fp32.
                 # TODO: int8 path: https://jira.devtools.intel.com/browse/MFDNN-6103
@@ -171,8 +250,7 @@ def validate(args):
                 if args.bf32:
                     ipex.set_fp32_math_mode(mode=ipex.FP32MathMode.BF32, device="cpu")
                     print("using bf32 fmath mode\n")
-
-                if args.bf16:
+                elif args.bf16:
                     model = ipex.optimize(model, dtype=torch.bfloat16, inplace=True)
                     print("running bfloat16 evaluation step\n")
                 else:
@@ -203,21 +281,34 @@ def validate(args):
         return optimizer
 
 
+    if args.dummy:
+        number_iter = args.steps if args.steps > 0 else 200
+        if args.int8:
+            number_iter = args.steps if args.steps > 0 else 200
+    else:
+        number_iter = args.steps if args.steps > 0 else None
+    if args.calibration:
+        number_iter = 100
+
     from bigdl.orca.learn.pytorch import Estimator
     from bigdl.orca.learn.metrics import Accuracy
 
+    config = vars(args)
+    config["number_iter"] = number_iter
+    batch_size = config.pop("batch_size")
     backend = "ray"
     est = Estimator.from_torch(model=model_creator,
                                optimizer=optimizer_creator,
                                loss=nn.CrossEntropyLoss(),
                                metrics=[Accuracy()],
                                backend=backend,
-                               workers_per_node=args.workers_per_node)
-    if not args.jit and args.bf16:
-        with torch.cpu.amp.autocast():
-            result = est.evaluate(data=val_loader_func, batch_size=args.batch_size, profile=True)
-    else:
-        result = est.evaluate(data=val_loader_func, batch_size=args.batch_size, profile=True)
+                               config=config,
+                               workers_per_node=args.workers_per_node,
+                               training_operator_cls=ResNetPerfOperator,
+                               use_tqdm=True)
+
+    result = est.evaluate(data=val_loader_func, batch_size=batch_size,
+                          num_steps=number_iter, profile=True)
     for r in result:
         print(r, ":", result[r])
 
