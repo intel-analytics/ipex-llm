fd3752de8473b53334c892e3d60bd18bbe8a45a6
hzjane <a1015616934@qq.com>
2022-10-26 13:32:52 +0800

    update sgx_debug

--- ppml/trusted-big-data-ml/scala/docker-occlum/run_spark_on_occlum_glibc.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/run_spark_on_occlum_glibc.sh
@@ -116,6 +116,10 @@ init_instance() {
     fi
 
     # check occlum log level for docker
+    if [[-z "$ENABLE_SGX_DEBUG"]]; then
+        echo "No ENABLE_SGX_DEBUG specified, set to off."
+        export ENABLE_SGX_DEBUG=false
+    fi
     export OCCLUM_LOG_LEVEL=off
     if [[ -z "$SGX_LOG_LEVEL" ]]; then
         echo "No SGX_LOG_LEVEL specified, set to off."
