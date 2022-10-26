fd2fc8ba272d94e08a05331ed09ac436001642e2
Yina Chen <33650826+cyita@users.noreply.github.com>
2022-10-26 09:55:16 +0800

    Add friesian serving profile (#6156)
    * add serving profile
    
    * add serving profile
    
    * test mvn package
    
    * fix yml style
    
    * change runson
    
    * test
    
    * test
    
    * test
    
    * update docker download
    
    * update action
    
    * update version for CVE
    
    * replace pwd env
    
    * add install wheel

--- docker/friesian-serving/download-friesian-serving.sh
+++ docker/friesian-serving/download-friesian-serving.sh
@@ -21,10 +21,4 @@ set -x
 echo $BIGDL_VERSION
 echo $SPARK_VERSION
 
-if [[ $BIGDL_VERSION == *"SNAPSHOT"* ]]; then
-  NIGHTLY_VERSION=$(echo $(echo `wget -qO - https://oss.sonatype.org/content/repositories/snapshots/com/intel/analytics/bigdl/bigdl-friesian-spark_$SPARK_VERSION/$BIGDL_VERSION/maven-metadata.xml | sed -n '/<value>[0-9]*\.[0-9]*\.[0-9]*-[0-9][0-9]*\.[0-9][0-9]*-[0-9][0-9]*.*value>/p' | head -n1 | awk -F'>' '{print $2}' | tr '</value' ' '`))
-  JAR_NAME=bigdl-friesian-spark_$SPARK_VERSION-$NIGHTLY_VERSION-serving.jar
-  wget https://oss.sonatype.org/content/repositories/snapshots/com/intel/analytics/bigdl/bigdl-friesian-spark_$SPARK_VERSION/$BIGDL_VERSION/$JAR_NAME -O bigdl-friesian-serving.jar
-else
-  wget https://repo1.maven.org/maven2/com/intel/analytics/bigdl/bigdl-friesian-spark_$SPARK_VERSION/$BIGDL_VERSION/bigdl-friesian-spark_$SPARK_VERSION-$BIGDL_VERSION-serving.jar -O bigdl-friesian-serving.jar
-fi
+wget https://sourceforge.net/projects/analytics-zoo/files/friesian-serving-jars/bigdl-friesian-spark_$SPARK_VERSION-$BIGDL_VERSION-serving.jar -O bigdl-friesian-serving.jar
