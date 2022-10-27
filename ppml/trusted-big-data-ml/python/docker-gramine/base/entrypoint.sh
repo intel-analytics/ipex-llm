239c2ef1d95a0927c715763a62b284af67d0fd08
Uxito-Ada <414416158@qq.com>
2022-10-27 12:09:31 +0800

    [PPML] Adjust attestation in /trusted-big-data-ml/gramine

--- ppml/trusted-big-data-ml/python/docker-gramine/base/entrypoint.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/base/entrypoint.sh
@@ -80,28 +80,25 @@ fi
 
 # Attestation
 if [ -z "$ATTESTATION" ]; then
-    echo "[INFO] Attestation is disabled!"
-    ATTESTATION="false"
-elif [ "$ATTESTATION" = "true" ]; then
-  echo "[INFO] Attestation is enabled!"
-  # Build ATTESTATION_COMMAND
-  if [ -z "$ATTESTATION_URL" ]; then
-    echo "[ERROR] Attestation is enabled, but ATTESTATION_URL is empty!"
-    echo "[INFO] PPML Application Exit!"
-    exit 1
-  fi
-  if [ -z "$ATTESTATION_ID" ]; then
-    echo "[ERROR] Attestation is enabled, but ATTESTATION_ID is empty!"
-    echo "[INFO] PPML Application Exit!"
-    exit 1
-  fi
-  if [ -z "$ATTESTATION_KEY" ]; then
-    echo "[ERROR] Attestation is enabled, but ATTESTATION_KEY is empty!"
-    echo "[INFO] PPML Application Exit!"
-    exit 1
-  fi
-  ATTESTATION_COMMAND="/opt/jdk8/bin/java -Xmx1g -cp $SPARK_CLASSPATH:$BIGDL_HOME/jars/* com.intel.analytics.bigdl.ppml.attestation.AttestationCLI -u ${ATTESTATION_URL} -i ${ATTESTATION_ID}  -k ${ATTESTATION_KEY}"
+  echo "[INFO] Attestation is disabled!"
+  ATTESTATION="false"
 fi
+
+echo $SGX_ENABLED
+
+runtime_command="$@"
+
+if [ "$SGX_ENABLED" == "true" ]; then
+  if [ "$ATTESTATION" ==  "true" ]; then 
+    bash attestation.sh
+    echo $runtime_command >> temp_command_file
+    export sgx_command="bash temp_command_file && rm temp_command_file"
+  else 
+    export sgx_command=$runtime_command
+  fi
+  ./init.sh && \
+  gramine-sgx bash 2>&1
+else
 
 
 if [ "$PYSPARK_MAJOR_PYTHON_VERSION" == "2" ]; then
