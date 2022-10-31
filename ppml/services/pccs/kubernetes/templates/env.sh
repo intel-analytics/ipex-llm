962accaf8a48d85231ecff0298cc6715b5f0dabd
liyao <1599208165@qq.com>
2022-10-31 15:47:44 +0800

    add pccs env.sh

---
+++ ppml/services/pccs/kubernetes/templates/env.sh
@@ -0,0 +1,12 @@
+export pccsIP=172.168.0.226
+export pccsImageName=intelanalytics/pccs:0.3.0-SNAPSHOTip
+export apiKey=59335c1eb98549ee92d51c82cc632ae9 # Refer to README for obtaining pcs api key
+# The below service IP should be a IP address that unused in your subnetwork
+# Please do not use the same real IPs as nodes
+export httpsProxyUrl=http://child-prc.intel.com:913
+export countryName=cn
+export cityName=sh
+export organizaitonName=intel
+export commonName=liyao
+export emailAddress=1599208165@qq.com
+export serverPassword=1234qwer
