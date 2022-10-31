145189d85097ef4de1cce4cb42079213c59883db
Uxito-Ada <414416158@qq.com>
2022-10-31 14:16:24 +0800

    rename flag

--- ppml/trusted-big-data-ml/python/docker-gramine/bigdl-gramine/build-custom-image.sh
+++ ppml/trusted-big-data-ml/python/docker-gramine/bigdl-gramine/build-custom-image.sh
@@ -4,7 +4,7 @@ export BASE_IMAGE_NAME=bigdl-ppml-trusted-big-data-ml-python-gramine-base
 export BASE_IMAGE_TAG=2.1.0-SNAPSHOT
 export SGX_MEM_SIZE=memory_size_of_sgx_in_custom_image
 export SGX_LOG_LEVEL=log_level_of_sgx_in_custom_image
-export ATTESTATION=true
+export ENABLE_DCAP_ATTESTATION=true
 
 if [[ "$SGX_MEM_SIZE" == "memory_size_of_sgx_in_custom_image" ]] || [[ "$SGX_LOG_LEVEL" == "log_level_of_sgx_in_custom_image" ]]
 then
@@ -15,7 +15,7 @@ else
          --build-arg BASE_IMAGE_TAG=${BASE_IMAGE_TAG} \
 	 --build-arg SGX_MEM_SIZE=${SGX_MEM_SIZE} \
 	 --build-arg SGX_LOG_LEVEL=${SGX_LOG_LEVEL} \
-	 --build-arg ATTESTATION=${ATTESTATION} \
+	 --build-arg ENABLE_DCAP_ATTESTATION=${ENABLE_DCAP_ATTESTATION} \
          -t ${CUSTOM_IMAGE_NAME}:${CUSTOM_IMAGE_TAG} -f ./Dockerfile .
 fi
 
