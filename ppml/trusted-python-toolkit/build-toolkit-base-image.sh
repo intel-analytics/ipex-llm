560f948018dd826d974494349f6b79024d765ca8
Jin Hanyu <476099001@qq.com>
2022-11-01 14:38:44 +0800

    Adjust Dockerfile's format, delete irrelevant parameters and rename files in ./examples.

--- ppml/trusted-python-toolkit/build-toolkit-base-image.sh
+++ ppml/trusted-python-toolkit/build-toolkit-base-image.sh
@@ -1,11 +1,11 @@
-export HTTP_PROXY_HOST=http://child-prc.intel.com
-export HTTP_PROXY_PORT=913
-export HTTPS_PROXY_HOST=http://child-prc.intel.com
-export HTTPS_PROXY_PORT=913
-export BASE_IMAGE_NAME=10.239.45.10/arda/intelanalytics/bigdl-ppml-gramine-base
-export BASE_IMAGE_TAG=test-HY
-export TOOLKIT_IMAGE_NAME=intelanalytics/gramine-toolkit-base
-export TOOLKIT_IMAGE_TAG=HY-1
+export HTTP_PROXY_HOST=your_http_proxy_host
+export HTTP_PROXY_PORT=your_http_proxy_port
+export HTTPS_PROXY_HOST=your_https_proxy_host
+export HTTPS_PROXY_PORT=your_https_proxy_port
+export BASE_IMAGE_NAME=your_base_image_name
+export BASE_IMAGE_TAG=your_base_image_tag
+export TOOLKIT_IMAGE_NAME=your_toolkit_image_name
+export TOOLKIT_IMAGE_TAG=your_toolkit_image_tag
 
 Proxy_Modified="sudo docker build \
     --build-arg http_proxy=$HTTP_PROXY_HOST \
@@ -23,17 +23,11 @@ No_Proxy_Modified="sudo docker build \
     --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG \
     -t ${FL_IMAGE_NAME}:${FL_IMAGE_TAG} -f ./Dockerfile ."
 
-
-if [[ "$JDK_URL" == "http://your-http-url-to-download-jdk" ]] || [[ "$SPARK_JAR_REPO_URL" == "http://your_spark_jar_repo_url" ]]
+if [[ "$HTTP_PROXY_HOST" == "your_http_proxy_host" ]] || [[ "$HTTP_PROXY_PORT" == "your_http_proxy_port" ]] || [[ "$HTTPS_PROXY_HOST" == "your_https_proxy_host" ]] || [[ "$HTTPS_PROXY_PORT" == "your_https_proxy_port" ]]
 then
-    echo "Please modify the path of JDK_URL and SPARK_JAR_REPO_URL to the suitable url in this script, then rerun this script. And if your environment don't need to set proxy, please ignore this notice information; if your environment need to set proxy, please modify the proxy in the script, then rerun this script."
+   echo "If your environment don't need to set proxy, please ignore this notice information; if your environment need to set proxy, please delet the image just created and modify the proxy in the script, then rerun this script."
+   $No_Proxy_Modified
 else
-    if [[ "$HTTP_PROXY_HOST" == "your_http_proxy_host" ]] || [[ "$HTTP_PROXY_PORT" == "your_http_proxy_port" ]] || [[ "$HTTPS_PROXY_HOST" == "your_https_proxy_host" ]] || [[ "$HTTPS_PROXY_PORT" == "your_https_proxy_port" ]]
-    then
-       echo "If your environment don't need to set proxy, please ignore this notice information; if your environment need to set proxy, please delet the image just created and modify the proxy in the script, then rerun this script."
-       $No_Proxy_Modified
-    else
-       $Proxy_Modified
-    fi
+   $Proxy_Modified
 fi
 
--- ppml/trusted-python-toolkit/custom-image/make-sgx.sh
+++ ppml/trusted-python-toolkit/custom-image/make-sgx.sh
@@ -6,5 +6,5 @@ echo ATTESTATION:$ATTESTATION
 if [[ "ENABLE_DCAP_ATTESTATION" == "false" ]]; then
    sed -i 's/"dcap"/"none"/g' bash.manifest.template
 fi
-make SGX=1 DEBUG=1 THIS_DIR=/ppml/trusted-big-data-ml  SPARK_USER=root G_SGX_SIZE=$SGX_MEM_SIZE G_LOG_LEVEL=$SGX_LOG_LEVEL
+make SGX=1 DEBUG=1 THIS_DIR=/ppml/trusted-big-data-ml G_SGX_SIZE=$SGX_MEM_SIZE G_LOG_LEVEL=$SGX_LOG_LEVEL
 
