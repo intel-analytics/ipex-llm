84b5c61a814bb20cc176cca7b2341999529f10ba
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2022-11-03 19:00:27 +0800

    Update model.py

--- python/orca/tutorial/pytorch/NCF/model.py
+++ python/orca/tutorial/pytorch/NCF/model.py
@@ -25,8 +25,7 @@ import torch.nn as nn
 
 class NCF(nn.Module):
     def __init__(self, user_num, item_num, factor_num, num_layers,
-                 dropout, model, GMF_model=None, MLP_model=None,
-                 cat_feats_dim=0, numeric_feats_dim=0, num_embed_cat_feats=None):
+                 dropout, model, GMF_model=None, MLP_model=None):
         super(NCF, self).__init__()
         """
         user_num: number of users;
@@ -37,35 +36,25 @@ class NCF(nn.Module):
         model: 'MLP', 'GMF', 'NeuMF-end', and 'NeuMF-pre';
         GMF_model: pre-trained GMF weights;
         MLP_model: pre-trained MLP weights;
-        cat_feats_dim: number of categorical features;
-        numeric_feats_dim: number of numerical features;
-        num_embed_cat_feats: list of num_embeddings for each categorical features.
         """
         self.dropout = dropout
         self.model = model
         self.GMF_model = GMF_model
         self.MLP_model = MLP_model
-        self.cat_feats_dim = cat_feats_dim
-        self.numeric_feats_dim = numeric_feats_dim
 
         self.embed_user_GMF = nn.Embedding(user_num, factor_num)
         self.embed_item_GMF = nn.Embedding(item_num, factor_num)
-        self.embed_user_MLP = nn.Embedding(user_num, factor_num)
-        self.embed_item_MLP = nn.Embedding(item_num, factor_num)
-        self.embed_catFeats_MLP = [nn.Embedding(num_embed_cat_feats[i], factor_num)
-                                   for i in range(cat_feats_dim)]
+        self.embed_user_MLP = nn.Embedding(user_num,
+                                           factor_num * (2 ** (num_layers - 1)))
+        self.embed_item_MLP = nn.Embedding(item_num,
+                                           factor_num * (2 ** (num_layers - 1)))
 
-        input_size = factor_num * (2 + cat_feats_dim) + numeric_feats_dim
-        output_size = factor_num * (2 ** (num_layers - 1))
         MLP_modules = []
-        MLP_modules.append(nn.Dropout(p=self.dropout))
-        MLP_modules.append(nn.Linear(input_size, output_size))
-        MLP_modules.append(nn.ReLU())
-        for i in range(num_layers-1):
+        for i in range(num_layers):
+            input_size = factor_num * (2 ** (num_layers - i))
             MLP_modules.append(nn.Dropout(p=self.dropout))
-            MLP_modules.append(nn.Linear(output_size, output_size//2))
+            MLP_modules.append(nn.Linear(input_size, input_size//2))
             MLP_modules.append(nn.ReLU())
-            output_size = output_size // 2
         self.MLP_layers = nn.Sequential(*MLP_modules)
 
         if self.model in ['MLP', 'GMF']:
@@ -117,10 +106,7 @@ class NCF(nn.Module):
             self.predict_layer.weight.data.copy_(0.5 * predict_weight)
             self.predict_layer.bias.data.copy_(0.5 * precit_bias)
 
-    def forward(self, *args):
-        user, item = args[0], args[1]
-        user = user.type(torch.LongTensor)
-        item = item.type(torch.LongTensor)
+    def forward(self, user, item):
         if not self.model == 'MLP':
             embed_user_GMF = self.embed_user_GMF(user)
             embed_item_GMF = self.embed_item_GMF(item)
@@ -129,15 +115,6 @@ class NCF(nn.Module):
             embed_user_MLP = self.embed_user_MLP(user)
             embed_item_MLP = self.embed_item_MLP(item)
             interaction = torch.cat((embed_user_MLP, embed_item_MLP), -1)
-            if self.cat_feats_dim > 0:
-                for i in range(self.cat_feats_dim):
-                    catFeat = args[2+i].type(torch.LongTensor)
-                    embed_catFeats_MLP = self.embed_catFeats_MLP[i](catFeat)
-                    interaction = torch.cat((interaction, embed_catFeats_MLP), -1)
-            if self.numeric_feats_dim > 0:
-                numeric_feats = torch.stack(args[2+self.cat_feats_dim:], dim=1) \
-                    .type(torch.FloatTensor)
-                interaction = torch.cat((interaction, numeric_feats), -1)
             output_MLP = self.MLP_layers(interaction)
 
         if self.model == 'GMF':
