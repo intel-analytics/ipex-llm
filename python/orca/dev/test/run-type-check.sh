4a1c512d5e164565f8ea46994f6503304c39e71d
leonardozcm <leonardo1997zcm@gmail.com>
2022-11-07 10:41:24 +0800

    shell

---
+++ python/orca/dev/test/run-type-check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export ORCA_HOME=${BIGDL_HOME}/python/orca
+export ORCA_DEVTEST_DIR=${BIGDL_HOME}/python/orca/dev/test
+
+# TODO: read from another file
+# "mypy --install-types --non-interactive" is to automatically install missing types 
+mypy --install-types --non-interactive --config-file ${ORCA_DEVTEST_DIR}/mypy.ini \
+        $ORCA_HOME/src/bigdl/orca/data/
