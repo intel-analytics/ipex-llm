74a9b9d8ecfd607a1fc046d49189a89f17165f9b
hzjane <a1015616934@qq.com>
2022-11-07 10:27:37 +0800

    remove musl

--- ppml/trusted-big-data-ml/scala/docker-occlum/get_quote_on_ppml.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/get_quote_on_ppml.sh
@@ -4,18 +4,10 @@ set -e
 BLUE='\033[1;34m'
 NC='\033[0m'
 
-if [[ $1 == "musl" ]]; then
-    echo "*** Build and run musl-libc dcap demo ***"
-    CC=occlum-gcc
-    LD=occlum-ld
-    LIBPATH="/opt/occlum/toolchains/dcap_lib/musl"
-else
-    echo "*** Build and run glibc dcap demo ***"
-    CC=gcc
-    LD=ld
-    LIBPATH="/opt/occlum/toolchains/dcap_lib/glibc"
-fi
-
+echo "*** Build and run glibc dcap demo ***"
+CC=gcc
+LD=ld
+LIBPATH="/opt/occlum/toolchains/dcap_lib/glibc"
 INCPATH="/opt/occlum/toolchains/dcap_lib/inc"
 
 CC=$CC LD=$LD LIBPATH=$LIBPATH make -C c_app clean
