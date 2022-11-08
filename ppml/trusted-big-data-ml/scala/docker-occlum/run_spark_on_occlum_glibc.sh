bef26d98df373771cf14051cc38bd89d348e4cc0
Wang Jian <61138589+hzjane@users.noreply.github.com>
2022-11-08 11:07:29 +0800

    [PPML] Apply pyspark in occlum2 (#6474)
    * add pi examples
    
    * add python lib yaml
    
    * update build bash and add pypi example
    
    * add install bash
    
    * update dockerfile

--- ppml/trusted-big-data-ml/scala/docker-occlum/run_spark_on_occlum_glibc.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/run_spark_on_occlum_glibc.sh
@@ -47,10 +47,12 @@ init_instance() {
         .resource_limits.kernel_space_heap_size="SGX_KERNEL_HEAP" |
         .entry_points = [ "/usr/lib/jvm/java-8-openjdk-amd64/bin", "/bin" ] |
         .env.untrusted = [ "DMLC_TRACKER_URI", "SPARK_DRIVER_URL", "SPARK_TESTING" , "_SPARK_AUTH_SECRET" ] |
-        .env.default = [ "LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk-amd64/lib/server:/usr/lib/jvm/java-8-openjdk-amd64/lib:/usr/lib/jvm/java-8-openjdk-amd64/../lib:/lib","SPARK_CONF_DIR=/opt/spark/conf","SPARK_ENV_LOADED=1","PYTHONHASHSEED=0","SPARK_HOME=/opt/spark","SPARK_SCALA_VERSION=2.12","SPARK_JARS_DIR=/opt/spark/jars","LAUNCH_CLASSPATH=/bin/jars/*",""]' Occlum.json)" && \
+        .env.default = [ "PYTHONHOME=/opt/python-occlum","LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk-amd64/lib/server:/usr/lib/jvm/java-8-openjdk-amd64/lib:/usr/lib/jvm/java-8-openjdk-amd64/../lib:/lib","SPARK_CONF_DIR=/opt/spark/conf","SPARK_ENV_LOADED=1","PYTHONHASHSEED=0","SPARK_HOME=/opt/spark","SPARK_SCALA_VERSION=2.12","SPARK_JARS_DIR=/opt/spark/jars","LAUNCH_CLASSPATH=/bin/jars/*",""]' Occlum.json)" && \
     echo "${new_json}" > Occlum.json
     echo "SGX_MEM_SIZE ${SGX_MEM_SIZE}"
 
+    #copy python lib
+    copy_bom -f /opt/python-glibc.yaml --root image --include-dir /opt/occlum/etc/template
     # enable tmp hostfs
     # --conf spark.executorEnv.USING_TMP_HOSTFS=true \
     if [[ $USING_TMP_HOSTFS == "true" ]]; then
@@ -136,6 +138,9 @@ init_instance() {
 }
 
 build_spark() {
+    # Copy python examples and unzip python lib
+    mkdir -p image/py-examples
+    cp -rf /opt/py-examples/* image/py-examples
     # Copy JVM and class file into Occlum instance and build
     cd /opt/occlum_spark
     mkdir -p image/usr/lib/jvm
@@ -241,6 +246,22 @@ build_spark() {
                 echo "verify success"
         fi
     fi
+}
+
+run_pyspark_pi() {
+    init_instance spark
+    build_spark
+    cd /opt/occlum_spark
+    echo -e "${BLUE}occlum run pyspark Pi${NC}"
+    occlum run /usr/lib/jvm/java-8-openjdk-amd64/bin/java \
+                -XX:-UseCompressedOops -XX:MaxMetaspaceSize=$META_SPACE \
+                -XX:ActiveProcessorCount=4 \
+                -Divy.home="/tmp/.ivy" \
+                -Dos.name="Linux" \
+                -Djdk.lang.Process.launchMechanism=vfork \
+                -cp "$SPARK_HOME/conf/:$SPARK_HOME/jars/*" \
+                -Xmx512m org.apache.spark.deploy.SparkSubmit \
+                /py-examples/pi.py
 }
 
 run_spark_pi() {
@@ -452,6 +473,10 @@ case "$arg" in
         export RUNTIME_ENV="native"
         init_instance
         build_spark
+        ;;
+    pypi)
+        run_pyspark_pi
+        cd ../
         ;;
     pi)
         run_spark_pi
