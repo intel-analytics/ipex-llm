449da7d67cd6bb967ef809c21865f607c8fce02f
zhaojiehu <zhaojie.hu@intel.com>
2022-11-09 15:56:19 +0800

    fix chronos install

--- python/chronos/benchmark/forecaster_inference/forecaster_inference_benchmark.sh
+++ python/chronos/benchmark/forecaster_inference/forecaster_inference_benchmark.sh
@@ -4,8 +4,8 @@ bash $ANALYTICS_ZOO_ROOT/python/chronos/dev/release/release.sh linux default fal
 bash $ANALYTICS_ZOO_ROOT/python/nano/dev/build_and_install.sh linux default false pytorch
 whl_name = `ls $ANALYTICS_ZOO_ROOT/python/nano/dist/`
 pip install $ANALYTICS_ZOO_ROOT/python/nano/dist/${whl_name}[pytorch,inference]
-whl_name=`ls $ANALYTICS_ZOO_ROOT/python/chronos/dist/`
-pip install $ANALYTICS_ZOO_ROOT/python/chronos/dist/${whl_name}[pytorch]
+whl_name=`ls $ANALYTICS_ZOO_ROOT/python/chronos/src/dist/`
+pip install $ANALYTICS_ZOO_ROOT/python/chronos/src/dist/${whl_name}[pytorch]
 
 cd $ANALYTICS_ZOO_ROOT/python/chronos/benchmark/forecaster_inference/
 
--- python/chronos/benchmark/tsdataset_processing/tsdataset_processing_benchmark.sh
+++ python/chronos/benchmark/tsdataset_processing/tsdataset_processing_benchmark.sh
@@ -2,8 +2,8 @@ set -e
 
 bash $ANALYTICS_ZOO_ROOT/python/chronos/dev//release/release.sh linux default false
 bash $ANALYTICS_ZOO_ROOT/python/nano/dev/build_and_install.sh linux default false pytorch
-whl_name=`ls $ANALYTICS_ZOO_ROOT/python/chronos/dist/`
-pip install $ANALYTICS_ZOO_ROOT/python/chronos/dist/${whl_name}
+whl_name=`ls $ANALYTICS_ZOO_ROOT/python/chronos/src/dist/`
+pip install $ANALYTICS_ZOO_ROOT/python/chronos/src/dist/${whl_name}
 
 cd $ANALYTICS_ZOO_ROOT/python/chronos/benchmark/tsdataset_processing/
 
