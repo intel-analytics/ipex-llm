6dab0200e214b3b8713645a28761540384b8af1a
zhaojiehu <zhaojie.hu@intel.com>
2022-11-09 17:00:38 +0800

    fix variable define

--- python/chronos/benchmark/forecaster_inference/forecaster_inference_benchmark.sh
+++ python/chronos/benchmark/forecaster_inference/forecaster_inference_benchmark.sh
@@ -2,7 +2,7 @@ set -e
 
 bash $ANALYTICS_ZOO_ROOT/python/chronos/dev/release/release.sh linux default false
 bash $ANALYTICS_ZOO_ROOT/python/nano/dev/build_and_install.sh linux default false pytorch
-whl_name = `ls $ANALYTICS_ZOO_ROOT/python/nano/dist/`
+whl_name=`ls $ANALYTICS_ZOO_ROOT/python/nano/dist/`
 pip install $ANALYTICS_ZOO_ROOT/python/nano/dist/${whl_name}[pytorch,inference]
 whl_name=`ls $ANALYTICS_ZOO_ROOT/python/chronos/src/dist/`
 pip install $ANALYTICS_ZOO_ROOT/python/chronos/src/dist/${whl_name}[pytorch]
