6b163a4a1e899f09cee02fcd0d17bd04ac1701a1
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2022-11-14 10:27:42 +0800

    Update process_xshards.py

--- python/orca/tutorial/NCF/process_xshards.py
+++ python/orca/tutorial/NCF/process_xshards.py
@@ -12,77 +12,89 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ==============================================================================
-# Some of the code is adapted from
-# https://github.com/guoyang9/NCF/blob/master/data_utils.py
 #
 
-import os
-import numpy as np
-import pandas as pd
-import scipy.sparse as sp
-from sklearn.model_selection import train_test_split
+# Step 0: Import necessary libraries
+import torch.nn as nn
+import torch.optim as optim
 
-from bigdl.orca.data.pandas import read_csv
+from process_xshards import prepare_data
+from pytorch_model import NCF
 
-
-def ng_sampling(data, user_num, item_num):
-    data_X = data.values.tolist()
-
-    # calculate a dok matrix
-    train_mat = sp.dok_matrix((user_num, item_num), dtype=np.int64)
-    for row in data_X:
-        train_mat[row[0], row[1]] = 1
-
-    # negative sampling
-    num_ng = 4  # sample 4 negative items for training
-    features_ps = data_X
-    features_ng = []
-    for x in features_ps:
-        u = x[0]
-        for t in range(num_ng):
-            j = np.random.randint(item_num)
-            while (u, j) in train_mat:
-                j = np.random.randint(item_num)
-            features_ng.append([u, j])
-
-    labels_ps = [1 for _ in range(len(features_ps))]
-    labels_ng = [0 for _ in range(len(features_ng))]
-
-    features_fill = features_ps + features_ng
-    labels_fill = labels_ps + labels_ng
-    data_XY = pd.DataFrame(data=features_fill, columns=["user", "item"], dtype=np.int64)
-    data_XY["label"] = labels_fill
-    data_XY["label"] = data_XY["label"].astype(np.float)
-    return data_XY
+from bigdl.orca import init_orca_context, stop_orca_context
+from bigdl.orca.learn.pytorch import Estimator
+from bigdl.orca.learn.metrics import Accuracy, Precision, Recall
 
 
-def split_dataset(data):
-    train_data, test_data = train_test_split(data, test_size=0.2, random_state=100)
-    return train_data, test_data
+# Step 1: Init Orca Context
+sc = init_orca_context()
 
 
-def prepare_data(dataset_dir):
-    data = read_csv(os.path.join(dataset_dir, 'ratings.dat'),
-                    sep="::", header=None, names=['user', 'item'],
-                    usecols=[0, 1], dtype={0: np.int64, 1: np.int64})
-    data = data.partition_by("user")
-
-    user_set = set(data["user"].unique())
-    item_set = set(data["item"].unique())
-    user_num = max(user_set) + 1
-    item_num = max(item_set) + 1
-
-    data = data.transform_shard(lambda shard: ng_sampling(shard, user_num, item_num))
-    train_data, test_data = data.transform_shard(split_dataset).split()
-    return train_data, test_data, user_num, item_num
+# Step 2: Define train and test datasets using Orca XShards
+dataset_dir = "./ml-1m"
+feature_cols = ['user', 'item',
+                'gender', 'occupation', 'zipcode', 'category',  # categorical feature
+                'age']  # numerical feature
+label_cols = ["label"]
+total_cols = feature_cols + label_cols
+train_data, test_data, num_embed_cat_feats = prepare_data(dataset_dir,
+                                                          num_ng=4, total_cols=total_cols)
 
 
-if __name__ == "__main__":
-    from bigdl.orca import init_orca_context, stop_orca_context
+# Step 3: Define the model, optimizer and loss
+def model_creator(config):
+    model = NCF(factor_num=config['factor_num'],
+                num_layers=config['num_layers'],
+                dropout=config['dropout'],
+                model=config['model'],
+                cat_feats_dim=config['cat_feats_dim'],
+                numeric_feats_dim=config['numeric_feats_dim'],
+                num_embed_cat_feats=num_embed_cat_feats)
+    model.train()
+    return model
 
-    sc = init_orca_context()
-    train_data, test_data, user_num, item_num = prepare_data("./ml-1m")
-    train_data.save_pickle("train_xshards")
-    test_data.save_pickle("test_xshards")
-    stop_orca_context()
+
+def optimizer_creator(model, config):
+    return optim.Adam(model.parameters(), lr=config['lr'])
+
+loss = nn.BCEWithLogitsLoss()
+
+
+# Step 4: Distributed training with Orca PyTorch Estimator
+backend = "spark"  # "ray" or "spark"
+
+est = Estimator.from_torch(model=model_creator,
+                           optimizer=optimizer_creator,
+                           loss=loss,
+                           metrics=[Accuracy(), Precision(), Recall()],
+                           backend=backend,
+                           config={'dataset_dir': dataset_dir,
+                                   'factor_num': 16,
+                                   'num_layers': 3,
+                                   'dropout': 0.0,
+                                   'lr': 0.001,
+                                   'model': "NeuMF-end",
+                                   'cat_feats_dim': 6,
+                                   'numeric_feats_dim': 1})
+est.fit(data=train_data, epochs=10,
+        feature_cols=feature_cols,
+        label_cols=label_cols,
+        batch_size=1024)
+
+
+# Step 5: Distributed evaluation of the trained model
+result = est.evaluate(data=test_data,
+                      feature_cols=feature_cols,
+                      label_cols=label_cols,
+                      batch_size=1024)
+print('Evaluation results:')
+for r in result:
+    print(r, ":", result[r])
+
+
+# Step 6: Save the trained PyTorch model
+est.save("NCF_model")
+
+
+# Step 7: Stop Orca Context when program finishes
+stop_orca_context()
