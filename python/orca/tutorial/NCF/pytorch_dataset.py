827b4d34412151227d771b72a2857b68522b954d
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2022-11-14 10:26:47 +0800

    Update pytorch_dataset.py

--- python/orca/tutorial/NCF/pytorch_dataset.py
+++ python/orca/tutorial/NCF/pytorch_dataset.py
@@ -26,66 +26,87 @@ import torch.utils.data as data
 
 
 class NCFData(data.Dataset):
-    def __init__(self, features,
-                 num_item=0, train_mat=None, num_ng=0):
-        super(NCFData, self).__init__()
-        """ Note that the labels are only useful when training, we thus
-            add them in the ng_sample() function.
-        """
-        self.features_ps = features
-        self.num_item = num_item
-        self.train_mat = train_mat
-        self.num_ng = num_ng
-        self.is_sampling = False
-        self.labels = [1 for _ in range(len(features))]
-
-    def ng_sample(self):
-        self.is_sampling = True
-        self.features_ng = []
-        for x in self.features_ps:
-            u = x[0]
-            for t in range(self.num_ng):
-                j = np.random.randint(self.num_item)
-                while (u, j) in self.train_mat:
-                    j = np.random.randint(self.num_item)
-                self.features_ng.append([u, j])
-
-        labels_ps = [1 for _ in range(len(self.features_ps))]
-        labels_ng = [0 for _ in range(len(self.features_ng))]
-
-        self.features_fill = self.features_ps + self.features_ng
-        self.labels_fill = labels_ps + labels_ng
+    def __init__(self, data):
+        self.data = list(map(lambda row: list(row[1:]), data.itertuples()))
 
     def __len__(self):
-        return (self.num_ng + 1) * len(self.labels)
+        return len(self.data)
 
     def __getitem__(self, idx):
-        features = self.features_fill if self.is_sampling \
-            else self.features_ps
-        labels = self.labels_fill if self.is_sampling \
-            else self.labels
-
-        user = features[idx][0]
-        item = features[idx][1]
-        label = float(labels[idx])
-        return user, item, label
+        return tuple(self.data[idx])
 
 
-def load_dataset(dataset_dir):
-    data_X = pd.read_csv(
+def load_dataset(dataset_dir, num_ng=4, total_cols=['user', 'item', "label"],
+                 cal_num_embed=False):
+    """
+    dataset_dir: the path of the datasets;
+    num_ng: number of negative samples to be sampled here;
+    total_cols: the list of total features;
+    cal_num_embed: if True, will calculate num_embed_cat_feats.
+    """
+    data_1 = pd.read_csv(
+        os.path.join(dataset_dir, 'users.dat'),
+        sep="::", header=None, names=['user', 'gender', 'age', 'occupation', 'zipcode'],
+        usecols=[0, 1, 2, 3, 4],
+        dtype={0: np.int64, 1: np.object, 2: np.int64, 3: np.int64, 4: np.object})
+    data_2 = pd.read_csv(
         os.path.join(dataset_dir, 'ratings.dat'),
         sep="::", header=None, names=['user', 'item'],
         usecols=[0, 1], dtype={0: np.int64, 1: np.int64})
+    data_3 = pd.read_csv(
+        os.path.join(dataset_dir, 'movies.dat'),
+        sep="::", header=None, names=['item', 'category'],
+        usecols=[0, 2], dtype={0: np.int64, 1: np.object})
 
-    user_num = data_X['user'].max() + 1
-    item_num = data_X['item'].max() + 1
+    user_num = data_2['user'].max() + 1
+    item_num = data_2['item'].max() + 1
 
-    # load ratings as a dok matrix
-    train_mat = sp.dok_matrix((user_num, item_num), dtype=np.int64)
-    for x in data_X.values.tolist():
-        train_mat[x[0], x[1]] = 1
+    # category encoding
+    data_1.gender, _ = pd.Series(data_1.gender).factorize()
+    data_1.zipcode, _ = pd.Series(data_1.zipcode).factorize()
+    data_3.category, _ = pd.Series(data_3.category).factorize()
 
-    return data_X, user_num, item_num, train_mat
+    # sample negative items for training datasets
+    if num_ng > 0:
+        # load ratings as a dok matrix
+        features_ps = data_2.values.tolist()
+        train_mat = sp.dok_matrix((user_num, item_num), dtype=np.int64)
+        for x in features_ps:
+            train_mat[x[0], x[1]] = 1
+
+        features_ng = []
+        for x in features_ps:
+            u = x[0]
+            for t in range(num_ng):
+                j = np.random.randint(item_num)
+                while (u, j) in train_mat:
+                    j = np.random.randint(item_num)
+                features_ng.append([u, j])
+        features = features_ps + features_ng
+        labels_ps = [1 for _ in range(len(features_ps))]
+        labels_ng = [0 for _ in range(len(features_ng))]
+        labels = labels_ps + labels_ng
+        data_2 = pd.DataFrame(features, columns=["user", "item"], dtype=np.int64)
+        data_2["label"] = labels
+    else:
+        data_2["label"] = [1 for _ in range(len(data_2))]
+
+    # merge dataframes
+    data_X = data_1.merge(data_2, on='user')
+    data_X = data_X.merge(data_3, on='item')
+    data_X = data_X.loc[:, total_cols]
+
+    # Calculate num_embeddings for each categorical features
+    num_embed_cat_feats = []
+    if cal_num_embed:
+        num_embed_cat_feats.append(user_num)
+        num_embed_cat_feats.append(item_num)
+        num_embed_cat_feats.append(data_1['gender'].max()+1)
+        num_embed_cat_feats.append(data_1['occupation'].max()+1)
+        num_embed_cat_feats.append(data_1['zipcode'].max()+1)
+        num_embed_cat_feats.append(data_3['category'].max()+1)
+
+    return data_X, num_embed_cat_feats
 
 
 if __name__ == "__main__":
