470d84cd9b0329fa1b0f0a24ef9831ab03dcffd9
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2022-11-16 15:05:25 +0800

    Update process_xshards.py

--- python/orca/tutorial/NCF/process_xshards.py
+++ python/orca/tutorial/NCF/process_xshards.py
@@ -12,87 +12,122 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# ==============================================================================
+# Some of the code is adapted from
+# https://github.com/guoyang9/NCF/blob/master/data_utils.py
 #
 
-# Step 0: Import necessary libraries
-import torch.nn as nn
-import torch.optim as optim
+import os
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+from sklearn.model_selection import train_test_split
 
-from process_xshards import prepare_data
-from pytorch_model import NCF
-
-from bigdl.orca import init_orca_context, stop_orca_context
-from bigdl.orca.learn.pytorch import Estimator
-from bigdl.orca.learn.metrics import Accuracy, Precision, Recall
+from bigdl.orca.data.pandas import read_csv
+from bigdl.orca.data.transformer import StringIndexer
 
 
-# Step 1: Init Orca Context
-sc = init_orca_context()
+def ng_sampling(data, user_num, item_num, num_ng):
+    clms = ['user', 'item']
+    data = data.loc[:, clms]
+    data_values = data.values.tolist()
+
+    # calculate a dok matrix
+    train_mat = sp.dok_matrix((user_num, item_num), dtype=np.int64)
+    for row in data_values:
+        train_mat[row[0], row[1]] = 1
+
+    # negative sampling
+    features_ps = data_values
+    features_ng = []
+    for x in features_ps:
+        u = x[0]
+        for t in range(num_ng):
+            j = np.random.randint(item_num)
+            while (u, j) in train_mat:
+                j = np.random.randint(item_num)
+            features_ng.append([u, j])
+
+    labels_ps = [1 for _ in range(len(features_ps))]
+    labels_ng = [0 for _ in range(len(features_ng))]
+
+    features_fill = features_ps + features_ng
+    labels_fill = labels_ps + labels_ng
+    data_XY = pd.DataFrame(data=features_fill, columns=clms, dtype=np.int64)
+    data_XY["label"] = labels_fill
+    data_XY["label"] = data_XY["label"].astype(np.float)
+    return data_XY
 
 
-# Step 2: Define train and test datasets using Orca XShards
-dataset_dir = "./ml-1m"
-train_data, test_data, user_num, item_num, cat_feats_dims, \
-    feature_cols, label_cols = prepare_data(dataset_dir, num_ng=4)
+def split_dataset(data):
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=100)
+    return train_data, test_data
 
 
-# Step 3: Define the model, optimizer and loss
-def model_creator(config):
-    model = NCF(user_num=config['user_num'],
-                item_num=config['item_num'],
-                factor_num=config['factor_num'],
-                num_layers=config['num_layers'],
-                dropout=config['dropout'],
-                model=config['model'],
-                cat_feats_dims=config['cat_feats_dims'],
-                num_numeric_feats=config['num_numeric_feats'])
-    model.train()
-    return model
+def prepare_data(dataset_dir, num_ng=4):
+    # Load the train and test datasets
+    feature_cols = ['user', 'item',
+                    'gender', 'occupation', 'zipcode', 'category',  # categorical feature
+                    'age']  # numerical feature
+    label_cols = ["label"]
+
+    users = read_csv(
+        os.path.join(dataset_dir, 'users.dat'),
+        sep="::", header=None, names=['user', 'gender', 'age', 'occupation', 'zipcode'],
+        usecols=[0, 1, 2, 3, 4])
+    ratings = read_csv(
+        os.path.join(dataset_dir, 'ratings.dat'),
+        sep="::", header=None, names=['user', 'item'],
+        usecols=[0, 1])
+    movies = read_csv(
+        os.path.join(dataset_dir, 'movies.dat'),
+        sep="::", header=None, names=['item', 'category'],
+        usecols=[0, 2])
+
+    # calculate numbers of user and item
+    user_set = set(users["user"].unique())
+    item_set = set(movies["item"].unique())
+    user_num = max(user_set) + 1
+    item_num = max(item_set) + 1
+
+    # Negative sampling
+    ratings = ratings.partition_by("user", ratings.num_partitions())
+    ratings = ratings.transform_shard(lambda shard: ng_sampling(shard, user_num, item_num, num_ng))
+
+    # Merge XShards
+    data = users.merge(ratings, on='user')
+    data = data.merge(movies, on='item')
+
+    # Category encoding
+    indexer = StringIndexer('gender')
+    data = indexer.fit_transform(data)
+    indexer.setInputCol('zipcode')
+    data = indexer.fit_transform(data)
+    indexer.setInputCol('category')
+    data = indexer.fit_transform(data)
+
+    # Calculate input_dims for each categorical features
+    cat_feats_dims = []
+    cat_feat_set = set(data["gender"].unique())
+    cat_feats_dims.append(max(cat_feat_set)+1)
+    cat_feat_set = set(data["occupation"].unique())
+    cat_feats_dims.append(max(cat_feat_set)+1)
+    cat_feat_set = set(data["zipcode"].unique())
+    cat_feats_dims.append(max(cat_feat_set)+1)
+    cat_feat_set = set(data["category"].unique())
+    cat_feats_dims.append(max(cat_feat_set)+1)
+
+    # Split dataset
+    train_data, test_data = data.transform_shard(split_dataset).split()
+    return train_data, test_data, user_num, item_num, cat_feats_dims, feature_cols, label_cols
 
 
-def optimizer_creator(model, config):
-    return optim.Adam(model.parameters(), lr=config['lr'])
+if __name__ == "__main__":
+    from bigdl.orca import init_orca_context, stop_orca_context
 
-loss = nn.BCEWithLogitsLoss()
-
-
-# Step 4: Distributed training with Orca PyTorch Estimator
-backend = "spark"  # "ray" or "spark"
-
-est = Estimator.from_torch(model=model_creator,
-                           optimizer=optimizer_creator,
-                           loss=loss,
-                           metrics=[Accuracy(), Precision(), Recall()],
-                           backend=backend,
-                           config={'dataset_dir': dataset_dir,
-                                   'user_num': user_num,
-                                   'item_num': item_num,
-                                   'factor_num': 16,
-                                   'num_layers': 3,
-                                   'dropout': 0.0,
-                                   'lr': 0.001,
-                                   'model': "NeuMF-end",
-                                   'cat_feats_dims': cat_feats_dims,
-                                   'num_numeric_feats': 1})
-est.fit(data=train_data, epochs=10,
-        feature_cols=feature_cols,
-        label_cols=label_cols,
-        batch_size=1024)
-
-
-# Step 5: Distributed evaluation of the trained model
-result = est.evaluate(data=test_data,
-                      feature_cols=feature_cols,
-                      label_cols=label_cols,
-                      batch_size=1024)
-print('Evaluation results:')
-for r in result:
-    print(r, ":", result[r])
-
-
-# Step 6: Save the trained PyTorch model
-est.save("NCF_model")
-
-
-# Step 7: Stop Orca Context when program finishes
-stop_orca_context()
+    sc = init_orca_context()
+    train_data, test_data, user_num, item_num, cat_feats_dims, \
+        feature_cols, label_cols = prepare_data("./ml-1m")
+    train_data.save_pickle("train_xshards")
+    test_data.save_pickle("test_xshards")
+    stop_orca_context()
