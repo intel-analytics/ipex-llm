4116537b8f486478bc6ed616a808c37dbdf03ac8
ForJadeForest <1113480910@qq.com>
2022-11-17 14:17:09 +0800

    review

--- python/orca/tutorial/NCF/tf_train_spark_dataframe.py
+++ python/orca/tutorial/NCF/tf_train_spark_dataframe.py
@@ -13,21 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-#
-# Copyright 2016 The BigDL Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
 # Step 0: Import necessary libraries
 import math
@@ -52,7 +37,7 @@ df, embedding_in_dim = generate_neg_sample(df, embedding_in_dim)
 item_num = embedding_in_dim['item'] + 1
 user_num = embedding_in_dim['user'] + 1
 
-# Step 3: Define the train hyperparameters, the ncf model
+# Step 3: Define the ncf model
 config = dict(
     embedding_size=16,
     lr=1e-3,
