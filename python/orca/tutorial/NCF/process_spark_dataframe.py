a37a73517255bd2e32a0235493389d09a8b567a9
ForJadeForest <1113480910@qq.com>
2022-11-17 10:47:53 +0800

    add tensorflow ncf train and data process using spark

--- python/orca/tutorial/NCF/process_spark_dataframe.py
+++ python/orca/tutorial/NCF/process_spark_dataframe.py
@@ -13,3 +13,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os.path
+
+import tensorflow as tf
+from pyspark.ml.feature import StringIndexer
+from pyspark.sql.types import StructField, StructType, IntegerType, ArrayType, StringType
+from pyspark.sql.functions import udf, lit, collect_list, explode
+
+
+def read_data(data_dir, spark):
+    schema = StructType([StructField('user', IntegerType(), False),
+                         StructField('item', IntegerType(), False)])
+    # Need spark3 to support delimiter with more than one character.
+    df = spark.read.csv(os.path.join(data_dir, "ratings.dat"), sep="::", schema=schema, header=False)
+    df = df.withColumn('label', lit(1.0))
+    return df
+
+
+def generate_neg_sample(df):
+    # cat_feature = user_feature + item_feature
+    embedding_in_dim = {}
+    for i, c, in enumerate(['user', 'item']):
+        print(f'[INFO] ==> begin calculate {c} embedding_in_dim')
+        embedding_in_dim[c] = df.agg({c: "max"}).collect()[0][f"max({c})"]
+    print(embedding_in_dim)
+
+    def neg_sample(x):
+        import random
+        neg_scale = 4
+        neg_res = []
+        for _ in x:
+            for i in range(neg_scale):
+                neg_item_index = random.randint(1, embedding_in_dim['item'])
+                while neg_item_index in x:
+                    neg_item_index = random.randint(1, embedding_in_dim['item'])
+                neg_res.append(neg_item_index)
+        return neg_res
+
+    neg_sample_udf = udf(neg_sample, ArrayType(IntegerType(), False))
+
+    df_neg = df.groupBy('user').agg(neg_sample_udf(collect_list('item')).alias('item_list'))
+    df_neg = df_neg.select(df_neg.user, explode(df_neg.item_list))
+    df_neg = df_neg.withColumn('label', lit(0.0))
+    df_neg = df_neg.withColumnRenamed('col', 'item')
+    df = df.unionByName(df_neg)
+    df = df.repartition(df.rdd.getNumPartitions())
+
+    return df, embedding_in_dim
+
+def split_dataset(df):
+    train_df, val_df = df.randomSplit([0.8, 0.2], 100)
+    train_size = train_df.count()
+    val_size = val_df.count()
+    return train_df, val_df, train_size, val_size
--- python/orca/tutorial/NCF/tf_train_spark_dataframe.py
+++ python/orca/tutorial/NCF/tf_train_spark_dataframe.py
@@ -13,3 +13,93 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Step 0: Import necessary libraries
+import math
+
+from bigdl.orca import init_orca_context, stop_orca_context, OrcaContext
+from bigdl.orca.learn.tf2 import Estimator
+
+from process_spark_dataframe import read_data, generate_neg_sample, split_dataset
+from tf_model import ncf_model
+
+if __name__ == '__main__':
+    # Step 1: Init Orca Context
+    init_orca_context(memory='4g')
+    spark = OrcaContext.get_spark_session()
+
+    # Step 2: read and process data using Spark DataFrame
+    data_dir = './ml-1m'  # path to ml-1m
+    df = read_data(data_dir, spark)
+    df, embedding_in_dim = generate_neg_sample(df)
+    item_num = embedding_in_dim['item'] + 1
+    user_num = embedding_in_dim['user'] + 1
+
+    # Step 3: Define the train hyperparameters, the ncf model
+    config = dict(
+        embedding_size=16,
+        lr=1e-3,
+        item_num=item_num,
+        user_num=user_num,
+        dropout=0.5,
+    )
+    epochs = 2
+    batch_size = 1024
+
+
+    def model_creator(config):
+        model = ncf_model(embedding_size=config['embedding_size'],
+                          user_num=config['user_num'],
+                          item_num=config['item_num'],
+                          dropout=config['dropout'],
+                          lr=config['lr'])
+        return model
+
+
+    train_df, val_df, train_size, val_size = split_dataset(df)
+    steps_per_epoch = math.ceil(train_size / batch_size)
+    val_steps = math.ceil(val_size / batch_size)
+
+    # Step 4: Distributed training with Orca keras Estimator
+    backend = 'spark'  # 'ray' of 'spark'
+    est = Estimator.from_keras(model_creator=model_creator,
+                               config=config,
+                               backend=backend)
+
+    est.fit(train_df,
+            epochs=epochs,
+            batch_size=batch_size,
+            feature_cols=['user', 'item'],
+            label_cols=['label'],
+            steps_per_epoch=steps_per_epoch,
+            validation_data=val_df,
+            validation_steps=val_steps)
+
+    # Step 5: Distributed evaluation of the trained model
+    stats = est.evaluate(val_df,
+                         feature_cols=['user', 'item'],
+                         label_cols=['label'],
+                         batch_size=batch_size,
+                         num_steps=val_steps)
+    print("Evaluation results:", stats)
+
+    # Step 6: Save the trained PyTorch model
+    est.save("NCF_model")
+
+    # Step 7: Stop Orca Context when program finishes
+    stop_orca_context()
