fe0c4f9f0d79a3c4a749667355d9c2c0c9a0206e
liangs6212 <liangx.shen@intel.com>
2022-11-25 23:31:02 +0800

    try fix

--- docs/readthedocs/source/_static/js/chronos_installation_guide.js
+++ docs/readthedocs/source/_static/js/chronos_installation_guide.js
@@ -71,6 +71,14 @@ function refresh_cmd(){
         enable(ais);
     }
 
+    if(version!="nightly"){
+        disable(inferences);
+    }
+    else{
+        enable(inferences);
+    }
+
+
     //disable other buttons in cases
     if(package=="docker"){
         disable(functionalities);
@@ -91,13 +99,6 @@ function refresh_cmd(){
         enable(packages);
         enable(hardwares);
         enable(automls);
-
-    if(version=="stable"){
-        disable(inferences);
-    }
-    else{
-        enable(inferences);
-    }
 
         //change cmd according to different choices
         if(model=="Deep_learning_models"){
