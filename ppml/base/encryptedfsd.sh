718aadd5122afbcf61a66800c9bf0df7a031a3b9
gc-fu <guancheng.fu@intel.com>
2022-11-29 15:32:20 +0800

    Fix errors in encryptedfsd

--- ppml/base/encryptedfsd.sh
+++ ppml/base/encryptedfsd.sh
@@ -4,9 +4,9 @@ set -x
 
 # Check if the user set EHSM_URL or not
 if [ -z "$EHSM_URL" ]; then
-	echo "[ERROR] Distributed encrypted filesystem is enabled, but EHSM_URL is empty!"
-	echo "[INFO] PPML Application Exit!"
-	exit 1
+    echo "[ERROR] Distributed encrypted filesystem is enabled, but EHSM_URL is empty!"
+    echo "[INFO] PPML Application Exit!"
+    exit 1
 fi
 
 if [ -z "$APP_ID" ]; then
@@ -22,22 +22,22 @@ if [ -z "$API_KEY" ]; then
 fi
 
 if [ -z "$EHSM_PORT" ]; then
-	echo "[INFO] Distributed encrypted filesystem is enabled, but EHSM_PORT is empty!"
-	echo "[INFO] Assume EHSM_PORT is 3000"
-	EHSM_PORT=3000
+    echo "[INFO] Distributed encrypted filesystem is enabled, but EHSM_PORT is empty!"
+    echo "[INFO] Assume EHSM_PORT is 3000"
+    export EHSM_PORT=3000
 fi
 
 # We need to check if encrypted_primary_key and encrypted_data_key exists or not
 if [ -z "$EHSM_PRIMARY_KEY" ]; then
-	echo "[INFO] Distributed encrypted filesystem is enabled, but EHSM_PRIMARY_KEY is empty!"
-	echo "[INFO] Assume /ppml/encrypted_keys/encrypted_primary_key"
-	EHSM_PRIMARY_KEY=/ppml/encrypted_keys/encrypted_primary_key
+    echo "[INFO] Distributed encrypted filesystem is enabled, but EHSM_PRIMARY_KEY is empty!"
+    echo "[INFO] Assume /ppml/encrypted_keys/encrypted_primary_key"
+    export EHSM_PRIMARY_KEY=/ppml/encrypted_keys/encrypted_primary_key
 fi
 
 if [ -z "$EHSM_DATA_KEY" ]; then
-	echo "[INFO] Distributed encrypted filesystem is enabled, but EHSM_DATA_KEY is empty!"
-	echo "[INFO] Assume /ppml/encrypted_keys/encrypted_data_key"
-	EHSM_DATA_KEY=/ppml/encrypted_keys/encrypted_data_key
+    echo "[INFO] Distributed encrypted filesystem is enabled, but EHSM_DATA_KEY is empty!"
+    echo "[INFO] Assume /ppml/encrypted_keys/encrypted_data_key"
+    export EHSM_DATA_KEY=/ppml/encrypted_keys/encrypted_data_key
 fi
 
 
