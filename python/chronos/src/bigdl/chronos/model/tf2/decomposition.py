a02e4bf7e51df37a1ef31f14b7ca3769456a24cb
WangBin <32730386+devWangBin@users.noreply.github.com>
2022-11-29 18:49:51 +0800

    Update decomposition.py

--- python/chronos/src/bigdl/chronos/model/tf2/decomposition.py
+++ python/chronos/src/bigdl/chronos/model/tf2/decomposition.py
@@ -14,11 +14,12 @@
 # limitations under the License.
 #
 
-import torch.nn as nn
-import torch
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+from bigdl.nano.tf.keras import Model
 
 
-class moving_avg(nn.Module):
+class moving_avg(Layer):
     """
     Moving average block to highlight the trend of time series
     """
@@ -26,19 +27,21 @@ class moving_avg(nn.Module):
     def __init__(self, kernel_size, stride):
         super(moving_avg, self).__init__()
         self.kernel_size = kernel_size
-        self.avg = nn.AvgPool1d(kernel_size=kernel_size, stride=stride, padding=0)
+        self.avg = tf.keras.layers.AveragePooling1D(pool_size=kernel_size, strides=stride,
+                                                    padding='valid',
+                                                    data_format='channels_first')
 
-    def forward(self, x):
+    def call(self, x):
         # padding on the both ends of time series
         front = x[:, 0:1, :].repeat(1, (self.kernel_size - 1) // 2, 1)
         end = x[:, -1:, :].repeat(1, self.kernel_size // 2, 1)
-        x = torch.cat([front, x, end], dim=1)
+        x = tf.concat([front, x, end], dim=1)
         x = self.avg(x.permute(0, 2, 1))
         x = x.permute(0, 2, 1)
         return x
 
 
-class series_decomp(nn.Module):
+class series_decomp(Layer):
     """
     Series decomposition block
     """
@@ -47,13 +50,13 @@ class series_decomp(nn.Module):
         super(series_decomp, self).__init__()
         self.moving_avg = moving_avg(kernel_size, stride=1)
 
-    def forward(self, x):
+    def call(self, x):
         moving_mean = self.moving_avg(x)
         res = x - moving_mean
         return res, moving_mean
 
 
-class DecompositionTSModel(nn.Module):
+class DecompositionTSModel(Model):
 
     def __init__(self, models, kernel_size=25):
         """
@@ -67,7 +70,7 @@ class DecompositionTSModel(nn.Module):
         self.linear_seasonal = models[0]
         self.linear_trend = models[1]
 
-    def forward(self, x):
+    def call(self, x):
         seasonal_init, trend_init = self.decompsition(x)
         seasonal_output = self.linear_seasonal(seasonal_init)
         trend_output = self.linear_trend(trend_init)
