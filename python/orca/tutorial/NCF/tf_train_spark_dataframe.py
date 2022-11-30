24ea4a34fd2c9495d2035a0d9700a81541d50b9d
YingzhePeng <78460824+ForJadeForest@users.noreply.github.com>
2022-11-30 15:31:05 +0800

    change NCF num_dense_feature type (#6813)
    * return num_dense_feature from data_prepare
    
    * change the num_dense_feats to int.
    
    * update
    
    * update
    
    * update
    
    * update

--- python/orca/tutorial/NCF/tf_train_spark_dataframe.py
+++ python/orca/tutorial/NCF/tf_train_spark_dataframe.py
@@ -29,8 +29,8 @@ init_orca_context(memory='4g')
 # Step 2: Read and process data using Spark DataFrame
 data_dir = './ml-1m'  # path to ml-1m
 
-train_df, val_df, sparse_feats_input_dims, user_num, item_num, feature_cols, label_cols = \
-    prepare_data(data_dir, neg_scale=4)
+train_df, test_df, user_num, item_num, sparse_feats_input_dims, num_dense_feats, \
+    feature_cols, label_cols = prepare_data(data_dir, neg_scale=4)
 
 # Step 3: Define the NCF model
 config = dict(
@@ -40,7 +40,7 @@ config = dict(
     user_num=user_num,
     dropout=0.5,
     sparse_feats_input_dims=sparse_feats_input_dims,
-    num_dense_feats=['age'],
+    num_dense_feats=num_dense_feats,
     sparse_feats_embed_dims=8,
     num_layers=3
 )
@@ -67,7 +67,7 @@ est = Estimator.from_keras(model_creator=model_creator,
 
 batch_size = 256
 train_steps = math.ceil(train_df.count() / batch_size)
-val_steps = math.ceil(val_df.count() / batch_size)
+val_steps = math.ceil(test_df.count() / batch_size)
 
 est.fit(train_df,
         epochs=10,
@@ -77,7 +77,7 @@ est.fit(train_df,
         steps_per_epoch=train_steps)
 
 # Step 5: Distributed evaluation of the trained model
-result = est.evaluate(val_df,
+result = est.evaluate(test_df,
                       feature_cols=feature_cols,
                       label_cols=label_cols,
                       batch_size=batch_size,
