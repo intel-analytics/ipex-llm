8a61e4d23a2cafd4cfa233f7f7ba6bb083259f2c
TheaperDeng <theaperdeng@outlook.com>
2022-11-30 10:17:10 +0800

    fix bug

--- python/nano/src/bigdl/nano/pytorch/utils.py
+++ python/nano/src/bigdl/nano/pytorch/utils.py
@@ -158,7 +158,7 @@ def load_model(path, model: pl.LightningModule = None):
         model = copy.deepcopy(model)
         checkpoint_path = metadata.get('checkpoint', None)
         thread_num = None
-        if metadata["thread_num"] is not None:
+        if "thread_num" in metadata and metadata["thread_num"] is not None:
             thread_num = int(metadata["thread_num"])
         if checkpoint_path:
             checkpoint_path = path / metadata['checkpoint']
