93707797cf6fe415395db25448c018fa202f9130
Jin Hanyu <476099001@qq.com>
2022-12-08 03:42:39 +0800

    Add torchserve to Gramine-python-toolkit image.

---
+++ ppml/trusted-python-toolkit/start-scripts/start-python-frontend-sgx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+configFile=""
+while getopts "c:" opt
+do
+    case $opt in
+        c)
+            configFile=$OPTARG
+        ;;
+    esac
+done
+
+cd /ppml
+export sgx_command="/opt/jdk11/bin/java \
+        -Dmodel_server_home=/usr/local/lib/python3.7/dist-packages \
+        -cp .:/ppml/work/data/pytorch-serve/frontend/server/build/libs/* \
+        -Xmx30g \
+        -Xms30g \
+        org.pytorch.serve.ModelServer \
+        --python /usr/bin/python3 \
+        -f /ppml/work/data/$configFile \
+        -ncs"
+gramine-sgx bash 2>&1 | tee frontend.log
+
---
+++ ppml/trusted-python-toolkit/start-scripts/start-torchserve-sgx.sh
@@ -0,0 +1,31 @@
+configFile=""
+while getopts "c:" opt
+do
+    case $opt in
+        c)
+            configFile=$OPTARG
+        ;;
+    esac
+done
+cd /ppml/work/data
+cat $configFile | while read line
+do
+        if [[ $line =~ "minWorkers" ]]
+        then
+                line=${line#*\"minWorkers\": }
+                line=${line%%,*}
+
+                port=9000
+                for ((i=0;i<line;i++,port++))
+                do
+                (
+                        bash /ppml/work/start-scripts/start-python-backend-sgx.sh -p $port
+                )&
+                done
+        fi
+done
+(
+        bash /ppml/work/start-scripts/start-python-frontend-sgx.sh -c $configFile
+)&
+wait
+
