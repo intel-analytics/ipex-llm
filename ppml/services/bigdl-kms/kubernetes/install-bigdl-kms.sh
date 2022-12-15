5fafeeb957cbcadab6b29e15952955d82d5a49e8
Uxito-Ada <414416158@qq.com>
2022-12-15 11:06:07 +0800

    develop according to test

--- ppml/services/bigdl-kms/kubernetes/install-bigdl-kms.sh
+++ ppml/services/bigdl-kms/kubernetes/install-bigdl-kms.sh
@@ -2,8 +2,8 @@
 export nfsServerIp=your_nfs_server_ip
 export nfsPath=a_nfs_shared_folder_path_on_the_server
 export mysqlImage=mysql:8
-export keywhizServerImage=intelanalytics/keywhiz
-export bigdlKmsFrontendImage=intelanalytics/keywhiz
+export keywhizServerImage=intelanalytics/keywhiz:2.2.0-SNAPSHOT
+export bigdlKmsFrontendImage=intelanalytics/keywhiz:2.2.0-SNAPSHOT
 export kmsIP=your_kms_ip_to_use_as
 
 # Create k8s namespace and apply BigDL-KMS
