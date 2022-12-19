bf56c94509826e7e92b30db4bfa9624c0d5ab185
Uxito-Ada <414416158@qq.com>
2022-12-19 14:28:42 +0800

    frontend

---
+++ ppml/services/bigdl-kms/docker/build-docker-image.sh
@@ -0,0 +1,16 @@
+export HTTP_PROXY_HOST=your_http_proxy_host
+export HTTP_PROXY_PORT=your_http_proxy_port
+export HTTPS_PROXY_HOST=your_https_proxy_host
+export HTTPS_PROXY_PORT=your_https_proxy_port
+export JDK_URL=http://your-http-url-to-download-jdk
+
+sudo docker build \
+    --build-arg http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT \
+    --build-arg https_proxy=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT \
+    --build-arg HTTP_PROXY_HOST=$HTTP_PROXY_HOST \
+    --build-arg HTTP_PROXY_PORT=$HTTP_PROXY_PORT \
+    --build-arg HTTPS_PROXY_HOST=$HTTPS_PROXY_HOST \
+    --build-arg HTTPS_PROXY_PORT=$HTTPS_PROXY_PORT \
+    --build-arg JDK_VERSION=8u192 \
+    --build-arg JDK_URL=$JDK_URL \
+    -t intelanalytics/bigdl-keywhiz:2.2.0-SNAPSHOT -f ./Dockerfile .
---
+++ ppml/services/bigdl-kms/docker/start-https-frontend.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+set -x
+
+echo "### Launching BigDL KMS HTTPS Frontend ###"
+
+keywhiz_port=$KEYWHIZ_PORT
+xmx_size=$XMX_SIZE
+core_num=$CORE_NUM
+https_key_store_path=/usr/app/server/src/main/resources/dev_and_test_keystore.p12
+https_secure_password=$HTTPS_SECURE_PASSWORD # k8s secret
+
+/opt/jdk8/bin/java \
+    -Xms2g \
+    -Xmx${xmx_size} \
+    -XX:ActiveProcessorCount=${core_num} \
+    -Dcom.intel.analytics.zoo.shaded.io.netty.tryReflectionSetAccessible=true \
+    -Dakka.http.host-connection-pool.max-connections=100 \
+    -Dakka.http.host-connection-pool.max-open-requests=128 \
+    -Dakka.actor.default-dispatcher.fork-join-executor.parallelism-min=100 \
+    -Dakka.actor.default-dispatcher.fork-join-executor.parallelism-max=120 \
+    -Dakka.actor.default-dispatcher.fork-join-executor.parallelism-factor=1 \
+    -jar /opt/bigdl-serving-http.jar \
+    --keywhizHost "keywhiz-service" \
+    --tokensPerSecond 30 \
+    --tokenBucketEnabled true \
+    --parallelism 30 \
+    --httpsEnabled true \
+    --httpsKeyStorePath "${https_key_store_path}" \
+    --httpsKeyStoreToken "${https_secure_password}" | tee ./https-frontend.log
