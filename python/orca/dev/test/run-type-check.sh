60c14d43bfd406b1da416a32ec3168b7d747efda
ZhouJian <jian.zhou@intel.com>
2022-12-20 13:37:11 +0800

    update

--- python/orca/dev/test/run-type-check.sh
+++ python/orca/dev/test/run-type-check.sh
@@ -20,5 +20,4 @@ mypy --install-types --non-interactive --config-file ${ORCA_DEVTEST_DIR}/mypy.in
                                                      $ORCA_HOME/src/bigdl/orca/learn/pytorch/pytorch_ray_estimator.py \
                                                      $ORCA_HOME/src/bigdl/orca/learn/pytorch/estimator.py \
                                                      $ORCA_HOME/src/bigdl/orca/learn/pytorch/core/base_ray_estimator.py \
-                                                     $ORCA_HOME/src/bigdl/orca/learn/pytorch/experimential/mmcv/mmcv_ray_estimator.py \
-                                                     $ORCA_HOME/src/bigdl/orca/learn/pytorch/experimential/mmcv/mmcv_ray_runner.py
+                                                     $ORCA_HOME/src/bigdl/orca/learn/pytorch/experimential/mmcv
