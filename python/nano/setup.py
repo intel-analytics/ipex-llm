cb8f5178c8c3f92a1824238a4a807890758c5906
Yishuo Wang <yishuo.wang@intel.com>
2022-12-26 14:52:51 +0800

    upgrade openvino

--- python/nano/setup.py
+++ python/nano/setup.py
@@ -151,7 +151,7 @@ def setup_package():
                           "onnxruntime-extensions==0.4.2; platform_system!='Darwin'",
                           "onnxruntime-extensions==0.3.1; platform_machine=='x86_64' and \
                           platform_system=='Darwin'",
-                          "openvino-dev==2022.2.0",
+                          "openvino-dev==2022.3.0",
                           "neural-compressor==1.13.1",
                           "onnxsim==0.4.8; platform_system!='Darwin'",
                           "onnxsim==0.4.1; platform_machine=='x86_64' and \
