a4a6378fa874bd9739b8b916cb09624293c08e16
gc-fu <guancheng.fu@intel.com>
2022-12-27 16:23:32 +0800

    test

--- python/ppml/src/bigdl/ppml/encryption/datasets/hf/load_test.py
+++ python/ppml/src/bigdl/ppml/encryption/datasets/hf/load_test.py
@@ -1,9 +1,51 @@
 # simple test, delete later
+import os
 from bigdl.ppml.encryption.datasets.hf.load import load_from_disk
+from cryptography.fernet import Fernet
+import random
+import base64
+
+def _create_random(length) -> str:
+    random.seed(0)
+    ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    chars = []
+    for i in range(length):
+        chars.append(random.choice(ALPHABET))
+    key = "".join(chars)
+    key_bytes = key.encode("ascii")
+    base64_str = base64.b64encode(key_bytes)
+    print(len(base64_str), flush=True)
+    return base64_str
+
+def read_data_file(data_file_path):
+    with open(data_file_path, 'rb') as file:
+        original = file.read()
+    return original
+
+def write_data_file(data_file_path, content):
+    with open(data_file_path, 'wb') as file:
+        file.write(content)
+
+def encrypt_directory_automation(input_dir, save_dir):
+    print('[INFO] Encrypt Files Start...')
+    if save_dir is None:
+        if input_dir[-1] == '/':
+            input_dir = input_dir[:-1]
+        save_dir = input_dir + '.encrypted'
+    if not os.path.isdir(save_dir):
+        os.mkdir(save_dir)
+    fernet = Fernet(_create_random(32))
+    for file_name in os.listdir(input_dir):
+        input_path = os.path.join(input_dir, file_name)
+        encrypted = fernet.encrypt(read_data_file(input_path))
+        save_path = os.path.join(save_dir, file_name + '.encrypted')
+        write_data_file(save_path, encrypted)
+        print('[INFO] Encrypt Successfully! Encrypted Output Is ' + save_path)
+    print('[INFO] Encrypted Files.')
 
 
-
-
+def encrypt():
+    encrypt_directory_automation("/ppml/save-datasets/", "/ppml/save-datasets-encrypted/")
 
 
 def main():
@@ -13,4 +55,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    encrypt()
