7846f064b2a310c214b18a7ff9a8ba8755191dde
binbin <binbin1.deng@intel.com>
2022-12-27 14:27:05 +0800

    new .sh

--- python/chronos/dev/app/run-app-tests-pip.sh
+++ python/chronos/dev/app/run-app-tests-pip.sh
@@ -27,6 +27,7 @@ execute_test(){
     return $((now-start))
 }
 
+
 ${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/AIOps/AIOps_anomaly_detect_unsupervised_forecast_based
 FILENAME="${BIGDL_ROOT}/python/chronos/use-case/AIOps/m_1932.csv"
 if [ ! -f "$FILENAME" ];then
@@ -34,7 +35,7 @@ if [ ! -f "$FILENAME" ];then
 fi
 
 sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/AIOps/AIOps_anomaly_detect_unsupervised_forecast_based.py
-sed -i "s/epochs=20/epochs=2/g" ${BIGDL_ROOT}/python/chronos/use-case/AIOps/AIOps_anomaly_detect_unsupervised_forecast_based.py
+sed -i "s/epochs=20/epochs=1/g" ${BIGDL_ROOT}/python/chronos/use-case/AIOps/AIOps_anomaly_detect_unsupervised_forecast_based.py
 cd ${BIGDL_ROOT}/python/chronos/use-case/AIOps/
 execute_test anomaly-detect-unsupervised-forecast-based "${BIGDL_ROOT}/python/chronos/use-case/AIOps/AIOps_anomaly_detect_unsupervised_forecast_based.py"
 time1=$?
@@ -47,275 +48,98 @@ execute_test anomaly-detect-unsupervised "${BIGDL_ROOT}/python/chronos/use-case/
 time2=$?
 cd -
 
+
+# temporarily skip electricity examples
+if [ ! -f ~/.chronos/dataset/electricity.csv ];then
+    wget -nv $FTP_URI/analytics-zoo-data/apps/electricity.csv -P ~/.chronos/dataset/
+fi
+sed -i "s/epochs=30/epochs=1/g" ${BIGDL_ROOT}/python/chronos/use-case/electricity/tcn.py
+execute_test electricity-tcn "${BIGDL_ROOT}/python/chronos/use-case/electricity/tcn.py"
+time3=$?
+
+sed -i "s/epochs=3/epochs=1/g" ${BIGDL_ROOT}/python/chronos/use-case/electricity/autoformer.py
+execute_test electricity-autoformer "${BIGDL_ROOT}/python/chronos/use-case/electricity/autoformer.py"
+time4=$?
+
+
+${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction
+cd ${BIGDL_ROOT}/python/chronos/use-case/fsi/
+if [ ! -d "data" ];then
+    mkdir data
+    cd data
+    wget https://github.com/CNuge/kaggle-code/raw/master/stock_data/individual_stocks_5yr.zip
+    wget https://raw.githubusercontent.com/CNuge/kaggle-code/master/stock_data/merge.sh
+    chmod +x merge.sh
+    unzip individual_stocks_5yr.zip
+    ./merge.sh
+    cd ..
+fi
+
+sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction.py
+sed -i "s/epochs\ =\ 50/epochs\ =\ 1/g; s/batch_size\ =\ 16/batch_size\ =\ 108/g" ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction.py
+execute_test stock-prediction "${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction.py"
+time5=$?
+cd -
+
+${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction_prophet
+sed -i '/get_ipython()/d; /plot./d; /plt./d' ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction_prophet.py
+sed -i "s/epochs\ =\ 50/epochs\ =\ 1/g; s/batch_size\ =\ 16/batch_size\ =\ 108/g" ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction_prophet.py
+cd ${BIGDL_ROOT}/python/chronos/use-case/fsi/
+execute_test stock-prediction-prophet "${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction_prophet.py"
+time6=$?
+cd -
+
+
+${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_model_forecasting
+FILENAME="${BIGDL_ROOT}/python/chronos/use-case/network_traffic/data/data.csv"
+if [ ! -f "$FILENAME" ];then
+   wget $FTP_URI/analytics-zoo-data/network-traffic/data/data.csv -P ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/data
+fi
+
+sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_model_forecasting.py
+sed -i "s/epochs=50/epochs=1/g; s/epochs=20/epochs=1/g" ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_model_forecasting.py
+cd ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/
+execute_test network-traffic-model-forecasting "${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_model_forecasting.py"
+time7=$?
+cd -
+
+${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_multivariate_multistep_tcnforecaster
+sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_multivariate_multistep_tcnforecaster.py
+sed -i "s/epochs=20/epochs=1/g" ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_multivariate_multistep_tcnforecaster.py
+cd ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/
+execute_test network-traffic-multivariate-multistep-tcnforecaster "${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_multivariate_multistep_tcnforecaster.py"
+time8=$?
+cd -
+
+${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_forecasting
+sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_forecasting.py
+sed -i "s/cores=10/cores=4/g; s/epochs=20/epochs=1/g; s/n_sampling=24/n_sampling=6/g" ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_forecasting.py
+cd ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/
+execute_test network-traffic-autots-forecasting "${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_forecasting.py"
+time9=$?
+cd -
+
+${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_customized_model
+sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_customized_model.py
+sed -i 's/epochs=5/epochs=1/g;s/choice([32,64])/choice([32])/g;s/grid_search([32, 64])/grid_search([2, 4])/g' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_customized_model.py
+if [ ! -f ~/.chronos/dataset/network_traffic/network_traffic_data.csv ];then
+    wget -nv $FTP_URI/analytics-zoo-data/network-traffic/data/data.csv -P ~/.chronos/dataset/network_traffic/
+    mv ~/.chronos/dataset/network_traffic/data.csv ~/.chronos/dataset/network_traffic/network_traffic_data.csv
+fi
+execute_test network-traffic-autots-customized-model "${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_customized_model.py"
+time10=$?
+
+
 echo "#1 anomaly-detect-unsupervised-forecast-based time used:$time1 seconds"
 echo "#2 anomaly-detect-unsupervised time used:$time2 seconds"
+echo "#3 electricity-tcn time used:$time3 seconds"
+echo "#4 electricity-autoformer time used:$time4 seconds"
+echo "#5 stock-prediction time used:$time5 seconds"
+echo "#6 stock-prediction-prophet time used:$time6 seconds"
+echo "#7 network-traffic-model-forecasting time used:$time7 seconds"
+echo "#8 network-traffic-multivarite-multistep-tcnforecaster time used:$time8 seconds"
+echo "#9 network-traffic-autots-forecasting time used:$time9 seconds"
+echo "#10 network-traffic-autots-customized-model time used:$time10 seconds"
 
-# echo "#5 start app test for chronos-anomaly-detect-unsupervised"
-# #timer
-# start=$(date "+%s")
-
-
-# wget $FTP_URI/analytics-zoo-data/chronos-aiops/m_1932.csv -O ${BIGDL_ROOT}/python/chronos/use-case/AIOps/m_1932.csv
-# echo "Finished downloading AIOps data"
-# #FILENAME="${BIGDL_ROOT}/python/chronos/use-case/AIOps/m_1932.csv"
-# #if [ -f "$FILENAME" ]
-# #then
-# #   echo "$FILENAME already exists."
-# #else
-# #   echo "Downloading AIOps data"
-# #
-# #   wget $FTP_URI/analytics-zoo-data/chronos-aiops/m_1932.csv -P ${BIGDL_ROOT}/python/chronos/use-case/AIOps
-# #
-# #   echo "Finished downloading AIOps data"
-# #fi
-
-
-# cd ${BIGDL_ROOT}/python/chronos/use-case/AIOps/
-
-# python 
-# cd -
-
-# exit_status=$?
-# if [ $exit_status -ne 0 ];
-# then
-#     clear_up
-#     echo "chronos-anomaly-detect-unsupervised failed"
-#     exit $exit_status
-# fi
-# now=$(date "+%s")
-# time5=$((now-start))
-
-
-# echo "#2 start app test for chronos-network-traffic-model-forecasting"
-# #timer
-# start=$(date "+%s")
-# ${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_model_forecasting
-
-# FILENAME="${BIGDL_ROOT}/python/chronos/use-case/network_traffic/data/data.csv"
-# if [ -f "$FILENAME" ]
-# then
-#    echo "$FILENAME already exists."
-# else
-#    echo "Downloading network traffic data"
-
-#    wget $FTP_URI/analytics-zoo-data/network-traffic/data/data.csv -P ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/data
-
-#    echo "Finished downloading network traffic data"
-# fi
-
-# sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_model_forecasting.py
-# sed -i "s/epochs=50/epochs=2/g; s/epochs=20/epochs=2/g" ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_model_forecasting.py
-# cd ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/
-
-# python ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_model_forecasting.py
-# cd -
-
-# exit_status=$?
-# if [ $exit_status -ne 0 ];
-# then
-#     clear_up
-#     echo "chronos network-traffic-model-forecasting failed"
-#     exit $exit_status
-# fi
-# now=$(date "+%s")
-# time2=$((now-start))
-# echo "#2 chronos-network-traffic-model-forecasting time used:$time2 seconds"
-
-
-
-# echo "#6 start app test for chronos-stock-prediction"
-# #timer
-# start=$(date "+%s")
-# ${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction
-
-# sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction.py
-# sed -i "s/epochs\ =\ 50/epochs\ =\ 2/g; s/batch_size\ =\ 16/batch_size\ =\ 108/g" ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction.py
-# cwd=$PWD
-# cd ${BIGDL_ROOT}/python/chronos/use-case/fsi/
-
-# # download data
-# if [ -d "data" ]
-# then
-#     echo "data already exists"
-# else
-#     echo "Downloading stock prediction data"
-
-#     mkdir data
-#     cd data
-#     wget https://github.com/CNuge/kaggle-code/raw/master/stock_data/individual_stocks_5yr.zip
-#     wget https://raw.githubusercontent.com/CNuge/kaggle-code/master/stock_data/merge.sh
-#     chmod +x merge.sh
-#     unzip individual_stocks_5yr.zip
-#     ./merge.sh
-#     cd ..
-
-#     echo "Finish downloading stock prediction data"
-# fi
-
-# python ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction.py
-# cd $cwd
-
-# exit_status=$?
-# if [ $exit_status -ne 0 ];
-# then
-#     clear_up
-#     echo "chronos-stock-prediction failed"
-#     exit $exit_status
-# fi
-# now=$(date "+%s")
-# time6=$((now-start))
-# echo "#6 chronos-stock-prediction time used:$time6 seconds"
-
-# echo "#7 start app test for chronos-network-traffic-multivarite-multistep-tcnforecaster"
-# #timer
-# start=$(date "+%s")
-# ${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_multivariate_multistep_tcnforecaster
-
-# FILENAME="${BIGDL_ROOT}/python/chronos/use-case/network_traffic/data/data.csv"
-# if [ -f "$FILENAME" ]
-# then
-#     echo "$FILENAME already exists."
-# else
-#     echo "Downloading network traffic data"
-
-#     wget $FTP_URI/analytics-zoo-data/network-traffic/data/data.csv -P ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/data
-
-#     echo "Finished downloading network traffic data"
-# fi
-
-# sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_multivariate_multistep_tcnforecaster.py
-# sed -i "s/epochs=20/epochs=1/g" ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_multivariate_multistep_tcnforecaster.py
-# cd ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/
-
-# python ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_multivariate_multistep_tcnforecaster.py
-# cd -
-
-# exit_status=$?
-# if [ $exit_status -ne 0 ];
-# then
-#     clear_up
-#     echo "chronos network-traffic-multivariate-multistep-tcnforecaster failed"
-#     exit $exit_status
-# fi
-
-# now=$(date "+%s")
-# time7=$((now-start))
-# echo "#7 chronos-network-traffic-multivarite-multistep-tcnforecaster time used:$time7 seconds"
-
-# echo "#8 start app test for chronos-stock-prediction-prophet"
-# #timer
-# start=$(date "+%s")
-# ${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction_prophet
-
-# sed -i '/get_ipython()/d; /plot./d; /plt./d' ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction_prophet.py
-# sed -i "s/epochs\ =\ 50/epochs\ =\ 2/g; s/batch_size\ =\ 16/batch_size\ =\ 108/g" ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction_prophet.py
-# cwd=$PWD
-# cd ${BIGDL_ROOT}/python/chronos/use-case/fsi/
-
-# # download data
-# if [ -d "data" ]
-# then
-#     echo "data already exists"
-# else
-#     echo "Downloading stock prediction data"
-
-#     mkdir data
-#     cd data
-#     wget https://github.com/CNuge/kaggle-code/raw/master/stock_data/individual_stocks_5yr.zip
-#     wget https://raw.githubusercontent.com/CNuge/kaggle-code/master/stock_data/merge.sh
-#     chmod +x merge.sh
-#     unzip individual_stocks_5yr.zip
-#     ./merge.sh
-#     cd ..
-
-#     echo "Finish downloading stock prediction data"
-# fi
-
-# python ${BIGDL_ROOT}/python/chronos/use-case/fsi/stock_prediction_prophet.py
-# cd $cwd
-
-# exit_status=$?
-# if [ $exit_status -ne 0 ];
-# then
-#     clear_up
-#     echo "chronos-stock-prediction-prophet failed"
-#     exit $exit_status
-# fi
-# now=$(date "+%s")
-# time8=$((now-start))
-# echo "#8 chronos-stock-prediction-prophet time used:$time8 seconds"
-
-# echo "#9 start app test for chronos-network-traffic-autots-forecasting"
-# #timer
-# start=$(date "+%s")
-# ${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_forecasting
-
-# FILENAME="${BIGDL_ROOT}/python/chronos/use-case/network_traffic/data/data.csv"
-# if [ -f "$FILENAME" ]
-# then
-#    echo "$FILENAME already exists."
-# else
-#    echo "Downloading network traffic data"
-
-#    wget $FTP_URI/analytics-zoo-data/network-traffic/data/data.csv -P ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/data
-
-#    echo "Finished downloading network traffic data"
-# fi
-
-# sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_forecasting.py
-# sed -i "s/cores=10/cores=4/g; s/epochs=20/epochs=1/g; s/n_sampling=24/n_sampling=6/g" ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_forecasting.py
-# cd ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/
-
-# python ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_forecasting.py
-# cd -
-
-# exit_status=$?
-# if [ $exit_status -ne 0 ];
-# then
-#     clear_up
-#     echo "chronos network-traffic-autots-forecasting failed"
-#     exit $exit_status
-# fi
-# now=$(date "+%s")
-# time9=$((now-start))
-# echo "#9 chronos-network-traffic-autots-forecasting time used:$time9 seconds"
-
-# echo "#10 start app test for chronos-network-traffic-autots-customized-model"
-# #timer
-# time10=$(date "+%s")
-# ${BIGDL_ROOT}/python/chronos/dev/app/ipynb2py.sh ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_customized_model
-
-# sed -i '/get_ipython()/d; /plot[.]/d; /plt[.]/d; /axs[.]/d' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_customized_model.py
-# sed -i 's/epochs=5/epochs=1/g;s/choice([32,64])/choice([32])/g;s/grid_search([32, 64])/grid_search([2, 4])/g' ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_customized_model.py
-
-# if [ -f ~/.chronos/dataset/network_traffic/network_traffic_data.csv ]
-# then
-#     echo "network_traffic_data.csv exists."
-# else
-
-#     echo "Download network traffic data."    
-#     wget $FTP_URI/analytics-zoo-data/network-traffic/data/data.csv -P ~/.chronos/dataset/network_traffic/
-
-#     cd ~/.chronos/dataset/network_traffic/
-#     mv data.csv network_traffic_data.csv
-
-#     echo "Finished downloading network_traffic_data.csv"
-
-#     cd -
-# fi
-
-# python ${BIGDL_ROOT}/python/chronos/use-case/network_traffic/network_traffic_autots_customized_model.py
-
-# exit_status=$?
-# if [ $exit_status -ne 0 ];
-# then
-#     clear_up
-#     echo "chronos network_traffic_autots_customized_model failed."
-#     exit $exit_status
-# fi
-# now=$(date "+%s")
-# time10=$((now-start))
-# echo "#10 network_traffic_autots_customized_model time used:$time10 seconds"
-
-# # This should be done at the very end after all tests finish.
-# clear_up
+# This should be done at the very end after all tests finish.
+clear_up
