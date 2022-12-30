3b83af2a1bd29ff5483f434b1a1a676152414148
Uxito-Ada <414416158@qq.com>
2022-12-30 11:24:54 +0800

    modify build codes after test

--- ppml/services/BKeywhiz/docker/build-docker-image.sh
+++ ppml/services/BKeywhiz/docker/build-docker-image.sh
@@ -7,10 +7,6 @@ export JDK_URL=http://your-http-url-to-download-jdk
 sudo docker build \
     --build-arg http_proxy=http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT \
     --build-arg https_proxy=http://$HTTPS_PROXY_HOST:$HTTPS_PROXY_PORT \
-    --build-arg HTTP_PROXY_HOST=$HTTP_PROXY_HOST \
-    --build-arg HTTP_PROXY_PORT=$HTTP_PROXY_PORT \
-    --build-arg HTTPS_PROXY_HOST=$HTTPS_PROXY_HOST \
-    --build-arg HTTPS_PROXY_PORT=$HTTPS_PROXY_PORT \
     --build-arg JDK_VERSION=8u192 \
     --build-arg JDK_URL=$JDK_URL \
     -t intelanalytics/bkeywhiz:2.2.0-SNAPSHOT -f ./Dockerfile .
