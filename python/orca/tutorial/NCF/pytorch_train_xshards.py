ba25a1438dc955eebf677cd503257bfa525d66f5
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-01-03 15:13:45 +0800

    Update pytorch_train_xshards.py

--- python/orca/tutorial/NCF/pytorch_train_xshards.py
+++ python/orca/tutorial/NCF/pytorch_train_xshards.py
@@ -39,21 +39,21 @@ train_data, test_data, user_num, item_num, sparse_feats_input_dims, num_dense_fe
 
 # Step 3: Define the model, optimizer and loss
 def model_creator(config):
-    model = NCF(user_num=config['user_num'],
-                item_num=config['item_num'],
-                factor_num=config['factor_num'],
-                num_layers=config['num_layers'],
-                dropout=config['dropout'],
-                model=config['model'],
-                sparse_feats_input_dims=config['sparse_feats_input_dims'],
-                sparse_feats_embed_dims=config['sparse_feats_embed_dims'],
-                num_dense_feats=config['num_dense_feats'])
+    model = NCF(user_num=config["user_num"],
+                item_num=config["item_num"],
+                factor_num=config["factor_num"],
+                num_layers=config["num_layers"],
+                dropout=config["dropout"],
+                model=config["model"],
+                sparse_feats_input_dims=config["sparse_feats_input_dims"],
+                sparse_feats_embed_dims=config["sparse_feats_embed_dims"],
+                num_dense_feats=config["num_dense_feats"])
     model.train()
     return model
 
 
 def optimizer_creator(model, config):
-    return optim.Adam(model.parameters(), lr=config['lr'])
+    return optim.Adam(model.parameters(), lr=config["lr"])
 
 loss = nn.BCEWithLogitsLoss()
 
@@ -68,16 +68,16 @@ est = Estimator.from_torch(model=model_creator,
                            metrics=[Accuracy(), Precision(), Recall()],
                            backend=backend,
                            use_tqdm=True,
-                           config={'user_num': user_num,
-                                   'item_num': item_num,
-                                   'factor_num': 16,
-                                   'num_layers': 3,
-                                   'dropout': 0.5,
-                                   'lr': 0.01,
-                                   'model': "NeuMF-end",
-                                   'sparse_feats_input_dims': sparse_feats_input_dims,
-                                   'sparse_feats_embed_dims': 8,
-                                   'num_dense_feats': num_dense_feats})
+                           config={"user_num": user_num,
+                                   "item_num": item_num,
+                                   "factor_num": 16,
+                                   "num_layers": 3,
+                                   "dropout": 0.5,
+                                   "lr": 0.01,
+                                   "model": "NeuMF-end",
+                                   "sparse_feats_input_dims": sparse_feats_input_dims,
+                                   "sparse_feats_embed_dims": 8,
+                                   "num_dense_feats": num_dense_feats})
 est.fit(data=train_data, epochs=2,
         feature_cols=feature_cols,
         label_cols=label_cols,
@@ -90,13 +90,15 @@ result = est.evaluate(data=test_data,
                       feature_cols=feature_cols,
                       label_cols=label_cols,
                       batch_size=10240)
-print('Evaluation results:')
+print("Evaluation results:")
 for r in result:
     print("{}: {}".format(r, result[r]))
 
 
-# Step 6: Save the trained PyTorch model
+# Step 6: Save the trained pytorch model and processed data for resuming training or prediction
 est.save("NCF_model")
+train_data.save_pickle("train_xshards")
+test_data.save_pickle("test_xshards")
 
 
 # Step 7: Stop Orca Context when program finishes
