2c0f49d80e242eb7a8fe424f75d100f8a7d858ff
ForJadeForest <1113480910@qq.com>
2023-01-03 10:39:23 +0800

    update

--- python/orca/tutorial/NCF/tf_resume_spark_dataframe.py
+++ python/orca/tutorial/NCF/tf_resume_spark_dataframe.py
@@ -23,6 +23,7 @@ from process_spark_dataframe import prepare_data
 from bigdl.orca import init_orca_context, stop_orca_context
 from bigdl.orca.learn.tf2 import Estimator
 
+
 # Step 1: Init Orca Context
 init_orca_context(cluster_mode="local")
 
@@ -57,15 +58,14 @@ result = est.evaluate(test_df,
                       feature_cols=feature_cols,
                       label_cols=label_cols,
                       batch_size=batch_size,
-                      num_steps=val_steps,
-                      )
+                      num_steps=val_steps)
 print('Evaluation results:')
 for r in result:
     print("{}: {}".format(r, result[r]))
 
 
 # Step 5: Save the trained TensorFlow model
-est.save("NCF_model")
+est.save("NCF_resume_model")
 
 
 # Step 6: Stop Orca Context when program finishes
