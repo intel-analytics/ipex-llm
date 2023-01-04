4632f3de6568ecc5c9b8d5a1087ca771a2f6f127
zhaojiehu <zhaojie.hu@intel.com>
2023-01-04 10:10:25 +0800

    fix rebase error

--- python/chronos/src/bigdl/chronos/data/utils/export_torchscript.py
+++ python/chronos/src/bigdl/chronos/data/utils/export_torchscript.py
@@ -136,16 +136,6 @@ class ExportWithStandardScaler(ExportJIT):
             data_unscale[:, :, i] = data[:, :, i] * value_scale + value_mean
         return data_unscale
 
-    def unscale(self, data):
-        data_unscale = torch.zeros(data.size(), dtype=torch.float64)
-        for i in self.scaler_index:
-            value_mean = self.mean_[i] if self.with_mean \
-                else torch.zeros(self.mean_.size(), dtype=torch.float64)
-            value_scale = self.scale_[i] if self.with_std \
-                else torch.ones(self.scale_.size(), dtype=torch.float64)
-            data_unscale[:, :, i] = data[:, :, i] * value_scale + value_mean
-        return data_unscale
-
 
 class ExporWithMaxAbsScaler(ExportJIT):
     def __init__(self, scaler: MaxAbsScaler, lookback: int,
