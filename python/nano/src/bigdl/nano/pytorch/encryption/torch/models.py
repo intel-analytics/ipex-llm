84a0fbb8f4315108e27d06bfb6b29b3222d149bb
Guancheng Fu <110874468+gc-fu@users.noreply.github.com>
2023-01-06 01:04:23 -0500

    [Nano] Add encryption feature to torch.save/torch.load in nano (#7145)
    * Nano: add patched encryption function to torch.save and torch.load
    
    * Add new dependency into nano's setup.py

--- python/nano/src/bigdl/nano/pytorch/encryption/torch/models.py
+++ python/nano/src/bigdl/nano/pytorch/encryption/torch/models.py
@@ -15,32 +15,35 @@
 #
 
 import torch
-import io, os
+import io
+import os
 import pathlib
 from cryptography.fernet import Fernet
-from typing import BinaryIO, Union, IO
+from typing import BinaryIO, Union, IO, Optional
 import pickle
-from bigdl.dllib.utils.log4Error import invalidInputError
+from bigdl.nano.utils.log4Error import invalidInputError
 
 DEFAULT_PROTOCOL = 2
+
 
 def _is_path(name_or_buf):
     return isinstance(name_or_buf, str) or \
         isinstance(name_or_buf, pathlib.Path)
 
-# Attention, the use of encrypt method and decrypt method here will change the file location
+
 class _buf_operator(object):
     def __init__(self, key):
-        self.secret_key = Fernet(key) 
+        self.secret_key = Fernet(key)
 
     def encrypt_buffer(self, buffer, encrypted_buffer):
         content = buffer.getvalue()
-        encrypted_content= self.secret_key.encrypt(content)
+        encrypted_content = self.secret_key.encrypt(content)
         encrypted_buffer.write(encrypted_content)
 
     def decrypt_buffer(self, buffer, decrypted_buffer):
         decrypted_content = self.secret_key.decrypt(buffer.getvalue())
         decrypted_buffer.write(decrypted_content)
+
 
 class _opener(object):
     def __init__(self, file_like):
@@ -52,21 +55,25 @@ class _opener(object):
     def __exit__(self, *args):
         pass
 
+
 class _open_file(_opener):
     def __init__(self, name, mode):
         super(_open_file, self).__init__(open(name, mode))
-    
+
     def __exit__(self, *args):
         # Flush is automatically done when closing the file
         self.file_like.close()
+
 
 class _open_buffer_reader(_opener):
     def __init__(self, buffer):
         super(_open_buffer_reader, self).__init__(buffer)
 
+
 class _open_buffer_writer(_opener):
     def __exit__(self, *args):
         self.file_like.flush()
+
 
 def _open_file_or_buffer(file_like, mode):
     if _is_path(file_like):
@@ -79,10 +86,26 @@ def _open_file_or_buffer(file_like, mode):
         else:
             invalidInputError(f"Expected 'r' or 'w' in mode but got {mode}")
 
-def save(obj, f: Union[str, os.PathLike, BinaryIO, IO[bytes]], encryption_key: str, pickle_module = pickle, pickle_protocol=DEFAULT_PROTOCOL, _use_new_zipfile_serialization=True) -> None:
+
+def save(obj, f: Union[str, os.PathLike, BinaryIO, IO[bytes]],
+         pickle_module=pickle, pickle_protocol=DEFAULT_PROTOCOL,
+         _use_new_zipfile_serialization=True, encryption_key: Optional[str] = None) -> None:
+    """
+    Save method that will be used to replace torch.save.
+
+    Only used in patched torch.
+
+    :param encryption_key: Optional[str] = None. If set to None, the original
+           torch.save will be invoked.  Otherwise, the content will be encrypted
+           before writing it to final destination.
+    """
+    if encryption_key is None:
+        return torch.old_save(obj, f, pickle_module=pickle_module, pickle_protocol=pickle_protocol,
+                              _use_new_zipfile_serialization=_use_new_zipfile_serialization)
     buffer = io.BytesIO()
     encrypted_buffer = io.BytesIO()
-    torch.save(obj, buffer, pickle_module=pickle_module, pickle_protocol=pickle_protocol, _use_new_zipfile_serialization=_use_new_zipfile_serialization)
+    torch.old_save(obj, buffer, pickle_module=pickle_module, pickle_protocol=pickle_protocol,
+                   _use_new_zipfile_serialization=_use_new_zipfile_serialization)
     encryptor = _buf_operator(encryption_key)
     encryptor.encrypt_buffer(buffer, encrypted_buffer)
     with _open_file_or_buffer(f, 'wb') as opened_file:
@@ -92,7 +115,20 @@ def save(obj, f: Union[str, os.PathLike, BinaryIO, IO[bytes]], encryption_key: s
     return
 
 
-def load(f, decryption_key, map_location=None, pickle_module=pickle, **pickle_load_args):
+def load(f, map_location=None, pickle_module=pickle,
+         decryption_key: Optional[str] = None, **pickle_load_args):
+    """
+    Load method that will be used to replace torch.load.
+
+    Only used in patched torch.
+
+    :param decryption_key: Optional[str] = None. If set to None, the original
+           torch.load will be invoked.  Otherwise, the content will be decrypted
+           before loading it back to memory.
+    """
+    if decryption_key is None:
+        return torch.old_load(f, pickle_module=pickle_module,
+                              map_location=map_location, **pickle_load_args)
     decrypted_buf = io.BytesIO()
     decryptor = _buf_operator(decryption_key)
     with _open_file_or_buffer(f, 'rb') as opened_file:
@@ -104,4 +140,5 @@ def load(f, decryption_key, map_location=None, pickle_module=pickle, **pickle_lo
             decryptor.decrypt_buffer(f, decrypted_buf)
     # After writing to the buffer, need to set it back to its original position
     decrypted_buf.seek(0)
-    return torch.load(decrypted_buf, map_location=map_location, **pickle_load_args)
+    return torch.old_load(decrypted_buf, pickle_module=pickle_module,
+                          map_location=map_location, **pickle_load_args)
