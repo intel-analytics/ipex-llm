30490e6b4a01ac4d7859f53b34b686593cadc761
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-01-09 15:03:50 +0800

    Update process_spark_dataframe.py

--- python/orca/tutorial/NCF/process_spark_dataframe.py
+++ python/orca/tutorial/NCF/process_spark_dataframe.py
@@ -17,7 +17,7 @@ import os.path
 
 from pyspark.ml.feature import StringIndexer, MinMaxScaler, VectorAssembler
 from pyspark.ml import Pipeline
-from pyspark.sql.types import StructField, StructType, IntegerType, ArrayType, StringType
+from pyspark.sql.types import StructField, StructType, LongType, ArrayType, StringType
 from pyspark.sql.functions import udf, lit, collect_list, explode
 
 from bigdl.orca import OrcaContext
@@ -28,20 +28,20 @@ dense_features = ["age"]
 
 def read_data(data_dir):
     spark = OrcaContext.get_spark_session()
-    schema = StructType([StructField("user", IntegerType(), False),
-                         StructField("item", IntegerType(), False)])
+    schema = StructType([StructField("user", LongType(), False),
+                         StructField("item", LongType(), False)])
     schema_user = StructType(
         [
-            StructField("user", IntegerType(), False),
+            StructField("user", LongType(), False),
             StructField("gender", StringType(), False),
-            StructField("age", IntegerType(), False),
-            StructField("occupation", IntegerType(), False),
+            StructField("age", LongType(), False),
+            StructField("occupation", LongType(), False),
             StructField("zipcode", StringType(), False)
         ]
     )
     schema_item = StructType(
         [
-            StructField("item", IntegerType(), False),
+            StructField("item", LongType(), False),
             StructField("title", StringType(), False),
             StructField("category", StringType(), False)
         ]
@@ -71,7 +71,7 @@ def generate_neg_sample(df, item_num, neg_scale):
 
     df = df.withColumn("label", lit(1.0))
 
-    neg_sample_udf = udf(neg_sample, ArrayType(IntegerType(), False))
+    neg_sample_udf = udf(neg_sample, ArrayType(LongType(), False))
     df_neg = df.groupBy("user").agg(neg_sample_udf(collect_list("item")).alias("item_list"))
     df_neg = df_neg.select(df_neg.user, explode(df_neg.item_list))
     df_neg = df_neg.withColumn("label", lit(0.0))
