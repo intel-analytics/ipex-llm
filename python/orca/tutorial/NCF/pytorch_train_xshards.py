3e51a26095a85030c618694072cfb565dc00f957
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-01-09 11:31:32 +0800

    Update pytorch_train_xshards.py

--- python/orca/tutorial/NCF/pytorch_train_xshards.py
+++ python/orca/tutorial/NCF/pytorch_train_xshards.py
@@ -15,6 +15,8 @@
 #
 
 # Step 0: Import necessary libraries
+import os
+import argparse
 import torch.nn as nn
 import torch.optim as optim
 
@@ -26,15 +28,79 @@ from bigdl.orca.learn.pytorch import Estimator
 from bigdl.orca.learn.pytorch.callbacks.tensorboard import TensorBoardCallback
 from bigdl.orca.learn.metrics import Accuracy, Precision, Recall
 
+parser = argparse.ArgumentParser(description="PyTorch Example")
+parser.add_argument("--data_dir", type=str,
+                    help="The path to load data from local or remote resources.")
+parser.add_argument("--cluster_mode", type=str, default="local",
+                    help="The cluster mode, such as local, yarn-client, yarn-cluster, "
+                         "k8s-client, k8s-cluster, spark-submit or bigdl-submit.")
+parser.add_argument("--backend", type=str, default="spark", help="ray or spark")
+parser.add_argument("--callback", type=bool, default=True,
+                    help="Whether to use TensorBoardCallback.")
+parser.add_argument("--workers_per_node", type=int, default=1,
+                    help="The number of PyTorch workers on each node.")
+args = parser.parse_args()
+
 
 # Step 1: Init Orca Context
-sc = init_orca_context(cluster_mode="local")
+if args.cluster_mode == "local":
+    sc = init_orca_context(cluster_mode="local", memory="12g")
+elif args.cluster_mode.startswith("yarn"):
+    if args.cluster_mode == "yarn-client":
+        sc = init_orca_context(cluster_mode="yarn-client", cores=4, memory="10g", num_nodes=2,
+                               driver_cores=2, driver_memory="2g",
+                               extra_python_lib="pytorch_model.py,process_xshards.py")
+    elif args.cluster_mode == "yarn-cluster":
+        sc = init_orca_context(cluster_mode="yarn-cluster", cores=4, memory="10g", num_nodes=2,
+                               driver_cores=2, driver_memory="2g",
+                               extra_python_lib="pytorch_model.py,process_xshards.py")
+elif args.cluster_mode.startswith("k8s"):
+    if args.cluster_mode == "k8s-client":
+        conf = {
+            "spark.kubernetes.executor.volumes.persistentVolumeClaim.nfsvolumeclaim"
+            ".options.claimName": "nfsvolumeclaim",
+            "spark.kubernetes.executor.volumes.persistentVolumeClaim.nfsvolumeclaim"
+            ".mount.path": "/bigdl/nfsdata"
+        }
+        sc = init_orca_context(cluster_mode="k8s-client", num_nodes=2, cores=4, memory="10g",
+                               driver_cores=2, driver_memory="2g",
+                               master=os.environ.get("RUNTIME_SPARK_MASTER"),
+                               container_image=os.environ.get("RUNTIME_K8S_SPARK_IMAGE"),
+                               extra_python_lib="pytorch_model.py,process_xshards.py",
+                               conf=conf)
+    elif args.cluster_mode == "k8s-cluster":
+        conf = {
+            "spark.kubernetes.driver.volumes.persistentVolumeClaim.nfsvolumeclaim"
+            ".options.claimName": "nfsvolumeclaim",
+            "spark.kubernetes.driver.volumes.persistentVolumeClaim.nfsvolumeclaim"
+            ".mount.path": "/bigdl/nfsdata",
+            "spark.kubernetes.executor.volumes.persistentVolumeClaim.nfsvolumeclaim"
+            ".options.claimName": "nfsvolumeclaim",
+            "spark.kubernetes.executor.volumes.persistentVolumeClaim.nfsvolumeclaim"
+            ".mount.path": "/bigdl/nfsdata",
+            "spark.kubernetes.authenticate.driver.serviceAccountName": "spark",
+            "spark.kubernetes.file.upload.path": "/bigdl/nfsdata/"
+        }
+        sc = init_orca_context(cluster_mode="k8s-cluster", num_nodes=2, cores=4, memory="10g",
+                               driver_cores=2, driver_memory="2g",
+                               master=os.environ.get("RUNTIME_SPARK_MASTER"),
+                               container_image=os.environ.get("RUNTIME_K8S_SPARK_IMAGE"),
+                               penv_archive="file:///bigdl/nfsdata/environment.tar.gz",
+                               extra_python_lib="pytorch_model.py,process_xshards.py",
+                               conf=conf)
+elif args.cluster_mode == "bigdl-submit":
+    sc = init_orca_context(cluster_mode="bigdl-submit")
+elif args.cluster_mode == "spark-submit":
+    sc = init_orca_context(cluster_mode="spark-submit")
+else:
+    print("init_orca_context failed. cluster_mode should be one of 'local', 'yarn-client', "
+          "'yarn-cluster', 'k8s-client', 'k8s-cluster', 'bigdl-submit' or 'spark-submit', "
+          "but got " + args.cluster_mode)
 
 
 # Step 2: Read and process data using Orca XShards
-dataset_dir = "./ml-1m"
 train_data, test_data, user_num, item_num, sparse_feats_input_dims, num_dense_feats, \
-    feature_cols, label_cols = prepare_data(dataset_dir, num_ng=4)
+    feature_cols, label_cols = prepare_data(args.data_dir, num_ng=4)
 
 
 # Step 3: Define the model, optimizer and loss
@@ -59,21 +125,21 @@ loss = nn.BCEWithLogitsLoss()
 
 
 # Step 4: Distributed training with Orca PyTorch Estimator
-backend = "spark"  # "ray" or "spark"
-callbacks = [TensorBoardCallback(log_dir="runs", freq=1000)]
+callbacks = [TensorBoardCallback(log_dir="runs", freq=1000)] if args.callback else []
 
 est = Estimator.from_torch(model=model_creator,
                            optimizer=optimizer_creator,
                            loss=loss,
                            metrics=[Accuracy(), Precision(), Recall()],
-                           backend=backend,
+                           backend=args.backend,
                            use_tqdm=True,
+                           workers_per_node=args.workers_per_node,
                            config={"user_num": user_num,
                                    "item_num": item_num,
                                    "factor_num": 16,
                                    "num_layers": 3,
                                    "dropout": 0.5,
-                                   "lr": 0.01,
+                                   "lr": 0.001,
                                    "model": "NeuMF-end",
                                    "sparse_feats_input_dims": sparse_feats_input_dims,
                                    "sparse_feats_embed_dims": 8,
@@ -95,8 +161,10 @@ for r in result:
     print("{}: {}".format(r, result[r]))
 
 
-# Step 6: Save the trained PyTorch model
+# Step 6: Save the trained pytorch model and processed data for resuming training or prediction
 est.save("NCF_model")
+train_data.save_pickle(args.data_dir, "train_xshards")
+test_data.save_pickle(args.data_dir, "test_xshards")
 
 
 # Step 7: Stop Orca Context when program finishes
