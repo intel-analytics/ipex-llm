64c0894b51a294208fa8ca4e28668f09a8a35647
pinggao187 <pingx.gao@intel.com>
2023-01-12 16:06:59 +0800

    remove tfpark test

--- python/orca/dev/test/run-pytests-spark.sh
+++ python/orca/dev/test/run-pytests-spark.sh
@@ -29,7 +29,7 @@ export PYSPARK_DRIVER_PYTHON=python
 
 #python -m pytest -v --doctest-modules ../../../../orca/src/bigdl/orca/tfpark
 
-python -m pytest -v test/bigdl/orca/tfpark
+#python -m pytest -v test/bigdl/orca/tfpark
 python -m pytest -v test/bigdl/orca/learn/spark --ignore=test/bigdl/orca/learn/spark/test_estimator_openvino.py
 python -m pytest -v test/bigdl/orca/learn/test_metrics.py
 python -m pytest -v test/bigdl/orca/learn/test_utils.py
