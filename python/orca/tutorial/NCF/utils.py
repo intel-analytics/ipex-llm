e8629bb02808ed7ddbc155f1277c3dff14b2b21c
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-01-13 15:40:13 +0800

    Update utils.py

--- python/orca/tutorial/NCF/utils.py
+++ python/orca/tutorial/NCF/utils.py
@@ -20,9 +20,8 @@ import argparse
 from bigdl.orca import init_orca_context
 
 
-def parse_args(framework, input_data_type):
-    parser = argparse.ArgumentParser(description="{} NCF Training with {}".format(framework,
-                                                                                  input_data_type))
+def parse_args(description):
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument("--data_dir", type=str,
                         help="The path to load data from local or remote resources.")
     parser.add_argument("--cluster_mode", type=str, default="local",
@@ -31,44 +30,23 @@ def parse_args(framework, input_data_type):
     parser.add_argument("--backend", type=str, default="spark", help="ray or spark")
     parser.add_argument("--tensorboard", action='store_true',
                         help="Whether to use TensorBoardCallback.")
-
-    if framework == "PyTorch":
-        parser.add_argument("--workers_per_node", type=int, default=1,
-                            help="The number of PyTorch workers on each node.")
-
+    parser.add_argument("--workers_per_node", type=int, default=1,
+                        help="The number of workers on each node.")
     args = parser.parse_args()
     return args
 
 
-def init_orca(args, framework, input_data_type):
-    if args.cluster_mode.startswith("yarn") or args.cluster_mode.startswith("k8s"):
-        cores = 4
-        memory = "10g"
-        num_nodes = 2
-        driver_cores = 2
-        driver_memory = "2g"
-
-        if framework == "PyTorch":
-            extra_python_lib = "pytorch_model.py"
-        else:
-            extra_python_lib = "tf_model.py"
-        if input_data_type == "DataLoader":
-            extra_python_lib += ",pytorch_dataset.py"
-        elif input_data_type == "Orca Xshards":
-            extra_python_lib += ",process_xshards.py"
-
+def init_orca(args, extra_python_lib=None):
     if args.cluster_mode == "local":
         sc = init_orca_context(cluster_mode="local")
     elif args.cluster_mode.startswith("yarn"):
         if args.cluster_mode == "yarn-client":
             sc = init_orca_context(cluster_mode="yarn-client",
-                                   cores=cores, memory=memory, num_nodes=num_nodes,
-                                   driver_cores=driver_cores, driver_memory=driver_memory,
+                                   cores=4, memory="10g", num_nodes=2,
                                    extra_python_lib=extra_python_lib)
         elif args.cluster_mode == "yarn-cluster":
             sc = init_orca_context(cluster_mode="yarn-cluster",
-                                   cores=cores, memory=memory, num_nodes=num_nodes,
-                                   driver_cores=driver_cores, driver_memory=driver_memory,
+                                   cores=4, memory="10g", num_nodes=2,
                                    extra_python_lib=extra_python_lib)
     elif args.cluster_mode.startswith("k8s"):
         if args.cluster_mode == "k8s-client":
@@ -79,8 +57,7 @@ def init_orca(args, framework, input_data_type):
                 ".mount.path": "/bigdl/nfsdata"
             }
             sc = init_orca_context(cluster_mode="k8s-client",
-                                   cores=cores, memory=memory, num_nodes=num_nodes,
-                                   driver_cores=driver_cores, driver_memory=driver_memory,
+                                   cores=4, memory="10g", num_nodes=2,
                                    extra_python_lib=extra_python_lib,
                                    master=os.environ.get("RUNTIME_SPARK_MASTER"),
                                    container_image=os.environ.get("RUNTIME_K8S_SPARK_IMAGE"),
@@ -99,8 +76,7 @@ def init_orca(args, framework, input_data_type):
                 "spark.kubernetes.file.upload.path": "/bigdl/nfsdata/"
             }
             sc = init_orca_context(cluster_mode="k8s-cluster",
-                                   cores=cores, memory=memory, num_nodes=num_nodes,
-                                   driver_cores=driver_cores, driver_memory=driver_memory,
+                                   cores=4, memory="10g", num_nodes=2,
                                    extra_python_lib=extra_python_lib,
                                    master=os.environ.get("RUNTIME_SPARK_MASTER"),
                                    container_image=os.environ.get("RUNTIME_K8S_SPARK_IMAGE"),
