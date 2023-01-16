03008dd8f0cb43c7b1c5ddc407b9fbcc15920d91
ruoan1.wang <ruonan1.wang@intel.com>
2023-01-16 09:23:59 -0500

    fix style

--- python/nano/src/bigdl/nano/tf/keras/inference/_worker.py
+++ python/nano/src/bigdl/nano/tf/keras/inference/_worker.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
     param_file = sys.argv[1]
     thread_num = sys.argv[2]
     if thread_num != 'None':
-        thread_num = int(thread_num)
+        thread_num = int(thread_num)  # type: ignore
         tf.config.threading.set_inter_op_parallelism_threads(1)
         tf.config.threading.set_intra_op_parallelism_threads(thread_num)
     params = pickle.load(open(param_file, "rb"))
