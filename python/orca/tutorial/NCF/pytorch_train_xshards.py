4476a5246302c778f416e74e4f06bf97d441eb05
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-01-16 11:25:35 +0800

    Orca: update NCF examples for running on yarn clusters (#7186)
    * Add files via upload
    
    * Update pytorch_train_spark_dataframe_yarn.py
    
    * Add files via upload
    
    * Delete pytorch_train_xshards_yarn.py
    
    * Delete pytorch_train_spark_dataframe_yarn.py
    
    * Delete pytorch_train_dataloader_yarn.py
    
    * Delete pytorch_dataset_yarn.py
    
    * Delete tf_train_spark_dataframe_yarn.py
    
    * Delete tf_train_xshards_yarn.py
    
    * Update pytorch_dataset.py
    
    * Update pytorch_train_dataloader.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update tf_train_xshards.py
    
    * Update pytorch_train_xshards.py
    
    * Update pytorch_train_dataloader.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update pytorch_train_xshards.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update pytorch_train_xshards.py
    
    * Update tf_train_xshards.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update pytorch_dataset.py
    
    * Update pytorch_train_dataloader.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update pytorch_train_xshards.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update tf_train_xshards.py
    
    * Update pytorch_dataset.py
    
    * Update tf_train_xshards.py
    
    * Update tf_train_xshards.py
    
    * Update pytorch_train_xshards.py
    
    * Update pytorch_dataset.py
    
    * Update pytorch_train_dataloader.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update pytorch_train_xshards.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update tf_train_xshards.py
    
    * Add files via upload
    
    * Add files via upload
    
    * Update pytorch_train_xshards.py
    
    * Update tf_train_xshards.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update pytorch_train_dataloader.py
    
    * Update pytorch_dataset.py
    
    * Update utils.py
    
    * Update utils.py
    
    * Update pytorch_train_dataloader.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update tf_train_xshards.py
    
    * Update pytorch_train_xshards.py
    
    * Update pytorch_train_dataloader.py
    
    * Update pytorch_dataset.py
    
    * Update pytorch_dataset.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update utils.py
    
    * Update pytorch_train_xshards.py
    
    * Update tf_train_xshards.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update pytorch_dataset.py
    
    * Update pytorch_train_dataloader.py
    
    * Update pytorch_train_spark_dataframe.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update tf_train_xshards.py
    
    * Update pytorch_train_xshards.py
    
    * Update utils.py
    
    * Update pytorch_dataset.py
    
    * Update pytorch_dataset.py

--- python/orca/tutorial/NCF/pytorch_train_xshards.py
+++ python/orca/tutorial/NCF/pytorch_train_xshards.py
@@ -15,26 +15,27 @@
 #
 
 # Step 0: Import necessary libraries
+import os
 import torch.nn as nn
 import torch.optim as optim
 
 from process_xshards import prepare_data
 from pytorch_model import NCF
+from utils import *
 
-from bigdl.orca import init_orca_context, stop_orca_context
 from bigdl.orca.learn.pytorch import Estimator
 from bigdl.orca.learn.pytorch.callbacks.tensorboard import TensorBoardCallback
 from bigdl.orca.learn.metrics import Accuracy, Precision, Recall
 
 
 # Step 1: Init Orca Context
-sc = init_orca_context(cluster_mode="local")
+args = parse_args("PyTorch NCF Training with Orca Xshards")
+init_orca(args, extra_python_lib="pytorch_model.py,process_xshards.py")
 
 
 # Step 2: Read and process data using Orca XShards
-dataset_dir = "./ml-1m"
 train_data, test_data, user_num, item_num, sparse_feats_input_dims, num_dense_feats, \
-    feature_cols, label_cols = prepare_data(dataset_dir, num_ng=4)
+    feature_cols, label_cols = prepare_data(args.data_dir, num_ng=4)
 
 
 # Step 3: Define the model, optimizer and loss
@@ -59,15 +60,16 @@ loss = nn.BCEWithLogitsLoss()
 
 
 # Step 4: Distributed training with Orca PyTorch Estimator
-backend = "spark"  # "ray" or "spark"
-callbacks = [TensorBoardCallback(log_dir="runs", freq=1000)]
+callbacks = [TensorBoardCallback(log_dir=os.path.join(args.model_dir, "logs"),
+                                 freq=1000)] if args.tensorboard else []
 
 est = Estimator.from_torch(model=model_creator,
                            optimizer=optimizer_creator,
                            loss=loss,
                            metrics=[Accuracy(), Precision(), Recall()],
-                           backend=backend,
+                           backend=args.backend,
                            use_tqdm=True,
+                           workers_per_node=args.workers_per_node,
                            config={"user_num": user_num,
                                    "item_num": item_num,
                                    "factor_num": 16,
@@ -95,8 +97,10 @@ for r in result:
     print("{}: {}".format(r, result[r]))
 
 
-# Step 6: Save the trained PyTorch model
-est.save("NCF_model")
+# Step 6: Save the trained PyTorch model and processed data for resuming training or prediction
+est.save(os.path.join(args.model_dir, "NCF_model"))
+train_data.save_pickle(os.path.join(args.data_dir, "train_processed_xshards"))
+test_data.save_pickle(os.path.join(args.data_dir, "test_processed_xshards"))
 
 
 # Step 7: Stop Orca Context when program finishes
