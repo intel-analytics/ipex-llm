60f2404c095c2f251b622ec0269fb98d0de67022
Ruonan Wang <105281011+rnwang04@users.noreply.github.com>
2023-01-17 11:26:51 +0800

    Nano: replace `torch.no_grad()` with `torch.inference_mode()` in context manager  (#7250)
    * support inference_mode
    
    * fix bf16

--- python/nano/src/bigdl/nano/pytorch/context_manager.py
+++ python/nano/src/bigdl/nano/pytorch/context_manager.py
@@ -15,6 +15,8 @@
 #
 
 import torch
+import operator
+from bigdl.nano.utils.util import compare_version
 
 
 class BaseContextManager(object):
@@ -25,17 +27,17 @@ class BaseContextManager(object):
     """
 
     def __init__(self, thread_num=None):
-        self.no_grad = torch.no_grad()
+        self.infer_mode = torch.inference_mode(mode=True)
         self.thread_num = thread_num
         self.original_thread_num = torch.get_num_threads()
 
     def __enter__(self):
         if self.thread_num is not None:
             torch.set_num_threads(self.thread_num)
-        self.no_grad.__enter__()
+        self.infer_mode.__enter__()
 
     def __exit__(self, exc_type, exc_value, exc_tb):
-        self.no_grad.__exit__(exc_type, exc_value, exc_tb)
+        self.infer_mode.__exit__(exc_type, exc_value, exc_tb)
         torch.set_num_threads(self.original_thread_num)
 
 
@@ -48,6 +50,9 @@ class AutocastContextManager(BaseContextManager):
     """
     def __init__(self, thread_num=None):
         super().__init__(thread_num=thread_num)
+        if compare_version("torch", operator.lt, "1.13.0"):
+            # In torch1.12, torch.inference_mode(mode=True) will cause bug for jit+bf16
+            self.infer_mode = torch.no_grad()
         self.autocast = torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16)
 
     def __enter__(self):
