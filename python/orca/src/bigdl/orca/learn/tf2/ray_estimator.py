517ea12e5ad94a744aacd1dd217ef2cd6e740985
Hamster <niyinchen@sjtu.edu.cn>
2023-01-17 10:32:23 +0800

    fix(ray_estimator.py): rm extra itertools

--- python/orca/src/bigdl/orca/learn/tf2/ray_estimator.py
+++ python/orca/src/bigdl/orca/learn/tf2/ray_estimator.py
@@ -282,7 +282,6 @@ class TensorFlow2Estimator(OrcaRayEstimator):
                                                                       data_config)
                     remote_worker_stats.append(worker.step.remote(**params))
                 worker_stats = ray.get(remote_worker_stats)
-                worker_stats = list(itertools.chain.from_iterable(worker_stats))
             else:
                 invalidInputError(isinstance(validation_data, ray.data.Dataset),
                                   "Validation data type should be the same as train data,"
@@ -302,7 +301,6 @@ class TensorFlow2Estimator(OrcaRayEstimator):
                                                                                  data_config)
                     remote_worker_stats.append(self.remote_workers[i].step.remote(**params))
                 worker_stats = ray.get(remote_worker_stats)
-                worker_stats = list(itertools.chain.from_iterable(worker_stats))
         else:
             params["data_creator"] = data
             params["validation_data_creator"] = validation_data
