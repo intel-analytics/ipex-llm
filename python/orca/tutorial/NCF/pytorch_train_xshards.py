d2e993941537a13e4a10ce26b958edd13ada8896
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-01-18 20:16:17 +0800

    Update pytorch_train_xshards.py

--- python/orca/tutorial/NCF/pytorch_train_xshards.py
+++ python/orca/tutorial/NCF/pytorch_train_xshards.py
@@ -21,21 +21,21 @@ import torch.optim as optim
 
 from process_xshards import prepare_data
 from pytorch_model import NCF
+from utils import *
 
-from bigdl.orca import init_orca_context, stop_orca_context
 from bigdl.orca.learn.pytorch import Estimator
 from bigdl.orca.learn.pytorch.callbacks.tensorboard import TensorBoardCallback
 from bigdl.orca.learn.metrics import Accuracy, Precision, Recall
 
 
 # Step 1: Init Orca Context
-sc = init_orca_context(cluster_mode="local")
+args = parse_args("PyTorch NCF Training with Orca Xshards")
+init_orca(args, extra_python_lib="pytorch_model.py,process_xshards.py")
 
 
 # Step 2: Read and process data using Orca XShards
-dataset_dir = "./ml-1m"
 train_data, test_data, user_num, item_num, sparse_feats_input_dims, num_dense_feats, \
-    feature_cols, label_cols = prepare_data(dataset_dir, num_ng=4)
+    feature_cols, label_cols = prepare_data(args.data_dir, args.dataset, num_ng=4)
 
 
 # Step 3: Define the model, optimizer and loss
@@ -76,15 +76,16 @@ loss = nn.BCEWithLogitsLoss()
 
 
 # Step 4: Distributed training with Orca PyTorch Estimator
-backend = "spark"  # "ray" or "spark"
-callbacks = [TensorBoardCallback(log_dir="runs", freq=1000)]
+callbacks = [TensorBoardCallback(log_dir=os.path.join(args.model_dir, "logs"),
+                                 freq=1000)] if args.tensorboard else []
 
 est = Estimator.from_torch(model=model_creator,
                            optimizer=optimizer_creator,
                            loss=loss,
                            metrics=[Accuracy(), Precision(), Recall()],
-                           backend=backend,
+                           backend=args.backend,
                            use_tqdm=True,
+                           workers_per_node=args.workers_per_node,
                            config=config)
 train_stats = est.fit(data=train_data, epochs=2,
                       feature_cols=feature_cols,
@@ -110,12 +111,12 @@ for k, v in eval_stats.items():
 
 
 # Step 6: Save the trained PyTorch model and processed data for resuming training or prediction
-est.save("NCF_model")
-with open("config.json", "w") as f:
+est.save(os.path.join(args.model_dir, "NCF_model"))
+with open(os.path.join(args.model_dir, "config.json"), "w") as f:
     json.dump(config, f)
 
-train_data.save_pickle("train_processed_xshards")
-test_data.save_pickle("test_processed_xshards")
+train_data.save_pickle(os.path.join(args.data_dir, "train_processed_xshards"))
+test_data.save_pickle(os.path.join(args.data_dir, "test_processed_xshards"))
 
 
 # Step 7: Stop Orca Context when program finishes
