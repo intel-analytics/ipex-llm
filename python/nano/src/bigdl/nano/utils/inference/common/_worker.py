b24fc8a1d33f41e0491c5e988dcc0e1ece9f323d
xingyuan <xingyuan.li@intel.com>
2023-01-30 16:47:51 +0800

    * add no_grad for worker

--- python/nano/src/bigdl/nano/utils/inference/common/_worker.py
+++ python/nano/src/bigdl/nano/utils/inference/common/_worker.py
@@ -17,20 +17,22 @@
 import os
 import pickle
 import sys
-import traceback
+
 import cloudpickle
+import torch
 
 RETURN_FILENAME = 'return_value'
 
 
 def main():
-    param_file = sys.argv[1]
-    with open(param_file, "rb") as f:
-        params = pickle.load(f)
-    tmp_dir = os.path.dirname(param_file)
-    return_value = params[0](*(params[1:]))
-    with open(os.path.join(tmp_dir, RETURN_FILENAME), 'wb') as f:
-        cloudpickle.dump(return_value, f)
+    with torch.no_grad():
+        param_file = sys.argv[1]
+        with open(param_file, "rb") as f:
+            params = pickle.load(f)
+        tmp_dir = os.path.dirname(param_file)
+        return_value = params[0](*(params[1:]))
+        with open(os.path.join(tmp_dir, RETURN_FILENAME), 'wb') as f:
+            cloudpickle.dump(return_value, f)
 
 
 if __name__ == "__main__":
