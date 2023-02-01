f5d022d977762403dbd8dc688ae867055ad211f2
ForJadeForest <1113480910@qq.com>
2023-02-01 16:28:54 +0800

    delete model.py

--- python/orca/tutorial/tf/transfer_learning.py
+++ python/orca/tutorial/tf/transfer_learning.py
@@ -19,12 +19,11 @@ import math
 
 import tensorflow as tf
 import tensorflow_datasets as tfds
-
-from model import model_creator
+from tensorflow import keras
+from tensorflow.keras import layers
 
 from bigdl.orca import init_orca_context, stop_orca_context
 from bigdl.orca.learn.tf2 import Estimator
-
 
 # Step 1: Init Orca Context
 init_orca_context(memory='4g')
@@ -80,8 +79,54 @@ def test_data_creator(config, batch_size):
     return dataset
 
 
-# Step 3: Define model config
+# Step 3: Define model
 config = dict(dropout=0.2)
+
+
+def xception_model(dropout):
+    base_model = keras.applications.Xception(
+        weights="imagenet",  # Load weights pre-trained on ImageNet.
+        input_shape=(150, 150, 3),
+        include_top=False,
+    )  # Do not include the ImageNet classifier at the top.
+
+    # Freeze the base_model
+    base_model.trainable = False
+    data_augmentation = keras.Sequential(
+        [layers.RandomFlip("horizontal"), layers.RandomRotation(0.1), ]
+    )
+    # Create new model on top
+    inputs = keras.Input(shape=(150, 150, 3))
+    x = data_augmentation(inputs)  # Apply random data augmentation
+
+    # Pre-trained Xception weights requires that input be scaled
+    # from (0, 255) to a range of (-1., +1.), the rescaling layer
+    # outputs: `(inputs * scale) + offset`
+    scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1)
+    x = scale_layer(x)
+
+    # The base model contains batchnorm layers. We want to keep them in inference mode
+    # when we unfreeze the base model for fine-tuning, so we make sure that the
+    # base_model is running in inference mode here.
+    x = base_model(x, training=False)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    x = keras.layers.Dropout(dropout)(x)  # Regularize with dropout
+    outputs = keras.layers.Dense(1)(x)
+    model = keras.Model(inputs, outputs)
+
+    model.summary()
+
+    model.compile(
+        optimizer=keras.optimizers.Adam(),
+        loss=keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=[keras.metrics.BinaryAccuracy()],
+    )
+    return model
+
+
+def model_creator(config):
+    model = xception_model(config['dropout'])
+    return model
 
 
 # Step 4: Distributed training with Orca keras Estimator
@@ -94,14 +139,16 @@ batch_size = 32
 train_steps = math.ceil(tf.data.experimental.cardinality(train_ds) / batch_size)
 val_steps = math.ceil(tf.data.experimental.cardinality(validation_ds) / batch_size)
 test_steps = math.ceil(tf.data.experimental.cardinality(test_ds) / batch_size)
-est.fit(data=train_data_creator,
-        epochs=1,
-        batch_size=batch_size,
-        steps_per_epoch=train_steps,
-        validation_data=val_data_creator,
-        validation_steps=val_steps,
-        data_config=config)
-
+train_stats = est.fit(data=train_data_creator,
+                      epochs=1,
+                      batch_size=batch_size,
+                      steps_per_epoch=train_steps,
+                      validation_data=val_data_creator,
+                      validation_steps=val_steps,
+                      data_config=config)
+print("Train results:")
+for k, v in train_stats.items():
+    print("{}: {}".format(k, v))
 
 # Step 5: Distributed evaluation of the trained model
 eval_stats = est.evaluate(test_data_creator,
@@ -112,10 +159,8 @@ print("Evaluation results:")
 for k, v in eval_stats.items():
     print("{}: {}".format(k, v))
 
-
 # Step 6: Save the trained Tensorflow model
 est.save("model")
-
 
 # Step 7: Stop Orca Context when program finishes
 stop_orca_context()
