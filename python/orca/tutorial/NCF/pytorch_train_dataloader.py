eff7c99055a8212a67dde937f8e2c4743e96e778
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-02-02 16:08:31 +0800

    Update pytorch_train_dataloader.py

--- python/orca/tutorial/NCF/pytorch_train_dataloader.py
+++ python/orca/tutorial/NCF/pytorch_train_dataloader.py
@@ -17,7 +17,6 @@
 # Step 0: Import necessary libraries
 import torch.nn as nn
 import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
 import torch.utils.data as data
 
 from pytorch_dataset import load_dataset, process_users_items, get_input_dims
@@ -36,14 +35,14 @@ init_orca(args, extra_python_lib="pytorch_model.py,pytorch_dataset.py")
 
 # Step 2: Define train and test datasets as PyTorch DataLoader
 def train_loader_func(config, batch_size):
-    train_dataset, _ = load_dataset(config["data_dir"], config["dataset"], config["num_ng"])
+    train_dataset, _ = load_dataset(config["dataset_dir"], config["num_ng"])
     train_loader = data.DataLoader(train_dataset, batch_size=batch_size,
                                    shuffle=True, num_workers=0)
     return train_loader
 
 
 def test_loader_func(config, batch_size):
-    _, test_dataset = load_dataset(config["data_dir"], config["dataset"], config["num_ng"])
+    _, test_dataset = load_dataset(config["dataset_dir"], config["num_ng"])
     test_loader = data.DataLoader(test_dataset, batch_size=batch_size,
                                   shuffle=False, num_workers=0)
     return test_loader
@@ -52,7 +51,7 @@ def test_loader_func(config, batch_size):
 # Step 3: Define the model, optimizer and loss
 def model_creator(config):
     users, items, user_num, item_num, sparse_features, dense_features, \
-        total_cols = process_users_items(config["data_dir"], config["dataset"])
+        total_cols = process_users_items(config["dataset_dir"])
     sparse_feats_input_dims, num_dense_feats = get_input_dims(users, items,
                                                               sparse_features, dense_features)
     model = NCF(user_num=user_num,
@@ -71,29 +70,20 @@ def model_creator(config):
 def optimizer_creator(model, config):
     return optim.Adam(model.parameters(), lr=config["lr"])
 
-
-def scheduler_creator(optimizer, config):
-    scheduler = StepLR(optimizer, step_size=1)
-    return scheduler
-
 loss = nn.BCEWithLogitsLoss()
 
 
 # Step 4: Distributed training with Orca PyTorch Estimator
 callbacks = [TensorBoardCallback(log_dir=os.path.join(args.model_dir, "logs"),
                                  freq=1000)] if args.tensorboard else []
-scheduler = scheduler_creator if args.lr_scheduler else None
 
-est = Estimator.from_torch(model=model_creator,
-                           optimizer=optimizer_creator,
-                           scheduler_creator=scheduler,
+est = Estimator.from_torch(model=model_creator, optimizer=optimizer_creator,
                            loss=loss,
                            metrics=[Accuracy(), Precision(), Recall()],
                            backend=args.backend,
                            use_tqdm=True,
                            workers_per_node=args.workers_per_node,
-                           config={"data_dir": args.data_dir,
-                                   "dataset": args.dataset,
+                           config={"dataset_dir": args.data_dir,
                                    "num_ng": 4,
                                    "factor_num": 16,
                                    "num_layers": 3,
