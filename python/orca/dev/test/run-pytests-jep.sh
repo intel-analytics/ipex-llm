15fe80c256cba21735224970859aae4d99872485
Hamster <niyinchen@sjtu.edu.cn>
2023-02-02 14:32:14 +0800

    perf(run-pytests-jep.sh): simplify pytest

--- python/orca/dev/test/run-pytests-jep.sh
+++ python/orca/dev/test/run-pytests-jep.sh
@@ -32,12 +32,5 @@ if [[ ! -z "${DATA_STORE_URI}" ]]; then
 fi
 
 cd ../../
-python -m pytest -v test/bigdl/orca/learn/jep/test_pytorch_estimator_for_spark.py
-python -m pytest -v test/bigdl/orca/learn/jep/test_pytorch_estimator_for_dataloader.py
-python -m pytest -v test/bigdl/orca/learn/jep/test_pytorch_estimator_for_spark_creator.py
-python -m pytest -v test/bigdl/orca/learn/jep/test_pytorch_estimator_for_dataframe.py
-python -m pytest -v test/bigdl/orca/learn/jep/test_pytorch_estimator_for_save_load.py
-python -m pytest -v test/bigdl/orca/torch/test_torch_estimator.py
-python -m pytest -v test/bigdl/orca/torch/test_torch.py
-python -m pytest -v test/bigdl/orca/torch/test_torch_optim.py
-python -m pytest -v test/bigdl/orca/torch/test_inference_model_torch.py
+python -m pytest -v test/bigdl/orca/learn/jep/
+python -m pytest -v test/bigdl/orca/torch/
