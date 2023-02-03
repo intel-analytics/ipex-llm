4e31255948cda490717634424b2fdef326755198
Yuwen Hu <yuwen.hu@intel.com>
2023-02-03 15:27:50 +0800

    Add actions for TensorFlow Inference guides, and comment out others for now

---
+++ python/nano/tutorial/notebook/inference/tensorflow/run-nano-howto-guides-inference-tensorflow-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
+export NANO_HOWTO_GUIDES_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/tutorial/notebook/inference/tensorflow
+
+set -e
+
+# comment out the install commands
+sed -i 's/!pip install/#!pip install/' $NANO_HOWTO_GUIDES_TEST_DIR/*.ipynb
+
+# comment out the environment setting commands
+sed -i 's/!source bigdl-nano-init/#!source bigdl-nano-init/' $NANO_HOWTO_GUIDES_TEST_DIR/*.ipynb
+
+# limit the iterations of inferece for testing purposes
+sed -i 's/range(100)/range(10)/' $NANO_HOWTO_GUIDES_TEST_DIR/tensorflow_inference_bf16.ipynb
+
+echo 'Start testing'
+start=$(date "+%s")
+
+python -m pytest -s --nbmake --nbmake-timeout=600 --nbmake-kernel=python3 ${NANO_HOWTO_GUIDES_TEST_DIR}
+
+now=$(date "+%s")
+time=$((now-start))
+
+echo "BigDL-Nano how-to guides tests for Infernece TensorFLow finished."
+echo "Time used: $time seconds"
