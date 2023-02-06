671621fe2def43f22b77b4ff19a6c2f7ad76357b
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-02-06 13:37:21 +0800

    Update tf_train_xshards.py

--- python/orca/tutorial/NCF/tf_train_xshards.py
+++ python/orca/tutorial/NCF/tf_train_xshards.py
@@ -28,13 +28,12 @@ from bigdl.orca.learn.tf2 import Estimator
 
 # Step 1: Init Orca Context
 args = parse_args("TensorFlow NCF Training with Orca Xshards")
-args.backend = "ray"  # TODO: fix spark backend for saving optimizer states
 init_orca(args, extra_python_lib="tf_model.py,process_xshards.py")
 
 
 # Step 2: Read and process data using Orca Xshards
 train_data, test_data, user_num, item_num, sparse_feats_input_dims, num_dense_feats, \
-    feature_cols, label_cols = prepare_data(args.data_dir, args.dataset, num_ng=4)
+    feature_cols, label_cols = prepare_data(args.data_dir, num_ng=4)
 
 
 # Step 3: Define the NCF model
