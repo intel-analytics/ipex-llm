040472276bceea7fde35cf924d6bacf80b9746ee
Yuwen Hu <54161268+Oscilloscope98@users.noreply.github.com>
2023-02-07 16:12:32 +0800

    [Nano] Complete and reorganize all Nano how-to guides actions (#7408)
    * Disable other Nano actions for now
    
    * Remove all Nano how-to related actions for further reorgnization, add actions for PyTorch Inference guides, updated related installatiocommands in guides
    
    * Add actions for PyTorch Training guides
    
    * Add actions for PyTorch Preprocessing guides
    
    * Remove unnecessary torchvision installation when testing pytorch inference guides
    
    * Limit training dataset instead of epochs for PyTorch Training related guides actions, and comments out others for now
    
    * Add actions for PyTorch Lightning Training guides, and comment out others for now
    
    * Add actions for TensorFlow Inference guides, and comment out others for now
    
    * Add actions for TensorFlow Training guides, and comment out others for now
    
    * Add actions for OpenVINO Infernece  guides, and comment out others for now
    
    * Add missing requirements for OpenVINO actions
    
    * Uncomment all Nano how-to guides related workflow
    
    * Enable other Nano actions again
    
    * Minor fixes
    
    * Add checks for AVX512 in action platform to decided whether to test tf bf16 related notebooks
    
    * Reduce action testing time for inference_optimizer_optimize.ipynb
    
    * Minor fixes
    
    * Update based on comments
    
    * Disable PyTorch training how-to guides tests for bf16+ipex when avx512 is not available
    
    * Small installation revision regarding to jit, ipex pytorch inference guides
    
    * Small fix

--- python/nano/tutorial/notebook/training/pytorch/run-nano-howto-guides-training-pytorch-tests.sh
+++ python/nano/tutorial/notebook/training/pytorch/run-nano-howto-guides-training-pytorch-tests.sh
@@ -2,7 +2,7 @@
 
 export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export NANO_HOME=${ANALYTICS_ZOO_ROOT}/python/nano/src
-export NANO_HOWTO_GUIDES_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/tutorial/notebook/training/pytorch-lightning
+export NANO_HOWTO_GUIDES_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/nano/tutorial/notebook/training/pytorch
 
 TORCH_VERSION_LESS_1_12=`python -c "from bigdl.nano.pytorch.utils import TORCH_VERSION_LESS_1_12; print(TORCH_VERSION_LESS_1_12)"`
 AVX512_AVAILABLE=`lscpu | grep avx512`
@@ -12,18 +12,18 @@ set -e
 # disable training with native pytorch bf16 amp if torch<1.12
 if [[ $TORCH_VERSION_LESS_1_12 == True ]]
 then
-    sed -i "s/trainer = Trainer(max_epochs=5, precision='bf16')/#trainer = Trainer(max_epochs=5, precision='bf16')/" $NANO_HOWTO_GUIDES_TEST_DIR/pytorch_lightning_training_bf16.ipynb
+    sed -i "s/MyNano(precision='bf16').train()/#MyNano(precision='bf16').train()/" $NANO_HOWTO_GUIDES_TEST_DIR/accelerate_pytorch_training_bf16.ipynb
 fi
 # disable training with bf16 + ipex when avx512 is not avaliable;
 # as it will automatically turn back to fp32 training,
 # and no need to test
 if [[ -z "$AVX512_AVAILABLE" ]]
 then
-    sed -i "s/trainer = Trainer(max_epochs=5, use_ipex=True, precision='bf16')/#trainer = Trainer(max_epochs=5, use_ipex=True, precision='bf16')/" $NANO_HOWTO_GUIDES_TEST_DIR/pytorch_lightning_training_bf16.ipynb
+    sed -i "s/MyNano(use_ipex=True, precision='bf16').train()/#MyNano(use_ipex=True, precision='bf16').train()/" $NANO_HOWTO_GUIDES_TEST_DIR/accelerate_pytorch_training_bf16.ipynb
 fi
 
-# the number of batches to run is limited for testing purposes
-sed -i "s/max_epochs=5/max_epochs=5, fast_dev_run=True/" $NANO_HOWTO_GUIDES_TEST_DIR/*.ipynb
+# the training dataset size is limited for testing purposes
+sed -i "s/DataLoader(train_dataset/DataLoader(torch.utils.data.Subset(train_dataset, torch.randperm(len(train_dataset))[:5])/" $NANO_HOWTO_GUIDES_TEST_DIR/*.ipynb
 
 # comment out the install commands
 sed -i "s/!pip install/#!pip install/" $NANO_HOWTO_GUIDES_TEST_DIR/*.ipynb
@@ -34,10 +34,10 @@ sed -i "s/!source bigdl-nano-init/#!source bigdl-nano-init/" $NANO_HOWTO_GUIDES_
 echo "Start testing"
 start=$(date "+%s")
 
-python -m pytest -s --nbmake --nbmake-timeout=600 --nbmake-kernel=python3 ${NANO_HOWTO_GUIDES_TEST_DIR}/*.ipynb
+python -m pytest -s --nbmake --nbmake-timeout=600 --nbmake-kernel=python3 ${NANO_HOWTO_GUIDES_TEST_DIR}
 
 now=$(date "+%s")
 time=$((now-start))
 
-echo "BigDL-Nano how-to guides tests for Training PyTorch Lightning finished."
+echo "BigDL-Nano how-to guides tests for Training PyTorch finished."
 echo "Time used: $time seconds"
