5b83e501dc3f267f8ff7a57f8e1bd97d921a6fa1
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-02-07 20:55:03 +0800

    Update and rename pytorch_resume_dataloader.py to pytorch_resume_train_dataloader.py

--- python/orca/tutorial/NCF/pytorch_resume_train_dataloader.py
+++ python/orca/tutorial/NCF/pytorch_resume_train_dataloader.py
@@ -109,18 +109,10 @@ for epoch_stats in train_stats:
     print()
 
 
-# Step 5: Distributed evaluation of the trained model
-eval_stats = est.evaluate(test_loader_func,
-                          batch_size=10240)
-print("Evaluation results:")
-for k, v in eval_stats.items():
-    print("{}: {}".format(k, v))
+# Step 5: Save the trained PyTorch model
+est.save(os.path.join(args.model_dir, "NCF_resume_model"))
 
 
-# Step 6: Save the trained PyTorch model
-est.save(os.path.join(args.model_dir, "NCF_model"))
-
-
-# Step 7: Shutdown the Estimator and stop Orca Context when the program finishes
+# Step 6: Shutdown the Estimator and stop Orca Context when the program finishes
 est.shutdown()
 stop_orca_context()
