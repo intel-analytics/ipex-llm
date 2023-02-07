0e0bd96ba51c7c0d9f7686a6f7e475425214bd91
Kai Huang <huangkaivision@gmail.com>
2023-02-07 18:38:59 +0800

    minor

--- python/orca/tutorial/pytorch/transfer_learning/train.py
+++ python/orca/tutorial/pytorch/transfer_learning/train.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-# Most of the pytorch code is adapted from
+# Most of the code is adapted from
 # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
 #
 
