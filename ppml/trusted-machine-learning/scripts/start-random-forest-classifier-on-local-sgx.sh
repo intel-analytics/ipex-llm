c5ba7d56ddec9af5dc4404b71eac6bf5d37551f0
liyao <1599208165@qq.com>
2023-02-09 11:26:10 +0800

    add trusted-machine-learning

---
+++ ppml/trusted-machine-learning/scripts/start-random-forest-classifier-on-local-sgx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export sgx_command="/opt/jdk8/bin/java \
+    -cp /ppml/spark-${SPARK_VERSION}/conf/:/ppml/spark-${SPARK_VERSION}/jars/*:/ppml/spark-${SPARK_VERSION}/examples/jars/* -Xmx16g \
+    org.apache.spark.deploy.SparkSubmit \
+    --master local[4] \
+    --executor-memory 8g \
+    --driver-memory 8g \
+    --class org.apache.spark.examples.ml.RandomForestClassifierExample \
+    --conf spark.network.timeout=10000000 \
+    --conf spark.executor.heartbeatInterval=10000000 \
+    --verbose \
+    local:///ppml/spark-${SPARK_VERSION}/examples/jars/spark-examples_2.12-${SPARK_VERSION}.jar 3000"
+gramine-sgx bash 2>&1 | tee random-forest-classfier-local-sgx.log
+
