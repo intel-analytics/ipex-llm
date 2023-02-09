3a0d3efa8b680c914d2ac466f74ab7c66d43c51e
liangs6212 <liangx.shen@intel.com>
2023-02-09 16:17:17 +0800

    fix code-style

--- python/orca/src/bigdl/orca/automl/metrics.py
+++ python/orca/src/bigdl/orca/automl/metrics.py
@@ -421,8 +421,10 @@ def MSE(y_true: Union[List[Union[int, float]], ndarray,
     return result
 
 
-def Accuracy(y_true: List[Union[int, float, List[Union[int, float]], List[List[Union[int, float]]]]],
-             y_pred: List[Union[int, float, List[Union[int, float]], List[List[Union[int, float]]]]],
+def Accuracy(y_true: List[Union[int, float, List[Union[int, float]],
+                                List[List[Union[int, float]]]]],
+             y_pred: List[Union[int, float, List[Union[int, float]],
+                                List[List[Union[int, float]]]]],
              multioutput=None) -> Union[float64, ndarray]:
     """
     Calculate the accuracy score (Accuracy).
