1d6a45873adbcc6ab07193be06c8e50efeb877f4
ruoan1.wang <ruonan1.wang@intel.com>
2023-02-13 04:52:14 -0500

    fix ut

--- python/nano/test/openvino/pytorch/test_openvino.py
+++ python/nano/test/openvino/pytorch/test_openvino.py
@@ -306,14 +306,18 @@ class TestOpenVINO(TestCase):
 
     def test_openvino_trace_stable_diffusion_unet(self):
         from diffusers.models import UNet2DConditionModel
-        unet = UNet2DConditionModel(sample_size=128, cross_attention_dim=10)
+        unet = UNet2DConditionModel(sample_size=64,
+                                    cross_attention_dim=10,
+                                    attention_head_dim=1,
+                                    down_block_types=("CrossAttnDownBlock2D", "DownBlock2D"),
+                                    up_block_types=("UpBlock2D", "CrossAttnUpBlock2D",))
+        latent_shape = (2, 4, 8, 8)
+        image_latents = torch.randn(latent_shape, device = "cpu", dtype=torch.float32)
+        encoder_hidden_states = torch.randn((2, 12, 10), device = "cpu", dtype=torch.float32)
+        input_sample = (image_latents, torch.Tensor([980]).long(), encoder_hidden_states, False)
         dynamic_axes= {"sample": [0],
                        "encoder_hidden_states": [0],
                        "unet_output": [0]}
-        latent_shape = (2, 4, 16, 16)
-        image_latents = torch.randn(latent_shape, device = "cpu", dtype=torch.float32)
-        encoder_hidden_states = torch.randn((2, 77, 10))
-        input_sample = (image_latents, torch.Tensor([980]).long(), encoder_hidden_states, False)
         nano_unet = InferenceOptimizer.trace(unet, accelerator="openvino",
                                              input_sample=input_sample,
                                              input_names=["sample", "timestep",
--- python/nano/test/pytorch/tests/inference/test_ipex_jit_inference.py
+++ python/nano/test/pytorch/tests/inference/test_ipex_jit_inference.py
@@ -446,10 +446,14 @@ class IPEXJITInference_gt_1_10:
 
     def test_ipex_jit_inference_stable_diffusion_unet(self):
         from diffusers.models import UNet2DConditionModel
-        unet = UNet2DConditionModel(sample_size=128, cross_attention_dim=10)
-        latent_shape = (2, 4, 16, 16)
+        unet = UNet2DConditionModel(sample_size=64,
+                                    cross_attention_dim=10,
+                                    attention_head_dim=1,
+                                    down_block_types=("CrossAttnDownBlock2D", "DownBlock2D"),
+                                    up_block_types=("UpBlock2D", "CrossAttnUpBlock2D",))
+        latent_shape = (2, 4, 8, 8)
         image_latents = torch.randn(latent_shape, device = "cpu", dtype=torch.float32)
-        encoder_hidden_states = torch.randn((2, 77, 10), device = "cpu", dtype=torch.float32)
+        encoder_hidden_states = torch.randn((2, 12, 10), device = "cpu", dtype=torch.float32)
         input_sample = (image_latents, torch.Tensor([980]).long(), encoder_hidden_states, torch.tensor(False))
         nano_unet = InferenceOptimizer.trace(unet, accelerator="jit",
                                              use_ipex=True,
