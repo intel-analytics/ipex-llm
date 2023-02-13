4b05e30708999841875e57a72ee4ed34415ae5ac
Hamster <niyinchen@sjtu.edu.cn>
2023-02-13 19:21:32 +0800

    fix(test_pytorch_basic.py): rm tensorboard test

--- python/orca/test/bigdl/orca/learn/test_pytorch_basic.py
+++ python/orca/test/bigdl/orca/learn/test_pytorch_basic.py
@@ -513,54 +513,6 @@ class TestPyTorchEstimatorBasic(TestCase):
         estimator.fit(train_data_loader, epochs=4, batch_size=128,
                       validation_data=val_data_loader, callbacks=callbacks)
 
-    def test_tensorboard_callback(self):
-        from bigdl.orca.learn.pytorch.callbacks.tensorboard import TensorBoardCallback
-        sc = OrcaContext.get_spark_context()
-        spark = SparkSession.builder.getOrCreate()
-        rdd = sc.range(0, 100)
-        epochs = 2
-        data = rdd.map(lambda x: (np.random.randn(50).astype(np.float).tolist(),
-                                  [float(np.random.randint(0, 2, size=()))])
-                       )
-        schema = StructType([
-            StructField("feature", ArrayType(FloatType()), True),
-            StructField("label", ArrayType(FloatType()), True)
-        ])
-        df = spark.createDataFrame(data=data, schema=schema)
-        df = df.cache()
-
-        estimator = get_estimator(workers_per_node=2, log_level=logging.DEBUG)
-
-        try:
-            temp_dir = tempfile.mkdtemp()
-            log_dir = os.path.join(temp_dir, "runs_epoch")
-
-            callbacks = [
-                TensorBoardCallback(log_dir=log_dir, freq="epoch")
-            ]
-            estimator.fit(df, batch_size=4, epochs=epochs,
-                          callbacks=callbacks,
-                          feature_cols=["feature"],
-                          label_cols=["label"])
-
-            assert len(os.listdir(log_dir)) > 0
-
-            log_dir = os.path.join(temp_dir, "runs_batch")
-
-            callbacks = [
-                TensorBoardCallback(log_dir=log_dir, freq="batch")
-            ]
-            estimator.fit(df, batch_size=4, epochs=epochs,
-                          callbacks=callbacks,
-                          feature_cols=["feature"],
-                          label_cols=["label"])
-
-            assert len(os.listdir(log_dir)) > 0
-        finally:
-            shutil.rmtree(temp_dir)
-
-        estimator.shutdown()
-    
     def test_optional_optimizer(self):
         sc = init_nncontext()
         rdd = sc.range(0, 110).map(lambda x: np.array([x] * 50))
