1fa2b8b2a26a6cc7c6f08d5faad1cfbd1708fa2a
binbin <binbin1.deng@intel.com>
2023-02-15 10:41:55 +0800

    fix

--- python/chronos/dev/test/run-installation-options.sh
+++ python/chronos/dev/test/run-installation-options.sh
@@ -41,7 +41,7 @@ python -m pytest -v -m "${OPTIONS}" test/bigdl/chronos/autots \
                                     test/bigdl/chronos/metric \
                                     test/bigdl/chronos/model \
                                     test/bigdl/chronos/pytorch \
-                                    test/bigdl/chronos/simulator
+                                    test/bigdl/chronos/simulator \
        -k "not test_tcn_keras_forecaster_quantization"
 python -m pytest -s -m "${OPTIONS}" test/bigdl/chronos/forecaster/tf/\
 test_tcn_keras_forecaster.py::TestTCNForecaster::test_tcn_keras_forecaster_quantization
