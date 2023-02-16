8332f1167f863dd8db050f6865c99eb178938865
liangs6212 <liangx.shen@intel.com>
2023-02-16 10:03:11 +0800

    fix known issue

--- python/orca/src/bigdl/orca/automl/xgboost/auto_xgb.py
+++ python/orca/src/bigdl/orca/automl/xgboost/auto_xgb.py
@@ -83,13 +83,11 @@ class AutoXGBClassifier(AutoEstimator):
             metric_mode: Optional[str]=None,
             metric_threshold: Optional[Union[int, float]]=None,
             n_sampling: int=1,
-            search_space: Optional[Dict[str, Union["Function", "Integer", "Float"]]]=None,
+            search_space: Optional[Dict]=None,
             search_alg: Optional[str]=None,
-            search_alg_params: Optional[
-                Dict[str, Dict[str, Union["Function", "Integer", "Float"]]]]=None,
+            search_alg_params: Optional[Dict]=None,
             scheduler: Optional[str]=None,
-            scheduler_params: Optional[
-                Dict[str, Dict[str, Union["Function", "Integer", "Float"]]]]=None,
+            scheduler_params: Optional[Dict]=None,
             feature_cols: Optional[List[str]]=None,
             label_cols: Optional[List[str]]=None,
             ) -> None:
@@ -210,13 +208,11 @@ class AutoXGBRegressor(AutoEstimator):
             metric_mode: Optional[str]=None,
             metric_threshold: Optional[Union["Float", "Integer"]]=None,
             n_sampling: int=1,
-            search_space: Optional[Dict[str, Union["Function", "Integer", "Float"]]]=None,
+            search_space: Optional[Dict]=None,
             search_alg: Optional[str]=None,
-            search_alg_params: Optional[
-                Dict[str, Dict[str, Union["Function", "Integer", "Float"]]]]=None,
+            search_alg_params: Optional[Dict]=None,
             scheduler: Optional[str]=None,
-            scheduler_params: Optional[
-                Dict[str, Dict[str, Union["Function", "Integer", "Float"]]]]=None,
+            scheduler_params: Optional[Dict]=None,
             feature_cols: Optional[List[str]]=None,
             label_cols: Optional[List[str]]=None,
             ) -> None:
