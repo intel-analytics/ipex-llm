adf394dad760f347207d5492358c3e6f8eab0043
Hamster <niyinchen@sjtu.edu.cn>
2023-02-16 17:33:17 +0800

    test(action.yml): rm numpy version require

--- python/orca/dev/test/run-pytests-ray.sh
+++ python/orca/dev/test/run-pytests-ray.sh
@@ -45,7 +45,8 @@ python -m pytest -v test/bigdl/orca/learn/ray \
       --ignore=test/bigdl/orca/learn/ray/pytorch/test_estimator_horovod_backend.py \
       --ignore=test/bigdl/orca/learn/ray/pytorch/test_estimator_ray_runtime.py \
       --ignore=test/bigdl/orca/learn/ray/pytorch/test_estimator_ray_dataset.py \
-      --ignore=test/bigdl/orca/learn/ray/tf/
+      --ignore=test/bigdl/orca/learn/ray/tf/ \
+      --ignore=test/bigdl/orca/learn/ray/mxnet/
 exit_status_2=$?
 if [ $exit_status_2 -ne 0 ];
 then
