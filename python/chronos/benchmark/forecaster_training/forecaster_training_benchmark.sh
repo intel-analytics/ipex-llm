4caf418f4396439adf601c426b89bf93718bf200
Yishuo Wang <yishuo.wang@intel.com>
2023-02-16 09:58:54 +0800

    Nano/Chronos: fix performance test (#7568)

--- python/chronos/benchmark/forecaster_training/forecaster_training_benchmark.sh
+++ python/chronos/benchmark/forecaster_training/forecaster_training_benchmark.sh
@@ -3,7 +3,7 @@ set -e
 bash $ANALYTICS_ZOO_ROOT/python/chronos/dev/release/release.sh linux default false
 bash $ANALYTICS_ZOO_ROOT/python/nano/dev/build_and_install.sh linux default false pytorch --force-reinstall
 whl_name=`ls $ANALYTICS_ZOO_ROOT/python/chronos/src/dist/`
-pip install $ANALYTICS_ZOO_ROOT/python/chronos/src/dist/${whl_name}[pytorch]
+pip install $ANALYTICS_ZOO_ROOT/python/chronos/src/dist/${whl_name}[pytorch] --force-reinstall
 
 cd $ANALYTICS_ZOO_ROOT/python/chronos/benchmark/forecaster_training/
 
