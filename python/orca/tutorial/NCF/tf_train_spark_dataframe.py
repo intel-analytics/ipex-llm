c076038d03af9b2ce8f1cc053b28064c1b5849f5
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-02-20 23:15:33 +0800

    Update tf_train_spark_dataframe.py

--- python/orca/tutorial/NCF/tf_train_spark_dataframe.py
+++ python/orca/tutorial/NCF/tf_train_spark_dataframe.py
@@ -106,8 +106,7 @@ for k, v in eval_stats.items():
 
 
 # Step 6: Save the trained TensorFlow model and processed data for resuming training or prediction
-# TODO: fix save model to HDFS
-est.save(os.path.join(args.model_dir, "NCF_model"), save_format="h5")
+est.save(os.path.join(args.model_dir, "NCF_model.h5"))
 save_model_config(config, args.model_dir, "config.json")
 train_df.write.parquet(os.path.join(args.data_dir,
                                     "train_processed_dataframe.parquet"), mode="overwrite")
