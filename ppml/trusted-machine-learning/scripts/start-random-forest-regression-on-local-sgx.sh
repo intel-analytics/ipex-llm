1a45ba57abe57240c13249e71958e9522adb08b9
liyao <1599208165@qq.com>
2023-02-21 10:53:47 +0800

    update scriptes

--- ppml/trusted-machine-learning/scripts/start-random-forest-regression-on-local-sgx.sh
+++ ppml/trusted-machine-learning/scripts/start-random-forest-regression-on-local-sgx.sh
@@ -12,7 +12,8 @@ else
     wget https://raw.githubusercontent.com/apache/spark/master/data/mllib/sample_libsvm_data.txt -O "$input_file"
 fi
 
-/opt/jdk8/bin/java \
+
+export sgx_command="/opt/jdk8/bin/java \
     -cp "/ppml/spark-${SPARK_VERSION}/conf/:/ppml/spark-${SPARK_VERSION}/jars/*:/ppml/spark-${SPARK_VERSION}/examples/jars/*" -Xmx1g \
     org.apache.spark.deploy.SparkSubmit \
     --master local[2] \
@@ -25,4 +26,5 @@ fi
     --name RandomForestClassifierExample \
     --verbose \
     --jars local://${SPARK_HOME}/examples/jars/spark-examples_2.12-${SPARK_VERSION}.jar \
-    local://${SPARK_HOME}/examples/jars/spark-examples_2.12-${SPARK_VERSION}.jar 3000
+    local://${SPARK_HOME}/examples/jars/spark-examples_2.12-${SPARK_VERSION}.jar 3000"
+gramine-sgx bash 2>&1 | tee random-forest-classfier-local-sgx.log
