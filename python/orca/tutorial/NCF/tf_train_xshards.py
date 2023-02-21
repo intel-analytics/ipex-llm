a5a153976a2975533858db4148686695111bf515
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-02-21 09:59:15 +0800

    Orca: Save the tensorflow model in NCF tutorial in h5 format (#7623)
    * Update tf_train_spark_dataframe.py
    
    * Update tf_train_xshards.py
    
    * Update tf_predict_spark_dataframe.py
    
    * Update tf_predict_xshards.py
    
    * Update tf_resume_train_spark_dataframe.py
    
    * Update tf_resume_train_xshards.py
    
    * Update tf_train_spark_dataframe.py
    
    * Update tf_train_xshards.py

--- python/orca/tutorial/NCF/tf_train_xshards.py
+++ python/orca/tutorial/NCF/tf_train_xshards.py
@@ -106,8 +106,7 @@ for k, v in eval_stats.items():
 
 
 # Step 6: Save the trained TensorFlow model and processed data for resuming training or prediction
-# TODO: fix save model to HDFS
-est.save(os.path.join(args.model_dir, "NCF_model"))
+est.save(os.path.join(args.model_dir, "NCF_model.h5"))
 save_model_config(config, args.model_dir, "config.json")
 train_data.save_pickle(os.path.join(args.data_dir, "train_processed_xshards"))
 test_data.save_pickle(os.path.join(args.data_dir, "test_processed_xshards"))
