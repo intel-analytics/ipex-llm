8e92018192baf968577f267baad56917ce2cb5a1
Hamster <niyinchen@sjtu.edu.cn>
2023-02-28 13:14:14 +0800

    perf(run-pytests-ray.sh): change order

--- python/orca/dev/test/run-pytests-ray.sh
+++ python/orca/dev/test/run-pytests-ray.sh
@@ -48,27 +48,19 @@ then
     exit $exit_status_2
 fi
 
-echo "Running orca learn tf2 ray backend tests"
-python -m pytest -v test/bigdl/orca/learn/ray/tf/
+echo "Running orca data ray related tests"
+python -m pytest -v test/bigdl/orca/data/ray
 exit_status_3=$?
 if [ $exit_status_3 -ne 0 ];
 then
     exit $exit_status_3
 fi
 
-echo "Running orca data ray related tests"
-python -m pytest -v test/bigdl/orca/data/ray
-exit_status_4=$?
-if [ $exit_status_4 -ne 0 ];
-then
-    exit $exit_status_4
-fi
-
 echo "Running orca learn mmcv ray backend tests"
 python -m pytest -v test/bigdl/orca/learn/experimental/
-exit_status_5=$?
-if [ $exit_status_5 -ne 0 ];then
-    exit $exit_status_5
+exit_status_4=$?
+if [ $exit_status_4 -ne 0 ];then
+    exit $exit_status_4
 fi
 
 # TODO: support mxnet test under python 3.8
@@ -76,15 +68,24 @@ python_version=$(python --version | awk '{print$2}')
 if [ $python_version == 3.7.10 ];then
     echo "Running orca mxnet tests"
     python -m pytest -v test/bigdl/orca/learn/ray/mxnet/
-    exit_status_6=$?
-    if [ $exit_status_6 -ne 0 ];then
-        exit $exit_status_6
+    exit_status_5=$?
+    if [ $exit_status_5 -ne 0 ];then
+        exit $exit_status_5
     fi
 fi
 ray stop -f
 
 echo "Running orca learn spark backend tests"
 python -m pytest -v test/bigdl/orca/learn/spark/
+exit_status_6=$?
+if [ $exit_status_6 -ne 0 ];
+then
+    exit $exit_status_6
+fi
+
+# TODO: the following test might arise random fail
+echo "Running orca learn tf2 ray backend tests"
+python -m pytest -v test/bigdl/orca/learn/ray/tf/
 exit_status_7=$?
 if [ $exit_status_7 -ne 0 ];
 then
