d64d0df079793633ac0ee46ac6a6807847055831
ShanSimu <shaojie.cui@intel.com>
2023-03-01 13:09:08 +0800

    pyspark simplequery

--- ppml/trusted-bigdata/scripts/start-pyspark-simplequery-on-k8s-client-sgx.sh
+++ ppml/trusted-bigdata/scripts/start-pyspark-simplequery-on-k8s-client-sgx.sh
@@ -14,17 +14,16 @@ bash bigdl-ppml-submit.sh \
     --executor-memory 5g \
     --executor-cores 8\
     --conf spark.cores.max=32 \
-    --conf spark.kubernetes.executor.container.image=$RUNTIME_K8S_SPARK_IMAGE \
+    --conf spark.kubernetes.container.image=$RUNTIME_K8S_SPARK_IMAGE \
     --conf spark.kubernetes.container.image.pullPolicy=Always \
     --py-files local://$BIGDL_HOME/python/bigdl-ppml-spark_$SPARK_VERSION-$BIGDL_VERSION-python-api.zip,local://$BIGDL_HOME/python/bigdl-dllib-spark_$SPARK_VERSION-$BIGDL_VERSION-python-api.zip,local://$BIGDL_HOME/python/bigdl-spark_$SPARK_VERSION-$BIGDL_VERSION-python-api.zip \
     --conf spark.kubernetes.file.upload.path=file:///tmp \
     --name pyspark-simple-query-sgx-on-client \
     --verbose \
-    local:///ppml/examples/simple_query_example.py \
+    local:///ppml/bigdl-ppml/src/bigdl/ppml/api/simple_query_example.py \
     --app_id 465227134889 \
     --api_key 799072978028 \
-    --primary_key /ppml/data/SimpleQueryExampleWithSimpleKMS/keys/simple/primaryKey \
-    --data_key /ppml/data/SimpleQueryExampleWithSimpleKMS/keys/simple/dataKey \
+    --primary_key_material /ppml/data/SimpleQueryExampleWithSimpleKMS/keys/simple/primaryKey \
     --input_path /ppml/data/SimpleQueryExampleWithSimpleKMS/ppml_test/input \
     --output_path /ppml/data/SimpleQueryExampleWithSimpleKMS/ppml_test/output \
     --input_encrypt_mode AES/CBC/PKCS5Padding \
--- ppml/trusted-bigdata/scripts/start-pyspark-simplequery-on-k8s-cluster-sgx.sh
+++ ppml/trusted-bigdata/scripts/start-pyspark-simplequery-on-k8s-cluster-sgx.sh
@@ -3,9 +3,8 @@ cd /ppml
 
 export secure_password=`openssl rsautl -inkey /ppml/password/key.txt -decrypt </ppml/password/output.bin`
 bash bigdl-ppml-submit.sh \
-    --master $RUNTIME_SPARK_MASTER \
-    --deploy-mode cluster \
-    --sgx-enabled true \
+    --master local[4] \
+    --sgx-enabled false \
     --sgx-driver-jvm-memory 6g\
     --sgx-executor-jvm-memory 6g\
     --num-executors 4 \
@@ -14,18 +13,17 @@ bash bigdl-ppml-submit.sh \
     --executor-memory 5g \
     --executor-cores 8\
     --conf spark.cores.max=32 \
-    --conf spark.kubernetes.driver.container.image=$RUNTIME_K8S_SPARK_IMAGE \
+    --conf spark.kubernetes.container.image=$RUNTIME_K8S_SPARK_IMAGE \
     --conf spark.kubernetes.executor.container.image=$RUNTIME_K8S_SPARK_IMAGE \
     --conf spark.kubernetes.container.image.pullPolicy=Always \
     --py-files $BIGDL_HOME/python/bigdl-ppml-spark_$SPARK_VERSION-$BIGDL_VERSION-python-api.zip,$BIGDL_HOME/python/bigdl-dllib-spark_$SPARK_VERSION-$BIGDL_VERSION-python-api.zip,$BIGDL_HOME/python/bigdl-spark_$SPARK_VERSION-$BIGDL_VERSION-python-api.zip \
     --conf spark.kubernetes.file.upload.path=file:///tmp \
     --name pyspark-simple-query-sgx-on-cluster \
     --verbose \
-    local:///ppml/examples/simple_query_example.py \
+    local:///ppml/bigdl-ppml/src/bigdl/ppml/api/simple_query_example.py \
     --app_id 465227134889 \
     --api_key 799072978028 \
-    --primary_key /ppml/data/SimpleQueryExampleWithSimpleKMS/keys/simple/primaryKey \
-    --data_key /ppml/data/SimpleQueryExampleWithSimpleKMS/keys/simple/dataKey \
+    --primary_key_material /ppml/data/SimpleQueryExampleWithSimpleKMS/keys/simple/primaryKey \
     --input_path /ppml/data/SimpleQueryExampleWithSimpleKMS/ppml_test/input \
     --output_path /ppml/data/SimpleQueryExampleWithSimpleKMS/ppml_test/output \
     --input_encrypt_mode AES/CBC/PKCS5Padding \
