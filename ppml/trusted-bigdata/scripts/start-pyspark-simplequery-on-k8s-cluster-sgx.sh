6b62f32d42f52f789d91150290ca9e8e27e6e24a
ShanSimu <shaojie.cui@intel.com>
2023-03-01 13:20:53 +0800

    fix

--- ppml/trusted-bigdata/scripts/start-pyspark-simplequery-on-k8s-cluster-sgx.sh
+++ ppml/trusted-bigdata/scripts/start-pyspark-simplequery-on-k8s-cluster-sgx.sh
@@ -3,8 +3,9 @@ cd /ppml
 
 export secure_password=`openssl rsautl -inkey /ppml/password/key.txt -decrypt </ppml/password/output.bin`
 bash bigdl-ppml-submit.sh \
-    --master local[4] \
-    --sgx-enabled false \
+    --master $RUNTIME_SPARK_MASTER \
+    --deploy-mode cluster \
+    --sgx-enabled true \
     --sgx-driver-jvm-memory 6g\
     --sgx-executor-jvm-memory 6g\
     --num-executors 4 \
@@ -14,7 +15,6 @@ bash bigdl-ppml-submit.sh \
     --executor-cores 8\
     --conf spark.cores.max=32 \
     --conf spark.kubernetes.container.image=$RUNTIME_K8S_SPARK_IMAGE \
-    --conf spark.kubernetes.executor.container.image=$RUNTIME_K8S_SPARK_IMAGE \
     --conf spark.kubernetes.container.image.pullPolicy=Always \
     --py-files $BIGDL_HOME/python/bigdl-ppml-spark_$SPARK_VERSION-$BIGDL_VERSION-python-api.zip,$BIGDL_HOME/python/bigdl-dllib-spark_$SPARK_VERSION-$BIGDL_VERSION-python-api.zip,$BIGDL_HOME/python/bigdl-spark_$SPARK_VERSION-$BIGDL_VERSION-python-api.zip \
     --conf spark.kubernetes.file.upload.path=file:///tmp \
