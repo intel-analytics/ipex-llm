53a9f9ae940be028c5a9c54213da94c89a4fb756
Ziyao Peng <42887453+zpeng1898@users.noreply.github.com>
2023-03-03 00:16:38 +0800

    Update pytorch_ray_estimator.py

--- python/orca/src/bigdl/orca/learn/pytorch/pytorch_ray_estimator.py
+++ python/orca/src/bigdl/orca/learn/pytorch/pytorch_ray_estimator.py
@@ -103,8 +103,7 @@ class PyTorchRayEstimator(BaseRayEstimator):
 
         # todo remove ray_ctx to run on workers
         ray_ctx = OrcaRayContext.get()
-        if not isinstance(model_creator, types.FunctionType):
-            # Torch model is also callable.
+        if not isinstance(model_creator, types.FunctionType):  # Torch model is also callable.
             invalidInputError(False,
                               "Must provide a function for model_creator.")
 
