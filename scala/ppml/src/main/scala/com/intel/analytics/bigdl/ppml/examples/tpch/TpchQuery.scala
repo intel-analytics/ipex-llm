134999754009ff21393f93b3a1709b5964094e5f
ShanSimu <shaojie.cui@intel.com>
2023-03-02 13:21:46 +0800

    fix tpch

--- scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/examples/tpch/TpchQuery.scala
+++ scala/ppml/src/main/scala/com/intel/analytics/bigdl/ppml/examples/tpch/TpchQuery.scala
@@ -130,7 +130,9 @@ object TpchQuery {
       queries.enqueue(query)
     }
 
-    queries.foreach((query:TpchQuery) => {
+    while(queries.nonEmpty){
+      val query = queries.dequeue
+
       val t0 = System.nanoTime()
 
       outputDF(query.execute(sc, schemaProvider), outputDir, query.getName(), sc, outputCryptoMode)
@@ -138,8 +140,19 @@ object TpchQuery {
       val t1 = System.nanoTime()
 
       val elapsed = (t1 - t0) / 1000000000.0f // second
+      
       results += new Tuple2(query.getName(), elapsed)
-    })
+    }
+    // queries.foreach((query:TpchQuery) => {
+    //   val t0 = System.nanoTime()
+
+    //   outputDF(query.execute(sc, schemaProvider), outputDir, query.getName(), sc, outputCryptoMode)
+
+    //   val t1 = System.nanoTime()
+
+    //   val elapsed = (t1 - t0) / 1000000000.0f // second
+    //   results += new Tuple2(query.getName(), elapsed)
+    // })
 
     return results
   }
