46c4f91b97e71cb16d8cadbae9ab862f4d7c3334
Shaojie Cui <shaojie.cui@intel.com>
2023-03-08 10:49:40 +0800

    [PPML]tag 8g epc bigdata as default image (#7789)
    * build 4g and tag 8g as latest
    
    * reduce script memory
    
    * doc

--- ppml/trusted-bigdata/scripts/start-spark-mysql-io-on-k8s-cluster-sgx.sh
+++ ppml/trusted-bigdata/scripts/start-spark-mysql-io-on-k8s-cluster-sgx.sh
@@ -6,12 +6,12 @@ bash bigdl-ppml-submit.sh \
         --master $RUNTIME_SPARK_MASTER \
         --deploy-mode client \
         --sgx-enabled true \
-        --sgx-driver-jvm-memory 6g\
-        --sgx-executor-jvm-memory 6g\
+        --sgx-driver-jvm-memory 1g\
+        --sgx-executor-jvm-memory 3g\
         --num-executors 2 \
-        --driver-memory 8g \
+        --driver-memory 1g \
         --driver-cores 8 \
-        --executor-memory 8g \
+        --executor-memory 1g \
         --executor-cores 8\
         --conf spark.cores.max=16 \
         --conf spark.kubernetes.container.image=$RUNTIME_K8S_SPARK_IMAGE \
