702c9bdf015cb4a27a8832f60661c84d20c4031a
jenniew <jenniewang123@gmail.com>
2023-03-08 12:17:10 -0800

    update

--- python/orca/test/bigdl/orca/learn/spark/test_pytorch_estimator.py
+++ python/orca/test/bigdl/orca/learn/spark/test_pytorch_estimator.py
@@ -248,8 +248,7 @@ class TestPyTorchEstimator(TestCase):
         estimator = get_estimator(workers_per_node=2)
         train_worker_stats = estimator.fit(df, batch_size=4, epochs=2,
                                            feature_cols=["feature"],
-                                           label_cols=["label"]
-                                           )
+                                           label_cols=["label"])
         # Total samples for one epoch
         assert train_worker_stats[0]["num_samples"] == 100
         eval_worker_stats = estimator.evaluate(df, batch_size=4,
