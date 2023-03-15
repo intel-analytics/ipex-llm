9992bc1c0ab746e6d67a3ce89328c153ab126cf7
ZhouJian <jian.zhou@intel.com>
2023-03-15 11:45:49 +0800

    update

--- python/orca/dev/example/run-example-test-ray.sh
+++ python/orca/dev/example/run-example-test-ray.sh
@@ -11,35 +11,6 @@ clear_up () {
 
 echo "#start orca ray example tests"
 
-echo "#15 start example for mmcv faster-rcnn training"
-if [ -f ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/kitti_tiny ]
-then
-    echo "kitti_tiny already exists"
-else
-    wget -nv $FTP_URI/analytics-zoo-data/mmcv/kitti_tiny.zip -P ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn
-    unzip -d ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/kitti_tiny.zip
-fi
-
-if [ -f ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth ]
-then
-    echo "faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth already exists"
-else
-    wget -nv $FTP_URI/analytics-zoo-data/mmcv/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth -P ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn
-fi
-
-if [ -f ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/mmdetection-master ]
-then
-    echo "mmdetection-master already exists"
-else
-    wget -nv $FTP_URI/analytics-zoo-data/mmcv/mmdetection-master.zip -P ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn
-    unzip -d ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/mmdetection-master.zip
-fi
-
-start=$(date "+%s")
-python ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/train.py --dataset ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/kitti_tiny/ --config ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/mmdetection-master/configs/faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco.py --load_from ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth
-now=$(date "+%s")
-time16=$((now-start))
-
 echo "#1 Start autoestimator example"
 start=$(date "+%s")
 python ${BIGDL_ROOT}/python/orca/example/automl/autoestimator/autoestimator_pytorch.py --trials 5 --epochs 2
@@ -221,6 +192,34 @@ python ${BIGDL_ROOT}/python/orca/example/learn/pytorch/resnet50/inference.py ${B
 now=$(date "+%s")
 time15=$((now-start))
 
+echo "#15 start example for mmcv faster-rcnn training"
+if [ -f ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/kitti_tiny ]
+then
+    echo "kitti_tiny already exists"
+else
+    wget -nv $FTP_URI/analytics-zoo-data/mmcv/kitti_tiny.zip -P ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn
+    unzip -d ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/kitti_tiny.zip
+fi
+
+if [ -f ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth ]
+then
+    echo "faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth already exists"
+else
+    wget -nv $FTP_URI/analytics-zoo-data/mmcv/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth -P ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn
+fi
+
+if [ -f ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/mmdetection-master ]
+then
+    echo "mmdetection-master already exists"
+else
+    wget -nv $FTP_URI/analytics-zoo-data/mmcv/mmdetection-master.zip -P ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn
+    unzip -d ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/mmdetection-master.zip
+fi
+
+start=$(date "+%s")
+python ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/train.py --dataset ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/kitti_tiny/ --config ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/mmdetection-master/configs/faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_1x_coco.py --load_from ${BIGDL_ROOT}/python/orca/example/learn/mmcv/faster_rcnn/faster_rcnn_r50_caffe_fpn_mstrain_3x_coco_20210526_095054-1f77628b.pth
+now=$(date "+%s")
+time16=$((now-start))
 
 echo "Ray example tests finished"
 
