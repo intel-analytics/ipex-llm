22964aaea80fde4d3d3acc8e123ec52e84f9795e
ShanSimu <shaojie.cui@intel.com>
2023-03-21 16:19:35 +0800

    fix

--- ppml/trusted-bigdata/scripts/start-pyspark-simpleexamples-on-local-sgx.sh
+++ ppml/trusted-bigdata/scripts/start-pyspark-simpleexamples-on-local-sgx.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
-status_1_local_spark_pi=1
 status_2_local_spark_wordcount=0
 export MALLOC_ARENA_MAX=12
 cd /ppml
-
-if [ $status_1_local_spark_pi -ne 0 ]; then
-echo "sexample.1 local spark, pi"
-export sgx_command="/opt/jdk8/bin/java \
-   -cp /ppml/spark-$SPARK_VERSION/conf/:/ppml/spark-$SPARK_VERSION/jars/*:/ppml/spark-$SPARK_VERSION/examples/jars/* \
-   -Xmx1g org.apache.spark.deploy.SparkSubmit \
-   --master local[4] \
-   --conf spark.python.use.daemon=false \
-   --conf spark.python.worker.reuse=false \
-   /ppml/spark-$SPARK_VERSION/examples/src/main/python/pi.py"
-gramine-sgx bash 2>&1 | tee test-pi-sgx.log
-cat test-pi-sgx.log | egrep 'roughly'
-status_1_local_spark_pi=$(echo $?)
-fi
-
 
 if [ $status_2_local_spark_wordcount -ne 0 ]; then
 echo "example.2 local spark, test-wordcount"
@@ -33,12 +17,6 @@ export sgx_command="/opt/jdk8/bin/java \
 gramine-sgx bash 2>&1 | tee test-wordcount-sgx.log
 cat test-wordcount-sgx.log | egrep 'import'
 status_2_local_spark_wordcount=$(echo $?)
-fi
-
-if [ $status_1_local_spark_pi -ne 0 ]; then
-echo "#### sexample.1 Excepted result(Pi): Pi is roughly XXX"
-echo "---- sexample.1 Actual result: "
-cat test-pi-sgx.log | egrep 'roughly'
 fi
 
 if [ $status_2_local_spark_wordcount -ne 0 ]; then
