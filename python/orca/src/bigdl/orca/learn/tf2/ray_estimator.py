ce65f60467a3d8f1e43d93038e9449a939939cff
sgwhat <ge.song@intel.com>
2023-03-21 15:20:41 +0800

    support multi-model with ray backend

--- python/orca/src/bigdl/orca/learn/tf2/ray_estimator.py
+++ python/orca/src/bigdl/orca/learn/tf2/ray_estimator.py
@@ -497,7 +497,8 @@ class TensorFlow2Estimator(OrcaRayEstimator):
                 callbacks: Optional[List["Callback"]]=None,
                 data_config: Optional[Dict]=None,
                 feature_cols: Optional[List[str]]=None,
-                min_partition_num: Optional[int]=None) -> Union["SparkXShards",
+                min_partition_num: Optional[int]=None,
+                output_cols: Optional[List[str]]=None) -> Union["SparkXShards",
                                                                 "SparkDataFrame"]:
         """
         Predict the input data
@@ -542,6 +543,7 @@ class TensorFlow2Estimator(OrcaRayEstimator):
             steps=steps,
             callbacks=callbacks,
             data_config=data_config,
+            output_cols=output_cols
         )
         from bigdl.orca.data import SparkXShards
         from pyspark.sql import DataFrame
@@ -556,7 +558,7 @@ class TensorFlow2Estimator(OrcaRayEstimator):
                                               accept_str_col=True,
                                               shard_size=local_batch_size)
             pred_shards = self._predict_spark_xshards(xshards, params)
-            result = convert_predict_xshards_to_dataframe(data, pred_shards)
+            result = convert_predict_xshards_to_dataframe(data, pred_shards, output_cols)
         elif isinstance(data, SparkXShards):
             xshards = data.to_lazy()
             if xshards._get_class_name() == 'pandas.core.frame.DataFrame':
