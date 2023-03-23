8285ad038dc5e03364a690864a7858bcbbf19fc9
Hamster <niyinchen@sjtu.edu.cn>
2023-03-23 13:25:09 +0800

    test(run-pytests-ray.sh): merge two tests

--- python/orca/dev/test/run-pytests-ray.sh
+++ python/orca/dev/test/run-pytests-ray.sh
@@ -40,21 +40,10 @@ then
 fi
 ray stop -f
 
-# TODO: fix extra fixture {"spark.python.worker.reuse": "false"}
-#       for tensorflow estimator ray backend unit tests
-python -m pytest -v test/bigdl/orca/learn/ray/tf/ -s
-exit_status_2=$?
-if [ $exit_status_2 -ne 0 ];
-then
-    exit $exit_status_2
-fi
-ray stop -f
-
 echo "Running orca learn ray backend tests"
 python -m pytest -v test/bigdl/orca/learn/ray \
       --ignore=test/bigdl/orca/learn/ray/horovod/ \
       --ignore=test/bigdl/orca/learn/ray/ctx/ \
-      --ignore=test/bigdl/orca/learn/ray/tf/ \
       --ignore=test/bigdl/orca/learn/ray/mxnet/
 exit_status_3=$?
 if [ $exit_status_3 -ne 0 ];
