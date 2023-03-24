61654e16233438b6f9fb002358c482e58645ee12
Hamster <niyinchen@sjtu.edu.cn>
2023-03-24 09:33:13 +0800

    style: rename
    gp

--- docker/orca/install-orca-env.sh
+++ docker/orca/install-orca-env.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/intel-analytics/BigDL/main/python/requirements/orca/requirements_automl.txt && \
-conda create -y -n bigdl-orca python=3.7 && \
-source activate bigdl-orca && \
+conda create -y -n bigdl python=3.7 && \
+source activate bigdl && \
 pip install --no-cache-dir -r requirements_automl.txt
