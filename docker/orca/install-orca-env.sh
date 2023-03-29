96938decf0675efe796ca4158f5bfdb19b4bbe16
Hamster <niyinchen@sjtu.edu.cn>
2023-03-29 16:56:03 +0800

    ci: update to py3.9

--- docker/orca/install-orca-env.sh
+++ docker/orca/install-orca-env.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/intel-analytics/BigDL/main/python/requirements/orca/requirements_automl.txt && \
-conda create -y -n bigdl python=3.7 && \
+conda create -y -n bigdl python=3.9 && \
 source activate bigdl && \
 pip install --no-cache-dir -r requirements_automl.txt
