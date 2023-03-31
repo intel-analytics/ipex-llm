50fc47de39f326b359409d4d0ec567f702b95b8b
liyao <1599208165@qq.com>
2023-03-31 09:25:31 +0800

    update entrypoint

--- ppml/services/kms-utils/docker/entrypoint.sh
+++ ppml/services/kms-utils/docker/entrypoint.sh
@@ -114,7 +114,7 @@ elif [ "$action" = "encrypt" ]; then
 			--conf spark.bigdl.primaryKey.AmyPK.kms.type=SimpleKeyManagementService \
 			--conf spark.bigdl.primaryKey.AmyPK.kms.appId=${appid} \
 			--conf spark.bigdl.primaryKey.AmyPK.kms.apiKey=${apikey} \
-			--conf spark.bigdl.primaryKey.AmyPK.material=/home/key/simplePrimaryKey \
+			--conf spark.bigdl.primaryKey.AmyPK.material=/home/key/simple_encrypted_primary_key \
 			--verbose \
 			--class com.intel.analytics.bigdl.ppml.utils.Encrypt \
 			--conf spark.executor.extraClassPath=$BIGDL_HOME/jars/* \
