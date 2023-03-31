9db0e90e8af93cb8a7abcea7814e17b6a2d622a7
Hamster <niyinchen@sjtu.edu.cn>
2023-03-31 15:30:16 +0800

    feat(Dockerfile): support different python version

--- docker/orca/install-orca-env.sh
+++ docker/orca/install-orca-env.sh
@@ -1,4 +1,6 @@
-wget https://raw.githubusercontent.com/intel-analytics/BigDL/main/python/requirements/orca/requirements_automl.txt && \
-conda create -y -n bigdl python=3.9 && \
-source activate bigdl && \
+wget https://raw.githubusercontent.com/intel-analytics/BigDL/main/python/requirements/orca/requirements_automl.txt
+# python version passed as the first argument
+# default version: 3.9
+conda create -y -n bigdl python=$1
+source activate bigdl
 pip install --no-cache-dir -r requirements_automl.txt
