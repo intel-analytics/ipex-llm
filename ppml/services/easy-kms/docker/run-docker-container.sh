34ebf488008abb6349b397acd207c733a87158c2
Uxito-Ada <414416158@qq.com>
2023-04-07 15:47:44 +0800

    data volume

--- ppml/services/easy-kms/docker/run-docker-container.sh
+++ ppml/services/easy-kms/docker/run-docker-container.sh
@@ -3,6 +3,7 @@ export SSL_KEYS_PATH=path_of_your_ssl_keys_folder
 export SSL_PASSWORD_PATH=path_of_your_ssl_password_folder
 export IMAGE_NAME=intelanalytics/easy-kms:2.3.0-SNAPSHOT
 export ROOT_KEY=your_256bit_base64_AES_key_string
+export DATA_STORAGE_PATH=a_host_path_for_persistent_stoarge
 
 sudo docker run -itd \
     --privileged \
@@ -11,6 +12,7 @@ sudo docker run -itd \
     --oom-kill-disable \
     -v $SSL_KEYS_PATH:/ppml/keys \
     -v $SSL_PASSWORD_PATH:/ppml/password \
+    -v $DATA_STORAGE_PATH:/ppml/data \
     -e ROOT_KEY=${ROOT_KEY} \
     --name=easy-key-management-server \
     $IMAGE_NAME bash
