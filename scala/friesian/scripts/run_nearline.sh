05486a568924589e8c37282236410b7370df5b9a
Hamster <niyinchen@sjtu.edu.cn>
2023-04-11 13:22:32 +0800

    ci(config): update path

--- scala/friesian/scripts/run_nearline.sh
+++ scala/friesian/scripts/run_nearline.sh
@@ -3,9 +3,12 @@ set -e
 
 docker_name=intelanalytics/friesian-serving:2.2.0-SNAPSHOT
 
-docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name feature-init -c mnt/nearline/config_feature.yaml
+cd ..
+nearlinePath=src/main/resources/nearlineConfig
 
-docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name feature-init -c mnt/nearline/config_feature_vec.yaml
+docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name feature-init -c mnt/$nearlinePath/config_feature.yaml
 
-docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name recall-init -c mnt/nearline/config_recall.yaml
+docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name feature-init -c mnt/$nearlinePath/config_feature_vec.yaml
+
+docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name recall-init -c mnt/$nearlinePath/config_recall.yaml
 
