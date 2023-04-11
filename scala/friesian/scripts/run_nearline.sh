4e9a490b5c8c449add0cbd197c6ace89f982706d
Hamster <niyinchen@sjtu.edu.cn>
2023-04-11 13:58:41 +0800

    style(yaml): rename configpath

--- scala/friesian/scripts/run_nearline.sh
+++ scala/friesian/scripts/run_nearline.sh
@@ -4,11 +4,11 @@ set -e
 docker_name=intelanalytics/friesian-serving:2.2.0-SNAPSHOT
 
 cd ..
-nearlinePath=src/main/resources/nearlineConfig
+configPath=src/main/resources/nearlineConfig
 
-docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name feature-init -c mnt/$nearlinePath/config_feature.yaml
+docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name feature-init -c mnt/$configPath/config_feature.yaml
 
-docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name feature-init -c mnt/$nearlinePath/config_feature_vec.yaml
+docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name feature-init -c mnt/$configPath/config_feature_vec.yaml
 
-docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name recall-init -c mnt/$nearlinePath/config_recall.yaml
+docker run -it --net host --rm -v $(pwd):/opt/work/mnt $docker_name recall-init -c mnt/$configPath/config_recall.yaml
 
--- scala/friesian/scripts/run_online.sh
+++ scala/friesian/scripts/run_online.sh
@@ -4,14 +4,14 @@ set -e
 docker_name=intelanalytics/friesian-serving:2.2.0-SNAPSHOT
 
 cd ..
-onlinePath=src/main/resources/config
+configPath=src/main/resources/config
 
-docker run -itd --net host  --rm --name ranking -v $(pwd):/opt/work/mnt -e OMP_NUM_THREADS=1 $docker_name ranking -c mnt/$onlinePath/config_ranking.yaml
+docker run -itd --net host  --rm --name ranking -v $(pwd):/opt/work/mnt -e OMP_NUM_THREADS=1 $docker_name ranking -c mnt/$configPath/config_ranking.yaml
 
-docker run -itd --net host --rm --name feature -v $(pwd):/opt/work/mnt $docker_name feature -c mnt/$onlinePath/config_feature.yaml
+docker run -itd --net host --rm --name feature -v $(pwd):/opt/work/mnt $docker_name feature -c mnt/$configPath/config_feature.yaml
 
-docker run -itd --net host --rm --name feature_recall -v $(pwd):/opt/work/mnt $docker_name feature -c mnt/$onlinePath/config_feature_vec.yaml
+docker run -itd --net host --rm --name feature_recall -v $(pwd):/opt/work/mnt $docker_name feature -c mnt/$configPath/config_feature_vec.yaml
 
-docker run -itd --net host --rm --name recall -v $(pwd):/opt/work/mnt $docker_name recall -c mnt/$onlinePath/config_recall.yaml
+docker run -itd --net host --rm --name recall -v $(pwd):/opt/work/mnt $docker_name recall -c mnt/$configPath/config_recall.yaml
 
-docker run -itd --net host  --rm --name recommender_http -v $(pwd):/opt/work/mnt $docker_name recommender-http -c mnt/$onlinePath/config_recommender.yaml -p 8000
+docker run -itd --net host  --rm --name recommender_http -v $(pwd):/opt/work/mnt $docker_name recommender-http -c mnt/$configPath/config_recommender.yaml -p 8000
