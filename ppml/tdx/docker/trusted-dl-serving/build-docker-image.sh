5bc182312b6e9271179577ba7e921f17d68031aa
Le-Zheng <alettazheng@hotmail.com>
2023-04-12 14:24:46 +0800

    use intelanalytics/bigdl-ppml-trusted-dl-serving-gramine-ref:2.3.0-SNAPSHOT as copy

---
+++ ppml/tdx/docker/trusted-dl-serving/build-docker-image.sh
@@ -0,0 +1,28 @@
+export HTTP_PROXY_HOST=your_http_proxy_host
+export HTTP_PROXY_PORT=your_http_proxy_port
+export HTTPS_PROXY_HOST=your_https_proxy_host
+export HTTPS_PROXY_PORT=your_https_proxy_port
+export BASE_IMAGE_NAME=your_base_image_name
+export BASE_IMAGE_TAG=your_base_image_tag
+
+Proxy_Modified="sudo docker build \
+    --build-arg http_proxy=http://${HTTP_PROXY_HOST}:${HTTP_PROXY_PORT} \
+    --build-arg https_proxy=http://${HTTPS_PROXY_HOST}:${HTTPS_PROXY_PORT} \
+    --build-arg BASE_IMAGE_NAME=$BASE_IMAGE_NAME \
+    --build-arg BASE_IMAGE_TAG=$BASE_IMAGE_TAG \
+    --build-arg no_proxy=x.x.x.x \
+    -t intelanalytics/bigdl-ppml-trusted-dl-serving:2.3.0-SNAPSHOT -f ./Dockerfile ."
+
+No_Proxy_Modified="sudo docker build \
+    --build-arg no_proxy=x.x.x.x \
+    -t intelanalytics/bigdl-ppml-trusted-dl-serving:2.3.0-SNAPSHOT:2.3.0-SNAPSHOT -f ./Dockerfile ."
+
+if [[ "$HTTP_PROXY_HOST" == "your_http_proxy_host" ]] || [[ "$HTTP_PROXY_PORT" == "your_http_proxy_port" ]] || [[ "$HTTPS_PROXY_HOST" == "your_https_proxy_host" ]] || [[ "$HTTPS_PROXY_PORT" == "your_https_proxy_port" ]]
+then
+    echo "If your environment don't need to set proxy, please ignore this notice information; if your environment need to set proxy, please delete the image just created and modify the proxy in the script, then rerun this script."
+    $No_Proxy_Modified
+    echo "If your environment don't need to set proxy, please ignore this notice information; if your environment need to set proxy, please delete the image just created and modify the proxy in the script, then rerun this script."
+else
+    $Proxy_Modified
+fi
+
