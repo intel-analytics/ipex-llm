a18c0e82d8aedbbfda3d50854eaeaf613dc6e0a5
binbin Deng <108676127+plusbang@users.noreply.github.com>
2023-04-17 14:47:09 +0800

    Chronos: fix thread num error when use `get_context` and `predict_with_onnx` together (#8045)

--- python/chronos/test/bigdl/chronos/forecaster/test_nbeats_forecaster.py
+++ python/chronos/test/bigdl/chronos/forecaster/test_nbeats_forecaster.py
@@ -782,8 +782,11 @@ class TestChronosNBeatsForecaster(TestCase):
             assert forecaster.context_enabled == True
             current_thread = torch.get_num_threads()
             assert current_thread == num
-            for x, y in test_loader:
-                yhat = forecaster.predict(x.numpy())
+            yhat = forecaster.predict(test_loader)
+            yhat = forecaster.predict_with_onnx(test_loader)
+            yhat = forecaster.predict_with_openvino(test_loader)
+            current_thread = torch.get_num_threads()
+            assert current_thread == num
 
     @op_automl
     def test_nbeats_forecaster_tune(self):
