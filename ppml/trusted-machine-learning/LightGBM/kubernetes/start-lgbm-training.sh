cbc3a9922a514f991a8e6cd02a12f6c960fbaf27
liyao <1599208165@qq.com>
2023-04-24 13:11:00 +0800

    update start lgbm

--- ppml/trusted-machine-learning/LightGBM/kubernetes/start-lgbm-training.sh
+++ ppml/trusted-machine-learning/LightGBM/kubernetes/start-lgbm-training.sh
@@ -1,6 +1,6 @@
 # Configure the variables to be passed into the templates.
 set -x
-export imageName=intelanalytics/bigdl-ppml-trusted-machine-learning-gramine-reference:2.3.0-SNAPSHOT
+export imageName=intelanalytics/bigdl-ppml-trusted-machine-learning-gramine-reference:2.3.0
 export totalTrainerCount=2
 export trainerPort=12400
 export nfsMountPath=a_host_path_mounted_by_nfs_to_upload_data_before_training
