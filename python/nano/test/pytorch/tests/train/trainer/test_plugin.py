7f8ad1da177619f026127e9705fb8eed7505a200
hoshibara <xingyuan.li@intel.com>
2023-04-24 14:22:17 +0800

    * remove test code

--- python/nano/test/pytorch/tests/train/trainer/test_plugin.py
+++ python/nano/test/pytorch/tests/train/trainer/test_plugin.py
@@ -180,10 +180,6 @@ class TestPlugin(TestCase):
             metrics=[torchmetrics.F1Score('multiclass', num_classes=num_classes),
                      torchmetrics.Accuracy('multiclass', num_classes=num_classes)]
         )
-        import lightning.pytorch as pl
-        test_trainer = pl.Trainer(max_epochs=1)
-        test_trainer.fit(pl_model, self.data_loader, self.test_data_loader)
-
         trainer = Trainer(num_processes=2, distributed_backend="spawn",
                           process_group_backend='ccl', max_epochs=4)
         trainer.fit(pl_model, self.data_loader, self.test_data_loader)
