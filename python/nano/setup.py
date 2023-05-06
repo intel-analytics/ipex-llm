d46359113dbb76595228aa6ef1463d51753aa3cc
rnwang04 <ruonan1.wang@intel.com>
2023-05-06 13:05:26 +0000

    upgrade onnxruntime-extensions to 0.7.0

--- python/nano/setup.py
+++ python/nano/setup.py
@@ -167,10 +167,7 @@ def setup_package():
 
     inference_requires = ["onnx==1.12.0",
                           "onnxruntime==1.12.1",
-                          "onnxruntime-extensions==0.4.2; platform_system!='Darwin' and \
-                          python_full_version<'3.10.0'",
-                          "onnxruntime-extensions==0.7.0; platform_system!='Darwin' and \
-                          python_full_version>='3.10.0'",
+                          "onnxruntime-extensions==0.7.0; platform_system!='Darwin'",
                           "onnxruntime-extensions==0.3.1; (platform_machine=='x86_64' or platform_machine == 'AMD64') and \
                           platform_system=='Darwin'",
                           "openvino-dev==2022.3.0",
