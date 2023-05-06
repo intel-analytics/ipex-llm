b9fa3131da422586b8b8a890f3b336bf187be118
Hamster <niyinchen@sjtu.edu.cn>
2023-05-06 09:59:10 +0800

    fix(tf_resume_train_xshards.py): move backend choices

--- python/orca/tutorial/NCF/tf_resume_train_xshards.py
+++ python/orca/tutorial/NCF/tf_resume_train_xshards.py
@@ -28,6 +28,7 @@ from bigdl.orca.learn.tf2 import Estimator
 
 # Step 1: Init Orca Context
 args = parse_args("TensorFlow NCF Resume Training with Orca XShards")
+# TODO: fix spark backend for saving optimizer states
 init_orca(args.cluster_mode, extra_python_lib="process_xshards.py,utils.py")
 
 
