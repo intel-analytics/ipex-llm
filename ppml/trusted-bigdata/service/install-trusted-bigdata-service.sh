51ad121dd7aa50c11ff281aaf9cb7897cabf61c1
Uxito-Ada <414416158@qq.com>
2023-05-08 16:57:53 +0800

    refactor and add new components

--- ppml/trusted-bigdata/service/install-trusted-bigdata-service.sh
+++ ppml/trusted-bigdata/service/install-trusted-bigdata-service.sh
@@ -1,12 +1,16 @@
 # Configure the variables to be passed into the templates.
+export nfsServerIp=your_nfs_server_ip
+export nfsPath=a_nfs_shared_folder_path_on_the_server
 export imageName=bigdl-ppml-trusted-bigdata-gramine-reference:2.4.0-SNAPSHOT # or replace with custom image of user
-export k8sMasterURL=k8s://$(sudo kubectl cluster-info | grep 'https.*6443' -o -m 1)
-export teeMode=sgx # sgx or tdx
+export TEEMode=sgx # sgx or tdx
 
-if [ "$teeMode" = "sgx" ]; then
+
+export k8sMasterURL=k8s://$(sudo kubectl cluster-info | grep 'https.*6443' -o -m 1)
+
+if [ "$TEEMode" = "sgx" ]; then
   envsubst < bigdl-ppml-notebook-sgx.yaml | kubectl apply -f -
-elif [ "$teeMode" = "tdx" ]; then
+elif [ "$TEEMode" = "tdx" ]; then
   envsubst < bigdl-ppml-notebook-tdx.yaml | kubectl apply -f -
 else
-  echo "Wrong teeMode $teeMode! Expected sgx or tdx"
+  echo "Wrong teeMode $TEEMode! Expected sgx or tdx"
 fi
