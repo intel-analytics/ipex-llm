756af90b4bfae205c1324992b07bcea522095fb0
Uxito-Ada <414416158@qq.com>
2023-05-09 15:56:37 +0800

    fix

--- python/ppml/src/bigdl/ppml/utils/ppml_conf.py
+++ python/ppml/src/bigdl/ppml/utils/ppml_conf.py
@@ -14,48 +14,58 @@
 # limitations under the License.
 #
 
+import os
+from pyspark import SparkConf
+
 class PPMLConf:
-    def __init__(self, app_name, sgx_enabled = True):
-        self.spark_conf = SparkConf().setAppName(app_name)
-        self.init_spark_on_k8s_conf(sgx_enabled)
-        return self
+    def __init__(self, sgx_enabled = True):
+        self.spark_conf = self.init_spark_on_k8s_conf(SparkConf(), sgx_enabled)
 
     def set(self, key, value):
-        self.spark_conf.set(key, value)
+        self.spark_conf = self.spark_conf.set(key, value)
+        return self
+    
+    def setAppName(self, app_name):
+        self.spark_conf = self.spark_conf.setAppName(app_name)
+        return self
     
     def conf(self):
         return self.spark_conf
 
-    def init_spark_on_k8s_conf(self, sgx_enabled):
+    def init_spark_on_k8s_conf(self, spark_conf, sgx_enabled):
         master = os.getenv("RUNTIME_SPARK_MASTER")
         image = os.getenv("RUNTIME_K8S_SPARK_IMAGE")
-        driver_ip = os.getenv("LOCAL_IP")
+        driver_ip = os.getenv("RUNTIME_DRIVER_HOST")
         print("k8s master url is " + str(master))
         print("executor image is " + str(image))
         print("driver ip is " + str(driver_ip))
 
         secure_password = os.getenv("secure_password")
 
-        self.spark_conf.setMaster(master)
-            .set("spark.submit.deployMode", "client")
-            .set("spark.kubernetes.container.image", image)
-            .set("spark.driver.host", driver_ip)
-            .set("spark.kubernetes.driver.podTemplateFile", "/ppml/spark-driver-template.yaml")
-            .set("spark.kubernetes.executor.podTemplateFile", "/ppml/spark-executor-template.yaml")
-            .set("spark.kubernetes.authenticate.driver.serviceAccountName", "spark")
-            .set("spark.kubernetes.executor.deleteOnTermination", "false")
-            .set("spark.network.timeout", "10000000")
-            .set("spark.executor.heartbeatInterval", "10000000")
-            .set("spark.python.use.daemon", "false")
-            .set("spark.python.worker.reuse", "false")
-            .set("spark.authenticate", "true")
-            .set("spark.authenticate.secret", secure_password)
-            .set("spark.kubernetes.executor.secretKeyRef.SPARK_AUTHENTICATE_SECRET", "spark-secret:secret")
+        spark_conf = spark_conf\
+            .setMaster(master)\
+            .set("spark.submit.deployMode", "client")\
+            .set("spark.kubernetes.container.image", image)\
+            .set("spark.driver.host", driver_ip)\
+            .set("spark.kubernetes.driver.podTemplateFile", "/ppml/spark-driver-template.yaml")\
+            .set("spark.kubernetes.executor.podTemplateFile", "/ppml/spark-executor-template.yaml")\
+            .set("spark.kubernetes.authenticate.driver.serviceAccountName", "spark")\
+            .set("spark.kubernetes.executor.deleteOnTermination", "false")\
+            .set("spark.network.timeout", "10000000")\
+            .set("spark.executor.heartbeatInterval", "10000000")\
+            .set("spark.python.use.daemon", "false")\
+            .set("spark.python.worker.reuse", "false")\
+            .set("spark.authenticate", "true")\
+            .set("spark.authenticate.secret", secure_password)\
+            .set("spark.kubernetes.executor.secretKeyRef.SPARK_AUTHENTICATE_SECRET", "spark-secret:secret")\
             .set("spark.kubernetes.driver.secretKeyRef.SPARK_AUTHENTICATE_SECRET", "spark-secret:secret")
 
         if sgx_enabled:
-            self.spark_conf.set("spark.kubernetes.sgx.enabled", "true")
-                .set("spark.kubernetes.sgx.driver.mem", "64g")
-                .set("spark.kubernetes.sgx.driver.jvm.mem", "12g")
-                .set("spark.kubernetes.sgx.executor.mem", "64g")
+            spark_conf = spark_conf\
+                .set("spark.kubernetes.sgx.enabled", "true")\
+                .set("spark.kubernetes.sgx.driver.mem", "64g")\
+                .set("spark.kubernetes.sgx.driver.jvm.mem", "12g")\
+                .set("spark.kubernetes.sgx.executor.mem", "64g")\
                 .set("spark.kubernetes.sgx.executor.jvm.mem", "12g")
+        
+        return spark_conf
