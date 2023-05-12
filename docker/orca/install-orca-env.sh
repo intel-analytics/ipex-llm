ad319c42ec96a75a97c74b3f3119d85d1a428a2e
lalalapotter <potterguang101@gmail.com>
2023-05-12 09:56:35 +0800

    feat: add requirement.

--- docker/orca/install-orca-env.sh
+++ docker/orca/install-orca-env.sh
@@ -1,6 +1,8 @@
 wget https://raw.githubusercontent.com/intel-analytics/BigDL/main/python/requirements/orca/requirements_automl.txt
+wget https://raw.githubusercontent.com/lalalapotter/BigDL/main/python/requirements/orca/requirements.txt
 # python version passed as the first argument
 # default version: 3.9
 conda create -y -n bigdl python=$1
 source activate bigdl
+pip install --no-cache-dir -r requirements.txt
 pip install --no-cache-dir -r requirements_automl.txt
