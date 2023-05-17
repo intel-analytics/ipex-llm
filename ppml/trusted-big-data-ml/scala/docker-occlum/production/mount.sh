a8f9de859a37bf32bc7ee9a4c3a2e74301324c8f
Wang Jian <61138589+hzjane@users.noreply.github.com>
2023-05-17 09:36:26 +0800

    [PPML] Update mount spark.properties (#8203)

--- ppml/trusted-big-data-ml/scala/docker-occlum/production/mount.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/production/mount.sh
@@ -2,9 +2,10 @@
 #To avoid soft-link error, need to mkdir copy dir
 [[ -d /opt/spark/conf/..data ]] || mkdir /opt/spark/conf/..data
 [[ -d /opt/spark/conf-copy ]] || mkdir /opt/spark/conf-copy
-cp /opt/spark/conf/..data/* /opt/spark/conf-copy
 # add spark conf
-cp -r /opt/spark/conf/* /opt/spark/conf-copy/
+cp -rf /opt/spark/conf/* /opt/spark/conf-copy/
+rm -rf /opt/spark/conf-copy/spark.properties
+cp /opt/spark/conf/..data/* /opt/spark/conf-copy
 
 [[ -d /opt/spark/pod-template/..data ]] || mkdir /opt/spark/pod-template/..data
 [[ -d /opt/spark/pod-template-copy ]] || mkdir /opt/spark/pod-template-copy
