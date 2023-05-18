ecd95f64af749ffbd4578933ee3e391f9daef75b
hzjane <a1015616934@qq.com>
2023-05-18 16:35:42 +0800

    update occlum encrypt bash

--- ppml/trusted-big-data-ml/scala/docker-occlum/ehsm_entry.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/ehsm_entry.sh
@@ -60,7 +60,8 @@ elif [ "$action" = "encrypt" ]; then
                     --inputDataSourcePath $input_path \
                     --outputDataSinkPath /opt/occlum_spark/data/encryptEhsm/ \
                     --cryptoMode aes/cbc/pkcs5padding \
-                    --dataSourceType csv
+                    --dataSourceType csv \
+                    --header true
 
 	elif [ "$KMS_TYPE" = "simple" ]; then
 		appid=123456654321
@@ -83,7 +84,8 @@ elif [ "$action" = "encrypt" ]; then
                     --inputDataSourcePath $input_path \
                     --outputDataSinkPath /opt/occlum_spark/data/encryptSimple/ \
                     --cryptoMode aes/cbc/pkcs5padding \
-                    --dataSourceType csv
+                    --dataSourceType csv \
+                    --header true
 	else
 		echo "Wrong KMS_TYPE! KMS_TYPE can be (1) ehsm, (2) simple "
                 return -1
