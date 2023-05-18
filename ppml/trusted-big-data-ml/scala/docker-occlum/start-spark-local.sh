1133760fbe03d8b9eab0464f37ed7d77d43afca0
Wang Jian <61138589+hzjane@users.noreply.github.com>
2023-05-18 09:33:47 +0800

    [PPML] Add client and cluster spark pi mode in docker container (#8200)
    * add copy bom
    
    * add kubeconfig_path in occlum
    
    * add_run_client_bash
    
    * update and add mount path
    
    * update
    
    * update
    
    * add readme
    
    * add cluster mode in container
    
    * fix not existed bug
    
    * add cluster readme
    
    * update readme
    
    * add empty line
    
    * delete

--- ppml/trusted-big-data-ml/scala/docker-occlum/start-spark-local.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/start-spark-local.sh
@@ -10,6 +10,8 @@ sudo docker run -it \
 	--device=/dev/sgx/provision \
 	-v /var/run/aesmd:/var/run/aesmd \
 	-v data:/opt/occlum_spark/data \
+	-e kubernetes_master_url=${kubernetes_master_url} \
+	-e LOCAL_IP=${LOCAL_IP} \
 	-e SGX_MEM_SIZE=20GB \
 	-e SGX_THREAD=1024 \
 	-e SGX_HEAP=512MB \
