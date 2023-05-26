8270ed623d15c92e9f5910deac5239e6d7a3c5d9
rnwang04 <ruonan1.wang@intel.com>
2023-05-26 16:13:01 +0800

    update based on comment

--- python/llm/src/bigdl/llm/ggml/quantize.py
+++ python/llm/src/bigdl/llm/ggml/quantize.py
@@ -41,46 +41,46 @@ _quantize_type = {"llama": _llama_quantize_type,
 _valid_types = set(list(_llama_quantize_type.keys()) + list(_bloomz_quantize_type.keys()) + list(_gptneox_quantize_type.keys()))
 
 
-def quantize(model_path: str, quantize_path: str=None, quantize_type: str='q4_0', model_family: str = 'llama'):
+def quantize(input_path: str, output_path: str=None, model_family: str = 'llama', dtype: str='q4_0'):
     """
     Quantize ggml file to lower precision.
 
-    :param model_path: Path of input ggml file, for example `./ggml-model-f16.bin`.
-    :param quantize_path: Save path of output quantized model. Default to `None`. 
+    :param input_path: Path of input ggml file, for example `./ggml-model-f16.bin`.
+    :param output_path: Save path of output quantized model. Default to `None`. 
             If you don't specify this parameter, quantized model will be saved in
             the same directory as the input and just replace precision with quantize_type
             like `./ggml-model-q4_0.bin`.
-    :param quantize_type: Quantization method which differs in the resulting model disk size and
+    :param model_family: Which model family your input model belongs to. Default to `llama`.
+            Now only `llama`/`bloomz`/`gptneox` are supported.
+    :param dtype: Quantization method which differs in the resulting model disk size and
             inference speed. Defalut to `q4_0`. Difference model family may support different types,
             now the supported list is:
             llama : "q4_0", "q4_1", "q4_2"
             bloomz : "q4_0", "q4_1"
             gptneox : "q4_0", "q4_1", "q4_2", "q5_0", "q5_1", "q8_0"
-    :param model_family: Which model family your input model belongs to. Default to `llama`.
-            Now only `llama`/`bloomz`/`gptneox` are supported.
     """
     invalidInputError(model_family in ['llama', 'bloomz', 'gptneox'],
                       "Now we only support quantization of model family('llama', 'bloomz', 'gptneox')",
                       "{} is not in the list.".format(model_family))
-    invalidInputError(os.path.isfile(model_path),
-                      "The file {} was not found".format(model_path))
+    invalidInputError(os.path.isfile(input_path),
+                      "The file {} was not found".format(input_path))
     # TODO : multi input model path
-    if quantize_path is None:
-        quantize_path = model_path.replace("f16", quantize_type)
+    if output_path is None:
+        output_path = input_path.replace("f16", dtype)
     # convert quantize type str into corresponding int value
     quantize_type_map = _quantize_type[model_family]
-    invalidInputError(quantize_type in quantize_type_map,
+    invalidInputError(dtype in quantize_type_map,
                       "{0} model just accept {1} now, but you pass in {2}.".format(
                         model_family,
                         list(quantize_type_map.keys()),
-                        quantize_type))
-    quantize_type = quantize_type_map[quantize_type]
+                        dtype))
+    quantize_type = quantize_type_map[dtype]
     quantize_args = "{0}/bin/quantize-{1} {2} {3} {4}".format(bin_dirname,
                                                               model_family,
-                                                              model_path,
-                                                              quantize_path,
+                                                              input_path,
+                                                              output_path,
                                                               str(quantize_type))
     p = subprocess.Popen(quantize_args.split())
     p.communicate()
     invalidInputError(not p.returncode,
-                      "Fail to quantize {}.".format(str(model_path)))
+                      "Fail to quantize {}.".format(str(input_path)))
