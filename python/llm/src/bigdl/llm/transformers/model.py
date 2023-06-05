e5db4f1898bad949393292604455b2acd95c8dd4
Yuwen Hu <yuwen.hu@intel.com>
2023-06-05 17:13:00 +0800

    Init commit for bigdl.llm.transformers.AutoModelForCausalLM

---
+++ python/llm/src/bigdl/llm/transformers/model.py
@@ -0,0 +1,84 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This would makes sure Python is aware there is more than one sub-package within bigdl,
+# physically located elsewhere.
+# Otherwise there would be module not found error in non-pip's setting as Python would
+# only search the first bigdl package and end up finding only one sub-package.
+
+import os
+from bigdl.llm.utils.common import invalidInputError
+from bigdl.llm.ggml import convert_model
+from bigdl.llm.ggml.model.llama import Llama
+from bigdl.llm.ggml.model.gptneox import Gptneox
+from bigdl.llm.ggml.model.bloom import Bloom
+
+
+class AutoModelForCausalLM:
+    """
+    A generic model class that mimics the behavior of
+    ``transformers.AutoModelForCausalLM.from_pretrained`` API
+    """
+
+    @classmethod
+    def from_pretrained(cls,
+                        pretrained_model_name_or_path: str,
+                        model_family: str = 'llama',
+                        dtype: str = 'int4',
+                        cache_dir: str = None,
+                        **kwargs):
+        """
+        :param pretrained_model_name_or_path: We support 2 kinds of pretrained model checkpoint
+
+               1. path for huggingface checkpoint that are directly pulled from hugginface hub.
+                  This should be a dir path that contains: weight bin, tokenizer config,
+                  tokenizer.model (required for llama) and added_tokens.json (if applied).
+                  For lora fine tuned model, the path should be pointed to a merged weight.
+               2. path for converted ggml binary checkpoint. The checkpoint should be converted by
+                  ``bigdl.llm.ggml.convert_model``.
+
+        :param model_family: the model family of the pretrained checkpoint.
+               Currently we support ``"llama"``, ``"bloom"``, ``"gptneox"``.
+        :param dtype: (optional) the data type for weight. Currently we only support ``"int4"``
+        :param cache_dir: (optional) this parameter will only be used when
+               ``pretrained_model_name_or_path`` is a hugginface checkpoint.
+               It indicates the saving path for the converted low precision model.
+        :param **kwargs: keyword arguments which will be passed to the model instance
+
+        :return: a model instance
+        """
+        invalidInputError(model_family in ['llama', 'gptneox', 'bloom'],
+                          "Now we only support model family: 'llama', 'gptneox', 'bloom'" \
+                          "{} is not in the list.".format(model_family))
+        invalidInputError(dtype == 'int4',
+                          "Now we only support int4 as date type for weight")
+        invalidInputError(os.path.exists(pretrained_model_name_or_path),
+                          "The path {} was not found".format(pretrained_model_name_or_path))
+
+        ggml_model_path = pretrained_model_name_or_path
+        if not os.path.isfile(pretrained_model_name_or_path):
+            # huggingface checkpoint
+            ggml_model_path = convert_model(input_path=pretrained_model_name_or_path,
+                                            output_path=cache_dir,
+                                            model_family=model_family,
+                                            dtype=dtype)
+        
+        if model_family == 'llama':
+            return Llama(model_path=ggml_model_path, **kwargs)
+        elif model_family == 'gptneox':
+            return Gptneox(model_path=ggml_model_path, **kwargs)
+        elif model_family == 'bloom':
+            return Bloom(model_path=ggml_model_path, **kwargs)
