36e30d447bebe8f9366392f9d06caa1549446991
Uxito-Ada <414416158@qq.com>
2023-06-14 14:04:42 +0800

    torchserve k8s service

---
+++ ppml/trusted-dl-serving/base/make-config.sh
@@ -0,0 +1,9 @@
+echo "inference_address=http://127.0.0.1:$INFERENCE_PORT" >> /ppml/config.yaml
+echo "management_address=http://127.0.0.1:$MANAGEMENT_PORT" >> /ppml/config.yaml
+echo "metrics_address=http://127.0.0.1:$METRICS_PORT" >> /ppml/config.yaml
+echo "grpc_inference_port=7070" >> /ppml.config.yaml
+echo "grpc_management_port=7071" >> /ppml.config.yaml
+echo "model_store=/tmp/model/torchserve" >> /ppml.config.yaml
+echo "load_models=$MODEL_NAME.mar" >> /ppml.config.yaml
+echo "enable_metrics_api=true" >> /ppml.config.yaml
+echo "models={ \"$MODEL_NAME\": { \"1.0\": {\"defaultVersion\": true,\"marName\": \"$MODEL_NAME\",\"workers\": $BACKEND_NUM,\"batchSize\": 1,\"maxBatchDelay\": 100,\"responseTimeout\": 1200}}}" >> /ppml/config.yaml
---
+++ ppml/trusted-dl-serving/base/torchserve-backend-entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+port=$BACKEND_PORT
+SGX_ENABLED=$SGX_ENABLED
+ATTESTATION=$ATTESTATION
+local_pod_ip=$( hostname -I | awk '{print $1}' )
+
+cd /ppml || exit
+# Set PCCS conf
+if [ "$PCCS_URL" != "" ] ; then
+    echo 'PCCS_URL='${PCCS_URL}'/sgx/certification/v3/' > /etc/sgx_default_qcnl.conf
+    echo 'USE_SECURE_CERT=FALSE' >> /etc/sgx_default_qcnl.conf
+fi
+
+if [[ $SGX_ENABLED == "false" ]]; then
+    if [ "$ATTESTATION" = "true" ]; then
+        rm /ppml/temp_command_file || true
+        bash attestation.sh
+        bash temp_command_file
+    fi
+    /usr/bin/python3 /usr/local/lib/python3.9/dist-packages/ts/model_service_worker.py --sock-type tcp --port $port --host $local_pod_ip --metrics-config /ppml/metrics.yaml
+else
+    export sgx_command="/usr/bin/python3 /usr/local/lib/python3.9/dist-packages/ts/model_service_worker.py --sock-type tcp --port $port --host $local_pod_ip --metrics-config /ppml/metrics.yaml"
+    if [ "$ATTESTATION" = "true" ]; then
+          # Also consider ENCRYPTEDFSD condition
+          rm /ppml/temp_command_file || true
+          bash attestation.sh
+          if [ "$ENCRYPTED_FSD" == "true" ]; then
+            echo "[INFO] Distributed encrypted file system is enabled"
+            bash encrypted-fsd.sh
+          fi
+          echo $sgx_command >>temp_command_file
+          export sgx_command="bash temp_command_file"
+    else
+          # ATTESTATION is false
+          if [ "$ENCRYPTED_FSD" == "true" ]; then
+            # ATTESTATION false, encrypted-fsd true
+            rm /ppml/temp_command_file || true
+            echo "[INFO] Distributed encrypted file system is enabled"
+            bash encrypted-fsd.sh
+            echo $sgx_command >>temp_command_file
+            export sgx_command="bash temp_command_file"
+          fi
+    fi
+    gramine-sgx bash 2>&1 | tee backend-sgx.log
+    rm /ppml/temp_command_file || true
+fi
+
