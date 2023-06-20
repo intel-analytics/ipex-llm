7dc1121dc232be6de18e5de72944c48efd7daf20
cyita <yitastudy@gmail.com>
2023-06-20 11:21:36 +0800

    revert

--- python/llm/test/run-llm-windows-tests.sh
+++ python/llm/test/run-llm-windows-tests.sh
@@ -16,7 +16,7 @@ echo "# Download the models"
 start=$(date "+%s")
 
 echo ${ANALYTICS_ZOO_ROOT}
-python ${ANALYTICS_ZOO_ROOT}/python/llm/test/windows/download_from_huggingface.py
+python ${ANALYTICS_ZOO_ROOT}/python/llm/test/win/download_from_huggingface.py
 
 now=$(date "+%s")
 time=$((now-start))
@@ -25,7 +25,7 @@ echo "Models downloaded in:$time seconds"
 echo "# Start testing convert model"
 start=$(date "+%s")
 
-python -m pytest -s ${ANALYTICS_ZOO_ROOT}/python/llm/test/convert/test_convert_model.py -k 'test_convert_gptneox'
+python -m pytest -s ${ANALYTICS_ZOO_ROOT}/python/llm/test/convert/test_convert_model.py -k 'test_convert_bloom'
 
 now=$(date "+%s")
 time=$((now-start))
@@ -37,7 +37,7 @@ echo "Time used:$time seconds"
 echo "# Start testing inference"
 start=$(date "+%s")
 
-python -m pytest -s ${ANALYTICS_ZOO_ROOT}/python/llm/test/inference/test_call_models.py -k 'test_gptneox_completion_success or test_gptneox_completion_with_stream_success'
+python -m pytest -s ${ANALYTICS_ZOO_ROOT}/python/llm/test/inference/test_call_models.py -k 'test_bloom_completion_success or test_bloom_completion_with_stream_success'
 
 now=$(date "+%s")
 time=$((now-start))
