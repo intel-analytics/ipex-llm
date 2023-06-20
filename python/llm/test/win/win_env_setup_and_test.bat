58c1226a37dcdcf778063f444a51ed0bc80b2889
cyita <yitastudy@gmail.com>
2023-06-20 09:46:39 +0800

    update pytests

--- python/llm/test/win/win_env_setup_and_test.bat
+++ python/llm/test/win/win_env_setup_and_test.bat
@@ -29,7 +29,7 @@ git pull
 pip uninstall bigdl-llm -y
 pip uninstall numpy torch transformers sentencepiece accelerate -y
 pip install numpy torch transformers sentencepiece accelerate
-pip install requests
+pip install requests pytest
 cd python\llm
 @REM pip install .[all] --use-pep517
 python setup.py clean --all install
@@ -37,14 +37,26 @@ python setup.py clean --all install
 
 @REM Run pytest
 mkdir converted_models
-python .\test\win\test_llama.py
+set BLOOM_ORIGIN_PATH=%base_dir%\models\bloomz-7b1
+set LLAMA_ORIGIN_PATH=%base_dir%\models\gpt4all-7b-hf
+set GPTNEOX_ORIGIN_PATH=%base_dir%\models\gptneox-7b-redpajama-bf16
+set INT4_CKPT_DIR=%base_dir%\converted_models
+set LLAMA_INT4_CKPT_PATH=%INT4_CKPT_DIR%\bigdl_llm_llama_q4_0.bin
+set GPTNEOX_INT4_CKPT_PATH=%INT4_CKPT_DIR%\bigdl_llm_gptneox_q4_0.bin
+set BLOOM_INT4_CKPT_PATH=%INT4_CKPT_DIR%/bigdl_llm_bloom_q4_0.bin
+
+echo "Running the convert models tests..."
+python -m pytest -s .\test\convert\test_convert_model.py
+
+echo "Running the inference models tests..."
+python -m pytest -s .\test\inference\test_call_models.py
 
 @REM Clean up
 pip uninstall bigdl-llm -y
 pip uninstall numpy torch transformers sentencepiece accelerate -y
 echo "Removing the quantized models and libs..."
-rmdir /s /q converted_models
-rmdir /s /q C:\Users\obe\bigdl-llm-test\BigDL\python\llm\src\bigdl\llm\libs
+rmdir /s /q %INT4_CKPT_DIR%
+rmdir /s /q %base_dir%\BigDL\python\llm\src\bigdl\llm\libs
 
 @REM Upload the log file
 echo "Uploading the test logs to ftp..."
