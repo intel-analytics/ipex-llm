304ec6919b65ce20793015c720049aeca9bdcc8c
Heyang Sun <60865256+Uxito-Ada@users.noreply.github.com>
2023-06-20 13:50:04 +0800

    torchserve k8s service (#8327)
    * torchserve k8s service
    
    * Update frontend_pod.yaml
    
    * Update backend_deployment.yaml
    
    * expose frontend api to backend
    
    * add model_name param in backend
    
    * refine
    
    * refine
    
    * fix after tests
    
    * Update frontend-entrypoint.sh

--- ppml/trusted-dl-serving/base/ppml/torchserve/frontend-entrypoint.sh
+++ ppml/trusted-dl-serving/base/ppml/torchserve/frontend-entrypoint.sh
@@ -1,43 +1,26 @@
 #!/bin/bash
-configFile=""
-core=""
 SGX_ENABLED=$SGX_ENABLED
 ATTESTATION=$ATTESTATION
-TEMP_CMD_PATH=/ppml/tmp/torchserve-frontend
-TEMP_CMD_FILE=$TEMP_CMD_PATH/temp_command_file
-mkdir -p $TEMP_CMD_PATH
-while getopts ":f:c:XA" opt
-do
-    case $opt in
-        c)
-            configFile=$OPTARG
-            ;;
-        f)
-            core=$OPTARG
-            ;;
-        X)
-            SGX_ENABLED="true"
-            ;;
-        A)
-            ATTESTATION="true"
-            ;;
-        *)
-            echo "Unknown argument passed in: $opt"
-            exit 1
-            ;;
-    esac
-done
+
+export config_file_path="/ppml/config.yaml"
+bash /ppml/torchserve/make-config.sh
+
+# Set PCCS conf
+if [ "$PCCS_URL" != "" ] ; then
+    echo 'PCCS_URL='${PCCS_URL}'/sgx/certification/v3/' > /etc/sgx_default_qcnl.conf
+    echo 'USE_SECURE_CERT=FALSE' >> /etc/sgx_default_qcnl.conf
+fi
 
 cd /ppml || exit
 
 if [[ $SGX_ENABLED == "false" ]]; then
     if [ "$ATTESTATION" = "true" ]; then
-        cd $TEMP_CMD_PATH
-        rm $TEMP_CMD_FILE || true
-        bash /ppml/attestation.sh
-        bash $TEMP_CMD_FILE
+        rm /ppml/temp_command_file || true
+        bash attestation.sh
+        bash temp_command_file
     fi
-    taskset -c "$core" /opt/jdk11/bin/java \
+    /opt/jdk11/bin/java \
+            -DBackends_IP=$backends_IP \
             -Dmodel_server_home=/usr/local/lib/python3.9/dist-packages \
             -cp .:/ppml/torchserve/* \
             -Xmx1g \
@@ -48,10 +31,11 @@ if [[ $SGX_ENABLED == "false" ]]; then
             -XX:MaxDirectMemorySize=128m \
             org.pytorch.serve.ModelServer \
             --python /usr/bin/python3 \
-            -f "$configFile" \
+            -f "$config_file_path" \
             -ncs
 else
     export sgx_command="/opt/jdk11/bin/java \
+            -DBackends_IP=$backends_IP \
             -Dmodel_server_home=/usr/local/lib/python3.9/dist-packages \
             -cp .:/ppml/torchserve/* \
             -Xmx1g \
@@ -62,33 +46,30 @@ else
             -XX:MaxDirectMemorySize=128m \
             org.pytorch.serve.ModelServer \
             --python /usr/bin/python3 \
-            -f $configFile \
+            -f $config_file_path \
             -ncs"
     if [ "$ATTESTATION" = "true" ]; then
           # Also consider ENCRYPTEDFSD condition
-          cd $TEMP_CMD_PATH
-          rm $TEMP_CMD_FILE || true
-          bash /ppml/attestation.sh
+          rm /ppml/temp_command_file || true
+          bash attestation.sh
           if [ "$ENCRYPTED_FSD" == "true" ]; then
             echo "[INFO] Distributed encrypted file system is enabled"
-            bash /ppml/encrypted-fsd.sh
+            bash encrypted-fsd.sh
           fi
-          echo $sgx_command >>$TEMP_CMD_FILE
-          export sgx_command="bash $TEMP_CMD_FILE"
+          echo $sgx_command >>temp_command_file
+          export sgx_command="bash temp_command_file"
     else
           # ATTESTATION is false
           if [ "$ENCRYPTED_FSD" == "true" ]; then
             # ATTESTATION false, encrypted-fsd true
-            cd $TEMP_CMD_PATH
-            rm $TEMP_CMD_FILE || true
+            rm /ppml/temp_command_file || true
             echo "[INFO] Distributed encrypted file system is enabled"
-            bash /ppml/encrypted-fsd.sh
-            echo $sgx_command >>$TEMP_CMD_FILE
-            export sgx_command="bash $TEMP_CMD_FILE"
+            bash encrypted-fsd.sh
+            echo $sgx_command >>temp_command_file
+            export sgx_command="bash temp_command_file"
           fi
     fi
-    cd /ppml || exit
-    taskset -c "$core" gramine-sgx bash 2>&1 | tee frontend-sgx.log
-    rm $TEMP_CMD_FILE || true
+    gramine-sgx bash 2>&1 | tee frontend-sgx.log
+    rm /ppml/temp_command_file || true
 fi
 
