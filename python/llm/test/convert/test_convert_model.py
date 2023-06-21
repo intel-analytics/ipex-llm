ce547d11e81dafaf99663ddacf6de2a4d01b5c7a
rnwang04 <ruonan1.wang@intel.com>
2023-06-21 13:14:21 +0000

    first commit to add path

--- python/llm/test/convert/test_convert_model.py
+++ python/llm/test/convert/test_convert_model.py
@@ -25,6 +25,7 @@ from bigdl.llm import llm_convert
 llama_model_path = os.environ.get('LLAMA_ORIGIN_PATH')
 gptneox_model_path = os.environ.get('GPTNEOX_ORIGIN_PATH')
 bloom_model_path = os.environ.get('BLOOM_ORIGIN_PATH')
+starcoder_model_path = os.environ.get('STARCODER_ORIGIN_PATH')
 output_dir = os.environ.get('INT4_CKPT_DIR')
 
 class TestConvertModel(TestCase):
@@ -52,6 +53,15 @@ class TestConvertModel(TestCase):
                                            model_format="pth",
                                            outtype='int4')
         assert os.path.isfile(converted_model_path)
+    
+    def test_convert_starcoder(self):
+        converted_model_path = llm_convert(model=starcoder_model_path,
+                                           outfile=output_dir,
+                                           model_family='starcoder',
+                                           model_format="pth",
+                                           outtype='int4')
+        assert os.path.isfile(converted_model_path)
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
