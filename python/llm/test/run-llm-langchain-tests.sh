ff6d7a3b31e621cbf44f2d064a0671056c8b6c4a
binbin <binbin1.deng@intel.com>
2023-06-29 13:58:04 +0800

    try add langchain test

--- python/llm/test/run-llm-langchain-tests.sh
+++ python/llm/test/run-llm-langchain-tests.sh
@@ -5,10 +5,11 @@ export LLM_HOME=${ANALYTICS_ZOO_ROOT}/python/llm/src
 export LLM_INFERENCE_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/llm/test/langchain
 set -e
 
-echo "# Start testing inference"
+echo "# Start testing langchain"
 start=$(date "+%s")
 
-python -m pytest -s ${LLM_INFERENCE_TEST_DIR}
+echo $BLOOM_INT4_CKPT_PATH
+python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k 'test_langchain_llm_bloom'
 
 now=$(date "+%s")
 time=$((now-start))
