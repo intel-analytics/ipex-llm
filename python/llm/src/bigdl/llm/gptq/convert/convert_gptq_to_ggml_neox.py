2dcd3663d6823857c4d6ae358e39f03542a06d25
qiuxin2012 <qiuxin2012cs@gmail.com>
2023-07-06 08:24:16 +0800

    add neox

--- python/llm/src/bigdl/llm/gptq/convert/convert_gptq_to_ggml_neox.py
+++ python/llm/src/bigdl/llm/gptq/convert/convert_gptq_to_ggml_neox.py
@@ -37,7 +37,7 @@ def write_header(fout, shape, dst_name, ftype_cur):
     fout.write(struct.pack("iii", len(shape), len(sname), ftype_cur))
     fout.write(struct.pack("i" * len(shape), *shape[::-1]))
     fout.write(sname)
-    # fout.seek((fout.tell() + 31) & -32)   # not in bloom
+    fout.seek((fout.tell() + 31) & -32)   # not in bloom
 
 
 def convert_non_q4(src_name, dst_name, model, fout):
@@ -179,12 +179,6 @@ def convert_q4(src_name, dst_name, model, fout, n_head, permute=False):
     # Split into groups of 8 columns (i.e. 64 columns of quantized data):
     # TODO: Only support act-order=false
     expanded = expandToInt4(qweight.reshape([qweight.shape[0], qweight.shape[1] // 8, 8]))
-    if permute:
-        qkv = torch.tensor(np.asarray(expanded, dtype=np.float32))\
-              .reshape([expanded.shape[0], expanded.shape[1] * expanded.shape[2]])
-        q, k, v = qkv.reshape(n_head, 3, -1).unbind(1)
-        expanded = np.asarray(torch.cat([q, k, v], dim=0).reshape(expanded.shape).numpy(), expanded.dtype)
-
     grouped = to_ggml_int16(expanded)
 
     # Repeat addends and scales:
@@ -199,15 +193,15 @@ def convert_q4(src_name, dst_name, model, fout, n_head, permute=False):
 
     blob = np.concatenate([scales_rep, addends_rep, grouped], axis=2, casting='no')
 
-    # if permute:
-    #     # Permute some rows to undo the permutation done by convert_llama_weights_to_hf.py.
-    #     # This can be done after the above conversion because it doesn't affect column order/layout.
-    #     blob = (blob.reshape(n_head, 2, shape[0] // n_head // 2, *blob.shape[1:])
-    #             .swapaxes(1, 2)
-    #             .reshape(blob.shape))
+    if permute:
+        # Permute some rows to undo the permutation done by convert_llama_weights_to_hf.py.
+        # This can be done after the above conversion because it doesn't affect column order/layout.
+        blob = (blob.reshape(n_head, 2, shape[0] // n_head // 2, *blob.shape[1:])
+                .swapaxes(1, 2)
+                .reshape(blob.shape))
 
     # header
-    write_he  ader(fout, shape, dst_name, ftype)  # ftype = Q4_1
+    write_header(fout, shape, dst_name, ftype)  # ftype = Q4_1
 
     # data
     blob.tofile(fout)
@@ -237,18 +231,19 @@ def convert_gptq2ggml(model_path, output_path, tokenizer_path=None):
 
     embedding_layer = model[next(iter(model))]
     n_vocab, n_embd = embedding_layer.shape
-    # layer_re = r'model\.layers\.([0-9]+)'
-    layer_re = r'transformer\.h\.([0-9]+)'
+    layer_re = r'gpt_neox\.layers\.([0-9]+)'
+    # layer_re = r'transformer\.h\.([0-9]+)'
     # n_vocab, n_embd = model['transformer.word_embeddings.weight'].shape
     # layer_re = r'transformer\.h\.([0-9]+)'
     n_layer = 1 + max(int(re.match(layer_re, name).group(1)) for name in model
                       if re.match(layer_re, name))
+    # n_layer = 1  # TODO: delete this, debug
 
     # hardcoded:
     # n_mult = 256
     # n_head = {32: 32, 40: 40, 60: 52, 80: 64}[n_layer]
     n_mult = 1
-    n_head = 32
+    n_head = 64
 
     if not tokenizer_path:
         tokenizer_path = os.path.join(model_path, "tokenizer.model")
@@ -270,78 +265,107 @@ def convert_gptq2ggml(model_path, output_path, tokenizer_path=None):
     if vocab_size < n_vocab:
         print(f"tokenizer's vocab size {vocab_size} is the smaller than embedding size {n_vocab}, "
               f"will delete the useless embedding layer.")
-        model["transformer.word_embeddings.weight"] = model["transformer.word_embeddings.weight"][0:vocab_size, :]
-        model["lm_head.weight"] = model["lm_head.weight"][0:vocab_size, :]
+        model["gpt_neox.embed_in.weight"] = model["gpt_neox.embed_in.weight"][0:vocab_size, :]
+        model["embed_out.weight"] = model["embed_out.weight"][0:vocab_size, :]
 
     fout = open(output_path, "wb")
 
     # fout.write(b"ggjt"[::-1])  # magic: ggmf in hex
     # fout.write(b"ggjt"[::-1])  # magic: ggmf in hex
-    values = [0x67676d6c,  # file version
+    max_position_embeddings = 2048
+    hidden_size = 6144
+    num_attention_heads = 64
+    num_hidden_layers = n_layer
+    rotary_pct = 0.25
+    use_parallel_residual = True
+    n_ctx = 512
+
+    ggml_file_magic = 0x67676a74  # 0x67676d6c is unversioned
+    # ggml_file_magic = b"ggjt"[::-1]
+    # fout.write(b"ggjt"[::-1])  # magic: ggmf in hex
+    # ggml_file_version = 3  # v1
+    ggml_file_version = 1  # v1
+    values = [ggml_file_magic,
+              ggml_file_version,  # file version
               vocab_size,
-              n_embd,
-              n_mult,
-              n_head,
-              n_layer,
+              n_ctx,
+              hidden_size,
+              num_attention_heads,
+              num_hidden_layers,
+              int((hidden_size / num_attention_heads
+                   ) * rotary_pct),
+              int(use_parallel_residual),
               3]
     # n_embd // n_head,  # rot (obsolete)
     fout.write(struct.pack("i" * len(values), *values))
 
+    vocab = tokenizer.vocab
+    id2token = {v: k for k, v in vocab.items()}
+    print(dir(vocab))
     for i in range(vocab_size):
-        if autoToken:
-            text = tokenizer.decode([i]).encode('utf-8')
-        elif tokenizer.is_unknown(i):
-            text = " \u2047 ".encode("utf-8")
-        elif tokenizer.is_control(i):
-            text = b""
-        elif tokenizer.is_byte(i):
-            piece = tokenizer.id_to_piece(i)
-            if len(piece) != 6:
-                print(f"Invalid token: {piece}")
-                sys.exit(1)
-            byte_value = int(piece[3:-1], 16)
-            text = struct.pack("B", byte_value)
+        if i in id2token:
+            text = id2token[i].encode('utf-8')
         else:
-            text = tokenizer.id_to_piece(i).replace("\u2581", " ").encode("utf-8")
+            text = tokenizer.decode([i]).encode('utf-8')
+        print(i, text)
         fout.write(struct.pack("i", len(text)))
         fout.write(text)
-        if not autoToken:
-            fout.write(struct.pack("f", tokenizer.get_score(i)))
 
-    convert_fp_to_q4("transformer.word_embeddings.weight", "tok_embeddings.weight", model, fout)
-    convert_non_q4("transformer.word_embeddings_layernorm.weight", "norm.weight", model, fout)
-    convert_non_q4("transformer.word_embeddings_layernorm.bias", "norm.bias", model, fout)
+    #for i in range(vocab_size):
+    #    if autoToken:
+    #        text = tokenizer.decode([i]).encode('utf-8')
+    #    elif tokenizer.is_unknown(i):
+    #        text = " \u2047 ".encode("utf-8")
+    #    elif tokenizer.is_control(i):
+    #        text = b""
+    #    elif tokenizer.is_byte(i):
+    #        piece = tokenizer.id_to_piece(i)
+    #        if len(piece) != 6:
+    #            print(f"Invalid token: {piece}")
+    #            sys.exit(1)
+    #        byte_value = int(piece[3:-1], 16)
+    #        text = struct.pack("B", byte_value)
+    #    else:
+    #        text = tokenizer.id_to_piece(i).replace("\u2581", " ").encode("utf-8")
+    #    fout.write(struct.pack("i", len(text)))
+    #    fout.write(text)
+    #    if not autoToken:
+    #        fout.write(struct.pack("f", tokenizer.get_score(i)))
+
+    convert_fp_to_q4("gpt_neox.embed_in.weight", "gpt_neox.embed_in.weight", model, fout)
+    # convert_non_q4("transformer.word_embeddings_layernorm.weight", "norm.weight", model, fout)
+    # convert_non_q4("transformer.word_embeddings_layernorm.bias", "norm.bias", model, fout)
 
     for i in range(n_layer):
-        convert_non_q4(f"transformer.h.{i}.input_layernorm.weight",
-                       f"layers.{i}.attention_norm.weight", model, fout)
-        convert_non_q4(f"transformer.h.{i}.input_layernorm.bias",
-                       f"layers.{i}.attention_norm.bias", model, fout)
-        convert_q4(f"transformer.h.{i}.self_attention.query_key_value",
-                   f"layers.{i}.attention.query_key_value.weight", model, fout, n_head, permute=True)
-        # convert_q4(f"transformer.h.{i}.self_attention.query_key_value",
-        #            f"layers.{i}.attention.query_key_value.weight", model, fout, n_head)
-        convert_non_q4(f"transformer.h.{i}.self_attention.query_key_value.bias",
-                       f"layers.{i}.attention.query_key_value.bias", model, fout)
-        convert_q4(f"transformer.h.{i}.self_attention.dense",
-                   f"layers.{i}.attention.wo.weight", model, fout, n_head)
-        convert_non_q4(f"transformer.h.{i}.self_attention.dense.bias",
-                       f"layers.{i}.attention.wo.bias", model, fout)
-        convert_non_q4(f"transformer.h.{i}.post_attention_layernorm.weight",
-                       f"layers.{i}.ffn_norm.weight", model, fout)
-        convert_non_q4(f"transformer.h.{i}.post_attention_layernorm.bias",
-                       f"layers.{i}.ffn_norm.bias", model, fout)
-        convert_q4(f"transformer.h.{i}.mlp.dense_h_to_4h",
-                   f"layers.{i}.feed_forward.w1.weight", model, fout, n_head)
-        convert_non_q4(f"transformer.h.{i}.mlp.dense_h_to_4h.bias",
-                       f"layers.{i}.feed_forward.w1.bias", model, fout)
-        convert_q4(f"transformer.h.{i}.mlp.dense_4h_to_h",
-                   f"layers.{i}.feed_forward.w2.weight", model, fout, n_head)
-        convert_non_q4(f"transformer.h.{i}.mlp.dense_4h_to_h.bias",
-                       f"layers.{i}.feed_forward.w2.bias", model, fout)
-    convert_non_q4("transformer.ln_f.weight", "output_norm.weight", model, fout)
-    convert_non_q4("transformer.ln_f.bias", "output_norm.bias", model, fout)
-    convert_fp_to_q4("lm_head.weight", "output.weight", model, fout)
+        convert_non_q4(f"gpt_neox.layers.{i}.input_layernorm.weight",
+                       f"gpt_neox.layers.{i}.input_layernorm.weight", model, fout)
+        convert_non_q4(f"gpt_neox.layers.{i}.input_layernorm.bias",
+                       f"gpt_neox.layers.{i}.input_layernorm.bias", model, fout)
+        convert_non_q4(f"gpt_neox.layers.{i}.post_attention_layernorm.weight",
+                       f"gpt_neox.layers.{i}.post_attention_layernorm.weight", model, fout)
+        convert_non_q4(f"gpt_neox.layers.{i}.post_attention_layernorm.bias",
+                       f"gpt_neox.layers.{i}.post_attention_layernorm.bias", model, fout)
+        convert_q4(f"gpt_neox.layers.{i}.attention.query_key_value",
+                   f"gpt_neox.layers.{i}.attention.query_key_value.weight", model, fout, n_head, permute=True)
+        # convert_q4(f"gpt_neox.layers.{i}.self_attention.query_key_value",
+        #            f"gpt_neox.layers.{i}.attention.query_key_value.weight", model, fout, n_head)
+        convert_non_q4(f"gpt_neox.layers.{i}.attention.query_key_value.bias",
+                       f"gpt_neox.layers.{i}.attention.query_key_value.bias", model, fout)
+        convert_q4(f"gpt_neox.layers.{i}.attention.dense",
+                   f"gpt_neox.layers.{i}.attention.dense.weight", model, fout, n_head)
+        convert_non_q4(f"gpt_neox.layers.{i}.attention.dense.bias",
+                       f"gpt_neox.layers.{i}.attention.dense.bias", model, fout)
+        convert_q4(f"gpt_neox.layers.{i}.mlp.dense_h_to_4h",
+                   f"gpt_neox.layers.{i}.mlp.dense_h_to_4h.weight", model, fout, n_head)
+        convert_non_q4(f"gpt_neox.layers.{i}.mlp.dense_h_to_4h.bias",
+                       f"gpt_neox.layers.{i}.mlp.dense_h_to_4h.bias", model, fout)
+        convert_q4(f"gpt_neox.layers.{i}.mlp.dense_4h_to_h",
+                   f"gpt_neox.layers.{i}.mlp.dense_4h_to_h.weight", model, fout, n_head)
+        convert_non_q4(f"gpt_neox.layers.{i}.mlp.dense_4h_to_h.bias",
+                       f"gpt_neox.layers.{i}.mlp.dense_4h_to_h.bias", model, fout)
+    convert_non_q4("gpt_neox.final_layer_norm.weight", "gpt_neox.final_layer_norm.weight", model, fout)
+    convert_non_q4("gpt_neox.final_layer_norm.bias", "gpt_neox.final_layer_norm.bias", model, fout)
+    convert_fp_to_q4("embed_out.weight", "embed_out.weight", model, fout)
     fout.close()
     print("Done. Output file: " + output_path)
     print("")
