b5d3aa66a560edb59954bd2f4498cf1d06469bd5
Romanticoseu <1251448395@qq.com>
2023-07-07 17:37:43 +0800

    Pentest fix: first fix command injection in pickle.load() in nano

--- python/nano/src/bigdl/nano/deps/openvino/tf/model.py
+++ python/nano/src/bigdl/nano/deps/openvino/tf/model.py
@@ -15,7 +15,7 @@
 #
 
 import os
-import pickle
+from bigdl.nano.utils.common.safepickle import SafePickle
 from pathlib import Path
 from typing import Sequence, Any, Union, Dict
 from tempfile import TemporaryDirectory
@@ -178,12 +178,12 @@ class KerasOpenVINOModel(KerasOptimizedModel):
                                    device=device,
                                    shapes=shapes)
         with open(Path(path) / status['attr_path'], "rb") as f:
-            attrs = pickle.load(f)
+            attrs = SafePickle.load(f)
         for attr_name, attr_value in attrs.items():
             setattr(model, attr_name, attr_value)
         if os.path.exists(Path(path) / status['compile_path']):
             with open(Path(path) / status['compile_path'], "rb") as f:
-                kwargs = pickle.load(f)
+                kwargs = SafePickle.load(f)
                 model.compile(**kwargs)
         return model
 
@@ -205,7 +205,7 @@ class KerasOpenVINOModel(KerasOptimizedModel):
         attrs = {"_mode": self._mode,
                  "_arg_names": self._arg_names}
         with open(path / self.status["attr_path"], "wb") as f:
-            pickle.dump(attrs, f)
+            SafePickle.dump(attrs, f)
 
         # save compile attr
         if self._is_compiled:
@@ -227,4 +227,4 @@ class KerasOpenVINOModel(KerasOptimizedModel):
                     else:
                         kwargs["weighted_metrics"] = weighted_metrics._name
             with open(Path(path) / self.status['compile_path'], "wb") as f:
-                pickle.dump(kwargs, f)
+                SafePickle.dump(kwargs, f)
