2e00cb63033cebb9a0d84911ccb5acbd661626c0
Yang Wang <yang3.wang@intel.com>
2023-07-13 06:17:47 +0800

    use pytorch linear for large input matrix

--- python/llm/src/bigdl/llm/transformers/linear_quant.py
+++ python/llm/src/bigdl/llm/transformers/linear_quant.py
@@ -82,6 +82,19 @@ def ggml_convert_quant(tensor: torch.Tensor, qtype: int, convert_shape_only=Fals
     return dst_tensor
 
 
+def ggml_int4_convert_fp32(tensor: torch.Tensor, weight_shape: tuple, k: int):
+    invalidInputError(tensor.dtype == torch.uint8,
+                      "Input tensor must be uint8")
+    src_ptr = ctypes.c_void_p(tensor.data.data_ptr())
+
+    dst_size = k
+    dst_tensor = torch.empty(weight_shape, dtype=torch.float)
+    dst_ptr = ctypes.c_void_p(dst_tensor.data.data_ptr())
+
+    ggml.ggml_dequantize_q4_0(src_ptr, dst_ptr, k)
+    return dst_tensor
+
+
 class ParamsQuant(torch.nn.Parameter):
     def __new__(cls,
                 data=None,
@@ -193,6 +206,7 @@ class LinearQuant(nn.Linear):
         self.in_len = input_features
         self.out_len = output_features
         self.weight_shape = (self.out_len, self.in_len)
+        self.weight_length = self.out_len * self.in_len
         self.qtype = qtype
 
     def forward(self, x: torch.Tensor):
@@ -201,15 +215,18 @@ class LinearQuant(nn.Linear):
             self.bias.data = self.bias.data.to(x.dtype)
 
         x_shape = x.shape
-        x = x.view(-1, x_shape[-1])
+        x_2d = x.view(-1, x_shape[-1])
 
         x0 = self.weight.data
 
-        result = ggml_matmul_src1_x_src0_t(x0, x, self.weight_shape, self.qtype)
-        new_shape = x_shape[:-1] + (self.out_len,)
-        result = result.view(new_shape)
-
-        if self.bias is not None:
-            result += self.bias
+        if x_2d.shape[0] < 96:  # todo may need to set a different number on different platform
+            result = ggml_matmul_src1_x_src0_t(x0, x_2d, self.weight_shape, self.qtype)
+            new_shape = x_shape[:-1] + (self.out_len,)
+            result = result.view(new_shape)
+            if self.bias is not None:
+                result += self.bias
+        else:
+            x0_fp32 = ggml_int4_convert_fp32(x0, self.weight_shape, self.weight_length)
+            result = F.linear(x, x0_fp32, self.bias)
 
         return result.to(x.dtype)
