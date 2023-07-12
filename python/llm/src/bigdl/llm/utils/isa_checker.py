944e211379152edcdab57ac271e74cace63eeb8a
Yishuo Wang <yishuo.wang@intel.com>
2023-07-12 09:01:22 +0800

    add avx check

--- python/llm/src/bigdl/llm/utils/isa_checker.py
+++ python/llm/src/bigdl/llm/utils/isa_checker.py
@@ -1,3 +1,20 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
 from cpuinfo import CPUID
 
 
@@ -17,6 +34,9 @@ class ISAChecker:
         )
         return ((0x10) & eax) != 0
 
+    def check_avx(self):
+        return 'avx' in self.flags
+
     def check_avx2(self):
         return 'avx2' in self.flags
 
@@ -35,6 +55,10 @@ class ISAChecker:
 
 
 isa_checker = ISAChecker()
+
+
+def check_avx():
+    return isa_checker.check_avx()
 
 
 def check_avx2():
