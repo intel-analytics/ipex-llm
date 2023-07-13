a6e29fd754bd458c72d68c7ff2952f9b1962ffa1
qiuxin2012 <qiuxin2012cs@gmail.com>
2023-07-13 16:42:40 +0800

    transformers save_low_bit load_low_bit

--- python/llm/src/bigdl/llm/transformers/model.py
+++ python/llm/src/bigdl/llm/transformers/model.py
@@ -21,6 +21,10 @@ from bigdl.llm.ggml.quantize import ggml_tensor_qtype
 from bigdl.llm.utils.common import invalidInputError
 
 
+def save_low_bit(self, *args, **kwargs):
+    self.save_pretrained(*args, **kwargs)
+
+
 class _BaseAutoModelClass:
 
     HF_MODEL = None
@@ -41,80 +45,105 @@ class _BaseAutoModelClass:
                              low-bit type. sym_int4 means symmetric int 4, asym_int4 means
                              asymmetric int 4.
         """
+        pretrained_model_name_or_path = kwargs.get("pretrained_model_name_or_path", None) \
+            if len(args) == 0 else args[0]
+        config_dict, _ = PretrainedConfig.get_config_dict(pretrained_model_name_or_path)
+        bigdl_transformers_low_bit = config_dict.pop("bigdl_transformers_low_bit", False)
+        if bigdl_transformers_low_bit:
+            print(f"Detect model is a low-bit({bigdl_transformers_low_bit}) model,"
+                  f" loading...")
+            return cls.from_low_bit(*args, **kwargs)
 
         # For huggingface transformers cls.HF_Model.from_pretrained could only restore the model
         # in the original format, which is not quantized,
         # we can convert the model to quantized later.
-        model = None
         load_in_4bit = kwargs.pop("load_in_4bit", False)
         load_in_low_bit = kwargs.pop("load_in_low_bit", None)
 
+        if load_in_4bit or load_in_low_bit:
+            # load int x-bit
+            kwargs["low_cpu_mem_usage"] = True
+            q_k = load_in_low_bit if load_in_low_bit else "sym_int4"
+            model = cls.load_convert(q_k, *args, **kwargs)
+        else:
+            # load default
+            model = cls.HF_Model.from_pretrained(*args, **kwargs)
+
+        return model
+
+    @classmethod
+    def load_convert(cls, q_k, *args, **kwargs):
+        from .convert import ggml_convert_quant
+        invalidInputError(q_k in ggml_tensor_qtype,
+                          f"Unknown load_in_low_bit value: {q_k}, expected:"
+                          f" sym_int4, asym_int4, sym_int5, asym_int5, sym_int8.")
+        qtype = ggml_tensor_qtype[q_k]
+        model = cls.HF_Model.from_pretrained(*args, **kwargs)
+        model = model.to("cpu")
+        model = ggml_convert_quant(model, qtype)
+        model.config.update({"bigdl_transformers_low_bit": q_k})
+
+        import types
+        model.save_low_bit = types.MethodType(save_low_bit, model)
+
+        return model
+
+    @classmethod
+    def from_low_bit(cls,
+                     *args,
+                     **kwargs):
         # Read bigdl_transformers_low_bit from config.json
         pretrained_model_name_or_path = kwargs.get("pretrained_model_name_or_path", None) \
             if len(args) == 0 else args[0]
         config_dict, _ = PretrainedConfig.get_config_dict(pretrained_model_name_or_path)
         bigdl_transformers_low_bit = config_dict.pop("bigdl_transformers_low_bit", False)
 
-        if load_in_4bit or load_in_low_bit or bigdl_transformers_low_bit:
-            # Speed up when loading model
-            kwargs["low_cpu_mem_usage"] = True
+        invalidInputError(bigdl_transformers_low_bit,
+                          "Detect this model is not a low-bit model, Please use from_pretrained"
+                          " with load_in_4bit or load_in_low_bit to get a low-bit model , and "
+                          " serialize the model using save_low_bit first.")
 
-        if bigdl_transformers_low_bit:
-            invalidInputError(bigdl_transformers_low_bit in ggml_tensor_qtype,
-                              f"Unknown bigdl_transformers_low_bit value:"
-                              f" {bigdl_transformers_low_bit},"
-                              f" expected: sym_int4, asym_int4, sym_int5, asym_int5 or sym_int8.")
-            qtype = ggml_tensor_qtype[bigdl_transformers_low_bit]
-            # Note that the int4 linear layers cannot currently
-            # be recorded in huggingface Pretrained Model or AutoConfig,
-            # and huggingface transformers cls.HF_Model.from_pretrained
-            # could only restore the model in the original format,
-            # which is not quantized. we can Initialize original model first,
-            # convert the model to quantized int4 format later, and then load the quantized model.
+        invalidInputError(bigdl_transformers_low_bit in ggml_tensor_qtype,
+                          f"Unknown bigdl_transformers_low_bit value: {bigdl_transformers_low_bit},"
+                          f" expected sym_int4, asym_int4, sym_int5, asym_int5, sym_int8.")
 
-            # Avoid KeyError
-            kwargs["ignore_mismatched_sizes"] = True
-            # Avoid reading from local file at the first initialization
-            kwargs["state_dict"] = {}
+        # Speed up when loading model
+        kwargs["low_cpu_mem_usage"] = True
 
-            # Maybe needed when extract_local_archive_file
-            subfolder = kwargs.get("subfolder", "")
-            variant = kwargs.get("variant", None)
+        qtype = ggml_tensor_qtype[bigdl_transformers_low_bit]
+        # Note that the int4 linear layers cannot currently
+        # be recorded in huggingface Pretrained Model or AutoConfig,
+        # and huggingface transformers cls.HF_Model.from_pretrained
+        # could only restore the model in the original format,
+        # which is not quantized. we can Initialize original model first,
+        # convert the model to quantized int4 format later, and then load the quantized model.
 
-            from .convert import ggml_convert_quant
-            model = cls.HF_Model.from_pretrained(*args, **kwargs)
-            print("Note: If there are warnings during the model loading process, "
-                  "they can be safely ignored; "
-                  "the model will be loaded with INT4 optimizations applied.")
+        # Avoid KeyError
+        kwargs["ignore_mismatched_sizes"] = True
+        # Avoid reading from local file at the first initialization
+        kwargs["state_dict"] = {}
 
-            # We forcefully modify the model's definition
-            # and the tensor shape of int4 weights without quantization.
-            model = ggml_convert_quant(model, qtype, convert_shape_only=True)
-            # Load the quantized model at last.
-            archive_file = extract_local_archive_file(pretrained_model_name_or_path,
-                                                      subfolder,
-                                                      variant)
-            state_dict = load_state_dict(archive_file)
-            load(model, state_dict)
-            del state_dict
+        # Maybe needed when extract_local_archive_file
+        subfolder = kwargs.get("subfolder", "")
+        variant = kwargs.get("variant", None)
 
-        elif load_in_4bit or load_in_low_bit:
-            q_k = load_in_low_bit if load_in_low_bit else "sym_int4"
-            model = cls.convert_quant(model, q_k, *args, **kwargs)
-
-        return model
-
-    @classmethod
-    def convert_quant(cls, model, q_k, *args, **kwargs):
         from .convert import ggml_convert_quant
-        invalidInputError(q_k in ggml_tensor_qtype,
-                          f"Unknown load_in_low_bit value: {q_k}, expected:"
-                          f" sym_int4, asym_int4, sym_int5, asym_int5 or sym_int8.")
-        qtype = ggml_tensor_qtype[q_k]
         model = cls.HF_Model.from_pretrained(*args, **kwargs)
-        model = model.to("cpu")
-        model = ggml_convert_quant(model, qtype)
-        model.config.update({"bigdl_transformers_low_bit": q_k})
+        print("Note: If there are warnings during the model loading process, "
+              "they can be safely ignored; "
+              "the model will be loaded with INT4 optimizations applied.")
+
+        # We forcefully modify the model's definition
+        # and the tensor shape of int4 weights without quantization.
+        model = ggml_convert_quant(model, qtype, convert_shape_only=True)
+        # Load the quantized model at last.
+        archive_file = extract_local_archive_file(pretrained_model_name_or_path,
+                                                  subfolder,
+                                                  variant)
+        state_dict = load_state_dict(archive_file)
+        load(model, state_dict)
+        del state_dict
+
         return model
 
 
