591a0ba2ae32cbd45fe4add0020d039968bf5c64
Zhao Changmin <changmin.zhao@intel.com>
2023-07-13 09:42:37 +0800

    Orca: Fix mxnet python shell blocked issue (#8504)
    * fix shell issue

--- python/orca/src/bigdl/orca/learn/mxnet/mxnet_runner.py
+++ python/orca/src/bigdl/orca/learn/mxnet/mxnet_runner.py
@@ -77,7 +77,7 @@ class MXNetRunner(object):
             modified_env = os.environ.copy()
             modified_env.update(env)
             # For servers, just import mxnet and no need to do anything else.
-            subprocess.Popen(["python", "-c", "'import mxnet'"], shell=True, env=modified_env)
+            subprocess.Popen(["python", "-c", "import mxnet"], shell=False, env=modified_env)
 
     def train(self, train_data, epochs=1, batch_size=32,
               validation_data=None, train_resize_batch_num=None):
