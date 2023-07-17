b61518198a004601bf1d1d74202c3cf2520cc0d6
jiaming <litchy233@gmail.com>
2023-07-17 15:59:44 +0800

    .

--- python/llm/example/llm-sd-notebook/code.py
+++ python/llm/example/llm-sd-notebook/code.py
@@ -10,8 +10,8 @@ from transformers import AutoTokenizer
 model_path = os.environ.get('LLM_MODEL_PATH')
 model = AutoModel.from_pretrained(model_path, trust_remote_code=True, load_in_4bit=True)
 tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-input_str = "Give me a text prompt to generate an image of a castle\n\n"
 
+input_str = "Give me a text prompt to generate an image of a castle\n\n"
 with torch.inference_mode():
     st = time.time()
     input_ids = tokenizer.encode(input_str, return_tensors="pt")
@@ -23,7 +23,6 @@ print('Output:', output_str)
 print(f'Inference time: {end-st} s')
 
 prompt = output_str.split('"')[-1]
-
 from bigdl_diffusion.diffusers.pipelines import *
 pipe = NanoStableDiffusionPipeline.from_pretrained(os.getenv('SD_MODEL_PATH'), device='GPU', backend='ov')
 output = pipe(prompt, num_inference_steps=10).images[0]
