411d896636c223abdc16023ebf4c852d25f1257c
Song Jiaming <litchy233@gmail.com>
2023-07-20 10:16:27 +0800

    LLM first transformers UT (#8514)
    * ut
    
    * transformers api first ut
    
    * name
    
    * dir issue
    
    * use chatglm instead of chatglm2
    
    * omp
    
    * set omp in sh
    
    * source
    
    * taskset
    
    * test
    
    * test omp
    
    * add test

--- python/llm/test/run-llm-inference-tests.sh
+++ python/llm/test/run-llm-inference-tests.sh
@@ -4,12 +4,16 @@ export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export LLM_HOME=${ANALYTICS_ZOO_ROOT}/python/llm/src
 export LLM_INFERENCE_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/llm/test/inference
 
+source bigdl-nano-init
+
 set -e
 
 echo "# Start testing inference"
 start=$(date "+%s")
 
-python -m pytest -s ${LLM_INFERENCE_TEST_DIR}
+python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k "not test_transformers_int4"
+export OMP_NUM_THREADS=24
+taskset -c 0-23 python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k test_transformers_int4
 
 now=$(date "+%s")
 time=$((now-start))
