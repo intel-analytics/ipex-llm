2266ca7d2b7cde4f52dec748c450ebbf739585ff
Yuwen Hu <54161268+Oscilloscope98@users.noreply.github.com>
2023-07-20 13:20:25 +0800

    [LLM] Small updates to transformers int4 ut (#8574)
    * Small fix to transformers int4 ut
    
    * Small fix

--- python/llm/test/run-llm-inference-tests.sh
+++ python/llm/test/run-llm-inference-tests.sh
@@ -4,16 +4,18 @@ export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export LLM_HOME=${ANALYTICS_ZOO_ROOT}/python/llm/src
 export LLM_INFERENCE_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/llm/test/inference
 
-source bigdl-nano-init
-
 set -e
 
 echo "# Start testing inference"
 start=$(date "+%s")
 
 python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k "not test_transformers_int4"
-export OMP_NUM_THREADS=24
-taskset -c 0-23 python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k test_transformers_int4
+
+if [ -z "$THREAD_NUM" ]; then
+  THREAD_NUM=2
+fi
+export OMP_NUM_THREADS=$THREAD_NUM
+taskset -c 0-$((THREAD_NUM - 1)) python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k test_transformers_int4
 
 now=$(date "+%s")
 time=$((now-start))
