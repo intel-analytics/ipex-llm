52cba838f23d8bfbad8b32ae023c8c05aa510a0d
Jiaming <litchy233@gmail.com>
2023-08-09 10:41:02 +0800

    remove conda

--- python/llm/dev/test/run-example-tests.sh
+++ python/llm/dev/test/run-example-tests.sh
@@ -4,7 +4,7 @@ if [ ! -d $ORIGINAL_LLAMA2_PATH ]; then
     wget -r -nH --no-verbose --cut-dirs=1 $LLM_FTP_URL/${ORIGINAL_LLAMA2_PATH:2} -P $LLM_DIR
 fi
 
-source $CONDA_HOME/bin/activate bigdl-llm-test
+
 std=$(python python/llm/example/transformers/transformers_int4/llama2/generate.py --repo-id-or-model-path $ORIGINAL_LLAMA2_PATH)
 echo "the output of the example is: " 
 echo $std
@@ -12,4 +12,3 @@ if [[ ! $std == *"AI is a term"* ]]; then
     echo "The expected output is not met."
     return 1
 fi
-source $CONDA_HOME/bin/deactivate
