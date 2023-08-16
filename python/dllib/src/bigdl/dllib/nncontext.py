03f4aaaf51f220ed27bf6d799144bdcd39b7f945
sgwhat <ge.song@intel.com>
2023-08-16 14:55:51 +0000

    fix dllib

--- python/dllib/src/bigdl/dllib/nncontext.py
+++ python/dllib/src/bigdl/dllib/nncontext.py
@@ -766,13 +766,15 @@ def init_spark_conf(conf=None):
         if 'PYSPARK_SUBMIT_ARGS' in os.environ:
             submit_args = os.environ['PYSPARK_SUBMIT_ARGS']
             start = submit_args.find('pyspark-shell')
-            submit_args = '{}--driver-class-path {} {}'.format(submit_args[:start],
-                                                               os.environ["BIGDL_JARS"],
-                                                               submit_args[start:])
+            submit_args = submit_args[:start] +
+                          "--driver-class-path " +
+                          os.environ["BIGDL_JARS"] +
+                          submit_args[start:]
         else:
-            submit_args = " --driver-class-path " + os.environ["BIGDL_JARS"] + " pyspark-shell "
-        print("pyspark_submit_args is: {}".format(submit_args))
+            submit_args = f" --driver-class-path {os.environ['BIGDL_JARS']} pyspark-shell "
+        print("pyspark_submit_args is:", submit_args)
         os.environ['PYSPARK_SUBMIT_ARGS'] = submit_args
+
 
     # add content in PYSPARK_FILES in spark.submit.pyFiles
     # This is a workaround for current Spark on k8s
