91ce0079bdd64b2a66debcf2c59b50938b934861
rnwang04 <ruonan1.wang@intel.com>
2023-08-17 06:58:38 +0800

    fix style

--- python/llm/src/bigdl/llm/transformers/linear_quant.py
+++ python/llm/src/bigdl/llm/transformers/linear_quant.py
@@ -244,7 +244,7 @@ class LinearQuant(nn.Linear):
             if x_2d.is_contiguous() is False:
                 x_2d = x_2d.contiguous()
             # current workaround to reduce first token latency of fp32 input
-            if x_2d.shape[0] > 1 and  x_2d.dtype == torch.float32:
+            if x_2d.shape[0] > 1 and x_2d.dtype == torch.float32:
                 x_2d = x_2d.half()
             # input format of linear_q4.forward is 1: input, 2: weight
             result = linear_q4_0.forward(x_2d, x0)
