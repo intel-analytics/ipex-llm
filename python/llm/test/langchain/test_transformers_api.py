e0fce2bb0f6e08365e8b0c870aaf9e3299ed1333
sgwhat <ge.song@intel.com>
2023-08-18 15:28:21 +0000

    ut fix for transformers embedding

--- python/llm/test/langchain/test_transformers_api.py
+++ python/llm/test/langchain/test_transformers_api.py
@@ -91,13 +91,13 @@ class Test_Langchain_Transformers_API(TestCase):
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.split_text(texts)
         query = 'What is AI?'
-        embeddings = ChatGLMEmbeddings(model_id=self.auto_model_path, model_kwargs={'trust_remote_code': True}, native=False)
+        embeddings = ChatGLMEmbeddings(model_path=self.auto_model_path, model_kwargs={'trust_remote_code': True}, native=False)
 
         docsearch = Chroma.from_texts(texts, embeddings, metadatas=[{"source": str(i)} for i in range(len(texts))]).as_retriever()
 
         #get relavant texts
         docs = docsearch.get_relevant_documents(query)
-        bigdl_llm = ChatGLMLLM(model_id=self.auto_model_path, model_kwargs={'trust_remote_code': True}, native=False)
+        bigdl_llm = ChatGLMLLM(model_path=self.auto_model_path, model_kwargs={'trust_remote_code': True}, native=False)
         doc_chain = load_qa_chain(bigdl_llm, chain_type="stuff", prompt=QA_PROMPT)
         output = doc_chain.run(input_documents=docs, question=query)
         res = "AI" in output
