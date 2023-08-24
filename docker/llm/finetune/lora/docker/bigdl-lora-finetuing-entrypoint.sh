912cbba357f84cdce76b3120265dfd4d9b284823
Uxito-Ada <414416158@qq.com>
2023-08-24 18:01:07 +0800

    fix output path in entrypoint

--- docker/llm/finetune/lora/docker/bigdl-lora-finetuing-entrypoint.sh
+++ docker/llm/finetune/lora/docker/bigdl-lora-finetuing-entrypoint.sh
@@ -8,7 +8,7 @@ if [ "$WORKER_ROLE" = "launcher" ]
 then
   sed "s/:1/ /g" /etc/mpi/hostfile > /home/mpiuser/hostfile
   export DATA_PATH="/ppml/data/$DATA_SUB_PATH"
-  export MODEL_SAVE_PATH="/ppml/model/output"
+  export MODEL_SAVE_PATH="/ppml/output"
   sleep 10
   mpirun \
     -n $WORLD_SIZE \
