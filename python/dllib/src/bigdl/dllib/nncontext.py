3e82960727b1e8c4b02f3dba27729f5659310552
sgwhat <ge.song@intel.com>
2023-08-25 15:18:52 +0000

    dllib code style fix

--- python/dllib/src/bigdl/dllib/nncontext.py
+++ python/dllib/src/bigdl/dllib/nncontext.py
@@ -766,8 +766,8 @@ def init_spark_conf(conf=None):
         if 'PYSPARK_SUBMIT_ARGS' in os.environ:
             submit_args = os.environ['PYSPARK_SUBMIT_ARGS']
             start = submit_args.find('pyspark-shell')
-            submit_args = submit_args[:start] + "--driver-class-path " + \
-                          os.environ["BIGDL_JARS"] + submit_args[start:]
+            submit_args = (submit_args[:start] + "--driver-class-path " +
+                           os.environ["BIGDL_JARS"] + submit_args[start:])
         else:
             submit_args = f" --driver-class-path {os.environ['BIGDL_JARS']} pyspark-shell "
         print("pyspark_submit_args is:", submit_args)
