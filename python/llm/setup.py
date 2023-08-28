6a902b892e018e8f974e77518e51eaa116b6052b
xingyuan li <108672484+hoshibara@users.noreply.github.com>
2023-08-28 17:41:18 +0900

    [LLM] Add amx build step (#8822)
    * add amx build step

--- python/llm/setup.py
+++ python/llm/setup.py
@@ -102,6 +102,10 @@ linux_binarys = [
     "libgptneox_avx512.so",
     "libbloom_avx512.so",
     "libstarcoder_avx512.so",
+    "libllama_amx.so",
+    "libgptneox_amx.so",
+    "libbloom_amx.so",
+    "libstarcoder_amx.so",
     "quantize-llama",
     "quantize-gptneox",
     "quantize-bloom",
@@ -116,6 +120,7 @@ linux_binarys = [
     "main-starcoder",
 
     "main-chatglm_vnni",
+    "main-chatglm_amx",
     "chatglm_C.cpython-39-x86_64-linux-gnu.so",
 ]
 
