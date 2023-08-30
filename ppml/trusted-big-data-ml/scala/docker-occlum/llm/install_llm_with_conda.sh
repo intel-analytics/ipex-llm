6acac331a46d143a6d574dbb196823868cc3e7bb
hzjane <a1015616934@qq.com>
2023-08-30 11:00:32 +0800

    init occlum llm image

---
+++ ppml/trusted-big-data-ml/scala/docker-occlum/llm/install_llm_with_conda.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Install python and dependencies to specified position
+
+[ -f Miniconda3-latest-Linux-x86_64.sh ] || wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+[ -d miniconda ] || bash ./Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda
+/opt/miniconda/bin/conda create --prefix /opt/python-occlum -y python=3.9.11 numpy=1.21.5 scipy=1.7.3 scikit-learn=1.0 pandas=1.3 Cython
+
+export PATH=$PATH:/opt/miniconda/bin
+source activate
+conda activate /opt/python-occlum
+pip install synapseml==0.10.2
+mkdir -p /opt/occlum_spark/data/
+cd /opt/occlum_spark/data/
+pip3 install fschat -i https://pypi.tuna.tsinghua.edu.cn/simple/
+git clone -b used-for-bench https://github.com/analytics-zoo/FastChat.git
+cd FastChat
+pip3 install --upgrade pip  # enable PEP 660 support
+pip3 install -e . -i https://pypi.tuna.tsinghua.edu.cn/simple/
+
+pip install --pre --upgrade bigdl-llm[all] -i https://pypi.tuna.tsinghua.edu.cn/simple/
