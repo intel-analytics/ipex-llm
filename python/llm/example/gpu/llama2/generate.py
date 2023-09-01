7d74d298927d1ab3614da893f3388edba0151364
cyita <yitastudy@gmail.com>
2023-09-01 16:17:23 +0800

    add optimize_model=True in llama2 chatglm2 examples

--- python/llm/example/gpu/llama2/generate.py
+++ python/llm/example/gpu/llama2/generate.py
@@ -57,7 +57,7 @@ if __name__ == '__main__':
     # which convert the relevant layers in the model into INT4 format
     model = AutoModelForCausalLM.from_pretrained(model_path,
                                                  load_in_4bit=True,
-                                                 optimize_model=False,
+                                                 optimize_model=True,
                                                  trust_remote_code=True)
     model = model.to('xpu')
 
