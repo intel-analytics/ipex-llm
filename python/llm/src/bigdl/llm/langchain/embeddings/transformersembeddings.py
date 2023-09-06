605e36638faf8f635427bed182e3eba43298a220
sgwhat <ge.song@intel.com>
2023-09-06 15:52:47 +0000

    update api doc

--- python/llm/src/bigdl/llm/langchain/embeddings/transformersembeddings.py
+++ python/llm/src/bigdl/llm/langchain/embeddings/transformersembeddings.py
@@ -95,7 +95,7 @@ class TransformersEmbeddings(BaseModel, Embeddings):
             model_kwargs: Keyword arguments that will be passed to the model and tokenizer.
             kwargs: Extra arguments that will be passed to the model and tokenizer.
         
-        Returns: Instances of the embedding model and tokenizer.
+        Returns: An object of TransformersEmbeddings.
         """
         try:
             from bigdl.llm.transformers import AutoModel
