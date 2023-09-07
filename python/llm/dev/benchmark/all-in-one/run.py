c61cf4bdc24a447db25981ea410c787aa8fae290
Xin Qiu <qiuxin2012@users.noreply.github.com>
2023-09-07 09:49:55 +0800

    transformer int4 and native int4's benchmark script for 32 256 1k 2k input (#8871)
    * transformer
    
    * move
    
    * update
    
    * add header
    
    * update all-in-one
    
    * clean up

--- python/llm/dev/benchmark/all-in-one/run.py
+++ python/llm/dev/benchmark/all-in-one/run.py
@@ -22,7 +22,8 @@ import argparse
 
 from bigdl.llm.transformers import AutoModel, AutoModelForCausalLM
 from transformers import AutoTokenizer
-
+import numpy as np
+from datetime import date
 
 import os
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -31,69 +32,77 @@ import sys
 sys.path.append(benchmark_util_path)
 from benchmark_util import BenchmarkWrapper
 
-
-
-input_str_32 = "Once upon a time, there existed a little girl who liked to have adventures. She wanted to go to places and meet new people, and have fun"
-input_str_1024 = "It is done, and submitted. You can play 'Survival of the Tastiest' on Android, and on the web. Playing on the web works, but you have to simulate multiple touch for table moving and that can be a bit confusing. There is a lot I'd like to talk about. I will go through every topic, insted of making the typical what went right/wrong list. Concept Working over the theme was probably one of the hardest tasks which I had to face. Originally, I had an idea of what kind of game I wanted to develop, gameplay wise - something with a lot of enemies/actors, simple graphics, maybe set in space, controlled from a top-down view. I was confident that I could fit any theme around it. In the end, the problem with a theme like 'Evolution' in a game is that evolution is unassisted. It happens through several seemingly random mutations over time, with the most apt permutation surviving. This genetic car simulator is, in my opinion, a great example of actual evolution of a species facing a challenge. But is it a game? In a game, you need to control something to reach an objective. That control goes against what evolution is supposed to be like. If you allow the user to pick how to evolve something, it's not evolution anymore - it's the equivalent of intelligent design, the fable invented by creationists to combat the idea of evolution. Being agnostic and a Pastafarian, that's not something that rubbed me the right way. Hence, my biggest dillema when deciding what to create was not with what I wanted to create, but with what I did not. I didn't want to create an 'intelligent design' simulator and wrongly call it evolution. This is a problem, of course, every other contestant also had to face. And judging by the entries submitted, not many managed to work around it. I'd say the only real solution was through the use of artificial selection, somehow. So far, I haven't seen any entry using this at its core gameplay. Alas, this is just a fun competition and after a while I decided not to be as strict with the game idea, and allowed myself to pick whatever I thought would work out. My initial idea was to create something where humanity tried to evolve to a next level, but had some kind of foe trying to stop them from doing so. I kind of had this image of human souls flying in space towards a monolith or a space baby (all based in 2001: A Space Odyssey of course) but I couldn't think of compelling (read: serious) mechanics for that. Borgs were my next inspiration, as their whole hypothesis fit pretty well into the evolution theme. But how to make it work? Are you the borg, or fighting the Borg? The third and final idea came to me through my girlfriend, who somehow gave me the idea of making something about the evolution of Pasta. The more I thought about it the more it sounded like it would work, so I decided to go with it. Conversations with my inspiring co-worker Roushey (who also created the 'Mechanical Underdogs' signature logo for my intros) further matured the concept, as it involved into the idea of having individual pieces of pasta flying around and trying to evolve until they became all-powerful. A secondary idea here was that the game would work to explain how the Flying Spaghetti Monster came to exist - by evolving from a normal dinner table. So the idea evolved more or less into this: you are sitting a table. You have your own plate, with is your 'base'. There are 5 other guests at the table, each with their own plate. Your plate can spawn little pieces of pasta. You do so by 'ordering' them through a menu. Some pastas are better than others; some are faster, some are stronger. They have varying 'costs', which are debited from your credits (you start with a number of credits). Once spawned, your pastas start flying around. Their instinct is to fly to other plates, in order to conquer them (the objective of the game is having your pasta conquer all the plates on the table). But they are really autonomous, so after being spawned, you have no control over your pasta (think DotA or LoL creeps). Your pasta doesn't like other people's pasta, so if they meet, they shoot sauce at each other until one dies. You get credits for other pastas your own pasta kill. Once a pasta is in vicinity of a plate"
-
 results = []
 
-def run_model(model_name, repo_id_or_model_path):
-    first, rest = run_pipeline(repo_id_or_model_path, '32/32')
-    results.append([model_name, first, rest, '32/32'])
-    first, rest = run_pipeline(repo_id_or_model_path, '1024/128')
-    results.append([model_name, first, rest, '1024/128'])
+
+def run_model(repo_id, local_model_hub=None, warm_up=1, num_trials=3):
+    # TODO: make a parameter
+    in_out_pairs = ['32-32', '1024-128']
+    result = run_transformer_int4(repo_id, local_model_hub, in_out_pairs, warm_up, num_trials)
+    for in_out_pair in in_out_pairs:
+        results.append([repo_id,
+                        np.mean(result[in_out_pair], axis=0)[0],
+                        np.mean(result[in_out_pair], axis=0)[1],
+                        np.mean(result[in_out_pair], axis=0)[2],
+                        in_out_pair])
 
 
-
-def run_pipeline(repo_id_or_model_path, in_out_pair):
-    if in_out_pair == '32/32':
-        prompt, n_predict = input_str_32, 32
+def run_transformer_int4(repo_id,
+                         local_model_hub,
+                         in_out_pairs,
+                         warm_up,
+                         num_trials,
+                         device='cpu'):
+    if local_model_hub:
+        repo_model_name = repo_id.split("/")[1]
+        model_path = local_model_hub + "/" + repo_model_name
     else:
-        prompt, n_predict = input_str_1024, 128
-    model_path = repo_id_or_model_path
-
+        model_path = repo_id
     # Load model in 4 bit,
     # which convert the relevant layers in the model into INT4 format
-    model = AutoModelForCausalLM.from_pretrained(model_path,
-                                                    load_in_4bit=True,
-                                                    trust_remote_code=True)
-
+    st = time.perf_counter()
+    if repo_id in ['THUDM/chatglm-6b', 'THUDM/chatglm2-6b']:
+        model = AutoModel.from_pretrained(model_path, load_in_4bit=True, trust_remote_code=True, torch_dtype='auto')
+        tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+    else:
+        model = AutoModelForCausalLM.from_pretrained(model_path, load_in_4bit=True)
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+    end = time.perf_counter()
+    print(">> loading of model costs {}s".format(end - st))
 
     model = BenchmarkWrapper(model)
 
-    # Load tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-
-    # Generate predicted tokens
+    result = {}
     with torch.inference_mode():
-        input_ids = tokenizer.encode(prompt, return_tensors="pt")
-        st = time.time()
-        # if your selected model is capable of utilizing previous key/value attentions
-        # to enhance decoding speed, but has `"use_cache": false` in its model config,
-        # it is important to set `use_cache=True` explicitly in the `generate` function
-        # to obtain optimal performance with BigDL-LLM INT4 optimizations
-        output = model.generate(input_ids,
-                                max_new_tokens=n_predict)
-        end = time.time()
-        output_str = tokenizer.decode(output[0], skip_special_tokens=True)
-        print(f'Inference time: {end-st} s')
-        print('-'*20, 'Prompt', '-'*20)
-        print(prompt)
-        print('-'*20, 'Output', '-'*20)
-        print(output_str)       
-        
-        return round(model.first_cost * 1000, 2), round(model.rest_cost_mean * 1000, 2)
-
-
+        for in_out in in_out_pairs:
+            in_out_len = in_out.split("-")
+            in_len = int(in_out_len[0])
+            out_len = int(in_out_len[1])
+            input_str = open(f"prompt/{in_len}.txt", 'r').read()
+            # As different tokenizer has different encodings,
+            # slice the input_ids to ensure the prompt length is required length.
+            input_ids = tokenizer.encode(input_str, return_tensors="pt")
+            input_ids = input_ids[:, :in_len]
+            result[in_out] = []
+            for i in range(num_trials + warm_up):
+                st = time.perf_counter()
+                output_ids = model.generate(input_ids, do_sample=False, max_new_tokens=out_len)
+                end = time.perf_counter()
+                print("model generate cost: " + str(end - st))
+                output = tokenizer.batch_decode(output_ids)
+                print(output[0])
+                if i >= warm_up:
+                    result[in_out].append([model.first_cost, model.rest_cost_mean, model.encoder_time])
+    return result
 
 
 if __name__ == '__main__':
     from omegaconf import OmegaConf
     conf = OmegaConf.load(f'{current_dir}/config.yaml')
+    today = date.today()
     
     import pandas as pd
-    for model in conf.keys():
-        run_model(model, conf[model])
-    df = pd.DataFrame(results, columns=['model', '1st token avg latency (ms/token)', '2+ avg latency (ms/token)', 'input/output tokens'])
-    df.to_csv(f'{current_dir}/results.csv')
+    for model in conf.repo_id:
+        run_model(model, conf['local_model_hub'], conf['warm_up'], conf['num_trials'])
+    df = pd.DataFrame(results, columns=['model', '1st token avg latency (s)', '2+ avg latency (s/token)', 'encoder time (s)', 'input/output tokens'])
+    df.to_csv(f'{current_dir}/results-{today}.csv')
