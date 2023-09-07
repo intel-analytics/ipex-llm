ed4f74b8ca978adb17c27842824efb323ecdb63c
rnwang04 <ruonan1.wang@intel.com>
2023-09-07 14:21:22 +0800

    update setup to specify bigdl-core-xe version

--- python/llm/setup.py
+++ python/llm/setup.py
@@ -259,7 +259,7 @@ def setup_package():
     xpu_requires += ["torch==2.0.1a0",
                      "torchvision==0.15.2a0",
                      "intel_extension_for_pytorch==2.0.110+xpu;platform_system=='Linux'",
-                     "bigdl-core-xe;platform_system=='Linux'"]
+                     "bigdl-core-xe==" + VERSION + ";platform_system=='Linux'"]
 
     metadata = dict(
         name='bigdl-llm',
