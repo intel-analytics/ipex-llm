eebb77dbbb4d099e202c15b29f64242c3357d236
Xiangyu Tian <109123695+xiangyuT@users.noreply.github.com>
2023-09-08 10:24:04 +0800

    [PPML] Add attestation for LLM Finetuning (#8908)
     Add TDX attestation for LLM Finetuning in TDX CoCo
    
    ---------
    
    Co-authored-by: Heyang Sun <60865256+Uxito-Ada@users.noreply.github.com>

---
+++ docker/llm/finetune/lora/docker/get_worker_quote.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x
+source /opt/intel/oneapi/setvars.sh
+export CCL_WORKER_COUNT=$WORLD_SIZE
+export CCL_WORKER_AFFINITY=auto
+export SAVE_PATH="/ppml/output"
+
+mpirun \
+    -n $WORLD_SIZE \
+    -ppn 1 \
+    -f /home/mpiuser/hostfile \
+    -iface eth0 \
+    -genv OMP_NUM_THREADS=$OMP_NUM_THREADS \
+    -genv KMP_AFFINITY="granularity=fine,none" \
+    -genv KMP_BLOCKTIME=1 \
+    -genv TF_ENABLE_ONEDNN_OPTS=1 \
+    sudo -E python /ppml/worker_quote_generate.py --user_report_data $1 > $SAVE_PATH/quote.log 2>&1
