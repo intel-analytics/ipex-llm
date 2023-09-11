4239b5c0754302f1bf8c5e035df30e749f21d09b
Yang Wang <yang3.wang@intel.com>
2023-09-11 12:05:19 +0800

    fix style

--- python/llm/example/gpu/qlora_finetuning/qlora_finetuning.py
+++ python/llm/example/gpu/qlora_finetuning/qlora_finetuning.py
@@ -1,3 +1,19 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import torch
 import os
 os.environ["ACCELERATE_USE_IPEX"] = "1"
@@ -41,13 +57,13 @@ if __name__ == "__main__":
         model=model,
         train_dataset=data["train"],
         args=TrainingArguments(
-            per_device_train_batch_size=1,
-            gradient_accumulation_steps=4,
-            warmup_steps=2,
-            max_steps=10,
+            per_device_train_batch_size=4,
+            gradient_accumulation_steps=1,
+            warmup_steps=20,
+            max_steps=200,
             learning_rate=2e-4,
             fp16=False,
-            logging_steps=1,
+            logging_steps=20,
             output_dir="outputs",
             optim="adamw_hf", # we currently do not have paged_adamw_8bit
         ),
