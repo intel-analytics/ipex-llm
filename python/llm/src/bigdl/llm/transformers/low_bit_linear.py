df165ad165870d76f24085d82bc123241d0efb47
Yina Chen <33650826+cyita@users.noreply.github.com>
2023-09-11 14:30:55 +0800

    init (#8933)

--- python/llm/src/bigdl/llm/transformers/low_bit_linear.py
+++ python/llm/src/bigdl/llm/transformers/low_bit_linear.py
@@ -61,6 +61,7 @@ IS_SPR = is_spr()
 TORCH_LINEAR_THRESHOLD = 96
 SYM_INT4 = ggml_tensor_qtype["sym_int4"]
 SYM_INT8 = ggml_tensor_qtype["sym_int8"]
+NF4 = ggml_tensor_qtype["nf4"]
 
 
 def ggml_convert_qtype(tensor: torch.Tensor, qtype: int, device=None):
@@ -99,7 +100,7 @@ def ggml_q_format_convet_cpu2xpu(tensor: torch.Tensor, num_elem: int, qtype: int
 
     src = ctypes.c_void_p(tensor.data.data_ptr())
 
-    if qtype in [SYM_INT4, SYM_INT8]:
+    if qtype in [SYM_INT4, SYM_INT8, NF4]:
         dst_tensor = torch.empty_like(tensor)
     elif qtype == ggml_tensor_qtype["sym_int5"]:
         QK = ggml.ggml_qk_size(qtype)
@@ -124,7 +125,7 @@ def ggml_q_format_convet_xpu2cpu(tensor: torch.Tensor, num_elem: int, qtype: int
 
     src = ctypes.c_void_p(tensor.data.data_ptr())
 
-    if qtype in [SYM_INT4, SYM_INT8]:
+    if qtype in [SYM_INT4, SYM_INT8, NF4]:
         dst_tensor = torch.empty_like(tensor)
     elif qtype == ggml_tensor_qtype["sym_int5"]:
         QK = ggml.ggml_qk_size(ggml_tensor_qtype["asym_int5"])
