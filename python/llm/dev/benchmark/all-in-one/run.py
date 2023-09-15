3e04f1def6193060c042cefb676994229c71dba9
qiuxin2012 <qiuxin2012cs@gmail.com>
2023-09-15 11:15:07 +0800

    uodate

--- python/llm/dev/benchmark/all-in-one/run.py
+++ python/llm/dev/benchmark/all-in-one/run.py
@@ -59,8 +59,8 @@ def get_model_path(repo_id, local_model_hub):
     if local_model_hub:
         repo_model_name = repo_id.split("/")[1]
         local_model_path = local_model_hub + os.path.sep + repo_model_name
-        invalidInputError(not os.path.isdir(local_model_path),
-                          local_model_path + "not exists!, Please check your models' folder.")
+        invalidInputError(os.path.isdir(local_model_path),
+                          local_model_path + " not exists!, Please check your models' folder.")
         return local_model_hub + os.path.sep + repo_model_name
     else:
         return repo_id
@@ -277,12 +277,7 @@ def run_transformer_int4_gpu(repo_id,
                              num_trials):
     from bigdl.llm.transformers import AutoModel, AutoModelForCausalLM
     from transformers import AutoTokenizer
-    import intel_extension_for_pytorch as ipex
-    if local_model_hub:
-        repo_model_name = repo_id.split("/")[1]
-        model_path = local_model_hub + "/" + repo_model_name
-    else:
-        model_path = repo_id
+    model_path = get_model_path(repo_id, local_model_hub)
     # Load model in 4 bit,
     # which convert the relevant layers in the model into INT4 format
     st = time.perf_counter()
@@ -334,28 +329,22 @@ def run_optimize_model_gpu(repo_id,
                            num_trials):
     from transformers import AutoModel, AutoModelForCausalLM, AutoTokenizer
     from bigdl.llm import optimize_model
-    import intel_extension_for_pytorch as ipex
-    if local_model_hub:
-        repo_model_name = repo_id.split("/")[1]
-        model_path = local_model_hub + "/" + repo_model_name
-    else:
-        model_path = repo_id
+    model_path = get_model_path(repo_id, local_model_hub)
     # Load model in 4 bit,
     # which convert the relevant layers in the model into INT4 format
     st = time.perf_counter()
     if repo_id in ['THUDM/chatglm-6b', 'THUDM/chatglm2-6b']:
         model = AutoModel.from_pretrained(model_path, torch_dtype='auto', low_cpu_mem_usage=True, trust_remote_code=True)
         model = optimize_model(model)
-        model = model.to('xpu')
         tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
     else:
-        model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype='auto', low_cpu_mem_usage=True)
+        model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype='auto', low_cpu_mem_usage=True, trust_remote_code=True)
         model = optimize_model(model)
-        model = model.to('xpu')
-        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
     end = time.perf_counter()
     print(">> loading of model costs {}s".format(end - st))
 
+    model = model.to('xpu')
     model = BenchmarkWrapper(model)
 
     result = {}
@@ -367,8 +356,10 @@ def run_optimize_model_gpu(repo_id,
             input_str = open(f"prompt/{in_len}.txt", 'r').read()
             # As different tokenizer has different encodings,
             # slice the input_ids to ensure the prompt length is required length.
-            input_ids = tokenizer.encode(input_str, return_tensors="pt").to('xpu')
+            input_ids = tokenizer.encode(input_str, return_tensors="pt")
             input_ids = input_ids[:, :in_len]
+            true_str = tokenizer.batch_decode(input_ids)[0]
+            input_ids = tokenizer.encode(true_str, return_tensors="pt").to('xpu')
             result[in_out] = []
             for i in range(num_trials + warm_up):
                 st = time.perf_counter()
