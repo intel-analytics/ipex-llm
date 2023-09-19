8fe678f5b2082ceada54d4705dc7f5f2f7aeb961
Ruonan Wang <105281011+rnwang04@users.noreply.github.com>
2023-09-19 15:38:54 +0800

    LLM: Support optimized kv_cache for baichuan family (#8997)
    * add initial support for baichuan attantion
    
    * support baichuan1
    
    * update based on comment
    
    * update based on comment
    
    * support baichuan2
    
    * update link, change how to jusge baichuan2
    
    * fix style
    
    * add model parameter for pob emb
    
    * update based on comment

--- python/llm/src/bigdl/llm/transformers/models/utils.py
+++ python/llm/src/bigdl/llm/transformers/models/utils.py
@@ -15,9 +15,12 @@
 #
 
 import torch
+from bigdl.llm.utils.common import invalidInputError
 
 
 def create_kv_cache(batch_size, num_heads, head_dim, current_length, max_length, dtype, device):
+    if device.type == 'xpu':
+        torch.xpu.empty_cache()
     key_cache_storage = torch.empty(batch_size, num_heads,
                                     max_length, head_dim,
                                     dtype=dtype, device=device)
@@ -46,3 +49,25 @@ def append_kv_cache(cache_k, cache_v, key_states, value_states):
     new_cache_v = cache_v.as_strided(new_size, cache_v.stride(), storage_offset=0)
     new_cache_v[:, :, cache_v.size(2):cache_k.size(2) + key_states.size(2), :] = value_states
     return new_cache_k, new_cache_v
+
+
+def rotate_half(x):
+    """Rotates half the hidden dims of the input."""
+    x1 = x[..., :x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2:]
+    return torch.cat((-x2, x1), dim=-1)
+
+
+def apply_rotary_pos_emb(q, k, cos, sin, position_ids, model_family):
+    if model_family in ["llama", "baichuan"]:
+        # The first two dimensions of cos and sin are always 1, so we can `squeeze` them.
+        cos = cos.squeeze(1).squeeze(0)  # [seq_len, dim]
+        sin = sin.squeeze(1).squeeze(0)  # [seq_len, dim]
+        cos = cos[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
+        sin = sin[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
+        q_embed = (q * cos) + (rotate_half(q) * sin)
+        k_embed = (k * cos) + (rotate_half(k) * sin)
+        return q_embed, k_embed
+    else:
+        invalidInputError(False,
+                          f"{model_family} is not supported.")
