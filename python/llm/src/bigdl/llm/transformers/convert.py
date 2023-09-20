384ac9f5bd693a255d1c5765b281efa57a6606f0
arda <arda@arda-arc11.sh.intel.com>
2023-09-20 16:28:44 +0800

    remove gptj arch

--- python/llm/src/bigdl/llm/transformers/convert.py
+++ python/llm/src/bigdl/llm/transformers/convert.py
@@ -174,14 +174,13 @@ def optimize(model):
                         chatglm_attention_forward
                         )
     elif "gptj" in model.config.model_type:
+        # dolly-v1-6b
         modeling_module_name = model.__class__.__module__
         module = importlib.import_module(modeling_module_name)
-        if "GPTJForCausalLM" in model.config.architectures:
-            # dolly-v1-6b
-            from bigdl.llm.transformers.models.gptj import gptj_attention_forward
-            convert_forward(model,
-                            module.GPTJAttention,
-                            gptj_attention_forward)
+        from bigdl.llm.transformers.models.gptj import gptj_attention_forward
+        convert_forward(model,
+                        module.GPTJAttention,
+                        gptj_attention_forward)
     elif "falcon" in model.config._name_or_path:
         modeling_module_name = model.__class__.__module__
         module = importlib.import_module(modeling_module_name)
