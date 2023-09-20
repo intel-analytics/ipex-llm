f4f72daf7ff10f103de3185d0d5b108da8332865
Kai Huang <huangkaivision@gmail.com>
2023-09-20 16:21:58 +0800

    grammar

--- python/llm/src/bigdl/llm/transformers/low_bit_linear.py
+++ python/llm/src/bigdl/llm/transformers/low_bit_linear.py
@@ -365,7 +365,7 @@ class LowBitLinear(nn.Linear):
             # CPU logic
             # todo may need to set a different number on different platforms
             invalidInputError(self.qtype != NF3 and self.qtype != NF4,
-                              "NF3 and NF4 quantization is currently not supported on CPU")
+                              "NF3 and NF4 quantization are currently not supported on CPU")
             if IS_SERVER and (not IS_SPR) and \
                     self.qtype == SYM_INT4 and x_2d.shape[0] >= TORCH_LINEAR_THRESHOLD:
                 x0_fp32 = ggml_int4_convert_fp32(x0, self.weight_shape, self.weight_length)
