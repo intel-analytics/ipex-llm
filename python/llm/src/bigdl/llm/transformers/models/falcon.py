92dbf96a56874bc44cb8ff8780ee96499187d3f2
lalalapotter <potterguang101@gmail.com>
2023-09-20 12:41:47 +0800

    fix.

--- python/llm/src/bigdl/llm/transformers/models/falcon.py
+++ python/llm/src/bigdl/llm/transformers/models/falcon.py
@@ -32,6 +32,7 @@
 # limitations under the License.
 """PyTorch Falcon model."""
 
+import math
 from typing import Optional, Tuple
 
 import torch
@@ -40,6 +41,7 @@ from bigdl.llm.transformers.models.utils import create_kv_cache, append_kv_cache
 
 
 KV_CACHE_ALLOC_BLOCK_LENGTH = 256
+
 
 def rw_attention_forward_7b(
     self,
@@ -58,13 +60,21 @@ def rw_attention_forward_7b(
 
     batch_size, q_length, _, _ = query_layer.shape
 
-    query_layer = query_layer.transpose(1, 2).reshape(batch_size * self.num_heads, q_length, self.head_dim)
+    query_layer = query_layer.transpose(1, 2).reshape(
+        batch_size * self.num_heads,
+        q_length,
+        self.head_dim
+    )
     key_layer = key_layer.transpose(1, 2).reshape(
         batch_size * self.num_kv,
         q_length,
         self.head_dim,
     )
-    value_layer = value_layer.transpose(1, 2).reshape(batch_size * self.num_kv, q_length, self.head_dim)
+    value_layer = value_layer.transpose(1, 2).reshape(
+        batch_size * self.num_kv,
+        q_length,
+        self.head_dim
+    )
 
     # query_layer, key_layer = self.maybe_rotary(query_layer, key_layer)
     _, seq_len, _ = query_layer.shape
@@ -153,18 +163,23 @@ def rw_attention_forward_7b(
         assert not output_attentions  # not supported.
         return outputs
     else:
-        attention_mask_float = (attention_mask * 1.0).masked_fill(attention_mask, -1e9).to(torch.bfloat16)
+        attention_mask_float = (attention_mask * 1.0) \
+            .masked_fill(attention_mask, -1e9).to(torch.bfloat16)
         matmul_result = query_layer @ key_layer.transpose(-1, -2)
 
         # change view to [batch_size, num_heads, q_length, kv_length]
         attention_scores = matmul_result.view(batch_size, self.num_heads, q_length, kv_length)
 
-        # cast attention scores to fp32, compute scaled softmax and cast back to initial dtype - [batch_size, num_heads, q_length, kv_length]
+        # cast attention scores to fp32,
+        # compute scaled softmax and cast back to initial dtype
+        #  - [batch_size, num_heads, q_length, kv_length]
         input_dtype = attention_scores.dtype
-        # `float16` has a minimum value of -65504.0, whereas `bfloat16` and `float32` have a minimum value of `-3.4e+38`
+        # `float16` has a minimum value of -65504.0,
+        # whereas `bfloat16` and `float32` have a minimum value of `-3.4e+38`
         if input_dtype == torch.float16 or input_dtype == torch.bfloat16:
             attention_scores = attention_scores.to(torch.float32)
-        # attn_weights = torch.masked_fill(attention_scores, attention_mask, torch.finfo(attention_scores.dtype).min)
+        # attn_weights = torch. \
+        # masked_fill(attention_scores, attention_mask, torch.finfo(attention_scores.dtype).min)
         attention_probs = F.softmax(
             (attention_scores + alibi) * self.inv_norm_factor + attention_mask_float,
             dim=-1,
@@ -177,7 +192,11 @@ def rw_attention_forward_7b(
             attention_probs = attention_probs * head_mask
 
         # change view [batch_size x num_heads, q_length, kv_length]
-        attention_probs_reshaped = attention_probs.view(batch_size * self.num_heads, q_length, kv_length)
+        attention_probs_reshaped = attention_probs.view(
+            batch_size * self.num_heads,
+            q_length,
+            kv_length
+        )
 
         # matmul: [batch_size * num_heads, q_length, head_dim]
         context_layer = attention_probs_reshaped @ value_layer
@@ -204,20 +223,29 @@ def rw_attention_forward_40b(
         use_cache: bool = False,
         output_attentions: bool = False,
     ):
-        fused_qkv = self.query_key_value(hidden_states)  # [batch_size, seq_length, 3 x hidden_size]
+        # [batch_size, seq_length, 3 x hidden_size]
+        fused_qkv = self.query_key_value(hidden_states)
 
         # 3 x [batch_size, seq_length, num_heads, head_dim]
         (query_layer, key_layer, value_layer) = self._split_heads(fused_qkv)
 
         batch_size, q_length, _, _ = query_layer.shape
 
-        query_layer = query_layer.transpose(1, 2).reshape(batch_size * self.num_heads, q_length, self.head_dim)
+        query_layer = query_layer.transpose(1, 2).reshape(
+            batch_size * self.num_heads,
+            q_length,
+            self.head_dim
+        )
         key_layer = key_layer.transpose(1, 2).reshape(
             batch_size * self.num_heads,
             q_length,
             self.head_dim,
         )
-        value_layer = value_layer.transpose(1, 2).reshape(batch_size * self.num_heads, q_length, self.head_dim)
+        value_layer = value_layer.transpose(1, 2).reshape(
+            batch_size * self.num_heads,
+            q_length,
+            self.head_dim
+        )
 
         # query_layer, key_layer = self.maybe_rotary(query_layer, key_layer)
         _, seq_len, _ = query_layer.shape
@@ -239,13 +267,15 @@ def rw_attention_forward_40b(
             cache_v = layer_past[1].view(batch_size, self.num_heads, -1, self.head_dim)
             if cache_k.stride()[1] <= cache_k.size(2) * cache_k.size(3):
                 # allocate new
-                new_cache_k, new_cache_v = create_kv_cache(batch_size,
-                                                        self.num_heads,  # Support GQA
-                                                        self.head_dim,
-                                                        cache_k.size(2),
-                                                        kv_length + KV_CACHE_ALLOC_BLOCK_LENGTH,
-                                                        dtype=cache_k.dtype,
-                                                        device=device)
+                new_cache_k, new_cache_v = create_kv_cache(
+                    batch_size,
+                    self.num_heads,  # Support GQA
+                    self.head_dim,
+                    cache_k.size(2),
+                    kv_length + KV_CACHE_ALLOC_BLOCK_LENGTH,
+                    dtype=cache_k.dtype,
+                    device=device
+                )
                 new_cache_k[:] = cache_k
                 new_cache_v[:] = cache_v
                 cache_k = new_cache_k
@@ -305,21 +335,27 @@ def rw_attention_forward_40b(
             assert not output_attentions  # not supported.
             return outputs
         else:
-            attention_mask_float = (attention_mask * 1.0).masked_fill(attention_mask, -1e9).to(torch.bfloat16)
+            attention_mask_float = (attention_mask * 1.0) \
+                .masked_fill(attention_mask, -1e9).to(torch.bfloat16)
             matmul_result = query_layer @ key_layer.transpose(-1, -2)
 
             # change view to [batch_size, num_heads, q_length, kv_length]
             attention_scores = matmul_result.view(batch_size, self.num_heads, q_length, kv_length)
 
-            # cast attention scores to fp32, compute scaled softmax and cast back to initial dtype - [batch_size, num_heads, q_length, kv_length]
+            # cast attention scores to fp32,
+            # compute scaled softmax and cast back to initial dtype
+            # - [batch_size, num_heads, q_length, kv_length]
             input_dtype = attention_scores.dtype
-            # `float16` has a minimum value of -65504.0, whereas `bfloat16` and `float32` have a minimum value of `-3.4e+38`
+            # `float16` has a minimum value of -65504.0,
+            # whereas `bfloat16` and `float32` have a minimum value of `-3.4e+38`
             if input_dtype == torch.float16 or input_dtype == torch.bfloat16:
                 attention_scores = attention_scores.to(torch.float32)
-            # attn_weights = torch.masked_fill(attention_scores, attention_mask, torch.finfo(attention_scores.dtype).min)
+            # attn_weights = torch \
+            # .masked_fill(
+            # attention_scores, attention_mask, torch.finfo(attention_scores.dtype).min)
             attention_probs = F.softmax(
-                (attention_scores + alibi.view(batch_size, self.num_heads, 1, -1)) * self.inv_norm_factor
-                + attention_mask_float,
+                (attention_scores + alibi.view(batch_size, self.num_heads, 1, -1))
+                * self.inv_norm_factor + attention_mask_float,
                 dim=-1,
                 dtype=hidden_states.dtype,
             )
@@ -330,7 +366,11 @@ def rw_attention_forward_40b(
                 attention_probs = attention_probs * head_mask
 
             # change view [batch_size x num_heads, q_length, kv_length]
-            attention_probs_reshaped = attention_probs.view(batch_size * self.num_heads, q_length, kv_length)
+            attention_probs_reshaped = attention_probs.view(
+                batch_size * self.num_heads,
+                q_length,
+                kv_length
+            )
 
             # matmul: [batch_size * num_heads, q_length, head_dim]
             context_layer = attention_probs_reshaped @ value_layer
@@ -356,20 +396,29 @@ def falcon_attention_forward(
         use_cache: bool = False,
         output_attentions: bool = False,
     ):
-        fused_qkv = self.query_key_value(hidden_states)  # [batch_size, seq_length, 3 x hidden_size]
+        # [batch_size, seq_length, 3 x hidden_size]
+        fused_qkv = self.query_key_value(hidden_states)
         num_kv_heads = self.num_heads if self.new_decoder_architecture else self.num_kv_heads
         # 3 x [batch_size, seq_length, num_heads, head_dim]
         (query_layer, key_layer, value_layer) = self._split_heads(fused_qkv)
 
         batch_size, query_length, _, _ = query_layer.shape
 
-        query_layer = query_layer.transpose(1, 2).reshape(batch_size * self.num_heads, query_length, self.head_dim)
+        query_layer = query_layer.transpose(1, 2).reshape(
+            batch_size * self.num_heads,
+            query_length,
+            self.head_dim
+        )
         key_layer = key_layer.transpose(1, 2).reshape(
             batch_size * num_kv_heads,
             query_length,
             self.head_dim,
         )
-        value_layer = value_layer.transpose(1, 2).reshape(batch_size * num_kv_heads, query_length, self.head_dim)
+        value_layer = value_layer.transpose(1, 2).reshape(
+            batch_size * num_kv_heads,
+            query_length,
+            self.head_dim
+        )
 
         past_kv_length = 0 if layer_past is None else layer_past[0].shape[1]
         query_layer, key_layer = self.maybe_rotary(query_layer, key_layer, past_kv_length)
@@ -414,15 +463,16 @@ def falcon_attention_forward(
             key_layer = new_key_states
             value_layer = new_value_states
 
-        query_layer = query_layer.view(batch_size*self.num_heads, -1, self.head_dim)
-        key_layer = key_layer.view(batch_size*self.num_heads, -1, self.head_dim)
-        value_layer = value_layer.view(batch_size*self.num_heads, -1, self.head_dim)
+        query_layer = query_layer.view(batch_size * self.num_heads, -1, self.head_dim)
+        key_layer = key_layer.view(batch_size * self.num_heads, -1, self.head_dim)
+        value_layer = value_layer.view(batch_size * self.num_heads, -1, self.head_dim)
         if use_cache:
             present = (key_layer, value_layer)
         else:
             present = None
 
-        attention_mask_float = (attention_mask * 1.0).masked_fill(attention_mask, float("-1e9")).to(query_layer.dtype)
+        attention_mask_float = (attention_mask * 1.0) \
+            .masked_fill(attention_mask, float("-1e9")).to(query_layer.dtype)
 
         query_layer_ = query_layer.reshape(batch_size, self.num_heads, -1, self.head_dim)
         key_layer_ = key_layer.reshape(batch_size, num_kv_heads, -1, self.head_dim)
@@ -441,13 +491,22 @@ def falcon_attention_forward(
                 attn_output = attention_scores @ value_layer_
             else:
                 attn_output = F.scaled_dot_product_attention(
-                    query_layer_, key_layer_, value_layer_, attention_mask_float, 0.0, is_causal=False
+                    query_layer_,
+                    key_layer_,
+                    value_layer_,
+                    attention_mask_float,
+                    0.0,
+                    is_causal=False
                 )
                 attention_scores = None
 
             attn_output = attn_output.view(batch_size, self.num_heads, query_length, self.head_dim)
             attn_output = attn_output.permute(0, 2, 1, 3)
-            attn_output = attn_output.reshape(batch_size, query_length, self.num_heads * self.head_dim)
+            attn_output = attn_output.reshape(
+                batch_size,
+                query_length,
+                self.num_heads * self.head_dim
+            )
 
             output_tensor = self.dense(attn_output)
 
@@ -460,20 +519,31 @@ def falcon_attention_forward(
             matmul_result = query_layer_ @ key_layer_.transpose(-1, -2)
 
             # change view to [batch_size, num_heads, q_length, kv_length]
-            attention_scores = matmul_result.view(batch_size, self.num_heads, query_length, kv_length)
+            attention_scores = matmul_result.view(
+                batch_size,
+                self.num_heads,
+                query_length,
+                kv_length
+            )
 
-            # cast attention scores to fp32, compute scaled softmax and cast back to initial dtype - [batch_size, num_heads, q_length, kv_length]
+            # cast attention scores to fp32,
+            # compute scaled softmax and cast back to initial dtype
+            # - [batch_size, num_heads, q_length, kv_length]
             input_dtype = attention_scores.dtype
-            # `float16` has a minimum value of -65504.0, whereas `bfloat16` and `float32` have a minimum value of `-3.4e+38`
+            # `float16` has a minimum value of -65504.0,
+            # whereas `bfloat16` and `float32` have a minimum value of `-3.4e+38`
             if input_dtype == torch.float16 or input_dtype == torch.bfloat16:
                 attention_scores = attention_scores.to(torch.float32)
             # Matt (HF) note: We could possibly use F.scaled_dot_product_attention here too, by
-            # adding (alibi * self.inv_norm_factor) to attention_mask_float. I think this would be mathematically
-            # equivalent and more performant, but there might be a numerical difference. If you're reading this
+            # adding (alibi * self.inv_norm_factor) to attention_mask_float.
+            # I think this would be mathematically
+            # equivalent and more performant, but there might be a numerical difference.
+            # If you're reading this
             # and you'd like to experiment and maybe file a PR, feel free!
             attention_logits = attention_scores + alibi.view(batch_size, self.num_heads, 1, -1)
             attention_logits *= self.inv_norm_factor
-            attention_probs = F.softmax(attention_logits + attention_mask_float, dim=-1, dtype=hidden_states.dtype)
+            attention_probs = F.softmax(attention_logits + attention_mask_float, dim=-1,
+                                        dtype=hidden_states.dtype)
             # [batch_size, num_heads, q_length, kv_length]
             attention_probs = self.attention_dropout(attention_probs)
 
@@ -481,7 +551,12 @@ def falcon_attention_forward(
                 attention_probs = attention_probs * head_mask
 
             # change view [batch_size, num_heads, q_length, kv_length]
-            attention_probs_reshaped = attention_probs.view(batch_size, self.num_heads, query_length, kv_length)
+            attention_probs_reshaped = attention_probs.view(
+                batch_size,
+                self.num_heads,
+                query_length,
+                kv_length
+            )
 
             # matmul: [batch_size * num_heads, q_length, head_dim]
             context_layer = (attention_probs_reshaped @ value_layer_).flatten(0, 1)
