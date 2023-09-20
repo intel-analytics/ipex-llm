126cc3b59792d9b30199a656a9a97e787e42181d
hzjane <a1015616934@qq.com>
2023-09-20 22:00:14 +0800

    remove verify AS and register

--- ppml/trusted-big-data-ml/scala/docker-occlum/llm/run_llm_on_occlum_glibc.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/llm/run_llm_on_occlum_glibc.sh
@@ -163,48 +163,20 @@ build_spark() {
     occlum build
 
     #before start occlum app after occlum build
+
+    #attestation
     if [[ $ATTESTATION == "true" ]]; then
         if [[ $PCCS_URL == "" ]]; then
             echo "[ERROR] Attestation set to true but NO PCCS"
             exit 1
         else
-            #verify ehsm service
-            cd /opt/
-            bash verify-attestation-service.sh
-            #register application
-
-            #get mrenclave mrsigner
-            MR_ENCLAVE_temp=$(bash print_enclave_signer.sh | grep mr_enclave)
-            MR_ENCLAVE_temp_arr=(${MR_ENCLAVE_temp})
-            export MR_ENCLAVE=${MR_ENCLAVE_temp_arr[1]}
-            MR_SIGNER_temp=$(bash print_enclave_signer.sh | grep mr_signer)
-            MR_SIGNER_temp_arr=(${MR_SIGNER_temp})
-            export MR_SIGNER=${MR_SIGNER_temp_arr[1]}
-
-            #register and get policy_Id
-            policy_Id_temp=$(bash register.sh | grep policy_Id)
-            policy_Id_temp_arr=(${policy_Id_temp})
-            export policy_Id=${policy_Id_temp_arr[1]}
-        fi
-        #register error
-        if [[ $? -gt 0 || -z "$policy_Id" ]]; then
-            echo "can not get policy_Id, register fail"
-            exit 1;
-        fi
-    fi
-
-    #attestation
-    if [[ $ATTESTATION == "true" ]]; then
-        if [[ $PCCS_URL == "" ]]; then
-            echo "[ERROR] Attestation set to /root/demos/remote_attestation/dcaprue but NO PCCS"
-            exit 1
-        else
                 #generate dcap quote
                 cd /opt/occlum_spark
-                occlum run /bin/dcap_c_test $REPORT_DATA
+                occlum start
+                occlum exec /bin/dcap_c_test $REPORT_DATA
                 echo "generate quote success"
                 #attest quote
-                occlum run /usr/lib/jvm/java-8-openjdk-amd64/bin/java \
+                occlum exec /usr/lib/jvm/java-8-openjdk-amd64/bin/java \
                             -XX:-UseCompressedOops \
                             -XX:ActiveProcessorCount=4 \
                             -Divy.home="/tmp/.ivy" \
