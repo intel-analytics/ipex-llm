481a715d44710f9c20278bd571db2f6c11715713
Kai Huang <huangkaivision@gmail.com>
2023-09-20 19:59:19 +0800

    Optimize kv_cache for gpt-neox model family (#9015)
    * override gptneox
    
    * style
    
    * move to utils
    
    * revert

--- python/llm/src/bigdl/llm/transformers/models/utils.py
+++ python/llm/src/bigdl/llm/transformers/models/utils.py
@@ -68,6 +68,14 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids, model_family):
         q_embed = (q * cos) + (rotate_half(q) * sin)
         k_embed = (k * cos) + (rotate_half(k) * sin)
         return q_embed, k_embed
+    elif model_family == "gpt_neox":
+        gather_indices = position_ids[:, None, :, None]  # [bs, 1, seq_len, 1]
+        gather_indices = gather_indices.repeat(1, cos.shape[1], 1, cos.shape[3])
+        cos = torch.gather(cos.repeat(gather_indices.shape[0], 1, 1, 1), 2, gather_indices)
+        sin = torch.gather(sin.repeat(gather_indices.shape[0], 1, 1, 1), 2, gather_indices)
+        q_embed = (q * cos) + (rotate_half(q) * sin)
+        k_embed = (k * cos) + (rotate_half(k) * sin)
+        return q_embed, k_embed
     else:
         invalidInputError(False,
                           f"{model_family} is not supported.")
