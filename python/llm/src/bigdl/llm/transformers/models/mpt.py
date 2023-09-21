cfb9965b7e509f52576f053d062b754f98ccfeba
jenniew <jenniewang123@gmail.com>
2023-09-20 19:25:32 -0700

    update

--- python/llm/src/bigdl/llm/transformers/models/mpt.py
+++ python/llm/src/bigdl/llm/transformers/models/mpt.py
@@ -14,24 +14,6 @@
 # limitations under the License.
 #
 # Some parts of this file is adapted from
-# https://github.com/huggingface/transformers/blob/v4.31.0/src/transformers/models/llama/modeling_llama.py
-# which is licensed under Apache License 2.0:
-#
-# Copyright 2021 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# This file is adapted from
 # https://huggingface.co/mosaicml/mpt-7b/blob/main/attention.py
 #
 
