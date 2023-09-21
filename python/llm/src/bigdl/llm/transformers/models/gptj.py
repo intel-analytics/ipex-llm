bf51ec40b22f379fe34821c1b1563f2bdab58462
Ruonan Wang <105281011+rnwang04@users.noreply.github.com>
2023-09-21 17:16:07 +0800

    LLM: Fix empty cache (#9024)
    * fix
    
    * fix
    
    * update example

--- python/llm/src/bigdl/llm/transformers/models/gptj.py
+++ python/llm/src/bigdl/llm/transformers/models/gptj.py
@@ -144,6 +144,8 @@ def gptj_attention_forward(
         past_length = cache_k.size(2)
 
         if cache_k.stride()[1] <= cache_k.size(2) * cache_k.size(3):
+            if device.type == 'xpu':
+                torch.xpu.empty_cache()
             new_cache_k, new_cache_v = create_kv_cache(batch_size,
                                                        self.num_attention_heads,
                                                        self.head_dim,
