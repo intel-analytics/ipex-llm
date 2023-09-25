9f8c9102954ece3bdf06b9880cc05d694bb36ff0
Wang Jian <61138589+hzjane@users.noreply.github.com>
2023-09-25 16:08:01 +0800

    [PPML] Add occlum llm image attestation service (#9007)
    * add attest files
    
    * add ppml libs
    
    * add py attest files
    
    * fix
    
    * fix
    
    * fix
    
    * remove verify AS and register
    
    * update to use occlum exec
    
    * add occlum generate.py
    
    * fix and update
    
    * add register SGX
    
    * update and fix
    
    * add readme
    
    * using main cli and update
    
    * fix
    
    * update and fix
    
    * update
    
    * change as_Type to behavior
    
    * fix typo

---
+++ ppml/trusted-big-data-ml/scala/docker-occlum/llm/attestation/attestation.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Attestation
+if [ -z "$ATTESTATION" ]; then
+    echo "[INFO] Attestation is disabled!"
+    ATTESTATION="false"
+elif [ "$ATTESTATION" = "true" ]; then
+  echo "[INFO] Attestation is enabled!"
+  # Build ATTESTATION_COMMAND
+  if [ -z "$ATTESTATION_URL" ]; then
+    echo "[ERROR] Attestation is enabled, but ATTESTATION_URL is empty!"
+    echo "[INFO] PPML Application Exit!"
+    exit 1
+  fi
+  if [ -z "$APP_ID" ]; then
+    echo "[ERROR] Attestation is enabled, but APP_ID is empty!"
+    echo "[INFO] PPML Application Exit!"
+    exit 1
+  fi
+  if [ -z "$API_KEY" ]; then
+    echo "[ERROR] Attestation is enabled, but API_KEY is empty!"
+    echo "[INFO] PPML Application Exit!"
+    exit 1
+  fi
+  if [ -z "$REPORT_DATA" ]; then
+    echo "[INFO] Attestation is enabled, use default REPORT_DATA ppml"
+    export REPORT_DATA="ppml"
+  fi
+  cd /opt/occlum_spark
+  occlum exec /bin/dcap_c_test $REPORT_DATA
+  echo "generate quote success"
+  echo "cd /opt/occlum_spark" > /opt/temp_command_file
+  ATTESTATION_COMMAND="occlum exec /bin/python3 /opt/attestation_cli.py -u ${ATTESTATION_URL} -i ${APP_ID} -k ${API_KEY} -O Occlum"
+  ## default is null
+  if [ -n "$ATTESTATION_POLICYID" ]; then
+    ATTESTATION_COMMAND="${ATTESTATION_COMMAND} -o ${ATTESTATION_POLICYID}"
+  fi
+  ## default is BigDL
+  if [ -n "$ATTESTATION_TYPE" ]; then
+    ATTESTATION_COMMAND="${ATTESTATION_COMMAND} -t ${ATTESTATION_TYPE}"
+  fi
+  echo $ATTESTATION_COMMAND >> /opt/temp_command_file
+  echo 'if [ $? -gt 0 ]; then ' >> /opt/temp_command_file
+  echo '  exit 1' >> /opt/temp_command_file
+  echo 'fi' >> /opt/temp_command_file
+fi
--- python/ppml/src/bigdl/ppml/attestation/attestation_cli.py
+++ python/ppml/src/bigdl/ppml/attestation/attestation_cli.py
@@ -14,8 +14,9 @@
 # limitations under the License.
 #
 
-import attestation_service, quote_generator
+from bigdl.ppml.attestation import attestation_service, quote_generator
 import argparse
+import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -26,6 +27,9 @@ if __name__ == "__main__":
     parser.add_argument('-O', '--quote_type', type=str, help='quote type', default='TDX')
     parser.add_argument('-o', '--policy_id', type=str, help='policy id', default='')
     parser.add_argument('-p', '--user_report', type=str, help='user report', default='ppml')
+    parser.add_argument("-b", "--behavior", type=str, help="the type of attestation behavior: attest or register", default='attest')
+    parser.add_argument("-e", "--mr_enclave", type=str, help="mrEnclave", default='')
+    parser.add_argument("-s", "--mr_signer", type=str, help="mrSigner", default='')
     parser.add_argument('--proxy', type=str, help='proxy', default='')
     args = parser.parse_args()
 
@@ -33,21 +37,36 @@ if __name__ == "__main__":
         'http': args.proxy,
         'https': args.proxy
     }
-    if args.quote_type == "TDX":
-        quote = quote_generator.generate_tdx_quote(args.user_report)
-    elif args.quote_type == "Gramine":
-        quote = quote_generator.generate_gramine_quote(args.user_report)
 
-    if args.as_type == "BigDL":
-        attestation_result = attestation_service.bigdl_attestation_service(args.url, args.app_id, args.api_key, quote, args.policy_id)
-    elif args.as_type == "Amber":
-        policy_list = args.policy_id.split(",")
-        attestation_result = attestation_service.amber(args.url, args.api_key, quote, policy_list, proxies)
+    if args.behavior == "attest":
+        if args.quote_type == "TDX":
+            quote = quote_generator.generate_tdx_quote(args.user_report)
+        elif args.quote_type == "Gramine":
+            quote = quote_generator.generate_gramine_quote(args.user_report)
+        elif args.quote_type == "Occlum":
+            quote = quote_generator.generate_occlum_quote()
+        attestation_result = -1
+        if args.as_type == "BigDL":
+            attestation_result = attestation_service.bigdl_attestation_service(args.url, args.app_id, args.api_key, quote, args.policy_id)
+        elif args.as_type == "Amber":
+            policy_list = args.policy_id.split(",")
+            attestation_result = attestation_service.amber(args.url, args.api_key, quote, policy_list, proxies)
 
-    if attestation_result == 0:
-        print("Attestation Success!")
-    if attestation_result == 1:
-        print("WARNING: Attestation pass but BIOS or the software is out of date.")
-        print("Attestation Success!")
-    if attestation_result == -1:
-        print("Attestation Failed!")
+        if attestation_result == 0:
+            print("Attestation Success!")
+        if attestation_result == 1:
+            print("WARNING: Attestation pass but BIOS or the software is out of date.")
+            print("Attestation Success!")
+        if attestation_result == -1:
+            print("Attestation Failed!")
+            sys.exit(1)
+    elif args.behavior == "register":
+        register_result = -1
+        if args.as_type == "BigDL":
+            register_result = attestation_service.bigdl_attestation_service_register(args.url, args.app_id, args.api_key, "SGXMREnclavePolicy", args.mr_enclave, args.mr_signer)
+        if register_result == -1:
+            print("register Failed!")
+            sys.exit(1)
+        else:
+            print("register Success!")
+            print("policyID: " + register_result)
