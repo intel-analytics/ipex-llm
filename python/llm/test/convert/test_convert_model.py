f4ea7f97cafe7e839e2e8b8345bdde6bba8d3a10
leonardozcm <leonardozcm@gmail.com>
2023-10-07 10:18:17 +0000

    avoid win python bug

--- python/llm/test/convert/test_convert_model.py
+++ python/llm/test/convert/test_convert_model.py
@@ -33,20 +33,25 @@ output_dir = os.environ.get('INT4_CKPT_DIR')
 
 def optimize_transformers_llm_test_pipeline(output_dir, model_path):
 
-    with tempfile.TemporaryDirectory(dir=output_dir) as tempdir:
-        model = AutoModelForCausalLM.from_pretrained(model_path,
+    tempdir = tempfile.mkdtemp(dir=output_dir)
+    try:
+        from transformers import AutoModelForCausalLM as AutoCLM
+        model = AutoCLM.from_pretrained(model_path,
                                         torch_dtype="auto",
                                         low_cpu_mem_usage=True,
                                         trust_remote_code=True)
         model = optimize_model(model)
         model.save_low_bit(tempdir)
         with low_memory_init():
-            new_model = AutoModelForCausalLM.from_pretrained(tempdir,
+            new_model = AutoCLM.from_pretrained(tempdir,
                                             torch_dtype="auto",
                                             trust_remote_code=True)
         new_model = load_low_bit(new_model,
                             model_path=tempdir)
         assert new_model is not None
+    finally:
+        if os.path.exists(tempdir):
+            os.rmdir(tempdir)
 
 class TestConvertModel(TestCase):
     
