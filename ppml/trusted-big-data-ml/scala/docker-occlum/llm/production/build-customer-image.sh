1e543691e3ddfc54e450f9d5bdf8fcb45a355f04
hzjane <a1015616934@qq.com>
2023-10-12 09:26:10 +0800

    update build customer

--- ppml/trusted-big-data-ml/scala/docker-occlum/llm/production/build-customer-image.sh
+++ ppml/trusted-big-data-ml/scala/docker-occlum/llm/production/build-customer-image.sh
@@ -1,7 +1,4 @@
-export HTTP_PROXY_HOST=your_http_proxy_host
-export HTTP_PROXY_PORT=your_http_proxy_port
-export HTTPS_PROXY_HOST=your_https_proxy_host
-export HTTPS_PROXY_PORT=your_https_proxy_port
+
 
 export image=intelanalytics/bigdl-ppml-trusted-llm-fastchat-occlum-production
 export TAG=2.4.0-SNAPSHOT-build
@@ -9,11 +6,8 @@ export image_customer=${image}-customer
 pwd
 docker build \
   --no-cache=true \
-  --build-arg HTTP_PROXY_HOST=${HTTP_PROXY_HOST_2} \
-  --build-arg HTTP_PROXY_PORT=${HTTP_PROXY_PORT_2} \
-  --build-arg HTTPS_PROXY_HOST=${HTTP_PROXY_HOST_2} \
-  --build-arg HTTPS_PROXY_PORT=${HTTP_PROXY_PORT_3} \
+  --build-arg http_proxy=${HTTP_PROXY} \
+  --build-arg https_proxy=${HTTP_PROXYS} \
   --build-arg no_proxy=${NO_PROXY} \
   --build-arg FINAL_NAME=${image}:${TAG} \
-  --build-arg SPARK_JAR_REPO_URL=${SPARK_JAR_REPO_URL} \
   -t ${image_customer}:${TAG} -f ./production/customer/Dockerfile .
