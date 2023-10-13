1d77cc43a3cf7d73d1256701c1f0a4d4e8e34584
NovTi <yx2432@nyu.edu>
2023-10-13 02:03:54 +0000

    Add sd module to nano

--- python/nano/setup.py
+++ python/nano/setup.py
@@ -144,6 +144,13 @@ def setup_package():
                         "packaging",
                         "sigfig",
                         "setuptools<66"]
+    
+    # diffusion requires
+    diffusion_requires = pytorch_requires + [
+        "diffusers==0.14.0",
+        "transformers",
+        "openvino-dev==2022.2.0"
+    ]
 
     package_data = [
         "libs/libjemalloc.so",
@@ -178,7 +185,8 @@ def setup_package():
                         "pytorch_113_xpu": pytorch_113_xpu_requires,
                         "pytorch_20_xpu": pytorch_20_xpu_requires,
                         "pytorch_nightly": pytorch_nightly_requires,
-                        "inference": inference_requires},
+                        "inference": inference_requires,
+                        "diffusion": diffusion_requires},
         package_data={"bigdl.nano": package_data},
         scripts=scripts,
         package_dir={"": "src"},
