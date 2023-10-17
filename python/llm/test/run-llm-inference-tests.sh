2bb420f5c004d66682ab7495f79bb4b261bb42d6
CheenHau Tan <33478814+chtanch@users.noreply.github.com>
2023-10-17 11:13:34 +0800

    Print out each passed test

--- python/llm/test/run-llm-inference-tests.sh
+++ python/llm/test/run-llm-inference-tests.sh
@@ -9,13 +9,13 @@ set -e
 echo "# Start testing inference"
 start=$(date "+%s")
 
-python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k "not test_transformers"
+python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k "not test_transformers" -v
 
 if [ -z "$THREAD_NUM" ]; then
   THREAD_NUM=2
 fi
 export OMP_NUM_THREADS=$THREAD_NUM
-python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k test_transformers
+python -m pytest -s ${LLM_INFERENCE_TEST_DIR} -k test_transformers -v
 
 now=$(date "+%s")
 time=$((now-start))
