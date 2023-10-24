ca41ac8a4c92f83dceb6fa3daa279ce0d70c7b48
WeiguangHan <weiguang.han@intel.com>
2023-10-24 15:05:16 +0800

    reslove some comments

--- python/llm/test/benchmark/csv_to_html.py
+++ python/llm/test/benchmark/csv_to_html.py
@@ -15,16 +15,25 @@
 #
 
 # Python program to convert CSV to HTML Table
- 
+
 import os
+import sys
+import argparse
 import pandas as pd
 
-folder_path = "../../dev/benchmark/all-in-one"
-csv_files = []
+def main():
+    parser = argparse.ArgumentParser(description="convert .csv file to .html file")
+    parser.add_argument("-f", "--folder_path", type=str, dest="folder_path",
+                        help="The directory which stores the .csv file", default="../../dev/benchmark/all-in-one")
+    args = parser.parse_args()
 
-for file_name in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, file_name)
-    if os.path.isfile(file_path) and file_name.endswith(".csv"):
-        csv_files.append(file_path)
+    csv_files = []
+    for file_name in os.listdir(args.folder_path):
+        file_path = os.path.join(args.folder_path, file_name)
+        if os.path.isfile(file_path) and file_name.endswith(".csv"):
+            csv_files.append(file_path)
 
-a = pd.read_csv(csv_files[0], index_col=0).to_html(csv_files[0].split("/")[-1].split(".")[0]+".html")
+    a = pd.read_csv(csv_files[0], index_col=0).to_html(csv_files[0].split("/")[-1].split(".")[0]+".html")
+
+if __name__ == "__main__":
+    sys.exit(main())
