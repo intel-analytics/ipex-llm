4b7e929ddece76587465dc3f0195bfead0c067ea
songhappy <guoqiongsong@gmail.com>
2023-10-25 00:21:50 -0700

    update

--- python/llm/example/CPU/HF-Transformers-AutoModels/streaming/streaming_llm/enable_streaming_llm.py
+++ python/llm/example/CPU/HF-Transformers-AutoModels/streaming/streaming_llm/enable_streaming_llm.py
@@ -47,7 +47,7 @@ from .kv_cache import StartRecentKVCache
 def enable_streaming_llm(model, start_size, recent_size):
     if "llama" in model.config.model_type:
         k_seq_dim = v_seq_dim = 2
-        from pos_shift.modify_llama import (
+        from .pos_shift.modify_llama import (
             enable_llama_pos_shift_attention,
         )
 
@@ -57,7 +57,7 @@ def enable_streaming_llm(model, start_size, recent_size):
         k_seq_dim = 3
     elif "gpt_neox" in model.config.model_type:
         k_seq_dim = v_seq_dim = 2
-        from pos_shift.modify_gpt_neox import (
+        from .pos_shift.modify_gpt_neox import (
             enable_gpt_neox_pos_shift_attention,
         )
 
@@ -65,7 +65,7 @@ def enable_streaming_llm(model, start_size, recent_size):
     elif "falcon" in model.config.model_type:
         v_seq_dim = 1
         k_seq_dim = 1
-        from pos_shift.modify_falcon import (
+        from .pos_shift.modify_falcon import (
             enable_falcon_pos_shift_attention,
         )
 
