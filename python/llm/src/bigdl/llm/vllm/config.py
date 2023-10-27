fcd375b21e39b266794c78d6ab15939f0a005a8f
gc-fu <guancheng.fu@intel.com>
2023-10-27 10:16:50 +0800

    add parallelconfig back

--- python/llm/src/bigdl/llm/vllm/config.py
+++ python/llm/src/bigdl/llm/vllm/config.py
@@ -9,6 +9,37 @@ logger = init_logger(__name__)
 
 _GB = 1 << 30
 
+# TODO(gc): later we can decide to remove this
+class ParallelConfig:
+    """Configuration for the distributed execution.
+
+    Args:
+        pipeline_parallel_size: Number of pipeline parallel groups.
+        tensor_parallel_size: Number of tensor parallel groups.
+        worker_use_ray: Whether to use Ray for model workers. Will be set to
+            True if either pipeline_parallel_size or tensor_parallel_size is
+            greater than 1.
+    """
+
+    def __init__(
+        self,
+        pipeline_parallel_size: int,
+        tensor_parallel_size: int,
+        worker_use_ray: bool,
+    ) -> None:
+        self.pipeline_parallel_size = pipeline_parallel_size
+        self.tensor_parallel_size = tensor_parallel_size
+        self.worker_use_ray = worker_use_ray
+
+        self.world_size = pipeline_parallel_size * tensor_parallel_size
+        if self.world_size > 1:
+            self.worker_use_ray = True
+        self._verify_args()
+
+    def _verify_args(self) -> None:
+        if self.pipeline_parallel_size > 1:
+            raise NotImplementedError(
+                "Pipeline parallelism is not supported yet.")
 
 # TODO(gc): later handle this function, seems that need more things.
 def get_config(model: str,
