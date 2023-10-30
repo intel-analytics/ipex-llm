379f12182a6fef4e509507b5f0d6937dd7b2ac61
Cheen Hau <cheen.hau.tan@intel.com>
2023-10-30 16:45:27 +0800

    Add test script and workflow for qlora fine-tuning

---
+++ python/llm/test/run-llm-finetune-tests-gpu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+
+set -e
+
+echo "# Start testing qlora fine-tuning"
+start=$(date "+%s")
+
+python ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/QLoRA-FineTuning/qlora_finetuning.py \
+--repo-id-or-model-path ${LLAMA2_7B_ORIGIN_PATH} \
+--dataset /mnt/disk1/datasets/abirate_english_quotes
+
+python ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/QLoRA-FineTuning/export_merged_model.py \
+--repo-id-or-model-path ${LLAMA2_7B_ORIGIN_PATH} \
+--adapter_path ${PWD}/outputs/checkpoint-200 \
+--output_path ${PWD}/outputs/checkpoint-200-merged
+
+
+now=$(date "+%s")
+time=$((now-start))
+
+echo "qlora fine-tuning tests finished"
+echo "Time used:$time seconds"
