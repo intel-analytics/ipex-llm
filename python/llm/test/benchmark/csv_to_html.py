408e3419173e643e28d95461c2d35c8ab1b48966
WeiguangHan <weiguang.han@intel.com>
2023-10-31 19:39:05 +0800

    modify some code logics

--- python/llm/test/benchmark/csv_to_html.py
+++ python/llm/test/benchmark/csv_to_html.py
@@ -34,10 +34,9 @@ def main():
             csv_files.append(file_path)
     csv_files.sort(reverse=True)
 
-    if len(csv_files)<=1:
-        pd.read_csv(csv_files[0], index_col=0).to_html(csv_files[0].split("/")[-1].split(".")[0]+".html")
-    else:
-        data1 = pd.read_csv(csv_files[0], index_col=0)
+    data1 = pd.read_csv(csv_files[0], index_col=0)
+
+    if len(csv_files)>1:
         data2 = pd.read_csv(csv_files[1], index_col=0)
 
         origin_column_1='1st token avg latency (ms)'
@@ -45,7 +44,6 @@ def main():
 
         added_column_1='last1'
         added_column_2='diff1(%)'
-
         added_column_3='last2'
         added_column_4='diff2(%)'
 
@@ -53,8 +51,9 @@ def main():
         data1.insert(loc=4,column=added_column_2,value=round((data2[origin_column_1]-data1[origin_column_1])*100/data2[origin_column_1],2))
         data1.insert(loc=5,column=added_column_3,value=data2[origin_column_2])
         data1.insert(loc=6,column=added_column_4,value=round((data2[origin_column_2]-data1[origin_column_2])*100/data2[origin_column_2],2))
-        daily_html=csv_files[0].split(".")[0]+".html"
-        data1.to_html(daily_html)
+
+    daily_html=csv_files[0].split(".")[0]+".html"
+    data1.to_html(daily_html)
 
 if __name__ == "__main__":
     sys.exit(main())
