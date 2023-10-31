d3cbf9c5f0a8ed22a29e9550195a2f07e5afd766
Cheen Hau <cheen.hau.tan@intel.com>
2023-10-31 16:57:39 +0800

    Reduce number of training steps

--- python/llm/test/run-llm-finetune-tests-gpu.sh
+++ python/llm/test/run-llm-finetune-tests-gpu.sh
@@ -7,14 +7,17 @@ set -e
 echo "# Start testing qlora fine-tuning"
 start=$(date "+%s")
 
+sed -i 's/max_steps=200/max_steps=2/; s/save_steps=100/save_steps=2/; s/logging_steps=20/logging_steps=1/' \
+    ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/QLoRA-FineTuning/qlora_finetuning.py
+
 python ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/QLoRA-FineTuning/qlora_finetuning.py \
 --repo-id-or-model-path ${LLAMA2_7B_ORIGIN_PATH} \
 --dataset ${ABIRATE_ENGLISH_QUOTES_PATH}
 
 python ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/QLoRA-FineTuning/export_merged_model.py \
 --repo-id-or-model-path ${LLAMA2_7B_ORIGIN_PATH} \
---adapter_path ${PWD}/outputs/checkpoint-200 \
---output_path ${PWD}/outputs/checkpoint-200-merged
+--adapter_path ${PWD}/outputs/checkpoint-2 \
+--output_path ${PWD}/outputs/checkpoint-2-merged
 
 
 now=$(date "+%s")
