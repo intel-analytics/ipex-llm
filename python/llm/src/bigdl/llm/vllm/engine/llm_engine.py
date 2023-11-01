9afe9f8c3934cf70d887be3413d8040340737e49
gc-fu <guancheng.fu@intel.com>
2023-11-01 10:28:16 +0800

    refine

--- python/llm/src/bigdl/llm/vllm/engine/llm_engine.py
+++ python/llm/src/bigdl/llm/vllm/engine/llm_engine.py
@@ -30,6 +30,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
 import time
 from typing import TYPE_CHECKING, Any, Iterable, List, Optional, Tuple, Union
 
