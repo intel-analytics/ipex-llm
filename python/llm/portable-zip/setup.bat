236a038274f88e58c5e746438ccf391a372d9e61
jinbridge <2635480475@qq.com>
2023-11-02 01:01:26 +0800

    fix: setup.bat

--- python/llm/portable-zip/setup.bat
+++ python/llm/portable-zip/setup.bat
@@ -1,20 +1,20 @@
 :: download python and extract zip
-powershell -Command "Start-BitsTransfer -Source https://www.python.org/ftp/python/3.9.13/python-3.9.13-embed-amd64.zip -Destination .\python\llm\portable-zip\python-3.9.13-embed-amd64.zip"
-powershell -Command "Expand-Archive .\python\llm\portable-zip\python-3.9.13-embed-amd64.zip -DestinationPath .\python\llm\portable-zip\python-embed"
+powershell -Command "Start-BitsTransfer -Source https://www.python.org/ftp/python/3.9.13/python-3.9.13-embed-amd64.zip -Destination python-3.9.13-embed-amd64.zip"
+powershell -Command "Expand-Archive .\python-3.9.13-embed-amd64.zip -DestinationPath .\python-embed"
 del .\python-3.9.13-embed-amd64.zip
 
-set "python-embed=.\python\llm\portable-zip\python-embed\python.exe"
+set "python-embed=.\python-embed\python.exe"
 
 :: download get-pip.py and install
-powershell -Command "Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile .\python\llm\portable-zip\python-embed\get-pip.py"
-%python-embed% .\python\llm\portable-zip\python-embed\get-pip.py
+powershell -Command "Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile .\python-embed\get-pip.py"
+%python-embed% .\python-embed\get-pip.py
 
 :: enable run site.main() automatically
-@REM cd .\python\llm\portable-zip\python-embed
+cd .\python-embed
 set "search=#import site"
 set "replace=import site"
-powershell -Command "(gc .\python\llm\portable-zip\python-embed\python39._pth) -replace '%search%', '%replace%' | Out-File -encoding ASCII .\python\llm\portable-zip\python-embed\python39._pth"
-@REM cd ..
+powershell -Command "(gc python39._pth) -replace '%search%', '%replace%' | Out-File -encoding ASCII python39._pth"
+cd ..
 
 :: install pip packages
 %python-embed% -m pip install --pre --upgrade bigdl-llm[all]
