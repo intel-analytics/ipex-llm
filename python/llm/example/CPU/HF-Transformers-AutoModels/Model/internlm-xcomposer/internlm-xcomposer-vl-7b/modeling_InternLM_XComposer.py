0117e1bb401ca30aa59a6c69f9953c673e04784b
Ricky-Ting <ricky.ting@outlook.com>
2023-11-02 14:24:44 +0800

    add license

--- python/llm/example/CPU/HF-Transformers-AutoModels/Model/internlm-xcomposer/internlm-xcomposer-vl-7b/modeling_InternLM_XComposer.py
+++ python/llm/example/CPU/HF-Transformers-AutoModels/Model/internlm-xcomposer/internlm-xcomposer-vl-7b/modeling_InternLM_XComposer.py
@@ -1,3 +1,26 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# ===========================================================================
+#
+# This file is adapted from
+# https://huggingface.co/internlm/internlm-xcomposer-vl-7b/blob/b06eb0c11653fe1568b6c5614b6b7be407ef8660/modeling_InternLM_XComposer.py
+#
+# Apache 2.0 license
+
 import copy
 import os
 import sys
