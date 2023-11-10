1747ffe60019567482b6976a24b05079274e7fc8
Heyang Sun <60865256+Uxito-Ada@users.noreply.github.com>
2023-11-10 13:51:24 +0800

    fix llm-init on deepspeed missing lib (#9419)

--- python/llm/dev/benchmark/all-in-one/run-deepspeed-spr.sh
+++ python/llm/dev/benchmark/all-in-one/run-deepspeed-spr.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-source bigdl-nano-init
+source bigdl-llm-init
 unset OMP_NUM_THREADS # deepspeed will set it for each instance automatically
 source /opt/intel/oneccl/env/setvars.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib
 export WORLD_SIZE=2 # run 1 instance per SPR socket, thus 2 instances on 2 sockets, 96 cores
 export MASTER_ADDR=127.0.0.1
 export CCL_ZE_IPC_EXCHANGE=sockets
