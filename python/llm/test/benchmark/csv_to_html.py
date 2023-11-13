79884e9cf5ec92642c251ca84d0b492ba693c017
WeiguangHan <weiguang.han@intel.com>
2023-11-13 16:57:16 +0800

    modify the script to generate html results more accurately

--- python/llm/test/benchmark/csv_to_html.py
+++ python/llm/test/benchmark/csv_to_html.py
@@ -39,18 +39,54 @@ def main():
     if len(csv_files)>1:
         data2 = pd.read_csv(csv_files[1], index_col=0)
 
+        data1_df=pd.DataFrame(data1)
+        data2_df=pd.DataFrame(data2)
+
+        last1=[]
+        diff1=[]
+        last2=[]
+        diff2=[]
+
         origin_column_1='1st token avg latency (ms)'
         origin_column_2='2+ avg latency (ms/token)'
+
+        for data1_ind,data1_row in data1_df.iterrows():
+            data1_model=data1_row['model'].strip()
+            data1_input_output_pairs=data1_row['input/output tokens'].strip()
+            flag=True
+
+            for data2_ind,data2_row in data2_df.iterrows():
+                data2_model=data2_row['model'].strip()
+                data2_input_output_pairs=data2_row['input/output tokens'].strip()
+                if data1_model==data2_model and data1_input_output_pairs==data2_input_output_pairs:
+                    data1_temp=data1_row[origin_column_1]
+                    data2_temp=data2_row[origin_column_1]
+                    data3_temp=data1_row[origin_column_2]
+                    data4_temp=data2_row[origin_column_2]
+
+                    last1.append(data2_temp)
+                    diff1.append(round((data2_temp-data1_temp)*100/data2_temp,2))
+                    last2.append(data4_temp)
+                    diff2.append(round((data4_temp-data3_temp)*100/data3_temp,2))
+
+                    flag=False
+                    break
+
+            if flag:
+                last1.append('')
+                diff1.append('')
+                last2.append('')
+                diff2.append('')
 
         added_column_1='last1'
         added_column_2='diff1(%)'
         added_column_3='last2'
         added_column_4='diff2(%)'
 
-        data1.insert(loc=3,column=added_column_1,value=data2[origin_column_1])
-        data1.insert(loc=4,column=added_column_2,value=round((data2[origin_column_1]-data1[origin_column_1])*100/data2[origin_column_1],2))
-        data1.insert(loc=5,column=added_column_3,value=data2[origin_column_2])
-        data1.insert(loc=6,column=added_column_4,value=round((data2[origin_column_2]-data1[origin_column_2])*100/data2[origin_column_2],2))
+        data1.insert(loc=3,column=added_column_1,value=last1)
+        data1.insert(loc=4,column=added_column_2,value=diff1)
+        data1.insert(loc=5,column=added_column_3,value=last2)
+        data1.insert(loc=6,column=added_column_4,value=diff2)
 
     daily_html=csv_files[0].split(".")[0]+".html"
     data1.to_html(daily_html)
