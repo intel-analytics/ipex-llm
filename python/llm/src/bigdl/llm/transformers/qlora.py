ca125f5acd56570c1cce6c613085c352f34b0241
cyita <yitastudy@gmail.com>
2023-12-07 19:50:53 +0800

    fix style

--- python/llm/src/bigdl/llm/transformers/qlora.py
+++ python/llm/src/bigdl/llm/transformers/qlora.py
@@ -165,7 +165,7 @@ def get_peft_model(*args, **kwargs):
     old_create_new_module = LoraModel._create_new_module
     LoraModel._create_new_module = staticmethod(functools.partial(_create_new_module,
                                                                   old_create_new_module))
-    lora_config, in_args = (args[1], True) if len(args) > 1 else (kwargs.get("peft_config", 
+    lora_config, in_args = (args[1], True) if len(args) > 1 else (kwargs.get("peft_config",
                                                                              LoraConfig()), False)
     if not isinstance(lora_config, LoraConfig):
         lora_config = LoraConfig(**lora_config.to_dict())
