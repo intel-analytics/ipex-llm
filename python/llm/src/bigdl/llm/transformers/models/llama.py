a054dd9668f5c4900d5652d5c8250f00f4352e19
rnwang04 <ruonan1.wang@intel.com>
2023-12-12 10:35:01 +0800

    small fix

--- python/llm/src/bigdl/llm/transformers/models/llama.py
+++ python/llm/src/bigdl/llm/transformers/models/llama.py
@@ -220,6 +220,7 @@ def llama_attention_forward_4_31(
                                                      is_causal=True)
         attn_weights = None
     elif use_esimd_sdp(q_len, self.head_dim, query_states):
+        import linear_fp16_esimd
         attn_output = linear_fp16_esimd.sdp_forward(query_states,
                                                     key_states.contiguous(),
                                                     value_states.contiguous())
