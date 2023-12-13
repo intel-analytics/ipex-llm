05c13398df882e931ce641917aea64d5ce58307e
Heyang Sun <60865256+Uxito-Ada@users.noreply.github.com>
2023-12-13 11:01:21 +0800

    fix str returned by get_int_from_str rather than expected int (#9667)

--- python/llm/example/CPU/QLoRA-FineTuning/alpaca-qlora/alpaca_qlora_finetuning_cpu.py
+++ python/llm/example/CPU/QLoRA-FineTuning/alpaca-qlora/alpaca_qlora_finetuning_cpu.py
@@ -58,7 +58,7 @@ def get_int_from_env(env_keys, default):
         val = int(os.environ.get(e, -1))
         if val >= 0:
             return val
-    return default
+    return int(default)
 
 def train(
     # model/data params
