772453c916825412382538571433acf98918033d
Qiyuan Gong <qiyuan.gong@intel.com>
2023-12-18 18:44:04 +0800

    Code styel

--- python/llm/src/bigdl/llm/utils/ipex_importer.py
+++ python/llm/src/bigdl/llm/utils/ipex_importer.py
@@ -13,9 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import warnings
+
 from importlib.metadata import distribution, PackageNotFoundError
 import sys
+
 
 class IPEXImporter:
     """
