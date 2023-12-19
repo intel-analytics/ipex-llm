a078aa19c6b860fd719941398d92be043b04e8a2
Yishuo Wang <yishuo.wang@intel.com>
2023-12-19 13:20:18 +0800

    fix mlp batch size check

--- python/llm/src/bigdl/llm/transformers/models/baichuan2.py
+++ python/llm/src/bigdl/llm/transformers/models/baichuan2.py
@@ -69,11 +69,11 @@ def baichuan_mlp_forward(
     self,
     x: torch.Tensor,
 ) -> torch.Tensor:
-    if x.shape[1] == 1 and x.dtype == torch.float32 and x.device.type == 'xpu' \
+    x_2d = x.view(-1, x.shape[-1])
+    if x_2d.shape[0] == 1 and x.dtype == torch.float32 and x.device.type == 'xpu' \
             and self.gate_proj.qtype == ggml_tensor_qtype["sym_int4"] \
             and not (self.training and x.requires_grad):
         import linear_q4_0
-        x_2d = x.view(-1, x.shape[-1])
         if not x_2d.is_contiguous():
             x_2d = x_2d.contiguous()
         return self.down_proj(linear_q4_0.mlp_forward_q4_0_xpu(
