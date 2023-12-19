5064533572317f35b8f615f8b47b2c14b293485f
Qiyuan Gong <qiyuan.gong@intel.com>
2023-12-19 12:58:41 +0800

    Change env to BIGDL_IMPORT_IPEX, default is false

--- python/llm/src/bigdl/llm/__init__.py
+++ python/llm/src/bigdl/llm/__init__.py
@@ -23,9 +23,9 @@ from .convert_model import llm_convert
 from .optimize import optimize_model
 import os
 
-# Default is true, set to false to avoid auto importing Intel Extension for PyTorch.
-AUTO_IMPORT_IPEX = os.getenv("AUTO_IMPORT_IPEX", 'True').lower() in ('true', '1', 't')
-if AUTO_IMPORT_IPEX:
+# Default is false, set to true to auto importing Intel Extension for PyTorch.
+BIGDL_IMPORT_IPEX = os.getenv("BIGDL_IMPORT_IPEX", 'False').lower() in ('true', '1', 't')
+if BIGDL_IMPORT_IPEX:
     # Import Intel Extension for PyTorch as ipex if XPU version is installed
     from .utils.ipex_importer import ipex_importer
     ipex_importer.import_ipex()
