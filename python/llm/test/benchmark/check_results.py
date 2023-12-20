f5fbee39b545d20be9d08c87752f1a9029cf88c2
WeiguangHan <weiguang.han@intel.com>
2023-12-20 15:38:21 +0800

    resolve some comments

--- python/llm/test/benchmark/check_results.py
+++ python/llm/test/benchmark/check_results.py
@@ -24,40 +24,47 @@ from omegaconf import OmegaConf
 
 def main():
     parser = argparse.ArgumentParser(description="check if the number of lines in html meets expectation")
-    parser.add_argument("-c", "--csv_name", type=str, dest="csv_name",
+    parser.add_argument("-c", "--csv_file", type=str, dest="csv_name",
                         help="name of csv", default="")
-    parser.add_argument("-y", "--yaml_name", type=str, dest="yaml_name",
+    parser.add_argument("-y", "--yaml_file", type=str, dest="yaml_name",
                         help="name of yaml", default="")
+    parser.add_argument("-n", "--expected_lines", type=int, dest="expected_lines",
+                            help="the number of expected csv lines", default=0)
     args = parser.parse_args()
 
-    csv_name = args.csv_name
-    file_list = [file for file in os.listdir() if file.endswith('.csv') and csv_name in file]
-    csv_dataframe = pd.read_csv(file_list[0], index_col=0)
-    num_already_test_cases = len(csv_dataframe)
-    already_test_cases = []
+    csv_file  = [file for file in os.listdir() if file.endswith('.csv') and args.csv_name in file][0]
+    csv_dataframe = pd.read_csv(csv_file, index_col=0)
+    actual_test_num = len(csv_dataframe)
+    actual_test_cases = []
     for index, row in csv_dataframe.iterrows():
-        already_test_cases.append(row['model'] + ":" + row['input/output tokens'].split('-')[0])
+        actual_test_cases.append(row['model'] + ":" + row['input/output tokens'].split('-')[0])
 
-    yaml_name = args.yaml_name
-    conf = OmegaConf.load(yaml_name)
-    all_test_cases = []
-    for model in conf.repo_id:
-        for in_out in conf['in_out_pairs']:
-            model_id_input = model + ':' + in_out.split('-')[0]
-            all_test_cases.append(model_id_input)
+    if args.yaml_name != "":
+        yaml_name = args.yaml_name
+        conf = OmegaConf.load(yaml_name)
+        all_test_cases = []
+        for model in conf.repo_id:
+            for in_out in conf['in_out_pairs']:
+                model_id_input = model + ':' + in_out.split('-')[0]
+                all_test_cases.append(model_id_input)
+        exclude_test_cases = []
+        if 'exclude' in conf and conf['exclude'] is not None:
+            exclude_test_cases = conf['exclude']
+        expected_test_num = len(all_test_cases) - len(exclude_test_cases)
+    elif args.expected_lines != 0:
+        expected_test_num = args.expected_lines
+    else:
+        raise ValueError("You should provide the value of either yaml_name or expected_lines!")
 
-    exclude_test_cases = []
-    if 'exclude' in conf and conf['exclude'] is not None:
-        exclude_test_cases = conf['exclude']
-
-    num_expected_test_cases = len(all_test_cases) - len(exclude_test_cases)
-
-    if num_already_test_cases != num_expected_test_cases:
-        print("---------------The test cases should be tested!------------")
-        for test_case in all_test_cases:
-            if test_case not in already_test_cases and test_case not in exclude_test_cases:
-                print(test_case)
-        raise ValueError("The above test cases should be tested! Please check the saved csv carefully! ! !")
+    if actual_test_num != expected_test_num:
+        if args.yaml_name != "":
+            print("---------------The test cases should be tested!------------")
+            for test_case in all_test_cases:
+                if test_case not in actual_test_cases and test_case not in exclude_test_cases:
+                    print(test_case)
+            raise ValueError("The above tests failed. Please check the errors in the log.")
+        else:
+            raise ValueError("Missing some expected test cases! Please check the yaml file and the given expected_lines manually.")
 
 if __name__ == "__main__":
     sys.exit(main())
