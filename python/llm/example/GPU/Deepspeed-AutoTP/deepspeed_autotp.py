a50994ef6bbb8bc76ab65e6b18c6a02054ed1cc6
plusbang <binbin1.deng@intel.com>
2023-12-20 15:07:47 +0000

    update

--- python/llm/example/GPU/Deepspeed-AutoTP/deepspeed_autotp.py
+++ python/llm/example/GPU/Deepspeed-AutoTP/deepspeed_autotp.py
@@ -19,8 +19,16 @@ import torch
 import transformers
 import deepspeed
 
-local_rank = int(os.getenv("LOCAL_RANK", "0"))
-world_size = int(os.getenv("WORLD_SIZE", "1"))
+def get_int_from_env(env_keys, default):
+    """Returns the first positive env value found in the `env_keys` list or the default."""
+    for e in env_keys:
+        val = int(os.environ.get(e, -1))
+        if val >= 0:
+            return val
+    return int(default)
+ 
+local_rank = get_int_from_env(["LOCAL_RANK","PMI_RANK"], "0")
+world_size = get_int_from_env(["WORLD_SIZE","PMI_SIZE"], "1")
 
 from bigdl.llm import optimize_model
 
