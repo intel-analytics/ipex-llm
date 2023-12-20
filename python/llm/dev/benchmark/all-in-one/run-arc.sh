97ce3db83fc027f33125986c6a39dc782c87fca9
Qiyuan Gong <qiyuan.gong@intel.com>
2023-12-20 16:38:12 +0800

    Add env to llm_performance_tests

--- python/llm/dev/benchmark/all-in-one/run-arc.sh
+++ python/llm/dev/benchmark/all-in-one/run-arc.sh
@@ -1,6 +1,5 @@
 source /opt/intel/oneapi/setvars.sh
 export USE_XETLA=OFF
-export BIGDL_IMPORT_IPEX="True"
 export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
 
 python run.py # make sure config YAML file
