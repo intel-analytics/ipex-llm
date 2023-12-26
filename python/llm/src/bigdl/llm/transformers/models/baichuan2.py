0758f0428c4550df4662a46ab95b1f3f69af9dc3
Kai Huang <huangkaivision@gmail.com>
2023-12-26 14:06:34 +0800

    revert empty cache

--- python/llm/src/bigdl/llm/transformers/models/baichuan2.py
+++ python/llm/src/bigdl/llm/transformers/models/baichuan2.py
@@ -19,7 +19,7 @@
 # https://huggingface.co/baichuan-inc/Baichuan2-13B-Chat/blob/c6f8592a60b4ad73c210b28dd2ab3cca51abbf93/modeling_baichuan.py
 
 import math
-from typing import List, Optional, Tuple, Union
+from typing import Optional, Tuple
 import torch
 import torch.utils.checkpoint
 from torch.nn import functional as F
@@ -28,7 +28,6 @@ from bigdl.llm.transformers.models.utils import init_kv_cache, extend_kv_cache, 
 from bigdl.llm.transformers.models.utils import apply_rotary_pos_emb
 from bigdl.llm.transformers.models.utils import apply_rotary_pos_emb_no_cache_xpu
 from transformers.utils import logging
-from transformers.modeling_outputs import BaseModelOutputWithPast
 logger = logging.get_logger(__name__)
 
 try:
@@ -389,156 +388,3 @@ def baichuan_13b_get_alibi_mask(self, tensor, seq_length_with_past):
             : self.n_head, :seq_length_with_past, :seq_length_with_past
         ]
     return mask
-
-
-def baichuan_13b_model_forward(
-    self,
-    input_ids: torch.LongTensor = None,
-    attention_mask: Optional[torch.Tensor] = None,
-    past_key_values: Optional[List[torch.FloatTensor]] = None,
-    inputs_embeds: Optional[torch.FloatTensor] = None,
-    use_cache: Optional[bool] = False,
-    output_attentions: Optional[bool] = False,
-    output_hidden_states: Optional[bool] = False,
-    return_dict: Optional[bool] = True,
-) -> Union[Tuple, BaseModelOutputWithPast]:
-    if input_ids is not None and inputs_embeds is not None:
-        raise ValueError(
-            "You cannot provide both input_ids and inputs_embeds simultaneously"
-        )
-    elif input_ids is not None:
-        batch_size, seq_length = input_ids.shape
-    elif inputs_embeds is not None:
-        batch_size, seq_length, _ = inputs_embeds.shape
-    else:
-        raise ValueError("You need to provide input_ids or inputs_embeds")
-
-    if self.first_run:
-        self.max_cache_pos = seq_length
-
-    return_dict = (
-        return_dict if return_dict is not None else self.config.use_return_dict
-    )
-
-    seq_length_with_past = seq_length
-
-    if past_key_values is not None:
-        past_key_values_length = past_key_values[0][0].shape[2]
-        seq_length_with_past = seq_length_with_past + past_key_values_length
-
-    if inputs_embeds is None:
-        inputs_embeds = self.embed_tokens(input_ids)
-
-    if self.training:
-        if (
-            self.alibi_mask is None
-            or self.alibi_mask.shape[-1] != seq_length_with_past
-        ):
-            self.alibi_mask = self.get_alibi_mask(
-                inputs_embeds, seq_length_with_past
-            )
-        alibi_mask = self.alibi_mask
-    else:
-        alibi_mask = self.get_alibi_mask(inputs_embeds, seq_length_with_past)
-
-    if attention_mask is not None:
-        if len(attention_mask.shape) == 2:
-            expanded_mask = attention_mask.to(alibi_mask.dtype)
-            expanded_mask = torch.tril(
-                torch.gt(expanded_mask[:, :, None] * expanded_mask[:, None, :], 0)
-            ) * torch.eq(expanded_mask[:, :, None] - expanded_mask[:, None, :], 0)
-        else:
-            expanded_mask = attention_mask
-        bsz = inputs_embeds.size(0)
-        src_len, tgt_len = alibi_mask.size()[-2:]
-        expanded_mask = (
-            expanded_mask.unsqueeze(1)
-            .expand(bsz, 1, src_len, tgt_len)
-            .to(alibi_mask.dtype)
-        )
-        inverted_mask = 1.0 - expanded_mask
-        inverted_mask = inverted_mask.masked_fill(
-            inverted_mask.to(torch.bool), torch.finfo(alibi_mask.dtype).min
-        )
-        attention_mask = inverted_mask + alibi_mask.unsqueeze(0)
-        if input_ids.device.type == "xpu":
-            del alibi_mask
-            del inverted_mask
-            del expanded_mask
-            torch.xpu.empty_cache()
-    else:
-        attention_mask = alibi_mask
-
-    hidden_states = inputs_embeds
-
-    if self.gradient_checkpointing and self.training:
-        if use_cache:
-            logger.warning_once(
-                "`use_cache=True` is incompatible with gradient checkpointing. Setting `use_cache=False`..."
-            )
-            use_cache = False
-
-    # decoder layers
-    all_hidden_states = () if output_hidden_states else None
-    all_self_attns = () if output_attentions else None
-    next_decoder_cache = () if use_cache else None
-
-    for idx, decoder_layer in enumerate(self.layers):
-        if output_hidden_states:
-            all_hidden_states += (hidden_states,)
-
-        past_key_value = (
-            past_key_values[idx] if past_key_values is not None else None
-        )
-
-        if self.gradient_checkpointing and self.training:
-
-            def create_custom_forward(module):
-                def custom_forward(*inputs):
-                    # None for past_key_value
-                    return module(*inputs, output_attentions, None)
-
-                return custom_forward
-
-            layer_outputs = torch.utils.checkpoint.checkpoint(
-                create_custom_forward(decoder_layer),
-                hidden_states,
-                attention_mask,
-                None,
-            )
-        else:
-            layer_outputs = decoder_layer(
-                hidden_states,
-                attention_mask=attention_mask,
-                past_key_value=past_key_value,
-                output_attentions=output_attentions,
-                use_cache=use_cache,
-            )
-
-        hidden_states = layer_outputs[0]
-
-        if use_cache:
-            next_decoder_cache += (layer_outputs[2 if output_attentions else 1],)
-
-        if output_attentions:
-            all_self_attns += (layer_outputs[1],)
-
-    hidden_states = self.norm(hidden_states)
-
-    # add hidden states from the last decoder layer
-    if output_hidden_states:
-        all_hidden_states += (hidden_states,)
-
-    next_cache = next_decoder_cache if use_cache else None
-    if not return_dict:
-        return tuple(
-            v
-            for v in [hidden_states, next_cache, all_hidden_states, all_self_attns]
-            if v is not None
-        )
-    return BaseModelOutputWithPast(
-        last_hidden_state=hidden_states,
-        past_key_values=next_cache,
-        hidden_states=all_hidden_states,
-        attentions=all_self_attns,
-    )
