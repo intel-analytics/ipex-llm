d299f108d0bc54fbcfb56c62eed6d8ad25cd32fa
Cengguang Zhang <potterguang101@gmail.com>
2023-12-28 09:11:59 +0800

    update falcon attention forward. (#9796)

--- python/llm/src/bigdl/llm/transformers/convert.py
+++ python/llm/src/bigdl/llm/transformers/convert.py
@@ -530,7 +530,7 @@ def _optimize_post(model, lightweight_bmm=False):
         modeling_module_name = model.__class__.__module__
         module = importlib.import_module(modeling_module_name)
         if "RWForCausalLM" in model.config.architectures:
-            if hasattr(model.config, "multi_query"):
+            if model.config.hidden_size == 4544:
                 # falcon-7b need to check performance drop after kv cache support.
                 # from bigdl.llm.transformers.models.falcon import rw_attention_forward_7b
                 # convert_forward(model,
@@ -546,12 +546,13 @@ def _optimize_post(model, lightweight_bmm=False):
                                 rw_attention_forward_40b
                                 )
         elif "FalconForCausalLM" in model.config.architectures:
-            # falcon-180b
-            from bigdl.llm.transformers.models.falcon import falcon_attention_forward
-            convert_forward(model,
-                            module.FalconAttention,
-                            falcon_attention_forward
-                            )
+            if model.config.hidden_size != 4544:
+                # falcon-180b and new falcon-40b
+                from bigdl.llm.transformers.models.falcon import falcon_attention_forward
+                convert_forward(model,
+                                module.FalconAttention,
+                                falcon_attention_forward
+                                )
     elif model.config.model_type == "baichuan" and model.config.vocab_size == 125696:
         # baichuan2
         if model.config.hidden_size == 4096:
