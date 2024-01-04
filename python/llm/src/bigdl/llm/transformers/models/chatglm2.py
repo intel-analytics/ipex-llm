ff80f5ee7d43fd1e60947001cafbe57041e13387
rnwang04 <ruonan1.wang@intel.com>
2024-01-03 22:22:21 -0800

    change attn calculation

--- python/llm/src/bigdl/llm/transformers/models/chatglm2.py
+++ python/llm/src/bigdl/llm/transformers/models/chatglm2.py
@@ -373,8 +373,9 @@ def core_attn_forward_8eb45c(self, query_layer, key_layer, value_layer, attentio
                                                                              value_layer,
                                                                              is_causal=True)
         elif attention_mask is None:
-            scaling_factor = 1 / math.sqrt(query_layer.size(-1))
-            attn = torch.matmul(query_layer * scaling_factor, key_layer.transpose(-2, -1))
+            head_dim = query_layer.size(-1)
+            attn_weights = torch.matmul(query_layer,
+                                        key_layer.transpose(2, 3)) / math.sqrt(head_dim)
             if L == S:
                 # first token, need attention mask
                 attn_bias = torch.zeros(L, S, dtype=query_layer.dtype,
