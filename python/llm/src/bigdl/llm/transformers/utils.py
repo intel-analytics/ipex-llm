29f9f11f54d74ca4613feddeba0b3dd023b44e65
Yishuo Wang <yishuo.wang@intel.com>
2024-01-05 17:11:36 +0800

    update

--- python/llm/src/bigdl/llm/transformers/utils.py
+++ python/llm/src/bigdl/llm/transformers/utils.py
@@ -39,6 +39,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import os
 from transformers.modeling_utils import _add_variant
 from ..utils.common import invalidInputError
 from typing import Union
