c201d3e496db517ac66f67197605a22661c056ff
lalalapotter <potterguang101@gmail.com>
2024-01-10 10:06:23 +0800

    fix style.

--- python/llm/dev/benchmark/ceval/evaluators/qwen.py
+++ python/llm/dev/benchmark/ceval/evaluators/qwen.py
@@ -1,12 +1,27 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # refer to https://github.com/QwenLM/Qwen/blob/main/eval/evaluate_chat_ceval.py
+
 import re
 from tqdm import tqdm
-
 import torch
 from thefuzz import process
-
 from transformers import AutoTokenizer
 from transformers.generation import GenerationConfig
+
 from bigdl.llm.transformers import AutoModelForCausalLM
 from evaluators.evaluator import Evaluator
 
