3f9811779ddbcc628ea561993feecc130266a78a
lalalapotter <potterguang101@gmail.com>
2024-01-16 19:03:11 +0800

    remove run scripts

--- python/llm/dev/benchmark/ceval/run.sh
+++ python/llm/dev/benchmark/ceval/run.sh
@@ -1,7 +1,3 @@
-source /opt/intel/oneapi/setvars.sh
-export USE_XETLA=OFF
-export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
-
 python eval.py \
     --model_family llama \
     --model_path "path to model" \
