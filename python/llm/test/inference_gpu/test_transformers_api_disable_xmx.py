bb4f25eb484870c68efc23da9d63354afb116da8
Mingyu-Wei <2370686225@qq.com>
2024-01-22 17:09:24 +0800

    address errors in action

--- python/llm/test/inference_gpu/test_transformers_api_disable_xmx.py
+++ python/llm/test/inference_gpu/test_transformers_api_disable_xmx.py
@@ -1,8 +1,27 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import os
 import pytest
 import torch
 from bigdl.llm.transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
+
+device = os.environ['DEVICE']
+print(f'Running on {device}')
 
 prompt = "Once upon a time, there existed a little girl who liked to have adventures. She wanted to go to places and meet new people, and have fun"
 
