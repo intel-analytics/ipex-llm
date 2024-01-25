d59f58577574b2098916244e91881c0d937d9f38
jinbridge <2635480475@qq.com>
2024-01-25 12:37:36 +0800

    modify starcoder

--- python/llm/example/GPU/HF-Transformers-AutoModels/Model/starcoder/generate.py
+++ python/llm/example/GPU/HF-Transformers-AutoModels/Model/starcoder/generate.py
@@ -39,6 +39,8 @@ if __name__ == '__main__':
 
     # Load model in 4 bit,
     # which convert the relevant layers in the model into INT4 format
+    # When running LLMs on Intel iGPUs for Windows users, we recommend setting `cpu_embedding=True` in the from_pretrained function.
+    # This will allow the memory-intensive embedding layer to utilize the CPU instead of iGPU.
     model = AutoModelForCausalLM.from_pretrained(model_path,
                                                  load_in_4bit=True,
                                                  optimize_model=False,
