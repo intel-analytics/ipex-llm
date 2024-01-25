e113814b497df69d2d4d0ed40cfe48d848f7e0c8
jinbridge <2635480475@qq.com>
2024-01-25 14:08:07 +0800

    correct the comments

--- python/llm/example/GPU/PyTorch-Models/Model/flan-t5/generate.py
+++ python/llm/example/GPU/PyTorch-Models/Model/flan-t5/generate.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     # "wo" module is not converted due to some issues of T5 model
     # (https://github.com/huggingface/transformers/issues/20287),
     # "lm_head" module is not converted to generate outputs with better quality
-    # When running LLMs on Intel iGPUs for Windows users, we recommend setting `cpu_embedding=True` in the from_pretrained function.
+    # When running LLMs on Intel iGPUs for Windows users, we recommend setting `cpu_embedding=True` in the optimize_model function.
     # This will allow the memory-intensive embedding layer to utilize the CPU instead of iGPU.
     model = optimize_model(model, modules_to_not_convert=["wo", "lm_head"])
 
