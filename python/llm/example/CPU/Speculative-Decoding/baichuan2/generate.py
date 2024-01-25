cf736b7752d23f0a2fe0ae500f6652bacc537608
Uxito-Ada <414416158@qq.com>
2024-01-25 22:25:40 +0800

    Baichuan2 CPU example of speculative decoding

---
+++ python/llm/example/CPU/Speculative-Decoding/baichuan2/generate.py
@@ -0,0 +1,104 @@
+import torch
+from bigdl.llm.transformers import AutoModel, AutoModelForCausalLM
+from transformers import LlamaTokenizer, AutoTokenizer
+import argparse
+import time
+import numpy as np
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "../../../"))
+from speculative_utils import SpeculativeWrapper
+
+torch.nn.Linear.reset_parameters = lambda x: None
+seed=42
+torch.manual_seed(seed)
+np.random.seed(seed)
+
+# you could tune the prompt based on your own model,
+# here the prompt tuning refers to https://huggingface.co/georgesung/llama2_7b_chat_uncensored#prompt-style
+BAICHUAN_PROMPT_FORMAT = "<human>{prompt} <bot>"
+
+long_input = """折纸的过程看似简单，其实想要做好，还是需要一套很复杂的工艺。以折一支玫瑰花为例，我们可以将整个折纸过程分成三个阶段，即：创建栅格折痕，制作立体基
+座，完成花瓣修饰。首先是创建栅格折痕：这一步有点像>我们折千纸鹤的第一步，即通过对称州依次对折，然后按照长和宽两个维度，依次进行多等分的均匀折叠；>最终在两个方向上的折痕会交织成一套完整均匀的小方格拼接图案；这些小方格就组成了类似二维坐标系的参考系统，使得我们在>该平面上，通过组合临近折痕的方>式从二维小方格上折叠出三维的高台或凹陷，以便于接下来的几座制作过程。需要注意的是，在建立栅格折痕的过程中，可能会出现折叠不对成的情况，这种错误所带
+来的后果可能是很严重的，就像是蝴>蝶效应，一开始只是毫厘之差，最后可能就是天壤之别。然后是制作立体基座：在这一步，我们需要基于栅格折痕折出对称的三>维高台或凹陷。从对称性分析不难发现，玫瑰花会有四个周对称的三维高台和配套凹陷。所以，我们可以先折>出四分之一的凹陷和高台图案，然后以这四分之一的部>分作为摸板，再依次折出其余三个部分的重复图案。值得注意的是，高台的布局不仅要考虑长和宽这两个唯独上的规整衬度和对称分布，还需要同时保证高这个维度上
+的整齐。与第一>阶段的注意事项类似，请处理好三个维度上的所有折角，确保它们符合计划中所要求的那种布局，以免出现三维折叠过程中的蝴蝶效应；为此，我们>常常会在折叠第一个四分之一图案的过程中，与成品玫瑰花进行反复比较，以便在第一时>间排除掉所有可能的错误。最后一个阶段是完成花瓣修饰。在这个阶段，我>们往往强调一个重要名词，叫用心折叠。这里的用心已经不是字面上的认真这个意思，而是指通过我们对于大自然中玫瑰花外型的理解，借助自然的曲线去不断修>正>花瓣的形状，以期逼近现实中的玫瑰花瓣外形。请注意，在这个阶段的最后一步，我们需要通过拉扯已经弯折的四个花瓣，来调整玫瑰花中心的绽放程度。这个过程可
+能会伴随玫瑰花整体结构的崩塌，所以，一定要控制好调整的力道，>以免出现不可逆的后果。最终，经过三个阶段的折叠，我们会得到一支栩栩如生的玫瑰花冠。如>果条件允许，我们可以在一根拉直的铁丝上缠绕绿色纸条，并将玫瑰花冠插在铁丝的一段。这样，我们就得到了一支手工玫瑰花。总之，通过>创建栅格折痕，制作立>体基座，以及完成花瓣修饰，我们从二维的纸面上创作出了一支三维的花朵。这个过程虽然看似简单，但它确实我们人类借助想象力和常见素材而创作出的艺术品。问
+: 请基于以上描述，分析哪些步骤做错了很大可>能会导致最终折叠失败？答: """
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Predict Tokens using `generate()` API for Llama2 model')
+    parser.add_argument('--repo-id-or-model-path', type=str, default="meta-llama/Llama-2-7b-chat-hf",
+                        help='The huggingface repo id for the Llama2 (e.g. `meta-llama/Llama-2-7b-chat-hf` and `meta-llama/Llama-2-13b-chat-hf`) to be downloaded'
+                             ', or the path to the huggingface checkpoint folder')
+    parser.add_argument('--prompt', type=str, default=long_input,
+                        help='Prompt to infer')
+    parser.add_argument('--precision', type=str, default='bf16',
+                        help='Main model Precision')
+    parser.add_argument('--n_predict', type=int, default=128,
+                        help='Max tokens to predict')
+    parser.add_argument('--max-draft', type=int, default=8,
+                        help='Max draft')
+    parser.add_argument('--xpu', action='store_true')
+    parser.add_argument('--temperature', type=float, default=0.2,
+                        help='Model temperature')
+
+    
+    args = parser.parse_args()
+    device = 'xpu' if args.xpu else 'cpu'
+    if device == 'cpu':
+        if args.precision == 'bf16':
+            amp_dtype = torch.bfloat16
+        elif args.precision == 'fp16':
+            amp_dtype = torch.float16
+        else:
+            amp_dtype = torch.float32
+    model_path = args.repo_id_or_model_path
+    max_step_draft = args.max_draft
+
+    draft_model = AutoModelForCausalLM.from_pretrained(model_path,
+                                                      load_in_4bit=True,
+                                                      optimize_model=True,
+                                                      trust_remote_code=True)
+    print("INT4 Assistant model loaded!")
+    if device == "xpu":
+        draft_model = draft_model.half()
+        print("Loading BigDL-FP16 target model ...")
+        model = AutoModelForCausalLM.from_pretrained(model_path,
+                                                     optimize_model=True,
+                                                     torch_dtype=torch.float16,
+                                                     load_in_low_bit="fp16",
+                                                     trust_remote_code=True,
+                                                     use_cache=True)
+    else:
+        print("Loading BigDL-BF16 target model ...")
+        model = AutoModelForCausalLM.from_pretrained(model_path,
+                                                     optimize_model=True,
+                                                     torch_dtype=torch.bfloat16,
+                                                     load_in_low_bit="bf16",
+                                                     trust_remote_code=True,
+                                                     use_cache=True)
+    print("Target model loaded!")
+    draft_model = draft_model.to(device)
+    model = model.to(device)
+    model = SpeculativeWrapper(model, do_print=False)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+
+    print(f"Model is {model.dtype}")
+    print(f"Max Draft number {max_step_draft}")
+    print(f"Max token number {args.n_predict}")
+    
+    with torch.inference_mode(), torch.autocast(device, dtype=amp_dtype if device == "cpu" else torch.float16):
+        prompt = BAICHUAN_PROMPT_FORMAT.format(prompt=args.prompt)
+        input_ids = tokenizer(prompt, return_tensors='pt').input_ids.to(model.device)
+        
+        output = model.speculative_generate(input_ids=input_ids,
+                                            draft_model=draft_model,
+                                            max_new_tokens=args.n_predict,
+                                            max_step_draft=max_step_draft,
+                                            do_sample=True)
+        print("token lists:")
+        print(output)
+        output_str = tokenizer.decode(output[0], skip_special_tokens=True)[len(long_input):]
+        print("model ouputs answer string:")
+        print(output_str)
