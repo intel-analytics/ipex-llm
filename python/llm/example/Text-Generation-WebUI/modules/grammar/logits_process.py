0246aafeb872f09c1e732b9b64b3bc6fb8819bd2
sgwhat <ge.song@intel.com>
2024-01-26 10:05:36 +0800

    add copyright license

--- python/llm/example/Text-Generation-WebUI/modules/grammar/logits_process.py
+++ python/llm/example/Text-Generation-WebUI/modules/grammar/logits_process.py
@@ -1,12 +1,21 @@
-'''
-This file has been 100% copied from this PR to the Transformers library:
-https://github.com/huggingface/transformers/pull/27557
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-Author: Saibo-creator
-Author GitHub: https://github.com/Saibo-creator
+# This file is adapted from
+# https://github.com/huggingface/transformers/pull/27557
 
-All credits go to the author.
-'''
 
 import math
 
