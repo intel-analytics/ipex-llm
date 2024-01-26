382e0e94ab81468227ecc1436b305cf35c5c3956
hzjane <a1015616934@qq.com>
2024-01-26 10:38:00 +0800

    fix llama draft_model dtype error

--- python/llm/src/bigdl/llm/transformers/model.py
+++ python/llm/src/bigdl/llm/transformers/model.py
@@ -247,6 +247,7 @@ class _BaseAutoModelClass:
 
             if speculative:
                 from .speculative import speculative_generate, clear_benchmarks
+                
                 # load a sym_int4 model as draft model
                 draft_model = cls.load_convert('sym_int4', optimize_model, *args, **kwargs)
                 model.draft_model = draft_model
