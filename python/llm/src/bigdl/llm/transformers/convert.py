9cc797bc027339ecc5e950787ea064dcb8cc8ef9
Yishuo Wang <yishuo.wang@intel.com>
2024-01-31 10:01:11 +0800

    Optimize rwkv v5 rest token again (#10043)

--- python/llm/src/bigdl/llm/transformers/convert.py
+++ python/llm/src/bigdl/llm/transformers/convert.py
@@ -973,13 +973,19 @@ def _optimize_post(model, lightweight_bmm=False):
         modeling_module_name = model.__class__.__module__
         module = importlib.import_module(modeling_module_name)
         from bigdl.llm.transformers.models.rwkv5 import rwkv_attention_forward
-        from bigdl.llm.transformers.models.rwkv5 import rwkv_ffn_forward
+        from bigdl.llm.transformers.models.rwkv5 import rwkv_ffn_forward_wrapper
+        from bigdl.llm.transformers.models.rwkv5 import rwkv_model_forward_wrapper
         convert_forward(model,
                         module.RwkvSelfAttention,
                         rwkv_attention_forward)
+        rwkv_ffn_forward = rwkv_ffn_forward_wrapper(module.RwkvFeedForward.forward)
         convert_forward(model,
                         module.RwkvFeedForward,
                         rwkv_ffn_forward)
+        rwkv_model_forward = rwkv_model_forward_wrapper(module.Rwkv5Model.forward)
+        convert_forward(model,
+                        module.Rwkv5Model,
+                        rwkv_model_forward)
     elif model.config.model_type == "deci":
         modeling_module_name = model.__class__.__module__
         module = importlib.import_module(modeling_module_name)
