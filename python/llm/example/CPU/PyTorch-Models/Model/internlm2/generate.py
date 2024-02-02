ad655ba87922cfd52add4eaf9352e21a472a803e
ivy-lv11 <lvzc@lamda.nju.edu.cn>
2024-02-02 14:27:06 +0800

    modify default prompt

--- python/llm/example/CPU/PyTorch-Models/Model/internlm2/generate.py
+++ python/llm/example/CPU/PyTorch-Models/Model/internlm2/generate.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     parser.add_argument('--repo-id-or-model-path', type=str, default="internlm/internlm2-chat-7b",
                         help='The huggingface repo id for the InternLM model to be downloaded'
                              ', or the path to the huggingface checkpoint folder')
-    parser.add_argument('--prompt', type=str, default="What is AI?",
+    parser.add_argument('--prompt', type=str, default="AI是什么？",
                         help='Prompt to infer') 
     parser.add_argument('--n-predict', type=int, default=32,
                         help='Max tokens to predict')
