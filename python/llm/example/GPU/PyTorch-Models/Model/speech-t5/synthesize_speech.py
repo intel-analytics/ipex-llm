2f1bd8071efe15feeec84c786f96f415444efbf7
jinbridge <2635480475@qq.com>
2024-02-05 15:28:40 +0800

    add speech t5 example

---
+++ python/llm/example/GPU/PyTorch-Models/Model/speech-t5/synthesize_speech.py
@@ -0,0 +1,77 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import torch
+import time
+import argparse
+
+from bigdl.llm import optimize_model
+from transformers import SpeechT5Processor, SpeechT5HifiGan, SpeechT5ForTextToSpeech
+from datasets import load_dataset
+import soundfile as sf
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Synthesize speech with the given input text using SpeechT5 model')
+    parser.add_argument('--repo-id-or-model-path', type=str, default='microsoft/speecht5_tts',
+                        help='The huggingface repo id for the SpeechT5 model to be downloaded'
+                             ', or the path to the huggingface checkpoint folder')
+    parser.add_argument('--repo-id-or-vocoder-path', type=str, default='microsoft/speecht5_hifigan',
+                        help='The huggingface repo id for the SpeechT5 vocoder to be downloaded'
+                             ', or the path to the huggingface checkpoint folder')
+    parser.add_argument('--repo-id-or-data-path', type=str, default="Matthijs/cmu-arctic-xvectors",
+                        help='The huggingface repo id for the audio dataset to be downloaded'
+                            ', or the path to the huggingface dataset folder')
+    parser.add_argument('--text', type=str, default="This is an example text for synthesize speech.",
+                        help='Text to synthesize speech')
+    
+    args = parser.parse_args()
+    model_path = args.repo_id_or_model_path
+    vocoder_path = args.repo_id_or_vocoder_path
+    dataset_path = args.repo_id_or_data_path
+    text = args.text
+
+    processor = SpeechT5Processor.from_pretrained(model_path)
+    model = SpeechT5ForTextToSpeech.from_pretrained(model_path)
+    vocoder = SpeechT5HifiGan.from_pretrained(vocoder_path)
+
+    # With only one line to enable BigDL-LLM optimization on model
+    # Skip optimizing these two modules to get higher audio quality
+    # When running LLMs on Intel iGPUs for Windows users, we recommend setting `cpu_embedding=True` in the optimize_model function.
+    # This will allow the memory-intensive embedding layer to utilize the CPU instead of iGPU.
+    model = optimize_model(model, cpu_embedding=True, 
+                           modules_to_not_convert=["speech_decoder_postnet.feat_out",
+                                                   "speech_decoder_postnet.prob_out"]) 
+    model = model.to('xpu')
+    vocoder = vocoder.to('xpu')
+
+    inputs = processor(text=text, return_tensors="pt").to('xpu')
+    
+    # load xvector containing speaker's voice characteristics from a dataset
+    embeddings_dataset = load_dataset(dataset_path, split="validation")
+    speaker_embeddings = torch.tensor(embeddings_dataset[0]["xvector"]).unsqueeze(0).to('xpu')
+    
+    with torch.inference_mode():
+        # ipex model needs a warmup, then inference time can be accurate
+        speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+
+        st = time.time()
+        speech = model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+        torch.xpu.synchronize()
+        end = time.time()
+        print(f"Inference time: {end-st} s")
+    
+    sf.write("speech_t5_out.wav", speech.to('cpu').numpy(), samplerate=16000)
