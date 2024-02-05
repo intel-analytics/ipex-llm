54e01cbcb50864c87a398ca600a60e91b9012c3d
hzjane <a1015616934@qq.com>
2024-02-05 09:29:24 +0800

    not return last tensor

--- python/llm/src/bigdl/llm/transformers/convert_ipex.py
+++ python/llm/src/bigdl/llm/transformers/convert_ipex.py
@@ -142,6 +142,8 @@ def _ipex_jit(model):
     sample_inputs = (
         get_dummy_input(model, return_dict=True)
     )
+    if "return_last_logit" in sample_inputs:
+        del sample_inputs["return_last_logit"]
     with torch.no_grad(), torch.cpu.amp.autocast(
         enabled=True
     ):
