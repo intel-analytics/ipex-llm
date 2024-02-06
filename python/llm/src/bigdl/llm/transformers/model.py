498ebd172532a2ec35e909dca26286b6cff0cc3c
plusbang <binbin1.deng@intel.com>
2024-02-06 17:45:59 +0000

    add model_hub para

--- python/llm/src/bigdl/llm/transformers/model.py
+++ python/llm/src/bigdl/llm/transformers/model.py
@@ -124,16 +124,26 @@ class _BaseAutoModelClass:
         :param imatrix: str value, represent filename of importance matrix pretrained on
             specific datasets for use with the improved quantization methods recently
             added to llama.cpp.
+        :param model_hub: str value, options are ``'huggingface'`` and ``'modelscope'``,
+            specify the model hub. Default to be ``'huggingface'``.
         :return: a model instance
         """
         pretrained_model_name_or_path = kwargs.get("pretrained_model_name_or_path", None) \
             if len(args) == 0 else args[0]
-        try:
+        model_hub = kwargs.pop("model_hub", "huggingface")
+        invalidInputError(model_hub in ["huggingface", "modelscope"],
+                          "The parameter `model_hub` is supposed to be `huggingface` or "
+                          f"`modelscope`, but got {model_hub}.")
+        if model_hub == "huggingface":
+            config_dict, _ = PretrainedConfig.get_config_dict(pretrained_model_name_or_path)
+        elif model_hub == "modelscope":
+            warnings.warn("The model hub is specified to be modelscope, "
+                          "please make sure you have installed it.")
             import modelscope
+            from modelscope.utils.hf_util import get_wrapped_class
+            cls.HF_Model = get_wrapped_class(cls.HF_Model)
             from .utils import get_modelscope_hf_config
             config_dict, _ = get_modelscope_hf_config(pretrained_model_name_or_path)
-        except ImportError:
-            config_dict, _ = PretrainedConfig.get_config_dict(pretrained_model_name_or_path)
         bigdl_transformers_low_bit = config_dict.pop("bigdl_transformers_low_bit", False)
         invalidInputError(not bigdl_transformers_low_bit,
                           f"Detected model is a low-bit({bigdl_transformers_low_bit}) model, "
@@ -402,6 +412,8 @@ class _BaseAutoModelClass:
         :param pretrained_model_name_or_path: str value, Path to load the optimized model ckpt.
         :param optimize_model: boolean value, Whether to further optimize the low_bit llm model.
                                Default to be True.
+        :param model_hub: str value, options are ``'huggingface'`` and ``'modelscope'``,
+                          specify the model hub. Default to be ``'huggingface'``.
 
         :return: a model instance
         """
@@ -426,6 +438,10 @@ class _BaseAutoModelClass:
         lightweight_bmm = kwargs.pop("lightweight_bmm", False)
         # Autofactory
         trust_remote_code = kwargs.pop("trust_remote_code", None)
+        model_hub = kwargs.pop("model_hub", "huggingface")
+        invalidInputError(model_hub in ["huggingface", "modelscope"],
+                          "The parameter `model_hub` is supposed to be `huggingface` or "
+                          f"`modelscope`, but got {model_hub}.")
         kwargs_orig = copy.deepcopy(kwargs)
 
         config, kwargs = AutoConfig.from_pretrained(
@@ -447,12 +463,16 @@ class _BaseAutoModelClass:
         torch_dtype = kwargs.pop("torch_dtype", "auto")
         sharded_metadata = None
 
-        try:
+        if model_hub == "huggingface":
+            config_dict, _ = PretrainedConfig.get_config_dict(pretrained_model_name_or_path)
+        elif model_hub == "modelscope":
+            warnings.warn("The model hub is specified to be modelscope, "
+                          "please make sure you have installed it.")
             import modelscope
+            from modelscope.utils.hf_util import get_wrapped_class
+            cls.HF_Model = get_wrapped_class(cls.HF_Model)
             from .utils import get_modelscope_hf_config
             config_dict, _ = get_modelscope_hf_config(pretrained_model_name_or_path)
-        except ImportError:
-            config_dict, _ = PretrainedConfig.get_config_dict(pretrained_model_name_or_path)
         bigdl_transformers_low_bit = config_dict.pop("bigdl_transformers_low_bit", False)
         bigdl_lcmu_enabled = config_dict.pop("bigdl_lcmu_enabled", True)
 
@@ -609,21 +629,11 @@ class _BaseAutoModelClass:
 
 
 class AutoModelForCausalLM(_BaseAutoModelClass):
-    try:
-        import modelscope
-        from modelscope import AutoModelForCausalLM
-        HF_Model = modelscope.AutoModelForCausalLM
-    except ImportError:
-        HF_Model = transformers.AutoModelForCausalLM
+    HF_Model = transformers.AutoModelForCausalLM
 
 
 class AutoModel(_BaseAutoModelClass):
-    try:
-        import modelscope
-        from modelscope import AutoModel
-        HF_Model = modelscope.AutoModel
-    except ImportError:
-        HF_Model = transformers.AutoModel
+    HF_Model = transformers.AutoModel
 
 
 class AutoModelForSpeechSeq2Seq(_BaseAutoModelClass):
@@ -631,21 +641,11 @@ class AutoModelForSpeechSeq2Seq(_BaseAutoModelClass):
 
 
 class AutoModelForSeq2SeqLM(_BaseAutoModelClass):
-    try:
-        import modelscope
-        from modelscope import AutoModelForSeq2SeqLM
-        HF_Model = modelscope.AutoModelForSeq2SeqLM
-    except ImportError:
-        HF_Model = transformers.AutoModelForSeq2SeqLM
+    HF_Model = transformers.AutoModelForSeq2SeqLM
 
 
 class AutoModelForSequenceClassification(_BaseAutoModelClass):
-    try:
-        import modelscope
-        from modelscope import AutoModelForSequenceClassification
-        HF_Model = modelscope.AutoModelForSequenceClassification
-    except ImportError:
-        HF_Model = transformers.AutoModelForSequenceClassification
+    HF_Model = transformers.AutoModelForSequenceClassification
 
 
 class AutoModelForMaskedLM(_BaseAutoModelClass):
@@ -665,9 +665,4 @@ class AutoModelForMultipleChoice(_BaseAutoModelClass):
 
 
 class AutoModelForTokenClassification(_BaseAutoModelClass):
-    try:
-        import modelscope
-        from modelscope import AutoModelForTokenClassification
-        HF_Model = modelscope.AutoModelForTokenClassification
-    except ImportError:
-        HF_Model = transformers.AutoModelForTokenClassification
+    HF_Model = transformers.AutoModelForTokenClassification
