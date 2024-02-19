a623e2cb1cb76d8cd36ebbadf9ce8b06620a31bb
qiuxin2012 <qiuxin2012cs@gmail.com>
2024-02-19 14:25:22 +0800

    cleanup

--- python/llm/src/bigdl/llm/transformers/convert.py
+++ python/llm/src/bigdl/llm/transformers/convert.py
@@ -901,9 +901,6 @@ def _optimize_post(model, lightweight_bmm=False):
         module = importlib.import_module(modeling_module_name)
         from bigdl.llm.transformers.models.qwen2 import qwen2_model_forward
         from bigdl.llm.transformers.models.qwen2 import qwen2_attention_forward
-        # TODO: add these optimization back
-        # RMSNorm and rotray embedding are disabled for now
-        # as they lead to obvious performance drop for Qwen 1.5
         convert_forward(model,
                         module.Qwen2Model,
                         qwen2_model_forward)
