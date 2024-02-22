dff06e4dc64e20181c930eb97056ee0e7fb7e22b
ivy-lv11 <lvzc@lamda.nju.edu.cn>
2024-02-22 15:18:52 +0800

    modify bat

--- python/llm/scripts/env-check.bat
+++ python/llm/scripts/env-check.bat
@@ -2,13 +2,18 @@
 
 
 python -V
-if ERRORLEVEL 1 echo No Python found! How to create environment could be found in the README.md
-
+if ERRORLEVEL 1 ( 
+    echo No Python found! How to create environment could be found in the README.md
+    goto:end
+)
 python check.py
 
 echo -----------------------------------------------------------------
 echo System Information
 systeminfo
-
+echo -----------------------------------------------------------------
 xpu-smi.exe
-if ERRORLEVEL 1 echo xpu-smi is not installed properly. 
+if ERRORLEVEL 1 ( 
+    echo xpu-smi is not installed properly. 
+    goto:end
+)
