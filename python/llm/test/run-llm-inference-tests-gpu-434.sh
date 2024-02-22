60e11b6739851411379cca71aa802c418de8a510
Ovo233 <76120304+Mingyu-Wei@users.noreply.github.com>
2024-02-22 13:44:45 +0800

    LLM: Add mlp layer unit tests (#10200)
    * add mlp layer unit tests
    
    * add download baichuan-13b
    
    * exclude llama for now
    
    * install additional packages
    
    * rename bash file
    
    * switch to Baichuan2
    
    * delete attention related code
    
    * fix name errors in yml file

---
+++ python/llm/test/run-llm-inference-tests-gpu-434.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+export LLM_HOME=${ANALYTICS_ZOO_ROOT}/python/llm/src
+export LLM_INFERENCE_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/llm/test/inference_gpu
+
+export USE_XETLA=OFF
+export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
+export DEVICE='xpu'
+
+set -e
+
+echo "# Start testing inference"
+start=$(date "+%s")
+
+if [ -z "$THREAD_NUM" ]; then
+  THREAD_NUM=2
+fi
+export OMP_NUM_THREADS=$THREAD_NUM
+export BIGDL_LLM_XMX_DISABLED=1
+pytest ${LLM_INFERENCE_TEST_DIR}/test_transformers_api_mlp.py -v -s -k "Mistral"
+unset BIGDL_LLM_XMX_DISABLED
+
+now=$(date "+%s")
+time=$((now-start))
+
+echo "Bigdl-llm gpu inference tests for transformers 4.34.0 finished"
+echo "Time used:$time seconds"
