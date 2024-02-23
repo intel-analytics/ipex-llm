27d5d07c7180eb866dcba410dba38871eeb410c8
Yuwen Hu <yuwen.hu@intel.com>
2024-02-23 16:46:23 +0800

    Small fix

--- python/llm/src/bigdl/llm/transformers/models/yuan.py
+++ python/llm/src/bigdl/llm/transformers/models/yuan.py
@@ -86,7 +86,11 @@ def yuan_attention_forward(
     if past_key_value is not None:
         kv_seq_len += past_key_value[0].shape[-2]
     cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-    query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+    query_states, key_states = apply_rotary_pos_emb(query_states,
+                                                    key_states,
+                                                    cos, sin,
+                                                    position_ids,
+                                                    "yuan")
 
     if past_key_value is not None:
         # reuse k, v, self_attention
