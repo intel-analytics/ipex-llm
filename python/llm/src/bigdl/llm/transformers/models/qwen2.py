b6a69c1cd3fcfdc398d47bc98238d9fd40043c3a
Yang Wang <yang3.wang@intel.com>
2024-02-22 16:17:18 -0800

    fix rope

--- python/llm/src/bigdl/llm/transformers/models/qwen2.py
+++ python/llm/src/bigdl/llm/transformers/models/qwen2.py
@@ -294,9 +294,13 @@ def qwen2_attention_forward_origin(
                 )
             kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
         cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-        query_states, key_states = apply_rotary_pos_emb_cache_freq_xpu(query_states, key_states,
-                                                                       sin, cos, "qwen2",
-                                                                       position_ids)
+        if use_fuse_rope:
+            query_states, key_states = apply_rotary_pos_emb_cache_freq_xpu(query_states, key_states,
+                                                                        sin, cos, "qwen2",
+                                                                        position_ids)
+        else:
+            query_states, key_states = apply_rotary_pos_emb(query_states, key_states,
+                                                            cos, sin, position_ids)
 
         if past_key_value is not None:
         # update the number of seen tokens
