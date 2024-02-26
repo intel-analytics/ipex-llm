336f5e8369f304816921663f58f32f4cb5cefcdc
sgwhat <ge.song@intel.com>
2024-02-26 11:16:22 +0800

    Separate forward_qkv into the later PR

--- python/llm/src/bigdl/llm/transformers/models/yuan.py
+++ python/llm/src/bigdl/llm/transformers/models/yuan.py
@@ -63,12 +63,7 @@ def yuan_attention_forward(
     is_first_step = False
     self.use_shareqk = False
 
-    use_fuse_rope = should_use_fuse_rope(self, hidden_states, position_ids)
     enough_kv_room = is_enough_kv_cache_room_4_31(past_key_value)
-    decoding_fast_path = use_decoding_fast_path(self.q_proj.qtype,
-                                                use_fuse_rope,
-                                                enough_kv_room,
-                                                bsz * q_len)
 
     if use_cache:
         if past_key_value is None:
@@ -91,90 +86,72 @@ def yuan_attention_forward(
             inference_hidden_states_memory = \
                 copy.deepcopy(torch.cat((hidden_states_tmp, hidden_states), dim=1))
 
-    if decoding_fast_path:
-        hidden_states = hidden_states.view(1, -1)
-        kv_seq_len = past_key_value[0].shape[-2]
+    value_states = \
+        self.v_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+
+    if self.use_shareqk:
+        # use_shareqk is disabled for now
+        qk_states = self.qk_proj(hidden_states).view(bsz, q_len, self.num_heads*self.head_dim)
+        query_key = qk_states.unsqueeze(2) * self.qk_weight + self.qk_bias
+        query_states, key_states = torch.unbind(query_key, dim=2)
+
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+    else:
+        hidden_states = self.lf_gate(hidden_states, before_hidden_states)
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        qk_states = torch.cat([query_states, key_states], dim=-1)
+        qk_states = qk_states.view(bsz, q_len,
+                                self.num_heads,
+                                int(qk_states.shape[-1]//self.num_heads))
+        (query_states, key_states) = torch.chunk(qk_states, 2, dim=-1)
+        query_states = query_states.transpose(1, 2)
+        key_states = key_states.transpose(1, 2)
+
+    kv_seq_len = key_states.shape[-2]
+    if past_key_value is not None:
+        kv_seq_len += past_key_value[0].shape[-2]
+    cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
+    query_states, key_states = apply_rotary_pos_emb(query_states,
+                                                    key_states,
+                                                    cos, sin,
+                                                    position_ids,
+                                                    "yuan")
+
+    if past_key_value is not None:
+        # reuse k, v, self_attention
         cache_k = past_key_value[0]
         cache_v = past_key_value[1]
-        import linear_q4_0
-        query_states, key_states, value_states = linear_q4_0.forward_qkv(hidden_states,
-                                                                         self.q_proj.weight,
-                                                                         self.k_proj.weight,
-                                                                         self.v_proj.weight,
-                                                                         position_ids,
-                                                                         cache_k, cache_v,
-                                                                         self.q_proj.weight.qtype,
-                                                                         kv_seq_len,
-                                                                         self.head_dim)
-        kv_seq_len += 1
+        if not enough_kv_room:
+            # allocate new
+            new_cache_k, new_cache_v = extend_kv_cache(bsz,
+                                                       self.num_heads,
+                                                       self.head_dim,
+                                                       cache_k.size(2),
+                                                       kv_seq_len + KV_CACHE_ALLOC_BLOCK_LENGTH,
+                                                       dtype=cache_k.d_type,
+                                                       device=device)
+            new_cache_k[:] = cache_k
+            new_cache_v[:] = cache_v
+            cache_k = new_cache_k
+            cache_v = new_cache_v
 
-    else:
-        value_states = \
-            self.v_proj(hidden_states).view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states, value_states = append_kv_cache(cache_k, cache_v, key_states, value_states)
 
-        if self.use_shareqk:
-            # use_shareqk is disabled for now
-            qk_states = self.qk_proj(hidden_states).view(bsz, q_len, self.num_heads*self.head_dim)
-            query_key = qk_states.unsqueeze(2) * self.qk_weight + self.qk_bias
-            query_states, key_states = torch.unbind(query_key, dim=2)
-
-            query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-            key_states = key_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
-        else:
-            hidden_states = self.lf_gate(hidden_states, before_hidden_states)
-            query_states = self.q_proj(hidden_states)
-            key_states = self.k_proj(hidden_states)
-            qk_states = torch.cat([query_states, key_states], dim=-1)
-            qk_states = qk_states.view(bsz, q_len,
-                                    self.num_heads,
-                                    int(qk_states.shape[-1]//self.num_heads))
-            (query_states, key_states) = torch.chunk(qk_states, 2, dim=-1)
-            query_states = query_states.transpose(1, 2)
-            key_states = key_states.transpose(1, 2)
-
-        kv_seq_len = key_states.shape[-2]
-        if past_key_value is not None:
-            kv_seq_len += past_key_value[0].shape[-2]
-        cos, sin = self.rotary_emb(value_states, seq_len=kv_seq_len)
-        query_states, key_states = apply_rotary_pos_emb(query_states,
-                                                        key_states,
-                                                        cos, sin,
-                                                        position_ids,
-                                                        "yuan")
-
-        if past_key_value is not None:
-            # reuse k, v, self_attention
-            cache_k = past_key_value[0]
-            cache_v = past_key_value[1]
-            if not enough_kv_room:
-                # allocate new
-                new_cache_k, new_cache_v = extend_kv_cache(bsz,
-                                                           self.num_heads,
-                                                           self.head_dim,
-                                                           cache_k.size(2),
-                                                           kv_seq_len + KV_CACHE_ALLOC_BLOCK_LENGTH,
-                                                           dtype=cache_k.d_type,
-                                                           device=device)
-                new_cache_k[:] = cache_k
-                new_cache_v[:] = cache_v
-                cache_k = new_cache_k
-                cache_v = new_cache_v
-            
-            key_states, value_states = append_kv_cache(cache_k, cache_v, key_states, value_states)
-
-        elif use_cache:
-            max_cache_length = kv_seq_len + KV_CACHE_ALLOC_BLOCK_LENGTH
-            new_key_states, new_value_states = init_kv_cache(bsz,
-                                                             self.num_heads,
-                                                             self.head_dim,
-                                                             kv_seq_len,
-                                                             max_cache_length,
-                                                             dtype=key_states.dtype,
-                                                             device=device)
-            new_key_states[:] = key_states
-            new_value_states[:] = value_states
-            key_states = new_key_states
-            value_states = new_value_states
+    elif use_cache:
+        max_cache_length = kv_seq_len + KV_CACHE_ALLOC_BLOCK_LENGTH
+        new_key_states, new_value_states = init_kv_cache(bsz,
+                                                         self.num_heads,
+                                                         self.head_dim,
+                                                         kv_seq_len,
+                                                         max_cache_length,
+                                                         dtype=key_states.dtype,
+                                                         device=device)
+        new_key_states[:] = key_states
+        new_value_states[:] = value_states
+        key_states = new_key_states
+        value_states = new_value_states
 
     past_key_value = \
         (key_states, value_states, inference_hidden_states_memory) if use_cache else None
