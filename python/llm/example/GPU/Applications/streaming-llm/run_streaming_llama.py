f4a2e32106d75b2c77fd18a407dd96586e0df543
Guoqiong Song <guoqiongsong@gmail.com>
2024-02-27 15:54:47 -0800

    Stream llm example for both GPU and CPU (#9390)

--- python/llm/example/GPU/Applications/streaming-llm/run_streaming_llama.py
+++ python/llm/example/GPU/Applications/streaming-llm/run_streaming_llama.py
@@ -44,9 +44,10 @@
 import warnings
 import torch
 import argparse
+import os
 import os, sys
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-stream_llm_src = CURRENT_DIR + "/streaming_llm/"
+stream_llm_src = CURRENT_DIR.replace("GPU", "CPU") + "/streaming_llm/"
 sys.path.append(stream_llm_src)
 from utils import load, download_url, load_jsonl
 from enable_streaming_llm import enable_streaming_llm
@@ -102,6 +103,7 @@ def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=10
         prompt = "USER: " + prompt + "\n\nASSISTANT: "
         print("\n" + prompt, end="")
         input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+        input_ids = input_ids.to(model.device)
         seq_len = input_ids.shape[1]
         if kv_cache is not None:
             space_needed = seq_len + max_gen_len
@@ -109,13 +111,14 @@ def streaming_inference(model, tokenizer, prompts, kv_cache=None, max_gen_len=10
 
         past_key_values = greedy_generate(
             model, tokenizer, input_ids, past_key_values, max_gen_len=max_gen_len
-        
-        )
+       
+        )        
 
 
 def main(args):
     model, tokenizer = load(args.repo_id_or_model_path)
-  
+    device = 'xpu' 
+    model = model.to(device)
     test_filepath = os.path.join(args.data_root, "mt_bench.jsonl")
     print(f"Loading data from {test_filepath} ...")
 
