1e64ac29ace6c09b7163d8250e1f994a5f66f469
Ariadne <wyn2000330@126.com>
2024-03-04 09:49:17 +0800

    fix langchain ut

--- python/llm/test/langchain_gpu/test_transformers_api.py
+++ python/llm/test/langchain_gpu/test_transformers_api.py
@@ -28,10 +28,7 @@ print(f'Running on {device}')
 
 class Test_Langchain_Transformers_API(TestCase):
     def setUp(self):
-        self.auto_model_path = os.environ.get('ORIGINAL_CHATGLM2_6B_PATH')
-        self.auto_causal_model_path = os.environ.get('ORIGINAL_REPLIT_CODE_PATH')
-        self.llama_model_path = os.environ.get('LLAMA_ORIGIN_PATH')
-        self.bloom_model_path = os.environ.get('BLOOM_ORIGIN_PATH')
+        self.llama_model_path = os.environ.get('LLAMA2_7B_ORIGIN_PATH')
         thread_num = os.environ.get('THREAD_NUM')
         if thread_num is not None:
             self.n_threads = int(thread_num)
@@ -40,11 +37,11 @@ class Test_Langchain_Transformers_API(TestCase):
 
 
     def test_bigdl_llm(self):
-        texts = 'def hello():\n  print("hello world")\n'
-        bigdl_llm = TransformersLLM.from_model_id(model_id=self.auto_causal_model_path, model_kwargs={'trust_remote_code': True, 'device_map': device})
+        texts = 'What is the capital of France?\n\n'
+        bigdl_llm = TransformersLLM.from_model_id(model_id=self.llama_model_path, model_kwargs={'trust_remote_code': True, 'device_map': device})
         
         output = bigdl_llm(texts)
-        res = "hello()" in output
+        res = "Paris" in output
         self.assertTrue(res)
 
 if __name__ == '__main__':
