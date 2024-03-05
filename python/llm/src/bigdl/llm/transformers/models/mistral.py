a1045f2580fb9d28c617cad7ce106c90ea8acedb
lalalapotter <potterguang101@gmail.com>
2024-03-05 15:58:17 +0800

    fix style.

--- python/llm/src/bigdl/llm/transformers/models/mistral.py
+++ python/llm/src/bigdl/llm/transformers/models/mistral.py
@@ -595,9 +595,12 @@ def mistral_attention_forward_4_36_quantized(
         kv_seq_len = key_states.shape[-2]
         if past_key_value is not None:
             if self.layer_idx is None:
-                raise ValueError(
-                    f"The cache structure has changed since version v4.36. If you are using {self.__class__.__name__} "
-                    "for auto-regressive decoding with k/v caching, please make sure to initialize the attention class "
+                invalidInputError(
+                    False,
+                    f"The cache structure has changed since version v4.36. "
+                    "If you are using {self.__class__.__name__} "
+                    "for auto-regressive decoding with k/v caching, "
+                    "please make sure to initialize the attention class "
                     "with a layer index."
                 )
             kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
