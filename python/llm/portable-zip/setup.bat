1340ec9ae1c9a0804e232af7b7faed9fdb35381f
jinbridge <2635480475@qq.com>
2024-03-08 15:58:37 +0800

    use python=3.11 instead

--- python/llm/portable-zip/setup.bat
+++ python/llm/portable-zip/setup.bat
@@ -1,12 +1,15 @@
 :: download python and extract zip
-if "%1"=="--python-zip" (
-    powershell -Command "Expand-Archive .\cpython-embed-zip.zip -DestinationPath .\python-embed"
-) else (
-    powershell -Command "Start-BitsTransfer -Source https://www.python.org/ftp/python/3.9.13/python-3.9.13-embed-amd64.zip -Destination python-3.9.13-embed-amd64.zip"
-    powershell -Command "Expand-Archive .\python-3.9.13-embed-amd64.zip -DestinationPath .\python-embed"
-    del .\python-3.9.13-embed-amd64.zip
-)
+@REM if "%1"=="--python-zip" (
+@REM     powershell -Command "Expand-Archive .\cpython-embed-zip.zip -DestinationPath .\python-embed"
+@REM ) else (
+@REM     powershell -Command "Start-BitsTransfer -Source https://www.python.org/ftp/python/3.11.8/python-3.11.8-embed-amd64.zip -Destination python-3.11.8-embed-amd64.zip"
+@REM     powershell -Command "Expand-Archive .\python-3.11.8-embed-amd64.zip -DestinationPath .\python-embed"
+@REM     del .\python-3.12.2-embed-amd64.zip
+@REM )
 
+@REM powershell -Command "Start-BitsTransfer -Source https://www.python.org/ftp/python/3.11.8/python-3.11.8-embed-amd64.zip -Destination python-3.11.8-embed-amd64.zip"
+powershell -Command "Expand-Archive .\python-3.11.8-embed-amd64.zip -DestinationPath .\python-embed"
+del .\python-3.12.2-embed-amd64.zip
 
 set "python-embed=.\python-embed\python.exe"
 
@@ -18,11 +21,12 @@ powershell -Command "Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -Out
 cd .\python-embed
 set "search=#import site"
 set "replace=import site"
-powershell -Command "(gc python39._pth) -replace '%search%', '%replace%' | Out-File -encoding ASCII python39._pth"
+powershell -Command "(gc python311._pth) -replace '%search%', '%replace%' | Out-File -encoding ASCII python311._pth"
 cd ..
 
 :: install pip packages
 %python-embed% -m pip install --pre --upgrade bigdl-llm[all]
+%python-embed% -m pip install transformers==4.36.2
 %python-embed% -m pip install transformers_stream_generator tiktoken einops colorama
 
 if "%1"=="--ui" (
