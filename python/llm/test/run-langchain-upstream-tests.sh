cf5a3022d2af10fe85f8dcc4255c58f8131af26a
Zhangky11 <2321096202@qq.com>
2024-03-15 11:14:59 +0800

    Modify path in llm_unit_tests.yml

--- python/llm/test/run-langchain-upstream-tests.sh
+++ python/llm/test/run-langchain-upstream-tests.sh
@@ -2,6 +2,10 @@ export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
 export LLM_HOME=${ANALYTICS_ZOO_ROOT}/python/llm/src
 export LLM_INFERENCE_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/llm/test/langchain_gpu
 
+export VICUNA_7B_1_3_ORIGIN_PATH=${VICUNA_7B_1_3_ORIGIN_PATH}
+
+set -e
+
 echo ">>> Testing LangChain upstream unit test"
 mkdir ${LLM_INFERENCE_TEST_DIR}/langchain_upstream
 wget https://raw.githubusercontent.com/langchain-ai/langchain/master/libs/community/tests/integration_tests/llms/test_bigdl.py -P ${LLM_INFERENCE_TEST_DIR}/langchain_upstream
