bbd0cec4153d44f066a66a942736c729fb95a9ce
Yishuo Wang <yishuo.wang@intel.com>
2024-03-15 14:04:38 +0800

    add mask support for fp8 sdp

--- python/llm/src/bigdl/llm/transformers/models/chatglm2.py
+++ python/llm/src/bigdl/llm/transformers/models/chatglm2.py
@@ -272,25 +272,29 @@ def chatglm2_quantized_attention_forward_8eb45c(
         k_cache, v_cache = append_fp8_kv_cache(k_cache, v_cache, key_layer, value_layer,
                                                new_layout=True)
 
+        if attention_mask is not None:
+            attention_mask = ~attention_mask
+            attn_bias = torch.zeros(attention_mask.shape, dtype=query_layer.dtype,
+                                    device=query_layer.device)
+            if attention_mask.dtype == torch.bool:
+                attn_bias.masked_fill_(attention_mask.logical_not(), float("-inf"))
+            else:
+                attn_bias += attention_mask
+        else:
+            attn_bias = None
+
         if seq_len != 1:
             key, value = restore_fp8_kv_cache(k_cache, v_cache, query_layer.dtype)
             key, value = repeat_kv(key, value, n_head)
             attn = torch.matmul(query_layer, key.transpose(2, 3)) / math.sqrt(head_dim)
-            if attention_mask is not None:
-                attention_mask = ~attention_mask
-                attn_bias = torch.zeros(attention_mask.shape, dtype=query_layer.dtype,
-                                        device=query_layer.device)
-                if attention_mask.dtype == torch.bool:
-                    attn_bias.masked_fill_(attention_mask.logical_not(), float("-inf"))
-                else:
-                    attn_bias += attention_mask
+            if attn_bias is not None:
                 attn += attn_bias
             attn = F.softmax(attn, dim=-1, dtype=torch.float32)
             context_layer = torch.matmul(attn.to(value.dtype), value)
         else:
             key, value = k_cache, v_cache
             import linear_q4_0
-            context_layer = linear_q4_0.sdp_fp8(query_layer, key, value)
+            context_layer = linear_q4_0.sdp_fp8(query_layer, key, value, attn_bias)
 
     # context_layer's shape: [bs, n_head, seq_len, head_dim] -> [seq_len, bs, n_head * head_dim]
     context_layer = context_layer.permute(2, 0, 1, 3).contiguous().view(seq_len, batch_size, -1)
