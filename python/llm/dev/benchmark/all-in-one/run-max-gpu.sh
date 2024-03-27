fbeb10c796deac5981bbc1808c90f197cfb1701b
WeiguangHan <weiguang.han@intel.com>
2024-03-27 17:56:33 +0800

    LLM: Set different env based on different Linux kernels (#10566)

--- python/llm/dev/benchmark/all-in-one/run-max-gpu.sh
+++ python/llm/dev/benchmark/all-in-one/run-max-gpu.sh
@@ -1,6 +1,11 @@
 source /opt/intel/oneapi/setvars.sh
+ 
 export LD_PRELOAD=${LD_PRELOAD}:${CONDA_PREFIX}/lib/libtcmalloc.so
-export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
 export ENABLE_SDP_FUSION=1
-
+export SYCL_CACHE_PERSISTENT=1
+KERNEL_VERSION=$(uname -r)
+if [[ $KERNEL_VERSION != *"6.5"* ]]; then
+    export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
+fi
+ 
 python run.py # make sure config YAML file
