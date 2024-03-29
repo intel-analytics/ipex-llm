0079bddd4789e7fcc413d57872309a88555c5d52
Qiyuan Gong <qiyuan.gong@intel.com>
2024-03-29 08:15:16 +0800

    Flex default kv q for batch > 2 and < 8

--- python/llm/src/ipex_llm/transformers/models/utils.py
+++ python/llm/src/ipex_llm/transformers/models/utils.py
@@ -82,7 +82,8 @@ def use_quantize_kv_cache(linear: torch.nn.Module, x: torch.Tensor) -> bool:
 
 def kv_cache_device_check(x: torch.Tensor) -> bool:
     return get_xpu_device_type(x) == "mtl" or \
-        (get_xpu_device_type(x) == "arc" and 1 < x.size(0) and x.size(0) < 8)
+        ((get_xpu_device_type(x) == "arc" or get_xpu_device_type(x) == "flex") and \
+            1 < x.size(0) and x.size(0) < 8)
 
 
 def init_fp8_kv_cache(batch_size, num_heads, current_length, head_dim, device, new_layout=False):
