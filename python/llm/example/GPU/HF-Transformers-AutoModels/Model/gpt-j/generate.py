800407be04e128cd815196c143c8994e14cec55c
rnwang04 <ruonan1.wang@intel.com>
2024-04-01 10:14:57 +0800

    remove ipex.optimize

--- python/llm/example/GPU/HF-Transformers-AutoModels/Model/gpt-j/generate.py
+++ python/llm/example/GPU/HF-Transformers-AutoModels/Model/gpt-j/generate.py
@@ -17,7 +17,6 @@
 import torch
 import time
 import argparse
-
 from ipex_llm.transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
 
@@ -46,7 +45,6 @@ if __name__ == '__main__':
                                                  trust_remote_code=True,
                                                  use_cache=True)
     model = model.to('xpu')
-    model = ipex.optimize(model.eval(), dtype="float16", inplace=True)
 
     # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(model_path,
