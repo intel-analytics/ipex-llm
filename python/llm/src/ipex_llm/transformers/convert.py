120387619fe25d47eb4a6d5506008278d9e8eb9d
Yuwen Hu <yuwen.hu@intel.com>
2024-04-02 14:56:53 +0800

    Add mlp optimizations

--- python/llm/src/ipex_llm/transformers/convert.py
+++ python/llm/src/ipex_llm/transformers/convert.py
@@ -1344,5 +1344,7 @@ def _optimize_post(model, lightweight_bmm=False):
                         module.StableLmAttention,
                         stablelm_attention_forward
                         )
-
+        convert_forward(model,
+                        module.StableLmMLP,
+                        llama_mlp_forward)
     return model
