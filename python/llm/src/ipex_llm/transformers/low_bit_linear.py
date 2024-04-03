dbc74a69c314fe617a50baa163537e9ab395037a
gc-fu <guancheng.fu@intel.com>
2024-04-03 10:10:14 +0800

    change low_bit_linear.py

--- python/llm/src/ipex_llm/transformers/low_bit_linear.py
+++ python/llm/src/ipex_llm/transformers/low_bit_linear.py
@@ -58,6 +58,7 @@ T = TypeVar("T", bound="torch.nn.Module")
 import ipex_llm.ggml.model.llama.llama_cpp as ggml
 import ctypes
 from ipex_llm.ggml.quantize import ggml_tensor_qtype
+from ipex_llm.transformers.convert import is_deepspeed_available
 
 TORCH_LINEAR_THRESHOLD = int(os.getenv("BIGDL_LLM_LINEAR_THRESHOLD", "512"))
 SYM_INT4 = ggml_tensor_qtype["sym_int4"]
@@ -704,16 +705,20 @@ class FP16Linear(nn.Linear):
                 if self.weight_type == 2:
                     self.weight = self.weight.transpose(0, 1).contiguous()
                     self.weight_type = 1
-                result = F.linear(x, self.weight, self.bias)
+                output = F.linear(x, self.weight, self.bias)
             else:
                 if self.weight_type == 1:
                     self.weight = self.weight.transpose(0, 1).contiguous()
                     self.weight_type = 2
-                result = torch.ops.torch_ipex.matmul_bias_out(x, self.weight, self.bias)
+                output = torch.ops.torch_ipex.matmul_bias_out(x, self.weight, self.bias)
             if self.mp_group is not None:
-                from deepspeed import comm as dist
-                dist.inference_all_reduce(result, group=self.mp_group)
-            return result
+                if is_deepspeed_available():
+                    from deepspeed import comm as dist
+                    dist.inference_all_reduce(output, group=self.mp_group)
+                else:
+                    # TODO: check vllm is available
+                    torch.distributed.all_reduce(output, group=self.mp_group)
+            return output
         else:
             if self.in_len == 4096 and self.weight_type != 3 or \
                     self.in_len == 11008 and self.weight_type != 1:
@@ -748,8 +753,13 @@ class FP16Linear(nn.Linear):
             new_shape = x_shape[:-1] + (self.out_len,)
             result = result.view(new_shape)
             if self.mp_group is not None:
-                from deepspeed import comm as dist
-                dist.inference_all_reduce(result, group=self.mp_group)
+                if is_deepspeed_available():
+                    from deepspeed import comm as dist
+                    dist.inference_all_reduce(result, group=self.mp_group)
+                else:
+                    # TODO: check vllm is available
+                    torch.distributed.all_reduce(result, group=self.mp_group)
+
             if self.bias is not None:
                 result += self.bias
 
