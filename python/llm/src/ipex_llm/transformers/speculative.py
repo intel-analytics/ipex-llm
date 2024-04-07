02ee5687063893707689d5007f8d4aa2452e04d8
hzjane <a1015616934@qq.com>
2024-04-07 15:20:18 +0800

    update only for chatglm

--- python/llm/src/ipex_llm/transformers/speculative.py
+++ python/llm/src/ipex_llm/transformers/speculative.py
@@ -691,13 +691,21 @@ def speculative_generate(self,
                         past_key_value_len = draft_past_key_values[0][0].shape[2]
                         position_ids = torch.Tensor([[past_key_value_len + step_draft]]).long()
                         position_ids = position_ids[:, :-draft_current_input_ids.size(0)]
-                        draft_output = draft_model.trace_graph(
-                            input_ids=draft_current_input_ids,
-                            attention_mask=draft_attention_mask,
-                            position_ids=position_ids,
-                            return_last_logit=torch.tensor(False),
-                            past_key_values=draft_past_key_values,
-                        )
+                        if self.config.model_type == "chatglm":
+                            draft_output = draft_model.trace_graph(
+                                input_ids=draft_current_input_ids,
+                                attention_mask=draft_attention_mask,
+                                position_ids=position_ids,
+                                return_last_logit=torch.tensor(False),
+                                past_key_values=draft_past_key_values,
+                            )
+                        else:
+                            draft_output = draft_model.trace_graph(
+                                input_ids=draft_current_input_ids,
+                                attention_mask=draft_attention_mask,
+                                position_ids=position_ids,
+                                past_key_values=draft_past_key_values,
+                            )
                     elif self.config.model_type == "baichuan":
                         if self.config.hidden_size == 4096:
                             past_key_value_len = draft_past_key_values[0][0].shape[2]
