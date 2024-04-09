8bb4ca2110ddd05600ecf133aac4fa4e25009d90
Chen, Zhentao <zhentao.chen@intel.com>
2024-04-09 12:15:25 +0800

    migrate to ipexlm

--- python/llm/dev/benchmark/harness/ipexlm.py
+++ python/llm/dev/benchmark/harness/ipexlm.py
@@ -35,7 +35,7 @@ def force_decrease_order(Reorderer):
 utils.Reorderer = force_decrease_order(utils.Reorderer)
 
 
-class BigDLLM(AutoCausalLM):
+class IPEXLM(AutoCausalLM):
     AUTO_MODEL_CLASS = AutoModelForCausalLM
     AutoCausalLM_ARGS = inspect.getfullargspec(AutoCausalLM.__init__).args
     def __init__(self, *args, **kwargs):
