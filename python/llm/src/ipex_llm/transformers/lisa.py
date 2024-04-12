cb84ae01fe70ab6fca04a9c264fb29d9a7fd2207
Jasonzzt <ziteng.zhang@intel.com>
2024-04-12 15:30:58 +0800

    fix style

--- python/llm/src/ipex_llm/transformers/lisa.py
+++ python/llm/src/ipex_llm/transformers/lisa.py
@@ -63,12 +63,14 @@ class DynamicLayerActivationCallback(TrainerCallback):
         # Randomly select n_layers to activate
         layers = eval('self.' + self.layers_attribute)  # Re-fetch layer references
         self.active_layers_indices = np.random.choice(
-                                        range(self.total_layers), 
-                                        self.n_layers, 
-                                        replace=False
-                                    )
-        print(f"Activating layers at indices: {self.active_layers_indices} for the next steps.",
-                flush=True)
+            range(self.total_layers),
+            self.n_layers,
+            replace=False
+        )
+        print(
+            f"Activating layers at indices: {self.active_layers_indices} for the next steps.",
+            flush=True
+        )
 
         # Enable gradients only for the selected layers
         for idx in self.active_layers_indices:
