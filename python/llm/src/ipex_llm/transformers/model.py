2cc8ee37dc33778a73c5718c1d00dcf5738b48c6
rnwang04 <ruonan1.wang@intel.com>
2024-04-18 15:37:10 +0800

    update

--- python/llm/src/ipex_llm/transformers/model.py
+++ python/llm/src/ipex_llm/transformers/model.py
@@ -119,7 +119,7 @@ class _BaseAutoModelClass:
                                 ``'sym_int5'``, ``'asym_int5'``, ``'sym_int8'``, ``'nf3'``,
                                 ``'nf4'``, ``'fp4'``, ``'fp8'``, ``'fp8_e4m3'``, ``'fp8_e5m2'``,
                                 ``'gguf_iq2_xxs'``, ``'gguf_iq2_xs'``, gguf_iq1_s'``,
-                                ``'fp16'``, ``'bf16'``, ``'q6_k'``,
+                                ``'fp16'``, ``'bf16'``, or ``'q6_k'``,
                                 ``'sym_int4'`` means symmetric int 4, ``'asym_int4'`` means
                                 asymmetric int 4, ``'nf4'`` means 4-bit NormalFloat, etc.
                                 Relevant low bit optimizations will be applied to the model.
