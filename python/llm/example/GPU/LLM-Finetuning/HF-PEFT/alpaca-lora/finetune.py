a7764a7925213221b47cd0b62625855ff03857bf
Qiyuan Gong <qiyuan.gong@intel.com>
2024-04-29 15:37:26 +0800

    Fix type in peft finetune example

--- python/llm/example/GPU/LLM-Finetuning/HF-PEFT/alpaca-lora/finetune.py
+++ python/llm/example/GPU/LLM-Finetuning/HF-PEFT/alpaca-lora/finetune.py
@@ -148,7 +148,7 @@ def train(
     model = LlamaForCausalLM.from_pretrained(
         base_model,
         load_in_8bit=True,
-        torch_dtype=torch.float16,
+        torch_dtype=torch.bfloat16,
         device_map=device_map,
     )
 
