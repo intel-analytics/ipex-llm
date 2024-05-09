1aa13bfb9cae97af7c8b321988a3aa6df322bd9f
Qiyuan Gong <qiyuan.gong@intel.com>
2024-05-09 15:35:05 +0800

    rename ipex-llm-finetune-qlora-xpu to ipex-llm-finetune-xpu

--- docker/llm/finetune/xpu/start-qlora-finetuning-on-xpu.sh
+++ docker/llm/finetune/xpu/start-qlora-finetuning-on-xpu.sh
@@ -14,4 +14,7 @@ then
   DATA_PARAM="--dataset ./data/alpaca-cleaned" # otherwise, default to download from HF dataset
 fi
 
+# QLoRA example dir
+cd /LLM-Finetuning/QLoRA/simple-example/
+
 python qlora_finetuning.py $MODEL_PARAM $DATA_PARAM
