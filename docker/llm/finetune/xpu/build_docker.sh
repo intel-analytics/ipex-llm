90b37b876ff5a13cbd8954e9c95a93ff92809c83
Qiyuan Gong <qiyuan.gong@intel.com>
2024-05-13 09:26:59 +0800

    Remove build docker xpu

--- docker/llm/finetune/xpu/build_docker.sh
+++
@@ -1,8 +0,0 @@
-export HTTP_PROXY=$http_proxy
-export HTTPS_PROXY=$https_proxy
-
-docker build \
-  --build-arg http_proxy=${HTTP_PROXY} \
-  --build-arg https_proxy=${HTTPS_PROXY} \
-  -t intelanalytics/ipex-llm-finetune-xpu:2.1.0-SNAPSHOT \
-  -f ./Dockerfile .
