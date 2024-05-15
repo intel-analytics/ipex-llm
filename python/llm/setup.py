2dfc5ceedccce40245e101d16368eae9f500f831
xiangyuT <xiangyu.tian@intel.com>
2024-05-15 12:57:03 +0800

    fix

--- python/llm/setup.py
+++ python/llm/setup.py
@@ -51,8 +51,9 @@ llm_home = os.path.join(os.path.dirname(os.path.abspath(__file__)), "src")
 github_artifact_dir = os.path.join(llm_home, '../llm-binary')
 libs_dir = os.path.join(llm_home, "ipex_llm", "libs")
 
-cpu_torch_version = 'torch == 2.1.2+cpu'
+cpu_torch_version_linux = 'torch == 2.1.2+cpu'
 cpu_torch_version_win = 'torch == 2.1.2'
+cpu_torch_version = cpu_torch_version_linux
 CONVERT_DEP = ['numpy == 1.26.4', # lastet 2.0.0b1 will cause error
                cpu_torch_version,
                'transformers == 4.31.0', 'sentencepiece', 'tokenizers == 0.13.3',
@@ -234,7 +235,8 @@ def setup_package():
         platform_name = "Windows"
         sys.argv.remove("--win")
         CONVERT_DEP.remove(cpu_torch_version)
-        CONVERT_DEP.append(cpu_torch_version_win)
+        cpu_torch_version = cpu_torch_version_win
+        CONVERT_DEP.append(cpu_torch_version)
     if "--linux" in sys.argv:
         platform_name = "Linux"
         sys.argv.remove("--linux")
