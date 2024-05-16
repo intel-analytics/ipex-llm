369a9c4d5e47823c8685ef7211499004b32c49ae
Kai Huang <huangkaivision@gmail.com>
2024-05-16 18:22:14 +0800

    update script

--- python/llm/dev/benchmark/perplexity/run_wikitext.py
+++ python/llm/dev/benchmark/perplexity/run_wikitext.py
@@ -1,22 +1,18 @@
 import argparse
 import torch
-from datasets import load_dataset
 from tqdm import tqdm
-
-
-def parse_kwargs(kwstr):
-    kvpair = [item.split('=') for item in kwstr.split(',') if item != ""]
-    return {k:v for k, v in kvpair}
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_path", required=True, type=str)
-parser.add_argument("--dataset", type=str, default='path=wikitext,name=wikitext-2-raw-v1')
+parser.add_argument("--data_path", type=str, default='wikitext-2-raw-v1/wikitext-2-raw/wiki.test.raw')
+parser.add_argument("--chunk_size", type=int, default=512)
+parser.add_argument("--stride", type=int, default=0)
 parser.add_argument("--device", type=str, default="xpu")
 parser.add_argument("--precision", type=str, default="sym_int4")
 parser.add_argument("--use-cache", action="store_true")
-parser.add_argument("--limit", type=int, default=None, help="Limit the number of examples per task. For debug only")
 args = parser.parse_args()
+
 
 if args.precision == "fp16":  # ipex fp16
     from transformers import AutoModelForCausalLM
@@ -31,24 +27,30 @@ else:  # ipex-llm
     model = model.half()
 model = model.to(args.device)
 
+with open(args.data_path, "rb") as f:
+    data = f.read()
+
 from transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
-
-test = load_dataset(**parse_kwargs(args.dataset), split="test")["text"]
-if args.limit:
-    test = test[:args.limit]
-encodings = tokenizer("\n\n".join(test), return_tensors="pt")
+encodings = tokenizer(data.decode("utf-8").strip("\n"), return_tensors="pt")
 
 max_length = model.config.max_position_embeddings
-stride = 512
+stride = args.chunk_size if args.stride <= 0 else args.stride
 seq_len = encodings.input_ids.size(1)
+num_chunks = seq_len // stride
 
 nlls = []
 prev_end_loc = 0
-for begin_loc in tqdm(range(0, seq_len, stride)):
-    end_loc = min(begin_loc + max_length, seq_len)
-    trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+for i in tqdm(range(num_chunks)):
+    begin_loc = i * stride
+    if args.stride > 0:
+        end_loc = min(begin_loc + max_length, seq_len)
+        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+    else:
+        end_loc = begin_loc + stride
+        trg_len = -stride//2
     input_ids = encodings.input_ids[:, begin_loc:end_loc].to(args.device)
+    if args.stride == 0: input_ids[:, 0] = tokenizer.bos_token_id
     target_ids = input_ids.clone()
     target_ids[:, :-trg_len] = -100
 
@@ -59,6 +61,7 @@ for begin_loc in tqdm(range(0, seq_len, stride)):
         # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
         # to the left by 1.
         neg_log_likelihood = outputs.loss
+        # print(neg_log_likelihood)
 
     nlls.append(neg_log_likelihood)
     if "xpu" in args.device:
@@ -69,4 +72,4 @@ for begin_loc in tqdm(range(0, seq_len, stride)):
         break
 
 ppl = torch.exp(torch.stack(nlls).mean())
-print("ppl result: {}".format(ppl.item()))
+print("Final ppl estimate: {}".format(ppl.item()))
