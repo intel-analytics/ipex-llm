6cd5ef7400fa34743dee2426dbd3ccd993a3dd9b
jinbridge <2635480475@qq.com>
2024-05-18 01:47:36 +0800

    retry

--- python/llm/test/run-langchain-upstream-tests.sh
+++ python/llm/test/run-langchain-upstream-tests.sh
@@ -6,8 +6,8 @@ export TEST_IPEXLLM_MODEL_IDS=${VICUNA_7B_1_3_ORIGIN_PATH}
 
 # Use Windows style path when running on Windows
 if [[ $1 == "--win-arc" ]]; then
-  export TEST_BIGDLLLM_MODEL_IDS=$(cygpath -w ${VICUNA_7B_1_3_ORIGIN_PATH})
-  export TEST_IPEXLLM_MODEL_IDS=$(cygpath -w ${VICUNA_7B_1_3_ORIGIN_PATH})
+  export TEST_BIGDLLLM_MODEL_IDS=$(cygpath -m ${VICUNA_7B_1_3_ORIGIN_PATH})
+  export TEST_IPEXLLM_MODEL_IDS=$(cygpath -m ${VICUNA_7B_1_3_ORIGIN_PATH})
 fi
 
 set -e
