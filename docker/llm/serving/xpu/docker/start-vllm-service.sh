d81a31d6c63e1cd7f799e262716f4db9150c3bb0
gc-fu <guancheng.fu@intel.com>
2024-05-17 14:36:14 +0800

    add internal patch

--- docker/llm/serving/xpu/docker/start-vllm-service.sh
+++ docker/llm/serving/xpu/docker/start-vllm-service.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 model="YOUR_MODEL_PATH"
 served_model_name="YOUR_MODEL_NAME"
+
+
+export USE_XETLA=OFF
+export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=0
+export TORCH_LLM_ALLREDUCE=0
+export CCL_DG2_ALLREDUCE=1
+
+
+# Tensor parallel related arguments:
+export CCL_WORKER_COUNT=2
+export FI_PROVIDER=shm
+export CCL_ATL_TRANSPORT=ofi
+export CCL_ZE_IPC_EXCHANGE=sockets
+export CCL_ATL_SHM=1
+
+source /opt/intel/1ccl-wks/setvars.sh
  
  
 python -m ipex_llm.vllm.entrypoints.openai.api_server \
