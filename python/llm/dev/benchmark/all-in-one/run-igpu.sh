27248f25535ac266693ebb238edc4d2032f10bc6
leonardozcm <leonardo1997zcm@gmail.com>
2024-05-22 17:07:16 +0800

    command list

--- python/llm/dev/benchmark/all-in-one/run-igpu.sh
+++ python/llm/dev/benchmark/all-in-one/run-igpu.sh
@@ -2,9 +2,5 @@ source /opt/intel/oneapi/setvars.sh
  
 export USE_XETLA=OFF
 export SYCL_CACHE_PERSISTENT=1
-KERNEL_VERSION=$(uname -r)
-if [[ $KERNEL_VERSION != *"6.5"* ]]; then
-    export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
-fi
  
 python run.py # make sure config YAML file
