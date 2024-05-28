25b64023158d19b06e5a092b6d91007da406e4a1
Jin Qiao <89779290+JinBridger@users.noreply.github.com>
2024-05-28 13:29:47 +0800

    Add Windows GPU unit test (#11050)
    * Add Windows GPU UT
    
    * temporarily remove ut on arc
    
    * retry
    
    * retry
    
    * retry
    
    * fix
    
    * retry
    
    * retry
    
    * fix
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * fix
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * merge into single workflow
    
    * retry inference test
    
    * retry
    
    * retrigger
    
    * try to fix inference test
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * retry
    
    * check lower_bound
    
    * retry
    
    * retry
    
    * try example test
    
    * try fix example test
    
    * retry
    
    * fix
    
    * seperate function into shell script
    
    * remove cygpath
    
    * try remove all cygpath
    
    * retry
    
    * retry
    
    * Revert "try remove all cygpath"
    
    This reverts commit 7ceeff3e48f08429062ecef548c1a3ad3488756f.
    
    * Revert "retry"
    
    This reverts commit 40ea2457843bff6991b8db24316cde5de1d35418.
    
    * Revert "retry"
    
    This reverts commit 817d0db3e5aec3bd449d3deaf4fb01d3ecfdc8a3.
    
    * enable ut
    
    * fix
    
    * retrigger
    
    * retrigger
    
    * update download url
    
    * fix
    
    * fix
    
    * retry
    
    * add comment
    
    * fix

--- python/llm/test/run-llm-example-tests-gpu.sh
+++ python/llm/test/run-llm-example-tests-gpu.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+if [[ $RUNNER_OS == "Linux" ]]; then
+  export ANALYTICS_ZOO_ROOT=${ANALYTICS_ZOO_ROOT}
+elif [[ $RUNNER_OS == "Windows" ]]; then
+  export ANALYTICS_ZOO_ROOT=$(cygpath -m ${ANALYTICS_ZOO_ROOT})
+fi
 
 set -e
 
@@ -10,11 +14,14 @@ start=$(date "+%s")
 sed -i 's/max_steps=200/max_steps=2/; s/save_steps=100/save_steps=2/; s/logging_steps=20/logging_steps=1/' \
     ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/LLM-Finetuning/QLoRA/simple-example/qlora_finetuning.py
 
-python ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/LLM-Finetuning/QLoRA/simple-example/qlora_finetuning.py \
+# import pytest_check_error function
+source ${ANALYTICS_ZOO_ROOT}/python/llm/test/run-llm-check-function.sh
+
+ipex_workaround_wrapper python ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/LLM-Finetuning/QLoRA/simple-example/qlora_finetuning.py \
 --repo-id-or-model-path ${LLAMA2_7B_ORIGIN_PATH} \
 --dataset ${YAHMA_ALPACA_CLEANED_PATH}
 
-python ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/LLM-Finetuning/QLoRA/simple-example/export_merged_model.py \
+ipex_workaround_wrapper python ${ANALYTICS_ZOO_ROOT}/python/llm/example/GPU/LLM-Finetuning/QLoRA/simple-example/export_merged_model.py \
 --repo-id-or-model-path ${LLAMA2_7B_ORIGIN_PATH} \
 --adapter_path ${PWD}/outputs/checkpoint-2 \
 --output_path ${PWD}/outputs/checkpoint-2-merged
