c47fdb39395ac5d206afe87fb7d1275a8ed2c1fc
cyita <yitastudy@gmail.com>
2024-05-28 17:19:33 +0800

    Support new fp8 e4m3

--- python/llm/src/ipex_llm/transformers/low_bit_linear.py
+++ python/llm/src/ipex_llm/transformers/low_bit_linear.py
@@ -329,13 +329,14 @@ def use_batch_forward(x: torch.Tensor, qtype: int, output_len: int):
         and output_len % 32 == 0
         and device in ["arc", "flex", "pvc", "mtl"]
         and qtype in [SYM_INT4, ASYM_INT4, SYM_INT8, FP4,
-                      FP8E5, FP6]
+                      FP8E5, FP6, FP8E4]
         and batch_size <= 64
     )
     if hard_condition:
         return (
             batch_size > 1
             or (device in ["arc", "flex"] and qtype in [SYM_INT8, FP4])
+            or (device in ["arc", "flex", "mtl"] and qtype in [FP8E4])
         )
     return False
 
