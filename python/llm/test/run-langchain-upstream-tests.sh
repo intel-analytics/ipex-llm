517df33ba79cf9c96208f461c5edd17276e9028b
jinbridge <2635480475@qq.com>
2024-05-28 10:52:32 +0800

    fix

--- python/llm/test/run-langchain-upstream-tests.sh
+++ python/llm/test/run-langchain-upstream-tests.sh
@@ -6,6 +6,7 @@ export TEST_IPEXLLM_MODEL_IDS=${VICUNA_7B_1_3_ORIGIN_PATH}
 
 # Use Windows style path when running on Windows
 if [[ $RUNNER_OS == "Windows" ]]; then
+  export ANALYTICS_ZOO_ROOT=$(cygpath -m ${ANALYTICS_ZOO_ROOT})
   export TEST_BIGDLLLM_MODEL_IDS=$(cygpath -m ${VICUNA_7B_1_3_ORIGIN_PATH})
   export TEST_IPEXLLM_MODEL_IDS=$(cygpath -m ${VICUNA_7B_1_3_ORIGIN_PATH})
 fi
@@ -16,8 +17,10 @@ echo ">>> Testing LangChain upstream unit test"
 cp ${ANALYTICS_ZOO_ROOT}/langchain_upstream/libs/community/tests/integration_tests/llms/test_bigdl_llm.py ${ANALYTICS_ZOO_ROOT}/langchain_upstream
 cp ${ANALYTICS_ZOO_ROOT}/langchain_upstream/libs/community/tests/integration_tests/llms/test_ipex_llm.py ${ANALYTICS_ZOO_ROOT}/langchain_upstream
 
-python -m pytest -s ${ANALYTICS_ZOO_ROOT}/langchain_upstream/test_bigdl_llm.py
-python -m pytest -s ${ANALYTICS_ZOO_ROOT}/langchain_upstream/test_ipex_llm.py
+source ${ANALYTICS_ZOO_ROOT}/python/llm/test/run-llm-check-function.sh
+
+pytest_check_error python -m pytest -s ${ANALYTICS_ZOO_ROOT}/langchain_upstream/test_bigdl_llm.py
+pytest_check_error python -m pytest -s ${ANALYTICS_ZOO_ROOT}/langchain_upstream/test_ipex_llm.py
 
 echo ">>> Testing LangChain upstream ipynb"
 cp ${ANALYTICS_ZOO_ROOT}/langchain_upstream/docs/docs/integrations/llms/ipex_llm.ipynb ${ANALYTICS_ZOO_ROOT}/langchain_upstream/langchain_example.ipynb
@@ -25,5 +28,5 @@ bash ./apps/ipynb2py.sh ${ANALYTICS_ZOO_ROOT}/langchain_upstream/langchain_examp
 sed -i '/^get_ipython/d' ${ANALYTICS_ZOO_ROOT}/langchain_upstream/langchain_example.py
 sed -i "s,model_id=\"[^\"]*\",model_id=\"$TEST_IPEXLLM_MODEL_IDS\",g" ${ANALYTICS_ZOO_ROOT}/langchain_upstream/langchain_example.py
 sed -i 's|saved_lowbit_model_path = "./vicuna-7b-1.5-low-bit"|saved_lowbit_model_path = "./langchain_upstream/vicuna-7b-1.5-low-bit"|' ${ANALYTICS_ZOO_ROOT}/langchain_upstream/langchain_example.py
-python ${ANALYTICS_ZOO_ROOT}/langchain_upstream/langchain_example.py
+ipex_workaround_wrapper python ${ANALYTICS_ZOO_ROOT}/langchain_upstream/langchain_example.py
 rm -rf ${ANALYTICS_ZOO_ROOT}/langchain_upstream
--- python/llm/test/run-llm-langchain-tests-gpu.sh
+++ python/llm/test/run-llm-langchain-tests-gpu.sh
@@ -8,6 +8,8 @@ if [[ $RUNNER_OS == "Linux" ]]; then
   export USE_XETLA=OFF
   export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
 elif [[ $RUNNER_OS == "Windows" ]]; then
+  export ANALYTICS_ZOO_ROOT=$(cygpath -m ${ANALYTICS_ZOO_ROOT})
+  export LLM_INFERENCE_TEST_DIR=${ANALYTICS_ZOO_ROOT}/python/llm/test/langchain_gpu
   export SYCL_CACHE_PERSISTENT=1
 fi
 
@@ -18,7 +20,9 @@ set -e
 echo "# Start testing inference"
 start=$(date "+%s")
 
-python -m pytest -s ${LLM_INFERENCE_TEST_DIR}
+source ${ANALYTICS_ZOO_ROOT}/python/llm/test/run-llm-check-function.sh
+
+pytest_check_error python -m pytest -s ${LLM_INFERENCE_TEST_DIR}
 
 now=$(date "+%s")
 time=$((now-start))
