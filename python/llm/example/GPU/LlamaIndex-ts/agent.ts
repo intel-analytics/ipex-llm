ee64bddbc84abbd555e34e88fe6f98e5ae1dfcb9
ivy-lv11 <zhicunlv@gmail.com>
2024-06-12 14:41:38 +0800

    add GPU example

---
+++ python/llm/example/GPU/LlamaIndex-ts/agent.ts
@@ -0,0 +1,63 @@
+import { 
+    OpenAI, 
+    FunctionTool, 
+    OpenAIAgent,
+    Settings,
+    Ollama,
+    ReActAgent,
+} from "llamaindex"
+import 'dotenv/config'
+
+async function main() {
+
+    Settings.llm = new Ollama({
+        model: "llama3:latest",
+      });
+    
+
+    Settings.callbackManager.on("llm-tool-call", (event) => {
+        console.log(event.detail.payload)
+    })
+    Settings.callbackManager.on("llm-tool-result", (event) => {
+        console.log(event.detail.payload)
+    })
+
+    const sumNumbers = ({a, b}) => {
+        return `${a + b}`;
+    }
+
+    const tools = [
+        FunctionTool.from(
+            sumNumbers,
+            {
+                name: "sumNumbers",
+                description: "Use this function to sum two numbers",
+                parameters: {
+                    type: "object",
+                    properties: {
+                        a: {
+                            type: "number",
+                            description: "First number to sum"
+                        },
+                        b: {
+                            type: "number",
+                            description: "Second number to sum"
+                        },
+                    },
+                    required: ["a", "b"]
+                }
+            }
+        )
+    ]
+
+    const agent = new ReActAgent({ tools });
+
+    let response = await agent.chat({
+        message: "Sum 100 and 201",
+    })
+
+    console.log(response)
+
+}
+
+main().catch(console.error);
