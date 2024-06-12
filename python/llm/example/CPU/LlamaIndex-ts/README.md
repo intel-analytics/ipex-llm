944b709a88d6d76a3da47157ac43a9eabbf56fe0
ivy-lv11 <zhicunlv@gmail.com>
2024-06-12 13:03:48 +0800

    add

---
+++ python/llm/example/CPU/LlamaIndex-ts/README.md
@@ -0,0 +1,22 @@
+# LlamaIndex-ts
+
+This folder contains examples showcasing how to use [**LlamaIndex-ts**](https://ts.llamaindex.ai/guides/agents/setup) with `ipex-llm`.
+> [**LlamaIndex-ts**](https://ts.llamaindex.ai/guides/agents/setup) helps you unlock domain-specific data and then build powerful applications with it.
+
+## Setting up Dependencies 
+
+### Install LlamaIndex-ts
+Following [this page](https://github.com/intel-analytics/ipex-llm/blob/main/python/llm/example/CPU/LlamaIndex/README.md) to create an environment for llamaindex.
+Then run the commands below
+```
+npm install llamaindex
+npm install dotenv
+```
+
+### Install Ollama
+* Origin Ollama: refer to [this page]() to install Ollama on CPU. 
+* Ollama enabled by IPEX-LLM: you can use commands below to install and run ollama. 
+    ```
+    pip install --pre --upgrade ipex-llm[cpp]
+    init-ollama
+    ```
---
+++ python/llm/example/CPU/LlamaIndex-ts/agent.ts
@@ -0,0 +1,77 @@
+import { 
+    OpenAI, 
+    FunctionTool, 
+    OpenAIAgent,
+    Settings,
+    Ollama,
+    ReActAgent,
+} from "llamaindex"
+import 'dotenv/config'
+
+async function main() {
+
+    // Settings.llm = new OpenAI({
+    //     apiKey: process.env.OPENAI_API_KEY,
+    //     model: "gpt-4o",
+    // })
+    Settings.llm = new Ollama({
+        model: "llama3:latest",
+      });
+    
+    /*
+    Set up logging so we can see the work in progress.
+    Available events:
+    llm-start
+    llm-end
+    agent-start
+    agent-end
+    llm-tool-call
+    llm-tool-result
+    */
+    Settings.callbackManager.on("llm-tool-call", (event) => {
+        console.log(event.detail.payload)
+    })
+    Settings.callbackManager.on("llm-tool-result", (event) => {
+        console.log(event.detail.payload)
+    })
+
+    const sumNumbers = ({a, b}) => {
+        return `${a + b}`;
+    }
+
+    const tools = [
+        FunctionTool.from(
+            sumNumbers,
+            {
+                name: "sumNumbers",
+                description: "Use this function to sum two numbers",
+                parameters: {
+                    type: "object",
+                    properties: {
+                        a: {
+                            type: "number",
+                            description: "First number to sum"
+                        },
+                        b: {
+                            type: "number",
+                            description: "Second number to sum"
+                        },
+                    },
+                    required: ["a", "b"]
+                }
+            }
+        )
+    ]
+
+    // const agent = new OpenAIAgent({tools})
+    const agent = new ReActAgent({ tools });
+
+    let response = await agent.chat({
+        message: "Add 101 and 303",
+    })
+
+    console.log(response)
+
+}
+
+main().catch(console.error);
