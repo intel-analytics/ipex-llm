3ea2490c9225efb3130b9f4c20ea102b6b1aedeb
Shaojun Liu <61072813+liu-shaojun@users.noreply.github.com>
2024-06-20 15:43:35 +0800

    avoid importing ipex again

--- docker/llm/inference/xpu/docker/chat.py
+++ docker/llm/inference/xpu/docker/chat.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import intel_extension_for_pytorch as ipex
 import torch
 import argparse
 import sys
