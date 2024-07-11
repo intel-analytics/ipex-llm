24377de1e009b1ffe9f8c7f9f473266f53733403
xiangyuT <xiangyu.tian@intel.com>
2024-07-11 14:10:20 +0800

    refine

--- python/llm/example/GPU/Pipeline-Parallel-FastAPI/run.sh
+++ python/llm/example/GPU/Pipeline-Parallel-FastAPI/run.sh
@@ -28,9 +28,15 @@ if [[ $KERNEL_VERSION != *"6.5"* ]]; then
     export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
 fi
 export TORCH_LLM_ALLREDUCE=0
+export IPEX_LLM_QUANTIZE_KV_CACHE=1
+export IPEX_LLM_LAST_LM_HEAD=1
 
 export MODEL_PATH=YOUR_MODEL_PATH
 export NUM_GPUS=2
-export IPEX_LLM_QUANTIZE_KV_CACHE=1
+export ZE_AFFINITY_MASK=0,1
+export LOW_BIT="fp8"
+# max requests = max_num_reqs * rank_num
+export MAX_NUM_SEQS="4"
+export MAX_PREFILLED_SEQS=0
 
-CCL_ZE_IPC_EXCHANGE=sockets torchrun --standalone --nnodes=1 --nproc-per-node $NUM_GPUS pipeline_serving.py --repo-id-or-model-path $MODEL_PATH --low-bit fp8 --max-num-seqs 4 --max-prefilled-seqs 0
+CCL_ZE_IPC_EXCHANGE=sockets torchrun --standalone --nnodes=1 --nproc-per-node $NUM_GPUS pipeline_serving.py --repo-id-or-model-path $MODEL_PATH --low-bit $LOW_BIT --max-num-seqs $MAX_NUM_SEQS --max-prefilled-seqs $MAX_PREFILLED_SEQS
