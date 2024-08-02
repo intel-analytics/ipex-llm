6df300cd92e22ec416583144a5769749a9ae3410
Heyang Sun <60865256+Uxito-Ada@users.noreply.github.com>
2024-08-02 15:07:31 +0800

    Update utils.py

--- python/llm/src/ipex_llm/transformers/utils.py
+++ python/llm/src/ipex_llm/transformers/utils.py
@@ -365,6 +365,7 @@ def is_torch_bf16_gpu_available():
 
 # Arc platfrom does not support FP64,
 # Disable FP64 in DeepSpeedZeroOptimizer_Stage3's _constant_buffered_norm2  method
+# original: https://github.com/microsoft/DeepSpeed/blob/master/deepspeed/runtime/zero/stage3.py#L1365-L1372
 def _constant_buffered_norm2(self, input, buffer_size=250000000):
     norm = None
     for part in input.view(-1).split(buffer_size):
