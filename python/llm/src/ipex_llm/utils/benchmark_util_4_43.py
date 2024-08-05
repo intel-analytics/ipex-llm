163fe2efde9e3d40600e2b904a42ed64e47e05a5
Zijie Li <michael20001122@gmail.com>
2024-08-05 16:18:19 +0800

    Update benchmark_util_4_43.py

--- python/llm/src/ipex_llm/utils/benchmark_util_4_43.py
+++ python/llm/src/ipex_llm/utils/benchmark_util_4_43.py
@@ -1,3 +1,22 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file is adapted from
+# https://github.com/huggingface/transformers/blob/v4.43.1/src/transformers/generation/utils.py
+
 # coding=utf-8
 # Copyright 2020 The Google AI Language Team Authors, Facebook AI Research authors and The HuggingFace Inc. team.
 # Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
