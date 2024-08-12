945bfb51124b34b8d8c5414d86bd358004ce4279
ATMxsp01 <shou.xu@intel.com>
2024-08-12 15:41:56 +0800

    lint

--- python/llm/src/ipex_llm/transformers/models/mistral.py
+++ python/llm/src/ipex_llm/transformers/models/mistral.py
@@ -273,7 +273,9 @@ def mistral_attention_forward_quantized(
     original_dtype = hidden_states.dtype
 
     use_fuse_rope = should_use_fuse_rope(self, hidden_states, position_ids)
-    if self.q_proj.qtype not in [FP6, FP16]: use_fuse_rope = False
+    if self.q_proj.qtype not in [FP6, FP16]: 
+        use_fuse_rope = False
+    
     enough_kv_room = is_enough_kv_cache_room_4_31(past_key_value)
     decoding_fast_path = use_decoding_fast_path(self.q_proj,
                                                 use_fuse_rope,
@@ -479,7 +481,9 @@ def mistral_attention_forward_original(
     original_dtype = hidden_states.dtype
 
     use_fuse_rope = should_use_fuse_rope(self, hidden_states, position_ids)
-    if self.q_proj.qtype not in [FP6, FP16]: use_fuse_rope = False
+    if self.q_proj.qtype not in [FP6, FP16]: 
+        use_fuse_rope = False
+
     enough_kv_room = is_enough_kv_cache_room_4_31(past_key_value)
     decoding_fast_path = use_decoding_fast_path(self.q_proj,
                                                 use_fuse_rope,
@@ -703,7 +707,9 @@ def mistral_attention_forward_4_36_quantized(
     original_dtype = hidden_states.dtype
 
     use_fuse_rope = should_use_fuse_rope(self, hidden_states, position_ids)
-    if self.q_proj.qtype not in [FP6, FP16]: use_fuse_rope = False
+    if self.q_proj.qtype not in [FP6, FP16]: 
+        use_fuse_rope = False
+
     enough_kv_room = is_enough_kv_cache_room_4_36(past_key_value, self.layer_idx,
                                                   seq_len=q_len)
     decoding_fast_path = use_decoding_fast_path(self.q_proj,
@@ -922,7 +928,9 @@ def mistral_attention_forward_4_36_original(
     use_compresskv = isinstance(past_key_value, DynamicCompressCache)
 
     use_fuse_rope = should_use_fuse_rope(self, hidden_states, position_ids)
-    if self.q_proj.qtype not in [FP6, FP16]: use_fuse_rope = False
+    if self.q_proj.qtype not in [FP6, FP16]: 
+        use_fuse_rope = False
+
     enough_kv_room = is_enough_kv_cache_room_4_36(past_key_value,
                                                   self.layer_idx,
                                                   q_len)
@@ -1181,7 +1189,9 @@ def mistral_attention_forward_4_39_original(
     use_compresskv = isinstance(past_key_value, DynamicCompressCache)
 
     use_fuse_rope = should_use_fuse_rope(self, hidden_states, position_ids)
-    if self.q_proj.qtype not in [FP6, FP16]: use_fuse_rope = False
+    if self.q_proj.qtype not in [FP6, FP16]: 
+        use_fuse_rope = False
+        
     enough_kv_room = is_enough_kv_cache_room_4_36(past_key_value, self.layer_idx,
                                                   q_len)
     decoding_fast_path = use_decoding_fast_path(self.q_proj,
