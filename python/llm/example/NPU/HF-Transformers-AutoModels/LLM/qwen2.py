d0b0f2abb4245ccca03390798d9294e360af076a
plusbang <binbin1.deng@intel.com>
2024-08-29 17:59:10 +0800

    small update

--- python/llm/example/NPU/HF-Transformers-AutoModels/LLM/qwen2.py
+++ python/llm/example/NPU/HF-Transformers-AutoModels/LLM/qwen2.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     parser.add_argument("--max-prompt-len", type=int, default=512)
     parser.add_argument("--disable-transpose-value-cache", action="store_true", default=False)
     parser.add_argument("--intra-pp", type=int, default=2)
-    parser.add_argument("--inter-pp", type=int, default=1)
+    parser.add_argument("--inter-pp", type=int, default=None)
 
     args = parser.parse_args()
     model_path = args.repo_id_or_model_path
