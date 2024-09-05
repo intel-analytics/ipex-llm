820f8a45543abdc5dfc2b288f47f7de9077a50cd
Ch1y0q <qiyue2001@gmail.com>
2024-09-05 15:31:01 +0800

    add `--lowbit-path` option for NPU llama example (#12020)
    * add option" `--lowbit-path`
    
    * add descriptions in `README.md` and formatting
    
    * Update llama.py

--- python/llm/example/NPU/HF-Transformers-AutoModels/LLM/llama.py
+++ python/llm/example/NPU/HF-Transformers-AutoModels/LLM/llama.py
@@ -50,6 +50,12 @@ if __name__ == "__main__":
         help="The huggingface repo id for the Llama2 model to be downloaded"
         ", or the path to the huggingface checkpoint folder",
     )
+    parser.add_argument("--lowbit-path", type=str,
+        default="",
+        help="The path to the lowbit model folder, leave blank if you do not want to save. \
+            If path not exists, lowbit model will be saved there. \
+            Else, lowbit model will be loaded.",
+    )
     parser.add_argument('--prompt', type=str, default="What is AI?",
                         help='Prompt to infer')
     parser.add_argument("--n-predict", type=int, default=32, help="Max tokens to predict")
@@ -62,21 +68,37 @@ if __name__ == "__main__":
     args = parser.parse_args()
     model_path = args.repo_id_or_model_path
 
-    model = AutoModelForCausalLM.from_pretrained(
-        model_path,
-        torch_dtype=torch.float16,
-        trust_remote_code=True,
-        attn_implementation="eager",
-        load_in_low_bit="sym_int4",
-        optimize_model=True,
-        max_output_len=args.max_output_len,
-        max_prompt_len=args.max_prompt_len,
-        intra_pp=args.intra_pp,
-        inter_pp=args.inter_pp,
-        transpose_value_cache=not args.disable_transpose_value_cache,
-    )
+    if not args.lowbit_path or not os.path.exists(args.lowbit_path):
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            torch_dtype=torch.float16,
+            trust_remote_code=True,
+            attn_implementation="eager",
+            load_in_low_bit="sym_int4",
+            optimize_model=True,
+            max_output_len=args.max_output_len,
+            max_prompt_len=args.max_prompt_len,
+            intra_pp=args.intra_pp,
+            inter_pp=args.inter_pp,
+            transpose_value_cache=not args.disable_transpose_value_cache,
+        )
+    else:
+        model = AutoModelForCausalLM.load_low_bit(
+            args.lowbit_path,
+            attn_implementation="eager",
+            torch_dtype=torch.float16,
+            optimize_model=True,
+            max_output_len=args.max_output_len,
+            max_prompt_len=args.max_prompt_len,
+            intra_pp=args.intra_pp,
+            inter_pp=args.inter_pp,
+            transpose_value_cache=not args.disable_transpose_value_cache,
+        )
 
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+
+    if args.lowbit_path and not os.path.exists(args.lowbit_path):
+        model.save_low_bit(args.lowbit_path)
 
     DEFAULT_SYSTEM_PROMPT = """\
     """
