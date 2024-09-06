6cedb601e4a5a0220e02c08c7bc7047ef65828a1
Yishuo Wang <yishuo.wang@intel.com>
2024-09-06 17:51:08 +0800

    remove some useless code (#12035)

--- python/llm/src/ipex_llm/transformers/models/llama.py
+++ python/llm/src/ipex_llm/transformers/models/llama.py
@@ -47,8 +47,7 @@ from ipex_llm.transformers.models.utils import init_fp8_kv_cache, append_fp8_kv_
 from ipex_llm.transformers.models.utils import is_enough_kv_cache_room_4_31, \
     apply_rotary_pos_emb, is_enough_kv_cache_room_4_36
 from ipex_llm.transformers.models.utils import apply_rotary_pos_emb_no_cache_xpu
-from ipex_llm.transformers.models.utils import use_flash_attention, use_sdp, use_sdp_fp8, \
-    use_sdp_causal
+from ipex_llm.transformers.models.utils import use_flash_attention, use_sdp, use_sdp_causal
 from ipex_llm.transformers.models.utils import mlp_fusion_check, fp16_fusion_check
 from ipex_llm.transformers.models.utils import use_decoding_fast_path, get_q_proj_or_qkv_proj
 from transformers.modeling_outputs import BaseModelOutputWithPast
@@ -599,7 +598,7 @@ def llama_attention_forward_4_31_quantized(
         kv_seq_len = key_states.shape[-2]
         past_key_value = (key_states, value_states)
 
-        if not use_sdp_fp8(q_len, key_states.shape[2], query_states):
+        if not use_sdp(q_len, key_states.shape[2], self.head_dim, query_states):
             key_states, value_states = restore_fp8_kv_cache(key_states, value_states,
                                                             query_states.dtype)
             # repeat k/v heads if n_kv_heads < n_heads
@@ -1282,7 +1281,7 @@ def llama_attention_forward_4_41_quantized(
             key_states, value_states = past_key_value.update(key_states, value_states,
                                                              self.layer_idx, cache_kwargs)
         kv_seq_len = key_states.shape[-2]
-        if not use_sdp_fp8(q_len, key_states.shape[2], query_states):
+        if not use_sdp(q_len, key_states.shape[2], self.head_dim, query_states):
             key_states, value_states = restore_fp8_kv_cache(key_states, value_states,
                                                             query_states.dtype)
             key_states = repeat_kv(key_states, self.num_key_value_groups)\
@@ -1873,7 +1872,7 @@ def llama_attention_forward_4_38_quantized(
             key_states, value_states = past_key_value.update(key_states, value_states,
                                                              self.layer_idx, cache_kwargs)
         kv_seq_len = key_states.shape[-2]
-        if not use_sdp_fp8(q_len, key_states.shape[2], query_states):
+        if not use_sdp(q_len, key_states.shape[2], self.head_dim, query_states):
             key_states, value_states = restore_fp8_kv_cache(key_states, value_states,
                                                             query_states.dtype)
             key_states = repeat_kv(key_states, self.num_key_value_groups)\
