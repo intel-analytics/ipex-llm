27d241150453bd67674bc4cdfa8643c849b229dc
Zijie Li <michael20001122@gmail.com>
2024-10-23 15:39:02 -0400

    Update save_openvino.py

--- python/llm/dev/benchmark/all-in-one/save_openvino.py
+++ python/llm/dev/benchmark/all-in-one/save_openvino.py
@@ -1,10 +1,94 @@
+#
+# Copyright 2016 The BigDL Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 from transformers import AutoTokenizer
 from optimum.intel import OVWeightQuantizationConfig
 from optimum.intel.openvino import OVModelForCausalLM
 
 import os
+import omegaconf
 from pathlib import Path
 import argparse
+
+from run import LLAMA_IDS, CHATGLM_IDS, LLAVA_IDS, PHI3VISION_IDS, QWENVL_IDS, get_model_path
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+def save_model_to_openvino(repo_id,
+                           local_model_hub,
+                           low_bit,
+                           output,
+                           group_size,
+                           modelscope,
+                           ):
+    model_path = get_model_path(repo_id, local_model_hub)
+
+    ir_model_path = Path(repo_id.split(
+        "/")[1] + '-ov') if output is None else Path(output)
+
+    if not ir_model_path.exists():
+        os.mkdir(ir_model_path)
+
+    compression_configs = {
+        "sym": True,
+        "group_size": group_size,
+        "ratio": 1.0,
+    }
+    if modelscope:
+        from modelscope import snapshot_download
+
+        print("====Downloading model from ModelScope=====")
+        model_path = snapshot_download(repo_id, cache_dir='./')
+
+    print("====Exporting IR=====")
+    if low_bit == "sym_int4":
+        compression_configs['sym'] = True
+        ov_model = OVModelForCausalLM.from_pretrained(model_path, export=True, 
+                                                      trust_remote_code=True,
+                                                      compile=False, quantization_config=OVWeightQuantizationConfig(
+                                                      bits=4, **compression_configs)).eval()
+    elif low_bit == "asym_int4":
+        compression_configs['sym'] = False
+        ov_model = OVModelForCausalLM.from_pretrained(model_path, export=True, 
+                                                      trust_remote_code=True,
+                                                      compile=False, quantization_config=OVWeightQuantizationConfig(
+                                                      bits=4, **compression_configs)).eval()
+    elif args.precision == "int8":
+        ov_model = OVModelForCausalLM.from_pretrained(model_path, export=True,
+                                                      trust_remote_code=True,
+                                                      compile=False, load_in_8bit=True).eval()
+    else:
+        ov_model = OVModelForCausalLM.from_pretrained(model_path, export=True,
+                                                      trust_remote_code=True,
+                                                      compile=False, load_in_8bit=False).eval()
+
+    print("====Saving IR=====")
+    ov_model.save_pretrained(ir_model_path)
+
+    print("====Exporting tokenizer=====")
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path)
+    tokenizer.save_pretrained(ir_model_path)
+
+    print("====Exporting IR tokenizer=====")
+    from optimum.exporters.openvino.convert import export_tokenizer
+    export_tokenizer(tokenizer, ir_model_path)
+    print("====Finished=====")
+    del ov_model
+    del model_path
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(add_help=False)
@@ -30,59 +114,30 @@ if __name__ == '__main__':
                         required=False,
                         type=str,
                         help='path to save the ir model')
+    parser.add_argument('-g',
+                        '--group_size',
+                        required=False,
+                        default=64,
+                        type=int,
+                        help='group size decided to use'
+    )
     parser.add_argument('-ms',
                         '--modelscope',
                         action='store_true',
                         help='download model from Model Scope')
     args = parser.parse_args()
 
-    ir_model_path = Path(args.model_id.split(
-        "/")[1] + '-ov') if args.output is None else Path(args.output)
+    output = args.output
+    group_size = args.group_size
+    modelscope = args.modelscope
 
-    if not ir_model_path.exists():
-        os.mkdir(ir_model_path)
+    from omegaconf import OmegaConf
+    conf = OmegaConf.load(f'{current_dir}/config.yaml')
 
-    compression_configs = {
-        "sym": True,
-        "group_size": 64,
-        "ratio": 1.0,
-    }
-    if args.modelscope:
-        from modelscope import snapshot_download
-
-        print("====Downloading model from ModelScope=====")
-        model_path = snapshot_download(args.model_id, cache_dir='./')
-    else:
-        model_path = args.model_id
-
-    print("====Exporting IR=====")
-    if args.precision == "int4":
-        ov_model = OVModelForCausalLM.from_pretrained(model_path, export=True, 
-                                                      trust_remote_code=True,
-                                                      compile=False, quantization_config=OVWeightQuantizationConfig(
-                                                      bits=4, **compression_configs))
-    elif args.precision == "int8":
-        ov_model = OVModelForCausalLM.from_pretrained(model_path, export=True,
-                                                      trust_remote_code=True,
-                                                      compile=False, load_in_8bit=True)
-    else:
-        ov_model = OVModelForCausalLM.from_pretrained(model_path, export=True,
-                                                      trust_remote_code=True,
-                                                      compile=False, load_in_8bit=False)
-
-    print("====Saving IR=====")
-    ov_model.save_pretrained(ir_model_path)
-
-    print("====Exporting tokenizer=====")
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_path)
-    tokenizer.save_pretrained(ir_model_path)
-
-    print("====Exporting IR tokenizer=====")
-    from optimum.exporters.openvino.convert import export_tokenizer
-    export_tokenizer(tokenizer, ir_model_path)
-    print("====Finished=====")
-    del ov_model
-    del model_path
-    
-    
+    for model in conf.repo_id:
+        save_model_to_openvino(repo_id=model,
+                              local_model_hub=conf['local_model_hub'],
+                              low_bit=conf['low_bit'],
+                              output=output,
+                              group_size=group_size,
+                              modelscope=modelscope)
