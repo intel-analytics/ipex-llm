e7491fd844258c8c5f31599fc3bba02296a78ec6
cyita <yitastudy@gmail.com>
2024-10-28 16:55:51 +0800

    remove run.bat

--- python/llm/example/NPU/HF-Transformers-AutoModels/LLM/Pipeline-Models/run.bat
+++
@@ -1,6 +0,0 @@
-set PYTHONPATH=D:\yina\BigDL\python\llm\src
-@REM set PYTHONPATH=
-
-set BIGDL_USE_NPU=1
-
-python llama.py --repo-id-or-model-path "D:\llm-models\Llama-2-7b-chat-hf" --quantization_group_size 128
