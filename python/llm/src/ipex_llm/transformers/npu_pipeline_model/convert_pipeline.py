bc9b8f5dcbb08ef3f664e1b77a986eba80d2a8d4
rnwang04 <ruonan1.wang@intel.com>
2024-10-28 14:26:56 +0800

    meet code review

--- python/llm/src/ipex_llm/transformers/npu_pipeline_model/convert_pipeline.py
+++ python/llm/src/ipex_llm/transformers/npu_pipeline_model/convert_pipeline.py
@@ -146,6 +146,8 @@ def generate(
         time_start = time.perf_counter()
 
         bdata = str.encode(str(temp_dir))
+        invalidInputError(len(bdata) <= 2000,
+                          f"Input directory is too long ({len(bdata)}), which may cause read error.")
         input_pipe.write(bdata)
         input_pipe.flush()
 
