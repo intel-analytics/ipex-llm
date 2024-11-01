d409d9d0eb978a4ced7c534ea24e65b1a76d6673
binbin Deng <108676127+plusbang@users.noreply.github.com>
2024-11-01 15:38:10 +0800

    [NPU L0] Update streaming mode of example (#12312)

--- python/llm/example/NPU/HF-Transformers-AutoModels/LLM/Pipeline-Models/minicpm.py
+++ python/llm/example/NPU/HF-Transformers-AutoModels/LLM/Pipeline-Models/minicpm.py
@@ -19,7 +19,7 @@ import torch
 import time
 import argparse
 from ipex_llm.transformers.npu_model import AutoModelForCausalLM
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, TextStreamer
 from transformers.utils import logging
 import os
 
@@ -48,6 +48,7 @@ if __name__ == "__main__":
     parser.add_argument("--max-context-len", type=int, default=1024)
     parser.add_argument("--max-prompt-len", type=int, default=512)
     parser.add_argument("--disable-transpose-value-cache", action="store_true", default=False)
+    parser.add_argument("--disable-streaming", action="store_true", default=False)
 
     args = parser.parse_args()
     model_path = args.repo_id_or_model_path
@@ -79,26 +80,31 @@ if __name__ == "__main__":
     if args.lowbit_path and not os.path.exists(args.lowbit_path):
         model.save_low_bit(args.lowbit_path)
 
+    if args.disable_streaming:
+        streamer = None
+    else:
+        streamer = TextStreamer(tokenizer=tokenizer, skip_special_tokens=True)
+
     print("-" * 80)
     print("done")
     with torch.inference_mode():
         print("finish to load")
-        for i in range(5):
+        for i in range(3):
             prompt = "<用户>{}<AI>".format(args.prompt)
             _input_ids = tokenizer.encode(prompt, return_tensors="pt")
+            print("-" * 20, "Input", "-" * 20)
             print("input length:", len(_input_ids[0]))
+            print(prompt)
+            print("-" * 20, "Output", "-" * 20)
             st = time.time()
             output = model.generate(
-                _input_ids, max_new_tokens=args.n_predict, do_print=True
+                _input_ids, max_new_tokens=args.n_predict, streamer=streamer
             )
             end = time.time()
+            if args.disable_streaming:
+                output_str = tokenizer.decode(output[0], skip_special_tokens=False)
+                print(output_str)
             print(f"Inference time: {end-st} s")
-            input_str = tokenizer.decode(_input_ids[0], skip_special_tokens=False)
-            print("-" * 20, "Input", "-" * 20)
-            print(input_str)
-            output_str = tokenizer.decode(output[0], skip_special_tokens=False)
-            print("-" * 20, "Output", "-" * 20)
-            print(output_str)
 
     print("-" * 80)
     print("done")
--- python/llm/example/NPU/HF-Transformers-AutoModels/LLM/Pipeline-Models/qwen.py
+++ python/llm/example/NPU/HF-Transformers-AutoModels/LLM/Pipeline-Models/qwen.py
@@ -20,7 +20,7 @@ import torch
 import time
 import argparse
 from ipex_llm.transformers.npu_model import AutoModelForCausalLM
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, TextStreamer
 from transformers.utils import logging
 
 logger = logging.get_logger(__name__)
@@ -50,6 +50,7 @@ if __name__ == "__main__":
     parser.add_argument('--load_in_low_bit', type=str, default="sym_int4",
                         help='Load in low bit to use')
     parser.add_argument("--disable-transpose-value-cache", action="store_true", default=False)
+    parser.add_argument("--disable-streaming", action="store_true", default=False)
 
     args = parser.parse_args()
     model_path = args.repo_id_or_model_path
@@ -81,6 +82,11 @@ if __name__ == "__main__":
     if args.lowbit_path and not os.path.exists(args.lowbit_path):
         model.save_low_bit(args.lowbit_path)
 
+    if args.disable_streaming:
+        streamer = None
+    else:
+        streamer = TextStreamer(tokenizer=tokenizer, skip_special_tokens=True)
+
     print("-" * 80)
     print("done")
     messages = [{"role": "system", "content": "You are a helpful assistant."},
@@ -90,21 +96,21 @@ if __name__ == "__main__":
                                          add_generation_prompt=True)
     with torch.inference_mode():
         print("finish to load")
-        for i in range(5):
+        for i in range(3):
             _input_ids = tokenizer([text], return_tensors="pt").input_ids
+            print("-" * 20, "Input", "-" * 20)
             print("input length:", len(_input_ids[0]))
+            print(text)
+            print("-" * 20, "Output", "-" * 20)
             st = time.time()
             output = model.generate(
-                _input_ids, max_new_tokens=args.n_predict, do_print=True
+                _input_ids, max_new_tokens=args.n_predict, streamer=streamer
             )
             end = time.time()
+            if args.disable_streaming:
+                output_str = tokenizer.decode(output[0], skip_special_tokens=False)
+                print(output_str)
             print(f"Inference time: {end-st} s")
-            input_str = tokenizer.decode(_input_ids[0], skip_special_tokens=False)
-            print("-" * 20, "Input", "-" * 20)
-            print(input_str)
-            output_str = tokenizer.decode(output[0], skip_special_tokens=False)
-            print("-" * 20, "Output", "-" * 20)
-            print(output_str)
 
     print("-" * 80)
     print("done")
