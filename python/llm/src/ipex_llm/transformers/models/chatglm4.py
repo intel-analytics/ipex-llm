3e28340de2ae4a529e75333bae1070a3f0d045cd
Yuwen Hu <yuwen.hu@intel.com>
2024-11-07 16:52:49 +0800

    Add fused mlp to glm4 models

--- python/llm/src/ipex_llm/transformers/models/chatglm4.py
+++ python/llm/src/ipex_llm/transformers/models/chatglm4.py
@@ -56,7 +56,7 @@ def chatglm4_model_forward(
     if use_cache:
         inputs = input_ids if input_ids is not None else inputs_embeds
         use_compress_kv = should_use_compresskv(inputs, inputs.shape[1])
-        use_quantize_kv = use_quantize_kv_cache(self.encoder.layers[0].mlp.dense_h_to_4h,
+        use_quantize_kv = use_quantize_kv_cache(self.encoder.layers[0].mlp.gate_proj,
                                                 inputs)
         if use_compress_kv and not isinstance(past_key_values,
                                               DynamicCompressCache):
