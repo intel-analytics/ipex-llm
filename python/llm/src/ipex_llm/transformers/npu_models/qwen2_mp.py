6bf5a8c230a8e5904e3bdf19b6d106e8b866467d
Ruonan Wang <ruonan1.wang@intel.com>
2024-11-12 16:59:44 +0800

    [NPU] Update qwen2 compile config (#12383)
    * update
    
    * fix

--- python/llm/src/ipex_llm/transformers/npu_models/qwen2_mp.py
+++ python/llm/src/ipex_llm/transformers/npu_models/qwen2_mp.py
@@ -234,7 +234,7 @@ class LowBitQwenMultiDecoderlayer(LLMBaseNNFactory):
         print(f"{mode} start compiling")
         if (
             group_size != 0
-            and (mode == "prefill" or num_layers == 2)
+            and (mode == "prefill" or num_layers == 2 or num_layers == 3)
             and os.environ.get("IPEX_LLM_NPU_DISABLE_COMPILE_OPT", "0") != "1"
         ):
             self.compile(npu_dpu_groups=6)
