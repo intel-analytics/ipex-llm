60832fe93dfca75b019647308de6a16d3918f7bb
rnwang04 <ruonan1.wang@intel.com>
2024-11-27 16:20:02 +0800

    fix style

--- python/llm/src/ipex_llm/transformers/npu_pipeline_model/convert_pipeline.py
+++ python/llm/src/ipex_llm/transformers/npu_pipeline_model/convert_pipeline.py
@@ -459,8 +459,8 @@ def convert_llm_for_deploy(model: torch.nn.Module,
         if not use_level_zero:
             # save fused_layers blobs of fused decoder layers
             convert_fused_qwen_layer(model, fused_layers, n_splits_linear, n_splits_down_proj,
-                                    save_directory, weight_dir, transpose_value_cache, kv_len,
-                                    group_size, layernorm_const, "decode")
+                                     save_directory, weight_dir, transpose_value_cache, kv_len,
+                                     group_size, layernorm_const, "decode")
         else:
             # save layer_num blobs of each decoder layer
             layer_num = len(model.model.layers)
