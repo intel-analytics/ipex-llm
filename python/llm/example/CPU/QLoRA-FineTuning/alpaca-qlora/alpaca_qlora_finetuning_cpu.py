c96400c27299540e876075ea6a8ca9e9cde1ae78
Ariadne <wyn2000330@126.com>
2024-11-28 13:12:57 +0800

    remove nf4 unsupport comment in cpu finetuning

--- python/llm/example/CPU/QLoRA-FineTuning/alpaca-qlora/alpaca_qlora_finetuning_cpu.py
+++ python/llm/example/CPU/QLoRA-FineTuning/alpaca-qlora/alpaca_qlora_finetuning_cpu.py
@@ -181,7 +181,7 @@ def train(
         bnb_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_use_double_quant=False,
-            bnb_4bit_quant_type="int4",  # nf4 not supported on cpu yet
+            bnb_4bit_quant_type="int4",
             bnb_4bit_compute_dtype=torch.bfloat16
         )
         model = AutoModelForCausalLM.from_pretrained(base_model,
