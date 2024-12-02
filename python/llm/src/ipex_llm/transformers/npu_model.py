a325d3b1b3bd5d7467dc7917515539bb317ac9da
jinbridge <2635480475@qq.com>
2024-12-02 16:24:20 +0800

    Fix style check

--- python/llm/src/ipex_llm/transformers/npu_model.py
+++ python/llm/src/ipex_llm/transformers/npu_model.py
@@ -276,7 +276,8 @@ class _BaseAutoModelClass:
         model.share_memory()
 
         if not pipeline:
-            if not hasattr(model, 'llm') and model.config.model_type in ["qwen2", "llama", "minicpm"]:
+            if (not hasattr(model, 'llm') and
+                    model.config.model_type in ["qwen2", "llama", "minicpm"]):
                 from ipex_llm.transformers.npu_models.convert import optimize_llm_single_process
                 optimize_llm_single_process(
                     llm,
