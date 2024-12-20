51ff9ebd8a07c5f94e1b78928e5710666ea690e7
Shaojun Liu <61072813+liu-shaojun@users.noreply.github.com>
2024-12-20 09:29:16 +0800

    Upgrade oneccl version to 0.0.6.3 (#12560)
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update start-vllm-service.sh

--- docker/llm/serving/xpu/docker/start-vllm-service.sh
+++ docker/llm/serving/xpu/docker/start-vllm-service.sh
@@ -3,6 +3,7 @@ model="YOUR_MODEL_PATH"
 served_model_name="YOUR_MODEL_NAME"
 
 export CCL_WORKER_COUNT=2
+export SYCL_CACHE_PERSISTENT=1
 export FI_PROVIDER=shm
 export CCL_ATL_TRANSPORT=ofi
 export CCL_ZE_IPC_EXCHANGE=sockets
@@ -11,6 +12,9 @@ export CCL_ATL_SHM=1
 export USE_XETLA=OFF
 export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=2
 export TORCH_LLM_ALLREDUCE=0
+
+export CCL_SAME_STREAM=1
+export CCL_BLOCKING_WAIT=0
  
 source /opt/intel/1ccl-wks/setvars.sh
 
@@ -20,14 +24,14 @@ python -m ipex_llm.vllm.xpu.entrypoints.openai.api_server \
   --model $model \
   --trust-remote-code \
   --block-size 8 \
-  --gpu-memory-utilization 0.9 \
+  --gpu-memory-utilization 0.95 \
   --device xpu \
   --dtype float16 \
   --enforce-eager \
   --load-in-low-bit fp8 \
   --max-model-len 2048 \
   --max-num-batched-tokens 4000 \
-  --max-num-seqs 12 \
+  --max-num-seqs 256 \
   --tensor-parallel-size 1 \
   --disable-async-output-proc \
   --distributed-executor-backend ray
