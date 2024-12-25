c51b10373c49eabb6fee374c4eead45b4a340ec9
Yishuo Wang <yishuo.wang@intel.com>
2024-12-25 16:08:57 +0800

    fix styl

--- python/llm/src/ipex_llm/transformers/models/llama.py
+++ python/llm/src/ipex_llm/transformers/models/llama.py
@@ -163,7 +163,8 @@ def llama_attention_forward(
                 cos, sin = self.rotary_emb(value_states, position_ids)
         else:
             cos, sin = position_embeddings
-        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states,
+                                                        cos, sin, position_ids)
 
     if past_key_value is not None:
         # [CompressKV]
