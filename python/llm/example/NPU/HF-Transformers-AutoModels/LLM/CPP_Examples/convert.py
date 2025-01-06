60095344ddbd8381f41c8acf4e0351aff2d07d01
Yuwen Hu <yuwen.hu@intel.com>
2025-01-06 17:56:42 +0800

    Small fix

--- python/llm/example/NPU/HF-Transformers-AutoModels/LLM/CPP_Examples/convert.py
+++ python/llm/example/NPU/HF-Transformers-AutoModels/LLM/CPP_Examples/convert.py
@@ -36,9 +36,9 @@ if __name__ == "__main__":
     parser.add_argument(
         "--repo-id-or-model-path",
         type=str,
-        default="Qwen/Qwen2.5-7B-Instruct",  # Or Qwen2-7B-Instruct, Qwen2-1.5B-Instruct
-        help="The huggingface repo id for the Qwen model to be downloaded"
-        ", or the path to the huggingface checkpoint folder",
+        default="meta-llama/Llama-2-7b-chat-hf",
+        help="The huggingface repo id for the model to be downloaded"
+        ", or the path to the huggingface checkpoint folder.",
     )
     parser.add_argument("--save-directory", type=str,
         required=True,
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     parser.add_argument("--max-prompt-len", type=int, default=960)
     parser.add_argument("--quantization-group-size", type=int, default=0)
     parser.add_argument('--low-bit', type=str, default="sym_int4",
-                        help='Low bit precision to quantize the model')
+                        help='Low bit precision to quantize the model.')
 
     args = parser.parse_args()
     model_path = args.repo_id_or_model_path
