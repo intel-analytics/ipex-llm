0e74a1392d50cafc67f1cb2ce488da1264d283c7
Yuwen Hu <yuwen.hu@intel.com>
2025-01-23 13:37:11 +0800

    Add chat example

--- python/llm/example/GPU/HuggingFace/Multimodal/MiniCPM-o-2_6/chat.py
+++ python/llm/example/GPU/HuggingFace/Multimodal/MiniCPM-o-2_6/chat.py
@@ -27,46 +27,35 @@ from transformers import AutoTokenizer
 from ipex_llm.transformers import AutoModel
 
 
-# The video chunk function is adpated from https://huggingface.co/openbmb/MiniCPM-o-2_6#chat-inference
-def get_video_chunk_content(video_path, temp_audio_name, flatten=True):
-    video = VideoFileClip(video_path)
-    print('video_duration:', video.duration)
-    
-    with open(temp_audio_name, 'wb') as temp_audio_file:
-        temp_audio_file_path = temp_audio_file.name
-        video.audio.write_audiofile(temp_audio_file_path, codec="pcm_s16le", fps=16000)
-        audio_np, sr = librosa.load(temp_audio_file_path, sr=16000, mono=True)
-    num_units = math.ceil(video.duration)
-    
-    # 1 frame + 1s audio chunk
-    contents= []
-    for i in range(num_units):
-        frame = video.get_frame(i+1)
-        image = Image.fromarray((frame).astype(np.uint8))
-        audio = audio_np[sr*i:sr*(i+1)]
-        if flatten:
-            contents.extend(["<unit>", image, audio])
-        else:
-            contents.append(["<unit>", image, audio])
-    
-    return contents
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Chat with MiniCPM-o-2_6 in Omni mode')
     parser.add_argument('--repo-id-or-model-path', type=str,
                         help='The Hugging Face or ModelScope repo id for the MiniCPM-o-2_6 model to be downloaded'
                              ', or the path to the checkpoint folder')
-    parser.add_argument('--video-path', type=str, required=True,
-                        help='The path to the video, which the model uses to conduct inference '
-                             'based on its images and audio.')
+    parser.add_argument('--image-path', type=str,
+                        help='The path to the image for inference.')
+    parser.add_argument('--audio-path', type=str,
+                        help='The path to the audio for inference.')
+    parser.add_argument('--prompt', type=str,
+                        help='Prompt for inference.')
     parser.add_argument('--n-predict', type=int, default=32,
                         help='Max tokens to predict')
     
     args = parser.parse_args()
 
     model_path = args.repo_id_or_model_path
-    video_path = args.video_path
+    image_path = args.image_path
+    audio_path = args.audio_path
+
+    modules_to_not_convert = []
+    init_vision = False
+    init_audio = False
+    if image_path is not None and os.path.exists(image_path):
+        init_vision = True
+        modules_to_not_convert += ["vpm", "resampler"]
+    if audio_path is not None and os.path.exists(audio_path):
+        init_audio = True
+        modules_to_not_convert += ["apm"]
 
     # Load model in 4 bit,
     # which convert the relevant layers in the model into INT4 format
@@ -76,10 +65,10 @@ if __name__ == '__main__':
                                       trust_remote_code=True,
                                       attn_implementation='sdpa',
                                       use_cache=True,
-                                      init_vision=True,
-                                      init_audio=True,
+                                      init_vision=init_vision,
+                                      init_audio=init_audio,
                                       init_tts=False,
-                                      modules_to_not_convert=["apm", "vpm", "resampler"])
+                                      modules_to_not_convert=modules_to_not_convert)
     
     model = model.half().to('xpu')
 
@@ -87,13 +76,20 @@ if __name__ == '__main__':
                                               trust_remote_code=True)
     
 
-    # The following code for generation is adapted from https://huggingface.co/Qwen/Qwen2-VL-7B-Instruct#quickstart
-    temp_audio_name = "temp_audio.wav"
-    contents = get_video_chunk_content(video_path, temp_audio_name)
-    messages = [{"role":"user", "content": contents}]
+    # The following code for generation is adapted from 
+    # https://huggingface.co/openbmb/MiniCPM-o-2_6#addressing-various-audio-understanding-tasks and
+    # https://huggingface.co/openbmb/MiniCPM-o-2_6#chat-with-single-image
+    content = []
+    if init_vision:
+        image_input = Image.open(image_path).convert('RGB')
+        content.append(image_input)
+    if args.prompt is not None:
+        content.append(args.prompt)
+    if init_audio:
+        audio_input, _ = librosa.load(audio_path, sr=16000, mono=True)
+        content.append(audio_input)
+    messages = [{'role': 'user', 'content': content}]
 
-    if os.path.exists(temp_audio_name):
-        os.remove(temp_audio_name)
 
     with torch.inference_mode():
         # ipex_llm model needs a warmup, then inference time can be accurate
@@ -101,13 +97,7 @@ if __name__ == '__main__':
             msgs=messages,
             tokenizer=tokenizer,
             sampling=True,
-            temperature=0.5,
             max_new_tokens=args.n_predict,
-            omni_input=True, # need to set omni_input=True when omni inference
-            use_tts_template=False,
-            generate_audio=False,
-            max_slice_nums=1,
-            use_image_id=False,
         )
 
         st = time.time()
@@ -115,19 +105,18 @@ if __name__ == '__main__':
             msgs=messages,
             tokenizer=tokenizer,
             sampling=True,
-            temperature=0.5,
             max_new_tokens=args.n_predict,
-            omni_input=True, # need to set omni_input=True when omni inference
-            use_tts_template=False,
-            generate_audio=False,
-            max_slice_nums=1,
-            use_image_id=False,
         )
         torch.xpu.synchronize()
         end = time.time()
 
     print(f'Inference time: {end-st} s')
-    print('-'*20, 'Input Video Path', '-'*20)
-    print(video_path)
+    print('-'*20, 'Input Image Path', '-'*20)
+    print(image_path)
+    print('-'*20, 'Input Audio Path', '-'*20)
+    print(audio_path)
+    print('-'*20, 'Input Prompt', '-'*20)
+    print(args.prompt)
     print('-'*20, 'Chat Output', '-'*20)
     print(response)
+
