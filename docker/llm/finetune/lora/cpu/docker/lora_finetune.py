c02e6b4f62ce1c17df0a4443363cf208197aa0d5
Danciu Georgian <62398759+DreadPirate09@users.noreply.github.com>
2025-02-20 14:13:17 +0200

    Simplify EOS token handling in tokenize function
    Let the tokenizer handle EOS token

--- docker/llm/finetune/lora/cpu/docker/lora_finetune.py
+++ docker/llm/finetune/lora/cpu/docker/lora_finetune.py
@@ -137,26 +137,18 @@ def train(
     tokenizer.padding_side = "left"  # Allow batched inference
 
     def tokenize(prompt, add_eos_token=True):
-        # there's probably a way to do this with the tokenizer settings
-        # but again, gotta move fast
         result = tokenizer(
             prompt,
             truncation=True,
             max_length=cutoff_len,
             padding=False,
             return_tensors=None,
-        )
-        if (
-            result["input_ids"][-1] != tokenizer.eos_token_id
-            and len(result["input_ids"]) < cutoff_len
-            and add_eos_token
-        ):
-            result["input_ids"].append(tokenizer.eos_token_id)
-            result["attention_mask"].append(1)
+            add_special_tokens=add_eos_token,
+    )
 
-        result["labels"] = result["input_ids"].copy()
+    result["labels"] = result["input_ids"].copy()
 
-        return result
+    return result
 
     def generate_and_tokenize_prompt(data_point):
         full_prompt = generate_prompt(data_point)
