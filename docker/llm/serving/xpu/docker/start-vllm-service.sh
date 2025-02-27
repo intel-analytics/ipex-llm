c0fd7d5b2055039b8997f94d710ce6047a9a987a
Shaojun Liu <61072813+liu-shaojun@users.noreply.github.com>
2025-02-27 15:44:14 +0800

    Update start-vllm-service.sh

--- docker/llm/serving/xpu/docker/start-vllm-service.sh
+++ docker/llm/serving/xpu/docker/start-vllm-service.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-model="YOUR_MODEL_PATH"
-served_model_name="YOUR_MODEL_NAME"
+MODEL_PATH=${MODEL_PATH:-"default_model_path"}
+SERVED_MODEL_NAME=${SERVED_MODEL_NAME:-"default_model_name"}
+
+echo "Starting service with model: $MODEL_PATH"
+echo "Served model name: $SERVED_MODEL_NAME"
 
 export CCL_WORKER_COUNT=2
 export SYCL_CACHE_PERSISTENT=1
@@ -19,9 +22,9 @@ export CCL_BLOCKING_WAIT=0
 source /opt/intel/1ccl-wks/setvars.sh
 
 python -m ipex_llm.vllm.xpu.entrypoints.openai.api_server \
-  --served-model-name $served_model_name \
+  --served-model-name $SERVED_MODEL_NAME \
   --port 8000 \
-  --model $model \
+  --model $MODEL_PATH \
   --trust-remote-code \
   --block-size 8 \
   --gpu-memory-utilization 0.95 \
@@ -29,8 +32,8 @@ python -m ipex_llm.vllm.xpu.entrypoints.openai.api_server \
   --dtype float16 \
   --enforce-eager \
   --load-in-low-bit fp8 \
-  --max-model-len 2048 \
-  --max-num-batched-tokens 4000 \
+  --max-model-len 2000 \
+  --max-num-batched-tokens 3000 \
   --max-num-seqs 256 \
   --tensor-parallel-size 1 \
   --disable-async-output-proc \
