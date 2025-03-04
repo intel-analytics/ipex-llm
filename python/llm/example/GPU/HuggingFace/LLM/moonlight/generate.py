fad5b6a6acf96c5ecc1d8136d073a429ad5e8b29
Yuwen Hu <yuwen.hu@intel.com>
2025-03-04 17:27:58 +0800

    Small fix

--- python/llm/example/GPU/HuggingFace/LLM/moonlight/generate.py
+++ python/llm/example/GPU/HuggingFace/LLM/moonlight/generate.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
                                                  optimize_model=True,
                                                  trust_remote_code=True,
                                                  use_cache=True)
-    model = model.half().to('xpu')
+    model = model.to('xpu')
 
     # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained(converted_model_path, trust_remote_code=True)
